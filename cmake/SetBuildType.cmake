b9a503b467ed4d020a04feaa2ff3de009276b8e6
Lawrence Kidder <kidder@astro.cornell.edu>
2017-04-20 16:43:21 -0400

    Set default build type to Debug

---
+++ cmake/SetBuildType.cmake
@@ -0,0 +1,19 @@
+
+# Distributed under the University of Illinois/NCSA Open Source License.
+# See LICENSE.txt for details.
+
+set(CMAKE_BUILD_TYPES "Debug" "Release" "RelWithDebInfo" "MinSizeRel")
+
+if (NOT CMAKE_BUILD_TYPE)
+  message(STATUS "CMAKE_BUILD_TYPE not specified, setting to 'Debug'")
+  set(CMAKE_BUILD_TYPE Debug
+    CACHE STRING "Choose the type of build: ${CMAKE_BUILD_TYPES}"
+    FORCE)
+else()
+  if(NOT ${CMAKE_BUILD_TYPE} IN_LIST CMAKE_BUILD_TYPES)
+    message(FATAL_ERROR "\n"
+      "Invalid value for CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}\n"
+      "Valid values: ${CMAKE_BUILD_TYPES}\n")
+  endif()
+  message(STATUS "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
+endif()
