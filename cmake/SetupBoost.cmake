83a617a7af5a03bc4fe1244e200a151f6ed58e44
Nils Deppe <nd357@cornell.edu>
2017-05-16 11:23:17 -0400

    Have CMake find Boost

---
+++ cmake/SetupBoost.cmake
@@ -0,0 +1,7 @@
+
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+find_package(Boost REQUIRED)
+include_directories(${Boost_INCLUDE_DIR})
+message(STATUS "Boost include: ${Boost_INCLUDE_DIR}")
