27714e1d845ec3cf9a21075b2006b628c55af356
Nils Deppe <nd357@cornell.edu>
2017-05-17 16:28:07 -0400

    Check compiler version

---
+++ cmake/CheckCompilerVersion.cmake
@@ -0,0 +1,23 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.2)
+    message(FATAL_ERROR "GCC version must be at least 5.2")
+  endif ()
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5)
+    message(FATAL_ERROR "Clang version must be at least 3.5")
+  endif ()
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+  message(FATAL_ERROR "Intel compiler is not supported.")
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
+  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
+    message(FATAL_ERROR "AppleClang version must be at least 6.0")
+  endif ()
+else ()
+  message(
+      WARNING "The compiler ${CMAKE_CXX_COMPILER_ID} is unsupported compiler! "
+      "Compilation has only been tested with Clang, and GCC."
+  )
+endif ()
