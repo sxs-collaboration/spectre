d4b4782c85d72245284f3d1bbf00c9110fce6478
Nils Deppe <nd357@cornell.edu>
2017-05-17 12:29:41 -0400

    Add support for listing all make targets

---
+++ cmake/SetupListTargets.cmake
@@ -0,0 +1,16 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+add_custom_target(
+    list-targets
+    COMMAND
+    ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target help
+    | grep -v " depend$"
+    | grep -v " edit_cache$"
+    | grep -v " module_"
+    VERBATIM
+)
+
+add_custom_target(targets COMMAND DEPENDS list_targets)
+
+add_custom_target(list COMMAND DEPENDS list_targets)
