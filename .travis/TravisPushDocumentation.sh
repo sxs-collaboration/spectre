fa5b7ce3086ba67b12e7563ff3a428286fd11266
Nils Deppe <nd357@cornell.edu>
2017-05-18 15:36:37 -0400

    Add containers directory and travis docker

---
+++ .travis/TravisPushDocumentation.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+# This script is called from Dockerfile.travis, which is where the various
+# environment variables are defined, or received from TravisCI.
+
+# Add executables for SonarSQUBE to PATH
+export PATH=$PATH:/work/sonarqube/sonar-scanner-2.8/bin
+export PATH=$PATH:/work/sonarqube/build-wrapper-linux-x86
+
+# Setup lmod and spack to load dependencies
+. /etc/profile.d/lmod.sh
+export PATH=$PATH:/work/spack/bin
+. /work/spack/share/spack/setup-env.sh
+spack load blaze
+spack load brigand
+spack load catch
+
+# We use cron jobs to deploy to gh-pages. Since this still runs all jobs we
+# only actually build documentation for one job but let the others run tests.
+if [ ${CC} = gcc ] \
+&& [ ${TRAVIS_EVENT_TYPE} = cron ] \
+&& [ ${TRAVIS_SECURE_ENV_VARS} = true ] \
+&& [ ${COVERAGE} ] \
+&& [ ${TRAVIS_BRANCH} = ${GH_PAGES_SOURCE_BRANCH} ] \
+&& [ ${TRAVIS_PULL_REQUEST} == false ]; then
+  cd /work/gh-pages
+  git fetch origin ${GH_PAGES_SOURCE_BRANCH}
+  git checkout ${GH_PAGES_SOURCE_BRANCH}
+  SHA=`git rev-parse --verify HEAD`
+
+  git fetch origin gh-pages && git checkout gh-pages
+
+  cp -r /work/build_${BUILD_TYPE}_${CC}/docs/html/* /work/gh-pages/
+
+  if git diff --quiet; then
+    echo "No changes to the documentation."
+  else
+    git add --all
+    git config --global user.name "Automatic Deployment (Travis CI)"
+    git config --global user.email "none@none.none"
+    git commit -m "Documentation Update: ${SHA}"
+    git push $GH_PAGES_REPO gh-pages:gh-pages
+  fi
+  rm /root/.ssh/id_rsa
+fi
