4d3a86f94aca6248b8415b4b26e86ce294f04167
Nils Deppe <nd357@cornell.edu>
2017-05-17 23:37:48 -0400

    Add PAPI support

---
+++ cmake/SetupPapi.cmake
@@ -0,0 +1,14 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+if(NOT USE_PAPI)
+  option(USE_PAPI "Search for and include PAPI" OFF)
+endif()
+
+# Do not set up PAPI again if it was already done once
+if(USE_PAPI AND NOT PAPI_FOUND)
+  find_package(PAPI REQUIRED)
+  include_directories(SYSTEM ${PAPI_INCLUDE_DIRS})
+  list(APPEND SPECTRE_LIBRARIES ${PAPI_LIBRARIES})
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}  -DSPECTRE_USE_PAPI")
+endif()
