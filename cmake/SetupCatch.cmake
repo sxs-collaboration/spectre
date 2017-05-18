1dc41224facd3d4a2678acdb13ec53eca49a0adc
Nils Deppe <nd357@cornell.edu>
2017-05-18 10:42:21 -0400

    Find Catch

---
+++ cmake/SetupCatch.cmake
@@ -0,0 +1,7 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+find_package(Catch REQUIRED)
+
+include_directories(SYSTEM "${CATCH_INCLUDE_DIR}")
+message(STATUS "Catch include: ${CATCH_INCLUDE_DIR}")
