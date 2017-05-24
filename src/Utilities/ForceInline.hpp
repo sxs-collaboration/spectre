2d3a5e1dd876122102b6e87ffd5f40b06f9d6dfe
Lawrence Kidder <kidder@astro.cornell.edu>
2017-05-24 16:20:51 -0400

    Add function to compute an integral power of 2
    Also add macro to force inlining.

---
+++ src/Utilities/ForceInline.hpp
@@ -0,0 +1,17 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines macro to always inline a function.
+
+#pragma once
+
+#if defined(__GNUC__)
+/// \ingroup Utilities
+/// Always inline a function. Only use this if you benchmarked the code.
+#define SPECTRE_ALWAYS_INLINE __attribute__((always_inline)) inline
+#else
+/// \ingroup Utilities
+/// Always inline a function. Only use this if you benchmarked the code.
+#define SPECTRE_ALWAYS_INLINE inline
+#endif
