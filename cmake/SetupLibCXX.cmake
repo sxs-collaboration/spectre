9213945d126bdfb1479d2ab61335dc2560af186a
Nils Deppe <nd357@cornell.edu>
2017-06-01 13:44:50 -0400

    Add optional libc++ debugging flag

--- cmake/SetupLibCXX.cmake
+++ cmake/SetupLibCXX.cmake
@@ -11,4 +11,13 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   set(CMAKE_CXX_FLAGS "-stdlib=libc++ ${CMAKE_CXX_FLAGS}")
   message(STATUS "libc++ include: ${LIBCXX_INCLUDE_DIRS}")
   message(STATUS "libc++ libraries: ${LIBCXX_LIBRARIES}")
+
+  option(
+      LIBCXX_DEBUG
+      "Enable debug mode for libc++ in Debug builds"
+      OFF
+  )
+  if(LIBCXX_DEBUG)
+    set(CMAKE_CXX_FLAGS_DEBUG  "${CMAKE_CXX_FLAGS_DEBUG} -D_LIBCPP_DEBUG=0")
+  endif()
 endif()
