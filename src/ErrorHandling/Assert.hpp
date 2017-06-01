9b272c436cad78c25385c25f09aa0843c04b94bd
Nils Deppe <nd357@cornell.edu>
2017-06-01 14:35:16 -0400

    Add ERROR and ASSERT macros, and required testing infrastructure

---
+++ src/ErrorHandling/Assert.hpp
@@ -0,0 +1,85 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines macro ASSERT.
+
+#pragma once
+
+#include <sstream>
+#include <string>
+
+#include "ErrorHandling/AbortWithErrorMessage.hpp"
+#include "Parallel/Abort.hpp"
+#include "Utilities/Literals.hpp"
+
+/*!
+ * \ingroup ErrorHandling
+ * \brief Assert that an expression should be true.
+ *
+ * If the preprocessor macro SPECTRE_DEBUG is defined and the expression is
+ * false, an error message is printed to the standard error stream, and the
+ * program aborts. ASSERT should be used to catch coding errors as it does
+ * nothing in production code.
+ * \param a the expression that must be true
+ * \param m the error message as an ostream
+ */
+#ifdef SPECTRE_DEBUG
+// isocpp.org recommends using an `if (true)` instead of a `do
+// while(false)` for macros because the latter can mess with inlining
+// in some (old?) compilers:
+// https://isocpp.org/wiki/faq/misc-technical-issues#macros-with-multi-stmts
+// https://isocpp.org/wiki/faq/misc-technical-issues#macros-with-if
+// However, Intel's reachability analyzer (as of version 16.0.3
+// 20160415) can't figure out that the else branch and everything
+// after it is unreachable, causing warnings (and possibly suboptimal
+// code generation).
+#define ASSERT(a, m)                                                \
+  do {                                                              \
+    if (!(a)) {                                                     \
+      std::ostringstream avoid_name_collisions_ASSERT;              \
+      avoid_name_collisions_ASSERT << m; /* NOLINT */               \
+      abort_with_error_message(#a, __FILE__, __LINE__,              \
+                               avoid_name_collisions_ASSERT.str()); \
+    }                                                               \
+  } while (false)
+#else
+#define ASSERT(a, m)                                   \
+  do {                                                 \
+    if (false) {                                       \
+      static_cast<void>(a);                            \
+      std::ostringstream avoid_name_collisions_ASSERT; \
+      avoid_name_collisions_ASSERT << m;               \
+      static_cast<void>(avoid_name_collisions_ASSERT); \
+    }                                                  \
+  } while (false)
+#endif
+
+// If SPECTRE_DEBUG is not defined, a void cast is used to avoid an unused
+// variable compiler warning when a passed parameter is only used in an ASSERT.
+// This is inside an if(false) statement so that the code never actually gets
+// executed and only placates the compiler.
+
+/*!
+ * \ingroup ErrorHandling
+ * \brief Assert that an expression should be true.
+ *
+ * Just like ASSERT and so the same guidelines apply. However, because
+ * it does not use std::stringstream it can be used in some constexpr
+ * functions where ASSERT cannot be.
+ * \param a the expression that must be satisfied
+ * \param m a std::string holding the error message
+ */
+#ifdef SPECTRE_DEBUG
+#define CASSERT(a, m)                                                         \
+  do {                                                                        \
+    if (!(a)) {                                                               \
+      Parallel::abort("\n############ ASSERT FAILED ############\nLine: "s +  \
+                      std::to_string(__LINE__) + " of file '"s + __FILE__ +   \
+                      "'\n"s + "Condition: "s + #a + "\n"s + m + /* NOLINT */ \
+                      "\n#######################################\n"s);        \
+    }                                                                         \
+  } while (false)
+#else
+#define CASSERT(a, m)
+#endif
---
+++ src/Parallel/Serialize.hpp
@@ -0,0 +1,54 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines the serialize and deserialize functions.
+
+#pragma once
+
+#include <vector>
+
+#include <pup.h>
+
+/*!
+ * \ingroup Parallel
+ * \brief Serialize an object using PUP.
+ *
+ * The type to serialize as must be explicitly specified.  We require
+ * this because a mismatch between the serialize and deserialize calls
+ * causes undefined behavior and we do not want this to depend on
+ * inferred types for safety.
+ *
+ * \tparam T type to serialize
+ */
+template <typename T, typename U>
+std::vector<char> serialize(const U& obj) {
+  static_assert(std::is_same<T, U>::value,
+                "Explicit type for serialization differs from deduced type");
+  const T& typed_obj = obj;
+  // pup routine is non-const, but shouldn't modify anything in serialization
+  // mode.
+  T& mut_obj = const_cast<T&>(typed_obj);
+
+  PUP::sizer sizer;
+  sizer | mut_obj;
+  std::vector<char> data(sizer.size());
+  PUP::toMem writer(data.data());
+  writer | mut_obj;
+
+  return data;
+}
+
+/*!
+ * \ingroup Parallel
+ * \brief Deserialize an object using PUP.
+ *
+ * \tparam T the type to deserialize to
+ */
+template <typename T>
+T deserialize(const void* const data) {
+  PUP::fromMem reader(data);
+  T result;
+  reader | result;
+  return result;
+}
