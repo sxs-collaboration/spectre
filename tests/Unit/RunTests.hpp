a6d36f8c848325e905f34e1b794429b0fe9db447
Nils Deppe <nd357@cornell.edu>
2017-06-08 15:02:36 -0400

    Add functions to serialize C++11 data structures

--- tests/Unit/RunTests.hpp
+++ tests/Unit/RunTests.hpp
@@ -7,6 +7,9 @@
 
 #include "tests/Unit/RunTests.decl.h"
 
+// Need to register derived classes to be serialized.
+void register_derived_classes_for_pup_stl_cpp11();
+
 /// Main executable for running the unit tests.
 class RunTests : public CBase_RunTests {
  public:
