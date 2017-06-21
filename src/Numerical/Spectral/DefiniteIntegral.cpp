1e09565ce391af9cdb14642f8ff1493f0ca9aff8
Gabriel <gsb76@cornell.edu>
2017-06-21 09:00:39 -0700

    PR comments addressed

--- src/Numerical/Spectral/DefiniteIntegral.cpp
+++ src/Numerical/Spectral/DefiniteIntegral.cpp
@@ -15,8 +15,7 @@ DataVector integrate_over_last_dimension(const DataVector& f,
   static_assert(Dim > 1, "Expect dimension to be at least 2.");
   const size_t extents_in_last_dim = extents[Dim - 1];
   const DataVector& w = Basis::lgl::quadrature_weights(extents_in_last_dim);
-  const size_t reduced_size =
-      extents.product() / extents_in_last_dim;  // yes, int division
+  const size_t reduced_size = extents.template product<Dim - 1>();
   DataVector integrated_data(reduced_size, 0.);
   dgemv_('N', reduced_size, extents_in_last_dim, 1., f.data(), reduced_size,
          w.data(), 1, 0., integrated_data.data(), 1);
@@ -25,7 +24,7 @@ DataVector integrate_over_last_dimension(const DataVector& f,
 }  // namespace
 
 template <size_t Dim>
-double definite_integral(const DataVector& f, const Index<Dim>& extents) {
+double definite_integral(const DataVector& f, const Index<Dim>& extents) noexcept{
   ASSERT(f.size() == extents.product(),
          "size = " << f.size() << ", product = " << extents.product());
   return definite_integral(integrate_over_last_dimension(f, extents),
@@ -33,7 +32,7 @@ double definite_integral(const DataVector& f, const Index<Dim>& extents) {
 }
 
 template <>
-double definite_integral<>(const DataVector& f, const Index<1>& extents) {
+double definite_integral<>(const DataVector& f, const Index<1>& extents) noexcept{
   const size_t N = f.size();
   ASSERT(N == extents.product(),
          "N = " << N << ", product = " << extents.product());
--- src/Numerical/Spectral/DefiniteIntegral.hpp
+++ src/Numerical/Spectral/DefiniteIntegral.hpp
@@ -12,16 +12,13 @@ class Index;
 
 /*! \ingroup Functors
  * \brief Compute the definite integral of a grid-function over a manifold.
- *
- * The integral is computed on the reference element by multiplyting the
- * DataVector
- * with the LegendreGaussLobatto integration weights in that dimension.
- *
- * \requires number of points in f and extents are equal.
- *
+ * The integral is computed on the reference element by multiplying the
+ * DataVector with the Basis::lgl integration weights in that
+ * dimension.
+ * \requires number of points in `f` and `extents` are equal.
  * \param f the grid function to integrate.
- * \param extents the extents of the manifold on which f is located.
- * \returns the definite integral of f on the manifold.
+ * \param extents the extents of the manifold on which `f` is located.
+ * \returns the definite integral of `f` on the manifold.
  */
 template <size_t Dim>
-double definite_integral(const DataVector& f, const Index<Dim>& extents);
+double definite_integral(const DataVector& f, const Index<Dim>& extents) noexcept;
