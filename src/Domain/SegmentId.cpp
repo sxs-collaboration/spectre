0e4142f14a9c8e82bf91b7634c4a4e12ec4344ea
Gabriel <gsb76@cornell.edu>
2017-06-27 08:56:35 -0700

    Addition of SegmentId and test

---
+++ src/Domain/SegmentId.cpp
@@ -0,0 +1,33 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Domain/SegmentId.hpp"
+
+#include <boost/functional/hash.hpp>
+#include <limits>
+#include <ostream>
+
+SegmentId::SegmentId(const size_t refinement_level, const size_t index)
+    : refinement_level_(refinement_level), index_(index) {
+  ASSERT(index < two_to_the(refinement_level),
+         "index = " << index << ", refinement_level = " << refinement_level);
+}
+
+void SegmentId::pup(PUP::er& p) {
+  p | refinement_level_;
+  p | index_;
+}
+
+std::ostream& operator<<(std::ostream& os, const SegmentId& id) {
+  os << 'L' << id.refinement_level() << 'I' << id.index();
+  return os;
+}
+
+// LCOV_EXCL_START
+size_t hash_value(const SegmentId& s) noexcept {
+  size_t h = 0;
+  boost::hash_combine(h, s.refinement_level());
+  boost::hash_combine(h, s.index());
+  return h;
+}
+// LCOV_EXCL_STOP
---
+++ tests/Unit/Domain/Test_SegmentId.cpp
@@ -0,0 +1,56 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include <catch.hpp>
+
+#include "Domain/SegmentId.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+
+TEST_CASE("Unit.Domain.SegmentId", "[Domain][Unit]") {
+  // Test default constructor:
+  SegmentId test_id;
+  CHECK(test_id.index() == std::numeric_limits<size_t>::max());
+  CHECK(test_id.refinement_level() == std::numeric_limits<size_t>::max());
+  static_cast<void>(test_id);
+
+  // Test equality operator:
+  SegmentId segment_one(4, 3);
+  SegmentId segment_two(4, 3);
+  SegmentId segment_three(4, 0);
+  SegmentId segment_four(5, 4);
+  CHECK(segment_one == segment_two);
+  CHECK(segment_two != segment_three);
+  CHECK(segment_two != segment_four);
+
+  // Test pup operations:
+  CHECK(segment_one == serialize_and_deserialize(segment_one));
+
+  // Test parent and child operations:
+  for (size_t level = 1; level < 5; ++level) {
+    const double segment_length = 2.0 / two_to_the(level);
+    double midpoint = -1.0 + 0.5 * segment_length;
+    for (size_t segment_index = 0; segment_index < two_to_the(level);
+         ++segment_index) {
+      SegmentId id(level, segment_index);
+      CHECK(id.midpoint() == midpoint);
+      midpoint += segment_length;
+      CHECK(id == id.id_of_child(Side::Lower).id_of_parent());
+      CHECK(id == id.id_of_child(Side::Upper).id_of_parent());
+      if (0 == segment_index % 2) {
+        CHECK(id == id.id_of_parent().id_of_child(Side::Lower));
+      } else {
+        CHECK(id == id.id_of_parent().id_of_child(Side::Upper));
+      }
+    }
+  }
+
+  // Test retrieval functions:
+  SegmentId level_2_index_3(2, 3);
+  CHECK(level_2_index_3.refinement_level() == 2);
+  CHECK(level_2_index_3.index() == 3);
+
+  // Test output operator:
+  SegmentId level_3_index_2(3, 2);
+  CHECK(get_output(level_3_index_2) == "L3I2");
+}
