85048309ed9316e3cbabc0368d250a394d5f0bfd
Nils Deppe <nd357@cornell.edu>
2017-06-28 14:22:35 -0400

    Add specific version of Boost that is required

--- cmake/SetupBoost.cmake
+++ cmake/SetupBoost.cmake
@@ -1,6 +1,6 @@
 # Distributed under the MIT License.
 # See LICENSE.txt for details.
 
-find_package(Boost REQUIRED)
+find_package(Boost 1.56.0 REQUIRED)
 include_directories(SYSTEM ${Boost_INCLUDE_DIR})
 message(STATUS "Boost include: ${Boost_INCLUDE_DIRS}")
