f46ed7e9d44bbd377ea9763c44c94696095bc4af
Eamonn O'Shea <edo27@cornell.edu>
2017-06-30 15:40:55 -0400

    Add affine map and test

--- tests/Unit/Domain/Test_EmbeddingMaps.cpp
+++ tests/Unit/Domain/Test_EmbeddingMaps.cpp
@@ -5,9 +5,44 @@
 #include <random>
 
 #include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/EmbeddingMaps/AffineMap.hpp"
 #include "Domain/EmbeddingMaps/Identity.hpp"
 #include "Utilities/ConstantExpressions.hpp"
 #include "tests/Unit/TestHelpers.hpp"
+
+TEST_CASE("Unit.Domain.EmbeddingMaps.Affine", "[Domain][Unit]") {
+  double xA = -1.0;
+  double xB = 1.0;
+  double xa = -2.0;
+  double xb = 2.0;
+
+  EmbeddingMaps::AffineMap affine_map(xA, xB, xa, xb);
+
+  double xi = 0.5 * (xA + xB);
+  double x = xb * (xi - xA) / (xB - xA) + xa * (xB - xi) / (xB - xA);
+
+  Point<1, Frame::Logical> point_A(xA);
+  Point<1, Frame::Logical> point_B(xB);
+  Point<1, Frame::Grid> point_a(xa);
+  Point<1, Frame::Grid> point_b(xb);
+  Point<1, Frame::Logical> point_xi(xi);
+  Point<1, Frame::Grid> point_x(x);
+
+  CHECK(affine_map(point_A) == point_a);
+  CHECK(affine_map(point_B) == point_b);
+  CHECK(affine_map(point_xi) == point_x);
+
+  CHECK(affine_map.inverse(point_a) == point_A);
+  CHECK(affine_map.inverse(point_b) == point_B);
+  CHECK(affine_map.inverse(point_x) == point_xi);
+
+  double inv_jacobian_00 = (xB - xA) / (xb - xa);
+
+  CHECK(affine_map.inv_jacobian(point_A, 0, 0) == inv_jacobian_00);
+  CHECK(affine_map.inv_jacobian(point_B, 0, 0) == inv_jacobian_00);
+  CHECK(affine_map.inv_jacobian(point_xi, 0, 0) == inv_jacobian_00);
+}
+
 
 template <size_t Dim>
 void test_identity() {
