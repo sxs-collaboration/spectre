b82e1bc52516359bf4a6e3c2136170f58a46422f
Gabriel <gsb76@cornell.edu>
2017-07-05 15:29:47 -0700

    Add Neighbors and test

---
+++ src/Domain/Neighbors.cpp
@@ -0,0 +1,65 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Domain/Neighbors.hpp"
+
+#include <charm.h>
+#include <ostream>
+
+#include "Domain/ElementId.hpp"
+#include "Parallel/PupStlCpp11.hpp"
+#include "Utilities/StdHelpers.hpp"
+
+template <size_t VolumeDim>
+Neighbors<VolumeDim>::Neighbors(std::unordered_set<ElementId<VolumeDim>> ids,
+                                Orientation<VolumeDim> orientation)
+    : ids_(std::move(ids)), orientation_(std::move(orientation)) {}
+
+template <size_t VolumeDim>
+void Neighbors<VolumeDim>::add_ids(
+    const std::unordered_set<ElementId<VolumeDim>>& additional_Ids) {
+  for (const auto& id : additional_Ids) {
+    ids_.insert(id);
+  }
+}
+
+template <size_t VolumeDim>
+std::ostream& operator<<(std::ostream& os, const Neighbors<VolumeDim>& n) {
+  os << "Ids = " << n.ids() << "; orientation = " << n.orientation();
+  return os;
+}
+
+template <size_t VolumeDim>
+bool operator==(const Neighbors<VolumeDim>& lhs,
+                const Neighbors<VolumeDim>& rhs) noexcept {
+  return (lhs.ids() == rhs.ids() and lhs.orientation() == rhs.orientation());
+}
+
+template <size_t VolumeDim>
+bool operator!=(const Neighbors<VolumeDim>& lhs,
+                const Neighbors<VolumeDim>& rhs) noexcept {
+  return not(lhs == rhs);
+}
+
+template <size_t VolumeDim>
+void Neighbors<VolumeDim>::pup(PUP::er& p) {
+  p | ids_;
+  p | orientation_;
+}
+
+// Explicit instantiations
+template class Neighbors<1>;
+template class Neighbors<2>;
+template class Neighbors<3>;
+
+template std::ostream& operator<<(std::ostream&, const Neighbors<1>&);
+template std::ostream& operator<<(std::ostream&, const Neighbors<2>&);
+template std::ostream& operator<<(std::ostream&, const Neighbors<3>&);
+
+template bool operator==(const Neighbors<1>&, const Neighbors<1>&);
+template bool operator==(const Neighbors<2>&, const Neighbors<2>&);
+template bool operator==(const Neighbors<3>&, const Neighbors<3>&);
+
+template bool operator!=(const Neighbors<1>&, const Neighbors<1>&);
+template bool operator!=(const Neighbors<2>&, const Neighbors<2>&);
+template bool operator!=(const Neighbors<3>&, const Neighbors<3>&);
---
+++ src/Domain/Neighbors.hpp
@@ -0,0 +1,107 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines class template Neighbors.
+
+#pragma once
+
+#include <iosfwd>
+#include <pup.h>
+#include <unordered_set>
+
+#include "Domain/Direction.hpp"
+#include "Domain/ElementId.hpp"
+#include "Domain/Orientation.hpp"
+
+/// \ingroup ComputationalDomain
+/// Information about the neighbors of an Element in a particular direction.
+///
+/// \tparam VolumeDim the volume dimension.
+template <size_t VolumeDim>
+class Neighbors {
+ public:
+  /// Construct with the ids and orientation of the neighbors.
+  ///
+  /// \param ids the ids of the neighbors.
+  /// \param orientation the Orientation of the neighbors.
+  Neighbors(std::unordered_set<ElementId<VolumeDim>> ids,
+            Orientation<VolumeDim> orientation);
+
+  /// Default constructor for Charm++ serialization.
+  Neighbors() = default;
+  ~Neighbors() = default;
+  Neighbors(const Neighbors<VolumeDim>& neighbor)  = default;
+  Neighbors(Neighbors<VolumeDim>&&) noexcept = default;  // NOLINT
+  Neighbors<VolumeDim>& operator=(const Neighbors<VolumeDim>& rhs) = default;
+  Neighbors<VolumeDim>& operator=(Neighbors<VolumeDim>&&) noexcept =  // NOLINT
+      default;                                                        // NOLINT
+
+  const std::unordered_set<ElementId<VolumeDim>>& ids() const noexcept {
+    return ids_;
+  }
+
+  const Orientation<VolumeDim>& orientation() const noexcept {
+    return orientation_;
+  }
+
+  /// Reset the ids of the neighbors.
+  void set_ids_to(
+      const std::unordered_set<ElementId<VolumeDim>> new_ids) noexcept {
+    ids_ = std::move(new_ids);
+  }
+
+  /// Add ids of neighbors.
+  /// Adding an existing neighbor is allowed.
+  void add_ids(const std::unordered_set<ElementId<VolumeDim>>& additional_ids);
+
+  /// Serialization for Charm++
+  void pup(PUP::er& p);  // NOLINT
+
+  /// The number of neighbors
+  size_t size() const noexcept { return ids_.size(); }
+
+  typename std::unordered_set<ElementId<VolumeDim>>::iterator begin() {
+    return ids_.begin();
+  }
+
+  typename std::unordered_set<ElementId<VolumeDim>>::iterator end() {
+    return ids_.end();
+  }
+
+  typename std::unordered_set<ElementId<VolumeDim>>::const_iterator begin()
+      const {
+    return ids_.begin();
+  }
+
+  typename std::unordered_set<ElementId<VolumeDim>>::const_iterator end()
+      const {
+    return ids_.end();
+  }
+
+  typename std::unordered_set<ElementId<VolumeDim>>::const_iterator cbegin()
+      const {
+    return ids_.begin();
+  }
+
+  typename std::unordered_set<ElementId<VolumeDim>>::const_iterator cend()
+      const {
+    return ids_.end();
+  }
+
+ private:
+  std::unordered_set<ElementId<VolumeDim>> ids_;
+  Orientation<VolumeDim> orientation_;
+};
+
+/// Output operator for Neighborss.
+template <size_t VolumeDim>
+std::ostream& operator<<(std::ostream& os, const Neighbors<VolumeDim>& n);
+
+template <size_t VolumeDim>
+bool operator==(const Neighbors<VolumeDim>& lhs,
+                const Neighbors<VolumeDim>& rhs) noexcept;
+
+template <size_t VolumeDim>
+bool operator!=(const Neighbors<VolumeDim>& lhs,
+                const Neighbors<VolumeDim>& rhs) noexcept;
