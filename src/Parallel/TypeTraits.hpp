593f52884cbf8b9df5e9d32bd06f9362fda60963
Lawrence Kidder <kidder@astro.cornell.edu>
2017-07-13 15:48:10 -0400

    Move pup type traits from Utilities to Parallel

--- src/Parallel/TypeTraits.hpp
+++ src/Parallel/TypeTraits.hpp
@@ -7,6 +7,9 @@
 #pragma once
 
 #include <charm++.h>
+#include <pup.h>
+
+#include "Utilities/TypeTraits.hpp"
 
 namespace Parallel {
 
@@ -29,5 +32,101 @@ struct is_group_proxy : std::is_base_of<CProxy_IrrGroup, T>::type {};
 /// Check if `T` is a Charm++ proxy for a node group chare
 template <typename T>
 struct is_node_group_proxy : std::is_base_of<CProxy_NodeGroup, T>::type {};
+
+// @{
+/// \ingroup Parallel
+/// \brief Check if `T` has a `pup` member function
+///
+/// \details
+/// Inherits from std::true_type if the type `T` has a `pup(PUP::er&)` member
+/// function, otherwise inherits from std::false_type
+///
+/// \usage
+/// For any type `T`,
+/// \code
+/// using result = tt::has_pup_member<T>;
+/// \endcode
+///
+/// \metareturns
+/// cpp17::bool_constant
+///
+/// \semantics
+/// If the type `T` has a `pup(PUP::er&)` member function, then
+/// \code
+/// typename result::type = std::true_type;
+/// \endcode
+/// otherwise
+/// \code
+/// typename result::type = std::false_type;
+/// \endcode
+///
+/// \example
+/// \snippet Parallel/Test_TypeTraits.cpp has_pup_member_example
+/// \see is_pupable
+/// \tparam T the type to check
+template <typename T, typename = cpp17::void_t<>>
+struct has_pup_member : std::false_type {};
+/// \cond HIDDEN_SYMBOLS
+template <typename T>
+struct has_pup_member<
+    T, cpp17::void_t<decltype(std::declval<T>().pup(std::declval<PUP::er&>()))>>
+    : std::true_type {};
+/// \endcond
+/// \see has_pup_member
+template <typename T>
+constexpr bool has_pup_member_v = has_pup_member<T>::value;
+
+/// \see has_pup_member
+template <typename T>
+using has_pup_member_t = typename has_pup_member<T>::type;
+// @}
+
+// @{
+/// \ingroup Parallel
+/// \brief Check if type `T` has operator| defined for Charm++ serialization
+///
+/// \details
+/// Inherits from std::true_type if the type `T` has operator| defined,
+/// otherwise inherits from std::false_type
+///
+/// \usage
+/// For any type `T`,
+/// \code
+/// using result = tt::is_pupable<T>;
+/// \endcode
+///
+/// \metareturns
+/// cpp17::bool_constant
+///
+/// \semantics
+/// If the type `T` has operator| defined, then
+/// \code
+/// typename result::type = std::true_type;
+/// \endcode
+/// otherwise
+/// \code
+/// typename result::type = std::false_type;
+/// \endcode
+///
+/// \example
+/// \snippet Parallel/Test_TypeTraits.cpp is_pupable_example
+/// \see has_pup_member
+/// \tparam T the type to check
+template <typename T, typename U = void>
+struct is_pupable : std::false_type {};
+/// \cond HIDDEN_SYMBOLS
+template <typename T>
+struct is_pupable<
+    T, cpp17::void_t<decltype(std::declval<PUP::er&>() | std::declval<T&>())>>
+    : std::true_type {};
+/// \endcond
+/// \see is_pupable
+template <typename T>
+constexpr bool is_pupable_v = is_pupable<T>::value;
+
+/// \see is_pupable
+template <typename T>
+using is_pupable_t = typename is_pupable<T>::type;
+// @}
 
 } // namespace Parallel
