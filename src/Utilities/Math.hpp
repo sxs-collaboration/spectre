ec2e09f0939dabbe60206948acc2253a64d8127d
Nils Deppe <nd357@cornell.edu>
2017-07-18 13:59:25 -0400

    Add Utilities/Math.hpp for non-constexpr math functions

---
+++ src/Utilities/Math.hpp
@@ -0,0 +1,19 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include <cmath>
+
+#include "Utilities/ForceInline.hpp"
+#include "Utilities/TypeTraits.hpp"
+
+/*!
+ * \ingroup Utilities
+ * \brief Returns the number of digits in an integer number
+ */
+template <typename T>
+SPECTRE_ALWAYS_INLINE T number_of_digits(const T number) {
+  static_assert(tt::is_integer_v<std::decay_t<T>>,
+                "Must call number_of_digits with an integer number");
+  return number == 0 ? 1 : static_cast<decltype(number)>(
+                               std::ceil(std::log10(std::abs(number) + 1)));
+}
---
+++ tests/Unit/Utilities/Test_Math.cpp
@@ -0,0 +1,15 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include <catch.hpp>
+
+#include "Utilities/Math.hpp"
+
+TEST_CASE("Unit.Utilities.Math", "[Unit][Utilities]") {
+  // Test number_of_digits
+  CHECK(2 == number_of_digits(10));
+  CHECK(1 == number_of_digits(0));
+  CHECK(1 == number_of_digits(-1));
+  CHECK(1 == number_of_digits(9));
+  CHECK(2 == number_of_digits(-99));
+}
