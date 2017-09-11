556c16e1571eb05f9e3576b4c4e29de9851064e5
Lawrence Kidder <kidder@astro.cornell.edu>
2017-09-11 12:40:04 -0400

    Add class Informer to manage textual output

---
+++ src/Informer/Informer.hpp
@@ -0,0 +1,20 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines class Informer.
+
+#pragma once
+
+#include <charm++.h>
+
+/// \ingroup Utilities
+/// The Informer manages textual output regarding the status of a simulation.
+class Informer {
+ public:
+  /// Print useful information at the beginning of a simulation.
+  static void print_startup_info(CkArgMsg* msg);
+
+  /// Print useful information at the end of a simulation.
+  static void print_exit_info();
+};
