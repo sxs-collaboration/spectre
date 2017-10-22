ae2b175052dad12547f973eeb45888e51db18c6f
Nils Deppe <nd357@cornell.edu>
2017-10-21 21:07:45 -0400

    Add dereference_wrapper to extract from reference_wrapper

---
+++ src/Utilities/DereferenceWrapper.hpp
@@ -0,0 +1,37 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines function dereference_wrapper
+
+#pragma once
+
+#include <functional>
+#include <utility>
+
+/// \ingroup Utilities
+/// \brief Returns the reference object held by a reference wrapper, if a
+/// non-reference_wrapper type is passed in then the object is returned
+template <typename T>
+decltype(auto) dereference_wrapper(T&& t) {
+  return std::forward<T>(t);
+}
+
+/// \cond
+template <typename T>
+T& dereference_wrapper(const std::reference_wrapper<T>& t) {
+  return t.get();
+}
+template <typename T>
+T& dereference_wrapper(std::reference_wrapper<T>& t) {
+  return t.get();
+}
+template <typename T>
+T&& dereference_wrapper(const std::reference_wrapper<T>&& t) {
+  return t.get();
+}
+template <typename T>
+T&& dereference_wrapper(std::reference_wrapper<T>&& t) {
+  return t.get();
+}
+/// \endcond
