ba27868ee25b03f73606f88c0862413479ff67a0
Mark Scheel <scheel@tapir.caltech.edu>
2017-11-13 14:41:01 -0800

    Add KerrSchild.

---
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/EinsteinSolutions/Test_KerrSchild.cpp
@@ -0,0 +1,305 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Index.hpp"
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GrTags.hpp"
+#include "Options/Options.hpp"
+#include "Options/ParseOptions.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/EinsteinSolutions/KerrSchild.hpp"
+#include "tests/Unit/PointwiseFunctions/AnalyticSolutions/EinsteinSolutions/VerifyEinsteinSolution.hpp"
+#include "tests/Unit/PointwiseFunctions/GeneralRelativity/GrTestHelpers.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+
+namespace {
+
+struct KerrSchild {
+  using type = EinsteinSolutions::KerrSchild;
+  static constexpr OptionString help{"A Kerr-Schild solution"};
+};
+
+template <typename DataType>
+tnsr::I<DataType, 3, Frame::Inertial> spatial_coords(
+    const DataType& used_for_size) noexcept {
+  auto x = make_with_value<tnsr::I<DataType, 3, Frame::Inertial>>(used_for_size,
+                                                                  0.0);
+  get<0>(x) = 1.32;
+  get<1>(x) = 0.82;
+  get<2>(x) = 1.24;
+  return x;
+}
+
+template <typename DataType>
+void test_schwarzschild(const DataType& used_for_size) noexcept {
+  // Schwarzschild solution is:
+  // H        = M/r
+  // l_mu     = (1,x/r,y/r,z/r)
+  // lapse    = (1+2M/r)^{-1/2}
+  // d_lapse  = (1+2M/r)^{-3/2}(Mx^i/r^3)
+  // shift^i  = (2Mx^i/r^2) / lapse^2
+  // g_{ij}   = delta_{ij} + 2 M x_i x_j/r^3
+  // d_i H    = -Mx_i/r^3
+  // d_i l_j  = delta_{ij}/r - x^i x^j/r^3
+  // d_k g_ij = -6M x_i x_j x_k/r^5 + 2 M x_i delta_{kj}/r^3
+  //                                + 2 M x_j delta_{ki}/r^3
+
+  // Parameters for KerrSchild solution
+  const double mass = 1.01;
+  const std::array<double, 3> spin{{0.0, 0.0, 0.0}};
+  const std::array<double, 3> center{{0.0, 0.0, 0.0}};
+  const auto x = spatial_coords(used_for_size);
+  const double t = 1.3;
+
+  // Evaluate solution
+  EinsteinSolutions::KerrSchild solution(mass, spin, center);
+
+  const auto vars = solution.solution(x, t);
+  const auto& lapse = get<gr::Tags::Lapse<3, Frame::Inertial, DataType>>(vars);
+  const auto& dt_lapse =
+      get<gr::Tags::DtLapse<3, Frame::Inertial, DataType>>(vars);
+  const auto& d_lapse =
+      get<EinsteinSolutions::KerrSchild::deriv_lapse<DataType>>(vars);
+  const auto& shift = get<gr::Tags::Shift<3, Frame::Inertial, DataType>>(vars);
+  const auto& d_shift =
+      get<EinsteinSolutions::KerrSchild::deriv_shift<DataType>>(vars);
+  const auto& dt_shift =
+      get<gr::Tags::DtShift<3, Frame::Inertial, DataType>>(vars);
+  const auto& g =
+      get<gr::Tags::SpatialMetric<3, Frame::Inertial, DataType>>(vars);
+  const auto& dt_g =
+      get<gr::Tags::DtSpatialMetric<3, Frame::Inertial, DataType>>(vars);
+  const auto& d_g =
+      get<EinsteinSolutions::KerrSchild::deriv_spatial_metric<DataType>>(vars);
+
+  // Check those quantities that should be zero.
+  const auto zero = make_with_value<DataType>(x, 0.);
+  CHECK(dt_lapse.get() == zero);
+  for (size_t i = 0; i < 3; ++i) {
+    CHECK(dt_shift.get(i) == zero);
+    for (size_t j = 0; j < 3; ++j) {
+      CHECK(dt_g.get(i, j) == zero);
+    }
+  }
+
+  const DataType r = magnitude(x);
+  const DataType one_over_r_squared = 1.0 / square(r);
+  const DataType one_over_r_cubed = 1.0 / cube(r);
+  const DataType one_over_r_fifth = one_over_r_squared * one_over_r_cubed;
+  auto expected_lapse = make_with_value<Scalar<DataType>>(x, 0.0);
+  get(expected_lapse) = 1.0 / sqrt(1.0 + 2.0 * mass / r);
+  CHECK_ITERABLE_APPROX(lapse, expected_lapse);
+
+  auto expected_shift =
+      make_with_value<tnsr::I<DataType, 3, Frame::Inertial>>(x, 0.0);
+  for (size_t i = 0; i < 3; ++i) {
+    expected_shift.get(i) =
+        2.0 * mass * x.get(i) * one_over_r_squared * square(get(lapse));
+  }
+  CHECK_ITERABLE_APPROX(shift, expected_shift);
+
+  auto expected_d_lapse =
+      make_with_value<tnsr::i<DataType, 3, Frame::Inertial>>(x, 0.0);
+  for (size_t i = 0; i < 3; ++i) {
+    expected_d_lapse.get(i) =
+        mass * x.get(i) * one_over_r_cubed * cube(get(lapse));
+  }
+  CHECK_ITERABLE_APPROX(d_lapse, expected_d_lapse);
+
+  auto expected_g =
+      make_with_value<tnsr::ii<DataType, 3, Frame::Inertial>>(x, 0.0);
+  for (size_t i = 0; i < 3; ++i) {
+    for (size_t j = i; j < 3; ++j) {
+      expected_g.get(i, j) =
+          2.0 * mass * x.get(i) * x.get(j) * one_over_r_cubed;
+    }
+    expected_g.get(i, i) += 1.0;
+  }
+  CHECK_ITERABLE_APPROX(g, expected_g);
+
+  auto expected_d_g =
+      make_with_value<tnsr::ijj<DataType, 3, Frame::Inertial>>(x, 0.0);
+  for (size_t k = 0; k < 3; ++k) {
+    for (size_t i = 0; i < 3; ++i) {
+      for (size_t j = i; j < 3; ++j) {  // Symmetry
+        expected_d_g.get(k, i, j) =
+            -6.0 * mass * x.get(i) * x.get(j) * x.get(k) * one_over_r_fifth;
+        if (k == j) {
+          expected_d_g.get(k, i, j) += 2.0 * mass * x.get(i) * one_over_r_cubed;
+        }
+        if (k == i) {
+          expected_d_g.get(k, i, j) += 2.0 * mass * x.get(j) * one_over_r_cubed;
+        }
+      }
+    }
+  }
+  CHECK_ITERABLE_APPROX(d_g, expected_d_g);
+}
+
+void test_einstein_solution() noexcept {
+  // Parameters
+  //   ...for KerrSchild solution
+  const double mass = 1.7;
+  const std::array<double, 3> spin{{0.1, 0.2, 0.3}};
+  const std::array<double, 3> center{{0.3, 0.2, 0.4}};
+  //   ...for grid
+  const size_t grid_size = 8;
+  const std::array<double, 3> lower_bound{{0.82, 1.24, 1.32}};
+  const std::array<double, 3> upper_bound{{0.8, 1.22, 1.30}};
+
+  EinsteinSolutions::KerrSchild solution(mass, spin, center);
+  verify_time_independent_einstein_solution(
+      solution, grid_size, lower_bound, upper_bound,
+      std::numeric_limits<double>::epsilon() * 1.e5);
+}
+
+void test_double_vs_datavector() noexcept {
+  // Parameters for KerrSchild solution
+  const double mass = 1.7;
+  const std::array<double, 3> spin{{0.1, 0.2, 0.3}};
+  const std::array<double, 3> center{{0.3, 0.2, 0.4}};
+  EinsteinSolutions::KerrSchild solution(mass, spin, center);
+
+  const double t = 1.3;
+  const auto x1 = spatial_coords(0.0);
+  const auto x2 = spatial_coords(DataVector{0.0, 0.0, 0.0});
+
+  const auto vars1 = solution.solution(x1, t);
+  const auto& lapse1 = get<gr::Tags::Lapse<3, Frame::Inertial, double>>(vars1);
+  const auto& dt_lapse1 =
+      get<gr::Tags::DtLapse<3, Frame::Inertial, double>>(vars1);
+  const auto& d_lapse1 =
+      get<EinsteinSolutions::KerrSchild::deriv_lapse<double>>(vars1);
+  const auto& shift1 = get<gr::Tags::Shift<3, Frame::Inertial, double>>(vars1);
+  const auto& d_shift1 =
+      get<EinsteinSolutions::KerrSchild::deriv_shift<double>>(vars1);
+  const auto& dt_shift1 =
+      get<gr::Tags::DtShift<3, Frame::Inertial, double>>(vars1);
+  const auto& g1 =
+      get<gr::Tags::SpatialMetric<3, Frame::Inertial, double>>(vars1);
+  const auto& dt_g1 =
+      get<gr::Tags::DtSpatialMetric<3, Frame::Inertial, double>>(vars1);
+  const auto& d_g1 =
+      get<EinsteinSolutions::KerrSchild::deriv_spatial_metric<double>>(vars1);
+
+  const auto vars2 = solution.solution(x2, t);
+  const auto& lapse2 =
+      get<gr::Tags::Lapse<3, Frame::Inertial, DataVector>>(vars2);
+  const auto& dt_lapse2 =
+      get<gr::Tags::DtLapse<3, Frame::Inertial, DataVector>>(vars2);
+  const auto& d_lapse2 =
+      get<EinsteinSolutions::KerrSchild::deriv_lapse<DataVector>>(vars2);
+  const auto& shift2 =
+      get<gr::Tags::Shift<3, Frame::Inertial, DataVector>>(vars2);
+  const auto& d_shift2 =
+      get<EinsteinSolutions::KerrSchild::deriv_shift<DataVector>>(vars2);
+  const auto& dt_shift2 =
+      get<gr::Tags::DtShift<3, Frame::Inertial, DataVector>>(vars2);
+  const auto& g2 =
+      get<gr::Tags::SpatialMetric<3, Frame::Inertial, DataVector>>(vars2);
+  const auto& dt_g2 =
+      get<gr::Tags::DtSpatialMetric<3, Frame::Inertial, DataVector>>(vars2);
+  const auto& d_g2 =
+      get<EinsteinSolutions::KerrSchild::deriv_spatial_metric<DataVector>>(
+          vars2);
+
+  check_tensor_doubles_approx_equals_tensor_datavectors(lapse2, lapse1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(shift2, shift1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(g2, g1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(dt_lapse2, dt_lapse1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(dt_shift2, dt_shift1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(dt_g2, dt_g1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(d_lapse2, d_lapse1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(d_shift2, d_shift1);
+  check_tensor_doubles_approx_equals_tensor_datavectors(d_g2, d_g1);
+}
+
+void test_serialize() noexcept {
+  EinsteinSolutions::KerrSchild solution(3.0, {{0.2, 0.3, 0.2}},
+                                         {{0.0, 3.0, 4.0}});
+  test_serialization(solution);
+}
+
+void test_copy_and_move() noexcept {
+  EinsteinSolutions::KerrSchild solution(3.0, {{0.2, 0.3, 0.2}},
+                                         {{0.0, 3.0, 4.0}});
+  test_copy_semantics(solution);
+  auto solution_copy = solution;
+  // clang-tidy: std::move of trivially copyable type
+  test_move_semantics(std::move(solution), solution_copy);  // NOLINT
+}
+
+void test_construct_from_options() {
+  Options<tmpl::list<KerrSchild>> opts("");
+  opts.parse(
+      "KerrSchild:\n"
+      "  Mass: 0.5\n"
+      "  Spin: [0.1,0.2,0.3]\n"
+      "  Center: [1.0,3.0,2.0]");
+  CHECK(
+      opts.get<KerrSchild>() ==
+      EinsteinSolutions::KerrSchild(0.5, {{0.1, 0.2, 0.3}}, {{1.0, 3.0, 2.0}}));
+}
+
+}  // namespace
+
+SPECTRE_TEST_CASE(
+    "Unit.PointwiseFunctions.AnalyticSolutions.EinsteinSolution.KerrSchild",
+    "[PointwiseFunctions][Unit]") {
+  test_schwarzschild<DataVector>(DataVector{0.0, 0.0, 0.0});
+  test_schwarzschild<double>(0.0);
+  test_einstein_solution();
+  test_double_vs_datavector();
+  test_copy_and_move();
+  test_serialize();
+  test_construct_from_options();
+}
+
+// [[OutputRegex, Spin magnitude must be < 1]]
+SPECTRE_TEST_CASE(
+    "Unit.PointwiseFunctions.AnalyticSolutions.EinsteinSolution.KerrSchildSpin",
+    "[PointwiseFunctions][Unit]") {
+  ERROR_TEST();
+  EinsteinSolutions::KerrSchild solution(1.0, {{1.0, 1.0, 1.0}},
+                                         {{0.0, 0.0, 0.0}});
+}
+
+// [[OutputRegex, Mass must be non-negative]]
+SPECTRE_TEST_CASE(
+    "Unit.PointwiseFunctions.AnalyticSolutions.EinsteinSolution.KerrSchildMass",
+    "[PointwiseFunctions][Unit]") {
+  ERROR_TEST();
+  EinsteinSolutions::KerrSchild solution(-1.0, {{0.0, 0.0, 0.0}},
+                                         {{0.0, 0.0, 0.0}});
+}
+
+// [[OutputRegex, In string:.*At line 2 column 9:.Value -0.5 is below the lower
+// bound of 0]]
+SPECTRE_TEST_CASE(
+    "Unit.PointwiseFunctions.AnalyticSolutions.EinsteinSolution.KerrSchildOptM",
+    "[PointwiseFunctions][Unit]") {
+  ERROR_TEST();
+  Options<tmpl::list<KerrSchild>> opts("");
+  opts.parse(
+      "KerrSchild:\n"
+      "  Mass: -0.5\n"
+      "  Spin: [0.1,0.2,0.3]\n"
+      "  Center: [1.0,3.0,2.0]");
+  opts.get<KerrSchild>();
+}
+
+// [[OutputRegex, In string:.*At line 2 column 3:.Spin magnitude must be < 1]]
+SPECTRE_TEST_CASE(
+    "Unit.PointwiseFunctions.AnalyticSolutions.EinsteinSolution.KerrSchildOptS",
+    "[PointwiseFunctions][Unit]") {
+  ERROR_TEST();
+  Options<tmpl::list<KerrSchild>> opts("");
+  opts.parse(
+      "KerrSchild:\n"
+      "  Mass: 0.5\n"
+      "  Spin: [1.1,0.9,0.3]\n"
+      "  Center: [1.0,3.0,2.0]");
+  opts.get<KerrSchild>();
+}
---
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/EinsteinSolutions/VerifyEinsteinSolution.hpp
@@ -0,0 +1,243 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+
+#include "DataStructures/DataBoxTag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Index.hpp"
+#include "DataStructures/Tensor/EagerMath/DeterminantAndInverse.hpp"
+#include "DataStructures/Variables.hpp"
+#include "Domain/CoordinateMaps/AffineMap.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.hpp"
+#include "Domain/CoordinateMaps/ProductMaps.hpp"
+#include "Domain/LogicalCoordinates.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/Equations.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GrTags.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Christoffel.hpp"
+#include "PointwiseFunctions/GeneralRelativity/ComputeGhQuantities.hpp"
+#include "PointwiseFunctions/GeneralRelativity/ComputeSpacetimeQuantities.hpp"
+#include "PointwiseFunctions/GeneralRelativity/IndexManipulation.hpp"
+#include "Utilities/TMPL.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+
+/// \ingroup TestingFrameworkGroup
+/// Determines if the given `solution` is a time-independent solution
+/// of the Einstein equations. Uses numerical derivatives to compute
+/// the solution, on a grid extending from `lower_bound` to `upper_bound`
+/// with `grid_size_each_dimension` points in each dimension.
+/// The right-hand side of the Einstein equations must be zero within
+/// `error_tolerance`
+template <typename Solution>
+void verify_time_independent_einstein_solution(
+    const Solution& solution, const size_t grid_size_each_dimension,
+    const std::array<double, 3>& lower_bound,
+    const std::array<double, 3>& upper_bound,
+    const double error_tolerance) noexcept {
+  // Shorter names for tags.
+  using SpacetimeMetric = gr::Tags::SpacetimeMetric<3, Frame::Inertial>;
+  using Pi = ::GeneralizedHarmonic::Pi<3, Frame::Inertial>;
+  using Phi = ::GeneralizedHarmonic::Phi<3, Frame::Inertial>;
+  using GaugeH = ::GeneralizedHarmonic::GaugeH<3, Frame::Inertial>;
+  using VariablesTags = typelist<SpacetimeMetric, Pi, Phi, GaugeH>;
+
+  // Set up grid
+  const size_t data_size = pow<3>(grid_size_each_dimension);
+  Index<3> extents(grid_size_each_dimension);
+
+  using AffineMap = CoordinateMaps::AffineMap;
+  using AffineMap3D =
+      CoordinateMaps::ProductOf3Maps<AffineMap, AffineMap, AffineMap>;
+  const auto coord_map =
+      make_coordinate_map<Frame::Logical, Frame::Inertial>(AffineMap3D{
+          AffineMap{-1., 1., lower_bound[0], upper_bound[0]},
+          AffineMap{-1., 1., lower_bound[1], upper_bound[1]},
+          AffineMap{-1., 1., lower_bound[2], upper_bound[2]},
+      });
+
+  // Set up coordinates
+  const auto x_logical = logical_coordinates(extents);
+  const auto x = coord_map(x_logical);
+  const double t = 1.3;  // Arbitrary time for time-independent solution.
+
+  // Evaluate analytic solution
+  const auto vars = solution.solution(x, t);
+  const auto& lapse = get<gr::Tags::Lapse<3>>(vars);
+  const auto& dt_lapse = get<gr::Tags::DtLapse<3>>(vars);
+  const auto& d_lapse =
+      get<typename Solution::template deriv_lapse<DataVector>>(vars);
+  const auto& shift = get<gr::Tags::Shift<3>>(vars);
+  const auto& d_shift =
+      get<typename Solution::template deriv_shift<DataVector>>(vars);
+  const auto& dt_shift = get<gr::Tags::DtShift<3>>(vars);
+  const auto& g = get<gr::Tags::SpatialMetric<3>>(vars);
+  const auto& dt_g = get<gr::Tags::DtSpatialMetric<3>>(vars);
+  const auto& d_g =
+      get<typename Solution::template deriv_spatial_metric<DataVector>>(vars);
+
+  // Check those quantities that should vanish identically.
+  CHECK(get(dt_lapse) == make_with_value<DataVector>(x, 0.));
+  CHECK(dt_shift ==
+        make_with_value<cpp20::remove_cvref_t<decltype(dt_shift)>>(x, 0.));
+  CHECK(dt_g == make_with_value<cpp20::remove_cvref_t<decltype(dt_g)>>(x, 0.));
+
+  // Need upper spatial metric for many things below.
+  const auto upper_spatial_metric = determinant_and_inverse(g).second;
+
+  // Compute generalized harmonic quantities.  Put them in a Variables
+  // so that we can take numerical derivatives of them.
+  // Also put gauge_function into this list since we need a numerical
+  // derivative of it too.
+  Variables<VariablesTags> gh_vars(data_size);
+  auto& psi = get<SpacetimeMetric>(gh_vars);
+  auto& pi = get<Pi>(gh_vars);
+  auto& phi = get<Phi>(gh_vars);
+  auto& gauge_function = get<GaugeH>(gh_vars);
+  psi = gr::spacetime_metric(lapse, shift, g);
+  phi = GeneralizedHarmonic::phi(lapse, d_lapse, shift, d_shift, g, d_g);
+  pi = GeneralizedHarmonic::pi(lapse, dt_lapse, shift, dt_shift, g, dt_g, phi);
+
+  // Compute gauge_function compatible with d/dt(lapse) and d/dt(shift).
+  gauge_function = GeneralizedHarmonic::gauge_source(
+      lapse, dt_lapse, d_lapse, shift, dt_shift, d_shift, g,
+      trace(gr::extrinsic_curvature(lapse, shift, d_shift, g, dt_g, d_g),
+            upper_spatial_metric),
+      trace_last_indices(compute_christoffel_first_kind(d_g),
+                         upper_spatial_metric));
+
+  // Compute numerical derivatives of psi,pi,phi,H.
+  // Normally one should not take numerical derivatives of H for
+  // plugging into the RHS of the generalized harmonic equations, but
+  // here this is just a test.
+  const auto gh_derivs =
+      partial_derivatives<VariablesTags, VariablesTags, 3, Frame::Inertial>(
+          gh_vars, extents, coord_map.inv_jacobian(x_logical));
+  const auto& d_psi =
+      get<Tags::deriv<SpacetimeMetric, tmpl::size_t<3>, Frame::Inertial>>(
+          gh_derivs);
+  const auto& d_pi =
+      get<Tags::deriv<Pi, tmpl::size_t<3>, Frame::Inertial>>(gh_derivs);
+  const auto& d_phi =
+      get<Tags::deriv<Phi, tmpl::size_t<3>, Frame::Inertial>>(gh_derivs);
+  const auto& d_H =
+      get<Tags::deriv<GaugeH, tmpl::size_t<3>, Frame::Inertial>>(gh_derivs);
+
+  Approx numerical_approx = Approx::custom().epsilon(error_tolerance);
+
+  // Test 3-index constraint
+  CHECK_ITERABLE_CUSTOM_APPROX(d_psi, phi, numerical_approx);
+
+  // Compute spacetime deriv of H.
+  // Assume time derivative of H is zero, for time-independent solution
+  auto d4_H = make_with_value<tnsr::ab<DataVector, 3>>(x, 0.0);
+  for (size_t a = 0; a < 4; ++a) {
+    for (size_t i = 0; i < 3; ++i) {
+      d4_H.get(i + 1, a) = d_H.get(i, a);
+    }
+  }
+
+  // Compute analytic derivatives of psi, for use in computing
+  // christoffel symbols.
+  auto d4_psi = make_with_value<tnsr::abb<DataVector, 3>>(x, 0.0);
+  for (size_t b = 0; b < 4; ++b) {
+    for (size_t c = b; c < 4; ++c) {  // symmetry
+      d4_psi.get(0, b, c) = -get(lapse) * pi.get(b, c);
+      for (size_t k = 0; k < 3; ++k) {
+        d4_psi.get(0, b, c) += shift.get(k) * phi.get(k, b, c);
+        d4_psi.get(k + 1, b, c) = phi.get(k, b, c);
+      }
+    }
+  }
+
+  // Compute derived spacetime quantities
+  const auto upper_psi =
+      gr::inverse_spacetime_metric(lapse, shift, upper_spatial_metric);
+  const auto christoffel_first_kind = compute_christoffel_first_kind(d4_psi);
+  const auto christoffel_second_kind =
+      raise_or_lower_first_index(christoffel_first_kind, upper_psi);
+  const auto trace_christoffel_first_kind =
+      trace_last_indices(christoffel_first_kind, upper_psi);
+  const auto normal_one_form =
+      gr::spacetime_normal_one_form<3, Frame::Inertial>(lapse);
+  const auto normal_vector = gr::spacetime_normal_vector(lapse, shift);
+
+  // Test ADM evolution equation gives zero
+  auto dt_g_adm = make_with_value<tnsr::ii<DataVector, 3>>(x, 0.0);
+  {
+    auto ex_curv = gr::extrinsic_curvature(lapse, shift, d_shift, g, dt_g, d_g);
+    for (size_t i = 0; i < 3; ++i) {
+      for (size_t j = i; j < 3; ++j) {  // Symmetry
+        dt_g_adm.get(i, j) -= 2.0 * get(lapse) * ex_curv.get(i, j);
+        for (size_t k = 0; k < 3; ++k) {
+          dt_g_adm.get(i, j) += shift.get(k) * d_g.get(k, i, j) +
+                                g.get(k, i) * d_shift.get(j, k) +
+                                g.get(k, j) * d_shift.get(i, k);
+        }
+      }
+    }
+    CHECK_ITERABLE_APPROX(dt_g_adm,
+                          make_with_value<decltype(dt_g_adm)>(x, 0.0));
+  }
+
+  // Test 1-index constraint
+  auto C_1 = make_with_value<tnsr::a<DataVector, 3>>(x, 0.0);
+  for (size_t a = 0; a < 4; ++a) {
+    C_1.get(a) = gauge_function.get(a);
+    for (size_t i = 0; i < 3; ++i) {
+      for (size_t j = 0; j < 3; ++j) {
+        C_1.get(a) += upper_spatial_metric.get(i, j) * phi.get(i, j + 1, a);
+      }
+    }
+    for (size_t b = 0; b < 4; ++b) {
+      C_1.get(a) += normal_vector.get(b) * pi.get(b, a);
+      for (size_t c = 0; c < 4; ++c) {
+        C_1.get(a) -=
+            0.5 * normal_one_form.get(a) * pi.get(b, c) * upper_psi.get(b, c);
+        if (a > 0) {
+          C_1.get(a) -= 0.5 * phi.get(a - 1, b, c) * upper_psi.get(b, c);
+        }
+        for (size_t i = 0; i < 3; ++i) {
+          C_1.get(a) -= 0.5 * normal_one_form.get(a) *
+                        normal_vector.get(i + 1) * phi.get(i, b, c) *
+                        upper_psi.get(b, c);
+        }
+      }
+    }
+  }
+  CHECK_ITERABLE_CUSTOM_APPROX(C_1, make_with_value<decltype(C_1)>(x, 0.0),
+                               numerical_approx);
+
+  // Constraint-damping parameters: Set to arbitrary values.
+  // gamma = 0 (for all gammas) and gamma1 = -1 are special because,
+  // they zero out various terms in the equations, so don't choose those.
+  const auto gamma0 = make_with_value<Scalar<DataVector>>(x, 1.0);
+  const auto gamma1 = make_with_value<Scalar<DataVector>>(x, 1.0);
+  const auto gamma2 = make_with_value<Scalar<DataVector>>(x, 1.0);
+
+  // Compute RHS of generalized harmonic Einstein equations.
+  auto dt_psi =
+      make_with_value<tnsr::aa<DataVector, 3, Frame::Inertial>>(x, 0.0);
+  auto dt_pi =
+      make_with_value<tnsr::aa<DataVector, 3, Frame::Inertial>>(x, 0.0);
+  auto dt_phi =
+      make_with_value<tnsr::iaa<DataVector, 3, Frame::Inertial>>(x, 0.0);
+  GeneralizedHarmonic::ComputeDuDt<3>::apply(
+      make_not_null(&dt_psi), make_not_null(&dt_pi), make_not_null(&dt_phi),
+      psi, pi, phi, d_psi, d_pi, d_phi, gamma0, gamma1, gamma2, gauge_function,
+      d4_H, lapse, shift, upper_spatial_metric, upper_psi,
+      trace_christoffel_first_kind, christoffel_first_kind,
+      christoffel_second_kind, normal_vector, normal_one_form);
+
+  // Make sure the RHS is zero.
+  CHECK_ITERABLE_CUSTOM_APPROX(
+      dt_psi, make_with_value<decltype(dt_psi)>(x, 0.0), numerical_approx);
+  CHECK_ITERABLE_CUSTOM_APPROX(dt_pi, make_with_value<decltype(dt_pi)>(x, 0.0),
+                               numerical_approx);
+  CHECK_ITERABLE_CUSTOM_APPROX(
+      dt_phi, make_with_value<decltype(dt_phi)>(x, 0.0), numerical_approx);
+}
