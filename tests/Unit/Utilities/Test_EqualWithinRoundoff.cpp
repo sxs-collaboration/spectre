9cbfb0437d640a83b5be32bd788dbc3af60a4fda
Matt Giesler <mgiesler@tapir.caltech.edu>
2017-11-16 20:58:38 -0800

    Add EqualWithinRoundoff and test

---
+++ tests/Unit/Utilities/Test_EqualWithinRoundoff.cpp
@@ -0,0 +1,22 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include <catch.hpp>
+
+#include "Utilities/EqualWithinRoundoff.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+
+static_assert(equal_within_roundoff(1.0, 1.0 - 4.0e-16, 1.0e-15),
+              "Failed testing EqualWithinRoundoff");
+static_assert(not equal_within_roundoff(1.0, 1.0 - 4.0e-15, 1.0e-15),
+              "Failed testing EqualWithinRoundoff");
+
+static_assert(equal_within_roundoff(1.0e16, 1.0e16 - 1.0e1, 1.0e-15, 1.0e16),
+              "Failed testing EqualWithinRoundoff");
+static_assert(not equal_within_roundoff(1.0e16, 1.0e16 - 1.0e1, 1.0e-15, 1.0),
+              "Failed testing EqualWithinRoundoff");
+
+static_assert(not equal_within_roundoff(1.0, 1.0 - 1.0e-8, 1.0e-8, 0.0),
+              "Failed testing EqualWithinRoundoff");
+static_assert(equal_within_roundoff(1.0, 1.0 - 1.0e-8, 1.0e-8, 1.0),
+              "Failed testing EqualWithinRoundoff");
