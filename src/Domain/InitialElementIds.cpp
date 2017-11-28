20a6da539873567c02dd83b22dd97960297ba3f9
Nils Deppe <nd357@cornell.edu>
2017-11-28 11:44:47 -0500

    Add function to compute initial ElementId for single block

--- src/Domain/InitialElementIds.cpp
+++ src/Domain/InitialElementIds.cpp
@@ -3,77 +3,84 @@
 
 #include "Domain/InitialElementIds.hpp"
 
+#include <iterator>
+
 #include "Domain/ElementId.hpp"
 #include "Domain/SegmentId.hpp"
 #include "Utilities/ConstantExpressions.hpp"
 #include "Utilities/MakeArray.hpp"
 
 template <>
-std::vector<ElementId<1>> initial_element_ids(
+std::vector<ElementId<1>> initial_element_ids<1>(
+    const size_t block_id, const std::array<size_t, 1> initial_ref_levs) {
+  std::vector<ElementId<1>> ids;
+  ids.reserve(two_to_the(initial_ref_levs[0]));
+  for (size_t x_i = 0; x_i < two_to_the(initial_ref_levs[0]); ++x_i) {
+    SegmentId x_segment_id(initial_ref_levs[0], x_i);
+    ids.emplace_back(block_id, make_array<1>(x_segment_id));
+  }
+  return ids;
+}
+
+template <>
+std::vector<ElementId<2>> initial_element_ids<2>(
+    const size_t block_id, const std::array<size_t, 2> initial_ref_levs) {
+  std::vector<ElementId<2>> ids;
+  ids.reserve(two_to_the(initial_ref_levs[0]) *
+              two_to_the(initial_ref_levs[1]));
+  for (size_t x_i = 0; x_i < two_to_the(initial_ref_levs[0]); ++x_i) {
+    SegmentId x_segment_id(initial_ref_levs[0], x_i);
+    for (size_t y_i = 0; y_i < two_to_the(initial_ref_levs[1]); ++y_i) {
+      SegmentId y_segment_id(initial_ref_levs[1], y_i);
+      ids.emplace_back(block_id, make_array(x_segment_id, y_segment_id));
+    }
+  }
+  return ids;
+}
+
+template <>
+std::vector<ElementId<3>> initial_element_ids<3>(
+    const size_t block_id, const std::array<size_t, 3> initial_ref_levs) {
+  std::vector<ElementId<3>> ids;
+  ids.reserve(two_to_the(initial_ref_levs[0]) *
+              two_to_the(initial_ref_levs[1]) *
+              two_to_the(initial_ref_levs[2]));
+  for (size_t x_i = 0; x_i < two_to_the(initial_ref_levs[0]); ++x_i) {
+    SegmentId x_segment_id(initial_ref_levs[0], x_i);
+    for (size_t y_i = 0; y_i < two_to_the(initial_ref_levs[1]); ++y_i) {
+      SegmentId y_segment_id(initial_ref_levs[1], y_i);
+      for (size_t z_i = 0; z_i < two_to_the(initial_ref_levs[2]); ++z_i) {
+        SegmentId z_segment_id(initial_ref_levs[2], z_i);
+        ids.emplace_back(block_id,
+                         make_array(x_segment_id, y_segment_id, z_segment_id));
+      }
+    }
+  }
+  return ids;
+}
+
+template <size_t VolumeDim>
+std::vector<ElementId<VolumeDim>> initial_element_ids(
+    const std::vector<std::array<size_t, VolumeDim>>&
+        initial_refinement_levels) noexcept {
+  std::vector<ElementId<VolumeDim>> element_ids;
+  for (size_t block_id = 0; block_id < initial_refinement_levels.size();
+       ++block_id) {
+    auto ids_for_block =
+        initial_element_ids(block_id, initial_refinement_levels[block_id]);
+    element_ids.reserve(element_ids.size() + ids_for_block.size());
+    std::move(ids_for_block.begin(), ids_for_block.end(),
+              std::back_inserter(element_ids));
+  }
+  return element_ids;
+}
+
+template std::vector<ElementId<1>> initial_element_ids<1>(
     const std::vector<std::array<size_t, 1>>&
-        initial_refinement_levels) noexcept {
-  std::vector<ElementId<1>> element_ids;
-  for (size_t block_id = 0; block_id < initial_refinement_levels.size();
-       ++block_id) {
-    const size_t xi_refinement_level = initial_refinement_levels[block_id][0];
-    for (size_t xi_index = 0; xi_index < two_to_the(xi_refinement_level);
-         ++xi_index) {
-      element_ids.emplace_back(
-          block_id, make_array<1>(SegmentId(xi_refinement_level, xi_index)));
-    }
-  }
-  return element_ids;
-}
-
-template <>
-std::vector<ElementId<2>> initial_element_ids(
+        initial_refinement_levels) noexcept;
+template std::vector<ElementId<2>> initial_element_ids<2>(
     const std::vector<std::array<size_t, 2>>&
-        initial_refinement_levels) noexcept {
-  std::vector<ElementId<2>> element_ids;
-  for (size_t block_id = 0; block_id < initial_refinement_levels.size();
-       ++block_id) {
-    const size_t xi_refinement_level = initial_refinement_levels[block_id][0];
-    for (size_t xi_index = 0; xi_index < two_to_the(xi_refinement_level);
-         ++xi_index) {
-      const size_t eta_refinement_level =
-          initial_refinement_levels[block_id][1];
-      for (size_t eta_index = 0; eta_index < two_to_the(eta_refinement_level);
-           ++eta_index) {
-        element_ids.emplace_back(
-            block_id, make_array(SegmentId(xi_refinement_level, xi_index),
-                                 SegmentId(eta_refinement_level, eta_index)));
-      }
-    }
-  }
-  return element_ids;
-}
-
-template <>
-std::vector<ElementId<3>> initial_element_ids(
+        initial_refinement_levels) noexcept;
+template std::vector<ElementId<3>> initial_element_ids<3>(
     const std::vector<std::array<size_t, 3>>&
-        initial_refinement_levels) noexcept {
-  std::vector<ElementId<3>> element_ids;
-  for (size_t block_id = 0; block_id < initial_refinement_levels.size();
-       ++block_id) {
-    const size_t xi_refinement_level = initial_refinement_levels[block_id][0];
-    for (size_t xi_index = 0; xi_index < two_to_the(xi_refinement_level);
-         ++xi_index) {
-      const size_t eta_refinement_level =
-          initial_refinement_levels[block_id][1];
-      for (size_t eta_index = 0; eta_index < two_to_the(eta_refinement_level);
-           ++eta_index) {
-        const size_t zeta_refinement_level =
-            initial_refinement_levels[block_id][2];
-        for (size_t zeta_index = 0;
-             zeta_index < two_to_the(zeta_refinement_level); ++zeta_index) {
-          element_ids.emplace_back(
-              block_id,
-              make_array(SegmentId(xi_refinement_level, xi_index),
-                         SegmentId(eta_refinement_level, eta_index),
-                         SegmentId(zeta_refinement_level, zeta_index)));
-        }
-      }
-    }
-  }
-  return element_ids;
-}
+        initial_refinement_levels) noexcept;
