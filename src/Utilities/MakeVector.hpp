5d3d95c1b0f828a27ff63b31038f15e88463934c
Gabriel <gsb76@cornell.edu>
2017-12-08 11:15:10 -0800

    Change Arg1 to Arg0 in MakeVector

--- src/Utilities/MakeVector.hpp
+++ src/Utilities/MakeVector.hpp
@@ -17,13 +17,13 @@
  * \example
  * \snippet Utilities/Test_MakeVector.cpp make_vector_example
  */
-template <class ValueType = void, class Arg1, class... Args>
-auto make_vector(Arg1&& arg_1, Args&&... remaining_args) {
+template <class ValueType = void, class Arg0, class... Args>
+auto make_vector(Arg0&& arg_0, Args&&... remaining_args) {
   std::vector<
-      std::conditional_t<cpp17::is_same_v<ValueType, void>, Arg1, ValueType>>
+      std::conditional_t<cpp17::is_same_v<ValueType, void>, Arg0, ValueType>>
       return_vector;
   return_vector.reserve(sizeof...(Args) + 1);
-  return_vector.emplace_back(std::forward<Arg1>(arg_1));
+  return_vector.emplace_back(std::forward<Arg0>(arg_0));
   (void)std::initializer_list<int>{
       (((void)return_vector.emplace_back(std::forward<Args>(remaining_args))),
        0)...};
