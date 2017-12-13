4c56f65b4ce06f2541e7985c0988c2dbc9d872fd
Mark Scheel <scheel@tapir.caltech.edu>
2017-12-13 12:12:26 -0800

    Move some GH tags into gr namespace and different files.
    Tags are now simple.  We can add more tmpl magic to make these into
    ComputeItems when the time comes to do this.
    Need a DataType template parameter to deal with Tensor<double...>.

---
+++ src/Evolution/Systems/GeneralizedHarmonic/GrTags.hpp
@@ -0,0 +1,87 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "DataStructures/DataBoxTag.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GrTagsDeclarations.hpp"
+
+namespace gr {
+namespace Tags {
+template <size_t Dim, typename Frame, typename DataType>
+struct SpacetimeMetric : db::DataBoxTag {
+  using type = tnsr::aa<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "SpacetimeMetric";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct InverseSpacetimeMetric : db::DataBoxTag {
+  using type = tnsr::AA<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "InverseSpacetimeMetric";
+};
+
+template <size_t Dim, typename Frame, typename DataType>
+struct SpatialMetric : db::DataBoxTag {
+  using type = tnsr::ii<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "SpatialMetric";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct InverseSpatialMetric : db::DataBoxTag {
+  using type = tnsr::II<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "InverseSpatialMetric";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct Shift : db::DataBoxTag {
+  using type = tnsr::I<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "Shift";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct Lapse : db::DataBoxTag {
+  using type = Scalar<DataType>;
+  static constexpr db::DataBoxString label = "Lapse";
+};
+
+template <size_t Dim, typename Frame, typename DataType>
+struct DtShift : db::DataBoxTag {
+  using type = tnsr::I<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "DtShift";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct DtLapse : db::DataBoxTag {
+  using type = Scalar<DataType>;
+  static constexpr db::DataBoxString label = "DtLapse";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct DtSpatialMetric : db::DataBoxTag {
+  using type = tnsr::ii<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "DtSpatialMetric";
+};
+
+template <size_t Dim, typename Frame, typename DataType>
+struct SpacetimeChristoffelFirstKind : db::DataBoxTag {
+  using type = tnsr::abb<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "SpacetimeChristoffelFirstKind";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct SpacetimeChristoffelSecondKind : db::DataBoxTag {
+  using type = tnsr::Abb<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "SpactimeChristoffelSecondKind";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct SpacetimeNormalOneForm : db::DataBoxTag {
+  using type = tnsr::a<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "SpacetimeNormalOneForm";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct SpacetimeNormalVector : db::DataBoxTag {
+  using type = tnsr::A<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label = "SpacetimeNormalVector";
+};
+template <size_t Dim, typename Frame, typename DataType>
+struct TraceSpacetimeChristoffelFirstKind : db::DataBoxTag {
+  using type = tnsr::a<DataType, Dim, Frame>;
+  static constexpr db::DataBoxString label =
+      "TraceSpacetimeChristoffelFirstKind";
+};
+}  // namespace Tags
+}  // namespace gr
---
+++ src/Evolution/Systems/GeneralizedHarmonic/GrTagsDeclarations.hpp
@@ -0,0 +1,60 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+#include "DataStructures/Tensor/IndexType.hpp"
+
+class DataVector;
+
+namespace gr {
+namespace Tags {
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct SpacetimeMetric;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct InverseSpacetimeMetric;
+
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct SpatialMetric;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct InverseSpatialMetric;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct Shift;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct Lapse;
+
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct DtShift;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct DtLapse;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct DtSpatialMetric;
+
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct SpacetimeChristoffelFirstKind;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct SpacetimeChristoffelSecondKind;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct SpacetimeNormalOneForm;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct SpacetimeNormalVector;
+template <size_t Dim, typename Frame = Frame::Inertial,
+          typename DataType = DataVector>
+struct TraceSpacetimeChristoffelFirstKind;
+}  // namespace Tags
+}  // namespace gr
