4149444f0e78c2dc99bfdf4c7e976f4a962dccfb
William Throwe <wtt6@cornell.edu>
2017-12-15 00:34:37 -0500

    Add more cases to FluxCommunication test
    * Sending to multiple neighbors in one direction
    * Receiving across a block boundary

--- tests/Unit/NumericalAlgorithms/DiscontinuousGalerkin/Actions/Test_FluxCommunication.cpp
+++ tests/Unit/NumericalAlgorithms/DiscontinuousGalerkin/Actions/Test_FluxCommunication.cpp
@@ -95,14 +95,17 @@ SPECTRE_TEST_CASE("Unit.DiscontinuousGalerkin.Actions.FluxCommunication",
   //      |   0   1   |
   // eta -+ +---+-+-+ eta
   //        |   |X| |
-  //        |   +-+-+
-  //        |   | | |
-  //        +---+-+-+
-  // We run the send action on the indicated element.
+  //        |   +++-+
+  //        |   ||| |
+  //        +---+++-+
+  // We run the actions on the indicated element.
   const ElementId<2> self_id(1, {{{1, 0}, {1, 0}}});
   const ElementId<2> west_id(0);
   const ElementId<2> east_id(1, {{{1, 1}, {1, 0}}});
-  const ElementId<2> south_id(1, {{{1, 0}, {1, 1}}});
+  // These are the halves of the box to the south, not elements in
+  // diagonal directions.
+  const ElementId<2> south_west_id(1, {{{2, 0}, {1, 1}}});
+  const ElementId<2> south_east_id(1, {{{2, 1}, {1, 1}}});
 
   // OrientationMap from block 1 to block 0
   const OrientationMap<2> block_orientation(
@@ -113,13 +116,13 @@ SPECTRE_TEST_CASE("Unit.DiscontinuousGalerkin.Actions.FluxCommunication",
   const CoordinateMaps::Affine eta_map{-1., 1., -2., 4.};
 
   auto start_box =
-      [&extents, &time_id, &self_id, &west_id, &east_id, &south_id,
-       &block_orientation, &xi_map, &eta_map]() {
+      [&extents, &time_id, &self_id, &west_id, &east_id, &south_west_id,
+       &south_east_id, &block_orientation, &xi_map, &eta_map]() {
     const Element<2> element(
         self_id,
        {{Direction<2>::lower_xi(), {{west_id}, block_orientation}},
         {Direction<2>::upper_xi(), {{east_id}, {}}},
-        {Direction<2>::upper_eta(), {{south_id}, {}}}});
+        {Direction<2>::upper_eta(), {{south_west_id, south_east_id}, {}}}});
 
     auto map = ElementMap<2, Frame::Inertial>(
         ElementId<2>{0},
@@ -157,7 +160,8 @@ SPECTRE_TEST_CASE("Unit.DiscontinuousGalerkin.Actions.FluxCommunication",
 
   // Check sent data
   CHECK((runner.nonempty_inboxes<component, fluxes_tag>()) ==
-        (std::unordered_set<Index_t>{west_id, east_id, south_id}));
+        (std::unordered_set<Index_t>{west_id, east_id, south_west_id,
+                                     south_east_id}));
   auto& inboxes = runner.inboxes<component>();
   const auto flux_inbox =
       [&inboxes, &time_id](const ElementId<2>& id) noexcept {
@@ -170,55 +174,57 @@ SPECTRE_TEST_CASE("Unit.DiscontinuousGalerkin.Actions.FluxCommunication",
   CHECK(flux_inbox(east_id).size() == 1);
   CHECK(get<Var>(flux_inbox(east_id)[{Direction<2>::lower_xi(), self_id}]).get()
         == (DataVector{3., 6., 9.}));
-  CHECK(flux_inbox(south_id).size() == 1);
-  CHECK(get<Var>(flux_inbox(south_id)[{Direction<2>::lower_eta(), self_id}])
-        .get()
-        == (DataVector{7., 8., 9.}));
+  CHECK(flux_inbox(south_west_id).size() == 1);
+  CHECK(
+      get<Var>(flux_inbox(south_west_id)[{Direction<2>::lower_eta(), self_id}])
+      .get()
+      == (DataVector{7., 8., 9.}));
+  CHECK(flux_inbox(south_east_id).size() == 1);
+  CHECK(
+      get<Var>(flux_inbox(south_east_id)[{Direction<2>::lower_eta(), self_id}])
+      .get()
+      == (DataVector{7., 8., 9.}));
 
   // Now check ComputeBoundaryFlux
-  // We can't handle the complex boundary yet, so remove it
+  // We can't handle complex boundaries yet, but the actions currently
+  // ignore the complexity and just pretend the elements are
+  // conforming, so we can use that for the test until h-refinement is
+  // implemented.  We do need to get rid of the "extra" southern
+  // neighbor, though.
   db::mutate<Tags::Element<2>>(
       sent_box,
-      [](auto& element) {
+      [&south_west_id](auto& element) {
         auto neighbors = element.neighbors();
-        neighbors.erase(Direction<2>::lower_xi());
+        neighbors[Direction<2>::upper_eta()].set_ids_to({south_west_id});
         element = Element<2>(element.id(), std::move(neighbors));
       });
 
   CHECK_FALSE((runner.is_ready<component, Actions::ComputeBoundaryFlux<System>>(
       sent_box, self_id)));
 
-  // Send from south neighbor
-  {
-    const Element<2> element(
-        south_id, {{Direction<2>::lower_eta(), {{self_id}, {}}}});
+  const auto send_data = [&extents, &runner, &self_id, &time_id](
+      const ElementId<2>& id, const Direction<2>& direction,
+      const OrientationMap<2>& orientation, DataVector volume_data) noexcept {
+    const Element<2> element(id, {{direction, {{self_id}, orientation}}});
 
     Variables<tmpl::list<Var>> variables(extents.product());
-    get<Var>(variables).get() =
-        DataVector{11., 12., 13., 14., 15., 16., 17., 18., 19.};
+    get<Var>(variables).get() = std::move(volume_data);
 
     auto box = db::create<db::AddTags<Tags::TimeId, Tags::Extents<2>,
                                       Tags::Element<2>, System::variables_tag>>(
         time_id, extents, element, std::move(variables));
-    runner.apply<component, Actions::SendDataForFluxes>(box, south_id);
-  }
+    runner.apply<component, Actions::SendDataForFluxes>(box, id);
+  };
+  send_data(south_west_id, Direction<2>::lower_eta(), {},
+            DataVector{11., 12., 13., 14., 15., 16., 17., 18., 19.});
   CHECK_FALSE((runner.is_ready<component, Actions::ComputeBoundaryFlux<System>>(
       sent_box, self_id)));
-
-  // Send from east neighbor
-  {
-    const Element<2> element(
-        east_id, {{Direction<2>::lower_xi(), {{self_id}, {}}}});
-
-    Variables<tmpl::list<Var>> variables(extents.product());
-    get<Var>(variables).get() =
-        DataVector{21., 22., 23., 24., 25., 26., 27., 28., 29.};
-
-    auto box = db::create<db::AddTags<Tags::TimeId, Tags::Extents<2>,
-                                      Tags::Element<2>, System::variables_tag>>(
-        time_id, extents, element, std::move(variables));
-    runner.apply<component, Actions::SendDataForFluxes>(box, east_id);
-  }
+  send_data(east_id, Direction<2>::lower_xi(), {},
+            DataVector{21., 22., 23., 24., 25., 26., 27., 28., 29.});
+  CHECK_FALSE((runner.is_ready<component, Actions::ComputeBoundaryFlux<System>>(
+      sent_box, self_id)));
+  send_data(west_id, Direction<2>::lower_eta(), block_orientation.inverse_map(),
+            DataVector{31., 32., 33., 34., 35., 36., 37., 38., 39.});
   CHECK((runner.is_ready<component, Actions::ComputeBoundaryFlux<System>>(
       sent_box, self_id)));
 
@@ -241,9 +247,9 @@ SPECTRE_TEST_CASE("Unit.DiscontinuousGalerkin.Actions.FluxCommunication",
   // For the test we set n.F* = 11 n.F + 1000 n.F_nbr, so
   //      n.(F* - F) = 10 n.F + 1000 n.F_nbr
   // F_xi = 10 U    F_eta = 20 U
-  const DataVector xi_boundaries{0., 0., 210300.,
-                                 0., 0., 240600.,
-                                 0., 0., 270900.};
+  const DataVector xi_boundaries{-330100., 0., 210300.,
+                                 -320400., 0., 240600.,
+                                 -310700., 0., 270900.};
   const DataVector eta_boundaries{0., 0., 0.,
                                   0., 0., 0.,
                                   221400., 241600., 261800.};
