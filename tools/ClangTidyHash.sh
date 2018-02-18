8a5242009296379976d2313d59ea4c269f8c1abf
Nils Deppe <nd357@cornell.edu>
2018-02-18 10:25:42 -0600

    Add clang-tidy-hash target for Travis compatibility
    Fix #244
    
    Usage is: make clang-tidy-hash HASH=YOUR_HASH
    
    This will check all files modified but not deleted from YOUR_HASH to HEAD.

---
+++ tools/ClangTidyHash.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+if [ $# != 3 ]; then
+    echo "Wrong number of arguments passed to ClangTidyHash.sh"
+    echo "Expecting BUILD_DIR SOURCE_DIR HASH"
+    exit 1
+fi
+
+BUILD_DIR=$1
+SOURCE_DIR=$2
+HASH=$3
+
+###############################################################################
+cd $SOURCE_DIR
+# Get list of non-deleted files
+MODIFIED_FILES=''
+
+for FILENAME in `git diff --name-only ${HASH} HEAD`
+do
+    if [ -f $FILENAME ] \
+           && [ ${FILENAME: -4} == ".cpp" ] \
+           && ! grep -q "FILE_IS_COMPILATION_TEST" $FILENAME; then
+        MODIFIED_FILES="${MODIFIED_FILES} $FILENAME"
+    fi
+done
+
+# Go to build directory and then run clang-tidy, writing output to file
+cd ${BUILD_DIR}
+
+for FILENAME in $MODIFIED_FILES
+do
+    printf "\n\nRunning clang-tidy on file $FILENAME\n"
+    make clang-tidy FILE=$SOURCE_DIR/${FILENAME}
+done
