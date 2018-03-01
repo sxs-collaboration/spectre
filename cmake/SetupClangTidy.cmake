e071577566d87cb6f06983befd1b1a816d80c1f3
Matt Giesler <mgiesler@tapir.caltech.edu>
2018-02-28 21:13:55 -0800

    Disable google-default-arguments warning
    Disable warnings regarding defaulted arguments in virtual functions

--- cmake/SetupClangTidy.cmake
+++ cmake/SetupClangTidy.cmake
@@ -10,13 +10,14 @@
 # -cppcoreguidelines-c-copy-assignment-signature: false positives
 # -cert-err58-cpp: many static variables we use do not throw, and if they do
 #                  we want to terminate anyway
+# -google-default-arguments: defaulting virtual functions in CoordinateMap
 #
 # Notes:
 # misc-move-const-arg: we keep this check because even though this gives
 #                      a lot of annoying warnings about moving trivially
 #                      copyable types, it warns about moving const objects,
 #                      which can have severe performance impacts.
-set(CLANG_TIDY_IGNORE_CHECKS "*,-llvm-header-guard,-google-runtime-int,-readability-else-after-return,-misc-noexcept-move-constructor,-misc-unconventional-assign-operator,-cppcoreguidelines-c-copy-assignment-signature,-modernize-raw-string-literal,-hicpp-noexcept-move,-hicpp-no-assembler,-android-*,-cert-err58-cpp")
+set(CLANG_TIDY_IGNORE_CHECKS "*,-llvm-header-guard,-google-runtime-int,-readability-else-after-return,-misc-noexcept-move-constructor,-misc-unconventional-assign-operator,-cppcoreguidelines-c-copy-assignment-signature,-modernize-raw-string-literal,-hicpp-noexcept-move,-hicpp-no-assembler,-android-*,-cert-err58-cpp,-google-default-arguments")
 
 if(NOT CMAKE_CXX_CLANG_TIDY AND CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   string(
