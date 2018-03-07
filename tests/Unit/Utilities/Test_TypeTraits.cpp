63aa59d66dd76bf2d33fa9f665b7847e6ee5baee
Gabriel Bonilla <gsb76@cornell.edu>
2018-03-07 14:24:41 -0800

    Make remove_reference_wrapper behave like stl type_traits

--- tests/Unit/Utilities/Test_TypeTraits.cpp
+++ tests/Unit/Utilities/Test_TypeTraits.cpp
@@ -550,6 +550,56 @@ static_assert(cpp17::is_same_v<
               "Failed testing remove_reference_wrapper");
 static_assert(cpp17::is_same_v<A, tt::remove_reference_wrapper_t<A>>,
               "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<double, tt::remove_reference_wrapper_t<
+                                 const std::reference_wrapper<double>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<double, tt::remove_reference_wrapper_t<
+                                 volatile std::reference_wrapper<double>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(cpp17::is_same_v<
+                  double, tt::remove_reference_wrapper_t<
+                              const volatile std::reference_wrapper<double>>>,
+              "Failed testing remove_reference_wrapper");
+static_assert(cpp17::is_same_v<const double,
+                               tt::remove_reference_wrapper_t<
+                                   const std::reference_wrapper<const double>>>,
+              "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<const double,
+                     tt::remove_reference_wrapper_t<
+                         volatile std::reference_wrapper<const double>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<const double,
+                     tt::remove_reference_wrapper_t<
+                         const volatile std::reference_wrapper<const double>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<
+        A, tt::remove_reference_wrapper_t<const std::reference_wrapper<A>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<
+        A, tt::remove_reference_wrapper_t<volatile std::reference_wrapper<A>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<A, tt::remove_reference_wrapper_t<
+                            const volatile std::reference_wrapper<A>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<const A, tt::remove_reference_wrapper_t<
+                                  const std::reference_wrapper<const A>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(
+    cpp17::is_same_v<const A, tt::remove_reference_wrapper_t<
+                                  volatile std::reference_wrapper<const A>>>,
+    "Failed testing remove_reference_wrapper");
+static_assert(cpp17::is_same_v<
+                  const A, tt::remove_reference_wrapper_t<
+                               const volatile std::reference_wrapper<const A>>>,
+              "Failed testing remove_reference_wrapper");
 /// [remove_reference_wrapper_example]
 
 /// [remove_cvref_wrap]
@@ -600,11 +650,15 @@ static_assert(
     "Failed testing remove_cvref_wrap");
 static_assert(
     cpp17::is_same_v<tt::remove_cvref_wrap_t<const std::reference_wrapper<int>>,
-                     std::reference_wrapper<int>>,
+                     int>,
+    "Failed testing remove_cvref_wrap");
+static_assert(
+    cpp17::is_same_v<
+        tt::remove_cvref_wrap_t<volatile std::reference_wrapper<int>>, int>,
     "Failed testing remove_cvref_wrap");
 static_assert(
     cpp17::is_same_v<
         tt::remove_cvref_wrap_t<const volatile std::reference_wrapper<int>>,
-        std::reference_wrapper<int>>,
+        int>,
     "Failed testing remove_cvref_wrap");
 /// [remove_cvref_wrap]
