e248d0de9e93fa89802b7ff27f4476d660ae127c
Lawrence Kidder <kidder@astro.cornell.edu>
2018-03-09 23:21:43 -0500

    Whitelist docs/config/footer.html from having license

--- tools/FileTestDefs.sh
+++ tools/FileTestDefs.sh
@@ -209,6 +209,7 @@ license() {
               'cmake/FindPythonModule.cmake$' \
               'cmake/Findcppcheck.cmake$' \
               'cmake/Findcppcheck.cpp$' \
+              'docs/config/footer.html' \
               'LICENSE' \
               '.github/ISSUE_TEMPLATE.md' \
               '.github/PULL_REQUEST_TEMPLATE.md' \
