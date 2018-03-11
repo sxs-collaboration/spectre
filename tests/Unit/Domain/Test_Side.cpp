dff3dfef18ef24befdf6d6488bcd483aea56946e
Gabriel Bonilla <gsb76@cornell.edu>
2018-03-10 16:53:34 -0800

    Add tests for stream operator for Side

--- tests/Unit/Domain/Test_Side.cpp
+++ tests/Unit/Domain/Test_Side.cpp
@@ -4,10 +4,13 @@
 #include <catch.hpp>
 
 #include "Domain/Side.hpp"
+#include "tests/Unit/TestHelpers.hpp"
 #include "tests/Unit/TestingFramework.hpp"
 
 SPECTRE_TEST_CASE("Unit.Domain.Side", "[Domain][Unit]") {
   Side side_lower = Side::Lower;
   CHECK(opposite(side_lower) == Side::Upper);
   CHECK(opposite(opposite(side_lower)) == Side::Lower);
+  CHECK(get_output(side_lower) == "Lower");
+  CHECK(get_output(Side::Upper) == "Upper");
 }
