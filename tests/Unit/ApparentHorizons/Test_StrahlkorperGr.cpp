615058be8ce5219c2c0c95e92a2ff61bdb3fc009
Nils Deppe <nd357@cornell.edu>
2018-03-17 17:14:25 -0400

    Combine Minkowski and KerrSchild tests for Strahlkorper
    Because of an interface difference between Minkowski and Strahlkorper several
    tests have been duplicated. With the unified interface now in place, this commit
    combines the duplicate tests.

--- tests/Unit/ApparentHorizons/Test_StrahlkorperGr.cpp
+++ tests/Unit/ApparentHorizons/Test_StrahlkorperGr.cpp
@@ -21,8 +21,9 @@
 #include "tests/Unit/TestingFramework.hpp"
 
 namespace {
-namespace TestExpansion {
-void test_schwarzschild() {
+template <typename Solution, typename ExpectedLambda>
+void test_expansion(const Solution& solution,
+                    const ExpectedLambda& expected) noexcept {
   // Make surface of radius 2.
   const auto box =
       db::create<db::AddTags<StrahlkorperTags::items_tags<Frame::Inertial>>,
@@ -34,19 +35,14 @@ void test_schwarzschild() {
   const auto& cart_coords =
       db::get<StrahlkorperTags::CartesianCoords<Frame::Inertial>>(box);
 
-  // Schwarzschild solution
-  const double mass = 1.0;
-  const std::array<double, 3> spin{{0.0, 0.0, 0.0}};
-  const std::array<double, 3> center{{0.0, 0.0, 0.0}};
-  EinsteinSolutions::KerrSchild solution(mass, spin, center);
   const auto vars = solution.variables(
-      cart_coords, t, EinsteinSolutions::KerrSchild::tags<DataVector>{});
+      cart_coords, t, typename Solution::template tags<DataVector>{});
 
   const auto& spatial_metric =
       get<gr::Tags::SpatialMetric<3, Frame::Inertial, DataVector>>(vars);
   const auto& deriv_spatial_metric =
-      get<EinsteinSolutions::KerrSchild::DerivSpatialMetric<DataVector>>(
-          vars);
+      get<Tags::deriv<gr::Tags::SpatialMetric<3, Frame::Inertial, DataVector>,
+                      tmpl::size_t<3>, Frame::Inertial>>(vars);
   const auto inverse_spatial_metric =
       determinant_and_inverse(spatial_metric).second;
 
@@ -76,65 +72,16 @@ void test_schwarzschild() {
       gr::extrinsic_curvature(
           get<gr::Tags::Lapse<3, Frame::Inertial, DataVector>>(vars),
           get<gr::Tags::Shift<3, Frame::Inertial, DataVector>>(vars),
-          get<EinsteinSolutions::KerrSchild::DerivShift<DataVector>>(vars),
+          get<Tags::deriv<gr::Tags::Shift<3, Frame::Inertial, DataVector>,
+                          tmpl::size_t<3>, Frame::Inertial>>(vars),
           spatial_metric,
           get<gr::Tags::DtSpatialMetric<3, Frame::Inertial, DataVector>>(vars),
           deriv_spatial_metric));
 
   Approx custom_approx = Approx::custom().epsilon(1.e-12).scale(1.0);
-  CHECK_ITERABLE_CUSTOM_APPROX(
-      get(residual), DataVector(get(residual).size(), 0.0), custom_approx);
+  CHECK_ITERABLE_CUSTOM_APPROX(get(residual), expected(get(residual).size()),
+                               custom_approx);
 }
-
-void test_minkowski() {
-  // Make surface of radius 2.
-  const auto box =
-      db::create<db::AddTags<StrahlkorperTags::items_tags<Frame::Inertial>>,
-                 db::AddComputeItemsTags<
-                     StrahlkorperTags::compute_items_tags<Frame::Inertial>>>(
-          Strahlkorper<Frame::Inertial>(8, 8, 2.0, {{0.0, 0.0, 0.0}}));
-
-  const double t = 0.0;
-  const auto& cart_coords =
-      db::get<StrahlkorperTags::CartesianCoords<Frame::Inertial>>(box);
-
-  EinsteinSolutions::Minkowski<3> solution{};
-
-  const auto deriv_spatial_metric =
-      solution.deriv_spatial_metric(cart_coords, t);
-  const auto inverse_spatial_metric =
-      solution.inverse_spatial_metric(cart_coords, t);
-
-  const DataVector one_over_one_form_magnitude =
-      1.0 / get(magnitude(
-                db::get<StrahlkorperTags::NormalOneForm<Frame::Inertial>>(box),
-                inverse_spatial_metric));
-  const auto unit_normal_one_form = StrahlkorperGr::unit_normal_one_form(
-      db::get<StrahlkorperTags::NormalOneForm<Frame::Inertial>>(box),
-      one_over_one_form_magnitude);
-  const auto grad_unit_normal_one_form =
-      StrahlkorperGr::grad_unit_normal_one_form(
-          db::get<StrahlkorperTags::Rhat<Frame::Inertial>>(box),
-          db::get<StrahlkorperTags::Radius<Frame::Inertial>>(box),
-          unit_normal_one_form,
-          db::get<StrahlkorperTags::D2xRadius<Frame::Inertial>>(box),
-          one_over_one_form_magnitude,
-          raise_or_lower_first_index(
-              gr::christoffel_first_kind(deriv_spatial_metric),
-              inverse_spatial_metric));
-  const auto inverse_surface_metric = StrahlkorperGr::inverse_surface_metric(
-      raise_or_lower_index(unit_normal_one_form, inverse_spatial_metric),
-      inverse_spatial_metric);
-
-  const auto residual = StrahlkorperGr::expansion(
-      grad_unit_normal_one_form, inverse_surface_metric,
-      solution.extrinsic_curvature(cart_coords, t));
-
-  Approx custom_approx = Approx::custom().epsilon(1.e-12);
-  CHECK_ITERABLE_CUSTOM_APPROX(
-      get(residual), DataVector(get(residual).size(), 1.0), custom_approx);
-}
-}  // namespace TestExpansion
 
 namespace TestExtrinsicCurvature {
 void test_minkowski() {
@@ -185,8 +132,11 @@ void test_minkowski() {
 }
 }  // namespace TestExtrinsicCurvature
 
-namespace TestRicciScalar {
-void test_schwarzschild() {
+template <typename Solution, typename SpatialRicciScalar,
+          typename ExpectedLambda>
+void test_ricci_scalar(const Solution& solution,
+                       const SpatialRicciScalar& spatial_ricci_scalar,
+                       const ExpectedLambda& expected) noexcept {
   // Make surface of radius 2.
   const auto box =
       db::create<db::AddTags<StrahlkorperTags::items_tags<Frame::Inertial>>,
@@ -198,19 +148,14 @@ void test_schwarzschild() {
   const auto& cart_coords =
       db::get<StrahlkorperTags::CartesianCoords<Frame::Inertial>>(box);
 
-  // Schwarzschild solution
-  const double mass = 1.0;
-  const std::array<double, 3> spin{{0.0, 0.0, 0.0}};
-  const std::array<double, 3> center{{0.0, 0.0, 0.0}};
-  EinsteinSolutions::KerrSchild solution(mass, spin, center);
   const auto vars = solution.variables(
-      cart_coords, t, EinsteinSolutions::KerrSchild::tags<DataVector>{});
+      cart_coords, t, typename Solution::template tags<DataVector>{});
 
   const auto& spatial_metric =
       get<gr::Tags::SpatialMetric<3, Frame::Inertial, DataVector>>(vars);
   const auto& deriv_spatial_metric =
-      get<EinsteinSolutions::KerrSchild::DerivSpatialMetric<DataVector>>(
-          vars);
+      get<Tags::deriv<gr::Tags::SpatialMetric<3, Frame::Inertial, DataVector>,
+                      tmpl::size_t<3>, Frame::Inertial>>(vars);
   const auto inverse_spatial_metric =
       determinant_and_inverse(spatial_metric).second;
 
@@ -235,73 +180,24 @@ void test_schwarzschild() {
   const auto unit_normal_vector =
       raise_or_lower_index(unit_normal_one_form, inverse_spatial_metric);
   const auto ricci_scalar = StrahlkorperGr::ricci_scalar(
-      TestHelpers::Schwarzschild::spatial_ricci(cart_coords, mass),
-      unit_normal_vector,
+      spatial_ricci_scalar(cart_coords), unit_normal_vector,
       StrahlkorperGr::extrinsic_curvature(
           grad_unit_normal_one_form, unit_normal_one_form, unit_normal_vector),
       inverse_spatial_metric);
 
-  CHECK_ITERABLE_APPROX(get(ricci_scalar), DataVector(get(ricci_scalar).size(),
-                                                      0.5 / square(mass)));
+  CHECK_ITERABLE_APPROX(get(ricci_scalar), expected(get(ricci_scalar).size()));
 }
-
-void test_minkowski() {
-  // Make surface of radius 2.
-  const auto box =
-      db::create<db::AddTags<StrahlkorperTags::items_tags<Frame::Inertial>>,
-                 db::AddComputeItemsTags<
-                     StrahlkorperTags::compute_items_tags<Frame::Inertial>>>(
-          Strahlkorper<Frame::Inertial>(8, 8, 2.0, {{0.0, 0.0, 0.0}}));
-
-  const double t = 0.0;
-  const auto& cart_coords =
-      db::get<StrahlkorperTags::CartesianCoords<Frame::Inertial>>(box);
-
-  EinsteinSolutions::Minkowski<3> solution{};
-
-  const auto deriv_spatial_metric =
-      solution.deriv_spatial_metric(cart_coords, t);
-  const auto inverse_spatial_metric =
-      solution.inverse_spatial_metric(cart_coords, t);
-
-  const DataVector one_over_one_form_magnitude =
-      1.0 / get(magnitude(
-                db::get<StrahlkorperTags::NormalOneForm<Frame::Inertial>>(box),
-                inverse_spatial_metric));
-  const auto unit_normal_one_form = StrahlkorperGr::unit_normal_one_form(
-      db::get<StrahlkorperTags::NormalOneForm<Frame::Inertial>>(box),
-      one_over_one_form_magnitude);
-  const auto grad_unit_normal_one_form =
-      StrahlkorperGr::grad_unit_normal_one_form(
-          db::get<StrahlkorperTags::Rhat<Frame::Inertial>>(box),
-          db::get<StrahlkorperTags::Radius<Frame::Inertial>>(box),
-          unit_normal_one_form,
-          db::get<StrahlkorperTags::D2xRadius<Frame::Inertial>>(box),
-          one_over_one_form_magnitude,
-          raise_or_lower_first_index(
-              gr::christoffel_first_kind(deriv_spatial_metric),
-              inverse_spatial_metric));
-
-  const auto unit_normal_vector =
-      raise_or_lower_index(unit_normal_one_form, inverse_spatial_metric);
-  const auto extrinsic_curvature = StrahlkorperGr::extrinsic_curvature(
-      grad_unit_normal_one_form, unit_normal_one_form, unit_normal_vector);
-
-  const auto ricci_scalar = StrahlkorperGr::ricci_scalar(
-      make_with_value<tnsr::ii<DataVector, 3, Frame::Inertial>>(
-          inverse_spatial_metric, 0.0),
-      unit_normal_vector, extrinsic_curvature, inverse_spatial_metric);
-
-  CHECK_ITERABLE_APPROX(get(ricci_scalar),
-                        DataVector(get(ricci_scalar).size(), 0.5));
-}
-}  // namespace TestRicciScalar
 }  // namespace
 
 SPECTRE_TEST_CASE("Unit.ApparentHorizons.StrahlkorperGr.Expansion",
                   "[ApparentHorizons][Unit]") {
-  TestExpansion::test_schwarzschild();
-  TestExpansion::test_minkowski();
+  test_expansion(
+      EinsteinSolutions::KerrSchild{1.0, {{0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}}},
+      [](const size_t size) noexcept { return DataVector(size, 0.0); });
+  test_expansion(
+      EinsteinSolutions::Minkowski<3>{}, [](const size_t size) noexcept {
+        return DataVector(size, 1.0);
+      });
 }
 
 SPECTRE_TEST_CASE("Unit.ApparentHorizons.StrahlkorperGr.ExtrinsicCurvature",
@@ -315,6 +211,21 @@ SPECTRE_TEST_CASE("Unit.ApparentHorizons.StrahlkorperGr.ExtrinsicCurvature",
 
 SPECTRE_TEST_CASE("Unit.ApparentHorizons.StrahlkorperGr.RicciScalar",
                   "[ApparentHorizons][Unit]") {
-  TestRicciScalar::test_schwarzschild();
-  TestRicciScalar::test_minkowski();
+  const double mass = 1.0;
+  test_ricci_scalar(
+      EinsteinSolutions::KerrSchild(mass, {{0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}}),
+      [&mass](const auto& cartesian_coords) noexcept {
+        return TestHelpers::Schwarzschild::spatial_ricci(cartesian_coords,
+                                                         mass);
+      },
+      [&mass](const size_t size) noexcept {
+        return DataVector(size, 0.5 / square(mass));
+      });
+  test_ricci_scalar(
+      EinsteinSolutions::Minkowski<3>{},
+      [](const auto& cartesian_coords) noexcept {
+        return make_with_value<tnsr::ii<DataVector, 3, Frame::Inertial>>(
+            cartesian_coords, 0.0);
+      },
+      [](const size_t size) noexcept { return DataVector(size, 0.5); });
 }
