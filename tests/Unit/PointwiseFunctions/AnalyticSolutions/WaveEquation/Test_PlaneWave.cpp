d1429f91d849a3704fa330a2a74222814985b753
Nils Deppe <nd357@cornell.edu>
2018-03-20 11:36:40 -0400

    Simplify PlaneWave tests

--- tests/Unit/PointwiseFunctions/AnalyticSolutions/WaveEquation/Test_PlaneWave.cpp
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/WaveEquation/Test_PlaneWave.cpp
@@ -32,207 +32,115 @@ inline tnsr::I<double, 3, Frame::Inertial> extract_point_from_coords(
       {x.get(0)[offset], x.get(1)[offset], x.get(2)[offset]}}};
 }
 
-template <size_t Dim>
-void check_solution_x(const double kx, const double omega, const DataVector& u,
-                      const ScalarWave::Solutions::PlaneWave<Dim>& pw,
-                      const tnsr::I<DataVector, Dim>& x, const double t) {
-  const DataVector psi = cube(u);
-  const DataVector dpsi_dt = -3.0 * omega * square(u);
-  const DataVector dpsi_dx = 3.0 * kx * square(u);
-  const DataVector d2psi_dt2 = 6.0 * square(omega) * u;
-  const DataVector d2psi_dtdx = -6.0 * omega * kx * u;
-  const DataVector d2psi_dxdx = 6.0 * square(kx) * u;
-  CHECK_ITERABLE_APPROX(psi, pw.psi(x, t).get());
-  CHECK_ITERABLE_APPROX(dpsi_dt, pw.dpsi_dt(x, t).get());
-  CHECK_ITERABLE_APPROX(d2psi_dt2, pw.d2psi_dt2(x, t).get());
-  CHECK_ITERABLE_APPROX(dpsi_dx, pw.dpsi_dx(x, t).get(0));
-  CHECK_ITERABLE_APPROX(d2psi_dtdx, pw.d2psi_dtdx(x, t).get(0));
-  CHECK_ITERABLE_APPROX(d2psi_dxdx, pw.d2psi_dxdx(x, t).get(0, 0));
-  for (size_t s = 0; s < u.size(); ++s) {
+template <size_t Dim, size_t DimSolution>
+void check_solution(
+    const DataVector& expected_psi, const DataVector& expected_dpsi_dt,
+    const DataVector& expected_d2psi_dt2, const DataVector& expected_dpsi_dlast,
+    const std::array<DataVector, Dim + 1>& expected_second_derivs,
+    const ScalarWave::Solutions::PlaneWave<DimSolution>& pw,
+    const tnsr::I<DataVector, DimSolution>& x, const double t) noexcept {
+  // expected_second_derivs is:
+  // 0 -> d^2 psi / dt dx^(Dim-1)
+  // 1-3 -> d^2 psi / dx^(Dim - 1) dx^i where i is in [0, 2]
+  CHECK_ITERABLE_APPROX(expected_psi, pw.psi(x, t).get());
+  CHECK_ITERABLE_APPROX(expected_dpsi_dt, pw.dpsi_dt(x, t).get());
+  CHECK_ITERABLE_APPROX(expected_d2psi_dt2, pw.d2psi_dt2(x, t).get());
+  CHECK_ITERABLE_APPROX(expected_dpsi_dlast, pw.dpsi_dx(x, t).get(Dim - 1));
+  CHECK_ITERABLE_APPROX(expected_second_derivs[0],
+                        pw.d2psi_dtdx(x, t).get(Dim - 1));
+  for (size_t i = 0; i < Dim; ++i) {
+    CHECK_ITERABLE_APPROX(gsl::at(expected_second_derivs, i + 1),
+                          pw.d2psi_dxdx(x, t).get(i, Dim - 1));
+    if (i < Dim - 1) {
+      CHECK_ITERABLE_APPROX(gsl::at(expected_second_derivs, i + 1),
+                            pw.d2psi_dxdx(x, t).get(Dim - 1, i));
+    }
+  }
+
+  for (size_t s = 0; s < x.get(0).size(); ++s) {
     const auto p = extract_point_from_coords(s, x);
-    CHECK(approx(psi[s]) == pw.psi(p, t).get());
-    CHECK(approx(dpsi_dt[s]) == pw.dpsi_dt(p, t).get());
-    CHECK(approx(d2psi_dt2[s]) == pw.d2psi_dt2(p, t).get());
-    CHECK(approx(dpsi_dx[s]) == pw.dpsi_dx(p, t).get(0));
-    CHECK(approx(d2psi_dtdx[s]) == pw.d2psi_dtdx(p, t).get(0));
-    CHECK(approx(d2psi_dxdx[s]) == pw.d2psi_dxdx(p, t).get(0, 0));
+    CHECK_ITERABLE_APPROX(expected_psi[s], pw.psi(p, t).get());
+    CHECK_ITERABLE_APPROX(expected_dpsi_dt[s], pw.dpsi_dt(p, t).get());
+    CHECK_ITERABLE_APPROX(expected_d2psi_dt2[s], pw.d2psi_dt2(p, t).get());
+    CHECK_ITERABLE_APPROX(expected_dpsi_dlast[s],
+                          pw.dpsi_dx(p, t).get(Dim - 1));
+    CHECK_ITERABLE_APPROX(expected_second_derivs[0][s],
+                          pw.d2psi_dtdx(p, t).get(Dim - 1));
+    for (size_t i = 0; i < Dim; ++i) {
+      CHECK_ITERABLE_APPROX(gsl::at(expected_second_derivs, i + 1)[s],
+                            pw.d2psi_dxdx(p, t).get(i, Dim - 1));
+      if (i < Dim - 1) {
+        CHECK_ITERABLE_APPROX(gsl::at(expected_second_derivs, i + 1)[s],
+                              pw.d2psi_dxdx(p, t).get(Dim - 1, i));
+      }
+    }
   }
 
   CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Psi>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<Dim>, ScalarWave::Psi>{})),
+      get<ScalarWave::Psi>(
+          pw.variables(x, t,
+                       tmpl::list<ScalarWave::Pi, ScalarWave::Phi<DimSolution>,
+                                  ScalarWave::Psi>{})),
       pw.psi(x, t));
   CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Phi<Dim>>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<Dim>, ScalarWave::Psi>{})),
+      get<ScalarWave::Phi<DimSolution>>(
+          pw.variables(x, t,
+                       tmpl::list<ScalarWave::Pi, ScalarWave::Phi<DimSolution>,
+                                  ScalarWave::Psi>{})),
       pw.dpsi_dx(x, t));
   CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Pi>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<Dim>, ScalarWave::Psi>{})),
+      get<ScalarWave::Pi>(
+          pw.variables(x, t,
+                       tmpl::list<ScalarWave::Pi, ScalarWave::Phi<DimSolution>,
+                                  ScalarWave::Psi>{})),
       Scalar<DataVector>(-1.0 * pw.dpsi_dt(x, t).get()));
 
+  CHECK_ITERABLE_APPROX(get<Tags::dt<ScalarWave::Psi>>(pw.variables(
+                            x, t,
+                            tmpl::list<Tags::dt<ScalarWave::Pi>,
+                                       Tags::dt<ScalarWave::Phi<DimSolution>>,
+                                       Tags::dt<ScalarWave::Psi>>{})),
+                        pw.dpsi_dt(x, t));
   CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Psi>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<Dim>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      pw.dpsi_dt(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Phi<Dim>>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<Dim>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
+      get<Tags::dt<ScalarWave::Phi<DimSolution>>>(
+          pw.variables(x, t,
+                       tmpl::list<Tags::dt<ScalarWave::Pi>,
+                                  Tags::dt<ScalarWave::Phi<DimSolution>>,
+                                  Tags::dt<ScalarWave::Psi>>{})),
       pw.d2psi_dtdx(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Pi>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<Dim>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      Scalar<DataVector>(-1.0 * pw.d2psi_dt2(x, t).get()));
-}
-
-template <size_t Dim>
-void check_solution_y(const double kx, const double ky, const double omega,
-                      const DataVector& u,
-                      const ScalarWave::Solutions::PlaneWave<Dim>& pw,
-                      const tnsr::I<DataVector, Dim>& x, const double t) {
-  const DataVector dpsi_dy = 3.0 * ky * square(u);
-  const DataVector d2psi_dtdy = -6.0 * omega * ky * u;
-  const DataVector d2psi_dxdy = 6.0 * kx * ky * u;
-  const DataVector d2psi_dydy = 6.0 * square(ky) * u;
-  CHECK_ITERABLE_APPROX(dpsi_dy, pw.dpsi_dx(x, t).get(1));
-  CHECK_ITERABLE_APPROX(d2psi_dtdy, pw.d2psi_dtdx(x, t).get(1));
-  CHECK_ITERABLE_APPROX(d2psi_dxdy, pw.d2psi_dxdx(x, t).get(0, 1));
-  CHECK_ITERABLE_APPROX(d2psi_dxdy, pw.d2psi_dxdx(x, t).get(1, 0));
-  CHECK_ITERABLE_APPROX(d2psi_dydy, pw.d2psi_dxdx(x, t).get(1, 1));
-  for (size_t s = 0; s < u.size(); ++s) {
-    const auto p = extract_point_from_coords(s, x);
-    CHECK(approx(dpsi_dy[s]) == pw.dpsi_dx(p, t).get(1));
-    CHECK(approx(d2psi_dtdy[s]) == pw.d2psi_dtdx(p, t).get(1));
-    CHECK(approx(d2psi_dxdy[s]) == pw.d2psi_dxdx(p, t).get(0, 1));
-    CHECK(approx(d2psi_dxdy[s]) == pw.d2psi_dxdx(p, t).get(1, 0));
-    CHECK(approx(d2psi_dydy[s]) == pw.d2psi_dxdx(p, t).get(1, 1));
-  }
-
-  CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Psi>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<Dim>, ScalarWave::Psi>{})),
-      pw.psi(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Phi<Dim>>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<Dim>, ScalarWave::Psi>{})),
-      pw.dpsi_dx(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Pi>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<Dim>, ScalarWave::Psi>{})),
-      Scalar<DataVector>(-1.0 * pw.dpsi_dt(x, t).get()));
-
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Psi>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<Dim>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      pw.dpsi_dt(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Phi<Dim>>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<Dim>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      pw.d2psi_dtdx(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Pi>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<Dim>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      Scalar<DataVector>(-1.0 * pw.d2psi_dt2(x, t).get()));
-}
-
-void check_solution_z(const double kx, const double ky, const double kz,
-                      const double omega, const DataVector& u,
-                      const ScalarWave::Solutions::PlaneWave<3>& pw,
-                      const tnsr::I<DataVector, 3>& x, const double t) {
-  const DataVector dpsi_dz = 3.0 * kz * square(u);
-  const DataVector d2psi_dtdz = -6.0 * omega * kz * u;
-  const DataVector d2psi_dxdz = 6.0 * kx * kz * u;
-  const DataVector d2psi_dydz = 6.0 * ky * kz * u;
-  const DataVector d2psi_dzdz = 6.0 * square(kz) * u;
-  CHECK_ITERABLE_APPROX(dpsi_dz, pw.dpsi_dx(x, t).get(2));
-  CHECK_ITERABLE_APPROX(d2psi_dtdz, pw.d2psi_dtdx(x, t).get(2));
-  CHECK_ITERABLE_APPROX(d2psi_dxdz, pw.d2psi_dxdx(x, t).get(0, 2));
-  CHECK_ITERABLE_APPROX(d2psi_dxdz, pw.d2psi_dxdx(x, t).get(2, 0));
-  CHECK_ITERABLE_APPROX(d2psi_dydz, pw.d2psi_dxdx(x, t).get(2, 1));
-  CHECK_ITERABLE_APPROX(d2psi_dydz, pw.d2psi_dxdx(x, t).get(1, 2));
-  CHECK_ITERABLE_APPROX(d2psi_dzdz, pw.d2psi_dxdx(x, t).get(2, 2));
-  for (size_t s = 0; s < u.size(); ++s) {
-    const auto p = extract_point_from_coords(s, x);
-    CHECK(approx(dpsi_dz[s]) == pw.dpsi_dx(p, t).get(2));
-    CHECK(approx(d2psi_dtdz[s]) == pw.d2psi_dtdx(p, t).get(2));
-    CHECK(approx(d2psi_dxdz[s]) == pw.d2psi_dxdx(p, t).get(0, 2));
-    CHECK(approx(d2psi_dxdz[s]) == pw.d2psi_dxdx(p, t).get(2, 0));
-    CHECK(approx(d2psi_dydz[s]) == pw.d2psi_dxdx(p, t).get(2, 1));
-    CHECK(approx(d2psi_dydz[s]) == pw.d2psi_dxdx(p, t).get(1, 2));
-    CHECK(approx(d2psi_dzdz[s]) == pw.d2psi_dxdx(p, t).get(2, 2));
-  }
-
-  CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Psi>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<3>, ScalarWave::Psi>{})),
-      pw.psi(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Phi<3>>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<3>, ScalarWave::Psi>{})),
-      pw.dpsi_dx(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<ScalarWave::Pi>(pw.variables(
-          x, t,
-          tmpl::list<ScalarWave::Pi, ScalarWave::Phi<3>, ScalarWave::Psi>{})),
-      Scalar<DataVector>(-1.0 * pw.dpsi_dt(x, t).get()));
-
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Psi>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<3>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      pw.dpsi_dt(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Phi<3>>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<3>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      pw.d2psi_dtdx(x, t));
-  CHECK_ITERABLE_APPROX(
-      get<Tags::dt<ScalarWave::Pi>>(pw.variables(
-          x, t,
-          tmpl::list<Tags::dt<ScalarWave::Pi>, Tags::dt<ScalarWave::Phi<3>>,
-                     Tags::dt<ScalarWave::Psi>>{})),
-      Scalar<DataVector>(-1.0 * pw.d2psi_dt2(x, t).get()));
+  CHECK_ITERABLE_APPROX(get<Tags::dt<ScalarWave::Pi>>(pw.variables(
+                            x, t,
+                            tmpl::list<Tags::dt<ScalarWave::Pi>,
+                                       Tags::dt<ScalarWave::Phi<DimSolution>>,
+                                       Tags::dt<ScalarWave::Psi>>{})),
+                        Scalar<DataVector>(-1.0 * pw.d2psi_dt2(x, t).get()));
 }
 
 void test_1d() {
-  const double k = -1.5;
+  const double kx = -1.5;
   const double center_x = 2.4;
-  const double omega = std::abs(k);
+  const double omega = std::abs(kx);
   const double t = 3.1;
   const double x1 = -0.2;
   const double x2 = 8.7;
   const tnsr::I<DataVector, 1> x(DataVector({x1, x2}));
   const DataVector u(
-      {k * (x1 - center_x) - omega * t, k * (x2 - center_x) - omega * t});
+      {kx * (x1 - center_x) - omega * t, kx * (x2 - center_x) - omega * t});
   const ScalarWave::Solutions::PlaneWave<1> pw(
-      {{k}}, {{center_x}}, std::make_unique<MathFunctions::PowX>(3));
-  check_solution_x(k, omega, u, pw, x, t);
+      {{kx}}, {{center_x}}, std::make_unique<MathFunctions::PowX>(3));
+  check_solution<1>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kx * square(u),
+      std::array<DataVector, 2>{{-6.0 * omega * kx * u, 6.0 * square(kx) * u}},
+      pw, x, t);
 
   Parallel::register_derived_classes_with_charm<MathFunction<1>>();
   const auto deserialized_pw = serialize_and_deserialize(pw);
-  check_solution_x(k, omega, u, deserialized_pw, x, t);
+  check_solution<1>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kx * square(u),
+      std::array<DataVector, 2>{{-6.0 * omega * kx * u, 6.0 * square(kx) * u}},
+      deserialized_pw, x, t);
 
   test_creation<ScalarWave::Solutions::PlaneWave<1>>(
       "  WaveVector: [3.5]\n"
@@ -260,13 +168,31 @@ void test_2d() {
   const ScalarWave::Solutions::PlaneWave<2> pw(
       {{kx, ky}}, {{center_x, center_y}},
       std::make_unique<MathFunctions::PowX>(3));
-  check_solution_x(kx, omega, u, pw, x, t);
-  check_solution_y(kx, ky, omega, u, pw, x, t);
+  check_solution<1>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kx * square(u),
+      std::array<DataVector, 2>{{-6.0 * omega * kx * u, 6.0 * square(kx) * u}},
+      pw, x, t);
+  check_solution<2>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * ky * square(u),
+      std::array<DataVector, 3>{
+          {-6.0 * omega * ky * u, 6.0 * kx * ky * u, 6.0 * square(ky) * u}},
+      pw, x, t);
 
   Parallel::register_derived_classes_with_charm<MathFunction<1>>();
   const auto deserialized_pw = serialize_and_deserialize(pw);
-  check_solution_x(kx, omega, u, deserialized_pw, x, t);
-  check_solution_y(kx, ky, omega, u, deserialized_pw, x, t);
+  check_solution<1>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kx * square(u),
+      std::array<DataVector, 2>{{-6.0 * omega * kx * u, 6.0 * square(kx) * u}},
+      deserialized_pw, x, t);
+  check_solution<2>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * ky * square(u),
+      std::array<DataVector, 3>{
+          {-6.0 * omega * ky * u, 6.0 * kx * ky * u, 6.0 * square(ky) * u}},
+      deserialized_pw, x, t);
 
   test_creation<ScalarWave::Solutions::PlaneWave<2>>(
       "  WaveVector: [-2, 3.5]\n"
@@ -300,15 +226,43 @@ void test_3d() {
   const ScalarWave::Solutions::PlaneWave<3> pw(
       {{kx, ky, kz}}, {{center_x, center_y, center_z}},
       std::make_unique<MathFunctions::PowX>(3));
-  check_solution_x(kx, omega, u, pw, x, t);
-  check_solution_y(kx, ky, omega, u, pw, x, t);
-  check_solution_z(kx, ky, kz, omega, u, pw, x, t);
+  check_solution<1>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kx * square(u),
+      std::array<DataVector, 2>{{-6.0 * omega * kx * u, 6.0 * square(kx) * u}},
+      pw, x, t);
+  check_solution<2>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * ky * square(u),
+      std::array<DataVector, 3>{
+          {-6.0 * omega * ky * u, 6.0 * kx * ky * u, 6.0 * square(ky) * u}},
+      pw, x, t);
+  check_solution<3>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kz * square(u),
+      std::array<DataVector, 4>{{-6.0 * omega * kz * u, 6.0 * kx * kz * u,
+                                 6.0 * ky * kz * u, 6.0 * square(kz) * u}},
+      pw, x, t);
 
   Parallel::register_derived_classes_with_charm<MathFunction<1>>();
   const auto deserialized_pw = serialize_and_deserialize(pw);
-  check_solution_x(kx, omega, u, deserialized_pw, x, t);
-  check_solution_y(kx, ky, omega, u, deserialized_pw, x, t);
-  check_solution_z(kx, ky, kz, omega, u, deserialized_pw, x, t);
+  check_solution<1>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kx * square(u),
+      std::array<DataVector, 2>{{-6.0 * omega * kx * u, 6.0 * square(kx) * u}},
+      deserialized_pw, x, t);
+  check_solution<2>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * ky * square(u),
+      std::array<DataVector, 3>{
+          {-6.0 * omega * ky * u, 6.0 * kx * ky * u, 6.0 * square(ky) * u}},
+      deserialized_pw, x, t);
+  check_solution<3>(
+      cube(u), -3.0 * omega * square(u), 6.0 * square(omega) * u,
+      3.0 * kz * square(u),
+      std::array<DataVector, 4>{{-6.0 * omega * kz * u, 6.0 * kx * kz * u,
+                                 6.0 * ky * kz * u, 6.0 * square(kz) * u}},
+      deserialized_pw, x, t);
 
   test_creation<ScalarWave::Solutions::PlaneWave<3>>(
       "  WaveVector: [-1, -2, 3.5]\n"
