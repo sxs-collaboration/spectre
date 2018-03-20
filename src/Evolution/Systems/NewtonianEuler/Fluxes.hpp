2e6dd05ec81b29c9c2c39d6c0293b519a1fcf33e
Cristobal Armaza <ca455@cornell.edu>
2018-03-20 11:15:01 -0400

    Add volume fluxes for the Newtonian Euler system

---
+++ src/Evolution/Systems/NewtonianEuler/Fluxes.hpp
@@ -0,0 +1,79 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+#include "DataStructures/DataBox/DataBoxTag.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+namespace gsl {
+template <typename T>
+class not_null;
+}  // namespace gsl
+
+class DataVector;
+
+namespace Tags {
+template <typename>
+struct Fluxes;
+}  // namespace Tags
+
+namespace NewtonianEuler {
+struct MassDensity;
+template <size_t Dim>
+struct MomentumDensity;
+struct EnergyDensity;
+template <size_t Dim>
+struct Velocity;
+struct Pressure;
+}  // namespace NewtonianEuler
+/// \endcond
+
+namespace NewtonianEuler {
+
+/*!
+ * \brief Compute the fluxes of the conservative variables of the
+ * Newtonian Euler system
+ *
+ * The fluxes are \f$(\text{Dim} + 2)\f$ vectors of
+ * dimension \f$\text{Dim}\f$. Denoting the flux of the conservative
+ * variable \f$u\f$ as \f$F(u)\f$, one has
+ *
+ * \f{align*}
+ * F^i(\rho) &= S^i\\
+ * F^i(S^j) &= S^i v^j + \delta^{ij}p\\
+ * F^i(e) &= (e + p)v^i
+ * \f}
+ *
+ * where \f$S^i\f$ is the momentum density, \f$e\f$ is the energy density,
+ * \f$v^i\f$ is the velocity, \f$p\f$ is the pressure, and \f$\delta^{ij}\f$
+ * is the Kronecker delta. This form of the fluxes combines conservative and
+ * primitive variables (while the velocity appears explicitly, the pressure
+ * implicitly depends, for instance, on the mass density and the specific
+ * internal energy), so the conversion from one variable set to the other
+ * must be known prior to calling this function.
+ */
+template <size_t Dim>
+struct ComputeFluxes {
+  using return_tags =
+      tmpl::list<Tags::Fluxes<MassDensity>, Tags::Fluxes<MomentumDensity<Dim>>,
+                 Tags::Fluxes<EnergyDensity>>;
+
+  using argument_tags =
+      tmpl::list<MomentumDensity<Dim>, EnergyDensity, Velocity<Dim>, Pressure>;
+
+  static void apply(
+      gsl::not_null<tnsr::I<DataVector, Dim>*> mass_density_flux,
+      gsl::not_null<tnsr::IJ<DataVector, Dim>*> momentum_density_flux,
+      gsl::not_null<tnsr::I<DataVector, Dim>*> energy_density_flux,
+      const tnsr::I<DataVector, Dim>& momentum_density,
+      const Scalar<DataVector>& energy_density,
+      const tnsr::I<DataVector, Dim>& velocity,
+      const Scalar<DataVector>& pressure) noexcept;
+};
+
+}  // namespace NewtonianEuler
---
+++ tests/Unit/Evolution/Systems/NewtonianEuler/Test_Fluxes.cpp
@@ -0,0 +1,34 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include <cstddef>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Evolution/Systems/NewtonianEuler/Fluxes.hpp"
+#include "tests/Unit/Pypp/CheckWithRandomValues.hpp"
+#include "tests/Unit/Pypp/SetupLocalPythonEnvironment.hpp"
+#include "tests/Unit/TestingFramework.hpp"
+
+namespace {
+
+template <size_t Dim>
+void test_fluxes(const DataVector& used_for_size) {
+  pypp::check_with_random_values<4>(
+      &NewtonianEuler::ComputeFluxes<Dim>::apply, "TestFunctions",
+      {"mass_density_flux", "momentum_density_flux", "energy_density_flux"},
+      {{{-1.0, 1.0}, {-1.0, 1.0}, {-1.0, 1.0}, {-1.0, 1.0}}}, used_for_size);
+}
+
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.Evolution.Systems.NewtonianEuler.Fluxes",
+                  "[Unit][Evolution]") {
+  pypp::SetupLocalPythonEnvironment local_python_env{
+      "Evolution/Systems/NewtonianEuler"};
+
+  const DataVector dv(5);
+  test_fluxes<1>(dv);
+  test_fluxes<2>(dv);
+  test_fluxes<3>(dv);
+}
