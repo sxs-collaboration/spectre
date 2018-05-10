8cfa16c328f2bf867e662f6bd5dcdefa31e6433d
Eamonn O'Shea <edo27@cornell.edu>
2018-05-10 14:02:37 -0400

    Allow Pypp to work with member functions which return by TaggedTuple

--- tests/Unit/Pypp/PyppPyTests.py
+++ tests/Unit/Pypp/PyppPyTests.py
@@ -220,5 +220,15 @@ def check2_by_value1_class1(t0, t1, a):
 def check2_by_value2_class1(t0, t1, a, b):
     return 2.0 * t0 + 5.0 * a + t1 * b
 
+
 def permute_array(a):
     return [a[2], a[0], a[1]]
+
+
+def check_solution_scalar(x, t, a, b):
+    return np.dot(x, b) + a - t
+
+
+def check_solution_vector(x, t, a, b):
+    # b is passed in as a list so must be explicitly converted to an array
+    return a * x - t * np.array(b)
