0f7e4cea9ed1f00334b3ed7b87a11d2d4f5849b4
Gabriel Bonilla <gsb76@cornell.edu>
2018-05-21 08:45:52 -0700

    Add EquatorialCompression map

---
+++ src/Domain/CoordinateMaps/EquatorialCompression.cpp
@@ -0,0 +1,179 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Domain/CoordinateMaps/EquatorialCompression.hpp"
+
+#include <cmath>
+#include <pup.h>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/SegmentId.hpp"  // IWYU pragma: keep
+#include "ErrorHandling/Assert.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/ContainerHelpers.hpp"
+#include "Utilities/DereferenceWrapper.hpp"
+#include "Utilities/EqualWithinRoundoff.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeWithValue.hpp"
+
+namespace CoordinateMaps {
+
+EquatorialCompression::EquatorialCompression(const double aspect_ratio) noexcept
+    : aspect_ratio_(aspect_ratio), inverse_aspect_ratio_(1.0 / aspect_ratio) {
+  ASSERT(aspect_ratio > 0.0, "The aspect_ratio must be greater than zero.");
+}
+
+template <typename T>
+std::array<tt::remove_cvref_wrap_t<T>, 3>
+EquatorialCompression::angular_distortion(const std::array<T, 3>& coords,
+                                          const double inverse_alpha) const
+    noexcept {
+  using ReturnType = tt::remove_cvref_wrap_t<T>;
+  const ReturnType& x = coords[0];
+  const ReturnType& y = coords[1];
+  const ReturnType& z = coords[2];
+  const ReturnType rho =
+      sqrt(square(x) + square(y) + square(inverse_alpha * z));
+  ReturnType radius_over_rho = sqrt(square(x) + square(y) + square(z));
+  for (size_t i = 0; i < get_size(rho); i++) {
+    if (LIKELY(not equal_within_roundoff(get_element(rho, i), 0.0))) {
+      get_element(radius_over_rho, i) /= get_element(rho, i);
+    } else {
+      ASSERT(equal_within_roundoff(get_element(radius_over_rho, i), 0.0),
+             "r == 0 must imply rho == 0. This has failed.");
+    }
+  }
+  return std::array<ReturnType, 3>{{radius_over_rho * x, radius_over_rho * y,
+                                    inverse_alpha * radius_over_rho * z}};
+}
+
+template <typename T>
+tnsr::Ij<tt::remove_cvref_wrap_t<T>, 3, Frame::NoFrame>
+EquatorialCompression::angular_distortion_jacobian(
+    const std::array<T, 3>& coords, const double inverse_alpha) const noexcept {
+  using ReturnType = tt::remove_cvref_wrap_t<T>;
+  const ReturnType& x = coords[0];
+  const ReturnType& y = coords[1];
+  const ReturnType& z = coords[2];
+  const ReturnType radius = sqrt(square(x) + square(y) + square(z));
+  const ReturnType rho =
+      sqrt(square(x) + square(y) + square(inverse_alpha * z));
+  // Various common factors:
+  const ReturnType lambda1 = (pow<2>(inverse_alpha) - 1.0) * z * z;
+  const ReturnType lambda2 = (1.0 - pow<2>(inverse_alpha)) * (x * x + y * y);
+  const ReturnType lambda3 = pow<2>(rho * radius);
+  const ReturnType lambda4 = radius * pow<3>(rho);
+
+  auto jacobian_matrix =
+      make_with_value<tnsr::Ij<tt::remove_cvref_wrap_t<T>, 3, Frame::NoFrame>>(
+          dereference_wrapper(coords[0]), 0.0);
+
+  get<0, 0>(jacobian_matrix) = (x * x * lambda1 + lambda3);
+  get<1, 0>(jacobian_matrix) = x * y * lambda1;
+  get<2, 0>(jacobian_matrix) = inverse_alpha * x * z * lambda1;
+
+  get<0, 1>(jacobian_matrix) = get<1, 0>(jacobian_matrix);
+  get<1, 1>(jacobian_matrix) = (y * y * lambda1 + lambda3);
+  get<2, 1>(jacobian_matrix) = inverse_alpha * y * z * lambda1;
+
+  get<0, 2>(jacobian_matrix) = x * z * lambda2;
+  get<1, 2>(jacobian_matrix) = y * z * lambda2;
+  get<2, 2>(jacobian_matrix) = inverse_alpha * (z * z * lambda2 + lambda3);
+
+  for (size_t i = 0; i < get_size(radius); i++) {
+    if (LIKELY(not equal_within_roundoff(get_element(lambda4, i), 0.0))) {
+      const double one_over_lambda4 = 1.0 / get_element(lambda4, i);
+      get_element(get<0, 0>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<1, 0>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<2, 0>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<0, 1>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<1, 1>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<2, 1>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<0, 2>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<1, 2>(jacobian_matrix), i) *= one_over_lambda4;
+      get_element(get<2, 2>(jacobian_matrix), i) *= one_over_lambda4;
+    } else {
+      // Let the jacobian of this map be the identity at the origin:
+      get_element(get<0, 0>(jacobian_matrix), i) = 1.0;
+      get_element(get<1, 0>(jacobian_matrix), i) = 0.0;
+      get_element(get<2, 0>(jacobian_matrix), i) = 0.0;
+      get_element(get<0, 1>(jacobian_matrix), i) = 0.0;
+      get_element(get<1, 1>(jacobian_matrix), i) = 1.0;
+      get_element(get<2, 1>(jacobian_matrix), i) = 0.0;
+      get_element(get<0, 2>(jacobian_matrix), i) = 0.0;
+      get_element(get<1, 2>(jacobian_matrix), i) = 0.0;
+      get_element(get<2, 2>(jacobian_matrix), i) = 1.0;
+    }
+  }
+  return jacobian_matrix;
+}
+
+template <typename T>
+std::array<tt::remove_cvref_wrap_t<T>, 3> EquatorialCompression::operator()(
+    const std::array<T, 3>& source_coords) const noexcept {
+  return angular_distortion(source_coords, inverse_aspect_ratio_);
+}
+
+template <typename T>
+std::array<tt::remove_cvref_wrap_t<T>, 3> EquatorialCompression::inverse(
+    const std::array<T, 3>& target_coords) const noexcept {
+  return angular_distortion(target_coords, aspect_ratio_);
+}
+
+template <typename T>
+tnsr::Ij<tt::remove_cvref_wrap_t<T>, 3, Frame::NoFrame>
+EquatorialCompression::jacobian(const std::array<T, 3>& source_coords) const
+    noexcept {
+  return angular_distortion_jacobian(source_coords, inverse_aspect_ratio_);
+}
+
+template <typename T>
+tnsr::Ij<tt::remove_cvref_wrap_t<T>, 3, Frame::NoFrame>
+EquatorialCompression::inv_jacobian(const std::array<T, 3>& source_coords) const
+    noexcept {
+  return angular_distortion_jacobian((*this)(source_coords), aspect_ratio_);
+}
+
+void EquatorialCompression::pup(PUP::er& p) noexcept {
+  p | aspect_ratio_;
+  p | inverse_aspect_ratio_;
+}
+
+bool operator==(const EquatorialCompression& lhs,
+                const EquatorialCompression& rhs) noexcept {
+  return lhs.aspect_ratio_ == rhs.aspect_ratio_ and
+         lhs.inverse_aspect_ratio_ == rhs.inverse_aspect_ratio_;
+}
+
+bool operator!=(const EquatorialCompression& lhs,
+                const EquatorialCompression& rhs) noexcept {
+  return not(lhs == rhs);
+}
+
+// Explicit instantiations
+/// \cond
+#define DTYPE(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATE(_, data)                                                 \
+  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 3>               \
+  EquatorialCompression::operator()(                                         \
+      const std::array<DTYPE(data), 3>& source_coords) const noexcept;       \
+  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 3>               \
+  EquatorialCompression::inverse(                                            \
+      const std::array<DTYPE(data), 3>& target_coords) const noexcept;       \
+  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 3, Frame::NoFrame> \
+  EquatorialCompression::jacobian(                                           \
+      const std::array<DTYPE(data), 3>& source_coords) const noexcept;       \
+  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 3, Frame::NoFrame> \
+  EquatorialCompression::inv_jacobian(                                       \
+      const std::array<DTYPE(data), 3>& source_coords) const noexcept;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (double, DataVector,
+                                      std::reference_wrapper<const double>,
+                                      std::reference_wrapper<const DataVector>))
+
+#undef DTYPE
+#undef INSTANTIATE
+/// \endcond
+}  // namespace CoordinateMaps
---
+++ tests/Unit/Domain/CoordinateMaps/Test_EquatorialCompression.cpp
@@ -0,0 +1,89 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "tests/Unit/TestingFramework.hpp"
+
+#include <array>
+#include <cmath>
+#include <random>
+
+#include "Domain/CoordinateMaps/EquatorialCompression.hpp"
+#include "ErrorHandling/Error.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/StdArrayHelpers.hpp"
+#include "tests/Unit/Domain/CoordinateMaps/TestMapHelpers.hpp"
+
+SPECTRE_TEST_CASE("Unit.Domain.CoordinateMaps.EquatorialCompression",
+                  "[Domain][Unit]") {
+  // Set up random number generator
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> aspect_ratio_dis(0.1, 10);
+
+  const double aspect_ratio = aspect_ratio_dis(gen);
+  CAPTURE_PRECISE(aspect_ratio);
+  const CoordinateMaps::EquatorialCompression angular_compression_map(
+      aspect_ratio);
+  test_suite_for_map(angular_compression_map);
+}
+
+SPECTRE_TEST_CASE("Unit.Domain.CoordinateMaps.EquatorialCompression.Radius",
+                  "[Domain][Unit]") {
+  // Set up random number generator:
+  const auto seed = std::random_device{}();
+  std::mt19937 gen(seed);
+  INFO("seed = " << seed);
+  std::uniform_real_distribution<> real_dis(-10, 10);
+  std::uniform_real_distribution<> aspect_ratio_dis(0.1, 10);
+  const double x = real_dis(gen);
+  CAPTURE_PRECISE(x);
+  const double y = real_dis(gen);
+  CAPTURE_PRECISE(y);
+  const double z = real_dis(gen);
+  CAPTURE_PRECISE(z);
+  const double aspect_ratio = aspect_ratio_dis(gen);
+  CAPTURE_PRECISE(aspect_ratio);
+  const std::array<double, 3> input_point{{x, y, z}};
+  const CoordinateMaps::EquatorialCompression angular_compression_map(
+      aspect_ratio);
+  const auto result_point = angular_compression_map(input_point);
+  const auto& result_x = result_point[0];
+  const auto& result_y = result_point[1];
+  const auto& result_z = result_point[2];
+
+  // The EquatorialCompression map should preserve radial lengths.
+  CHECK(magnitude(result_point) == approx(magnitude(input_point)));
+
+  // It is easy to mix up \alpha with one over \alpha,
+  // so this comment serves as a reminder to future readers
+  // as to which direction the \alpha should go in the
+  // CHECK statement.
+  //
+  // The aspect ratio is the ratio of the horizontal length
+  // to the vertical height. The parameter name `aspect_ratio` is
+  // used because points with tan \theta = 1 get mapped to points
+  // with tan \theta = \alpha, the value passed to `aspect_ratio`.
+  //
+  // The angular compression map is:
+  //  tan \theta' = \alpha tan\theta.
+  // Then, if the argument provided to the EquatorialCompression
+  // map is, for example, 4, points on the undistorted sphere
+  // that have an angle tan \theta = sqrt(x^2 + y^2)/z are
+  // mapped to points that have an angle
+  //  tan \theta' = 4*sqrt(x^2+y^2)/z
+  //              = sqrt(x'^2 + y'^2)/z'.
+  CHECK(aspect_ratio * sqrt(pow<2>(x) + pow<2>(y)) / z ==
+        approx(sqrt(pow<2>(result_x) + pow<2>(result_y)) / result_z));
+}
+
+// [[OutputRegex, The aspect_ratio must be greater than zero.]]
+[[noreturn]] SPECTRE_TEST_CASE(
+    "Unit.Domain.CoordinateMaps.EquatorialCompression.Assert2",
+    "[Domain][Unit]") {
+  ASSERTION_TEST();
+#ifdef SPECTRE_DEBUG
+  auto failed_angular_compression = CoordinateMaps::EquatorialCompression(-0.2);
+  static_cast<void>(failed_angular_compression);
+  ERROR("Failed to trigger ASSERT in an assertion test");
+#endif
+}
