c478f86f65c0fb264a2c5ddd0a49c5f6eee044b2
William Throwe <wtt6@cornell.edu>
2018-05-28 01:43:23 -0400

    Split InitializeElement into categories of tags

--- src/Evolution/DiscontinuousGalerkin/InitializeElement.hpp
+++ src/Evolution/DiscontinuousGalerkin/InitializeElement.hpp
@@ -12,7 +12,6 @@
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/DataBoxTag.hpp"
 #include "DataStructures/DataBox/Prefixes.hpp"
-#include "DataStructures/Index.hpp"
 #include "DataStructures/Tensor/EagerMath/Magnitude.hpp"  // IWYU pragma: keep
 #include "DataStructures/Variables.hpp"                   // IWYU pragma: keep
 #include "Domain/Mesh.hpp"
@@ -23,7 +22,6 @@
 #include "Domain/ElementId.hpp"
 #include "Domain/ElementMap.hpp"
 #include "Domain/FaceNormal.hpp"
-#include "Domain/LogicalCoordinates.hpp"
 #include "Domain/Tags.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/FluxCommunicationTypes.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/Tags.hpp"
@@ -87,54 +85,219 @@ namespace Actions {
 /// - Modifies: nothing
 template <size_t Dim>
 struct InitializeElement {
-  template <typename Tag>
-  using interface_tag = Tags::Interface<Tags::InternalDirections<Dim>, Tag>;
+  // Items related to the basic structure of the domain
+  struct DomainTags {
+    using simple_tags =
+        db::AddSimpleTags<Tags::Mesh<Dim>, Tags::Element<Dim>,
+                          Tags::ElementMap<Dim>>;
 
-  template <typename Tag>
-  using boundary_tag = Tags::Interface<Tags::BoundaryDirections<Dim>, Tag>;
+    using compute_tags = db::AddComputeTags<
+        Tags::LogicalCoordinates<Dim>,
+        Tags::Coordinates<Tags::ElementMap<Dim>, Tags::LogicalCoordinates<Dim>>,
+        Tags::InverseJacobian<Tags::ElementMap<Dim>,
+                              Tags::LogicalCoordinates<Dim>>>;
+
+    template <typename TagsList>
+    static auto initialize(
+        db::DataBox<TagsList>&& box, const ElementIndex<Dim>& array_index,
+        const std::vector<std::array<size_t, Dim>>& initial_extents,
+        const Domain<Dim, Frame::Inertial>& domain) noexcept {
+      const ElementId<Dim> element_id{array_index};
+      const auto& my_block = domain.blocks()[element_id.block_id()];
+      Mesh<Dim> mesh{initial_extents[element_id.block_id()],
+                     Spectral::Basis::Legendre,
+                     Spectral::Quadrature::GaussLobatto};
+      Element<Dim> element = create_initial_element(element_id, my_block);
+      ElementMap<Dim, Frame::Inertial> map{
+          element_id, my_block.coordinate_map().get_clone()};
+
+      return db::create_from<db::RemoveTags<>, simple_tags, compute_tags>(
+          std::move(box), std::move(mesh), std::move(element), std::move(map));
+    }
+  };
+
+  // Tags related only to the system
+  template <typename System>
+  struct SystemTags {
+    using simple_tags = db::AddSimpleTags<typename System::variables_tag>;
+
+    using compute_tags = db::AddComputeTags<>;
+
+    template <typename TagsList, typename Metavariables>
+    static auto initialize(
+        db::DataBox<TagsList>&& box,
+        const Parallel::ConstGlobalCache<Metavariables>& cache,
+        const Time& initial_time) noexcept {
+      using Vars = typename System::variables_tag::type;
+
+      const size_t num_grid_points =
+          db::get<Tags::Mesh<Dim>>(box).number_of_grid_points();
+      const auto& inertial_coords =
+          db::get<Tags::Coordinates<Tags::ElementMap<Dim>,
+                                    Tags::LogicalCoordinates<Dim>>>(box);
+
+      // Set initial data from analytic solution
+      using solution_tag = CacheTags::AnalyticSolutionBase;
+      Vars vars{num_grid_points};
+      vars.assign_subset(Parallel::get<solution_tag>(cache).variables(
+          inertial_coords, initial_time.value(), typename Vars::tags_list{}));
+
+      return db::create_from<db::RemoveTags<>, simple_tags, compute_tags>(
+          std::move(box), std::move(vars));
+    }
+  };
+
+  // Items related to the structure of the interfaces
+  template <typename System>
+  struct DomainInterfaceTags {
+    using simple_tags = db::AddSimpleTags<>;
+
+    template <typename Directions>
+    using face_tags = tmpl::list<
+        Directions, Tags::Interface<Directions, Tags::Direction<Dim>>,
+        Tags::Interface<Directions, Tags::Mesh<Dim - 1>>,
+        Tags::Interface<Directions, typename System::variables_tag>,
+        Tags::Interface<Directions, Tags::UnnormalizedFaceNormal<Dim>>,
+        Tags::Interface<Directions, typename System::template magnitude_tag<
+                                        Tags::UnnormalizedFaceNormal<Dim>>>,
+        Tags::Interface<Directions,
+                        Tags::Normalized<Tags::UnnormalizedFaceNormal<Dim>>>>;
+
+    using compute_tags = tmpl::append<face_tags<Tags::InternalDirections<Dim>>,
+                                      face_tags<Tags::BoundaryDirections<Dim>>>;
+
+    template <typename TagsList>
+    static auto initialize(db::DataBox<TagsList>&& box) noexcept {
+      return db::create_from<db::RemoveTags<>, simple_tags, compute_tags>(
+          std::move(box));
+    }
+  };
+
+  // Tags related to time-evolution of the system.
+  template <typename System>
+  struct EvolutionTags {
+    using variables_tag = typename System::variables_tag;
+    using dt_variables_tag = db::add_tag_prefix<Tags::dt, variables_tag>;
+
+    using simple_tags = db::AddSimpleTags<
+        Tags::TimeId, Tags::Next<Tags::TimeId>, Tags::TimeStep,
+        dt_variables_tag,
+        Tags::HistoryEvolvedVariables<variables_tag, dt_variables_tag>>;
+
+    using compute_tags = db::AddComputeTags<
+        Tags::Time,
+        Tags::deriv<typename variables_tag::tags_list,
+                    typename System::gradients_tags,
+                    Tags::InverseJacobian<Tags::ElementMap<Dim>,
+                                          Tags::LogicalCoordinates<Dim>>>>;
+
+    template <typename TagsList, typename Metavariables>
+    static auto initialize(
+        db::DataBox<TagsList>&& box,
+        const Parallel::ConstGlobalCache<Metavariables>& cache,
+        const Time& initial_time, const TimeDelta& initial_dt) noexcept {
+      using Vars = typename variables_tag::type;
+      using DtVars = typename dt_variables_tag::type;
+
+      const auto& time_stepper = Parallel::get<CacheTags::TimeStepper>(cache);
+
+      const TimeId time_id(initial_dt.is_positive(), 0, initial_time);
+      TimeId next_time_id = time_stepper.next_time_id(time_id, initial_dt);
+      if (next_time_id.is_at_slab_boundary()) {
+        const auto next_time = next_time_id.step_time();
+        next_time_id = TimeId(
+            initial_dt.is_positive(), 1,
+            next_time.with_slab(next_time.slab().advance_towards(initial_dt)));
+      }
+
+      const size_t num_grid_points =
+          db::get<Tags::Mesh<Dim>>(box).number_of_grid_points();
+      const auto& inertial_coords =
+          db::get<Tags::Coordinates<Tags::ElementMap<Dim>,
+                                    Tags::LogicalCoordinates<Dim>>>(box);
+
+      // Will be overwritten before use
+      DtVars dt_vars{num_grid_points, 0.0};
+
+      typename Tags::HistoryEvolvedVariables<variables_tag,
+                                             dt_variables_tag>::type history;
+      using solution_tag = CacheTags::AnalyticSolutionBase;
+      if (not time_stepper.is_self_starting()) {
+        // We currently just put initial points at past slab boundaries.
+        Time past_t = initial_time;
+        TimeDelta past_dt = initial_dt;
+        for (size_t i = time_stepper.number_of_past_steps(); i > 0; --i) {
+          past_dt = past_dt.with_slab(past_dt.slab().advance_towards(-past_dt));
+          past_t -= past_dt;
+          Vars hist_vars{num_grid_points};
+          DtVars dt_hist_vars{num_grid_points};
+          hist_vars.assign_subset(Parallel::get<solution_tag>(cache).variables(
+              inertial_coords, past_t.value(), typename Vars::tags_list{}));
+          dt_hist_vars.assign_subset(
+              Parallel::get<solution_tag>(cache).variables(
+                  inertial_coords, past_t.value(),
+                  typename DtVars::tags_list{}));
+          history.insert_initial(past_t, std::move(hist_vars),
+                                 std::move(dt_hist_vars));
+        }
+      }
+
+      return db::create_from<db::RemoveTags<>, simple_tags, compute_tags>(
+          std::move(box), time_id, next_time_id, initial_dt, std::move(dt_vars),
+          std::move(history));
+    }
+  };
+
+  // Tags related to DG details (numerical fluxes, etc.).
+  template <typename Metavariables>
+  struct DgTags {
+    using temporal_id_tag = typename Metavariables::temporal_id;
+
+    using simple_tags =
+        db::AddSimpleTags<typename dg::FluxCommunicationTypes<
+                              Metavariables>::simple_mortar_data_tag,
+                          Tags::Mortars<Tags::Next<temporal_id_tag>, Dim>>;
+
+    using compute_tags = db::AddComputeTags<>;
+
+    template <typename TagsList>
+    static auto initialize(db::DataBox<TagsList>&& box) noexcept {
+      const auto& element = db::get<Tags::Element<Dim>>(box);
+
+      typename dg::FluxCommunicationTypes<
+          Metavariables>::simple_mortar_data_tag::type mortar_data{};
+      typename Tags::Mortars<Tags::Next<temporal_id_tag>, Dim>::type
+          mortar_next_temporal_ids{};
+      const auto& temporal_id = get<temporal_id_tag>(box);
+      for (const auto& direction_neighbors : element.neighbors()) {
+        const auto& direction = direction_neighbors.first;
+        const auto& neighbors = direction_neighbors.second;
+        for (const auto& neighbor : neighbors) {
+          const auto mortar_id = std::make_pair(direction, neighbor);
+          mortar_data.insert({mortar_id, {}});
+          mortar_next_temporal_ids.insert({mortar_id, temporal_id});
+        }
+      }
+
+      return db::create_from<db::RemoveTags<>, simple_tags, compute_tags>(
+          std::move(box), std::move(mortar_data),
+          std::move(mortar_next_temporal_ids));
+    }
+  };
 
   template <class Metavariables>
-  using return_tag_list = tmpl::list<
-      // Simple items
-      Tags::TimeId, Tags::Next<Tags::TimeId>, Tags::TimeStep, Tags::Mesh<Dim>,
-      Tags::Element<Dim>, Tags::ElementMap<Dim>,
-      typename Metavariables::system::variables_tag,
-      Tags::HistoryEvolvedVariables<
-          typename Metavariables::system::variables_tag,
-          db::add_tag_prefix<Tags::dt,
-                             typename Metavariables::system::variables_tag>>,
-      db::add_tag_prefix<Tags::dt,
-                         typename Metavariables::system::variables_tag>,
-      typename dg::FluxCommunicationTypes<
-          Metavariables>::simple_mortar_data_tag,
-      Tags::Mortars<Tags::Next<Tags::TimeId>, Dim>,
-      // Compute items
-      Tags::Time, Tags::LogicalCoordinates<Dim>,
-      Tags::Coordinates<Tags::ElementMap<Dim>, Tags::LogicalCoordinates<Dim>>,
-      Tags::InverseJacobian<Tags::ElementMap<Dim>,
-                            Tags::LogicalCoordinates<Dim>>,
-      Tags::deriv<typename Metavariables::system::variables_tag::tags_list,
-                  typename Metavariables::system::gradients_tags,
-                  Tags::InverseJacobian<Tags::ElementMap<Dim>,
-                                        Tags::LogicalCoordinates<Dim>>>,
-      Tags::InternalDirections<Dim>, interface_tag<Tags::Direction<Dim>>,
-      interface_tag<Tags::Mesh<Dim - 1>>,
-      interface_tag<Tags::UnnormalizedFaceNormal<Dim>>,
-      interface_tag<typename Metavariables::system::template magnitude_tag<
-          Tags::UnnormalizedFaceNormal<Dim>>>,
-      interface_tag<Tags::Normalized<Tags::UnnormalizedFaceNormal<Dim>>>,
-      // This should be the boundary forms of
-      // System::normal_dot_fluxes::argument_tags, but it is not clear
-      // how to get that.  System::variables_tag should generally be a
-      // superset.
-      interface_tag<typename Metavariables::system::variables_tag>,
-      Tags::BoundaryDirections<Dim>, boundary_tag<Tags::Direction<Dim>>,
-      boundary_tag<Tags::Mesh<Dim - 1>>,
-      boundary_tag<Tags::UnnormalizedFaceNormal<Dim>>,
-      boundary_tag<typename Metavariables::system::template magnitude_tag<
-          Tags::UnnormalizedFaceNormal<Dim>>>,
-      boundary_tag<Tags::Normalized<Tags::UnnormalizedFaceNormal<Dim>>>,
-      boundary_tag<typename Metavariables::system::variables_tag>>;
+  using return_tag_list = tmpl::append<
+      typename DomainTags::simple_tags,
+      typename SystemTags<typename Metavariables::system>::simple_tags,
+      typename DomainInterfaceTags<typename Metavariables::system>::simple_tags,
+      typename EvolutionTags<typename Metavariables::system>::simple_tags,
+      typename DgTags<Metavariables>::simple_tags,
+      typename DomainTags::compute_tags,
+      typename SystemTags<typename Metavariables::system>::compute_tags,
+      typename DomainInterfaceTags<
+          typename Metavariables::system>::compute_tags,
+      typename EvolutionTags<typename Metavariables::system>::compute_tags,
+      typename DgTags<Metavariables>::compute_tags>;
 
   template <typename... InboxTags, typename Metavariables, typename ActionList,
             typename ParallelComponent>
@@ -147,93 +310,17 @@ struct InitializeElement {
                     std::vector<std::array<size_t, Dim>> initial_extents,
                     Domain<Dim, Frame::Inertial> domain, Time initial_time,
                     TimeDelta initial_dt) noexcept {
-    using solution_tag = CacheTags::AnalyticSolutionBase;
-    using variables_tags =
-        typename Metavariables::system::variables_tag::tags_list;
-
-    ElementId<Dim> element_id{array_index};
-
-    const auto& my_block = domain.blocks()[element_id.block_id()];
-
-    ElementMap<Dim, Frame::Inertial> map{element_id,
-                                         my_block.coordinate_map().get_clone()};
-
-    Element<Dim> element = create_initial_element(element_id, my_block);
-
-    Mesh<Dim> mesh{initial_extents[element_id.block_id()],
-                   Spectral::Basis::Legendre,
-                   Spectral::Quadrature::GaussLobatto};
-    const auto num_grid_points = mesh.number_of_grid_points();
-    auto logical_coords = logical_coordinates(mesh);
-    auto inertial_coords = map(logical_coords);
-
-    // Set up initial time
-    const auto& time_stepper = Parallel::get<CacheTags::TimeStepper>(cache);
-    const TimeId time_id(initial_dt.is_positive(), 0, initial_time);
-    TimeId next_time_id = time_stepper.next_time_id(time_id, initial_dt);
-    if (next_time_id.is_at_slab_boundary()) {
-      const auto next_time = next_time_id.step_time();
-      next_time_id = TimeId(
-          initial_dt.is_positive(), 1,
-          next_time.with_slab(next_time.slab().advance_towards(initial_dt)));
-    }
-
-    // Set initial data from analytic solution
-    Variables<variables_tags> vars{num_grid_points};
-    vars.assign_subset(Parallel::get<solution_tag>(cache).variables(
-        inertial_coords, initial_time.value(), variables_tags{}));
-
-    typename Tags::HistoryEvolvedVariables<
-        Tags::Variables<variables_tags>,
-        Tags::dt<Tags::Variables<db::wrap_tags_in<Tags::dt, variables_tags>>>>::
-        type history_dt_vars;
-    if (not time_stepper.is_self_starting()) {
-      // We currently just put initial points at past slab boundaries.
-      Time past_t = initial_time;
-      TimeDelta past_dt = initial_dt;
-      for (size_t i = time_stepper.number_of_past_steps(); i > 0; --i) {
-        past_dt = past_dt.with_slab(past_dt.slab().advance_towards(-past_dt));
-        past_t -= past_dt;
-        Variables<variables_tags> hist_vars{num_grid_points};
-        Variables<db::wrap_tags_in<Tags::dt, variables_tags>> dt_hist_vars{
-            num_grid_points};
-        hist_vars.assign_subset(Parallel::get<solution_tag>(cache).variables(
-            inertial_coords, past_t.value(), variables_tags{}));
-        dt_hist_vars.assign_subset(Parallel::get<solution_tag>(cache).variables(
-            inertial_coords, past_t.value(),
-            db::wrap_tags_in<Tags::dt, variables_tags>{}));
-        history_dt_vars.insert_initial(past_t, std::move(hist_vars),
-                                       std::move(dt_hist_vars));
-      }
-    }
-
-    // Set up boundary information
-    db::item_type<typename dg::FluxCommunicationTypes<
-        Metavariables>::simple_mortar_data_tag>
-        boundary_data{};
-    typename Tags::Mortars<Tags::Next<Tags::TimeId>, Dim>::type
-        mortar_next_time_ids{};
-    for (const auto& direction_neighbors : element.neighbors()) {
-      const auto& direction = direction_neighbors.first;
-      const auto& neighbors = direction_neighbors.second;
-      for (const auto& neighbor : neighbors) {
-        const auto mortar_id = std::make_pair(direction, neighbor);
-        boundary_data.insert({mortar_id, {}});
-        mortar_next_time_ids.insert({mortar_id, time_id});
-      }
-    }
-
-    db::compute_databox_type<return_tag_list<Metavariables>> outbox =
-        db::create<db::get_items<return_tag_list<Metavariables>>,
-                   db::get_compute_items<return_tag_list<Metavariables>>>(
-            time_id, next_time_id, initial_dt, std::move(mesh),
-            std::move(element), std::move(map), std::move(vars),
-            std::move(history_dt_vars),
-            Variables<db::wrap_tags_in<Tags::dt, variables_tags>>{
-                num_grid_points, 0.0},
-            std::move(boundary_data), std::move(mortar_next_time_ids));
-
-    return std::make_tuple(std::move(outbox));
+    using system = typename Metavariables::system;
+    auto domain_box = DomainTags::initialize(
+        db::DataBox<tmpl::list<>>{}, array_index, initial_extents, domain);
+    auto system_box = SystemTags<system>::initialize(std::move(domain_box),
+                                                     cache, initial_time);
+    auto domain_interface_box =
+        DomainInterfaceTags<system>::initialize(std::move(system_box));
+    auto evolution_box = EvolutionTags<system>::initialize(
+        std::move(domain_interface_box), cache, initial_time, initial_dt);
+    auto dg_box = DgTags<Metavariables>::initialize(std::move(evolution_box));
+    return std::make_tuple(std::move(dg_box));
   }
 };
 }  // namespace Actions
