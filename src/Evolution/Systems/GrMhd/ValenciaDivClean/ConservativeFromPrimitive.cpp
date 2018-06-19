657c7697d2bc456564addd92908a7646e498924f
Lawrence Kidder <kidder@astro.cornell.edu>
2018-06-19 14:54:55 -0400

    Add function to compute conservative variables for GRMHD

---
+++ src/Evolution/Systems/GrMhd/ValenciaDivClean/ConservativeFromPrimitive.cpp
@@ -0,0 +1,78 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Evolution/Systems/GrMhd/ValenciaDivClean/ConservativeFromPrimitive.hpp"
+
+#include <algorithm>
+#include <cstddef>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/DotProduct.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "PointwiseFunctions/GeneralRelativity/IndexManipulation.hpp"
+#include "Utilities/Gsl.hpp"
+
+// IWYU pragma: no_forward_declare Tensor
+// IWYU pragma: no_include <array>
+
+/// \cond
+namespace grmhd {
+namespace ValenciaDivClean {
+
+void conservative_from_primitive(
+    const gsl::not_null<Scalar<DataVector>*> tilde_d,
+    const gsl::not_null<Scalar<DataVector>*> tilde_tau,
+    const gsl::not_null<tnsr::i<DataVector, 3, Frame::Inertial>*> tilde_s,
+    const gsl::not_null<tnsr::I<DataVector, 3, Frame::Inertial>*> tilde_b,
+    const gsl::not_null<Scalar<DataVector>*> tilde_phi,
+    const Scalar<DataVector>& rest_mass_density,
+    const Scalar<DataVector>& specific_enthalpy,
+    const Scalar<DataVector>& pressure,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& spatial_velocity,
+    const Scalar<DataVector>& lorentz_factor,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& magnetic_field,
+    const Scalar<DataVector>& sqrt_det_spatial_metric,
+    const tnsr::ii<DataVector, 3, Frame::Inertial>& spatial_metric,
+    const Scalar<DataVector>& divergence_cleaning_field) noexcept {
+  const auto spatial_velocity_one_form =
+      raise_or_lower_index(spatial_velocity, spatial_metric);
+  const auto magnetic_field_one_form =
+      raise_or_lower_index(magnetic_field, spatial_metric);
+  const auto magnetic_field_dot_spatial_velocity =
+      dot_product(magnetic_field, spatial_velocity_one_form);
+  const auto spatial_velocity_squared =
+      dot_product(spatial_velocity, spatial_velocity_one_form);
+  // not const to save an allocation later
+  auto magnetic_field_squared =
+      dot_product(magnetic_field, magnetic_field_one_form);
+
+  get(*tilde_d) = get(sqrt_det_spatial_metric) * get(rest_mass_density) *
+                  get(lorentz_factor);
+
+  get(*tilde_tau) =
+      get(*tilde_d) * (get(lorentz_factor) * get(specific_enthalpy) - 1.0) +
+      get(sqrt_det_spatial_metric) *
+          (0.5 * get(magnetic_field_squared) *
+               (1.0 + get(spatial_velocity_squared)) -
+           0.5 * get(magnetic_field_dot_spatial_velocity) - get(pressure));
+
+  Scalar<DataVector> common_factor = std::move(magnetic_field_squared);
+  get(common_factor) *= get(sqrt_det_spatial_metric);
+  get(common_factor) +=
+      get(*tilde_d) * get(lorentz_factor) * get(specific_enthalpy);
+  for (size_t i = 0; i < 3; ++i) {
+    tilde_s->get(i) = get(common_factor) * spatial_velocity_one_form.get(i) -
+                      get(magnetic_field_dot_spatial_velocity) *
+                          get(sqrt_det_spatial_metric) *
+                          magnetic_field_one_form.get(i);
+  }
+  for (size_t i = 0; i < 3; ++i) {
+    tilde_b->get(i) = get(sqrt_det_spatial_metric) * magnetic_field.get(i);
+  }
+  get(*tilde_phi) =
+      get(sqrt_det_spatial_metric) * get(divergence_cleaning_field);
+}
+
+}  // namespace ValenciaDivClean
+}  // namespace grmhd
+/// \endcond
---
+++ src/Evolution/Systems/GrMhd/ValenciaDivClean/ConservativeFromPrimitive.hpp
@@ -0,0 +1,60 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "DataStructures/Tensor/TypeAliases.hpp"
+
+namespace gsl {
+template <typename T>
+class not_null;
+}  // namespace gsl
+
+class DataVector;
+
+// IWYU pragma: no_forward_declare Tensor
+
+namespace grmhd {
+namespace ValenciaDivClean {
+
+/*!
+ * \brief Compute the conservative variables from primitive variables
+ *
+ * \f{align*}
+ * {\tilde D} = & \sqrt{\gamma} \rho W \\
+ * {\tilde S}_i = & \sqrt{\gamma} \left( \rho h W^2 v_i + B^m B_m v_i - B^m v_m
+ * B_i \right) \\
+ * {\tilde \tau} = & \sqrt{\gamma} \left[ \rho h W^2 - p - \rho W - \frac{1}{2}
+ * B^m v_m + \frac{1}{2} B^m B_m \left( 1 + v^m v_m \right) \right] \\
+ * {\tilde B}^i = & \sqrt{\gamma} B^i \\
+ * {\tilde \Phi} = & \sqrt{\gamma} \Phi
+ * \f}
+ *
+ * where the conserved variables \f${\tilde D}\f$, \f${\tilde S}_i\f$,
+ * \f${\tilde \tau}\f$, \f${\tilde B}^i\f$, and \f${\tilde \Phi}\f$ are a
+ * generalized mass-energy density, momentum density, specific internal energy
+ * density, magnetic field, and divergence cleaning field.  Furthermore
+ * \f$\gamma\f$ is the determinant of the spatial metric, \f$\rho\f$ is the rest
+ * mass density, \f$W = 1/\sqrt{1-v_i v^i}\f$ is the Lorentz factor, \f$h = 1 +
+ * \epsilon + \frac{p}{\rho}\f$ is the specific enthalpy, \f$v^i\f$ is the
+ * spatial velocity, \f$\epsilon\f$ is the specific internal energy, \f$p\f$ is
+ * the pressure, \f$B^i\f$ is the spatial magnetic field measured by an Eulerian
+ * observer, and \f$\Phi\f$ is a divergence cleaning field.
+ */
+void conservative_from_primitive(
+    gsl::not_null<Scalar<DataVector>*> tilde_d,
+    gsl::not_null<Scalar<DataVector>*> tilde_tau,
+    gsl::not_null<tnsr::i<DataVector, 3, Frame::Inertial>*> tilde_s,
+    gsl::not_null<tnsr::I<DataVector, 3, Frame::Inertial>*> tilde_b,
+    gsl::not_null<Scalar<DataVector>*> tilde_phi,
+    const Scalar<DataVector>& rest_mass_density,
+    const Scalar<DataVector>& specific_enthalpy,
+    const Scalar<DataVector>& pressure,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& spatial_velocity,
+    const Scalar<DataVector>& lorentz_factor,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& magnetic_field,
+    const Scalar<DataVector>& sqrt_det_spatial_metric,
+    const tnsr::ii<DataVector, 3, Frame::Inertial>& spatial_metric,
+    const Scalar<DataVector>& divergence_cleaning_field) noexcept;
+}  // namespace ValenciaDivClean
+}  // namespace grmhd
