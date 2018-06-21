2c3e0f43e4da3ffed6c53cb7ebcfb604559a6d48
Lawrence Kidder <kidder@astro.cornell.edu>
2018-06-21 14:16:05 -0400

    Add function to compute fluxes for GRMHD

---
+++ tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/Test_Fluxes.cpp
@@ -0,0 +1,24 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "tests/Unit/TestingFramework.hpp"
+
+#include "DataStructures/DataVector.hpp"
+#include "Evolution/Systems/GrMhd/ValenciaDivClean/Fluxes.hpp"
+#include "tests/Unit/Pypp/CheckWithRandomValues.hpp"
+#include "tests/Unit/Pypp/SetupLocalPythonEnvironment.hpp"
+
+// IWYU pragma: no_include "DataStructures/Tensor/Tensor.hpp"
+// IWYU pragma: no_include "Utilities/Gsl.hpp"
+// IWYU pragma: no_include <string>
+
+SPECTRE_TEST_CASE("Unit.GrMhd.ValenciaDivClean.Fluxes", "[Unit][GrMhd]") {
+  pypp::SetupLocalPythonEnvironment local_python_env{
+      "Evolution/Systems/GrMhd/ValenciaDivClean"};
+
+  pypp::check_with_random_values<1>(
+      &grmhd::ValenciaDivClean::fluxes, "TestFunctions",
+      {"tilde_d_flux", "tilde_tau_flux", "tilde_s_flux", "tilde_b_flux",
+       "tilde_phi_flux"},
+      {{{0.0, 1.0}}}, DataVector{5});
+}
