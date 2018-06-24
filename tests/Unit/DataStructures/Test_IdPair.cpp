6b3ec8b7304cc1e96d11f29d738b79965f8923d1
Nils Deppe <nd357@cornell.edu>
2018-06-24 19:14:17 -0400

    Add class IdPair
    IdPair is a POD that holds an ID and data. It is designed to replace uses of
    `std::pair<ID, Type>` so that access is clearer, ie id_pair.id and id_pair.data.

---
+++ tests/Unit/DataStructures/Test_IdPair.cpp
@@ -0,0 +1,28 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "tests/Unit/TestingFramework.hpp"
+
+#include <vector>
+
+#include "DataStructures/IdPair.hpp"
+#include "Utilities/TypeTraits.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+
+SPECTRE_TEST_CASE("Unit.DataStructures.IdPair",
+                  "[Unit][DataStructures]") {
+  auto id_pair = make_id_pair(5, std::vector<double>{1.2, 7.8});
+  static_assert(cpp17::is_same_v<decltype(id_pair.id), int>,
+                "Failed checking type decay in make_id_pair");
+  static_assert(cpp17::is_same_v<decltype(id_pair.data), std::vector<double>>,
+                "Failed checking type decay in make_id_pair");
+  CHECK(id_pair.id == 5);
+  CHECK(id_pair.data == std::vector<double>{1.2, 7.8});
+  CHECK(id_pair == make_id_pair(5, std::vector<double>{1.2, 7.8}));
+  CHECK(id_pair != make_id_pair(6, std::vector<double>{1.2, 7.8}));
+  CHECK(id_pair != make_id_pair(5, std::vector<double>{1.3, 7.8}));
+  CHECK(id_pair != make_id_pair(7, std::vector<double>{1.3, 7.8}));
+
+  // Test PUP
+  test_serialization(id_pair);
+}
