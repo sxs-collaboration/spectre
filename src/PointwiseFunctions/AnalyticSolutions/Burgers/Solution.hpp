a942f75cb9912c23518acd7a6362dd90343d219d
William Throwe <wtt6@cornell.edu>
2018-06-25 19:33:15 -0400

    Add Linear Burgers solution

---
+++ src/PointwiseFunctions/AnalyticSolutions/Burgers/Solution.hpp
@@ -0,0 +1,16 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines Burgers::Solutions
+
+#pragma once
+
+namespace Burgers {
+/*!
+ * \ingroup AnalyticSolutions
+ * Holds classes implementing a solution to the Burgers equation
+ * \f$0 = \partial_t U + \partial_x\left(U^2/2\right)\f$.
+ */
+namespace Solutions {}
+}  // namespace Burgers
