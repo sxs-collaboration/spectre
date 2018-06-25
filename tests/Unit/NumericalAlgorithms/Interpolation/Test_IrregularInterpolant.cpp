97eade29448868b0934205ec6df506ac476e6bfe
Nils Leif Fischer <hello@nilsleiffischer.de>
2018-06-25 16:04:29 +0200

    Test IrregularInterpolant on non-LGL meshes

--- tests/Unit/NumericalAlgorithms/Interpolation/Test_IrregularInterpolant.cpp
+++ tests/Unit/NumericalAlgorithms/Interpolation/Test_IrregularInterpolant.cpp
@@ -202,3 +202,32 @@ SPECTRE_TEST_CASE("Unit.Numerical.Interpolation.IrregularInterpolant",
     }
   }
 }
+
+SPECTRE_TEST_CASE("Unit.Numerical.Interpolation.IrregularInterpolant.Meshes",
+                  "[Unit][NumericalAlgorithms]") {
+  const size_t start_points = 4;
+  const size_t end_points = 6;
+  for (size_t n0 = start_points; n0 < end_points; ++n0) {
+    test_interpolate_to_points<1>(
+        Mesh<1>{n0, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss});
+    for (size_t n1 = start_points; n1 < end_points; ++n1) {
+      test_interpolate_to_points<2>(Mesh<2>{
+          {{n0, n1}}, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss});
+      test_interpolate_to_points<2>(Mesh<2>{
+          {{n0, n1}},
+          {{Spectral::Basis::Legendre, Spectral::Basis::Legendre}},
+          {{Spectral::Quadrature::GaussLobatto, Spectral::Quadrature::Gauss}}});
+      for (size_t n2 = start_points; n2 < end_points; ++n2) {
+        test_interpolate_to_points<3>(Mesh<3>{{{n0, n1, n2}},
+                                              Spectral::Basis::Legendre,
+                                              Spectral::Quadrature::Gauss});
+        test_interpolate_to_points<3>(Mesh<3>{
+            {{n0, n1, n2}},
+            {{Spectral::Basis::Legendre, Spectral::Basis::Legendre,
+              Spectral::Basis::Legendre}},
+            {{Spectral::Quadrature::GaussLobatto, Spectral::Quadrature::Gauss,
+              Spectral::Quadrature::GaussLobatto}}});
+      }
+    }
+  }
+}
