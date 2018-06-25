fa5b3bd44c0cc60ee5a110ce7f06854797232555
Nils Leif Fischer <hello@nilsleiffischer.de>
2018-06-25 15:29:25 +0200

    Refactor IrregularInterpolant to use Mesh

--- src/NumericalAlgorithms/Interpolation/IrregularInterpolant.cpp
+++ src/NumericalAlgorithms/Interpolation/IrregularInterpolant.cpp
@@ -6,40 +6,40 @@
 #include <array>
 
 #include "DataStructures/DataVector.hpp"
-#include "DataStructures/Index.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
-#include "NumericalAlgorithms/Spectral/LegendreGaussLobatto.hpp"
+#include "Domain/Mesh.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
 // IWYU pragma: no_forward_declare Tensor
 
 namespace {
 
 template <size_t Dim>
 Matrix interpolation_matrix(
-    const Index<Dim>& mesh,
+    const Mesh<Dim>& mesh,
     const tnsr::I<DataVector, Dim, Frame::Logical>& points) noexcept;
 
 template <>
 Matrix interpolation_matrix(
-    const Index<1>& mesh,
+    const Mesh<1>& mesh,
     const tnsr::I<DataVector, 1, Frame::Logical>& points) noexcept {
   // For 1d interpolation matrix, simply return the legendre-gauss-lobatto
   // matrix.
-  return Basis::lgl::interpolation_matrix(mesh[0], get<0>(points));
+  return Spectral::interpolation_matrix(mesh, get<0>(points));
 }
 
 template <>
 Matrix interpolation_matrix(
-    const Index<2>& mesh,
+    const Mesh<2>& mesh,
     const tnsr::I<DataVector, 2, Frame::Logical>& points) noexcept {
   const std::array<Matrix, 2> matrices{
-      {Basis::lgl::interpolation_matrix(mesh[0], get<0>(points)),
-       Basis::lgl::interpolation_matrix(mesh[1], get<1>(points))}};
+      {Spectral::interpolation_matrix(mesh.slice_through(0), get<0>(points)),
+       Spectral::interpolation_matrix(mesh.slice_through(1), get<1>(points))}};
 
   const auto number_of_points = get<0>(points).size();
-  Matrix result(number_of_points, mesh.product());
+  Matrix result(number_of_points, mesh.number_of_grid_points());
   // First dimension of DataVector varies fastest.
-  for (size_t j = 0, s = 0; j < mesh[1]; ++j) {
-    for (size_t i = 0; i < mesh[0]; ++i) {
+  for (size_t j = 0, s = 0; j < mesh.extents(1); ++j) {
+    for (size_t i = 0; i < mesh.extents(0); ++i) {
       for (size_t p = 0; p < number_of_points; ++p) {
         result(p, s) = matrices[0](p, i) * matrices[1](p, j);
       }
@@ -51,19 +51,19 @@ Matrix interpolation_matrix(
 
 template <>
 Matrix interpolation_matrix(
-    const Index<3>& mesh,
+    const Mesh<3>& mesh,
     const tnsr::I<DataVector, 3, Frame::Logical>& points) noexcept {
   const std::array<Matrix, 3> matrices{
-      {Basis::lgl::interpolation_matrix(mesh[0], get<0>(points)),
-       Basis::lgl::interpolation_matrix(mesh[1], get<1>(points)),
-       Basis::lgl::interpolation_matrix(mesh[2], get<2>(points))}};
+      {Spectral::interpolation_matrix(mesh.slice_through(0), get<0>(points)),
+       Spectral::interpolation_matrix(mesh.slice_through(1), get<1>(points)),
+       Spectral::interpolation_matrix(mesh.slice_through(2), get<2>(points))}};
 
   const auto number_of_points = get<0>(points).size();
-  Matrix result(number_of_points, mesh.product());
+  Matrix result(number_of_points, mesh.number_of_grid_points());
   // First dimension of DataVector varies fastest.
-  for (size_t k = 0, s = 0; k < mesh[2]; ++k) {
-    for (size_t j = 0; j < mesh[1]; ++j) {
-      for (size_t i = 0; i < mesh[0]; ++i) {
+  for (size_t k = 0, s = 0; k < mesh.extents(2); ++k) {
+    for (size_t j = 0; j < mesh.extents(1); ++j) {
+      for (size_t i = 0; i < mesh.extents(0); ++i) {
         for (size_t p = 0; p < number_of_points; ++p) {
           result(p, s) =
               matrices[0](p, i) * matrices[1](p, j) * matrices[2](p, k);
@@ -83,7 +83,7 @@ Irregular<Dim>::Irregular() = default;
 
 template <size_t Dim>
 Irregular<Dim>::Irregular(
-    const Index<Dim>& source_mesh,
+    const Mesh<Dim>& source_mesh,
     const tnsr::I<DataVector, Dim, Frame::Logical>& target_points) noexcept
     : interpolation_matrix_(interpolation_matrix(source_mesh, target_points)) {}
 
