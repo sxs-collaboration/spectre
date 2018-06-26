b02e52423dcbb117c6f388f03c2777ed16253c07
Lawrence Kidder <kidder@astro.cornell.edu>
2018-06-26 17:24:27 -0400

    Add function to compute source terms for GRMHD

---
+++ tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/Test_Sources.cpp
@@ -0,0 +1,24 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "tests/Unit/TestingFramework.hpp"
+
+#include "DataStructures/DataVector.hpp"
+#include "Evolution/Systems/GrMhd/ValenciaDivClean/Sources.hpp"
+#include "tests/Unit/Pypp/CheckWithRandomValues.hpp"
+#include "tests/Unit/Pypp/SetupLocalPythonEnvironment.hpp"
+
+// IWYU pragma: no_include "DataStructures/Tensor/Tensor.hpp"
+// IWYU pragma: no_include "Utilities/Gsl.hpp"
+// IWYU pragma: no_include <string>
+
+SPECTRE_TEST_CASE("Unit.GrMhd.ValenciaDivClean.Sources", "[Unit][GrMhd]") {
+  pypp::SetupLocalPythonEnvironment local_python_env{
+      "Evolution/Systems/GrMhd/ValenciaDivClean"};
+
+  pypp::check_with_random_values<1>(
+      &grmhd::ValenciaDivClean::compute_source_terms_of_u, "TestFunctions",
+      {"source_tilde_d", "source_tilde_tau", "source_tilde_s", "source_tilde_b",
+       "source_tilde_phi"},
+      {{{0.0, 1.0}}}, DataVector{5});
+}
