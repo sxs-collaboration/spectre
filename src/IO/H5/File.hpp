b7ae4e9ebd96ed25d8a2f8a3f43ac742c56148d0
Nils Deppe <nd357@cornell.edu>
2018-09-13 18:28:06 -0400

    Add missing include to IO/H5/File.hpp

--- src/IO/H5/File.hpp
+++ src/IO/H5/File.hpp
@@ -20,16 +20,11 @@
 #include "ErrorHandling/Error.hpp"
 #include "IO/H5/AccessType.hpp"
 #include "IO/H5/CheckH5.hpp"
+#include "IO/H5/Header.hpp"  // IWYU pragma: keep
 #include "IO/H5/Object.hpp"
 #include "IO/H5/OpenGroup.hpp"
 #include "Utilities/FileSystem.hpp"
 #include "Utilities/PrettyType.hpp"
-
-/// \cond
-namespace h5 {
-class Header;
-}  // namespace h5
-/// \endcond
 
 namespace h5 {
 /*!
