8bcfdf937144d657f8f7b01b1c29e85e96129054
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2018-10-12 18:01:16 +0200

    Vectorize Python implementation of Poisson solutions

--- tests/Unit/PointwiseFunctions/AnalyticSolutions/Poisson/Moustache.py
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/Poisson/Moustache.py
@@ -6,22 +6,25 @@ import numpy as np
 
 # Functions for testing ProductOfSinusoids.cpp
 def poly(x, coeffs):
-    return np.sum(c * x**p for p, c in enumerate(coeffs))
+    return np.sum([c * x**p for p, c in enumerate(coeffs)], axis=0)
 
 def field(x):
-    return np.prod(x * (1. - x)) * np.sum((x - 0.5)**2)**(3. / 2.)
+    x = np.asarray(x)
+    return np.prod(x * (1. - x), axis=0) * (np.sum((x - 0.5)**2, axis=0)**(3. / 2.))
 
 def auxiliary_field(x):
+    x = np.asarray(x)
     if len(x) == 1:
         return np.abs(x - 0.5) * poly(x, [0.25, -3, 7.5, -5])
     elif len(x) == 2:
-        return np.sqrt(np.sum((x - 0.5)**2)) * np.array([
+        return np.sqrt(np.sum((x - 0.5)**2, axis=0)).reshape(1, *x.shape[1:]) * np.array([
             x[d - 1] * (1. - x[d - 1]) * (poly(x[d], [
                 0.5, poly(x[d - 1], [-3.5, 2, -2]), 7.5, -5
                 ]) + poly(x[d - 1], [0, -1, 1]))
             for d in range(len(x))])
 
 def source(x):
+    x = np.asarray(x)
     if len(x) == 1:
         return (poly(x, [0.875, -8.5, 28.5, -40., 20.]) / np.abs(x - 0.5))[0]
     elif len(x) == 2:
@@ -32,8 +35,9 @@ def source(x):
             poly(x[1], [12., -78., 78.]),
             poly(x[1], [-11., 39., -39.]),
             6., -2.
-        ]) / np.linalg.norm(x - 0.5)
+        ]) / np.linalg.norm(x - 0.5, axis=0)
 
 def auxiliary_source(x):
-    return np.zeros(len(x))
+    x = np.asarray(x)
+    return np.zeros(x.shape)
 # End functions for testing ProductOfSinusoids.cpp
--- tests/Unit/PointwiseFunctions/AnalyticSolutions/Poisson/ProductOfSinusoids.py
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/Poisson/ProductOfSinusoids.py
@@ -5,22 +5,37 @@ import numpy as np
 
 
 # Functions for testing ProductOfSinusoids.cpp
-def field(x, wave_numbers):
+def parse_arguments(x, wave_numbers):
     x, wave_numbers = np.asarray(x), np.asarray(wave_numbers)
-    return np.prod(np.sin(wave_numbers * x))
+    if wave_numbers.ndim == 0:
+        x = np.expand_dims(x, axis=0)
+        wave_numbers = np.expand_dims(wave_numbers, axis=0)
+    assert len(x) == len(wave_numbers), "Requires the same number of coordinates and wave numbers"
+    return x, wave_numbers
+
+def phase(x, wave_numbers):
+    x, wave_numbers = parse_arguments(x, wave_numbers)
+    return np.array([
+        wave_numbers[d] * x[d]
+        for d in range(len(x))
+    ])
+
+def field(x, wave_numbers):
+    x, wave_numbers = parse_arguments(x, wave_numbers)
+    return np.prod(np.sin(phase(x, wave_numbers)), axis=0)
 
 def auxiliary_field(x, wave_numbers):
-    x, wave_numbers = np.asarray(x), np.asarray(wave_numbers)
-    try:
-        dim = len(x)
-    except TypeError:
-        dim = 1
-    return wave_numbers * np.cos(wave_numbers * x) * np.array([field(np.delete(x, d), np.delete(wave_numbers, d)) for d in range(dim)])
+    x, wave_numbers = parse_arguments(x, wave_numbers)
+    return np.array([
+        wave_numbers[d] * np.cos(np.squeeze(phase(x[d], wave_numbers[d]))) * field(np.delete(x, d, axis=0), np.delete(wave_numbers, d, axis=0))
+        for d in range(len(x))
+    ])
 
 def source(x, wave_numbers):
-    x, wave_numbers = np.asarray(x), np.asarray(wave_numbers)
+    x, wave_numbers = parse_arguments(x, wave_numbers)
     return np.sum(wave_numbers**2) * field(x, wave_numbers)
 
 def auxiliary_source(x, wave_numbers):
+    x, wave_numbers = parse_arguments(x, wave_numbers)
     return np.zeros(x.shape)
 # End functions for testing ProductOfSinusoids.cpp
