ba390e466508597ed8a3a58b4b74690b386b3932
Nils Deppe <nd357@cornell.edu>
2018-10-14 11:00:15 -0400

    Prefix SmoothFlow python functions with smooth_flow_

--- tests/Unit/PointwiseFunctions/AnalyticSolutions/GrMhd/TestFunctions.py
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/GrMhd/TestFunctions.py
@@ -5,33 +5,35 @@ import numpy as np
 
 
 # Functions for testing SmoothFlow.cpp
-def rest_mass_density(x, t, mean_velocity, wave_vector, pressure,
-                      adiabatic_exponent, density_amplitude):
-    return 1.0 + (density_amplitude * np.sin(
+def smooth_flow_rest_mass_density(x, t, mean_velocity, wave_vector, pressure,
+                                  adiabatic_exponent, density_amplitude):
+    return (1.0 + (density_amplitude * np.sin(
         np.dot(
             np.asarray(wave_vector),
-            np.asarray(x) - np.asarray(mean_velocity) * t)))
+            np.asarray(x) - np.asarray(mean_velocity) * t))))
 
 
-def spatial_velocity(x, t, mean_velocity, wave_vector, pressure,
-                     adiabatic_exponent, density_amplitude):
+def smooth_flow_spatial_velocity(x, t, mean_velocity, wave_vector, pressure,
+                                 adiabatic_exponent, density_amplitude):
     return np.asarray(mean_velocity)
 
 
-def specific_internal_energy(x, t, mean_velocity, wave_vector, pressure,
-                             adiabatic_exponent, density_amplitude):
-    return (pressure / ((adiabatic_exponent - 1.0) * rest_mass_density(
-        x, t, mean_velocity, wave_vector, pressure, adiabatic_exponent,
-        density_amplitude)))
+def smooth_flow_specific_internal_energy(x, t, mean_velocity, wave_vector,
+                                         pressure, adiabatic_exponent,
+                                         density_amplitude):
+    return (
+        pressure / ((adiabatic_exponent - 1.0) * smooth_flow_rest_mass_density(
+            x, t, mean_velocity, wave_vector, pressure, adiabatic_exponent,
+            density_amplitude)))
 
 
-def pressure(x, t, mean_velocity, wave_vector, pressure, adiabatic_exponent,
-             density_amplitude):
+def smooth_flow_pressure(x, t, mean_velocity, wave_vector, pressure,
+                         adiabatic_exponent, density_amplitude):
     return pressure
 
 
-def magnetic_field(x, t, mean_velocity, wave_vector, pressure,
-                   adiabatic_exponent, density_amplitude):
+def smooth_flow_magnetic_field(x, t, mean_velocity, wave_vector, pressure,
+                               adiabatic_exponent, density_amplitude):
     return np.array([0.0, 0.0, 0.0])
 
 
