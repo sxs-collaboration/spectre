cace243d4b268f28da25bbead42979430ea88561
Geoffrey Lovelace <geoffrey4444@gmail.com>
2018-10-16 08:18:58 -0700

    Add gauge constraint

--- src/Evolution/Systems/GeneralizedHarmonic/Constraints.hpp
+++ src/Evolution/Systems/GeneralizedHarmonic/Constraints.hpp
@@ -37,4 +37,44 @@ void three_index_constraint(
     const tnsr::iaa<DataType, SpatialDim, Frame>& d_spacetime_metric,
     const tnsr::iaa<DataType, SpatialDim, Frame>& phi) noexcept;
 // @}
+
+// @{
+/*!
+ * \brief Computes the generalized-harmonic gauge constraint.
+ *
+ * \details Computes the generalized-harmonic gauge constraint
+ * [Eq. (40) of http://arXiv.org/abs/gr-qc/0512093v3],
+ * \f[
+ * C_a = H_a + g^{ij} \Phi_{ija} + t^b \Pi_{ba}
+ * - \frac{1}{2} g^i_a \psi^{bc} \Phi_{ibc}
+ * - \frac{1}{2} t_a \psi^{bc} \Pi_{bc},
+ * \f]
+ * where \f$H_a\f$ is the gauge function,
+ * \f$\psi_{ab}\f$ is the spacetime metric,
+ * \f$\Pi_{ab}=-t^c\partial_c \psi_{ab}\f$, and
+ * \f$\Phi_{iab} = \partial_i\psi_{ab}\f$; \f$t^a\f$ is the timelike unit
+ * normal vector to the spatial slice, \f$g^{ij}\f$ is the inverse spatial
+ * metric, and \f$g^b_c = \delta^b_c + t^b t_c\f$.
+ */
+template <size_t SpatialDim, typename Frame, typename DataType>
+tnsr::a<DataType, SpatialDim, Frame> gauge_constraint(
+    const tnsr::a<DataType, SpatialDim, Frame>& gauge_function,
+    const tnsr::a<DataType, SpatialDim, Frame>& spacetime_normal_one_form,
+    const tnsr::A<DataType, SpatialDim, Frame>& spacetime_normal_vector,
+    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric,
+    const tnsr::AA<DataType, SpatialDim, Frame>& inverse_spacetime_metric,
+    const tnsr::aa<DataType, SpatialDim, Frame>& pi,
+    const tnsr::iaa<DataType, SpatialDim, Frame>& phi) noexcept;
+
+template <size_t SpatialDim, typename Frame, typename DataType>
+void gauge_constraint(
+    gsl::not_null<tnsr::a<DataType, SpatialDim, Frame>*> constraint,
+    const tnsr::a<DataType, SpatialDim, Frame>& gauge_function,
+    const tnsr::a<DataType, SpatialDim, Frame>& spacetime_normal_one_form,
+    const tnsr::A<DataType, SpatialDim, Frame>& spacetime_normal_vector,
+    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric,
+    const tnsr::AA<DataType, SpatialDim, Frame>& inverse_spacetime_metric,
+    const tnsr::aa<DataType, SpatialDim, Frame>& pi,
+    const tnsr::iaa<DataType, SpatialDim, Frame>& phi) noexcept;
+// @}
 }  // namespace GeneralizedHarmonic
--- tests/Unit/Evolution/Systems/GeneralizedHarmonic/TestFunctions.py
+++ tests/Unit/Evolution/Systems/GeneralizedHarmonic/TestFunctions.py
@@ -25,3 +25,21 @@ def phi_dot_flux(spacetime_metric, pi, phi, gamma1, gamma2, lapse, shift,
         + lapse * np.einsum("i,ab->iab", unit_normal, pi) \
            - gamma2 * lapse * np.einsum("i,ab->iab", unit_normal,
                                         spacetime_metric)
+
+def gauge_constraint(gauge_function, spacetime_normal_one_form,
+                     spacetime_normal_vector, inverse_spatial_metric,
+                     inverse_spacetime_metric, pi, phi):
+    # Sums only go over spatial indices of second index of phi
+    phi_ija = phi[:,1:,:]
+    spacetime_normal_vector_I = spacetime_normal_vector[1:]
+    constraint = gauge_function \
+      + np.einsum("ij,ija->a", inverse_spatial_metric, phi_ija) \
+      + np.einsum("b,ba->a", spacetime_normal_vector, pi) \
+      - 0.5 * np.insert(np.einsum("bc,ibc->i", inverse_spacetime_metric, phi), \
+                                  0, np.zeros(phi[0][0][0].shape)) \
+      - 0.5 * np.einsum("a,i,bc,ibc->a", spacetime_normal_one_form, \
+                        spacetime_normal_vector_I, \
+                        inverse_spacetime_metric, phi) \
+      - 0.5 * np.einsum("a,bc,bc->a", spacetime_normal_one_form, \
+                        inverse_spacetime_metric, pi)
+    return constraint
