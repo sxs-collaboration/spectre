e0fcc41a3a7973bb57e5b24a58bd5e1c27dc424e
Jordan Moxon <jem497@cornell.edu>
2018-10-20 14:32:04 -0700

    Treat unweighted values as spin 0; more tests
    - Add treatment of wrapped types as implicitly of Spin-0.
    - Add test that invalid spin-weight operations fails to compile (via SFINAE)

--- src/DataStructures/SpinWeighted.hpp
+++ src/DataStructures/SpinWeighted.hpp
@@ -25,8 +25,9 @@ struct SpinWeighted {
   constexpr static int spin = Spin;
 };
 
+// {@
 /// \brief Add two spin-weighted quantities if the types are compatible and
-/// spins are the same.
+/// spins are the same. Un-weighted quantities are assumed to be spin 0.
 template <typename T1, typename T2, int Spin>
 SPECTRE_ALWAYS_INLINE
     SpinWeighted<decltype(std::declval<T1>() + std::declval<T2>()), Spin>
@@ -34,9 +35,21 @@ SPECTRE_ALWAYS_INLINE
               const SpinWeighted<T2, Spin>& rhs) noexcept {
   return {lhs.data + rhs.data};
 }
+template <typename T>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, 0> operator+(
+    const SpinWeighted<T, 0>& lhs, const T& rhs) noexcept {
+  return {lhs.data + rhs};
+}
+template <typename T>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, 0> operator+(
+    const T& lhs, const SpinWeighted<T, 0>& rhs) noexcept {
+  return {lhs + rhs.data};
+}
+// @}
 
+// @{
 /// \brief Subtract two spin-weighted quantities if the types are compatible and
-/// spins are the same.
+/// spins are the same. Un-weighted quantities are assumed to be spin 0.
 template <typename T1, typename T2, int Spin>
 SPECTRE_ALWAYS_INLINE
     SpinWeighted<decltype(std::declval<T1>() - std::declval<T2>()), Spin>
@@ -44,9 +57,21 @@ SPECTRE_ALWAYS_INLINE
               const SpinWeighted<T2, Spin>& rhs) noexcept {
   return {lhs.data - rhs.data};
 }
+template <typename T>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, 0> operator-(
+    const SpinWeighted<T, 0>& lhs, const T& rhs) noexcept {
+  return {lhs.data - rhs};
+}
+template <typename T>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, 0> operator-(
+    const T& lhs, const SpinWeighted<T, 0>& rhs) noexcept {
+  return {lhs - rhs.data};
+}
+// @}
 
+// @{
 /// \brief Multiply two spin-weighted quantities if the types are compatible and
-/// add the spins.
+/// add the spins. Un-weighted quantities are assumed to be spin 0.
 template <typename T1, typename T2, int Spin1, int Spin2>
 SPECTRE_ALWAYS_INLINE SpinWeighted<
     decltype(std::declval<T1>() * std::declval<T2>()), Spin1 + Spin2>
@@ -54,9 +79,21 @@ operator*(const SpinWeighted<T1, Spin1>& lhs,
           const SpinWeighted<T2, Spin2>& rhs) noexcept {
   return {lhs.data * rhs.data};
 }
+template <typename T, int Spin>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, Spin> operator*(
+    const SpinWeighted<T, Spin>& lhs, const T& rhs) noexcept {
+  return {lhs.data * rhs};
+}
+template <typename T, int Spin>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, Spin> operator*(
+    const T& lhs, const SpinWeighted<T, Spin>& rhs) noexcept {
+  return {lhs * rhs.data};
+}
+// @}
 
+// @{
 /// \brief Divide two spin-weighted quantities if the types are compatible and
-/// subtract the spins.
+/// subtract the spins. Un-weighted quantities are assumed to be spin 0.
 template <typename T1, typename T2, int Spin1, int Spin2>
 SPECTRE_ALWAYS_INLINE SpinWeighted<
     decltype(std::declval<T1>() / std::declval<T2>()), Spin1 - Spin2>
@@ -64,21 +101,58 @@ operator/(const SpinWeighted<T1, Spin1>& lhs,
           const SpinWeighted<T2, Spin2>& rhs) noexcept {
   return {lhs.data / rhs.data};
 }
+template <typename T, int Spin>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, Spin> operator/(
+    const SpinWeighted<T, Spin>& lhs, const T& rhs) noexcept {
+  return {lhs.data / rhs};
+}
+template <typename T, int Spin>
+SPECTRE_ALWAYS_INLINE SpinWeighted<T, -Spin> operator/(
+    const T& lhs, const SpinWeighted<T, Spin>& rhs) noexcept {
+  return {lhs / rhs.data};
+}
+// @}
 
+// @{
 /// \brief Test equivalence of spin-weighted quantities if the types are
-/// compatible and spins are the same.
+/// compatible and spins are the same. Un-weighted quantities are assumed to be
+/// spin 0.
 template <typename T1, typename T2, int Spin>
 SPECTRE_ALWAYS_INLINE bool operator==(
     const SpinWeighted<T1, Spin>& lhs,
     const SpinWeighted<T2, Spin>& rhs) noexcept {
   return lhs.data == rhs.data;
 }
+template <typename T>
+SPECTRE_ALWAYS_INLINE bool operator==(const SpinWeighted<T, 0>& lhs,
+                                      const T& rhs) noexcept {
+  return lhs.data == rhs;
+}
+template <typename T>
+SPECTRE_ALWAYS_INLINE bool operator==(const T& lhs,
+                                      const SpinWeighted<T, 0>& rhs) noexcept {
+  return lhs == rhs.data;
+}
+// @}
 
+// @{
 /// \brief Test inequivalence of spin-weighted quantities if the types are
-/// compatible and spins are the same.
+/// compatible and spins are the same. Un-weighted quantities are assumed to be
+/// spin 0.
 template <typename T1, typename T2, int Spin>
 SPECTRE_ALWAYS_INLINE bool operator!=(
     const SpinWeighted<T1, Spin>& lhs,
     const SpinWeighted<T2, Spin>& rhs) noexcept {
   return not(lhs == rhs);
 }
+template <typename T>
+SPECTRE_ALWAYS_INLINE bool operator!=(const SpinWeighted<T, 0>& lhs,
+                                      const T& rhs) noexcept {
+  return not(lhs == rhs);
+}
+template <typename T>
+SPECTRE_ALWAYS_INLINE bool operator!=(const T& lhs,
+                                      const SpinWeighted<T, 0>& rhs) noexcept {
+  return not(lhs == rhs);
+}
+// @}
