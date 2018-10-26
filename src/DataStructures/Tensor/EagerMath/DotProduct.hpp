0bdc0152307032c85d382852df0bec0ff7c27a29
Nils Deppe <nd357@cornell.edu>
2018-10-26 16:31:32 -0400

    Add not_null versions of dot_product
    We can save on memory allocations of temporaries by allowing these to return by
    not_null.

--- src/DataStructures/Tensor/EagerMath/DotProduct.hpp
+++ src/DataStructures/Tensor/EagerMath/DotProduct.hpp
@@ -11,6 +11,7 @@
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "Utilities/MakeWithValue.hpp"
 
+// @{
 /*!
  * \ingroup TensorGroup
  * \brief Compute the Euclidean dot product of two vectors or one forms
@@ -20,16 +21,27 @@
  * or \f$A_a B_b \delta^{ab}\f$ for input one forms \f$A_a\f$ and \f$B_b\f$.
  */
 template <typename DataType, typename Index>
+void dot_product(
+    const gsl::not_null<Scalar<DataType>*> dot_product,
+    const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_a,
+    const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_b) noexcept {
+  get(*dot_product) = get<0>(vector_a) * get<0>(vector_b);
+  for (size_t d = 1; d < Index::dim; ++d) {
+    get(*dot_product) += vector_a.get(d) * vector_b.get(d);
+  }
+}
+
+template <typename DataType, typename Index>
 Scalar<DataType> dot_product(
     const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_a,
     const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_b) noexcept {
-  auto dot_product = make_with_value<Scalar<DataType>>(vector_a, 0.);
-  for (size_t d = 0; d < Index::dim; ++d) {
-    get(dot_product) += vector_a.get(d) * vector_b.get(d);
-  }
+  Scalar<DataType> dot_product(vector_a.size());
+  ::dot_product(make_not_null(&dot_product), vector_a, vector_b);
   return dot_product;
 }
+// @}
 
+// @{
 /*!
  * \ingroup TensorGroup
  * \brief Compute the dot product of a vector and a one form
@@ -41,17 +53,29 @@ Scalar<DataType> dot_product(
  * input vector \f$B^b\f$.
  */
 template <typename DataType, typename Index>
+void dot_product(
+    const gsl::not_null<Scalar<DataType>*> dot_product,
+    const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_a,
+    const Tensor<DataType, Symmetry<1>, index_list<change_index_up_lo<Index>>>&
+        vector_b) noexcept {
+  get(*dot_product) = get<0>(vector_a) * get<0>(vector_b);
+  for (size_t d = 1; d < Index::dim; ++d) {
+    get(*dot_product) += vector_a.get(d) * vector_b.get(d);
+  }
+}
+
+template <typename DataType, typename Index>
 Scalar<DataType> dot_product(
     const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_a,
     const Tensor<DataType, Symmetry<1>, index_list<change_index_up_lo<Index>>>&
         vector_b) noexcept {
-  auto dot_product = make_with_value<Scalar<DataType>>(vector_a, 0.);
-  for (size_t d = 0; d < Index::dim; ++d) {
-    get(dot_product) += vector_a.get(d) * vector_b.get(d);
-  }
+  Scalar<DataType> dot_product(vector_a.size());
+  ::dot_product(make_not_null(&dot_product), vector_a, vector_b);
   return dot_product;
 }
+// @}
 
+// @{
 /*!
  * \ingroup TensorGroup
  * \brief Compute the dot_product of two vectors or one forms
@@ -63,17 +87,34 @@ Scalar<DataType> dot_product(
  * and \f$B_b\f$ with an inverse metric \f$g^{ab}\f$.
  */
 template <typename DataType, typename Index>
+void dot_product(
+    const gsl::not_null<Scalar<DataType>*> dot_product,
+    const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_a,
+    const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_b,
+    const Tensor<DataType, Symmetry<1, 1>,
+                 index_list<change_index_up_lo<Index>,
+                            change_index_up_lo<Index>>>& metric) noexcept {
+  get(*dot_product) = get<0>(vector_a) * get<0>(vector_b) * get<0, 0>(metric);
+  for (size_t b = 1; b < Index::dim; ++b) {
+    get(*dot_product) += get<0>(vector_a) * vector_b.get(b) * metric.get(0, b);
+  }
+
+  for (size_t a = 1; a < Index::dim; ++a) {
+    for (size_t b = 0; b < Index::dim; ++b) {
+      get(*dot_product) += vector_a.get(a) * vector_b.get(b) * metric.get(a, b);
+    }
+  }
+}
+
+template <typename DataType, typename Index>
 Scalar<DataType> dot_product(
     const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_a,
     const Tensor<DataType, Symmetry<1>, index_list<Index>>& vector_b,
     const Tensor<DataType, Symmetry<1, 1>,
                  index_list<change_index_up_lo<Index>,
                             change_index_up_lo<Index>>>& metric) noexcept {
-  auto dot_product = make_with_value<Scalar<DataType>>(vector_a, 0.);
-  for (size_t a = 0; a < Index::dim; ++a) {
-    for (size_t b = 0; b < Index::dim; ++b) {
-      get(dot_product) += vector_a.get(a) * vector_b.get(b) * metric.get(a, b);
-    }
-  }
+  Scalar<DataType> dot_product(vector_a.size());
+  ::dot_product(make_not_null(&dot_product), vector_a, vector_b, metric);
   return dot_product;
 }
+// @}
