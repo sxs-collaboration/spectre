91fc92609a2dc1eb473407911ca039c848bbc846
Nils Deppe <nd357@cornell.edu>
2018-10-28 15:03:14 -0400

    Add not_null versions of linearize

--- src/NumericalAlgorithms/LinearOperators/Linearize.cpp
+++ src/NumericalAlgorithms/LinearOperators/Linearize.cpp
@@ -4,43 +4,77 @@
 #include "NumericalAlgorithms/LinearOperators/Linearize.hpp"
 
 #include <functional>
+#include <ostream>
 
 #include "DataStructures/DataVector.hpp"
 #include "DataStructures/Matrix.hpp"
 #include "Domain/Mesh.hpp"  // IWYU pragma: keep
+#include "ErrorHandling/Assert.hpp"
 #include "NumericalAlgorithms/LinearOperators/ApplyMatrices.hpp"
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/MakeArray.hpp"
 
 template <size_t Dim>
-DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh) {
+void linearize(const gsl::not_null<DataVector*> result, const DataVector& u,
+               const Mesh<Dim>& mesh) noexcept {
+  ASSERT(result->size() == u.size(),
+         "The size of result passed to linearize must be equal to the size of "
+         "u. result.size(): "
+             << result->size() << " u.size(): " << u.size());
   const Matrix empty{};
   auto filter = make_array<Dim>(std::cref(empty));
   for (size_t d = 0; d < Dim; d++) {
     gsl::at(filter, d) =
         std::cref(Spectral::linear_filter_matrix(mesh.slice_through(d)));
   }
-  return apply_matrices(filter, u, mesh.extents());
+  apply_matrices(result, filter, u, mesh.extents());
 }
 
-template DataVector linearize<1>(const DataVector&, const Mesh<1>&);
-template DataVector linearize<2>(const DataVector&, const Mesh<2>&);
-template DataVector linearize<3>(const DataVector&, const Mesh<3>&);
+template <size_t Dim>
+DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh) noexcept {
+  DataVector result(mesh.number_of_grid_points());
+  linearize(&result, u, mesh);
+  return result;
+}
 
 template <size_t Dim>
-DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh,
-                     const size_t d) {
+void linearize(const gsl::not_null<DataVector*> result, const DataVector& u,
+               const Mesh<Dim>& mesh, const size_t d) noexcept {
+  ASSERT(result->size() == u.size(),
+         "The size of result passed to linearize must be equal to the size of "
+         "u. result.size(): "
+             << result->size() << " u.size(): " << u.size());
   const Matrix empty{};
   auto filter = make_array<Dim>(std::cref(empty));
   gsl::at(filter, d) =
       std::cref(Spectral::linear_filter_matrix(mesh.slice_through(d)));
-  return apply_matrices(filter, u, mesh.extents());
+  apply_matrices(result, filter, u, mesh.extents());
 }
 
-template DataVector linearize<1>(const DataVector&, const Mesh<1>&,
-                                 const size_t);
-template DataVector linearize<2>(const DataVector&, const Mesh<2>&,
-                                 const size_t);
-template DataVector linearize<3>(const DataVector&, const Mesh<3>&,
-                                 const size_t);
+template <size_t Dim>
+DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh,
+                     const size_t d) noexcept {
+  DataVector result(mesh.number_of_grid_points());
+  linearize(&result, u, mesh, d);
+  return result;
+}
+
+#define GET_DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+#define INSTANTIATION(r, data)                                                 \
+  template void linearize<GET_DIM(data)>(const gsl::not_null<DataVector*>,     \
+                                         const DataVector&,                    \
+                                         const Mesh<GET_DIM(data)>&) noexcept; \
+  template DataVector linearize<GET_DIM(data)>(                                \
+      const DataVector&, const Mesh<GET_DIM(data)>&) noexcept;                 \
+  template void linearize<GET_DIM(data)>(                                      \
+      const gsl::not_null<DataVector*>, const DataVector&,                     \
+      const Mesh<GET_DIM(data)>&, const size_t);                               \
+  template DataVector linearize<GET_DIM(data)>(                                \
+      const DataVector&, const Mesh<GET_DIM(data)>&, const size_t);
+
+GENERATE_INSTANTIATIONS(INSTANTIATION, (1, 2, 3))
+
+#undef GET_DIM
+#undef INSTANTIATION
--- src/NumericalAlgorithms/LinearOperators/Linearize.hpp
+++ src/NumericalAlgorithms/LinearOperators/Linearize.hpp
@@ -8,29 +8,51 @@
 
 #include <cstddef>
 
+/// \cond
 class DataVector;
 template <size_t>
 class Mesh;
+namespace gsl {
+template <class>
+class not_null;
+}  // namespace gsl
+/// \endcond
 
+// @{
 /*!
  * \ingroup NumericalAlgorithmsGroup
- * Truncate u to a linear function in each dimension.
+ * \brief Truncate u to a linear function in each dimension.
+ *
  * Ex in 2D: \f$u^{Lin} = U_0 + U_x x + U_y y + U_{xy} xy\f$
  *
- * \returns the linearization of `u`
+ * \warning the `gsl::not_null` variant assumes `*result` is of the correct
+ * size.
  */
 template <size_t Dim>
-DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh);
+void linearize(gsl::not_null<DataVector*> result, const DataVector& u,
+               const Mesh<Dim>& mesh) noexcept;
+template <size_t Dim>
+DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh) noexcept;
+// @}
 
+// @{
 /*!
  * \ingroup NumericalAlgorithmsGroup
- * Truncate u to a linear function in the given dimension.
+ * \brief Truncate u to a linear function in the given dimension.
  *
- * \returns the linearization of `u`
- * \param u the function to linearize.
- * \param mesh the Mesh of the grid on the manifold on which `u` is
+ * **Parameters**
+ * - `u` the function to linearize.
+ * - `mesh` the Mesh of the grid on the manifold on which `u` is
  * located.
- * \param d the dimension that is to be linearized.
+ * - `d` the dimension that is to be linearized.
+ *
+ * \warning the `gsl::not_null` variant assumes `*result` is of the correct
+ * size.
  */
 template <size_t Dim>
-DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh, size_t d);
+void linearize(gsl::not_null<DataVector*> result, const DataVector& u,
+               const Mesh<Dim>& mesh, size_t d) noexcept;
+template <size_t Dim>
+DataVector linearize(const DataVector& u, const Mesh<Dim>& mesh,
+                     size_t d) noexcept;
+// @}
