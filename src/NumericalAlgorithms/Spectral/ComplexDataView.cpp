a40db20b6e1121cf2aea1b00e7387e205292cdcc
Jordan Moxon <jem497@cornell.edu>
2018-10-31 11:17:05 -0700

    Add ComplexDataView
    - This class provides a reference into a contiguous block of complex data, such
      that the real and imaginary parts of the complex data can easily be mutated or
      retrieved individually while minimizing copies.
    - The option between two possible representations of complex numbers is
      provided, either alternating real and imaginary parts or a pair of contiguous
      real and contiguous imaginary blocks.

---
+++ src/NumericalAlgorithms/Spectral/ComplexDataView.cpp
@@ -0,0 +1,297 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "NumericalAlgorithms/Spectral/ComplexDataView.hpp"
+
+#include "DataStructures/ComplexDataVector.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "ErrorHandling/Assert.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/Math.hpp"  // IWYU pragma: keep
+
+namespace Spectral {
+namespace SWSH {
+namespace detail {
+
+template <typename T>
+void sizes_assert(const T& vector, const size_t size) {
+  ASSERT(vector.size() == size,
+         "Assignment must be to the same size,"
+         " not "
+             << vector.size() << " assigned to " << size);
+}
+
+// The constructor of `ComplexDataView<ComplexRepresentation::Interleaved>`
+// makes no copies or allocations
+
+// Due to the nature of this class performing manual memory management, indexing
+// into complex data with perscribed strides, it is necessary to violate type
+// system conventions to index into the complex data as though it is double
+// data, thus using a `reinterpret_cast`. The C++ standard guarantees that
+// complex numbers are represented in memory in such a way that this is correct,
+// despite the type subversion. We also cannot take the address of the
+// components of the complex data, as those are returned by value, not lvalue
+// reference.
+template <>
+ComplexDataView<ComplexRepresentation::Interleaved>::ComplexDataView(
+    const gsl::not_null<ComplexDataVector*> vector, const size_t size,
+    const size_t offset) noexcept
+    : size_{size},
+      real_slices_up_to_date_{false},
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+      data_real_{reinterpret_cast<double*>(vector->data() + offset)},
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      data_imag_{data_real_ + 1} {
+  complex_view_.set_data_ref(vector->data() + offset, size);
+}
+
+// Due to the nature of this class performing manual memory management, indexing
+// into complex data with perscribed strides, it is necessary to violate type
+// system conventions to index into the complex data as though it is double
+// data, thus using a `reinterpret_cast`. The C++ standard guarantees that
+// complex numbers are represented in memory in such a way that this is correct,
+// despite the type subversion. We also cannot take the address of the
+// components of the complex data, as those are returned by value, not lvalue
+// reference.
+template <>
+ComplexDataView<ComplexRepresentation::Interleaved>::ComplexDataView(
+    std::complex<double>* const start, const size_t size) noexcept
+    : size_{size},
+      real_slices_up_to_date_{false},
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+      data_real_{reinterpret_cast<double*>(start)},
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      data_imag_{data_real_ + 1} {
+  complex_view_.set_data_ref(start, size);
+}
+
+// RealsThenImags constructors cause an allocation and a copy for each of the
+// real and imaginary components of the vector. If it becomes a performance
+// concern, the `real_slice_` and `imag_slice_` could be made as non-owning
+// DataVectors which reference a larger block of memory allocated only once.
+template <>
+ComplexDataView<ComplexRepresentation::RealsThenImags>::ComplexDataView(
+    const gsl::not_null<ComplexDataVector*> vector, const size_t size,
+    const size_t offset) noexcept
+    : size_{size}, real_slices_up_to_date_{true}, complex_view_{} {
+  complex_view_.set_data_ref(vector->data() + offset, size_);
+  real_slice_ = real(complex_view_);
+  imag_slice_ = imag(complex_view_);
+  data_real_ = real_slice_.data();
+  data_imag_ = imag_slice_.data();
+}
+
+template <>
+ComplexDataView<ComplexRepresentation::RealsThenImags>::ComplexDataView(
+    std::complex<double>* const start, const size_t size) noexcept
+    : size_{size}, real_slices_up_to_date_{true}, complex_view_{} {
+  complex_view_.set_data_ref(start, size_);
+  real_slice_ = real(complex_view_);
+  imag_slice_ = imag(complex_view_);
+  data_real_ = real_slice_.data();
+  data_imag_ = imag_slice_.data();
+}
+
+// For `ComplexDataView<ComplexRepresentation::Interleaved>`, assigning
+// individual components is comparatively slow, and is currently implemented as
+// loop with the appropriate stride in the contiguous view.
+
+template <>
+ComplexDataView<ComplexRepresentation::Interleaved>&
+ComplexDataView<ComplexRepresentation::Interleaved>::assign_real(
+    const DataVector& vector) noexcept {
+  if (real_slice_.data() != vector.data() or not real_slices_up_to_date_) {
+    sizes_assert(vector, size_);
+    for (size_t i = 0; i < size_; i++) {
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      data_real_[stride_ * i] = vector[i];
+    }
+    real_slices_up_to_date_ = false;
+  }
+  return *this;
+}
+
+template <>
+ComplexDataView<ComplexRepresentation::Interleaved>&
+ComplexDataView<ComplexRepresentation::Interleaved>::assign_imag(
+    const DataVector& vector) noexcept {
+  if (imag_slice_.data() != vector.data() or not real_slices_up_to_date_) {
+    sizes_assert(vector, size_);
+    for (size_t i = 0; i < size_; i++) {
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      data_imag_[stride_ * i] = vector[i];
+    }
+    real_slices_up_to_date_ = false;
+  }
+  return *this;
+}
+
+// For `ComplexDataView<ComplexRepresentation::RealsThenImags>` views, the
+// assignment to individual components assigns directly to the internal
+// DataVectors of the separate components, which is comparatively fast.
+template <>
+ComplexDataView<ComplexRepresentation::RealsThenImags>&
+ComplexDataView<ComplexRepresentation::RealsThenImags>::assign_real(
+    const DataVector& vector) noexcept {
+  if (real_slice_.data() != vector.data()) {
+    sizes_assert(vector, size_);
+    real_slice_ = vector;
+  }
+  return *this;
+}
+
+template <>
+ComplexDataView<ComplexRepresentation::RealsThenImags>&
+ComplexDataView<ComplexRepresentation::RealsThenImags>::assign_imag(
+    const DataVector& vector) noexcept {
+  if (imag_slice_.data() != vector.data()) {
+    sizes_assert(vector, size_);
+    imag_slice_ = vector;
+  }
+  return *this;
+}
+
+// `ComplexDataView<ComplexRepresentation::Interleaved>` views are simply
+// references back to the source data, therefore the source is perpetually in
+// sync with the view. This function is a no-op.
+template <>
+void ComplexDataView<
+    ComplexRepresentation::Interleaved>::copy_back_to_source() noexcept {}
+
+// `ComplexDataView<ComplexRepresentation::RealsThenImags>` views perform a true
+// copy back to the source vector when this function is called
+template <>
+void ComplexDataView<
+    ComplexRepresentation::RealsThenImags>::copy_back_to_source() noexcept {
+  for (size_t i = 0; i < size_; i++) {
+    complex_view_[i] = std::complex<double>{real_slice_[i], imag_slice_[i]};
+  }
+}
+
+// `ComplexDataView<ComplexRepresentation::Interleaved>` views perform
+// assignments from complex vectors comparatively quickly, via the assignments
+// using the underlying complex vector types.
+template <>
+ComplexDataView<ComplexRepresentation::Interleaved>&
+ComplexDataView<ComplexRepresentation::Interleaved>::operator=(
+    const ComplexDataVector& vector) noexcept {
+  if (complex_view_.data() != vector.data()) {
+    sizes_assert(vector, size_);
+    complex_view_ = vector;
+    real_slices_up_to_date_ = false;
+  }
+  return *this;
+}
+
+template <>
+ComplexDataView<ComplexRepresentation::Interleaved>&
+ComplexDataView<ComplexRepresentation::Interleaved>::operator=(
+    const ComplexDataView<ComplexRepresentation::Interleaved>& view) noexcept {
+  if (this != &view) {
+    sizes_assert(view, size_);
+    complex_view_ = view.complex_view_;
+    real_slices_up_to_date_ = false;
+  }
+  return *this;
+}
+
+template <>
+void ComplexDataView<ComplexRepresentation::Interleaved>::conjugate() noexcept {
+  complex_view_ = conj(complex_view_);
+  real_slices_up_to_date_ = false;
+}
+
+// `ComplexDataView<ComplexRepresentation::RealsThenImags>` views perform
+// assignments from complex vectors comparatively slowly. The complex data must
+// be separately converted to the real and imaginary components in the form of
+// DataVectors, then stored in the appropriate internal components.
+template <>
+ComplexDataView<ComplexRepresentation::RealsThenImags>&
+ComplexDataView<ComplexRepresentation::RealsThenImags>::operator=(
+    const ComplexDataVector& vector) noexcept {
+  sizes_assert(vector, size_);
+  real_slice_ = real(vector);
+  imag_slice_ = imag(vector);
+  return *this;
+}
+
+template <>
+ComplexDataView<ComplexRepresentation::RealsThenImags>&
+ComplexDataView<ComplexRepresentation::RealsThenImags>::operator=(
+    const ComplexDataView<ComplexRepresentation::RealsThenImags>&
+        view) noexcept {
+  sizes_assert(view, size_);
+  if (real_slice_.data() != view.real_slice_.data()) {
+    real_slice_ = view.real_slice_;
+  }
+  if (imag_slice_.data() != view.imag_slice_.data()) {
+    imag_slice_ = view.imag_slice_;
+  }
+  return *this;
+}
+
+template <>
+void ComplexDataView<
+    ComplexRepresentation::RealsThenImags>::conjugate() noexcept {
+  imag_slice_ = -imag_slice_;
+}
+
+// `ComplexDataView<ComplexRepresentation::Interleaved>` views have the
+// individual pointer positions only one double length apart, with a memory
+// stride of 2, as the real and imaginary parts alternate in memory.
+template <>
+double*
+ComplexDataView<ComplexRepresentation::Interleaved>::real_data() noexcept {
+  return data_real_;
+}
+
+template <>
+const double* ComplexDataView<ComplexRepresentation::Interleaved>::real_data()
+    const noexcept {
+  return data_real_;
+}
+
+template <>
+double*
+ComplexDataView<ComplexRepresentation::Interleaved>::imag_data() noexcept {
+  return data_imag_;
+}
+
+template <>
+const double* ComplexDataView<ComplexRepresentation::Interleaved>::imag_data()
+    const noexcept {
+  return data_imag_;
+}
+
+// `ComplexDataView<ComplexRepresentation::RealsThenImags>` views have
+// potentially very different pointer positions for the start of the individual
+// vectors, but have a memory stride of 1, as each of the two blocks is a
+// contiguous representation.
+template <>
+double*
+ComplexDataView<ComplexRepresentation::RealsThenImags>::real_data() noexcept {
+  return real_slice_.data();
+}
+
+template <>
+const double*
+ComplexDataView<ComplexRepresentation::RealsThenImags>::real_data() const
+    noexcept {
+  return real_slice_.data();
+}
+
+template <>
+double*
+ComplexDataView<ComplexRepresentation::RealsThenImags>::imag_data() noexcept {
+  return imag_slice_.data();
+}
+
+template <>
+const double*
+ComplexDataView<ComplexRepresentation::RealsThenImags>::imag_data() const
+    noexcept {
+  return imag_slice_.data();
+}
+}  // namespace detail
+}  // namespace SWSH
+}  // namespace Spectral
---
+++ tests/Unit/NumericalAlgorithms/Spectral/Test_ComplexDataView.cpp
@@ -0,0 +1,250 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "tests/Unit/TestingFramework.hpp"
+
+#include <complex>
+#include <cstddef>
+#include <random>
+#include <vector>
+
+#include "DataStructures/ComplexDataVector.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "ErrorHandling/Error.hpp"  // IWYU pragma: keep
+#include "NumericalAlgorithms/Spectral/ComplexDataView.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/Math.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+#include "tests/Utilities/MakeWithRandomValues.hpp"
+
+namespace Spectral {
+namespace SWSH {
+namespace detail {
+namespace {
+
+template <ComplexRepresentation Representation>
+void test_basic_view_functionality(
+    const ComplexDataView<Representation>& view,
+    std::complex<double>* const source_vec_data, const size_t view_size,
+    const size_t offset) noexcept {
+  CHECK(view.size() == view_size);
+  // clang-tidy: This class uses manual memory management deliberately,
+  // so silence complaints about pointer math and casts.
+  // The reinterpret casts are intentional. See
+  // NumericalAlgorithms/Spectral/ComplexDataView.cpp for an explanation
+  if (Representation == ComplexRepresentation::Interleaved) {
+    CHECK(view.real_data() ==
+          reinterpret_cast<double*>(source_vec_data + offset));  // NOLINT
+    CHECK(view.real_data() ==
+          reinterpret_cast<double*>(source_vec_data + offset));  // NOLINT
+  }
+  CHECK(view.stride() ==
+        (Representation == ComplexRepresentation::Interleaved ? 2 : 1));
+}
+
+template <ComplexRepresentation Representation>
+void test_view() noexcept {
+  MAKE_GENERATOR(gen);
+  UniformCustomDistribution<double> dist{-100.0, 100.0};
+  UniformCustomDistribution<size_t> sdist{5, 50};
+  const size_t overall_size = sdist(gen);
+  const size_t view_size = sdist(gen) % (overall_size) + 1;
+  const size_t offset = sdist(gen) % (overall_size - view_size + 1);
+  const size_t stride = ComplexDataView<Representation>::stride();
+  ComplexDataVector source_vec{overall_size};
+  std::vector<std::complex<double>> ptr_source_vec(overall_size);
+  ComplexDataVector assign_vec_1{view_size};
+  ComplexDataVector assign_vec_2{view_size};
+  ComplexDataVector assign_view_source{view_size};
+
+  fill_with_random_values(make_not_null(&source_vec), make_not_null(&gen),
+                          make_not_null(&dist));
+  fill_with_random_values(make_not_null(&ptr_source_vec), make_not_null(&gen),
+                          make_not_null(&dist));
+
+  ComplexDataView<Representation> vector_view{make_not_null(&source_vec),
+                                              view_size, offset};
+  test_basic_view_functionality(vector_view, source_vec.data(), view_size,
+                                offset);
+
+  const ComplexDataVector source_vec_copy = source_vec;
+  // check conjugation utility
+  vector_view.conjugate();
+  for (size_t i = 0; i < view_size; i++) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.real_data()[stride * i] ==
+          real(source_vec_copy[i + offset]));
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.imag_data()[stride * i] ==
+          -imag(source_vec_copy[i + offset]));
+  }
+
+  vector_view.copy_back_to_source();
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(source_vec[i + offset] == conj(source_vec_copy)[i + offset]);
+  }
+
+  // reverse the conjugation and check again
+  vector_view.conjugate();
+  for (size_t i = 0; i < view_size; i++) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.real_data()[stride * i] ==
+          real(source_vec_copy[i + offset]));
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.imag_data()[stride * i] ==
+          imag(source_vec_copy[i + offset]));
+  }
+
+  vector_view.copy_back_to_source();
+  const ComplexDataView<Representation> ptr_view{
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      ptr_source_vec.data() + offset, view_size};
+  test_basic_view_functionality(ptr_view, ptr_source_vec.data(), view_size,
+                                offset);
+
+  const ComplexDataVector pre_change =
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      ComplexDataVector{source_vec.data() + offset, view_size};
+  // check the various assignment operators
+  for (size_t i = 0; i < view_size; i++) {
+    assign_vec_1[i] = source_vec[i + offset] + std::complex<double>{1.0, 1.0};
+    assign_vec_2[i] = source_vec[i + offset] + std::complex<double>{2.0, 2.0};
+  }
+
+  // real assignment
+  vector_view.assign_real(real(assign_vec_1));
+  for (size_t i = 0; i < view_size; i++) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.real_data()[stride * i] == real(assign_vec_1)[i]);
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.imag_data()[stride * i] == imag(pre_change)[i]);
+  }
+
+  vector_view.copy_back_to_source();
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(real(assign_vec_1[i]) == real(source_vec[i + offset]));
+    CHECK(imag(pre_change[i]) == imag(source_vec[i + offset]));
+  }
+
+  // imag assignment
+  vector_view.assign_imag(imag(assign_vec_2));
+  for (size_t i = 0; i < view_size; i++) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.real_data()[stride * i] == real(assign_vec_1)[i]);
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    CHECK(vector_view.imag_data()[stride * i] == imag(assign_vec_2)[i]);
+  }
+
+  vector_view.copy_back_to_source();
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(real(assign_vec_1[i]) == real(source_vec[i + offset]));
+    CHECK(imag(assign_vec_2[i]) == imag(source_vec[i + offset]));
+  }
+
+  // check self-assignment from the referenced vector
+  assign_vec_1 = assign_vec_1;
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(real(assign_vec_1[i]) == real(source_vec[i + offset]));
+    CHECK(imag(assign_vec_1[i]) == imag(assign_vec_1[i]));
+  }
+
+  // full assignment from vector
+  fill_with_random_values(make_not_null(&assign_vec_1), make_not_null(&gen),
+                          make_not_null(&dist));
+  vector_view = assign_vec_1;
+  vector_view.copy_back_to_source();
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(source_vec[i + offset] == assign_vec_1[i]);
+  }
+
+  // full assignment from a view
+  fill_with_random_values(make_not_null(&assign_view_source),
+                          make_not_null(&gen), make_not_null(&dist));
+  ComplexDataView<Representation> assign_view{
+      make_not_null(&assign_view_source), assign_view_source.size()};
+  vector_view = assign_view;
+  vector_view.copy_back_to_source();
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(source_vec[i + offset] == assign_view_source[i]);
+  }
+
+  // check self-assignment from the view
+  // clang-tidy and gcc ignore for allowing the intentional self-assignment
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wextra"
+  vector_view = vector_view; // NOLINT
+#pragma GCC diagnostic pop
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(source_vec[i + offset] == assign_view_source[i]);
+  }
+  vector_view.copy_back_to_source();
+  for (size_t i = 0; i < view_size; i++) {
+    CHECK(source_vec[i + offset] == assign_view_source[i]);
+  }
+}
+
+SPECTRE_TEST_CASE("Unit.NumericalAlgorithms.Spectral.ComplexDataView",
+                  "[Unit][NumericalAlgorithms]") {
+    {
+        INFO("Test Interleaved view");
+        test_view<ComplexRepresentation::Interleaved>();
+    }
+    {
+        INFO("Test RealsThenImags view");
+        test_view<ComplexRepresentation::RealsThenImags>();
+    }
+}
+
+// spot-test two assignment asserts - they all call the same size-checking
+// function, so this should be sufficiently robust.
+
+// [[OutputRegex, Assignment must be to the same size]]
+[[noreturn]] SPECTRE_TEST_CASE(
+    "Unit.NumericalAlgorithms.Spectral.ComplexDataView.ComplexSizeError",
+    "[Unit][NumericalAlgorithms]") {
+  ASSERTION_TEST();
+#ifdef SPECTRE_DEBUG
+  MAKE_GENERATOR(gen);
+  UniformCustomDistribution<size_t> sdist{5, 50};
+  const size_t overall_size = sdist(gen);
+  const size_t view_size = sdist(gen) % (overall_size - 1) + 1;
+  const size_t offset = sdist(gen) % (overall_size - view_size + 1);
+
+  ComplexDataVector source_vec{overall_size};
+  ComplexDataView<ComplexRepresentation::Interleaved> vector_view_1{
+      make_not_null(&source_vec), view_size, offset};
+  ComplexDataView<ComplexRepresentation::Interleaved> vector_view_2{
+      make_not_null(&source_vec), view_size + 1, offset};
+
+  // this line should fail the size assert
+  vector_view_1 = vector_view_2;
+  ERROR("Failed to trigger ASSERT in an assertion test");
+#endif
+}
+
+// [[OutputRegex, Assignment must be to the same size]]
+[[noreturn]] SPECTRE_TEST_CASE(
+    "Unit.NumericalAlgorithms.Spectral.ComplexDataView.RealSizeError",
+    "[Unit][NumericalAlgorithms]") {
+  ASSERTION_TEST();
+#ifdef SPECTRE_DEBUG
+  MAKE_GENERATOR(gen);
+  UniformCustomDistribution<size_t> sdist{5, 50};
+  const size_t overall_size = sdist(gen);
+  const size_t view_size = sdist(gen) % (overall_size - 1) + 1;
+  const size_t offset = sdist(gen) % (overall_size - view_size + 1);
+
+  ComplexDataVector source_vec{overall_size};
+  ComplexDataView<ComplexRepresentation::RealsThenImags> vector_view_1{
+      make_not_null(&source_vec), view_size, offset};
+
+  // this line should fail the size assert
+  vector_view_1.assign_real(real(source_vec));
+  ERROR("Failed to trigger ASSERT in an assertion test");
+#endif
+}
+}  // namespace
+}  // namespace detail
+}  // namespace SWSH
+}  // namespace Spectral
+
