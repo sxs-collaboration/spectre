1489ffd945a4b020d878423677d1be701533f943
Geoffrey Lovelace <geoffrey4444@gmail.com>
2018-11-05 08:18:54 -0800

    Add 2-index constraint

--- tests/Unit/Evolution/Systems/GeneralizedHarmonic/TestFunctions.py
+++ tests/Unit/Evolution/Systems/GeneralizedHarmonic/TestFunctions.py
@@ -5,6 +5,8 @@ import numpy as np
 
 
 # Test functions for normal dot fluxes
+
+
 def spacetime_metric_normal_dot_flux(spacetime_metric, pi, phi, gamma1, gamma2,
                                      lapse, shift, inverse_spatial_metric,
                                      unit_normal):
@@ -26,20 +28,159 @@ def phi_dot_flux(spacetime_metric, pi, phi, gamma1, gamma2, lapse, shift,
            - gamma2 * lapse * np.einsum("i,ab->iab", unit_normal,
                                         spacetime_metric)
 
+# End test functions for normal dot fluxes
+
+# Test functions for gauge constraint
+
+
 def gauge_constraint(gauge_function, spacetime_normal_one_form,
                      spacetime_normal_vector, inverse_spatial_metric,
                      inverse_spacetime_metric, pi, phi):
     # Sums only go over spatial indices of second index of phi
-    phi_ija = phi[:,1:,:]
+    phi_ija = phi[:, 1:, :]
     spacetime_normal_vector_I = spacetime_normal_vector[1:]
     constraint = gauge_function \
-      + np.einsum("ij,ija->a", inverse_spatial_metric, phi_ija) \
-      + np.einsum("b,ba->a", spacetime_normal_vector, pi) \
-      - 0.5 * np.insert(np.einsum("bc,ibc->i", inverse_spacetime_metric, phi), \
-                                  0, np.zeros(phi[0][0][0].shape)) \
-      - 0.5 * np.einsum("a,i,bc,ibc->a", spacetime_normal_one_form, \
-                        spacetime_normal_vector_I, \
-                        inverse_spacetime_metric, phi) \
-      - 0.5 * np.einsum("a,bc,bc->a", spacetime_normal_one_form, \
-                        inverse_spacetime_metric, pi)
+        + np.einsum("ij,ija->a", inverse_spatial_metric, phi_ija) \
+        + np.einsum("b,ba->a", spacetime_normal_vector, pi) \
+        - 0.5 * np.insert(np.einsum("bc,ibc->i", inverse_spacetime_metric, phi),
+                          0, np.zeros(phi[0][0][0].shape)) \
+        - 0.5 * np.einsum("a,i,bc,ibc->a", spacetime_normal_one_form,
+                          spacetime_normal_vector_I,
+                          inverse_spacetime_metric, phi) \
+        - 0.5 * np.einsum("a,bc,bc->a", spacetime_normal_one_form,
+                          inverse_spacetime_metric, pi)
     return constraint
+
+# End test functions for gauge constraint
+
+# Test functions for two-index constraint
+
+
+def two_index_constraint_term_1_of_11(inverse_spatial_metric, d_phi):
+    d_phi_jika = d_phi[:, :, 1:, :]
+    return np.einsum("jk,jika->ia", inverse_spatial_metric, d_phi_jika)
+
+
+def two_index_constraint_term_2_of_11(spacetime_normal_vector,
+                                      spacetime_normal_one_form, inverse_spacetime_metric, d_phi):
+    spacetime_normal_vector_I = spacetime_normal_vector[1:]
+    d_phi_aicd = np.pad(d_phi, ((1, 0), (0, 0), (0, 0), (0, 0)), 'constant')
+    term = np.einsum("cd,aicd->ia", inverse_spacetime_metric, d_phi_aicd)
+    term += np.einsum("j,a,cd,jicd->ia", spacetime_normal_vector_I,
+                      spacetime_normal_one_form, inverse_spacetime_metric, d_phi)
+    return -0.5 * term
+
+
+def two_index_constraint_term_3_of_11(spacetime_normal_vector, d_pi):
+    return np.einsum("b,iba->ia", spacetime_normal_vector, d_pi)
+
+
+def two_index_constraint_term_4_of_11(spacetime_normal_one_form,
+                                      inverse_spacetime_metric, d_pi):
+    return -0.5 * np.einsum("cd,icd,a->ia", inverse_spacetime_metric, d_pi,
+                            spacetime_normal_one_form)
+
+
+def two_index_constraint_term_5_of_11(d_gauge_function):
+    return d_gauge_function
+
+
+def two_index_constraint_term_6_of_11(spacetime_normal_vector,
+                                      spacetime_normal_one_form, phi, inverse_spacetime_metric):
+    spacetime_normal_vector_I = spacetime_normal_vector[1:]
+    phi_acd = np.pad(phi, ((1, 0), (0, 0), (0, 0)), 'constant')
+    term = np.einsum("acd,ief,ce,df->ia", phi_acd, phi, inverse_spacetime_metric,
+                     inverse_spacetime_metric)
+    term += np.einsum("j,a,jcd,ief,ce,df->ia", spacetime_normal_vector_I,
+                      spacetime_normal_one_form, phi, phi,
+                      inverse_spacetime_metric,
+                      inverse_spacetime_metric)
+    return 0.5 * term
+
+
+def two_index_constraint_term_7_of_11(inverse_spatial_metric, phi,
+                                      inverse_spacetime_metric, spacetime_normal_vector,
+                                      spacetime_normal_one_form):
+    phi_ike = phi[:, 1:, :]
+    return 0.5 * np.einsum("jk,jcd,ike,cd,e,a->ia", inverse_spatial_metric, phi,
+                           phi_ike, inverse_spacetime_metric,
+                           spacetime_normal_vector,
+                           spacetime_normal_one_form)
+
+
+def two_index_constraint_term_8_of_11(inverse_spatial_metric, phi):
+    phi_jma = phi[:, 1:, :]
+    phi_ikn = phi[:, 1:, 1:]
+    return -1.0 * np.einsum("jk,mn,jma,ikn->ia", inverse_spatial_metric, inverse_spatial_metric, phi_jma, phi_ikn)
+
+
+def two_index_constraint_term_9_of_11(phi, pi, spacetime_normal_one_form,
+                                      inverse_spacetime_metric,
+                                      spacetime_normal_vector):
+    term = 0.5 * np.einsum("icd,be,a,cb,de->ia", phi, pi,
+                           spacetime_normal_one_form, inverse_spacetime_metric,
+                           inverse_spacetime_metric)
+    term += 0.25 * np.einsum("icd,be,a,be,c,d->ia", phi, pi,
+                             spacetime_normal_one_form, inverse_spacetime_metric,
+                             spacetime_normal_vector, spacetime_normal_vector)
+    return term
+
+
+def two_index_constraint_term_10_of_11(phi, pi, spacetime_normal_vector,
+                                       inverse_spacetime_metric):
+    term = -1.0 * np.einsum("icd,ba,c,bd->ia", phi, pi,
+                            spacetime_normal_vector, inverse_spacetime_metric)
+    term -= 0.5 * np.einsum("icd,ba,c,b,d->ia", phi, pi,
+                            spacetime_normal_vector, spacetime_normal_vector,
+                            spacetime_normal_vector)
+    return term
+
+
+def two_index_constraint_term_11_of_11(gamma2, spacetime_normal_one_form, inverse_spacetime_metric, spacetime_normal_vector, three_index_constraint):
+    term = 0.5 * gamma2 * np.einsum("a,cd,icd->ia",
+                                    spacetime_normal_one_form,
+                                    inverse_spacetime_metric,
+                                    three_index_constraint)
+    term -= gamma2 * np.einsum("d,iad->ia",
+                               spacetime_normal_vector, three_index_constraint)
+    return term
+
+
+def two_index_constraint(d_gauge_function, spacetime_normal_one_form,
+                         spacetime_normal_vector, inverse_spatial_metric,
+                         inverse_spacetime_metric, pi, phi, d_pi, d_phi, gamma2,
+                         three_index_constraint):
+    constraint = two_index_constraint_term_1_of_11(
+        inverse_spatial_metric, d_phi)
+    constraint += two_index_constraint_term_2_of_11(spacetime_normal_vector,
+                                                    spacetime_normal_one_form,
+                                                    inverse_spacetime_metric,
+                                                    d_phi)
+    constraint += two_index_constraint_term_3_of_11(
+        spacetime_normal_vector, d_pi)
+    constraint += two_index_constraint_term_4_of_11(spacetime_normal_one_form,
+                                                    inverse_spacetime_metric,
+                                                    d_pi)
+    constraint += two_index_constraint_term_5_of_11(d_gauge_function)
+    constraint += two_index_constraint_term_6_of_11(spacetime_normal_vector,
+                                                    spacetime_normal_one_form,
+                                                    phi, inverse_spacetime_metric)
+    constraint += two_index_constraint_term_7_of_11(inverse_spatial_metric, phi,
+                                                    inverse_spacetime_metric, spacetime_normal_vector,
+                                                    spacetime_normal_one_form)
+    constraint += two_index_constraint_term_8_of_11(
+        inverse_spatial_metric, phi)
+    constraint += two_index_constraint_term_9_of_11(phi, pi,
+                                                    spacetime_normal_one_form,
+                                                    inverse_spacetime_metric,
+                                                    spacetime_normal_vector)
+    constraint += two_index_constraint_term_10_of_11(
+        phi, pi, spacetime_normal_vector, inverse_spacetime_metric)
+    constraint += two_index_constraint_term_11_of_11(gamma2,
+                                                     spacetime_normal_one_form,
+                                                     inverse_spacetime_metric,
+                                                     spacetime_normal_vector,
+                                                     three_index_constraint)
+    return constraint
+
+# End test functions for two-index constraint
--- tests/Unit/Evolution/Systems/GeneralizedHarmonic/Test_Constraints.cpp
+++ tests/Unit/Evolution/Systems/GeneralizedHarmonic/Test_Constraints.cpp
@@ -13,12 +13,15 @@
 #include "DataStructures/DataVector.hpp"  // IWYU pragma: keep
 #include "DataStructures/Tensor/EagerMath/DeterminantAndInverse.hpp"
 #include "DataStructures/Tensor/TypeAliases.hpp"
+#include "DataStructures/Variables.hpp"
 #include "Domain/CoordinateMaps/Affine.hpp"
 #include "Domain/CoordinateMaps/CoordinateMap.hpp"
 #include "Domain/CoordinateMaps/ProductMaps.hpp"
 #include "Domain/LogicalCoordinates.hpp"
 #include "Domain/Mesh.hpp"
 #include "Evolution/Systems/GeneralizedHarmonic/Constraints.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.tpp"  // IWYU pragma: keep
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrSchild.hpp"
 #include "PointwiseFunctions/GeneralRelativity/Christoffel.hpp"
@@ -26,14 +29,17 @@
 #include "PointwiseFunctions/GeneralRelativity/ComputeSpacetimeQuantities.hpp"
 #include "PointwiseFunctions/GeneralRelativity/IndexManipulation.hpp"
 #include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/ConstantExpressions.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/MakeWithValue.hpp"
+#include "Utilities/TMPL.hpp"
 #include "Utilities/TaggedTuple.hpp"
 #include "tests/Unit/Pypp/CheckWithRandomValues.hpp"
 #include "tests/Unit/Pypp/SetupLocalPythonEnvironment.hpp"
 
 // IWYU pragma: no_forward_declare Tensor
 // IWYU pragma: no_forward_declare Variables
+// IWYU pragma: no_forward_declare Tags::deriv
 
 namespace {
 template <size_t SpatialDim, typename Frame, typename DataType>
@@ -63,7 +69,7 @@ void test_gauge_constraint_random(const DataType& used_for_size) noexcept {
       "TestFunctions", "gauge_constraint", {{{-10.0, 10.0}}}, used_for_size);
 }
 
-// Test test return-by-reference gauge constraint by comparing to Kerr-Schild
+// Test the return-by-reference gauge constraint by comparing to Kerr-Schild
 template <typename Solution>
 void test_gauge_constraint_analytic(
     const Solution& solution, const size_t grid_size_each_dimension,
@@ -78,9 +84,9 @@ void test_gauge_constraint_analytic(
   using Affine3D = CoordinateMaps::ProductOf3Maps<Affine, Affine, Affine>;
   const auto coord_map =
       make_coordinate_map<Frame::Logical, Frame::Inertial>(Affine3D{
-          Affine{-1., 1., lower_bound[0], upper_bound[0]},
-          Affine{-1., 1., lower_bound[1], upper_bound[1]},
-          Affine{-1., 1., lower_bound[2], upper_bound[2]},
+          Affine{-1.0, 1.0, lower_bound[0], upper_bound[0]},
+          Affine{-1.0, 1.0, lower_bound[1], upper_bound[1]},
+          Affine{-1.0, 1.0, lower_bound[2], upper_bound[2]},
       });
 
   // Set up coordinates
@@ -136,6 +142,147 @@ void test_gauge_constraint_analytic(
       inverse_spatial_metric, inverse_spacetime_metric, pi, phi);
   CHECK_ITERABLE_APPROX(constraint,
                         make_with_value<decltype(constraint)>(x, 0.0));
+}
+
+// Test the return-by-value two-index constraint function using random values
+template <size_t SpatialDim, typename Frame, typename DataType>
+void test_two_index_constraint_random(const DataType& used_for_size) noexcept {
+  pypp::check_with_random_values<1>(
+      static_cast<tnsr::ia<DataType, SpatialDim, Frame> (*)(
+          const tnsr::ia<DataType, SpatialDim, Frame>&,
+          const tnsr::a<DataType, SpatialDim, Frame>&,
+          const tnsr::A<DataType, SpatialDim, Frame>&,
+          const tnsr::II<DataType, SpatialDim, Frame>&,
+          const tnsr::AA<DataType, SpatialDim, Frame>&,
+          const tnsr::aa<DataType, SpatialDim, Frame>&,
+          const tnsr::iaa<DataType, SpatialDim, Frame>&,
+          const tnsr::iaa<DataType, SpatialDim, Frame>&,
+          const tnsr::ijaa<DataType, SpatialDim, Frame>&,
+          const Scalar<DataType>&,
+          const tnsr::iaa<DataType, SpatialDim, Frame>&)>(
+          &GeneralizedHarmonic::two_index_constraint<SpatialDim, Frame,
+                                                     DataType>),
+      "TestFunctions", "two_index_constraint", {{{-10.0, 10.0}}},
+      used_for_size);
+}
+
+// Test the return-by-reference two-index constraint
+// by comparing to a time-independent analytic solution (e.g. Kerr-Schild)
+template <typename Solution>
+void test_two_index_constraint_analytic(
+    const Solution& solution, const size_t grid_size_each_dimension,
+    const std::array<double, 3>& lower_bound,
+    const std::array<double, 3>& upper_bound,
+    const double error_tolerance) noexcept {
+  // Shorter names for tags.
+  using SpacetimeMetric = gr::Tags::SpacetimeMetric<3, Frame::Inertial>;
+  using Pi = ::GeneralizedHarmonic::Pi<3, Frame::Inertial>;
+  using Phi = ::GeneralizedHarmonic::Phi<3, Frame::Inertial>;
+  using GaugeH = ::GeneralizedHarmonic::GaugeH<3, Frame::Inertial>;
+  using VariablesTags = tmpl::list<SpacetimeMetric, Pi, Phi, GaugeH>;
+
+  // Check vs. time-independent analytic solution
+  // Set up grid
+  const size_t data_size = pow<3>(grid_size_each_dimension);
+  Mesh<3> mesh{grid_size_each_dimension, Spectral::Basis::Legendre,
+               Spectral::Quadrature::GaussLobatto};
+
+  using Affine = CoordinateMaps::Affine;
+  using Affine3D = CoordinateMaps::ProductOf3Maps<Affine, Affine, Affine>;
+  const auto coord_map =
+      make_coordinate_map<Frame::Logical, Frame::Inertial>(Affine3D{
+          Affine{-1.0, 1.0, lower_bound[0], upper_bound[0]},
+          Affine{-1.0, 1.0, lower_bound[1], upper_bound[1]},
+          Affine{-1.0, 1.0, lower_bound[2], upper_bound[2]},
+      });
+
+  // Set up coordinates
+  const auto x_logical = logical_coordinates(mesh);
+  const auto x = coord_map(x_logical);
+  // Arbitrary time for time-independent solution.
+  const double t = std::numeric_limits<double>::signaling_NaN();
+
+  // Evaluate analytic solution
+  const auto vars =
+      solution.variables(x, t, typename Solution::template tags<DataVector>{});
+  const auto& lapse = get<gr::Tags::Lapse<>>(vars);
+  const auto& dt_lapse = get<Tags::dt<gr::Tags::Lapse<>>>(vars);
+  const auto& d_lapse =
+      get<typename Solution::template DerivLapse<DataVector>>(vars);
+  const auto& shift = get<gr::Tags::Shift<3>>(vars);
+  const auto& d_shift =
+      get<typename Solution::template DerivShift<DataVector>>(vars);
+  const auto& dt_shift = get<Tags::dt<gr::Tags::Shift<3>>>(vars);
+  const auto& spatial_metric = get<gr::Tags::SpatialMetric<3>>(vars);
+  const auto& dt_spatial_metric =
+      get<Tags::dt<gr::Tags::SpatialMetric<3>>>(vars);
+  const auto& d_spatial_metric =
+      get<typename Solution::template DerivSpatialMetric<DataVector>>(vars);
+
+  // Compute quantities from variables for the two-index constraint
+  const auto& inverse_spatial_metric =
+      determinant_and_inverse(spatial_metric).second;
+  const auto& inverse_spacetime_metric =
+      gr::inverse_spacetime_metric(lapse, shift, inverse_spatial_metric);
+  const auto& normal_one_form =
+      gr::spacetime_normal_one_form<3, Frame::Inertial>(lapse);
+  const auto& normal_vector = gr::spacetime_normal_vector(lapse, shift);
+
+  // Arbitrary choice for gamma2
+  const auto& gamma2 = make_with_value<Scalar<DataVector>>(x, 4.0);
+
+  // Compute derivatives d_psi, d_phi, d_pi, and d_gauge_function numerically
+  Variables<VariablesTags> gh_vars(data_size);
+  auto& spacetime_metric = get<SpacetimeMetric>(gh_vars);
+  auto& pi = get<Pi>(gh_vars);
+  auto& phi = get<Phi>(gh_vars);
+  auto& gauge_function = get<GaugeH>(gh_vars);
+  spacetime_metric = gr::spacetime_metric(lapse, shift, spatial_metric);
+  phi = GeneralizedHarmonic::phi(lapse, d_lapse, shift, d_shift, spatial_metric,
+                                 d_spatial_metric);
+  pi = GeneralizedHarmonic::pi(lapse, dt_lapse, shift, dt_shift, spatial_metric,
+                               dt_spatial_metric, phi);
+  gauge_function = GeneralizedHarmonic::gauge_source(
+      lapse, dt_lapse, d_lapse, shift, dt_shift, d_shift, spatial_metric,
+      trace(gr::extrinsic_curvature(lapse, shift, d_shift, spatial_metric,
+                                    dt_spatial_metric, d_spatial_metric),
+            inverse_spatial_metric),
+      trace_last_indices(gr::christoffel_first_kind(d_spatial_metric),
+                         inverse_spatial_metric));
+
+  // Compute numerical derivatives of psi,pi,phi,H
+  const auto gh_derivs =
+      partial_derivatives<VariablesTags, VariablesTags, 3, Frame::Inertial>(
+          gh_vars, mesh, coord_map.inv_jacobian(x_logical));
+  const auto& d_spacetime_metric =
+      get<Tags::deriv<SpacetimeMetric, tmpl::size_t<3>, Frame::Inertial>>(
+          gh_derivs);
+  const auto& d_pi =
+      get<Tags::deriv<Pi, tmpl::size_t<3>, Frame::Inertial>>(gh_derivs);
+  const auto& d_phi =
+      get<Tags::deriv<Phi, tmpl::size_t<3>, Frame::Inertial>>(gh_derivs);
+  const auto& d_gauge_function =
+      get<Tags::deriv<GaugeH, tmpl::size_t<3>, Frame::Inertial>>(gh_derivs);
+
+  // Compute the three-index constraint
+  const auto& three_index_constraint =
+      GeneralizedHarmonic::three_index_constraint(d_spacetime_metric, phi);
+
+  // Get the constraint, and check that it vanishes to error_tolerance
+  auto two_index_constraint =
+      make_with_value<tnsr::ia<DataVector, 3, Frame::Inertial>>(
+          x, std::numeric_limits<double>::signaling_NaN());
+  GeneralizedHarmonic::two_index_constraint(
+      make_not_null(&two_index_constraint), d_gauge_function, normal_one_form,
+      normal_vector, inverse_spatial_metric, inverse_spacetime_metric, pi, phi,
+      d_pi, d_phi, gamma2, three_index_constraint);
+
+  Approx numerical_approx =
+      Approx::custom().epsilon(error_tolerance).scale(1.0);
+  CHECK_ITERABLE_CUSTOM_APPROX(
+      two_index_constraint,
+      make_with_value<decltype(two_index_constraint)>(x, 0.0),
+      numerical_approx);
 }
 }  // namespace
 
@@ -216,5 +363,56 @@ SPECTRE_TEST_CASE("Unit.Evolution.Systems.GeneralizedHarmonic.GaugeConstraint",
   test_gauge_constraint_random<3, Frame::Grid, double>(
       std::numeric_limits<double>::signaling_NaN());
   test_gauge_constraint_random<3, Frame::Inertial, double>(
+      std::numeric_limits<double>::signaling_NaN());
+}
+
+SPECTRE_TEST_CASE(
+    "Unit.Evolution.Systems.GeneralizedHarmonic.TwoIndexConstraint",
+    "[Unit][Evolution]") {
+  pypp::SetupLocalPythonEnvironment local_python_env{
+      "Evolution/Systems/GeneralizedHarmonic/"};
+
+  // Test the two-index constraint against Kerr Schild
+  const double mass = 1.4;
+  const std::array<double, 3> dimensionless_spin{{0.4, 0.3, 0.2}};
+  const std::array<double, 3> center{{0.2, 0.3, 0.4}};
+  const gr::Solutions::KerrSchild solution(mass, dimensionless_spin, center);
+
+  const size_t grid_size = 8;
+  const std::array<double, 3> upper_bound{{0.82, 1.24, 1.32}};
+  const std::array<double, 3> lower_bound{{0.8, 1.22, 1.30}};
+
+  // Note: looser numerical tolerance because this check
+  // uses numerical derivatives
+  test_two_index_constraint_analytic(
+      solution, grid_size, lower_bound, upper_bound,
+      std::numeric_limits<double>::epsilon() * 1.e6);
+
+  // Test the two-index constraint with random numbers
+  test_two_index_constraint_random<1, Frame::Grid, DataVector>(
+      DataVector(4, std::numeric_limits<double>::signaling_NaN()));
+  test_two_index_constraint_random<1, Frame::Inertial, DataVector>(
+      DataVector(4, std::numeric_limits<double>::signaling_NaN()));
+  test_two_index_constraint_random<1, Frame::Grid, double>(
+      std::numeric_limits<double>::signaling_NaN());
+  test_two_index_constraint_random<1, Frame::Inertial, double>(
+      std::numeric_limits<double>::signaling_NaN());
+
+  test_two_index_constraint_random<2, Frame::Grid, DataVector>(
+      DataVector(4, std::numeric_limits<double>::signaling_NaN()));
+  test_two_index_constraint_random<2, Frame::Inertial, DataVector>(
+      DataVector(4, std::numeric_limits<double>::signaling_NaN()));
+  test_two_index_constraint_random<2, Frame::Grid, double>(
+      std::numeric_limits<double>::signaling_NaN());
+  test_two_index_constraint_random<2, Frame::Inertial, double>(
+      std::numeric_limits<double>::signaling_NaN());
+
+  test_two_index_constraint_random<3, Frame::Grid, DataVector>(
+      DataVector(4, std::numeric_limits<double>::signaling_NaN()));
+  test_two_index_constraint_random<3, Frame::Inertial, DataVector>(
+      DataVector(4, std::numeric_limits<double>::signaling_NaN()));
+  test_two_index_constraint_random<3, Frame::Grid, double>(
+      std::numeric_limits<double>::signaling_NaN());
+  test_two_index_constraint_random<3, Frame::Inertial, double>(
       std::numeric_limits<double>::signaling_NaN());
 }
