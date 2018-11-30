4f4418ba054a1aba551c3e0a5e7cf871d7c5d624
Isaac Legred <inl7@cornell.edu>
2018-11-30 10:05:02 -0800

    Add Matrix Bounds Check

--- src/PythonBindings/BoundChecks.hpp
+++ src/PythonBindings/BoundChecks.hpp
@@ -23,4 +23,20 @@ void bounds_check(const T& t, const size_t i) {
                              " of size " + std::to_string(t.size())};
   }
 }
+/*!
+ * \ingroup PythonBindingsGroup
+ * \brief Check if a matrix-like object access is in bounds. Throws
+ * std::runtime_error if it is not.
+ */
+template <typename T>
+void matrix_bounds_check(const T& matrix, const size_t row,
+                         const size_t column) {
+  if (row >= matrix.rows() or column >= matrix.columns()) {
+    throw std::runtime_error{"Out of bounds access (" + std::to_string(row) +
+                             ", " + std::to_string(column) +
+                             ") into Matrix of size (" +
+                             std::to_string(matrix.rows()) + ", " +
+                             std::to_string(matrix.columns()) + ")"};
+  }
+}
 }  // namespace py_bindings
