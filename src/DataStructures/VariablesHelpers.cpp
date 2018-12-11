c1424e58a4f47b11f344ee2cb22c85ed7ce855fe
Francois Hebert <fmh33@cornell.edu>
2018-12-11 00:08:34 -0800

    Factor orient_variables_on_slice

--- src/DataStructures/VariablesHelpers.cpp
+++ src/DataStructures/VariablesHelpers.cpp
@@ -258,116 +258,39 @@ std::vector<size_t> oriented_offset(
       neighbor_third_axis_is_aligned, neighbor_axis_permutation);
 }
 
-}  // namespace OrientVariables_detail
-
-namespace OrientVariablesOnSlice_detail {
-
-std::vector<size_t> oriented_offset(
+std::vector<size_t> oriented_offset_on_slice(
     const Index<1>& slice_extents, const size_t sliced_dim,
     const OrientationMap<2>& orientation_of_neighbor) noexcept {
-  // The slice of a 2D mesh is either aligned or anti-aligned
   const Direction<2> my_slice_axis =
       (0 == sliced_dim ? Direction<2>::upper_eta() : Direction<2>::upper_xi());
   const Direction<2> neighbor_slice_axis =
       orientation_of_neighbor(my_slice_axis);
-  std::vector<size_t> oriented_offsets(slice_extents.product());
-  std::iota(oriented_offsets.begin(), oriented_offsets.end(), 0);
-
-  if (neighbor_slice_axis.side() == Side::Lower) {
-    std::reverse(oriented_offsets.begin(), oriented_offsets.end());
-  }
-  return oriented_offsets;
+  const bool is_aligned = (neighbor_slice_axis.side() == Side::Upper);
+  return compute_offset_permutation(slice_extents, is_aligned);
 }
 
-// The slice of a 3D mesh can have eight different data-storage orders
-// depending upon the relative orientation of the neighbor.  These are
-// determined by whether the new data-storage order varies fastest by the
-// lowest dim or the highest dim, and by whether each axis is aligned or
-// anti-aligned.
-std::vector<size_t> oriented_offset(
+std::vector<size_t> oriented_offset_on_slice(
     const Index<2>& slice_extents, const size_t sliced_dim,
     const OrientationMap<3>& orientation_of_neighbor) noexcept {
   const std::array<size_t, 2> dims_of_slice =
       (0 == sliced_dim ? make_array(1_st, 2_st)
                        : (1 == sliced_dim) ? make_array(0_st, 2_st)
                                            : make_array(0_st, 1_st));
-  const bool transpose_needed = (orientation_of_neighbor(dims_of_slice[0]) >
-                                 orientation_of_neighbor(dims_of_slice[1]));
+  const bool neighbor_axes_are_transposed =
+      (orientation_of_neighbor(dims_of_slice[0]) >
+       orientation_of_neighbor(dims_of_slice[1]));
   const Direction<3> neighbor_first_axis =
       orientation_of_neighbor(Direction<3>(dims_of_slice[0], Side::Upper));
   const Direction<3> neighbor_second_axis =
       orientation_of_neighbor(Direction<3>(dims_of_slice[1], Side::Upper));
-  const bool neighbor_first_axis_flipped =
-      (Side::Lower == neighbor_first_axis.side());
-  const bool neighbor_second_axis_flipped =
-      (Side::Lower == neighbor_second_axis.side());
+  const bool neighbor_first_axis_is_aligned =
+      (Side::Upper == neighbor_first_axis.side());
+  const bool neighbor_second_axis_is_aligned =
+      (Side::Upper == neighbor_second_axis.side());
 
-  std::vector<size_t> oriented_offsets(slice_extents.product());
-  const size_t num_pts_1 = slice_extents[0];
-  const size_t num_pts_2 = slice_extents[1];
-  if (transpose_needed) {
-    if (neighbor_first_axis_flipped) {
-      if (neighbor_second_axis_flipped) {
-        for (size_t i2 = 0; i2 < num_pts_2; ++i2) {
-          for (size_t i1 = 0; i1 < num_pts_1; ++i1) {
-            oriented_offsets[i1 + num_pts_1 * i2] =
-                (num_pts_2 - 1 - i2) + num_pts_2 * (num_pts_1 - 1 - i1);
-          }
-        }
-      } else {
-        for (size_t i2 = 0; i2 < num_pts_2; ++i2) {
-          for (size_t i1 = 0; i1 < num_pts_1; ++i1) {
-            oriented_offsets[i1 + num_pts_1 * i2] =
-                i2 + num_pts_2 * (num_pts_1 - 1 - i1);
-          }
-        }
-      }
-    } else {
-      if (neighbor_second_axis_flipped) {
-        for (size_t i2 = 0; i2 < num_pts_2; ++i2) {
-          for (size_t i1 = 0; i1 < num_pts_1; ++i1) {
-            oriented_offsets[i1 + num_pts_1 * i2] =
-                (num_pts_2 - 1 - i2) + num_pts_2 * i1;
-          }
-        }
-      } else {
-        for (size_t i2 = 0; i2 < num_pts_2; ++i2) {
-          for (size_t i1 = 0; i1 < num_pts_1; ++i1) {
-            oriented_offsets[i1 + num_pts_1 * i2] = i2 + num_pts_2 * i1;
-          }
-        }
-      }
-    }
-  } else {
-    if (neighbor_first_axis_flipped) {
-      if (neighbor_second_axis_flipped) {
-        for (size_t i2 = 0; i2 < num_pts_2; ++i2) {
-          for (size_t i1 = 0; i1 < num_pts_1; ++i1) {
-            oriented_offsets[i1 + num_pts_1 * i2] =
-                (num_pts_1 - 1 - i1) + num_pts_1 * (num_pts_2 - 1 - i2);
-          }
-        }
-      } else {
-        for (size_t i2 = 0; i2 < num_pts_2; ++i2) {
-          for (size_t i1 = 0; i1 < num_pts_1; ++i1) {
-            oriented_offsets[i1 + num_pts_1 * i2] =
-                (num_pts_1 - 1 - i1) + num_pts_1 * i2;
-          }
-        }
-      }
-    } else {
-      if (neighbor_second_axis_flipped) {
-        for (size_t i2 = 0; i2 < num_pts_2; ++i2) {
-          for (size_t i1 = 0; i1 < num_pts_1; ++i1) {
-            oriented_offsets[i1 + num_pts_1 * i2] =
-                i1 + num_pts_1 * (num_pts_2 - 1 - i2);
-          }
-        }
-      } else {
-        std::iota(oriented_offsets.begin(), oriented_offsets.end(), 0);
-      }
-    }
-  }
-  return oriented_offsets;
+  return compute_offset_permutation(
+      slice_extents, neighbor_first_axis_is_aligned,
+      neighbor_second_axis_is_aligned, neighbor_axes_are_transposed);
 }
-}  // namespace OrientVariablesOnSlice_detail
+
+}  // namespace OrientVariables_detail
--- src/DataStructures/VariablesHelpers.hpp
+++ src/DataStructures/VariablesHelpers.hpp
@@ -211,25 +211,22 @@ std::vector<size_t> oriented_offset(
     const Index<VolumeDim>& extents,
     const OrientationMap<VolumeDim>& orientation_of_neighbor) noexcept;
 
-}  // namespace OrientVariables_detail
-
-namespace OrientVariablesOnSlice_detail {
-
-inline std::vector<size_t> oriented_offset(
+inline std::vector<size_t> oriented_offset_on_slice(
     const Index<0>& /*slice_extents*/, const size_t /*sliced_dim*/,
     const OrientationMap<1>& /*orientation_of_neighbor*/) noexcept {
   // There is only one point on a slice of a 1D mesh
   return {0};
 }
 
-std::vector<size_t> oriented_offset(
+std::vector<size_t> oriented_offset_on_slice(
     const Index<1>& slice_extents, size_t sliced_dim,
     const OrientationMap<2>& orientation_of_neighbor) noexcept;
 
-std::vector<size_t> oriented_offset(
+std::vector<size_t> oriented_offset_on_slice(
     const Index<2>& slice_extents, size_t sliced_dim,
     const OrientationMap<3>& orientation_of_neighbor) noexcept;
-}  // namespace OrientVariablesOnSlice_detail
+
+}  // namespace OrientVariables_detail
 
 /// \ingroup DataStructuresGroup
 /// Orient variables to the data-storage order of a neighbor element with
@@ -266,31 +263,24 @@ Variables<TagsList> orient_variables_on_slice(
     const Variables<TagsList>& variables_on_slice,
     const Index<VolumeDim - 1>& slice_extents, const size_t sliced_dim,
     const OrientationMap<VolumeDim>& orientation_of_neighbor) noexcept {
+  // Skip work (aside from a copy) if neighbor slice is aligned
+  if (orientation_of_neighbor.is_aligned()) {
+    return variables_on_slice;
+  }
+
   const size_t number_of_grid_points = slice_extents.product();
-
+  ASSERT(variables_on_slice.number_of_grid_points() == number_of_grid_points,
+         "Inconsistent `variables_on_slice` and `slice_extents`:\n"
+         "  variables_on_slice.number_of_grid_points() = "
+             << variables_on_slice.number_of_grid_points()
+             << "\n"
+                "  slice_extents.product() = "
+             << slice_extents.product());
   Variables<TagsList> oriented_variables(number_of_grid_points);
-  const auto oriented_offset = OrientVariablesOnSlice_detail::oriented_offset(
+  const auto oriented_offset = OrientVariables_detail::oriented_offset_on_slice(
       slice_extents, sliced_dim, orientation_of_neighbor);
-
-  tmpl::for_each<TagsList>([&oriented_variables, &variables_on_slice,
-                            &oriented_offset,
-                            &number_of_grid_points](auto tag) {
-    using Tag = tmpl::type_from<decltype(tag)>;
-    auto& oriented_tensor = get<Tag>(oriented_variables);
-    const auto& tensor_on_slice = get<Tag>(variables_on_slice);
-    using vector_type = typename Variables<TagsList>::vector_type;
-    for (decltype(auto) oriented_and_slice_tensor_components :
-         boost::combine(oriented_tensor, tensor_on_slice)) {
-      vector_type& oriented_tensor_component =
-          boost::get<0>(oriented_and_slice_tensor_components);
-      const vector_type& tensor_component_on_slice =
-          boost::get<1>(oriented_and_slice_tensor_components);
-      for (size_t s = 0; s < number_of_grid_points; ++s) {
-        oriented_tensor_component[oriented_offset[s]] =
-            tensor_component_on_slice[s];
-      }
-    }
-  });
+  OrientVariables_detail::orient_each_component(
+      make_not_null(&oriented_variables), variables_on_slice, oriented_offset);
 
   return oriented_variables;
 }
