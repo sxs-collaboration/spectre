1d1bc84c8ffebe0395a5376a9f061b96796efdb4
Francois Hebert <fmh33@cornell.edu>
2019-01-08 00:07:56 -0800

    Do not limit div-clean field in ValenciaDivClean executables

--- src/Evolution/Executables/GrMhd/ValenciaDivClean/EvolveValenciaDivClean.hpp
+++ src/Evolution/Executables/GrMhd/ValenciaDivClean/EvolveValenciaDivClean.hpp
@@ -90,8 +90,12 @@ struct EvolutionMetavars {
       typename analytic_solution_tag::type::equation_of_state_type>;
   using normal_dot_numerical_flux = OptionTags::NumericalFluxParams<
       dg::NumericalFluxes::LocalLaxFriedrichs<system>>;
-  using limiter = OptionTags::SlopeLimiterParams<
-      SlopeLimiters::Minmod<3, system::variables_tag::tags_list>>;
+  // Do not limit the divergence-cleaning field Phi
+  using limiter = OptionTags::SlopeLimiterParams<SlopeLimiters::Minmod<
+      3, tmpl::list<grmhd::ValenciaDivClean::Tags::TildeD,
+                    grmhd::ValenciaDivClean::Tags::TildeTau,
+                    grmhd::ValenciaDivClean::Tags::TildeS<Frame::Inertial>,
+                    grmhd::ValenciaDivClean::Tags::TildeB<Frame::Inertial>>>>;
   using step_choosers =
       tmpl::list<StepChoosers::Registrars::Cfl<3, Frame::Inertial>,
                  StepChoosers::Registrars::Constant,
