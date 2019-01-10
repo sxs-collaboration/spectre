602166e242363f6753b946a2e2aaf0279bd31315
Nils Deppe <nd357@cornell.edu>
2019-01-09 21:37:29 -0500

    Separate out global executable config

--- cmake/SetupPch.cmake
+++ cmake/SetupPch.cmake
@@ -179,26 +179,4 @@ if (USE_PCH)
         )
     endif()
   endfunction()
-
-  function(add_spectre_executable TARGET_NAME)
-    _add_spectre_executable(${TARGET_NAME} ${ARGN})
-    add_dependencies(${TARGET_NAME} pch)
-    set_source_files_properties(
-      ${ARGN}
-      OBJECT_DEPENDS "${PCH_PATH};${PCH_PATH}.gch"
-      )
-    get_target_property(
-      TARGET_IS_IMPORTED
-      ${TARGET_NAME}
-      IMPORTED
-      )
-    if(NOT ${TARGET_IS_IMPORTED})
-      target_compile_options(
-        ${TARGET_NAME}
-        PRIVATE
-        $<$<COMPILE_LANGUAGE:CXX>:${PCH_FLAG}>
-        )
-    endif()
-  endfunction()
-
 endif (USE_PCH)
