c83fa843f4a5c20c814ad588ed80a2776c1a7ff6
Jordan Moxon <jem497@cornell.edu>
2019-01-24 17:33:05 -0800

    Add is_derived_of_vector_impl
    - type trait for working out if an object is a SpECTRE vector

--- tests/Unit/DataStructures/Test_VectorImpl.cpp
+++ tests/Unit/DataStructures/Test_VectorImpl.cpp
@@ -3,7 +3,9 @@
 
 #include <array>
 #include <complex>
+#include <vector>
 
+#include "DataStructures/ComplexDataVector.hpp"  // IWYU pragma: keep
 #include "DataStructures/DataVector.hpp"  // IWYU pragma: keep
 #include "DataStructures/VectorImpl.hpp"  // IWYU pragma: keep
 #include "Utilities/TypeTraits.hpp"
@@ -23,3 +25,12 @@ static_assert(cpp17::is_same_v<get_vector_element_type_t<std::complex<double>*>,
 static_assert(cpp17::is_same_v<get_vector_element_type_t<DataVector&>, double>,
               "Failed testing type trait get_vector_element_type");
 /// [get_vector_element_type_example]
+
+static_assert(is_derived_of_vector_impl_v<DataVector>,
+              "Failed testing type trait is_derived_of_vector_impl");
+static_assert(is_derived_of_vector_impl_v<ComplexDataVector>,
+              "Failed testing type trait is_derived_of_vector_impl");
+static_assert(not is_derived_of_vector_impl_v<std::complex<double>>,
+              "Failed testing type trait is_derived_of_vector_impl");
+static_assert(not is_derived_of_vector_impl_v<std::vector<int>>,
+              "Failed testing type trait is_derived_of_vector_impl");
