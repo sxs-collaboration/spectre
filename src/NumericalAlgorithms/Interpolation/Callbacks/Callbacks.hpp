a735f2e9093c810ecbe15bdc563cf3af2243f912
Mark Scheel <scheel@tapir.caltech.edu>
2019-01-30 12:58:15 -0800

    Add Callbacks.hpp to document the intrp::callbacks namespace.

---
+++ src/NumericalAlgorithms/Interpolation/Callbacks/Callbacks.hpp
@@ -0,0 +1,12 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace intrp {
+/*!
+ * \ingroup NumericalAlgorithmsGroup
+ * \brief Contains callback functions called by `InterpolationTarget`s.
+ */
+namespace callbacks;
+}  // namespace intrp
