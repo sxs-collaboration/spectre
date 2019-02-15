dc4c38174dc3925f1b9cceebd220e2c3117b1783
Nils Deppe <nd357@cornell.edu>
2019-02-15 14:40:19 -0500

    Format cmake/SetupBuildInfo.cmake

--- cmake/SetupBuildInfo.cmake
+++ cmake/SetupBuildInfo.cmake
@@ -4,18 +4,18 @@
 find_package(Git REQUIRED)
 
 configure_file(
-    ${CMAKE_SOURCE_DIR}/src/Informer/InfoFromBuild.cpp
-    ${CMAKE_BINARY_DIR}/Informer/InfoFromBuild.cpp
-)
+  ${CMAKE_SOURCE_DIR}/src/Informer/InfoFromBuild.cpp
+  ${CMAKE_BINARY_DIR}/Informer/InfoFromBuild.cpp
+  )
 
 configure_file(
-    ${CMAKE_SOURCE_DIR}/tools/WrapLinker.sh
-    ${CMAKE_BINARY_DIR}/WrapLinker.sh
-    @ONLY
-)
+  ${CMAKE_SOURCE_DIR}/tools/WrapLinker.sh
+  ${CMAKE_BINARY_DIR}/WrapLinker.sh
+  @ONLY
+  )
 
 string(
-    REGEX REPLACE "<CMAKE_CXX_COMPILER>"
-    "${CMAKE_BINARY_DIR}/WrapLinker.sh <CMAKE_CXX_COMPILER>"
-    CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE}"
-)
+  REGEX REPLACE "<CMAKE_CXX_COMPILER>"
+  "${CMAKE_BINARY_DIR}/WrapLinker.sh <CMAKE_CXX_COMPILER>"
+  CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE}"
+  )
