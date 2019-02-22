0a4cbec8a07b034113cbc495f023801f707f0cbe
Nils Deppe <nd357@cornell.edu>
2019-02-22 15:57:01 -0500

    Allow larger file sizes

--- tools/Hooks/CheckFileSize.py
+++ tools/Hooks/CheckFileSize.py
@@ -8,10 +8,11 @@ import sys
 import os
 
 # The maximum file-size for a file to be committed in kB
-max_file_size = 130.0
+max_file_size = 200.0
 
 # The path to the git-binary:
 git_executable = "@GIT_EXECUTABLE@"
+
 
 def sizeof_fmt(num):
     """
@@ -19,10 +20,11 @@ def sizeof_fmt(num):
     like "3.5 MB" for its given 'num'-parameter.
     From http://stackoverflow.com/questions/1094841
     """
-    for x in ['bytes','KB','MB','GB','TB']:
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
         num /= 1024.0
+
 
 # Check all files in the staging-area:
 text = subprocess.check_output(
@@ -35,9 +37,10 @@ for file_s in file_list:
     if not os.path.isfile(file_s[3:]):
         continue
     stat = os.stat(file_s[3:])
-    if stat.st_size > (max_file_size*1024):
-        print("File '"+file_s[3:]+"' is too large to be committed. The file "
-                                  "is %s and the limit is %s" %
+    if stat.st_size > (max_file_size * 1024):
+        print("File '" + file_s[3:] +
+              "' is too large to be committed. The file "
+              "is %s and the limit is %s" %
               (sizeof_fmt(stat.st_size), sizeof_fmt(max_file_size * 1024)))
         sys.exit(1)
 
