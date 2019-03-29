5f7b56c0e20cff8459068566791a9bb99500b30f
Isaac Legred <inl7@cornell.edu>
2019-03-29 00:13:38 -0700

    Add Vector to Python List and Python List to Vector functions

--- src/PythonBindings/VectorPyList.hpp
+++ src/PythonBindings/VectorPyList.hpp
@@ -11,13 +11,28 @@
 
 namespace bp = boost::python;
 
+/*!
+ * \ingroup PythonBindingsGroup
+ * \brief A namespace containing functions for binding Python wrappers
+ * to their respective c++ classes and functions, as well as helper functions
+ * for these wrappers.
+ */
 namespace py_bindings {
+
+/*!
+ * \ingroup PythonBindingsGroup
+ * \brief Convert a bp::list of bp::object& to a std::vector
+ */
 template <typename T>
 std::vector<T> py_list_to_std_vector(const bp::object& iterable) {
   return std::vector<T>(bp::stl_input_iterator<T>(iterable),
                         bp::stl_input_iterator<T>());
 }
 
+/*!
+ * \ingroup PythonBindingsGroup
+ * \brief Convert a std::vector into a bp::list of bp::objects
+ */
 template <class T>
 bp::list std_vector_to_py_list(const std::vector<T>& vector) {
   bp::list list;
