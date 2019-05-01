2f920118b8c0f79c1f6f11855f327380b613b2e7
Lawrence Kidder <kidder@astro.cornell.edu>
2019-05-01 13:20:10 -0400

    Ignore warning for AppleClang
    The self-assign-overloaded warning is now recognized by AppleClang, so
    it can be now appear in a diagnostic ignored pragma

--- tests/Unit/NumericalAlgorithms/Spectral/Test_ComplexDataView.cpp
+++ tests/Unit/NumericalAlgorithms/Spectral/Test_ComplexDataView.cpp
@@ -142,18 +142,14 @@ void test_view() noexcept {
   }
 
   // check self-assignment from the referenced vector
-#ifndef __APPLE__
 #if defined(__clang__) && __clang_major__ > 6
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wself-assign-overloaded"
 #endif  // defined(__clang__) && __clang_major__ > 6
-#endif  // ! __APPLE__
   assign_vec_1 = assign_vec_1;
-#ifndef __APPLE__
 #if defined(__clang__) && __clang_major__ > 6
 #pragma GCC diagnostic pop
 #endif  // defined(__clang__) && __clang_major__ > 6
-#endif  // ! __APPLE__
   for (size_t i = 0; i < view_size; i++) {
     CHECK(real(assign_vec_1[i]) == real(source_vec[i + offset]));
     CHECK(imag(assign_vec_1[i]) == imag(assign_vec_1[i]));
@@ -180,22 +176,18 @@ void test_view() noexcept {
   }
 
   // check self-assignment from the view
-#ifndef __APPLE__
 #if defined(__clang__) && __clang_major__ > 6
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wself-assign-overloaded"
 #endif  // defined(__clang__) && __clang_major__ > 6
-#endif  // ! __APPLE__
   // clang-tidy and gcc ignore for allowing the intentional self-assignment
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wextra"
   vector_view = vector_view; // NOLINT
 #pragma GCC diagnostic pop
-#ifndef __APPLE__
 #if defined(__clang__) && __clang_major__ > 6
 #pragma GCC diagnostic pop
 #endif  // defined(__clang__) && __clang_major__ > 6
-#endif  // ! __APPLE__
   for (size_t i = 0; i < view_size; i++) {
     CHECK(source_vec[i + offset] == assign_view_source[i]);
   }
