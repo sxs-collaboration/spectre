02a748e42ed1fbef2b928b70bf41e64f75325581
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-05-14 17:45:58 +0200

    Make the evolution system appear in the input file

--- src/Evolution/Tags.hpp
+++ src/Evolution/Tags.hpp
@@ -19,4 +19,16 @@ struct EvolutionGroup {
   static constexpr OptionString help{"Options for the time evolution"};
 };
 
+/*!
+ * \ingroup OptionGroupsGroup
+ * \brief Groups option tags related to the evolution system.
+ *
+ * The option tags for the evolution system should be placed in a subgroup that
+ * carries the system name. See e.g. `OptionTags::ValenciaDivCleanGroup`.
+ */
+struct EvolutionSystemGroup {
+  static std::string name() noexcept { return "EvolutionSystem"; }
+  static constexpr OptionString help{"The system of hyperbolic PDEs"};
+};
+
 }  // namespace OptionTags
