ae7cd9590617d6ea1b9ea4dd40000303a85c37a9
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-05-14 17:26:36 +0200

    Make limiter type appear in input file

--- src/Evolution/DiscontinuousGalerkin/SlopeLimiters/Tags.hpp
+++ src/Evolution/DiscontinuousGalerkin/SlopeLimiters/Tags.hpp
@@ -7,13 +7,24 @@
 
 namespace OptionTags {
 /*!
+ * \ingroup OptionGroupsGroup
+ * \brief Holds the `OptionTags::SlopeLimiter` option in the input file
+ */
+struct SlopeLimiterGroup {
+  static std::string name() noexcept { return "SlopeLimiter"; }
+  static constexpr OptionString help = "Options for limiting troubled cells";
+};
+
+/*!
  * \ingroup OptionTagsGroup
  * \brief The global cache tag that retrieves the parameters for the slope
  * limiter from the input file
  */
 template <typename SlopeLimiterType>
 struct SlopeLimiter {
+  static std::string name() noexcept { return option_name<SlopeLimiterType>(); }
   static constexpr OptionString help = "Options for the slope limiter";
   using type = SlopeLimiterType;
+  using group = SlopeLimiterGroup;
 };
 }  // namespace OptionTags
