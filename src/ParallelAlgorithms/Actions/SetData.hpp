bd7e82bcaa1be6702d436474fc6c1661945f7a7e
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-05-16 15:26:06 +0200

    Add SetData simple action
    Takes a tagged tuple and mutates the DataBox with its content

---
+++ src/ParallelAlgorithms/Actions/SetData.hpp
@@ -0,0 +1,58 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/DataBoxTag.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+/// \cond
+namespace Parallel {
+template <typename Metavariables>
+struct ConstGlobalCache;
+}  // namespace Parallel
+/// \endcond
+
+namespace Actions {
+
+/*!
+ * \ingroup ActionsGroup
+ * \brief Mutate the DataBox tags in `TagsList` according to the `data`.
+ *
+ * An example use case for this action is as the callback for the
+ * `importers::ThreadedActions::ReadVolumeData`.
+ *
+ * DataBox changes:
+ * - Modifies:
+ *   - All tags in `TagsList`
+ */
+template <typename TagsList>
+struct SetData;
+
+/// \cond
+template <typename... Tags>
+struct SetData<tmpl::list<Tags...>> {
+  template <
+      typename ParallelComponent, typename DataBox, typename Metavariables,
+      typename ArrayIndex,
+      Requires<cpp17::conjunction_v<db::tag_is_retrievable<Tags, DataBox>...>> =
+          nullptr>
+  static void apply(DataBox& box,
+                    const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    tuples::TaggedTuple<Tags...> data) noexcept {
+    tmpl::for_each<tmpl::list<Tags...>>([&box, &data](auto tag_v) noexcept {
+      using tag = tmpl::type_from<decltype(tag_v)>;
+      db::mutate<tag>(
+          make_not_null(&box),
+          [&data](const gsl::not_null<db::item_type<tag>*> value) noexcept {
+            *value = std::move(tuples::get<tag>(data));
+          });
+    });
+  }
+};
+/// \endcond
+
+}  // namespace Actions
