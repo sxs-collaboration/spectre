295f08aa1bb0251eabf57a463e383068aaaa2146
Francois Hebert <fmh33@cornell.edu>
2019-06-14 15:48:24 -0700

    Use test_enum_creation to test WenoType

--- tests/Unit/Evolution/DiscontinuousGalerkin/SlopeLimiters/Test_WenoType.cpp
+++ tests/Unit/Evolution/DiscontinuousGalerkin/SlopeLimiters/Test_WenoType.cpp
@@ -6,29 +6,15 @@
 #include <string>
 
 #include "Evolution/DiscontinuousGalerkin/SlopeLimiters/WenoType.hpp"
-#include "Options/Options.hpp"
-#include "Options/ParseOptions.hpp"
 #include "Utilities/GetOutput.hpp"
-#include "Utilities/TMPL.hpp"
-
-namespace {
-struct ExampleLimiterType {
-  using type = SlopeLimiters::WenoType;
-  static constexpr OptionString help = {"Example help text"};
-};
-
-void check_weno_type_parse(const std::string& input,
-                           const SlopeLimiters::WenoType& expected_weno_type) {
-  Options<tmpl::list<ExampleLimiterType>> opts("");
-  opts.parse("ExampleLimiterType: " + input);
-  CHECK(opts.get<ExampleLimiterType>() == expected_weno_type);
-}
-}  // namespace
+#include "tests/Unit/TestCreation.hpp"
 
 SPECTRE_TEST_CASE("Unit.Evolution.DG.SlopeLimiters.WenoType",
                   "[SlopeLimiters][Unit]") {
-  check_weno_type_parse("Hweno", SlopeLimiters::WenoType::Hweno);
-  check_weno_type_parse("SimpleWeno", SlopeLimiters::WenoType::SimpleWeno);
+  CHECK(SlopeLimiters::WenoType::Hweno ==
+        test_enum_creation<SlopeLimiters::WenoType>("Hweno"));
+  CHECK(SlopeLimiters::WenoType::SimpleWeno ==
+        test_enum_creation<SlopeLimiters::WenoType>("SimpleWeno"));
 
   CHECK(get_output(SlopeLimiters::WenoType::Hweno) == "Hweno");
   CHECK(get_output(SlopeLimiters::WenoType::SimpleWeno) == "SimpleWeno");
@@ -38,7 +24,5 @@ SPECTRE_TEST_CASE("Unit.Evolution.DG.SlopeLimiters.WenoType",
 SPECTRE_TEST_CASE("Unit.Evolution.DG.SlopeLimiters.WenoType.OptionParseError",
                   "[SlopeLimiters][Unit]") {
   ERROR_TEST();
-  Options<tmpl::list<ExampleLimiterType>> opts("");
-  opts.parse("ExampleLimiterType: BadType");
-  opts.get<ExampleLimiterType>();
+  test_enum_creation<SlopeLimiters::WenoType>("BadType");
 }
