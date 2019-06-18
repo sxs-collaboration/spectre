32b01b836d2c3a74121649bee5799749e58dd4da
Isaac Legred <inl7@cornell.edu>
2019-06-18 11:04:45 -0700

    Add contiguous VolumeData class

--- src/IO/H5/VolumeData.hpp
+++ src/IO/H5/VolumeData.hpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <hdf5.h>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "IO/H5/Object.hpp"
@@ -25,7 +26,7 @@ namespace h5 {
  * The volume data inside the subfile can be of any dimensionality greater than
  * zero. This means that in a 3D simulation, data on 2-dimensional surfaces are
  * written as a VolumeData subfile. Data can be written using the
- * `insert_tensor_data()` method. An integral observation id is used to keep
+ * `write_volume_data()` method. An integral observation id is used to keep
  * track of the observation instance at which the data is written, and
  * associated with it is a floating point observation value, such as the
  * simulation time at which the data was written. The observation id will
@@ -33,13 +34,25 @@ namespace h5 {
  * simulation.
  *
  * The data stored in the subfile are the tensor components passed to the
- * `insert_tensor_data()` method as a `ExtentsAndTensorVolumeData`. The name of
- * each tensor component must follow the format
+ * `write_volume_data()` method as a  `std::vector<ExtentsAndTensorVolumeData>`.
+ * The name of each tensor component must follow the format
  * `GRID_NAME/TENSOR_NAME_COMPONENT`, e.g. `Element0/T_xx`. Typically the
  * `GRID_NAME` should be the output of the stream operator of the spatial ID of
  * the parallel component element sending the data to be observed. For example,
  * in the case of a dG evolution where the spatial IDs are `ElementId`s, the
- * grid names would be of the form `[B0,(L2I3,L2I3,L2I3)]`.
+ * grid names would be of the form `[B0,(L2I3,L2I3,L2I3)]`.  The data are
+ * written contiguously inside of the H5 subfile, in that each tensor
+ * component has a single dataset which holds all of the data from
+ * all elements, e.g. a tensor component `T_xx` which is found on all grids
+ * appears in the path `H5_FILE_NAME/element_data.vol/T_xx` and that is where
+ * all of the `T_xx` data from all of the grids resides.  Note that coordinates
+ * must be written as tensors in order to visualize the data in ParaView,
+ * Visit, etc.   In order to reconstruct which data came from which grid,
+ * the `get_grid_names()`, and `get_extents()` methods list the grids
+ * and their extents in the order which they and the data were written.
+ * For example, if the first grid has name `GRID_NAME` with extents
+ * `{2, 2, 2}`, it was responsible for contributing the first 2*2*2 = 8 grid
+ * points worth of data in each tensor dataset.
  *
  * \warning Currently the topology of the grids is assumed to be tensor products
  * of lines, i.e. lines, quadrilaterals, and hexahedrons. However, this can be
@@ -50,7 +63,7 @@ class VolumeData : public h5::Object {
  public:
   static std::string extension() noexcept { return ".vol"; }
 
-  VolumeData(bool exists, detail::OpenGroup&& group, hid_t location,
+  VolumeData(bool subfile_exists, detail::OpenGroup&& group, hid_t location,
              const std::string& name, uint32_t version = 1) noexcept;
 
   VolumeData(const VolumeData& /*rhs*/) = delete;
@@ -78,9 +91,9 @@ class VolumeData : public h5::Object {
   ///
   /// \requires The names of the tensor components is of the form
   /// `GRID_NAME/TENSOR_NAME_COMPONENT`, e.g. `Element0/T_xx`
-  void insert_tensor_data(
+  void write_volume_data(
       size_t observation_id, double observation_value,
-      const ExtentsAndTensorVolumeData& extents_and_tensors) noexcept;
+      const std::vector<ExtentsAndTensorVolumeData>& elements) noexcept;
 
   /// List all the integral observation ids in the subfile
   std::vector<size_t> list_observation_ids() const noexcept;
@@ -89,26 +102,29 @@ class VolumeData : public h5::Object {
   /// subfile
   double get_observation_value(size_t observation_id) const noexcept;
 
-  /// List all the grid ids at the the integral observation id in the
-  /// subfile
-  std::vector<std::string> list_grids(size_t observation_id) const noexcept;
+  /// List all the tensor components at observation id `observation_id`
+  std::vector<std::string> list_tensor_components(size_t observation_id) const
+      noexcept;
 
-  /// List all the tensor components on the grid `grid_name` at observation id
-  /// `observation_id`
-  std::vector<std::string> list_tensor_components(
-      size_t observation_id, const std::string& grid_name) const noexcept;
+  /// List the names of all the grids at observation id `observation_id`
+  std::vector<std::string> get_grid_names(size_t observation_id) const noexcept;
 
-  /// Read a tensor component with name `tensor_component` from the grid
-  /// `grid_name` at the observation id `observation_id`
+  /// Read a tensor component with name `tensor_component` at observation id
+  /// `observation_id` from all grids in the file
   DataVector get_tensor_component(size_t observation_id,
-                                  const std::string& grid_name,
                                   const std::string& tensor_component) const
       noexcept;
 
-  /// Read the extents of the grid `grid_name` at the observation id
+  /// Read the extents of all the grids stored in the file at the observation id
   /// `observation_id`
-  std::vector<size_t> get_extents(size_t observation_id,
-                                  const std::string& grid_name) const noexcept;
+  std::vector<std::vector<size_t>> get_extents(size_t observation_id) const
+      noexcept;
+
+  /// Read the dimensionality of the grids.
+  size_t get_dimension() const noexcept;
+
+  /// Return the character used as a separator between grids in the subfile.
+  static char separator() noexcept { return ':'; }
 
  private:
   detail::OpenGroup group_{};
