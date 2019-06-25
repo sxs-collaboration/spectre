585e4abbeeaa8c666886d2bbb0aba13a3d550957
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-06-25 14:35:12 +0200

    Move MutateApply action and add test

---
+++ src/ParallelAlgorithms/Actions/MutateApply.hpp
@@ -0,0 +1,53 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <tuple>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+namespace tuples {
+template <typename...>
+class TaggedTuple;
+}  // namespace tuples
+
+namespace Parallel {
+template <typename Metavariables>
+class ConstGlobalCache;
+}  // namespace Parallel
+/// \endcond
+
+namespace Actions {
+/*!
+ * \ingroup ActionsGroup
+ * \brief Apply the function `Mutator::apply` to the DataBox
+ *
+ * The function `Mutator::apply` is invoked with the `Mutator::argument_tags`.
+ * The result of this computation is stored in the `Mutator::return_tags`.
+ *
+ * Uses:
+ * - DataBox:
+ *   - All elements in `Mutator::argument_tags`
+ *
+ * DataBox changes:
+ * - Modifies:
+ *   - All elements in `Mutator::return_tags`
+ */
+template <typename Mutator>
+struct MutateApply {
+  template <typename DataBox, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent>
+  static std::tuple<DataBox&&> apply(
+      DataBox& box, const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+      const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+      const ArrayIndex& /*array_index*/, const ActionList /*meta*/,
+      const ParallelComponent* const /*meta*/) noexcept {
+    db::mutate_apply<Mutator>(make_not_null(&box));
+    return {std::move(box)};
+  }
+};
+}  // namespace Actions
