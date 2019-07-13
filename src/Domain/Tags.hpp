409a91c5095c0a22d4ae4b58bb40d390f9d5b2e9
William Throwe <wtt6@cornell.edu>
2019-07-13 17:46:18 -0400

    Assert that InterfaceComputeItem argument is sane

--- src/Domain/Tags.hpp
+++ src/Domain/Tags.hpp
@@ -441,6 +441,8 @@ struct InterfaceComputeItem
     : Interface<DirectionsTag, Tag>,
       db::ComputeTag,
       virtual db::PrefixTag {
+  static_assert(db::is_compute_item_v<Tag>,
+                "Cannot use a non compute item as an interface compute item.");
   // Defining name here prevents an ambiguous function call when using base
   // tags; Both Interface<Dirs, Tag> and Interface<Dirs, Tag::base> will have a
   // name function and so cannot be disambiguated.
