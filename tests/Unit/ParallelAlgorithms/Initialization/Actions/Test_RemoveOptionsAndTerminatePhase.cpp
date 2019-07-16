8a6470b14b57faf36d5f24de45e0804e923d5e8d
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-07-16 23:01:28 +0900

    Remove dependence on Evolution in ParallelAlgorithms/Initialization test

--- tests/Unit/ParallelAlgorithms/Initialization/Actions/Test_RemoveOptionsAndTerminatePhase.cpp
+++ tests/Unit/ParallelAlgorithms/Initialization/Actions/Test_RemoveOptionsAndTerminatePhase.cpp
@@ -9,7 +9,6 @@
 
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/DataBoxTag.hpp"
-#include "Evolution/Initialization/Tags.hpp"
 #include "Parallel/Actions/TerminatePhase.hpp"
 #include "Parallel/AddOptionsToDataBox.hpp"
 #include "Parallel/ConstGlobalCache.hpp"
@@ -24,6 +23,11 @@
 namespace {
 struct TemporalId {};
 
+struct InitialTime : db::SimpleTag {
+  using type = double;
+  static std::string name() noexcept { return "InitialTime"; }
+};
+
 struct DummyTimeTag : db::SimpleTag {
   static std::string name() noexcept { return "DummyTime"; }
   using type = double;
@@ -37,32 +41,28 @@ struct TagMultiplyByTwo : db::ComputeTag {
 };
 
 struct Action0 {
-  using initialization_option_tags =
-      tmpl::list<Initialization::Tags::InitialTime>;
+  using initialization_option_tags = tmpl::list<InitialTime>;
 
   template <typename DbTagsList, typename... InboxTags, typename Metavariables,
             typename ArrayIndex, typename ActionList,
             typename ParallelComponent,
-            Requires<tmpl::list_contains_v<
-                DbTagsList, Initialization::Tags::InitialTime>> = nullptr>
+            Requires<tmpl::list_contains_v<DbTagsList, InitialTime>> = nullptr>
   static auto apply(db::DataBox<DbTagsList>& box,
                     const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
                     const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
                     const ArrayIndex& /*array_index*/, ActionList /*meta*/,
                     const ParallelComponent* const /*meta*/) noexcept {
-    const double initial_time_value =
-        db::get<Initialization::Tags::InitialTime>(box);
+    const double initial_time_value = db::get<InitialTime>(box);
     return std::make_tuple(
         Initialization::merge_into_databox<Action0,
                                            db::AddSimpleTags<DummyTimeTag>>(
             std::move(box), 3.0 * initial_time_value));
   }
 
-  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
-            typename ArrayIndex, typename ActionList,
-            typename ParallelComponent,
-            Requires<not tmpl::list_contains_v<
-                DbTagsList, Initialization::Tags::InitialTime>> = nullptr>
+  template <
+      typename DbTagsList, typename... InboxTags, typename Metavariables,
+      typename ArrayIndex, typename ActionList, typename ParallelComponent,
+      Requires<not tmpl::list_contains_v<DbTagsList, InitialTime>> = nullptr>
   static std::tuple<db::DataBox<DbTagsList>&&> apply(
       db::DataBox<DbTagsList>& box,
       const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
@@ -131,45 +131,36 @@ SPECTRE_TEST_CASE("Unit.Evolution.Initialization.RemoveOptionsFromDataBox",
   const double initial_time = 3.7;
   ActionTesting::emplace_component<component>(&runner, 0, initial_time);
   runner.set_phase(Metavariables::Phase::Initialization);
-  CHECK(ActionTesting::tag_is_retrievable<component,
-                                          Initialization::Tags::InitialTime>(
-      runner, 0));
+  CHECK(ActionTesting::tag_is_retrievable<component, InitialTime>(runner, 0));
   CHECK(not ActionTesting::tag_is_retrievable<component, DummyTimeTag>(runner,
                                                                        0));
   CHECK(not ActionTesting::tag_is_retrievable<component,
                                               TagMultiplyByTwo<DummyTimeTag>>(
       runner, 0));
-  CHECK(ActionTesting::get_databox_tag<component,
-                                       Initialization::Tags::InitialTime>(
-            runner, 0) == initial_time);
+  CHECK(ActionTesting::get_databox_tag<component, InitialTime>(runner, 0) ==
+        initial_time);
   CHECK_FALSE(ActionTesting::get_terminate<component>(runner, 0));
   // Runs Action0
   runner.next_action<component>(0);
-  CHECK(ActionTesting::tag_is_retrievable<component,
-                                          Initialization::Tags::InitialTime>(
-      runner, 0));
+  CHECK(ActionTesting::tag_is_retrievable<component, InitialTime>(runner, 0));
   CHECK(ActionTesting::tag_is_retrievable<component, DummyTimeTag>(runner, 0));
   CHECK(not ActionTesting::tag_is_retrievable<component,
                                               TagMultiplyByTwo<DummyTimeTag>>(
       runner, 0));
-  CHECK(ActionTesting::get_databox_tag<component,
-                                       Initialization::Tags::InitialTime>(
-            runner, 0) == initial_time);
+  CHECK(ActionTesting::get_databox_tag<component, InitialTime>(runner, 0) ==
+        initial_time);
   CHECK(ActionTesting::get_databox_tag<component, DummyTimeTag>(runner, 0) ==
         3.0 * initial_time);
   CHECK_FALSE(ActionTesting::get_terminate<component>(runner, 0));
   // Runs Action1
   runner.next_action<component>(0);
-  CHECK(ActionTesting::tag_is_retrievable<component,
-                                          Initialization::Tags::InitialTime>(
-      runner, 0));
+  CHECK(ActionTesting::tag_is_retrievable<component, InitialTime>(runner, 0));
   CHECK(ActionTesting::tag_is_retrievable<component, DummyTimeTag>(runner, 0));
   CHECK(ActionTesting::tag_is_retrievable<component,
                                           TagMultiplyByTwo<DummyTimeTag>>(
       runner, 0));
-  CHECK(ActionTesting::get_databox_tag<component,
-                                       Initialization::Tags::InitialTime>(
-            runner, 0) == initial_time);
+  CHECK(ActionTesting::get_databox_tag<component, InitialTime>(runner, 0) ==
+        initial_time);
   CHECK(ActionTesting::get_databox_tag<component, DummyTimeTag>(runner, 0) ==
         3.0 * initial_time);
   CHECK(
@@ -178,8 +169,8 @@ SPECTRE_TEST_CASE("Unit.Evolution.Initialization.RemoveOptionsFromDataBox",
   CHECK_FALSE(ActionTesting::get_terminate<component>(runner, 0));
   // Runs RemoveOptionsFromDataBox
   runner.next_action<component>(0);
-  CHECK(not ActionTesting::tag_is_retrievable<
-        component, Initialization::Tags::InitialTime>(runner, 0));
+  CHECK(
+      not ActionTesting::tag_is_retrievable<component, InitialTime>(runner, 0));
   CHECK(ActionTesting::tag_is_retrievable<component, DummyTimeTag>(runner, 0));
   CHECK(ActionTesting::tag_is_retrievable<component,
                                           TagMultiplyByTwo<DummyTimeTag>>(
