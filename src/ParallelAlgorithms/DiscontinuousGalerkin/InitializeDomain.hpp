ca0ea0080f5a9bc9d2f6e8567118fbec25be3fb2
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-07-29 11:36:40 +0200

    Generalize domain initializer
    Cleans up code and documentation and adds a test.
    This initializer will be used by both the evolution and elliptic code.

--- src/ParallelAlgorithms/DiscontinuousGalerkin/InitializeDomain.hpp
+++ src/ParallelAlgorithms/DiscontinuousGalerkin/InitializeDomain.hpp
@@ -40,77 +40,90 @@ class ConstGlobalCache;
 
 namespace dg {
 namespace Actions {
-/// \ingroup InitializationGroup
-/// \brief Initialize items related to the basic structure of the Domain
-///
-/// DataBox changes:
-/// - Adds:
-///   * `Tags::Mesh<Dim>`
-///   * `Tags::Element<Dim>`
-///   * `Tags::ElementMap<Dim>`
-///   * `Tags::LogicalCoordinates<Dim>`
-///   * `Tags::MappedCoordinates`
-///   * `Tags::InverseJacobian`
-///   * `Tags::MinimumGridSpacing<Dim, Frame::Inertial>>`
-/// - Removes: nothing
-/// - Modifies: nothing
+/*!
+ * \ingroup InitializationGroup
+ * \brief Initialize items related to the basic structure of the element
+ *
+ * DataBox:
+ * - Uses:
+ *   - `Tags::Domain<Dim, Frame::Inertial>`
+ *   - `Tags::InitialExtents<Dim>`
+ * - Adds:
+ *   - `Tags::Mesh<Dim>`
+ *   - `Tags::Element<Dim>`
+ *   - `Tags::ElementMap<Dim, Frame::Inertial>`
+ *   - `Tags::Coordinates<Dim, Frame::Logical>`
+ *   - `Tags::Coordinates<Dim, Frame::Inertial>`
+ *   - `Tags::InverseJacobian<
+ *   Tags::ElementMap<Dim>, Tags::Coordinates<Dim, Frame::Logical>>`
+ *   - `Tags::MinimumGridSpacing<Dim, Frame::Inertial>>`
+ * - Removes: nothing
+ * - Modifies: nothing
+ */
 template <size_t Dim>
 struct InitializeDomain {
-  using simple_tags = db::AddSimpleTags<::Tags::Mesh<Dim>, ::Tags::Element<Dim>,
-                                        ::Tags::ElementMap<Dim>>;
-
-  using compute_tags = db::AddComputeTags<
-      ::Tags::LogicalCoordinates<Dim>,
-      ::Tags::MappedCoordinates<::Tags::ElementMap<Dim>,
-                                ::Tags::LogicalCoordinates<Dim>>,
-      ::Tags::InverseJacobian<::Tags::ElementMap<Dim>,
-                              ::Tags::LogicalCoordinates<Dim>>,
-      ::Tags::MinimumGridSpacing<Dim, Frame::Inertial>>;
-
   using initialization_option_tags =
-      tmpl::list<::Tags::InitialExtents<Dim>, ::Tags::Domain<Dim>>;
+      tmpl::list<::Tags::InitialExtents<Dim>,
+                 ::Tags::Domain<Dim, Frame::Inertial>>;
 
-  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
-            typename ArrayIndex, typename ActionList,
-            typename ParallelComponent,
-            Requires<tmpl::list_contains_v<
-                typename db::DataBox<DbTagsList>::simple_item_tags,
-                ::Tags::InitialExtents<Dim>>> = nullptr>
-  static auto apply(db::DataBox<DbTagsList>& box,
+  template <
+      typename DataBox, typename... InboxTags, typename Metavariables,
+      typename ActionList, typename ParallelComponent,
+      Requires<
+          db::tag_is_retrievable_v<::Tags::InitialExtents<Dim>, DataBox> and
+          db::tag_is_retrievable_v<::Tags::Domain<Dim, Frame::Inertial>,
+                                   DataBox>> = nullptr>
+  static auto apply(DataBox& box,
                     const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
                     const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
-                    const ArrayIndex& array_index, ActionList /*meta*/,
+                    const ElementIndex<Dim>& array_index,
+                    const ActionList /*meta*/,
                     const ParallelComponent* const /*meta*/) noexcept {
+    using simple_tags =
+        db::AddSimpleTags<::Tags::Mesh<Dim>, ::Tags::Element<Dim>,
+                          ::Tags::ElementMap<Dim>>;
+    using compute_tags = tmpl::append<db::AddComputeTags<
+        ::Tags::LogicalCoordinates<Dim>,
+        ::Tags::MappedCoordinates<::Tags::ElementMap<Dim>,
+                                  ::Tags::Coordinates<Dim, Frame::Logical>>,
+        ::Tags::InverseJacobian<::Tags::ElementMap<Dim>,
+                                ::Tags::Coordinates<Dim, Frame::Logical>>,
+        ::Tags::MinimumGridSpacing<Dim, Frame::Inertial>>>;
+
     const auto& initial_extents = db::get<::Tags::InitialExtents<Dim>>(box);
-    const ::Domain<Dim, Frame::Inertial>& domain =
-        db::get<::Tags::Domain<Dim>>(box);
+    const auto& domain = db::get<::Tags::Domain<Dim, Frame::Inertial>>(box);
+
     const ElementId<Dim> element_id{array_index};
     const auto& my_block = domain.blocks()[element_id.block_id()];
     Mesh<Dim> mesh = domain::Initialization::create_initial_mesh(
         initial_extents, element_id);
     Element<Dim> element =
         domain::Initialization::create_initial_element(element_id, my_block);
-    ElementMap<Dim, Frame::Inertial> map{element_id,
-                                         my_block.coordinate_map().get_clone()};
+    ElementMap<Dim, Frame::Inertial> element_map{
+        element_id, my_block.coordinate_map().get_clone()};
 
-    return std::make_tuple(::Initialization::merge_into_databox<
-                           InitializeDomain, simple_tags, compute_tags>(
-        std::move(box), std::move(mesh), std::move(element), std::move(map)));
+    return std::make_tuple(
+        ::Initialization::merge_into_databox<InitializeDomain, simple_tags,
+                                             compute_tags>(
+            std::move(box), std::move(mesh), std::move(element),
+            std::move(element_map)));
   }
 
-  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
-            typename ArrayIndex, typename ActionList,
-            typename ParallelComponent,
-            Requires<not tmpl::list_contains_v<
-                typename db::DataBox<DbTagsList>::simple_item_tags,
-                ::Tags::InitialExtents<Dim>>> = nullptr>
-  static std::tuple<db::DataBox<DbTagsList>&&> apply(
-      db::DataBox<DbTagsList>& /*box*/,
-      const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+  template <
+      typename DataBox, typename... InboxTags, typename Metavariables,
+      typename ArrayIndex, typename ActionList, typename ParallelComponent,
+      Requires<
+          not db::tag_is_retrievable_v<::Tags::InitialExtents<Dim>, DataBox> or
+          not db::tag_is_retrievable_v<::Tags::Domain<Dim, Frame::Inertial>,
+                                       DataBox>> = nullptr>
+  static std::tuple<DataBox&&> apply(
+      DataBox& /*box*/, const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
       const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
       const ArrayIndex& /*array_index*/, ActionList /*meta*/,
       const ParallelComponent* const /*meta*/) noexcept {
-    ERROR("Could not find dependency '::Tags::InitialExtents' in DataBox.");
+    ERROR(
+        "Dependencies not fulfilled. Did you forget to terminate the phase "
+        "after removing options?");
   }
 };
 }  // namespace Actions
