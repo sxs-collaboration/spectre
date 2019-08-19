db3062622b9071c023b874143616335f968825ab
Jordan Moxon <jem497@cornell.edu>
2019-08-19 11:04:45 -0500

    Add enum capabilities to Static Cache

--- src/Utilities/StaticCache.hpp
+++ src/Utilities/StaticCache.hpp
@@ -13,15 +13,54 @@
 #include "Utilities/TypeTraits.hpp"
 
 /// \ingroup UtilitiesGroup
+/// Range of values for StaticCache indices.  The `Start` is inclusive
+/// and the `End` is exclusive.  The range must not be empty.
+template <size_t Start, size_t End>
+struct CacheRange {
+  static_assert(Start < End, "CacheRange must include at least one value");
+  constexpr static size_t start = Start;
+  constexpr static size_t end = End;
+  constexpr static size_t size = end - start;
+  using value_type = size_t;
+};
+
+/// \ingroup UtilitiesGroup
+/// Possible enumeration values for the StaticCache. Only values specified here
+/// are retrievable.
+///
+/// \note The `EnumerationType` must be streamable.
+template <typename EnumerationType, EnumerationType... Enums>
+struct CacheEnumeration {
+  constexpr static size_t size = sizeof...(Enums);
+  using value_type = EnumerationType;
+};
+
+/// \ingroup UtilitiesGroup
 /// A cache of objects intended to be stored in a static variable.
 ///
-/// Objects can be accessed using several `size_t` arguments in the
-/// ranges specified as template parameters.  The CacheRange template
-/// parameters give first and one-past-last values for the valid
-/// ranges for each argument.
+/// Objects can be accessed via a combination of several `size_t` and `enum`
+/// arguments. The range of each `size_t` argument is specified via a template
+/// parameter of type `CacheRange<start, end>`, giving the first and
+/// one-past-last values for the range. Each `enum` argument is specified by a
+/// template parameter of type `CacheEnumeration<EnumerationType, Members...>`
+/// giving the enumeration type and an explicit set of every enum member to be
+/// cached.
 ///
 /// \example
+/// A cache with only numeric indices:
 /// \snippet Test_StaticCache.cpp static_cache
+///
+/// \example
+/// A cache with enumeration indices:
+/// \snippet Test_StaticCache.cpp static_cache_with_enum
+///
+/// \example
+/// A cache with mixed numeric and enumeration indices:
+/// \snippet Test_StaticCache.cpp static_cache_with_enum_and_numeric
+///
+/// \example
+/// A cache with no arguments at all (caching only a single object)
+/// \snippet Test_StaticCache.cpp static_cache_no_args
 ///
 /// \see make_static_cache
 ///
@@ -30,27 +69,48 @@
 template <typename Generator, typename T, typename... Ranges>
 class StaticCache {
  public:
-  explicit StaticCache(Generator&& generator) : generator_{generator} {}
+  template <typename Gen>
+  // NOLINTNEXTLINE(misc-forwarding-reference-overload)
+  explicit StaticCache(Gen&& generator) noexcept
+      : generator_{std::forward<Gen>(generator)} {}
 
   template <typename... Args>
   const T& operator()(const Args... parameters) const noexcept {
     static_assert(sizeof...(parameters) == sizeof...(Ranges),
                   "Number of arguments must match number of ranges.");
-    return unwrap_cache(
-        std::make_tuple(static_cast<size_t>(parameters),
-                        std::integral_constant<size_t, Ranges::start>{},
-                        std::make_index_sequence<Ranges::size>{})...);
+    return unwrap_cache(generate_tuple<Ranges>(parameters)...);
   }
 
  private:
-  template <size_t... Indices>
+  template <typename Range, typename T1,
+            Requires<not std::is_enum<T1>::value> = nullptr>
+  auto generate_tuple(const T1 parameter) const noexcept {
+    static_assert(
+        tt::is_integer_v<std::decay_t<T1>>,
+        "The parameter passed for a CacheRange must be an integer type.");
+    return std::make_tuple(static_cast<size_t>(parameter),
+                           std::integral_constant<size_t, Range::start>{},
+                           std::make_index_sequence<Range::size>{});
+  }
+
+  template <typename Range, typename T1,
+            Requires<std::is_enum<T1>::value> = nullptr>
+  std::tuple<std::decay_t<T1>, Range> generate_tuple(const T1 parameter) const
+      noexcept {
+    static_assert(
+        std::is_same<typename Range::value_type, std::decay_t<T1>>::value,
+        "Mismatched enum parameter type and cached type.");
+    return {parameter, Range{}};
+  }
+
+  template <typename... IntegralConstantValues>
   const T& unwrap_cache() const noexcept {
-    static const T cached_object = generator_(Indices...);
+    static const T cached_object = generator_(IntegralConstantValues::value...);
     return cached_object;
   }
 
-  template <size_t... Indices, size_t IndexOffset, size_t... Is,
-            typename... Args>
+  template <typename... IntegralConstantValues, size_t IndexOffset,
+            size_t... Is, typename... Args>
   const T& unwrap_cache(
       std::tuple<size_t, std::integral_constant<size_t, IndexOffset>,
                  std::index_sequence<Is...>>
@@ -68,10 +128,45 @@ class StaticCache {
         const T& (StaticCache<Generator, T, Ranges...>::*)(Args...) const,
         sizeof...(Is)>
         cache{{&StaticCache<Generator, T, Ranges...>::unwrap_cache<
-            Indices..., Is + IndexOffset>...}};
+            IntegralConstantValues...,
+            std::integral_constant<size_t, Is + IndexOffset>>...}};
+    // The array `cache` holds pointers to member functions, so we dereference
+    // the pointer and invoke it on `this`.
     return (this->*gsl::at(cache, std::get<0>(parameter0) - IndexOffset))(
         parameters...);
   }
+
+  template <typename... IntegralConstantValues, typename EnumType,
+            EnumType... EnumValues, typename... Args>
+  const T& unwrap_cache(
+      std::tuple<EnumType, CacheEnumeration<EnumType, EnumValues...>>
+          parameter0,
+      Args... parameters) const noexcept {
+    size_t array_location = std::numeric_limits<size_t>::max();
+    static const std::array<EnumType, sizeof...(EnumValues)> values{
+        {EnumValues...}};
+    for (size_t i = 0; i < sizeof...(EnumValues); ++i) {
+      if (std::get<0>(parameter0) == gsl::at(values, i)) {
+        array_location = i;
+        break;
+      }
+    }
+    if (UNLIKELY(array_location == std::numeric_limits<size_t>::max())) {
+      ERROR("Uncached enumeration value: " << std::get<0>(parameter0));
+    }
+    // note that the act of assigning to the specified function pointer type
+    // fixes the template arguments that need to be inferred.
+    static const std::array<
+        const T& (StaticCache<Generator, T, Ranges...>::*)(Args...) const,
+        sizeof...(EnumValues)>
+        cache{{&StaticCache<Generator, T, Ranges...>::unwrap_cache<
+            IntegralConstantValues...,
+            std::integral_constant<EnumType, EnumValues>>...}};
+    // The array `cache` holds pointers to member functions, so we dereference
+    // the pointer and invoke it on `this`.
+    return (this->*gsl::at(cache, array_location))(parameters...);
+  }
+
   const Generator generator_;
 };
 
@@ -79,18 +174,8 @@ class StaticCache {
 /// Create a StaticCache, inferring the cached type from the generator.
 template <typename... Ranges, typename Generator>
 auto make_static_cache(Generator&& generator) noexcept {
-  using CachedType = std::decay_t<decltype(generator((Ranges{}, size_t{})...))>;
-  return StaticCache<Generator, CachedType, Ranges...>(
+  using CachedType = std::decay_t<decltype(
+      generator(std::declval<typename Ranges::value_type>()...))>;
+  return StaticCache<std::decay_t<Generator>, CachedType, Ranges...>(
       std::forward<Generator>(generator));
 }
-
-/// \ingroup UtilitiesGroup
-/// Range of values for StaticCache indices.  The `Start` is inclusive
-/// and the `End` is exclusive.  The range must not be empty.
-template <size_t Start, size_t End>
-struct CacheRange {
-  static_assert(Start < End, "CacheRange must include at least one value");
-  constexpr static size_t start = Start;
-  constexpr static size_t end = End;
-  constexpr static size_t size = end - start;
-};
--- tests/Unit/Utilities/Test_StaticCache.cpp
+++ tests/Unit/Utilities/Test_StaticCache.cpp
@@ -9,13 +9,46 @@
 #include <vector>
 
 #include "ErrorHandling/Error.hpp"
+#include "Utilities/MakeString.hpp"
 #include "Utilities/StaticCache.hpp"
+
+namespace {
+enum class Color { Red, Green, Purple };
+
+std::ostream& operator<<(std::ostream& os, Color t) noexcept {
+  switch (t) {
+    case Color::Red:
+      return os << "Red";
+    case Color::Green:
+      return os << "Green";
+    case Color::Purple:
+      return os << "Purple";
+    default:
+      ERROR("Unknown color");
+  }
+}
+
+enum class Animal { Goldendoodle, Labradoodle, Poodle };
+
+std::ostream& operator<<(std::ostream& os, Animal t) noexcept {
+  switch (t) {
+    case Animal::Goldendoodle:
+      return os << "Goldendoodle";
+    case Animal::Labradoodle:
+      return os << "Labradoodle";
+    case Animal::Poodle:
+      return os << "Poodle";
+    default:
+      ERROR("Unknown Animal");
+  }
+}
+}  // namespace
 
 SPECTRE_TEST_CASE("Unit.Utilities.StaticCache", "[Utilities][Unit]") {
   /// [static_cache]
   const static auto cache =
-      make_static_cache<CacheRange<0, 3>, CacheRange<3, 5>>([](
-          const size_t a, const size_t b) noexcept { return a + b; });
+      make_static_cache<CacheRange<0, 3>, CacheRange<3, 5>>(
+          [](const size_t a, const size_t b) noexcept { return a + b; });
   CHECK(cache(0, 3) == 3);  // smallest entry
   CHECK(cache(2, 4) == 6);  // largest entry
   /// [static_cache]
@@ -28,7 +61,7 @@ SPECTRE_TEST_CASE("Unit.Utilities.StaticCache", "[Utilities][Unit]") {
         return a + b;
       });
   // cache is lazy, shouldn't have called at all before retrieving
-  CHECK(calls.size() == 0);
+  CHECK(calls.empty());
 
   // explicitly call the cache creation to check its contents
   cache2(0, 3);
@@ -62,6 +95,93 @@ SPECTRE_TEST_CASE("Unit.Utilities.StaticCache", "[Utilities][Unit]") {
   CHECK(small_calls == 0);
   CHECK(small_cache() == 5);
   CHECK(small_calls == 1);
+
+  /// [static_cache_no_args]
+  const auto simple_small_cache =
+      make_static_cache([]() noexcept { return size_t{10}; });
+  CHECK(simple_small_cache() == 10);
+  /// [static_cache_no_args]
+
+  // check enum caching functionality
+  const auto enum_generator_tuple =
+      [](const Color& color, const size_t value = 5,
+         const Animal animal = Animal::Goldendoodle) noexcept {
+    size_t offset_animal = 0;
+    switch (animal) {
+      case Animal::Goldendoodle:
+        offset_animal = 3;
+        break;
+      case Animal::Labradoodle:
+        offset_animal = 2;
+        break;
+      case Animal::Poodle:
+        offset_animal = 1;
+        break;
+    };
+
+    switch (color) {
+      case Color::Red:
+        return std::make_tuple(offset_animal, 1, value);
+      case Color::Green:
+        return std::make_tuple(offset_animal, 2, value);
+      case Color::Purple:
+        return std::make_tuple(offset_animal, 3, value);
+    };
+  };
+  /// [static_cache_with_enum]
+  const auto simple_enum_cache = make_static_cache<
+      CacheEnumeration<Color, Color::Red, Color::Green, Color::Purple>>([
+  ](const Color color) noexcept { return std::string{MakeString{} << color}; });
+  CHECK(simple_enum_cache(Color::Red) == "Red");
+  /// [static_cache_with_enum]
+  const auto enum_cache = make_static_cache<
+      CacheEnumeration<Color, Color::Red, Color::Green, Color::Purple>>(
+      enum_generator_tuple);
+  for (const auto color : {Color::Red, Color::Green, Color::Purple}) {
+    CHECK(enum_cache(color) ==
+          std::make_tuple(3, static_cast<size_t>(color) + 1, 5));
+  }
+
+  const auto enum_size_t_cache = make_static_cache<
+      CacheEnumeration<Color, Color::Red, Color::Green, Color::Purple>,
+      CacheRange<3, 5>>(enum_generator_tuple);
+  for (const auto color : {Color::Red, Color::Green, Color::Purple}) {
+    CHECK(enum_size_t_cache(color, 3) ==
+          std::make_tuple(3, static_cast<size_t>(color) + 1, 3));
+    CHECK(enum_size_t_cache(color, 4) ==
+          std::make_tuple(3, static_cast<size_t>(color) + 1, 4));
+  }
+
+  /// [static_cache_with_enum_and_numeric]
+  const auto simple_enum_size_t_enum_cache = make_static_cache<
+      CacheEnumeration<Color, Color::Red, Color::Green, Color::Purple>,
+      CacheRange<3, 5>,
+      CacheEnumeration<Animal, Animal::Goldendoodle, Animal::Labradoodle,
+                       Animal::Poodle>>(
+      [](const Color color, const size_t value, const Animal animal) noexcept {
+        return std::string{MakeString{} << color << value << animal};
+      });
+  CHECK(simple_enum_size_t_enum_cache(Color::Red, 3, Animal::Labradoodle) ==
+        "Red3Labradoodle");
+  CHECK(simple_enum_size_t_enum_cache(Color::Purple, 4, Animal::Poodle) ==
+        "Purple4Poodle");
+  /// [static_cache_with_enum_and_numeric]
+  const auto enum_size_t_enum_cache = make_static_cache<
+      CacheEnumeration<Color, Color::Red, Color::Green, Color::Purple>,
+      CacheRange<3, 5>,
+      CacheEnumeration<Animal, Animal::Goldendoodle, Animal::Labradoodle,
+                       Animal::Poodle>>(enum_generator_tuple);
+  for (const auto color : {Color::Red, Color::Green, Color::Purple}) {
+    for (const auto animal :
+         {Animal::Goldendoodle, Animal::Labradoodle, Animal::Poodle}) {
+      CHECK(enum_size_t_enum_cache(color, 3, animal) ==
+            std::make_tuple(3 - static_cast<size_t>(animal),
+                            static_cast<size_t>(color) + 1, 3));
+      CHECK(enum_size_t_enum_cache(color, 4, animal) ==
+            std::make_tuple(3 - static_cast<size_t>(animal),
+                            static_cast<size_t>(color) + 1, 4));
+    }
+  }
 }
 
 // [[OutputRegex, Index out of range: 3 <= 2 < 5]]
@@ -69,8 +189,8 @@ SPECTRE_TEST_CASE("Unit.Utilities.StaticCache", "[Utilities][Unit]") {
                                "[Utilities][Unit]") {
   ASSERTION_TEST();
 #ifdef SPECTRE_DEBUG
-  const auto cache = make_static_cache<CacheRange<3, 5>>([](
-      const size_t x) noexcept { return x; });
+  const auto cache = make_static_cache<CacheRange<3, 5>>(
+      [](const size_t x) noexcept { return x; });
   cache(2);
   ERROR("Failed to trigger ASSERT in an assertion test");
 #endif
@@ -81,8 +201,8 @@ SPECTRE_TEST_CASE("Unit.Utilities.StaticCache", "[Utilities][Unit]") {
                                "[Utilities][Unit]") {
   ASSERTION_TEST();
 #ifdef SPECTRE_DEBUG
-  const auto cache = make_static_cache<CacheRange<3, 5>>([](
-      const size_t x) noexcept { return x; });
+  const auto cache = make_static_cache<CacheRange<3, 5>>(
+      [](const size_t x) noexcept { return x; });
   cache(5);
   ERROR("Failed to trigger ASSERT in an assertion test");
 #endif
