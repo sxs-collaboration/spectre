20e387409890b3732a81b94e5cfbd321d30cb729
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-09-05 14:11:05 +0200

    Wrap evolution option tags in evolution namespace

--- src/Evolution/Tags.hpp
+++ src/Evolution/Tags.hpp
@@ -7,6 +7,7 @@
 
 #include "Options/Options.hpp"
 
+namespace evolution {
 namespace OptionTags {
 
 /*!
@@ -14,7 +15,7 @@ namespace OptionTags {
  * \brief Groups option tags related to the time evolution, e.g. time step and
  * time stepper.
  */
-struct EvolutionGroup {
+struct Group {
   static std::string name() noexcept { return "Evolution"; }
   static constexpr OptionString help{"Options for the time evolution"};
 };
@@ -26,9 +27,10 @@ struct EvolutionGroup {
  * The option tags for the evolution system should be placed in a subgroup that
  * carries the system name. See e.g. `OptionTags::ValenciaDivCleanGroup`.
  */
-struct EvolutionSystemGroup {
+struct SystemGroup {
   static std::string name() noexcept { return "EvolutionSystem"; }
   static constexpr OptionString help{"The system of hyperbolic PDEs"};
 };
 
 }  // namespace OptionTags
+}  // namespace evolution
