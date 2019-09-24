86da9eab64b516c258cd57c60940dfd055e603f9
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-09-24 17:55:19 +0200

    Make PUP::able classes compatible with Python bindings

--- src/PythonBindings/CharmCompatibility.cpp
+++ src/PythonBindings/CharmCompatibility.cpp
@@ -4,6 +4,8 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
+#include <pup.h>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -11,6 +13,8 @@
 // provide resonable replacements for them. Getting the correct Charm++ library
 // linked is non-trivial since Charm++ generally wants you to use their `charmc`
 // script, but we want to avoid that.
+
+/// \cond
 
 __attribute__ ((format (printf, 1, 2)))
 // NOLINTNEXTLINE(cert-dcl50-cpp)
@@ -66,3 +70,48 @@ std::string get_library_versions() noexcept {
 
 std::string get_paths() noexcept { return "Not supported in python."; }
 }  // namespace formaline
+
+/*
+ * PUP::able support
+ *
+ * Here we provide dummy-implementations of functions in Charm++'s `pup.h` so
+ * that we can write Python wrappers for classes that derive from `PUP::able`.
+ * These functions are not intended to be called, so we throw exceptions that
+ * propagate to Python and provide an error message when one of the functions
+ * gets called accidentally.
+ */
+
+// Destructor will be called in Python, but doesn't need to do anything
+PUP::able::~able() = default;
+
+// gcc suggests to mark these functions `noreturn`, but they are declared in
+// pup.h
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsuggest-attribute=noreturn"
+#endif  // defined(__GNUC__) && !defined(__clang__)
+
+void PUP::able::pup(PUP::er& /*p*/) {
+  throw std::runtime_error{
+      "The function 'PUP::able::pup' is provided for compatibility with "
+      "Charm++ and is not intended to be called."};
+}
+
+PUP::able::PUP_ID PUP::able::register_constructor(const char* /*className*/,
+                                                  constructor_function /*fn*/) {
+  throw std::runtime_error{
+      "The function 'PUP::able::register_constructor' is provided for "
+      "compatibility with Charm++ and is not intended to be called."};
+}
+
+PUP::able* PUP::able::clone() const {
+  throw std::runtime_error{
+      "The function 'PUP::able::clone' is provided for compatibility with "
+      "Charm++ and is not intended to be called."};
+}
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif  // defined(__GNUC__) && !defined(__clang__)
+
+/// \endcond
