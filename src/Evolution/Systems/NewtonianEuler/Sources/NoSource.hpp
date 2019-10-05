f3881c71a47d55d5880ab52dfdc1fe75a47e0173
Cristobal Armaza <ca455@cornell.edu>
2019-10-05 00:36:50 -0400

    Add NoSource type for NewtonianEuler

---
+++ src/Evolution/Systems/NewtonianEuler/Sources/NoSource.hpp
@@ -0,0 +1,22 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "Utilities/TMPL.hpp"
+
+namespace NewtonianEuler {
+namespace Sources {
+
+/*!
+ * \brief Used to mark that the initial data do not require source
+ * terms in the evolution equations.
+ */
+struct NoSource {
+  using sourced_variables = tmpl::list<>;
+
+  // clang-tidy: google-runtime-references
+  void pup(PUP::er& /*p*/) noexcept {}  // NOLINT
+};
+}  // namespace Sources
+}  // namespace NewtonianEuler
