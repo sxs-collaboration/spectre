63dd838d09209aa559dd600dd0647a99f403c2c2
Cristobal Armaza <ca455@cornell.edu>
2019-10-08 22:32:56 -0400

    Remove assignment of vanishing TildeD source in Valencia

--- tests/Unit/Evolution/Systems/RelativisticEuler/Valencia/TestFunctions.py
+++ tests/Unit/Evolution/Systems/RelativisticEuler/Valencia/TestFunctions.py
@@ -54,13 +54,6 @@ def tilde_s(rest_mass_density, specific_internal_energy,
 
 
 # Functions for testing Equations.cpp
-def source_tilde_d(tilde_d, tilde_tau, tilde_s, spatial_velocity, pressure,
-                   lapse, d_lapse, d_shift, d_spatial_metric,
-                   inv_spatial_metric, sqrt_det_spatial_metric,
-                   extrinsic_curvature):
-    return 0.0
-
-
 def source_tilde_tau(tilde_d, tilde_tau, tilde_s, spatial_velocity, pressure,
                      lapse, d_lapse, d_shift, d_spatial_metric,
                      inv_spatial_metric, sqrt_det_spatial_metric,
