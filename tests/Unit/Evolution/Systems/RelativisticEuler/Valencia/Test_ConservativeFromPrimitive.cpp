a88169e26642deaa9506da9a6414ac101eaebdbd
Cristobal Armaza <ca455@cornell.edu>
2019-10-09 14:06:01 -0400

    Remove DataType template parameter from prim <--> cons functions in Valencia
     - Functions only handle DataVectors now, in consistency with other systems

--- tests/Unit/Evolution/Systems/RelativisticEuler/Valencia/Test_ConservativeFromPrimitive.cpp
+++ tests/Unit/Evolution/Systems/RelativisticEuler/Valencia/Test_ConservativeFromPrimitive.cpp
@@ -15,7 +15,7 @@ namespace {
 template <size_t Dim, typename DataType>
 void test_conservative_from_primitive(const DataType& used_for_size) noexcept {
   pypp::check_with_random_values<1>(
-      &RelativisticEuler::Valencia::conservative_from_primitive<DataType, Dim>,
+      &RelativisticEuler::Valencia::conservative_from_primitive<Dim>,
       "TestFunctions", {"tilde_d", "tilde_tau", "tilde_s"}, {{{0.0, 1.0}}},
       used_for_size);
 }
@@ -26,6 +26,6 @@ SPECTRE_TEST_CASE("Unit.RelativisticEuler.Valencia.ConservativeFromPrimitive",
   pypp::SetupLocalPythonEnvironment local_python_env{
       "Evolution/Systems/RelativisticEuler/Valencia"};
 
-  GENERATE_UNINITIALIZED_DOUBLE_AND_DATAVECTOR;
-  CHECK_FOR_DOUBLES_AND_DATAVECTORS(test_conservative_from_primitive, (1, 2, 3))
+  GENERATE_UNINITIALIZED_DATAVECTOR;
+  CHECK_FOR_DATAVECTORS(test_conservative_from_primitive, (1, 2, 3))
 }
