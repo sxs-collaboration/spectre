4426b153c670bd27eca0a59c1b3f4ce48ed57a6d
Jordan Moxon <jem497@cornell.edu>
2019-10-10 16:45:35 -0700

    Add lapack wrappers

---
+++ src/NumericalAlgorithms/LinearSolver/Lapack.hpp
@@ -0,0 +1,84 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "Utilities/Gsl.hpp"
+
+/// \cond
+class DataVector;
+class Matrix;
+/// \endcond
+
+/// LAPACK wrappers
+namespace lapack {
+// @{
+/*!
+ * \ingroup LinearSolverGroup
+ * \brief Wrapper for LAPACK dgesv, which solves the general linear equation
+ * \f$A x = b\f$ by LUP (Lower-triangular, upper-triangular, and permutation)
+ * decomposition.
+ * \details Several interfaces are provided with combinations of
+ * input parameters due to the versatility of the LAPACK utility.
+ * - `rhs_in_solution_out` or `solution` : If the `rhs` `DataVector` is not
+ * separately supplied, this single pass-by-pointer `DataVector` acts as the
+ * input \f$ b\f$, and is overwritten with the solution \f$x\f$; in this case
+ * `rhs_in_solution_out` must be sufficiently large to contain the solution. If
+ * `rhs` is separately supplied, `solution` will contain \f$x\f$ at the end of
+ * the algorithm, and must be sufficiently large to contain the solution.
+ * - `rhs` (optional) : An input `DataVector` representing \f$b\f$ that is not
+ * overwritten.
+ * - `matrix_operator`: The `Matrix` \f$A\f$. If passed by pointer, this matrix
+ * will be 'destroyed' and overwritten with LU decomposition information.
+ * Optionally, a `pivots` vector may be passed by pointer so that the full LUP
+ * decomposition information can be recovered from the LAPACK output. If passed
+ * by const reference, the wrapper will make a copy so that LAPACK does not
+ * overwrite the supplied matrix.
+ * - `pivots` (optional) : a `std::vector<int>`, passed by pointer, which will
+ * contain the permutation information necessary to reassemble the full matrix
+ * information if LAPACK is permitted to modify the input matrix in-place.
+ * - `number_of_rhs` : The number of columns of \f$x\f$ and \f$b\f$. This is
+ * thought of as the 'number of right-hand-sides' to be solved for. In most
+ * cases, that additional dimension can be inferred from the dimensionality of
+ * the matrix and the input vector(s), which occurs if `number_of_rhs` is set to
+ * 0 (default). If the provided `DataVector` is an inappropriate size for that
+ * inference (i.e. is not a multiple of the largest dimension of the matrix), or
+ * if you only want to solve for the first `number_of_rhs` columns, the
+ * parameter `number_of_rhs` must be supplied.
+ *
+ * The function return `int` is the value provided by the `INFO` field of the
+ * LAPACK call. It is 0 for a successful linear solve, and nonzero values code
+ * for types of failures of the algorithm.
+ * See LAPACK documentation for further details about `dgesv`:
+ * http://www.netlib.org/lapack/
+ */
+int general_matrix_linear_solve(gsl::not_null<DataVector*> rhs_in_solution_out,
+                                gsl::not_null<Matrix*> matrix_operator,
+                                int number_of_rhs = 0) noexcept;
+
+int general_matrix_linear_solve(gsl::not_null<DataVector*> rhs_in_solution_out,
+                                gsl::not_null<std::vector<int>*> pivots,
+                                gsl::not_null<Matrix*> matrix_operator,
+                                int number_of_rhs = 0) noexcept;
+
+int general_matrix_linear_solve(gsl::not_null<DataVector*> solution,
+                                gsl::not_null<Matrix*> matrix_operator,
+                                const DataVector& rhs,
+                                int number_of_rhs = 0) noexcept;
+
+int general_matrix_linear_solve(gsl::not_null<DataVector*> solution,
+                                gsl::not_null<std::vector<int>*> pivots,
+                                gsl::not_null<Matrix*> matrix_operator,
+                                const DataVector& rhs,
+                                int number_of_rhs = 0) noexcept;
+
+int general_matrix_linear_solve(gsl::not_null<DataVector*> rhs_in_solution_out,
+                                const Matrix& matrix_operator,
+                                int number_of_rhs = 0) noexcept;
+
+int general_matrix_linear_solve(gsl::not_null<DataVector*> solution,
+                                const Matrix& matrix_operator,
+                                const DataVector& rhs,
+                                int number_of_rhs = 0) noexcept;
+// @}
+}  // namespace lapack
---
+++ tests/Unit/NumericalAlgorithms/LinearSolver/Test_Lapack.cpp
@@ -0,0 +1,108 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "tests/Unit/TestingFramework.hpp"
+
+#include <cstddef>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Index.hpp"
+#include "DataStructures/Matrix.hpp"
+#include "NumericalAlgorithms/LinearOperators/ApplyMatrices.hpp"
+#include "NumericalAlgorithms/LinearSolver/Lapack.hpp"
+#include "Utilities/Gsl.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+#include "tests/Utilities/MakeWithRandomValues.hpp"
+
+template <typename Generator>
+void test_square_general_matrix_linear_solve(
+    const gsl::not_null<Generator*> generator) noexcept {
+  UniformCustomDistribution<size_t> size_dist(2, 6);
+  // the size of the matrix in the linear solve
+  const size_t rows = size_dist(*generator);
+  const size_t columns = rows;
+
+  // number of distinct rhs that will be solved, as LAPACK can efficiently do
+  // several linear solves at once
+  const size_t number_of_rhs = size_dist(*generator);
+  // refer to the canonical linear system A x = b
+
+  UniformCustomDistribution<double> value_dist(0.1, 0.5);
+  // the vector x
+  DataVector expected_solution_vector = make_with_random_values<DataVector>(
+      generator, make_not_null(&value_dist), number_of_rhs * rows);
+  // the matrix A
+  Matrix operator_matrix{rows, columns};
+  for (size_t row = 0; row < rows; ++row) {
+    for (size_t column = 0; column < columns; ++column) {
+      operator_matrix(row, column) = value_dist(*generator);
+    }
+  }
+  for (size_t i = 0; i < rows; ++i) {
+    operator_matrix(i, i) += 1.0;
+  }
+  CAPTURE_PRECISE(operator_matrix);
+  // the vector b
+  DataVector input_vector = apply_matrices<DataVector, Matrix>(
+      {{operator_matrix, Matrix{}}}, expected_solution_vector,
+      Index<2>{rows, number_of_rhs});
+  CAPTURE_PRECISE(input_vector);
+  // version which copies the matrix, and preserves the input
+  DataVector solution_vector{number_of_rhs * rows, 0.0};
+  lapack::general_matrix_linear_solve(make_not_null(&solution_vector),
+                                      operator_matrix, input_vector);
+  CHECK_ITERABLE_APPROX(solution_vector, expected_solution_vector);
+
+  // solve only a subset of the rhs's given
+  DataVector single_column_solution_vector{rows};
+  lapack::general_matrix_linear_solve(
+      make_not_null(&single_column_solution_vector), operator_matrix,
+      input_vector, 1);
+  for (size_t i = 0; i < rows; ++i) {
+    CHECK(single_column_solution_vector[i] ==
+          approx(expected_solution_vector[i]));
+  }
+
+  // version which copies the matrix, overwrites the input
+  solution_vector = input_vector;
+  lapack::general_matrix_linear_solve(make_not_null(&solution_vector),
+                                      operator_matrix);
+  CHECK_ITERABLE_APPROX(solution_vector, expected_solution_vector);
+
+  // solve only a subset of the rhs's given
+  solution_vector = input_vector;
+  lapack::general_matrix_linear_solve(make_not_null(&solution_vector),
+                                      operator_matrix, 1);
+  for (size_t i = 0; i < rows; ++i) {
+    CHECK(solution_vector[i] == approx(expected_solution_vector[i]));
+  }
+  // the rest of the entries should not have been changed by the LAPACK call.
+  for (size_t i = rows; i < rows * number_of_rhs; ++i) {
+    CHECK(solution_vector[i] == input_vector[i]);
+  }
+
+  // version which overwrites the matrix but preserves the input
+  Matrix operator_matrix_copy = operator_matrix;
+  lapack::general_matrix_linear_solve(make_not_null(&solution_vector),
+                                      make_not_null(&operator_matrix_copy),
+                                      input_vector);
+  CHECK_ITERABLE_APPROX(solution_vector, expected_solution_vector);
+  CHECK(operator_matrix_copy != operator_matrix);
+
+  // version which overwrites the matrix and the input
+  operator_matrix_copy = operator_matrix;
+  solution_vector = input_vector;
+  lapack::general_matrix_linear_solve(make_not_null(&solution_vector),
+                                      make_not_null(&operator_matrix));
+  CHECK_ITERABLE_APPROX(solution_vector, expected_solution_vector);
+  CHECK(operator_matrix_copy != operator_matrix);
+}
+
+SPECTRE_TEST_CASE("Unit.Numerical.LinearSolver.Lapack",
+                  "[Unit][NumericalAlgorithms][LinearSolver]") {
+  MAKE_GENERATOR(gen);
+  {
+    INFO("Test general linear solve on invertible square matrix")
+    test_square_general_matrix_linear_solve(make_not_null(&gen));
+  }
+}
