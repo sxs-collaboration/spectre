11441e55479ad0eee743da96d5354fded5dcedf1
William Throwe <wtt6@cornell.edu>
2019-10-28 17:05:06 -0400

    Improve error messages, docs in quadratic solvers
    Also modernize test structure.

--- src/NumericalAlgorithms/RootFinding/QuadraticEquation.cpp
+++ src/NumericalAlgorithms/RootFinding/QuadraticEquation.cpp
@@ -7,24 +7,24 @@
 #include <limits>
 
 #include "ErrorHandling/Assert.hpp"
-#include "ErrorHandling/ExpectsAndEnsures.hpp"
 
-double positive_root(const double a, const double b, const double c) {
-  double x0 = std::numeric_limits<double>::signaling_NaN();
-  double x1 = std::numeric_limits<double>::signaling_NaN();
-  // clang-tidy: value stored ... never read (true if in Release Build)
-  int num_real_roots = gsl_poly_solve_quadratic(a, b, c, &x0, &x1);  // NOLINT
-  ASSERT(num_real_roots == 2, "Assumes that there are two real roots");
-  Ensures(x0 <= 0.0 and x1 >= 0.0);
-  return x1;
+double positive_root(const double a, const double b, const double c) noexcept {
+  const auto roots = real_roots(a, b, c);
+  ASSERT(roots[0] <= 0.0 and roots[1] >= 0.0,
+         "There are two positive roots, " << roots[0] << " and " << roots[1]
+         << ", with a=" << a << " b=" << b << " c=" << c);
+  return roots[1];
 }
 
 std::array<double, 2> real_roots(const double a, const double b,
-                                 const double c) {
+                                 const double c) noexcept {
   double x0 = std::numeric_limits<double>::signaling_NaN();
   double x1 = std::numeric_limits<double>::signaling_NaN();
   // clang-tidy: value stored ... never read (true if in Release Build)
-  int num_real_roots = gsl_poly_solve_quadratic(a, b, c, &x0, &x1);  // NOLINT
-  ASSERT(num_real_roots == 2, "Assumes that there are two real roots");
+  // NOLINTNEXTLINE
+  const int num_real_roots = gsl_poly_solve_quadratic(a, b, c, &x0, &x1);
+  ASSERT(num_real_roots == 2,
+         "There are only " << num_real_roots << " real roots with a=" << a
+         << " b=" << b << " c=" << c);
   return {{x0, x1}};
 }
