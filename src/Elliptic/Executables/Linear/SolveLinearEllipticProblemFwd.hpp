543ca2c08de8d0575a3b9ee112827b523f5e870e
Nils Leif Fischer <hello@nilsleiffischer.de>
2019-11-22 13:28:42 -0800

    Setup linear elliptic executables
    Add executables for the existing Poisson problems, as is done
    for some of the evolution problems. Since the Metavariables
    is not specific to the Poisson system anymore (as of #1671),
    the system is now also chosen when setting up the executable.

--- src/Elliptic/Executables/Linear/SolveLinearEllipticProblemFwd.hpp
+++ src/Elliptic/Executables/Linear/SolveLinearEllipticProblemFwd.hpp
@@ -6,6 +6,19 @@
 #include <cstddef>
 
 /// \cond
+namespace Poisson {
 template <size_t Dim>
+struct FirstOrderSystem;
+namespace Solutions {
+template <size_t Dim>
+struct Lorentzian;
+template <size_t Dim>
+struct Moustache;
+template <size_t Dim>
+struct ProductOfSinusoids;
+}  // namespace Solutions
+}  // namespace Poisson
+
+template <typename System, typename InitialGuess, typename BoundaryConditions>
 struct Metavariables;
 /// \endcond
