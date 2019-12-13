e97ca78f79e64c7f630930689065a5401145a358
Francois Hebert <fmh33@cornell.edu>
2019-12-13 13:20:57 -0800

    Remove flatten from more PDALs
    An earlier commit removed flattens from EvolveValenciaDivClean; this
    commit makes the same change to other executables.

--- src/Evolution/Executables/ScalarWave/EvolveScalarWave.hpp
+++ src/Evolution/Executables/ScalarWave/EvolveScalarWave.hpp
@@ -197,12 +197,12 @@ struct EvolutionMetavars {
 
               Parallel::PhaseActions<
                   Phase, Phase::Evolve,
-                  tmpl::flatten<tmpl::list<
+                  tmpl::list<
                       Actions::RunEventsAndTriggers,
                       tmpl::conditional_t<
                           local_time_stepping,
                           Actions::ChangeStepSize<step_choosers>, tmpl::list<>>,
-                      step_actions, Actions::AdvanceTime>>>>>>;
+                      step_actions, Actions::AdvanceTime>>>>>;
 
   static constexpr OptionString help{
       "Evolve a Scalar Wave in Dim spatial dimension.\n\n"
