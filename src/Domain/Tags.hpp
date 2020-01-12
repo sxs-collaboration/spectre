fe85fde4590064ecd0f00a5af2c037953bbc7728
Nils Leif Fischer <hello@nilsleiffischer.de>
2020-01-12 12:55:27 +0000

    Fix another missing `db::tag_name`

--- src/Domain/Tags.hpp
+++ src/Domain/Tags.hpp
@@ -288,8 +288,8 @@ template <typename DirectionsTag, typename Tag>
 struct Interface : virtual db::SimpleTag,
                    Interface_detail::GetBaseTagIfPresent<DirectionsTag, Tag> {
   static std::string name() noexcept {
-    return "Interface<" + DirectionsTag::name() + ", " + db::tag_name<Tag>() +
-           ">";
+    return "Interface<" + db::tag_name<DirectionsTag>() + ", " +
+           db::tag_name<Tag>() + ">";
   };
   using tag = Tag;
   // The use of db::const_item_type<Tag> assumes we will never store
