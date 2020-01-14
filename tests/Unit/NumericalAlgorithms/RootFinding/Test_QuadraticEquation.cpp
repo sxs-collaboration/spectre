f395537d849fc949a79082d3e8f4c04a241a79e3
Nils Deppe <nd357@cornell.edu>
2020-01-14 14:14:04 -0800

    Reduce test cases for quadratic equation

--- tests/Unit/NumericalAlgorithms/RootFinding/Test_QuadraticEquation.cpp
+++ tests/Unit/NumericalAlgorithms/RootFinding/Test_QuadraticEquation.cpp
@@ -42,13 +42,12 @@
 #endif
 }
 
-SPECTRE_TEST_CASE("Unit.Numerical.RootFinding.positive_root",
+SPECTRE_TEST_CASE("Unit.Numerical.RootFinding.QuadraticEquation",
                   "[NumericalAlgorithms][RootFinding][Unit]") {
+  // Positive root
   CHECK(approx(6.31662479035539985) == positive_root(1.0, -6.0, -2.0));
-}
 
-SPECTRE_TEST_CASE("Unit.Numerical.RootFinding.real_roots",
-                  "[NumericalAlgorithms][RootFinding][Unit]") {
+  // Real roots
   const auto roots = real_roots(2.0, -11.0, 5.0);
   CHECK(approx(0.5) == roots[0]);
   CHECK(approx(5.0) == roots[1]);
