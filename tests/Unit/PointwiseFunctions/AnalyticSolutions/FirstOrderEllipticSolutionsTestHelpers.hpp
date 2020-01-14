27693a5f62da8f996110ff25806d2e14cbb2497f
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-01-14 18:55:50 +0100

    Support arguments for fluxes and sources in elliptic verify_solution

--- tests/Unit/PointwiseFunctions/AnalyticSolutions/FirstOrderEllipticSolutionsTestHelpers.hpp
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/FirstOrderEllipticSolutionsTestHelpers.hpp
@@ -32,23 +32,67 @@ struct OperatorAppliedTo : db::SimpleTag, db::PrefixTag {
 
 }  // namespace Tags
 
-/// \ingroup TestingFrameworkGroup
-/// Test that the `solution` numerically solves the `System` on the given grid
-/// for the given tolerance
-template <typename System, typename SolutionType, typename... FluxesArgs,
-          typename... SourcesArgs, typename... Maps>
-void verify_solution(
-    const SolutionType& solution,
-    const typename System::fluxes& fluxes_computer,
-    const FluxesArgs&... fluxes_args, const SourcesArgs&... sources_args,
-    const Mesh<System::volume_dim>& mesh,
-    const domain::CoordinateMap<Frame::Logical, Frame::Inertial, Maps...>
-        coord_map,
-    const double tolerance) {
-  constexpr size_t volume_dim = System::volume_dim;
+namespace detail {
+
+template <typename System,
+          typename FluxesArgs =
+              tmpl::transform<typename System::fluxes::argument_tags,
+                              tmpl::bind<db::const_item_type, tmpl::_1>>,
+          typename FluxesArgsIndices =
+              std::make_index_sequence<tmpl::size<FluxesArgs>::value>,
+          typename SourcesArgs =
+              tmpl::transform<typename System::sources::argument_tags,
+                              tmpl::bind<db::const_item_type, tmpl::_1>>,
+          typename SourcesArgsIndices =
+              std::make_index_sequence<tmpl::size<SourcesArgs>::value>>
+struct ExpandTuplesImpl;
+
+template <typename System, typename... FluxesArgs, size_t... FluxesArgsIndices,
+          typename... SourcesArgs, size_t... SourcesArgsIndices>
+struct ExpandTuplesImpl<System, tmpl::list<FluxesArgs...>,
+                        std::index_sequence<FluxesArgsIndices...>,
+                        tmpl::list<SourcesArgs...>,
+                        std::index_sequence<SourcesArgsIndices...>> {
+  static constexpr size_t volume_dim = System::volume_dim;
   using all_fields = db::get_variables_tags_list<typename System::fields_tag>;
   using primal_fields = typename System::primal_fields;
   using auxiliary_fields = typename System::auxiliary_fields;
+
+  static auto first_order_fluxes(
+      const Variables<all_fields>& vars,
+      const typename System::fluxes& fluxes_computer,
+      const std::tuple<FluxesArgs...>& fluxes_args) noexcept {
+    return elliptic::first_order_fluxes<volume_dim, primal_fields,
+                                        auxiliary_fields>(
+        vars, fluxes_computer, get<FluxesArgsIndices>(fluxes_args)...);
+  }
+
+  static auto first_order_sources(
+      const Variables<all_fields>& vars,
+      const std::tuple<SourcesArgs...>& sources_args) noexcept {
+    return elliptic::first_order_sources<primal_fields, auxiliary_fields,
+                                         typename System::sources>(
+        vars, get<SourcesArgsIndices>(sources_args)...);
+  }
+};
+
+}  // namespace detail
+
+/// \ingroup TestingFrameworkGroup
+/// Test that the `solution` numerically solves the `System` on the given grid
+/// for the given tolerance
+template <typename System, typename SolutionType, typename... Maps,
+          typename... FluxesArgs, typename... SourcesArgs>
+void verify_solution(
+    const SolutionType& solution,
+    const typename System::fluxes& fluxes_computer,
+    const Mesh<System::volume_dim>& mesh,
+    const domain::CoordinateMap<Frame::Logical, Frame::Inertial, Maps...>
+        coord_map,
+    const double tolerance,
+    const std::tuple<FluxesArgs...>& fluxes_args = std::tuple<>{},
+    const std::tuple<SourcesArgs...>& sources_args = std::tuple<>{}) {
+  using all_fields = db::get_variables_tags_list<typename System::fields_tag>;
 
   const size_t num_points = mesh.number_of_grid_points();
   const auto logical_coords = logical_coordinates(mesh);
@@ -57,14 +101,12 @@ void verify_solution(
       solution.variables(inertial_coords, all_fields{}));
 
   // Apply operator to solution fields
-  auto fluxes =
-      elliptic::first_order_fluxes<volume_dim, primal_fields, auxiliary_fields>(
-          solution_fields, fluxes_computer, fluxes_args...);
+  auto fluxes = detail::ExpandTuplesImpl<System>::first_order_fluxes(
+      solution_fields, fluxes_computer, fluxes_args);
   auto div_fluxes = divergence(std::move(fluxes), mesh,
                                coord_map.inv_jacobian(logical_coords));
-  auto sources = elliptic::first_order_sources<primal_fields, auxiliary_fields,
-                                               typename System::sources>(
-      solution_fields, sources_args...);
+  auto sources = detail::ExpandTuplesImpl<System>::first_order_sources(
+      solution_fields, sources_args);
   Variables<db::wrap_tags_in<Tags::OperatorAppliedTo, all_fields>>
       operator_applied_to_fields{num_points};
   elliptic::first_order_operator(make_not_null(&operator_applied_to_fields),
