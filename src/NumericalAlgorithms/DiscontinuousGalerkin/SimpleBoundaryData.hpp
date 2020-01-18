57f4f3364e51006f312fe50c6a6ac592c0490198
Nils Leif Fischer <hello@nilsleiffischer.de>
2020-01-18 13:40:25 +0100

    Add SimpleBoundaryData class
    Data on an element face that distinguishes between field data, which can
    be projected to a mortar, and extra data, which will not be projected.
    
    Note that another class was recently renamed from `SimpleBoundaryData` to
    `SimpleMortarData` because it represents boundary data on both sides of
    a mortar.

---
+++ src/NumericalAlgorithms/DiscontinuousGalerkin/SimpleBoundaryData.hpp
@@ -0,0 +1,90 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <pup.h>
+
+#include "DataStructures/Variables.hpp"
+#include "Domain/OrientationMapHelpers.hpp"
+#include "NumericalAlgorithms/DiscontinuousGalerkin/MortarHelpers.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+/// \cond
+template <size_t Dim>
+struct Index;
+template <size_t Dim>
+struct Mesh;
+template <size_t Dim>
+struct OrientationMap;
+namespace Spectral {
+enum class MortarSize;
+}  // namespace Spectral
+/// \endcond
+
+namespace dg {
+
+/*!
+ * \brief Distinguishes between field data, which can be projected to a mortar,
+ * and extra data, which will not be projected.
+ */
+template <typename FieldTags, typename ExtraDataTags = tmpl::list<>>
+struct SimpleBoundaryData {
+  using field_tags = FieldTags;
+  using extra_data_tags = ExtraDataTags;
+
+  /// Data projected to the mortar mesh
+  Variables<FieldTags> field_data;
+
+  /// Data on the element face that needs no projection to the mortar mesh.
+  /// This is a `TaggedTuple` to support non-tensor quantities. It also helps
+  /// supporting an empty list of `ExtraDataTags`.
+  tuples::tagged_tuple_from_typelist<ExtraDataTags> extra_data;
+
+  SimpleBoundaryData() = default;
+  SimpleBoundaryData(const SimpleBoundaryData&) noexcept = default;
+  SimpleBoundaryData& operator=(const SimpleBoundaryData&) noexcept = default;
+  SimpleBoundaryData(SimpleBoundaryData&&) noexcept = default;
+  SimpleBoundaryData& operator=(SimpleBoundaryData&&) noexcept = default;
+  ~SimpleBoundaryData() noexcept = default;
+
+  explicit SimpleBoundaryData(const size_t num_points) noexcept
+      : field_data{num_points}, extra_data{} {}
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& p) noexcept {
+    p | field_data;
+    p | extra_data;
+  }
+
+  /// Project the `field_data` to the mortar
+  ///
+  /// \see `dg::project_to_mortar`
+  template <size_t MortarDim>
+  SimpleBoundaryData<FieldTags, ExtraDataTags> project_to_mortar(
+      const Mesh<MortarDim>& face_mesh, const Mesh<MortarDim>& mortar_mesh,
+      const std::array<Spectral::MortarSize, MortarDim>& mortar_size) const
+      noexcept {
+    SimpleBoundaryData<FieldTags, ExtraDataTags> projected_data{};
+    projected_data.field_data = dg::project_to_mortar(
+        this->field_data, face_mesh, mortar_mesh, mortar_size);
+    projected_data.extra_data = this->extra_data;
+    return projected_data;
+  }
+
+  /// Orient the `field_data`
+  ///
+  /// \see `orient_variables_on_slice`
+  template <size_t MortarDim>
+  void orient_on_slice(
+      const Index<MortarDim>& slice_extents, const size_t sliced_dim,
+      const OrientationMap<MortarDim + 1>& orientation_of_neighbor) noexcept {
+    this->field_data = orient_variables_on_slice(
+        this->field_data, slice_extents, sliced_dim, orientation_of_neighbor);
+  }
+};
+
+}  // namespace dg
---
+++ tests/Unit/NumericalAlgorithms/DiscontinuousGalerkin/Test_SimpleBoundaryData.cpp
@@ -0,0 +1,72 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "tests/Unit/TestingFramework.hpp"
+
+#include <array>
+#include <cstddef>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/Direction.hpp"
+#include "Domain/Mesh.hpp"
+#include "Domain/OrientationMap.hpp"
+#include "Domain/OrientationMapHelpers.hpp"
+#include "NumericalAlgorithms/DiscontinuousGalerkin/MortarHelpers.hpp"
+#include "NumericalAlgorithms/DiscontinuousGalerkin/SimpleBoundaryData.hpp"
+#include "NumericalAlgorithms/Spectral/Projection.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
+#include "tests/Unit/TestHelpers.hpp"
+
+namespace {
+
+struct SomeField {
+  using type = Scalar<DataVector>;
+};
+
+struct ExtraData {
+  using type = int;
+};
+
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.DG.SimpleBoundaryData", "[Unit][NumericalAlgorithms]") {
+  const size_t num_points = 5;
+  dg::SimpleBoundaryData<tmpl::list<SomeField>, tmpl::list<ExtraData>> data{
+      num_points};
+  const Scalar<DataVector> field{num_points, 1.};
+  get<SomeField>(data.field_data) = field;
+  get<ExtraData>(data.extra_data) = 2;
+
+  // Test serialization
+  data = serialize_and_deserialize(data);
+  CHECK(get<SomeField>(data.field_data) == field);
+  CHECK(get<ExtraData>(data.extra_data) == 2);
+
+  // Test projections
+  const Mesh<1> face_mesh{num_points, Spectral::Basis::Legendre,
+                          Spectral::Quadrature::GaussLobatto};
+  const Mesh<1> mortar_mesh{num_points + 1, Spectral::Basis::Legendre,
+                            Spectral::Quadrature::GaussLobatto};
+  const std::array<Spectral::MortarSize, 1> mortar_size{
+      {Spectral::MortarSize::UpperHalf}};
+  const auto projected_data =
+      data.project_to_mortar(face_mesh, mortar_mesh, mortar_size);
+  CHECK(projected_data.field_data ==
+        dg::project_to_mortar(data.field_data, face_mesh, mortar_mesh,
+                              mortar_size));
+  CHECK(projected_data.extra_data == data.extra_data);
+
+  // Test orientation
+  const size_t sliced_dim = 1;
+  const auto slice_extents = face_mesh.extents();
+  const OrientationMap<2> orientation_of_neighbor{
+      {{Direction<2>::lower_xi(), Direction<2>::lower_eta()}},
+      {{Direction<2>::upper_eta(), Direction<2>::lower_xi()}}};
+  auto oriented_data = data;
+  oriented_data.orient_on_slice(slice_extents, sliced_dim,
+                                orientation_of_neighbor);
+  CHECK(oriented_data.field_data ==
+        orient_variables_on_slice(data.field_data, slice_extents, sliced_dim,
+                                  orientation_of_neighbor));
+  CHECK(oriented_data.extra_data == data.extra_data);
+}
