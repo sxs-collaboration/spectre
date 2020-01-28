51ef7ad0b19de420d836d002e23e71d04bd60865
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-01-28 14:37:21 +0100

    Add Numpy interoperability to Matrix

--- src/DataStructures/Python/Matrix.cpp
+++ src/DataStructures/Python/Matrix.cpp
@@ -20,8 +20,38 @@ namespace py_bindings {
 
 void bind_matrix(py::module& m) {  // NOLINT
   // Wrapper for basic Matrix operations
-  py::class_<Matrix>(m, "Matrix")
+  py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
       .def(py::init<size_t, size_t>(), py::arg("rows"), py::arg("columns"))
+      .def(py::init([](py::buffer buffer) {
+             py::buffer_info info = buffer.request();
+             // Sanity-check the buffer
+             if (info.format != py::format_descriptor<double>::format()) {
+               throw std::runtime_error(
+                   "Incompatible format: expected a double array.");
+             }
+             if (info.ndim != 2) {
+               throw std::runtime_error("Incompatible dimension.");
+             }
+             const auto rows = static_cast<size_t>(info.shape[0]);
+             const auto columns = static_cast<size_t>(info.shape[1]);
+             auto data = static_cast<double*>(info.ptr);
+             return Matrix(rows, columns, data);
+           }),
+           py::arg("buffer"))
+      // Expose the data as a Python buffer so it can be cast into Numpy arrays
+      .def_buffer([](Matrix& matrix) {
+        return py::buffer_info(
+            matrix.data(),
+            // Size of one scalar
+            sizeof(double), py::format_descriptor<double>::format(),
+            // Number of dimensions
+            2,
+            // Size of the buffer
+            {matrix.rows(), matrix.columns()},
+            // Stride for each index (in bytes). Data is stored
+            // in column-major layout (see `Matrix.hpp`).
+            {sizeof(double), sizeof(double) * matrix.rows()});
+      })
       .def_property_readonly(
           "shape",
           +[](const Matrix& self) {
--- tests/Unit/DataStructures/Test_Matrix.py
+++ tests/Unit/DataStructures/Test_Matrix.py
@@ -39,19 +39,25 @@ class TestMatrix(unittest.TestCase):
 
         self.assertRaises(RuntimeError, assignment_test)
 
-    # Re-enabling this test in the next commit
-    # def test_to_numpy(self):
-    #     M = Matrix(2, 2)
-    #     M[0, 0] = 1.0
-    #     M[1, 0] = 2.7
-    #     M[0, 1] = 5.42
-    #     M[1, 1] = -2.3
-    #     A = M.to_numpy()
-    #     self.assertTrue(isinstance(A, np.ndarray))
-    #     self.assertEquals(A[0, 0], 1.0)
-    #     self.assertEquals(A[1, 0], 2.7)
-    #     self.assertEquals(A[0, 1], 5.42)
-    #     self.assertEquals(A[1, 1], -2.3)
+    def test_numpy_interoperability(self):
+        M = Matrix(2, 2)
+        M[0, 0] = 1.0
+        M[1, 0] = 2.7
+        M[0, 1] = 5.42
+        M[1, 1] = -2.3
+        # Convert Matrix to Numpy array
+        A = np.array(M)
+        self.assertTrue(isinstance(A, np.ndarray))
+        self.assertEquals(A[0, 0], 1.0)
+        self.assertEquals(A[1, 0], 2.7)
+        self.assertEquals(A[0, 1], 5.42)
+        self.assertEquals(A[1, 1], -2.3)
+        # Convert Numpy array to Matrix
+        M_from_array = Matrix(A)
+        self.assertEquals(M_from_array[0, 0], 1.0)
+        self.assertEquals(M_from_array[1, 0], 2.7)
+        self.assertEquals(M_from_array[0, 1], 5.42)
+        self.assertEquals(M_from_array[1, 1], -2.3)
 
 
 if __name__ == '__main__':
