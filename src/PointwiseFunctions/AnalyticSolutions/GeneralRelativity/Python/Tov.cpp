4da62782944121beed8619cb043ba4006f4b8243
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-01-28 16:33:00 +0100

    Convert PyGeneralRelativitySolutions to Pybind11

--- src/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/Python/Tov.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/Python/Tov.cpp
@@ -1,27 +1,26 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#include <boost/python.hpp>
+#include <pybind11/pybind11.h>
 #include <string>
 
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/Tov.hpp"
 #include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
 
-namespace bp = boost::python;
+namespace py = pybind11;
 
 namespace gr {
 namespace Solutions {
 namespace py_bindings {
 
-void bind_tov() {
-  bp::class_<TovSolution, boost::noncopyable>(
-      "Tov",
-      // boost::python receives no compile-time information on default
-      // arguments, so we don't expose them to Python for now. We could write
-      // thin wrappers for them if we needed to:
-      // https://www.boost.org/doc/libs/1_66_0/libs/python/doc/html/tutorial/tutorial/functions.html#tutorial.functions.default_arguments
-      bp::init<const EquationsOfState::EquationOfState<true, 1>&, double>(
-          (bp::arg("equation_of_state"), bp::arg("central_mass_density"))))
+void bind_tov(py::module& m) {  // NOLINT
+  py::class_<TovSolution>(m, "Tov")
+      .def(py::init<const EquationsOfState::EquationOfState<true, 1>&, double,
+                    double, double, double>(),
+           py::arg("equation_of_state"), py::arg("central_mass_density"),
+           py::arg("log_enthalpy_at_outer_radius") = 0.,
+           py::arg("absolute_tolerance") = 1.e-14,
+           py::arg("relative_tolerance") = 1.e-14)
       .def("outer_radius", &TovSolution::outer_radius)
       .def("mass_over_radius", &TovSolution::mass_over_radius)
       .def("mass", &TovSolution::mass)
