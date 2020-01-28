bb663e52345b764cf49b2f49797914c578c612b5
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-01-28 16:07:26 +0100

    Convert PyInformer to Pybind11

--- src/Informer/Python/InfoAtCompile.cpp
+++ src/Informer/Python/InfoAtCompile.cpp
@@ -1,23 +1,20 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#include <boost/python.hpp>
-#include <memory>
-#include <sstream>
+#include <pybind11/pybind11.h>
 #include <string>
-#include <utility>
 
 #include "Informer/InfoFromBuild.hpp"
-#include "Utilities/GetOutput.hpp"
-namespace bp = boost::python;
+
+namespace py = pybind11;
 
 namespace py_bindings {
-void bind_info_at_compile() {
+void bind_info_at_compile(py::module& m) {  // NOLINT
   // Wrapper to make Build Info available from python
-  bp::def("spectre_version", &spectre_version);
-  bp::def("spectre_major_version", &spectre_major_version);
-  bp::def("spectre_minor_version", &spectre_minor_version);
-  bp::def("spectre_patch_version", &spectre_patch_version);
-  bp::def("unit_test_path", &unit_test_path);
+  m.def("spectre_version", &spectre_version);
+  m.def("spectre_major_version", &spectre_major_version);
+  m.def("spectre_minor_version", &spectre_minor_version);
+  m.def("spectre_patch_version", &spectre_patch_version);
+  m.def("unit_test_path", &unit_test_path);
 }
 }  // namespace py_bindings
