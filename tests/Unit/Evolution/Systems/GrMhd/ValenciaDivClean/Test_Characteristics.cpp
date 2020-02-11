a107a80d61d8e7f37790738b053b5812c05c904a
Cristobal Armaza <ca455@cornell.edu>
2020-02-11 01:23:47 -0500

    Use test helpers in ValenciaDivClean

--- tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/Test_Characteristics.cpp
+++ tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/Test_Characteristics.cpp
@@ -4,23 +4,22 @@
 #include "tests/Unit/TestingFramework.hpp"
 
 #include <array>
-#include <random>
 
 #include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/DeterminantAndInverse.hpp"
 #include "DataStructures/Tensor/EagerMath/DotProduct.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "Domain/Direction.hpp"
 #include "Evolution/Systems/GrMhd/ValenciaDivClean/Characteristics.hpp"
 #include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
 #include "PointwiseFunctions/Hydro/EquationsOfState/PolytropicFluid.hpp"  // IWYU pragma: keep
-#include "PointwiseFunctions/Hydro/LorentzFactor.hpp"
 #include "Utilities/ConstantExpressions.hpp"
 #include "Utilities/Gsl.hpp"
 #include "tests/Unit/Domain/DomainTestHelpers.hpp"
+#include "tests/Unit/PointwiseFunctions/Hydro/TestHelpers.hpp"
 #include "tests/Unit/Pypp/Pypp.hpp"
 #include "tests/Unit/Pypp/SetupLocalPythonEnvironment.hpp"
 #include "tests/Unit/TestHelpers.hpp"
-#include "tests/Utilities/MakeWithRandomValues.hpp"
 
 // IWYU pragma: no_forward_declare EquationsOfState::EquationOfState
 // IWYU pragma: no_forward_declare Tensor
@@ -47,34 +46,28 @@ void test_characteristic_speeds(const DataVector& /*used_for_size*/) noexcept {
 void test_with_normal_along_coordinate_axes(
     const DataVector& used_for_size) noexcept {
   MAKE_GENERATOR(generator);
-  std::uniform_real_distribution<> distribution(0.0, 1.0);
-  std::uniform_real_distribution<> small_distribution(0.0, 0.1);
-
-  const auto nn_generator = make_not_null(&generator);
-  const auto nn_distribution = make_not_null(&distribution);
-
-  const auto rest_mass_density = make_with_random_values<Scalar<DataVector>>(
-      nn_generator, nn_distribution, used_for_size);
+  namespace helper = hydro::TestHelpers;
+  const auto nn_gen = make_not_null(&generator);
+  const auto rest_mass_density = helper::random_density(nn_gen, used_for_size);
   EquationsOfState::PolytropicFluid<true> eos(0.001, 4.0 / 3.0);
   const auto specific_internal_energy =
       eos.specific_internal_energy_from_density(rest_mass_density);
   const auto specific_enthalpy =
       eos.specific_enthalpy_from_density(rest_mass_density);
 
-  const auto lapse = make_with_random_values<Scalar<DataVector>>(
-      nn_generator, nn_distribution, used_for_size);
-  const auto shift = make_with_random_values<tnsr::I<DataVector, 3>>(
-      nn_generator, nn_distribution, used_for_size);
-  const auto spatial_velocity = make_with_random_values<tnsr::I<DataVector, 3>>(
-      nn_generator, make_not_null(&small_distribution), used_for_size);
-  const auto spatial_metric = make_with_random_values<tnsr::ii<DataVector, 3>>(
-      nn_generator, nn_distribution, used_for_size);
+  const auto lapse = helper::random_lapse(nn_gen, used_for_size);
+  const auto shift = helper::random_shift<3>(nn_gen, used_for_size);
+  const auto spatial_metric =
+      helper::random_spatial_metric<3>(nn_gen, used_for_size);
+  const auto lorentz_factor =
+      helper::random_lorentz_factor(nn_gen, used_for_size);
+  const auto spatial_velocity =
+      helper::random_velocity(nn_gen, lorentz_factor, spatial_metric);
   const auto spatial_velocity_squared =
       dot_product(spatial_velocity, spatial_velocity, spatial_metric);
-  const auto lorentz_factor = hydro::lorentz_factor(spatial_velocity_squared);
 
-  const auto magnetic_field = make_with_random_values<tnsr::I<DataVector, 3>>(
-      nn_generator, nn_distribution, used_for_size);
+  const auto magnetic_field = helper::random_magnetic_field(
+      nn_gen, eos.pressure_from_density(rest_mass_density), spatial_metric);
   const auto magnetic_field_squared =
       dot_product(magnetic_field, magnetic_field, spatial_metric);
   const auto magnetic_field_dot_spatial_velocity =
@@ -93,20 +86,20 @@ void test_with_normal_along_coordinate_axes(
       get(specific_enthalpy)};
 
   for (const auto& direction : Direction<3>::all_directions()) {
-    const auto normal = euclidean_basis_vector(direction, used_for_size);
+    const auto normal = unit_basis_form(
+        direction, determinant_and_inverse(spatial_metric).second);
 
     const auto& eos_base =
         static_cast<const EquationsOfState::EquationOfState<true, 1>&>(eos);
-    const auto computed = grmhd::ValenciaDivClean::characteristic_speeds(
-        rest_mass_density, specific_internal_energy, specific_enthalpy,
-        spatial_velocity, lorentz_factor, magnetic_field, lapse, shift,
-        spatial_metric, normal, eos_base);
-
-    const auto expected = pypp::call<std::array<DataVector, 9>>(
-        "TestFunctions", "characteristic_speeds", lapse, shift,
-        spatial_velocity, spatial_velocity_squared, sound_speed_squared,
-        alfven_speed_squared, normal);
-    CHECK_ITERABLE_APPROX(computed, expected);
+    CHECK_ITERABLE_APPROX(
+        grmhd::ValenciaDivClean::characteristic_speeds(
+            rest_mass_density, specific_internal_energy, specific_enthalpy,
+            spatial_velocity, lorentz_factor, magnetic_field, lapse, shift,
+            spatial_metric, normal, eos_base),
+        (pypp::call<std::array<DataVector, 9>>(
+            "TestFunctions", "characteristic_speeds", lapse, shift,
+            spatial_velocity, spatial_velocity_squared, sound_speed_squared,
+            alfven_speed_squared, normal)));
   }
 }
 
