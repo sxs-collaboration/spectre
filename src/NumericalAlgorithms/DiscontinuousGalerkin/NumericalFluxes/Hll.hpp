61c114d39eed73e24196a3169a6da5a9c5586943
Cristobal Armaza <ca455@cornell.edu>
2020-02-14 20:48:01 -0500

    Rework estimation of HLL signal speeds
    - rename Min/MaxSignalSpeed LargestIngoing/OutgoingSpeed; rename variables accordingly
    - remove name() function from simple tags
    - add comment explaining how signal speeds are packaged
    - add missing noexcept's
    - move comparison with 0.0 to final comparison between speeds on both sides of the mortar

--- src/NumericalAlgorithms/DiscontinuousGalerkin/NumericalFluxes/Hll.hpp
+++ src/NumericalAlgorithms/DiscontinuousGalerkin/NumericalFluxes/Hll.hpp
@@ -35,9 +35,9 @@ namespace NumericalFluxes {
  *
  * where "int" and "ext" stand for interior and exterior, respectively.
  * \f$S_\text{min}\f$ and \f$S_\text{max}\f$ are estimates on the minimum
- * and maximum signal velocities bounding the interior-moving and
- * exterior-moving wavespeeds that arise when solving the Riemann problem.
- * Here we use the simple estimates \cite Davis1988
+ * and maximum signal velocities bounding the ingoing and outgoing wavespeeds
+ * that arise when solving the Riemann problem. Here we use the simple
+ * estimates \cite Davis1988
  *
  * \f{align*}
  * S_\text{min} &=
@@ -58,24 +58,20 @@ struct Hll {
   using variables_tag = typename System::variables_tag;
 
  public:
-  /// The minimum signal velocity bounding
-  /// the wavespeeds on one side of the interface.
-  struct MinSignalSpeed : db::SimpleTag {
+  /// Estimate for the largest ingoing signal speed
+  struct LargestIngoingSpeed : db::SimpleTag {
     using type = Scalar<DataVector>;
-    static std::string name() noexcept { return "MinSignalSpeed"; }
   };
 
-  /// The maximum signal velocity bounding
-  /// the wavespeeds on one side of the interface.
-  struct MaxSignalSpeed : db::SimpleTag {
+  /// Estimate for the largest outgoing signal speed
+  struct LargestOutgoingSpeed : db::SimpleTag {
     using type = Scalar<DataVector>;
-    static std::string name() noexcept { return "MaxSignalSpeed"; }
   };
 
   using package_tags = tmpl::append<
       db::split_tag<db::add_tag_prefix<::Tags::NormalDotFlux, variables_tag>>,
       db::split_tag<variables_tag>,
-      tmpl::list<MinSignalSpeed, MaxSignalSpeed>>;
+      tmpl::list<LargestIngoingSpeed, LargestOutgoingSpeed>>;
 
   using argument_tags =
       tmpl::push_back<tmpl::append<db::split_tag<db::add_tag_prefix<
@@ -100,29 +96,32 @@ struct Hll {
       expand_pack(
           (get<NormalDotFluxTags>(*packaged_data) = n_dot_f_to_package)...);
 
-      get<MinSignalSpeed>(*packaged_data) = make_with_value<Scalar<DataVector>>(
-          characteristic_speeds[0],
-          std::numeric_limits<double>::signaling_NaN());
-      get<MaxSignalSpeed>(*packaged_data) = make_with_value<Scalar<DataVector>>(
-          characteristic_speeds[0],
-          std::numeric_limits<double>::signaling_NaN());
+      get<LargestIngoingSpeed>(*packaged_data) =
+          make_with_value<Scalar<DataVector>>(
+              characteristic_speeds[0],
+              std::numeric_limits<double>::signaling_NaN());
+      get<LargestOutgoingSpeed>(*packaged_data) =
+          make_with_value<Scalar<DataVector>>(
+              characteristic_speeds[0],
+              std::numeric_limits<double>::signaling_NaN());
 
-      // This finds the min and max characteristic speeds at each grid point,
-      // which are used as estimates of the min and max signal speeds.
+      // When packaging interior data, LargestIngoingSpeed and
+      // LargestOutgoingSpeed will hold the min and max char speeds,
+      // respectively. On the other hand, when packaging exterior data, the
+      // characteristic speeds will be computed along *minus* the exterior
+      // normal, so LargestIngoingSpeed will hold *minus* the max speed, while
+      // LargestOutgoingSpeed will store *minus* the min speed.
       for (size_t s = 0; s < characteristic_speeds[0].size(); ++s) {
-        // This ensures that local_min_signal_speed <= 0.0
-        const double local_min_signal_speed = (*std::min_element(
+        get(get<LargestIngoingSpeed>(*packaged_data))[s] = (*std::min_element(
             characteristic_speeds.begin(), characteristic_speeds.end(),
-            [&s](const auto& a, const auto& b) { return a[s] < b[s]; }))[s];
-        get(get<MinSignalSpeed>(*packaged_data))[s] =
-            std::min(local_min_signal_speed, 0.0);
-
-        // Likewise, local_max_signal_speed >= 0.0
-        const double local_max_signal_speed = (*std::max_element(
+            [&s](const DataVector& a, const DataVector& b) noexcept {
+              return a[s] < b[s];
+            }))[s];
+        get(get<LargestOutgoingSpeed>(*packaged_data))[s] = (*std::max_element(
             characteristic_speeds.begin(), characteristic_speeds.end(),
-            [&s](const auto& a, const auto& b) { return a[s] < b[s]; }))[s];
-        get(get<MaxSignalSpeed>(*packaged_data))[s] =
-            std::max(local_max_signal_speed, 0.0);
+            [&s](const DataVector& a, const DataVector& b) noexcept {
+              return a[s] < b[s];
+            }))[s];
       }
     }
   };
@@ -140,46 +139,56 @@ struct Hll {
             db::item_type<NormalDotNumericalFluxTags>*>... n_dot_numerical_f,
         const db::const_item_type<NormalDotFluxTags>&... n_dot_f_interior,
         const db::const_item_type<VariablesTags>&... u_interior,
-        const db::const_item_type<MinSignalSpeed>& min_signal_speed_interior,
-        const db::const_item_type<MaxSignalSpeed>& max_signal_speed_interior,
+        const db::const_item_type<LargestIngoingSpeed>&
+            largest_ingoing_speed_interior,
+        const db::const_item_type<LargestOutgoingSpeed>&
+            largest_outgoing_speed_interior,
         const db::const_item_type<NormalDotFluxTags>&... minus_n_dot_f_exterior,
         const db::const_item_type<VariablesTags>&... u_exterior,
-        const db::const_item_type<MinSignalSpeed>& min_signal_speed_exterior,
-        const db::const_item_type<MaxSignalSpeed>&
-            max_signal_speed_exterior) noexcept {
-      auto min_signal_speed =
-          make_with_value<db::const_item_type<MinSignalSpeed>>(
-              min_signal_speed_interior,
+        // names are inverted w.r.t. interior data. See package_data()
+        const db::const_item_type<LargestIngoingSpeed>&
+            minus_largest_outgoing_speed_exterior,
+        const db::const_item_type<LargestOutgoingSpeed>&
+            minus_largest_ingoing_speed_exterior) noexcept {
+      auto largest_ingoing_speed =
+          make_with_value<db::const_item_type<LargestIngoingSpeed>>(
+              largest_ingoing_speed_interior,
               std::numeric_limits<double>::signaling_NaN());
-      auto max_signal_speed =
-          make_with_value<db::const_item_type<MaxSignalSpeed>>(
-              max_signal_speed_interior,
+      auto largest_outgoing_speed =
+          make_with_value<db::const_item_type<LargestOutgoingSpeed>>(
+              largest_outgoing_speed_interior,
               std::numeric_limits<double>::signaling_NaN());
-      for (size_t s = 0; s < min_signal_speed.begin()->size(); ++s) {
-        get(min_signal_speed)[s] = std::min(get(min_signal_speed_interior)[s],
-                                            -get(max_signal_speed_exterior)[s]);
-        get(max_signal_speed)[s] = std::max(get(max_signal_speed_interior)[s],
-                                            -get(min_signal_speed_exterior)[s]);
+      for (size_t s = 0; s < largest_ingoing_speed.begin()->size(); ++s) {
+        get(largest_ingoing_speed)[s] =
+            std::min({get(largest_ingoing_speed_interior)[s],
+                      -get(minus_largest_ingoing_speed_exterior)[s], 0.0});
+        get(largest_outgoing_speed)[s] =
+            std::max({get(largest_outgoing_speed_interior)[s],
+                      -get(minus_largest_outgoing_speed_exterior)[s], 0.0});
       }
-      ASSERT(min(get(max_signal_speed) - get(min_signal_speed)) > 0.0,
-             "Max, min speeds are the same:\n"
-             "  max_signal_speed = "
-                 << get(max_signal_speed)
-                 << "\n"
-                    "  min_signal_speed = "
-                 << get(min_signal_speed));
+      ASSERT(
+          min(get(largest_outgoing_speed) - get(largest_ingoing_speed)) > 0.0,
+          "Max, min speeds are the same:\n"
+          "  largest_outgoing_speed = "
+              << get(largest_outgoing_speed)
+              << "\n"
+                 "  largest_ingoing_speed = "
+              << get(largest_ingoing_speed));
       const DataVector one_over_sp_minus_sm =
-          1.0 / (get(max_signal_speed) - get(min_signal_speed));
+          1.0 / (get(largest_outgoing_speed) - get(largest_ingoing_speed));
       const auto assemble_numerical_flux =
-          [&min_signal_speed, &max_signal_speed, &one_over_sp_minus_sm ](
-              const auto n_dot_num_f, const auto& n_dot_f_in, const auto& u_in,
-              const auto& minus_n_dot_f_ex, const auto& u_ex) noexcept {
+          [
+            &largest_ingoing_speed, &largest_outgoing_speed,
+            &one_over_sp_minus_sm
+          ](const auto n_dot_num_f, const auto& n_dot_f_in, const auto& u_in,
+            const auto& minus_n_dot_f_ex, const auto& u_ex) noexcept {
         for (size_t i = 0; i < n_dot_num_f->size(); ++i) {
-          (*n_dot_num_f)[i] = one_over_sp_minus_sm *
-                              (get(max_signal_speed) * n_dot_f_in[i] +
-                               get(min_signal_speed) * minus_n_dot_f_ex[i] -
-                               get(max_signal_speed) * get(min_signal_speed) *
-                                   (u_in[i] - u_ex[i]));
+          (*n_dot_num_f)[i] =
+              one_over_sp_minus_sm *
+              (get(largest_outgoing_speed) * n_dot_f_in[i] +
+               get(largest_ingoing_speed) * minus_n_dot_f_ex[i] -
+               get(largest_outgoing_speed) * get(largest_ingoing_speed) *
+                   (u_in[i] - u_ex[i]));
         }
         return nullptr;
       };
