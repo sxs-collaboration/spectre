2ad8a97a1527d0aa9823bab309df567ecd4f5f5f
Lawrence Kidder <kidder@astro.cornell.edu>
2020-02-16 22:22:27 -0500

    Fix code in Benchmark.cpp

--- src/Executables/Benchmark/Benchmark.cpp
+++ src/Executables/Benchmark/Benchmark.cpp
@@ -86,11 +86,12 @@ void bench_all_gradient(benchmark::State& state) {  // NOLINT
   constexpr const size_t Dim = 3;
   const Mesh<Dim> mesh{pts_1d, Spectral::Basis::Legendre,
                        Spectral::Quadrature::GaussLobatto};
-  CoordinateMaps::Affine map1d(-1.0, 1.0, -1.0, 1.0);
-  using Map3d = CoordinateMaps::ProductOf3Maps<CoordinateMaps::Affine,
-                                               CoordinateMaps::Affine,
-                                               CoordinateMaps::Affine>;
-  CoordinateMap<Frame::Logical, Frame::Grid, Map3d> map(
+  domain::CoordinateMaps::Affine map1d(-1.0, 1.0, -1.0, 1.0);
+  using Map3d =
+      domain::CoordinateMaps::ProductOf3Maps<domain::CoordinateMaps::Affine,
+                                             domain::CoordinateMaps::Affine,
+                                             domain::CoordinateMaps::Affine>;
+  domain::CoordinateMap<Frame::Logical, Frame::Grid, Map3d> map(
       Map3d{map1d, map1d, map1d});
 
   using VarTags = tmpl::list<Kappa<Dim>, Psi<Dim>>;
