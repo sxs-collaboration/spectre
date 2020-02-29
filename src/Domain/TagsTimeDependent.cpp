e10dc9fb997bebdadd1ed54220e023810781b260
Nils Deppe <nd357@cornell.edu>
2020-02-29 16:22:23 -0500

    Add tags for moving mesh

---
+++ src/Domain/TagsTimeDependent.cpp
@@ -0,0 +1,128 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Domain/TagsTimeDependent.hpp"
+
+#include <memory>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/Domain.hpp"
+#include "NumericalAlgorithms/LinearOperators/Divergence.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace domain {
+namespace Tags {
+template <size_t Dim>
+void InertialFromGridCoordinatesCompute<Dim>::function(
+    const gsl::not_null<tnsr::I<DataVector, Dim, Frame::Inertial>*>
+        target_coords,
+    const tnsr::I<DataVector, Dim, Frame::Grid>& source_coords,
+    const boost::optional<std::tuple<
+        tnsr::I<DataVector, Dim, Frame::Inertial>,
+        ::InverseJacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+        ::Jacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+        tnsr::I<DataVector, Dim, Frame::Inertial>>>&
+        grid_to_inertial_quantities) noexcept {
+  if (not grid_to_inertial_quantities) {
+    // We use a const_cast to point the data into the existing allocation
+    // inside `source_coords` to avoid copying. This is safe because the output
+    // of a compute tag is immutable.
+    for (size_t i = 0; i < Dim; ++i) {
+      target_coords->get(i).set_data_ref(
+          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+          &const_cast<DataVector&>(source_coords.get(i)));
+    }
+  } else {
+    // We use a const_cast to point the data into the existing allocation
+    // inside `grid_to_inertial_quantities` to avoid copying. This is
+    // effectively unpacking the tuple. This is safe because the output
+    // of a compute tag is immutable.
+    for (size_t i = 0; i < Dim; ++i) {
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+      target_coords->get(i).set_data_ref(&const_cast<DataVector&>(
+          std::get<0>(*grid_to_inertial_quantities).get(i)));
+    }
+  }
+}
+
+template <size_t Dim>
+void ElementToInertialInverseJacobian<Dim>::function(
+    const gsl::not_null<
+        ::InverseJacobian<DataVector, Dim, Frame::Logical, Frame::Inertial>*>
+        inv_jac_logical_to_inertial,
+    const ::InverseJacobian<DataVector, Dim, Frame::Logical, Frame::Grid>&
+        inv_jac_logical_to_grid,
+    const boost::optional<std::tuple<
+        tnsr::I<DataVector, Dim, Frame::Inertial>,
+        ::InverseJacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+        ::Jacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+        tnsr::I<DataVector, Dim, Frame::Inertial>>>&
+        grid_to_inertial_quantities) noexcept {
+  if (not grid_to_inertial_quantities) {
+    // We use a const_cast to point the data into the existing allocation
+    // inside `inv_jac_logical_to_grid` to avoid copying. This is safe because
+    // the output of a compute tag is immutable.
+    for (size_t i = 0; i < inv_jac_logical_to_inertial->size(); ++i) {
+      inv_jac_logical_to_inertial->operator[](i).set_data_ref(
+          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+          &const_cast<DataVector&>(inv_jac_logical_to_grid[i]));
+    }
+  } else {
+    const auto& inv_jac_grid_to_inertial =
+        std::get<1>(*grid_to_inertial_quantities);
+    for (size_t logical_i = 0; logical_i < Dim; ++logical_i) {
+      for (size_t inertial_i = 0; inertial_i < Dim; ++inertial_i) {
+        inv_jac_logical_to_inertial->get(logical_i, inertial_i) =
+            inv_jac_logical_to_grid.get(logical_i, 0) *
+            inv_jac_grid_to_inertial.get(0, inertial_i);
+        for (size_t grid_i = 1; grid_i < Dim; ++grid_i) {
+          inv_jac_logical_to_inertial->get(logical_i, inertial_i) +=
+              inv_jac_logical_to_grid.get(logical_i, grid_i) *
+              inv_jac_grid_to_inertial.get(grid_i, inertial_i);
+        }
+      }
+    }
+  }
+}
+
+template <size_t Dim>
+void InertialMeshVelocityCompute<Dim>::function(
+    const gsl::not_null<return_type*> mesh_velocity,
+    const boost::optional<std::tuple<
+        tnsr::I<DataVector, Dim, Frame::Inertial>,
+        ::InverseJacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+        ::Jacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+        tnsr::I<DataVector, Dim, Frame::Inertial>>>&
+        grid_to_inertial_quantities) noexcept {
+  if (not grid_to_inertial_quantities) {
+    *mesh_velocity = boost::none;
+  } else {
+    if (not*mesh_velocity) {
+      *mesh_velocity = typename return_type::value_type{};
+    }
+    // We use a const_cast to point the data into the existing allocation
+    // inside `grid_to_inertial_quantities` to avoid copying. This is
+    // effectively unpacking the tuple. This is safe because the output
+    // of a compute tag is immutable.
+    for (size_t i = 0; i < Dim; ++i) {
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+      mesh_velocity->get().operator[](i).set_data_ref(&const_cast<DataVector&>(
+          std::get<3>(*grid_to_inertial_quantities).get(i)));
+    }
+  }
+}
+
+#define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATE(_, data)                                     \
+  template struct InertialFromGridCoordinatesCompute<DIM(data)>; \
+  template struct ElementToInertialInverseJacobian<DIM(data)>;   \
+  template struct InertialMeshVelocityCompute<DIM(data)>;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3))
+
+#undef INSTANTIATE
+#undef DIM
+}  // namespace Tags
+}  // namespace domain
---
+++ src/Domain/TagsTimeDependent.hpp
@@ -0,0 +1,164 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <boost/optional.hpp>
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"  // For Tags::Normalized
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.hpp"
+#include "Domain/FaceNormal.hpp"
+#include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
+#include "Domain/FunctionsOfTime/Tags.hpp"
+#include "Domain/Tags.hpp"
+#include "Time/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace domain {
+/// \ingroup ComputationalDomainGroup
+/// \brief %Tags for the domain.
+namespace Tags {
+/// The Inertial coordinates, the inverse Jacobian from the Grid to the Inertial
+/// frame, the Jacobian from the Grid to the Inertial frame, and the Inertial
+/// mesh velocity.
+///
+/// The type is a `boost::optional`, which, when is not valid, signals that the
+/// mesh is not moving. Thus,
+/// `static_cast<bool>(coordinates_velocity_and_jacobian)` can be used to check
+/// if the mesh is moving.
+template <size_t Dim>
+struct CoordinatesMeshVelocityAndJacobians : db::SimpleTag {
+  using type = boost::optional<std::tuple<
+      tnsr::I<DataVector, Dim, Frame::Inertial>,
+      ::InverseJacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+      ::Jacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+      tnsr::I<DataVector, Dim, Frame::Inertial>>>;
+};
+
+/// Computes the Inertial coordinates, the inverse Jacobian from the Grid to the
+/// Inertial frame, the Jacobian from the Grid to the Inertial frame, and the
+/// Inertial mesh velocity.
+template <typename MapTagGridToInertial>
+struct CoordinatesMeshVelocityAndJacobiansCompute
+    : CoordinatesMeshVelocityAndJacobians<MapTagGridToInertial::dim>,
+      db::ComputeTag {
+  static constexpr size_t dim = MapTagGridToInertial::dim;
+  using base = CoordinatesMeshVelocityAndJacobians<dim>;
+
+  using return_type = typename base::type;
+
+  static void function(
+      const gsl::not_null<return_type*> result,
+      const domain::CoordinateMapBase<Frame::Grid, Frame::Inertial, dim>&
+          grid_to_inertial_map,
+      const tnsr::I<DataVector, dim, Frame::Grid>& source_coords,
+      const double time,
+      const std::unordered_map<
+          std::string,
+          std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>&
+          functions_of_time) noexcept {
+    // Use identity to signal time-independent
+    if (not grid_to_inertial_map.is_identity()) {
+      *result = grid_to_inertial_map.coords_frame_velocity_jacobians(
+          source_coords, time, functions_of_time);
+    } else {
+      *result = boost::none;
+    }
+  }
+
+  using argument_tags =
+      tmpl::list<MapTagGridToInertial, Tags::Coordinates<dim, Frame::Grid>,
+                 ::Tags::Time, Tags::FunctionsOfTime>;
+};
+
+/// Computes the Inertial coordinates from
+/// `CoordinatesVelocityAndJacobians`
+template <size_t Dim>
+struct InertialFromGridCoordinatesCompute
+    : Tags::Coordinates<Dim, Frame::Inertial>,
+      db::ComputeTag {
+  using base = Tags::Coordinates<Dim, Frame::Inertial>;
+  using return_type = typename base::type;
+
+  static void function(
+      gsl::not_null<tnsr::I<DataVector, Dim, Frame::Inertial>*> target_coords,
+      const tnsr::I<DataVector, Dim, Frame::Grid>& source_coords,
+      const boost::optional<std::tuple<
+          tnsr::I<DataVector, Dim, Frame::Inertial>,
+          ::InverseJacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+          ::Jacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+          tnsr::I<DataVector, Dim, Frame::Inertial>>>&
+          grid_to_inertial_quantities) noexcept;
+
+  using argument_tags = tmpl::list<Tags::Coordinates<Dim, Frame::Grid>,
+                                   CoordinatesMeshVelocityAndJacobians<Dim>>;
+};
+
+/// Computes the Logical to Inertial inverse Jacobian from
+/// `CoordinatesVelocityAndJacobians`
+template <size_t Dim>
+struct ElementToInertialInverseJacobian
+    : Tags::InverseJacobian<Dim, Frame::Logical, Frame::Inertial>,
+      db::ComputeTag {
+  using base = Tags::InverseJacobian<Dim, Frame::Logical, Frame::Inertial>;
+  using return_type = typename base::type;
+
+  static void function(
+      gsl::not_null<
+          ::InverseJacobian<DataVector, Dim, Frame::Logical, Frame::Inertial>*>
+          inv_jac_logical_to_inertial,
+      const ::InverseJacobian<DataVector, Dim, Frame::Logical, Frame::Grid>&
+          inv_jac_logical_to_grid,
+      const boost::optional<std::tuple<
+          tnsr::I<DataVector, Dim, Frame::Inertial>,
+          ::InverseJacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+          ::Jacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+          tnsr::I<DataVector, Dim, Frame::Inertial>>>&
+          grid_to_inertial_quantities) noexcept;
+
+  using argument_tags =
+      tmpl::list<Tags::InverseJacobian<Dim, Frame::Logical, Frame::Grid>,
+                 CoordinatesMeshVelocityAndJacobians<Dim>>;
+};
+
+/// The mesh velocity
+///
+/// The type is a `boost::optional`, which when it is not set indicates that the
+/// mesh is not moving.
+template <size_t Dim, typename Frame = ::Frame::Inertial>
+struct MeshVelocity : db::SimpleTag {
+  using type = boost::optional<tnsr::I<DataVector, Dim, Frame>>;
+};
+
+/// Computes the Inertial mesh velocity from `CoordinatesVelocityAndJacobians`
+///
+/// The type is a `boost::optional`, which when it is not set indicates that the
+/// mesh is not moving.
+template <size_t Dim>
+struct InertialMeshVelocityCompute : MeshVelocity<Dim, Frame::Inertial>,
+                                     db::ComputeTag {
+  using base = MeshVelocity<Dim, Frame::Inertial>;
+  using return_type = typename base::type;
+
+  static void function(
+      gsl::not_null<return_type*> mesh_velocity,
+      const boost::optional<std::tuple<
+          tnsr::I<DataVector, Dim, Frame::Inertial>,
+          ::InverseJacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+          ::Jacobian<DataVector, Dim, Frame::Grid, Frame::Inertial>,
+          tnsr::I<DataVector, Dim, Frame::Inertial>>>&
+          grid_to_inertial_quantities) noexcept;
+
+  using argument_tags = tmpl::list<CoordinatesMeshVelocityAndJacobians<Dim>>;
+};
+}  // namespace Tags
+}  // namespace domain
