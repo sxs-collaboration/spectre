49dda59edeb971bd014d5101504c1d60219477e3
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-03-04 14:47:19 +0100

    Add imported interface library for YamlCpp

--- cmake/SetupYamlCpp.cmake
+++ cmake/SetupYamlCpp.cmake
@@ -2,8 +2,19 @@
 # See LICENSE.txt for details.
 
 find_package(YAMLCPP REQUIRED)
-spectre_include_directories(${YAMLCPP_INCLUDE_DIRS})
-list(APPEND SPECTRE_LIBRARIES ${YAMLCPP_LIBRARIES})
 
 message(STATUS "yaml-cpp libs: " ${YAMLCPP_LIBRARIES})
 message(STATUS "yaml-cpp incl: " ${YAMLCPP_INCLUDE_DIRS})
+
+add_library(YamlCpp INTERFACE IMPORTED)
+# Have to use `set_property` before CMake version 3.11. See:
+# https://gitlab.kitware.com/cmake/cmake/-/merge_requests/1264
+set_property(TARGET YamlCpp
+  APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${YAMLCPP_INCLUDE_DIRS})
+set_property(TARGET YamlCpp
+  APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${YAMLCPP_LIBRARIES})
+
+# Add to catch-all lists of dependencies. Can be removed once all targets that
+# depend on yaml-cpp link with the `Options` library or with `YamlCpp` directly.
+spectre_include_directories(${YAMLCPP_INCLUDE_DIRS})
+list(APPEND SPECTRE_LIBRARIES ${YAMLCPP_LIBRARIES})
