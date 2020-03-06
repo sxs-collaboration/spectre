a0502c909d882629aff72a100273858cf3f24ae5
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-03-06 10:23:51 +0100

    Add function to package data for the first-order scheme

--- src/NumericalAlgorithms/DiscontinuousGalerkin/BoundarySchemes/FirstOrder/BoundaryData.hpp
+++ src/NumericalAlgorithms/DiscontinuousGalerkin/BoundarySchemes/FirstOrder/BoundaryData.hpp
@@ -5,9 +5,13 @@
 
 #include "DataStructures/DataBox/DataBoxTag.hpp"
 #include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/Variables.hpp"
+#include "Domain/Mesh.hpp"
+#include "NumericalAlgorithms/DiscontinuousGalerkin/NumericalFluxes/NumericalFluxHelpers.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/Protocols.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/SimpleBoundaryData.hpp"
 #include "Utilities/ProtocolHelpers.hpp"
+#include "Utilities/Requires.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace dg {
@@ -41,5 +45,35 @@ struct BoundaryDataImpl {
  */
 template <typename NumericalFluxType>
 using BoundaryData = typename detail::BoundaryDataImpl<NumericalFluxType>::type;
+
+/*!
+ * \brief Package the data on element boundaries that's needed for the (strong)
+ * first-order boundary scheme.
+ *
+ * This function currently packages the data required by the `NumericalFluxType`
+ * plus all "normal-dot-fluxes", which are needed for the strong first-order
+ * boundary scheme (see `dg::FirstOrderScheme::BoundaryData`). Note that for
+ * the weak formulation the normal-dot-fluxes need not be included explicitly,
+ * but the `NumericalFluxType` may require a subset of them.
+ */
+template <
+    size_t FaceDim, typename NumericalFluxType, typename... NumericalFluxArgs,
+    Requires<tt::conforms_to_v<NumericalFluxType, protocols::NumericalFlux>> =
+        nullptr>
+auto package_boundary_data(
+    const NumericalFluxType& numerical_flux_computer,
+    const Mesh<FaceDim>& face_mesh,
+    const Variables<db::wrap_tags_in<
+        ::Tags::NormalDotFlux, typename NumericalFluxType::variables_tags>>&
+        normal_dot_fluxes,
+    const NumericalFluxArgs&... args) noexcept {
+  BoundaryData<NumericalFluxType> boundary_data{
+      face_mesh.number_of_grid_points()};
+  boundary_data.field_data.assign_subset(normal_dot_fluxes);
+  dg::NumericalFluxes::package_data(make_not_null(&boundary_data),
+                                    numerical_flux_computer, args...);
+  return boundary_data;
+}
+
 }  // namespace FirstOrderScheme
 }  // namespace dg
