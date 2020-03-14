afe66c859148b5a1176975c4bbdeb66a9151c4dd
Nils Deppe <nd357@cornell.edu>
2020-03-14 14:44:46 -0400

    Add dox for evolution::dg::(Actions|Initialization)

--- src/Evolution/Evolution.hpp
+++ src/Evolution/Evolution.hpp
@@ -2,9 +2,21 @@
 // See LICENSE.txt for details.
 
 /// \file
-/// Documents the `evolution` namespace
+/// Documents the `evolution`, `evolution::dg`, `evolution::dg::Actions`, and
+/// `evolution::dg::Initialization` namespaces
 
 #pragma once
 
 /// Functionality for evolving hyperbolic partial differential equations.
-namespace evolution {}
+namespace evolution {
+/// Functionality for evolving hyperbolic partial differential equations using
+/// the discontinuous Galerkin method.
+namespace dg {
+/// %Actions for using the discontinuous Galerkin to evolve hyperbolic partial
+/// differential equations.
+namespace Actions {}
+/// Functionality for initializing the discontinuous Galerkin to evolve
+/// hyperbolic partial differential equations.
+namespace Initialization {}
+}  // namespace dg
+}  // namespace evolution
