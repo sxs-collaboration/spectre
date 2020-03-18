e63c4aea85890c31b61e93f163072ee0ee0fc2f8
Mark Scheel <scheel@tapir.caltech.edu>
2020-03-18 11:23:56 -0700

    Refactor InterpolationTargetReceiveVars.
    Only some of the code in InterpolationTargetReceiveVars will be
    used when bypassing an Interpolator parallel component.
    
    Add comments as well.

--- src/NumericalAlgorithms/Interpolation/InterpolationTargetReceiveVars.hpp
+++ src/NumericalAlgorithms/Interpolation/InterpolationTargetReceiveVars.hpp
@@ -136,16 +136,24 @@ void fill_invalid_points(const gsl::not_null<db::DataBox<DbTags>*> box,
   }
 }
 
-/// Calls the callback function, tells interpolators to clean up the current
-/// temporal_id, and then if there are more temporal_ids to be interpolated,
-/// starts the next one.
+/// Wraps calling the callback function on an InterpolationTarget.
+///
+/// First prepares for the callback, then calls the callback,
+/// and returns true if the callback is done and
+/// false if the callback is not done (e.g. if the callback is an
+/// apparent horizon search and it needs another iteration).
+///
+/// call_callback is called by an Action of InterpolationTarget.
+///
+/// Currently two Actions call call_callback:
+/// - InterpolationTargetReceiveVars (called by Interpolator ParallelComponent)
+/// - InterpolationTargetVarsFromElement (called by DgElementArray)
 template <typename InterpolationTargetTag, typename DbTags,
           typename Metavariables, typename TemporalId>
-void callback_and_cleanup(
+bool call_callback(
     const gsl::not_null<db::DataBox<DbTags>*> box,
     const gsl::not_null<Parallel::ConstGlobalCache<Metavariables>*> cache,
     const TemporalId& temporal_id) noexcept {
-
   // Before doing anything else, deal with the possibility that some
   // of the points might be outside of the Domain.
   fill_invalid_points<InterpolationTargetTag>(box, temporal_id);
@@ -172,23 +180,30 @@ void callback_and_cleanup(
   // apply_callback should return true if we are done with this
   // temporal_id.  It should return false only if the callback
   // calls another `intrp::Action` that needs the volume data at this
-  // same temporal_id.  If it returns false, we exit here and do not
-  // clean up.
-  const bool done_with_temporal_id =
-      apply_callback<InterpolationTargetTag>(box, cache, temporal_id);
+  // same temporal_id.
+  return apply_callback<InterpolationTargetTag>(box, cache, temporal_id);
+}
 
-  if (not done_with_temporal_id) {
-    return;
-  }
-
+/// Frees InterpolationTarget's memory associated with the supplied
+/// temporal_id.
+///
+/// clean_up_interpolation_target is called by an Action of InterpolationTarget.
+///
+/// Currently two Actions call clean_up_interpolation_target:
+/// - InterpolationTargetReceiveVars (called by Interpolator ParallelComponent)
+/// - InterpolationTargetVarsFromElement (called by DgElementArray)
+template <typename InterpolationTargetTag, typename DbTags, typename TemporalId>
+void clean_up_interpolation_target(
+    const gsl::not_null<db::DataBox<DbTags>*> box,
+    const TemporalId& temporal_id) noexcept {
   // We are now done with this temporal_id, so we can pop it and
-  // clean up volume data associated with it.
+  // clean up data associated with it.
   db::mutate<Tags::TemporalIds<TemporalId>,
              Tags::CompletedTemporalIds<TemporalId>>(
-      box, [](const gsl::not_null<std::deque<TemporalId>*> ids,
-              const gsl::not_null<std::deque<TemporalId>*>
-                  completed_ids) noexcept {
-        completed_ids->push_back(ids->front());
+      box, [&temporal_id](const gsl::not_null<std::deque<TemporalId>*> ids,
+                          const gsl::not_null<std::deque<TemporalId>*>
+                              completed_ids) noexcept {
+        completed_ids->push_back(temporal_id);
         ids->pop_front();
         // We want to keep track of all completed temporal_ids to deal with
         // the possibility of late calls to
@@ -201,28 +216,105 @@ void callback_and_cleanup(
           completed_ids->pop_front();
         }
       });
-
-  // Tell interpolators to clean up at this temporal_id for this
-  // InterpolationTargetTag.
-  auto& interpolator_proxy =
-      Parallel::get_parallel_component<Interpolator<Metavariables>>(*cache);
-  Parallel::simple_action<Actions::CleanUpInterpolator<InterpolationTargetTag>>(
-      interpolator_proxy, temporal_id);
-
-  // If we have a sequential target, and there are further
-  // temporal_ids, begin interpolation for the next one.
-  if (InterpolationTargetTag::compute_target_points::is_sequential::value) {
-    const auto& temporal_ids = db::get<Tags::TemporalIds<TemporalId>>(*box);
-    if (not temporal_ids.empty()) {
-      auto& my_proxy = Parallel::get_parallel_component<
-          InterpolationTarget<Metavariables, InterpolationTargetTag>>(*cache);
-      Parallel::simple_action<
-          typename InterpolationTargetTag::compute_target_points>(
-          my_proxy, temporal_ids.front());
-    }
-  }
 }
 
+/// Returns true if this InterpolationTarget has received data
+/// at all its points.
+///
+/// have_data_at_all_points is called by an Action of InterpolationTarget.
+///
+/// Currently two Actions call have_data_at_all_points:
+/// - InterpolationTargetReceiveVars (called by Interpolator ParallelComponent)
+/// - InterpolationTargetVarsFromElement (called by DgElementArray)
+template <typename InterpolationTargetTag, typename DbTags, typename TemporalId>
+bool have_data_at_all_points(const db::DataBox<DbTags>& box,
+                             const TemporalId& temporal_id) noexcept {
+  const size_t filled_size =
+      db::get<Tags::IndicesOfFilledInterpPoints<TemporalId>>(box)
+          .at(temporal_id)
+          .size();
+  const size_t invalid_size = [&box, &temporal_id ]() noexcept {
+    const auto& invalid_indices =
+        db::get<Tags::IndicesOfInvalidInterpPoints<TemporalId>>(box);
+    if (invalid_indices.count(temporal_id) > 0) {
+      return invalid_indices.at(temporal_id).size();
+    }
+    return 0_st;
+  }
+  ();
+  const size_t interp_size =
+      db::get<Tags::InterpolatedVars<InterpolationTargetTag, TemporalId>>(box)
+          .at(temporal_id)
+          .number_of_grid_points();
+  return (invalid_size + filled_size == interp_size);
+}
+
+/// Adds the supplied interpolated variables and offsets to the
+/// InterpolationTarget's internal DataBox.
+///
+/// Note that the template argument to Variables in vars_src is called
+/// InterpolationTargetTag::vars_to_interpolate_to_target.  This is a list
+/// of tags, and is used for both the interpolated variables (in
+/// this function add_received_variables) and for the source variables
+/// (in other functions). The source and interpolated quantities are
+/// the same set of variables (but at different points).
+///
+/// add_received_variables is called by an Action of InterpolationTarget.
+///
+/// Currently two Actions call add_received_variables:
+/// - InterpolationTargetReceiveVars (called by Interpolator ParallelComponent)
+/// - InterpolationTargetVarsFromElement (called by DgElementArray)
+template <typename InterpolationTargetTag, typename DbTags, typename TemporalId>
+void add_received_variables(
+    const gsl::not_null<db::DataBox<DbTags>*> box,
+    const std::vector<Variables<
+        typename InterpolationTargetTag::vars_to_interpolate_to_target>>&
+        vars_src,
+    const std::vector<std::vector<size_t>>& global_offsets,
+    const TemporalId& temporal_id) noexcept {
+  db::mutate<Tags::IndicesOfFilledInterpPoints<TemporalId>,
+             Tags::InterpolatedVars<InterpolationTargetTag, TemporalId>>(
+      box, [&temporal_id, &vars_src, &global_offsets ](
+               const gsl::not_null<
+                   std::unordered_map<TemporalId, std::unordered_set<size_t>>*>
+                   indices_of_filled,
+               const gsl::not_null<std::unordered_map<
+                   TemporalId, Variables<typename InterpolationTargetTag::
+                                             vars_to_interpolate_to_target>>*>
+                   vars_dest_all_times) noexcept {
+        auto& vars_dest = vars_dest_all_times->at(temporal_id);
+        // Here we assume that vars_dest has been allocated to the correct
+        // size (but could contain garbage, since below we are filling it).
+        const size_t npts_dest = vars_dest.number_of_grid_points();
+        const size_t nvars = vars_dest.number_of_independent_components;
+        for (size_t j = 0; j < global_offsets.size(); ++j) {
+          const size_t npts_src = global_offsets[j].size();
+          for (size_t i = 0; i < npts_src; ++i) {
+            // If a point is on the boundary of two (or more)
+            // elements, it is possible that we have received data
+            // for this point from more than one Interpolator.
+            // This will rarely occur, but it does occur, e.g. when
+            // a point is exactly on some symmetry
+            // boundary (such as the x-y plane) and this symmetry
+            // boundary is exactly the boundary between two
+            // elements.  If this happens, we accept the first
+            // duplicated point, and we ignore subsequent
+            // duplicated points.  The points are easy to keep track
+            // of because global_offsets uniquely identifies them.
+            if ((*indices_of_filled)[temporal_id]
+                    .insert(global_offsets[j][i])
+                    .second) {
+              for (size_t v = 0; v < nvars; ++v) {
+                // clang-tidy: no pointer arithmetic
+                vars_dest.data()[global_offsets[j][i] +    // NOLINT
+                                 v * npts_dest] =          // NOLINT
+                    vars_src[j].data()[i + v * npts_src];  // NOLINT
+              }
+            }
+          }
+        }
+      });
+}
 }  // namespace InterpolationTarget_detail
 
 namespace Actions {
@@ -273,69 +365,38 @@ struct InterpolationTargetReceiveVars {
           vars_src,
       const std::vector<std::vector<size_t>>& global_offsets,
       const TemporalId& temporal_id) noexcept {
-    db::mutate<Tags::IndicesOfFilledInterpPoints<TemporalId>,
-               Tags::InterpolatedVars<InterpolationTargetTag, TemporalId>>(
-        make_not_null(&box),
-        [&temporal_id, &vars_src, &global_offsets ](
-            const gsl::not_null<
-                std::unordered_map<TemporalId, std::unordered_set<size_t>>*>
-                indices_of_filled,
-            const gsl::not_null<std::unordered_map<
-                TemporalId, Variables<typename InterpolationTargetTag::
-                                          vars_to_interpolate_to_target>>*>
-                vars_dest_all_times) noexcept {
-          auto& vars_dest = (*vars_dest_all_times)[temporal_id];
-          const size_t npts_dest = vars_dest.number_of_grid_points();
-          const size_t nvars = vars_dest.number_of_independent_components;
-          for (size_t j = 0; j < global_offsets.size(); ++j) {
-            const size_t npts_src = global_offsets[j].size();
-            for (size_t i = 0; i < npts_src; ++i) {
-              // If a point is on the boundary of two (or more)
-              // elements, it is possible that we have received data
-              // for this point from more than one Interpolator.
-              // This will rarely occur, but it does occur, e.g. when
-              // a point is exactly on some symmetry
-              // boundary (such as the x-y plane) and this symmetry
-              // boundary is exactly the boundary between two
-              // elements.  If this happens, we accept the first
-              // duplicated point, and we ignore subsequent
-              // duplicated points.  The points are easy to keep track
-              // of because global_offsets uniquely identifies them.
-              if ((*indices_of_filled)[temporal_id]
-                      .insert(global_offsets[j][i])
-                      .second) {
-                for (size_t v = 0; v < nvars; ++v) {
-                  // clang-tidy: no pointer arithmetic
-                  vars_dest.data()[global_offsets[j][i] +    // NOLINT
-                                   v * npts_dest] =          // NOLINT
-                      vars_src[j].data()[i + v * npts_src];  // NOLINT
-                }
-              }
-            }
-          }
-        });
-
-    const size_t filled_size =
-        db::get<Tags::IndicesOfFilledInterpPoints<TemporalId>>(box)
-            .at(temporal_id)
-            .size();
-    const size_t invalid_size = [&box, &temporal_id ]() noexcept {
-      const auto& invalid_indices =
-          db::get<Tags::IndicesOfInvalidInterpPoints<TemporalId>>(box);
-      if (invalid_indices.count(temporal_id) > 0) {
-        return invalid_indices.at(temporal_id).size();
-      }
-      return 0_st;
-    }
-    ();
-    const size_t interp_size =
-        db::get<Tags::InterpolatedVars<InterpolationTargetTag, TemporalId>>(box)
-            .at(temporal_id)
-            .number_of_grid_points();
-    if (invalid_size + filled_size == interp_size) {
+    InterpolationTarget_detail::add_received_variables<InterpolationTargetTag>(
+        make_not_null(&box), vars_src, global_offsets, temporal_id);
+    if (InterpolationTarget_detail::have_data_at_all_points<
+            InterpolationTargetTag>(box, temporal_id)) {
       // All the valid points have been interpolated.
-      InterpolationTarget_detail::callback_and_cleanup<InterpolationTargetTag>(
-          make_not_null(&box), make_not_null(&cache), temporal_id);
+      if (InterpolationTarget_detail::call_callback<InterpolationTargetTag>(
+              make_not_null(&box), make_not_null(&cache), temporal_id)) {
+        InterpolationTarget_detail::clean_up_interpolation_target<
+            InterpolationTargetTag>(make_not_null(&box), temporal_id);
+        auto& interpolator_proxy =
+            Parallel::get_parallel_component<Interpolator<Metavariables>>(
+                cache);
+        Parallel::simple_action<
+            Actions::CleanUpInterpolator<InterpolationTargetTag>>(
+            interpolator_proxy, temporal_id);
+
+        // If we have a sequential target, and there are further
+        // temporal_ids, begin interpolation for the next one.
+        if (InterpolationTargetTag::compute_target_points::is_sequential::
+                value) {
+          const auto& temporal_ids =
+              db::get<Tags::TemporalIds<TemporalId>>(box);
+          if (not temporal_ids.empty()) {
+            auto& my_proxy = Parallel::get_parallel_component<
+                InterpolationTarget<Metavariables, InterpolationTargetTag>>(
+                cache);
+            Parallel::simple_action<
+                typename InterpolationTargetTag::compute_target_points>(
+                my_proxy, temporal_ids.front());
+          }
+        }
+      }
     }
   }
 };
