7bbb343e5252106eb9d860da54f778cce75b9c70
Nils Deppe <nd357@cornell.edu>
2020-03-20 19:35:35 -0400

    Make position-independent code flag target
    Also adds an explanation as to what breaks if we use CMakes PIC option.

--- cmake/SetupPic.cmake
+++ cmake/SetupPic.cmake
@@ -3,5 +3,15 @@
 
 # Set up position independent code by default since this is required
 # for our python libraries.
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+#
+# We cannot use CMake's set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+# because that enables -fPIC in shared libraries and -fPIE on executables.
+# While what CMake does is technically the optimal thing to do, it means
+# we would need to generate two PCHs, one built with -fPIC and one with
+# -fPIE. More specifically, we'd need to generate one using a library target
+# and one using an executable target, which would double the amount of code
+# in SetupPch.cmake for a currently unknown benefit.
+set_property(TARGET SpectreFlags
+  APPEND PROPERTY
+  INTERFACE_COMPILE_OPTIONS
+  $<$<COMPILE_LANGUAGE:CXX>:-fPIC>)
