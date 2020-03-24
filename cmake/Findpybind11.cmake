dceaf3700264508772c6ef90018cbb39bf9939c7
Nils Deppe <nd357@cornell.edu>
2020-03-24 12:35:52 -0400

    Fix formatting in FindPybind11.cmake

--- cmake/Findpybind11.cmake
+++ cmake/Findpybind11.cmake
@@ -2,18 +2,18 @@
 # See LICENSE.txt for details.
 
 find_path(
-    pybind11_INCLUDE_DIR
-    PATH_SUFFIXES include
-    NAMES pybind11/pybind11.h
-    HINTS ${pybind11_ROOT}
-    DOC "Pybind11 include directory. Used pybind11_ROOT to set a search dir."
-)
+  pybind11_INCLUDE_DIR
+  PATH_SUFFIXES include
+  NAMES pybind11/pybind11.h
+  HINTS ${pybind11_ROOT}
+  DOC "Pybind11 include directory. Used pybind11_ROOT to set a search dir."
+  )
 
 set(pybind11_INCLUDE_DIRS ${pybind11_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-    pybind11
-    DEFAULT_MSG pybind11_INCLUDE_DIR pybind11_INCLUDE_DIRS
-)
+  pybind11
+  DEFAULT_MSG pybind11_INCLUDE_DIR pybind11_INCLUDE_DIRS
+  )
 mark_as_advanced(pybind11_INCLUDE_DIR pybind11_INCLUDE_DIRS)
