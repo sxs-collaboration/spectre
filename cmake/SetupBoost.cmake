5ceafd2b4195defc11bb3b3ce4e31bc29840e222
Nils Deppe <nd357@cornell.edu>
2020-04-01 23:28:38 -0400

    Remove adding Boost targets

--- cmake/SetupBoost.cmake
+++ cmake/SetupBoost.cmake
@@ -14,19 +14,3 @@ file(APPEND
 # Boost organizes targets as:
 # - Boost::boost is the header-only parts of Boost
 # - Boost::COMPONENT are the components that need linking, e.g. program_options
-
-# This is needed if your Boost version is newer than your CMake version
-# or if you have an old version of CMake (<3.5)
-if(NOT TARGET Boost::boost)
-  add_library(Boost::boost INTERFACE IMPORTED)
-  set_property(TARGET Boost::boost PROPERTY
-    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
-endif(NOT TARGET Boost::boost)
-
-if(NOT TARGET Boost::program_options)
-  add_library(Boost::program_options INTERFACE IMPORTED)
-  set_property(TARGET Boost::program_options
-    APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${Boost_LIBRARIES})
-  set_property(TARGET Boost::program_options PROPERTY
-    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
-endif(NOT TARGET Boost::program_options)
