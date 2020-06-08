003ce95add143001e175b089b9b3e41e865574b2
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-06-08 09:07:59 +0200

    Add overlap helpers for Schwarz solver
    These functions restrict data to an overlap region or extend
    data from an overlap to the full element.

--- src/ParallelAlgorithms/LinearSolver/Schwarz/OverlapHelpers.cpp
+++ src/ParallelAlgorithms/LinearSolver/Schwarz/OverlapHelpers.cpp
@@ -5,9 +5,13 @@
 
 #include <cmath>
 #include <cstddef>
+#include <numeric>  // std::accumulate
 #include <string>
 
 #include "DataStructures/DataVector.hpp"
+#include "DataStructures/Index.hpp"
+#include "Domain/Structure/Direction.hpp"
+#include "Domain/Structure/Side.hpp"
 #include "ErrorHandling/Assert.hpp"
 #include "Utilities/EqualWithinRoundoff.hpp"
 #include "Utilities/GenerateInstantiations.hpp"
@@ -61,10 +65,119 @@ double overlap_width(const size_t overlap_extent,
   return collocation_points[overlap_extent] - collocation_points[0];
 }
 
+template <size_t Dim>
+OverlapIterator::OverlapIterator(const Index<Dim>& volume_extents,
+                                 const size_t overlap_extent,
+                                 const Direction<Dim>& direction) noexcept
+    : size_{overlap_num_points(volume_extents, overlap_extent,
+                               direction.dimension())},
+      num_slices_{overlap_extent},
+      stride_{std::accumulate(volume_extents.begin(),
+                              volume_extents.begin() + direction.dimension(),
+                              1_st, std::multiplies<size_t>())},
+      stride_count_{0},
+      jump_{(volume_extents[direction.dimension()] - num_slices_) * stride_},
+      initial_offset_{stride_ * (direction.side() == Side::Lower
+                                     ? 0
+                                     : (volume_extents[direction.dimension()] -
+                                        num_slices_))},
+      volume_offset_{initial_offset_},
+      overlap_offset_{0} {
+  assert_overlap_extent(volume_extents, overlap_extent, direction.dimension());
+}
+
+OverlapIterator::operator bool() const noexcept {
+  return overlap_offset_ < size_;
+}
+
+OverlapIterator& OverlapIterator::operator++() {
+  ++volume_offset_;
+  ++overlap_offset_;
+  ++stride_count_;
+  if (stride_count_ == stride_ * num_slices_) {
+    volume_offset_ += jump_;
+    stride_count_ = 0;
+  }
+  return *this;
+}
+
+size_t OverlapIterator::volume_offset() const noexcept {
+  return volume_offset_;
+}
+
+size_t OverlapIterator::overlap_offset() const noexcept {
+  return overlap_offset_;
+}
+
+void OverlapIterator::reset() noexcept {
+  volume_offset_ = initial_offset_;
+  overlap_offset_ = 0;
+  stride_count_ = 0;
+}
+
+namespace detail {
+
+template <size_t Dim>
+void data_on_overlap_impl(double* overlap_data, const double* volume_data,
+                          const size_t num_components,
+                          const Index<Dim>& volume_extents,
+                          const size_t overlap_extent,
+                          const Direction<Dim>& direction) noexcept {
+  const size_t volume_num_points = volume_extents.product();
+  const size_t overlap_num_points = LinearSolver::Schwarz::overlap_num_points(
+      volume_extents, overlap_extent, direction.dimension());
+  for (OverlapIterator overlap_iterator{volume_extents, overlap_extent,
+                                        direction};
+       overlap_iterator; ++overlap_iterator) {
+    for (size_t j = 0; j < num_components; ++j) {
+      const size_t volume_data_index =
+          overlap_iterator.volume_offset() + j * volume_num_points;
+      const size_t overlap_data_index =
+          overlap_iterator.overlap_offset() + j * overlap_num_points;
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      overlap_data[overlap_data_index] = volume_data[volume_data_index];
+    }
+  }
+}
+
+template <size_t Dim>
+void add_overlap_data_impl(double* volume_data, const double* overlap_data,
+                           const size_t num_components,
+                           const Index<Dim>& volume_extents,
+                           const size_t overlap_extent,
+                           const Direction<Dim>& direction) noexcept {
+  const size_t volume_num_points = volume_extents.product();
+  const size_t overlap_num_points = LinearSolver::Schwarz::overlap_num_points(
+      volume_extents, overlap_extent, direction.dimension());
+  for (OverlapIterator overlap_iterator{volume_extents, overlap_extent,
+                                        direction};
+       overlap_iterator; ++overlap_iterator) {
+    for (size_t j = 0; j < num_components; ++j) {
+      const size_t volume_data_index =
+          overlap_iterator.volume_offset() + j * volume_num_points;
+      const size_t overlap_data_index =
+          overlap_iterator.overlap_offset() + j * overlap_num_points;
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      volume_data[volume_data_index] += overlap_data[overlap_data_index];
+    }
+  }
+}
+
+}  // namespace detail
+
 /// \cond
 #define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
-#define INSTANTIATE(r, data) \
-  template size_t overlap_num_points(const Index<DIM(data)>&, size_t, size_t);
+#define INSTANTIATE(r, data)                                                  \
+  template size_t overlap_num_points(const Index<DIM(data)>&, size_t,         \
+                                     size_t) noexcept;                        \
+  template OverlapIterator::OverlapIterator(                                  \
+      const Index<DIM(data)>&, size_t, const Direction<DIM(data)>&) noexcept; \
+  template void detail::add_overlap_data_impl(                                \
+      double*, const double*, size_t, const Index<DIM(data)>&, size_t,        \
+      const Direction<DIM(data)>&) noexcept;                                  \
+  template void detail::data_on_overlap_impl(                                 \
+      double*, const double*, size_t, const Index<DIM(data)>&, size_t,        \
+      const Direction<DIM(data)>&) noexcept;
 
 GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3))
 
--- tests/Unit/ParallelAlgorithms/LinearSolver/Schwarz/Test_OverlapHelpers.cpp
+++ tests/Unit/ParallelAlgorithms/LinearSolver/Schwarz/Test_OverlapHelpers.cpp
@@ -5,10 +5,133 @@
 
 #include "DataStructures/DataVector.hpp"
 #include "DataStructures/Index.hpp"
+#include "DataStructures/Tags/TempTensor.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "DataStructures/Variables.hpp"
+#include "Domain/Structure/Direction.hpp"
 #include "ErrorHandling/Error.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
 #include "ParallelAlgorithms/LinearSolver/Schwarz/OverlapHelpers.hpp"
+#include "Utilities/TMPL.hpp"
 
 namespace LinearSolver::Schwarz {
+
+template <size_t Dim>
+void test_data_on_overlap_consistency(const Index<Dim>& volume_extents,
+                                      const size_t overlap_extent,
+                                      const Direction<Dim>& direction) {
+  MAKE_GENERATOR(generator);
+  std::uniform_real_distribution<> dist(-1., 1.);
+  const DataVector used_for_size{volume_extents.product()};
+  using tags_list = tmpl::list<::Tags::TempScalar<0>, ::Tags::TempI<1, Dim>,
+                               ::Tags::Tempijj<2, Dim>>;
+  const auto vars = make_with_random_values<Variables<tags_list>>(
+      make_not_null(&generator), make_not_null(&dist), used_for_size);
+  CAPTURE(vars);
+
+  const Variables<tags_list> vars_on_overlap =
+      data_on_overlap(vars, volume_extents, overlap_extent, direction);
+  CAPTURE(vars_on_overlap);
+
+  {
+    INFO("Test Variables and Tensor data on overlap is consistent");
+    tmpl::for_each<tags_list>([&](auto tag_v) {
+      using tag = tmpl::type_from<decltype(tag_v)>;
+      CAPTURE(db::tag_name<tag>());
+      const auto tensor_on_overlap = data_on_overlap(
+          get<tag>(vars), volume_extents, overlap_extent, direction);
+      for (size_t i = 0; i < tensor_on_overlap.size(); i++) {
+        CAPTURE(i);
+        CHECK_ITERABLE_APPROX(tensor_on_overlap[i],
+                              get<tag>(vars_on_overlap)[i]);
+      }
+    });
+  }
+  {
+    INFO("Test extended_overlap_data and add_overlap_data are consistent");
+    Variables<tags_list> extended_vars{used_for_size.size(), 0.};
+    add_overlap_data(make_not_null(&extended_vars), vars_on_overlap,
+                     volume_extents, overlap_extent, direction);
+    CHECK_VARIABLES_APPROX(
+        extended_vars, extended_overlap_data(vars_on_overlap, volume_extents,
+                                             overlap_extent, direction));
+  }
+}
+
+template <size_t Dim>
+void test_data_on_overlap(const DataVector& scalar_volume_data,
+                          const Index<Dim>& volume_extents,
+                          const size_t overlap_extent,
+                          const Direction<Dim>& direction,
+                          const DataVector& expected_overlap_data,
+                          const DataVector& expected_extended_data) {
+  CAPTURE(volume_extents);
+  CAPTURE(overlap_extent);
+  CAPTURE(direction);
+  {
+    INFO("Tensor data on overlap");
+    const Scalar<DataVector> scalar{scalar_volume_data};
+    CHECK_ITERABLE_APPROX(
+        get(data_on_overlap(scalar, volume_extents, overlap_extent, direction)),
+        expected_overlap_data);
+  }
+  Variables<tmpl::list<::Tags::TempScalar<0>>> vars{scalar_volume_data.size()};
+  get(get<::Tags::TempScalar<0>>(vars)) = scalar_volume_data;
+  const auto vars_on_overlap =
+      data_on_overlap(vars, volume_extents, overlap_extent, direction);
+  {
+    INFO("Variables data on overlap");
+    CHECK_ITERABLE_APPROX(get(get<::Tags::TempScalar<0>>(vars_on_overlap)),
+                          expected_overlap_data);
+  }
+  Variables<tmpl::list<::Tags::TempScalar<0>>> extended_vars{
+      scalar_volume_data.size(), 0.};
+  add_overlap_data(make_not_null(&extended_vars), vars_on_overlap,
+                   volume_extents, overlap_extent, direction);
+  {
+    INFO("Add overlap data");
+    CHECK_ITERABLE_APPROX(get(get<::Tags::TempScalar<0>>(extended_vars)),
+                          expected_extended_data);
+  }
+  {
+    INFO("Extended overlap data");
+    CHECK_VARIABLES_APPROX(
+        extended_overlap_data(vars_on_overlap, volume_extents, overlap_extent,
+                              direction),
+        extended_vars);
+  }
+  {
+    INFO("Test consistency with non-scalars");
+    test_data_on_overlap_consistency(volume_extents, overlap_extent, direction);
+  }
+}
+
+template <size_t Dim>
+void test_overlap_iterator(const Index<Dim>& volume_extents,
+                           const size_t overlap_extent,
+                           const Direction<Dim>& direction,
+                           const std::vector<size_t>& expected_volume_offsets) {
+  CAPTURE(Dim);
+  CAPTURE(volume_extents);
+  CAPTURE(overlap_extent);
+  CAPTURE(direction);
+  ASSERT(std::is_sorted(expected_volume_offsets.begin(),
+                        expected_volume_offsets.end()),
+         "Volume indices must be sorted to optimize array access performance.");
+
+  OverlapIterator overlap_iterator{volume_extents, overlap_extent, direction};
+  // Reset the iterator once to test resetting works
+  ++overlap_iterator;
+  overlap_iterator.reset();
+  for (size_t i = 0; i < expected_volume_offsets.size(); ++i) {
+    CHECK(overlap_iterator);
+    CHECK(overlap_iterator.volume_offset() == expected_volume_offsets[i]);
+    CHECK(overlap_iterator.overlap_offset() == i);
+    ++overlap_iterator;
+  }
+  CHECK_FALSE(overlap_iterator);
+}
 
 SPECTRE_TEST_CASE("Unit.ParallelSchwarz.OverlapHelpers",
                   "[Unit][ParallelAlgorithms][LinearSolver]") {
@@ -55,6 +178,182 @@ SPECTRE_TEST_CASE("Unit.ParallelSchwarz.OverlapHelpers",
     CHECK(overlap_width(2, coords) == approx(1.));
     CHECK(overlap_width(3, coords) == approx(1.8));
     CHECK(overlap_width(4, coords) == approx(2.));
+  }
+  {
+    INFO("Overlap iterator");
+    {
+      // Give an example to include in the docs
+      /// [overlap_iterator]
+      // Overlap region:
+      // + - - - + -xi->
+      // | X X O |
+      // | X X O |
+      // + - - - +
+      // v eta
+      const Index<2> volume_extents{{{3, 2}}};
+      const size_t overlap_extent = 2;
+      const auto direction = Direction<2>::lower_xi();
+      const std::array<size_t, 4> expected_volume_offsets{{0, 1, 3, 4}};
+      size_t i = 0;
+      for (OverlapIterator overlap_iterator{volume_extents, overlap_extent,
+                                            direction};
+           overlap_iterator; ++overlap_iterator) {
+        CHECK(overlap_iterator.volume_offset() ==
+              gsl::at(expected_volume_offsets, i));
+        CHECK(overlap_iterator.overlap_offset() == i);
+        ++i;
+      }
+      CHECK(i == expected_volume_offsets.size());
+      /// [overlap_iterator]
+    }
+    // Overlap region: [X X O] -xi->
+    test_overlap_iterator(Index<1>{3}, 2, Direction<1>::lower_xi(), {{0, 1}});
+    // Overlap region: [O X X] -xi->
+    test_overlap_iterator(Index<1>{3}, 2, Direction<1>::upper_xi(), {{1, 2}});
+    // Overlap region:
+    // + - - - + -xi->
+    // | X X O |
+    // | X X O |
+    // + - - - +
+    // v eta
+    test_overlap_iterator(Index<2>{{{3, 2}}}, 2, Direction<2>::lower_xi(),
+                          {{0, 1, 3, 4}});
+    // Overlap region:
+    // + - - - + -xi->
+    // | O X X |
+    // | O X X |
+    // + - - - +
+    // v eta
+    test_overlap_iterator(Index<2>{{{3, 2}}}, 2, Direction<2>::upper_xi(),
+                          {{1, 2, 4, 5}});
+    // Overlap region:
+    // + - - - - + -xi->
+    // | X X X X |
+    // | X X X X |
+    // | O O O O |
+    // + - - - - +
+    // v eta
+    test_overlap_iterator(Index<2>{{{4, 3}}}, 2, Direction<2>::lower_eta(),
+                          {{0, 1, 2, 3, 4, 5, 6, 7}});
+    // Overlap region:
+    // + - - + -xi->
+    // | O O |
+    // | X X |
+    // | X X |
+    // + - - +
+    // v eta
+    test_overlap_iterator(Index<2>{{{2, 3}}}, 2, Direction<2>::upper_eta(),
+                          {{2, 3, 4, 5}});
+    test_overlap_iterator(Index<3>{{{3, 3, 2}}}, 2, Direction<3>::lower_xi(),
+                          {{0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16}});
+    test_overlap_iterator(Index<3>{{{3, 3, 2}}}, 2, Direction<3>::upper_xi(),
+                          {{1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17}});
+    test_overlap_iterator(Index<3>{{{3, 3, 2}}}, 2, Direction<3>::lower_eta(),
+                          {{0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14}});
+    test_overlap_iterator(Index<3>{{{3, 3, 2}}}, 2, Direction<3>::upper_eta(),
+                          {{3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17}});
+    test_overlap_iterator(Index<3>{{{3, 2, 3}}}, 2, Direction<3>::lower_zeta(),
+                          {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}});
+    test_overlap_iterator(Index<3>{{{3, 2, 3}}}, 2, Direction<3>::upper_zeta(),
+                          {{6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}});
+  }
+  {
+    INFO("Data on overlap");
+    {
+      INFO("1D");
+      const Index<1> volume_extents{3};
+      const size_t overlap_extent = 2;
+      DataVector scalar{1., 2., 3.};
+      // Overlap region: [X X O] -xi->
+      test_data_on_overlap(scalar, volume_extents, overlap_extent,
+                           Direction<1>::lower_xi(), {1., 2.}, {1., 2., 0.});
+      // Overlap region: [O X X] -xi->
+      test_data_on_overlap(scalar, volume_extents, overlap_extent,
+                           Direction<1>::upper_xi(), {2., 3.}, {0., 2., 3.});
+    }
+    {
+      INFO("2D");
+      const Index<2> volume_extents{{{3, 2}}};
+      const size_t overlap_extent_xi = 2;
+      const size_t overlap_extent_eta = 1;
+      DataVector scalar{1., 2., 3., 4., 5., 6.};
+      // Overlap region:
+      // + - - - + -xi->
+      // | X X O |
+      // | X X O |
+      // + - - - +
+      // v eta
+      test_data_on_overlap(scalar, volume_extents, overlap_extent_xi,
+                           Direction<2>::lower_xi(), {1., 2., 4., 5.},
+                           {1., 2., 0., 4., 5., 0.});
+      // Overlap region:
+      // + - - - + -xi->
+      // | O X X |
+      // | O X X |
+      // + - - - +
+      // v eta
+      test_data_on_overlap(scalar, volume_extents, overlap_extent_xi,
+                           Direction<2>::upper_xi(), {2., 3., 5., 6.},
+                           {0., 2., 3., 0., 5., 6.});
+      // Overlap region:
+      // + - - - + -xi->
+      // | X X X |
+      // | O O O |
+      // + - - - +
+      // v eta
+      test_data_on_overlap(scalar, volume_extents, overlap_extent_eta,
+                           Direction<2>::lower_eta(), {1., 2., 3.},
+                           {1., 2., 3., 0., 0., 0.});
+      // Overlap region:
+      // + - - - + -xi->
+      // | O O O |
+      // | X X X |
+      // + - - - +
+      // v eta
+      test_data_on_overlap(scalar, volume_extents, overlap_extent_eta,
+                           Direction<2>::upper_eta(), {4., 5., 6.},
+                           {0., 0., 0., 4., 5., 6.});
+    }
+    {
+      INFO("3D");
+      const Index<3> volume_extents{{{3, 2, 3}}};
+      const size_t overlap_extent_xi = 2;
+      const size_t overlap_extent_eta = 1;
+      const size_t overlap_extent_zeta = 2;
+      DataVector scalar{1.,  2.,  3.,  4.,  5.,  6.,  7.,  8.,  9.,
+                        10., 11., 12., 13., 14., 15., 16., 17., 18.};
+      test_data_on_overlap(
+          scalar, volume_extents, overlap_extent_xi, Direction<3>::lower_xi(),
+          {1., 2., 4., 5., 7., 8., 10., 11., 13., 14., 16., 17.},
+          {1., 2., 0., 4., 5., 0., 7., 8., 0., 10., 11., 0., 13., 14., 0., 16.,
+           17., 0.});
+      test_data_on_overlap(
+          scalar, volume_extents, overlap_extent_xi, Direction<3>::upper_xi(),
+          {2., 3., 5., 6., 8., 9., 11., 12., 14., 15., 17., 18.},
+          {0., 2., 3., 0., 5., 6., 0., 8., 9., 0., 11., 12., 0., 14., 15., 0.,
+           17., 18.});
+      test_data_on_overlap(scalar, volume_extents, overlap_extent_eta,
+                           Direction<3>::lower_eta(),
+                           {1., 2., 3., 7., 8., 9., 13., 14., 15.},
+                           {1., 2., 3., 0., 0., 0., 7., 8., 9., 0., 0., 0., 13.,
+                            14., 15., 0., 0., 0.});
+      test_data_on_overlap(scalar, volume_extents, overlap_extent_eta,
+                           Direction<3>::upper_eta(),
+                           {4., 5., 6., 10., 11., 12., 16., 17., 18.},
+                           {0., 0., 0., 4., 5., 6., 0., 0., 0., 10., 11., 12.,
+                            0., 0., 0., 16., 17., 18.});
+      test_data_on_overlap(scalar, volume_extents, overlap_extent_zeta,
+                           Direction<3>::lower_zeta(),
+                           {1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12.},
+                           {1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12.,
+                            0., 0., 0., 0., 0., 0.});
+      test_data_on_overlap(
+          scalar, volume_extents, overlap_extent_zeta,
+          Direction<3>::upper_zeta(),
+          {7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 17., 18.},
+          {0., 0., 0., 0., 0., 0., 7., 8., 9., 10., 11., 12., 13., 14., 15.,
+           16., 17., 18.});
+    }
   }
 }
 
