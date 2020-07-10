7bd9d6fad3f498f2520b246829ce2c973842bbed
Jordan Moxon <jem497@cornell.edu>
2020-07-10 16:58:56 -0700

    Clean out PupStlCpp11.hpp

--- tests/Unit/Parallel/Test_PupStlCpp11.cpp
+++ tests/Unit/Parallel/Test_PupStlCpp11.cpp
@@ -3,9 +3,6 @@
 
 #include "Framework/TestingFramework.hpp"
 
-#include <algorithm>
-#include <array>
-#include <cstddef>
 #include <deque>
 #include <memory>
 #include <pup.h>
@@ -13,12 +10,11 @@
 #include <string>
 #include <tuple>
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
+#include <utility>
 
 #include "Framework/TestHelpers.hpp"
 #include "Parallel/CharmPupable.hpp"
-#include "Utilities/MakeArray.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace Test_Classes {
@@ -57,25 +53,7 @@ struct DerivedInPupStlCpp11 : public Base {
 
 }  // namespace Test_Classes
 
-namespace {
-enum class eDummyEnum { test1, test2 };
-}  // namespace
-
 SPECTRE_TEST_CASE("Unit.Serialization.PupStlCpp11", "[Serialization][Unit]") {
-  {
-    INFO("unordered_map");
-    std::unordered_map<std::string, double> um;
-    um["aaa"] = 1.589;
-    um["bbb"] = -10.7392;
-    test_serialization(um);
-  }
-
-  {
-    INFO("enum");
-    eDummyEnum e = eDummyEnum::test2;
-    test_serialization(e);
-  }
-
   /// [example_serialize_comparable]
   {
     INFO("tuple");
@@ -84,66 +62,18 @@ SPECTRE_TEST_CASE("Unit.Serialization.PupStlCpp11", "[Serialization][Unit]") {
     um["bbb"] = -10.7392;
     auto test_tuple = std::make_tuple<int, double, std::string,
                                       std::unordered_map<std::string, double>>(
-        2, 0.57, "blah", std::move(um));
+                                          2, 0.57, "blah", std::move(um));
     test_serialization(test_tuple);
   }
   /// [example_serialize_comparable]
-
-  {
-    INFO("array");
-    auto t = make_array(1.0, 3.64, 9.23);
-    test_serialization(t);
-
-    auto t2 =
-        make_array(std::vector<double>{1, 4, 8}, std::vector<double>{7, 9, 4});
-    test_serialization(t2);
-  }
-
-  {
-    INFO("deque");
-    std::deque<double> t{1.0, 3.64, 9.23};
-    test_serialization(t);
-  }
-
-  {
-    INFO("unordered_set");
-    std::unordered_set<size_t> test_set = {1, 2, 5, 100};
-    test_serialization(test_set);
-  }
-
-  {
-    INFO("unordered_set.empty");
-    std::unordered_set<size_t> test_set{};
-    test_serialization(test_set);
-  }
-
-  {
-    INFO("unique_ptr.double");
-    auto test_unique_ptr = std::make_unique<double>(3.8273);
-    // clang-tidy: false positive use after free
-    CHECK(3.8273 == *serialize_and_deserialize(test_unique_ptr));  // NOLINT
-  }
-
   /// [example_serialize_derived]
   {
     INFO("unique_ptr.abstract_base");
     test_serialization_via_base<Test_Classes::Base,
                                 Test_Classes::DerivedInPupStlCpp11>(
-        std::vector<double>{-1, 12.3, -7, 8});
+                                    std::vector<double>{-1, 12.3, -7, 8});
   }
   /// [example_serialize_derived]
-
-  {
-    INFO("unique_ptr.double.nullptr");
-    std::unique_ptr<double> derived_ptr = nullptr;
-    auto blah = serialize_and_deserialize(derived_ptr);
-    CHECK(nullptr == blah);
-  }
-  {
-    INFO("unique_ptr.abstract_base.nullptr");
-    std::unique_ptr<Test_Classes::Base> base{nullptr};
-    CHECK(serialize_and_deserialize(base) == nullptr);
-  }
 }
 
 /// \cond
