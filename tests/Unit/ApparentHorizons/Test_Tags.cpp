67dbaa15e5ebb2f73b9ed4015b523be92087f01c
MarloMo <moralesmarlo486@gmail.com>
2020-08-05 17:18:37 -0700

    Add UnitNormalOneFormCompute Test Tag

--- tests/Unit/ApparentHorizons/Test_Tags.cpp
+++ tests/Unit/ApparentHorizons/Test_Tags.cpp
@@ -359,4 +359,7 @@ SPECTRE_TEST_CASE("Unit.ApparentHorizons.StrahlkorperDataBox",
       StrahlkorperTags::OneOverOneFormMagnitudeCompute<3, Frame::Inertial,
                                                        DataVector>>(
       "OneOverOneFormMagnitude");
+  TestHelpers::db::test_compute_tag<
+      StrahlkorperTags::UnitNormalOneFormCompute<Frame::Inertial>>(
+      "UnitNormalOneFormCompute");
 }
