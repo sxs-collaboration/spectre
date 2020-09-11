9fa2d65d2a8a8dfe4465b263d746d3987a325d06
Nils Deppe <nd357@cornell.edu>
2020-09-10 18:34:48 -0700

    Add serialization for std::variant

--- src/Parallel/PupStlCpp17.hpp
+++ src/Parallel/PupStlCpp17.hpp
@@ -6,6 +6,10 @@
 #include <optional>
 #include <pup_stl.h>
 #include <utility>
+#include <variant>
+
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
 
 namespace PUP {
 /// \ingroup ParallelGroup
@@ -35,6 +39,42 @@ void pup(er& p, std::optional<T>& t) noexcept {  // NOLINT
 /// Serialization of std::optional for Charm++
 template <typename T>
 void operator|(er& p, std::optional<T>& t) noexcept {  // NOLINT
+  pup(p, t);
+}
+
+/// \ingroup ParallelGroup
+/// Serialization of std::variant for Charm++
+template <typename... Ts>
+void pup(er& p, std::variant<Ts...>& t) noexcept {  // NOLINT
+  // clang tidy: complains about pointer decay, I don't understand the error
+  // since nothing here is doing anything with pointers.
+  assert(not t.valueless_by_exception());  // NOLINT
+
+  size_t current_index = 0;
+  size_t send_index = t.index();
+  p | send_index;
+
+  const auto pup_helper = [&current_index, &p, send_index,
+                           &t](auto type_v) noexcept {
+    using type = tmpl::type_from<decltype(type_v)>;
+    if (UNLIKELY(current_index == send_index)) {
+      if (p.isUnpacking()) {
+        type temp{};
+        p | temp;
+        t = std::move(temp);
+      } else {
+        p | std::get<type>(t);
+      }
+    }
+    ++current_index;
+  };
+  EXPAND_PACK_LEFT_TO_RIGHT(pup_helper(tmpl::type_<Ts>{}));
+}
+
+/// \ingroup ParallelGroup
+/// Serialization of std::variant for Charm++
+template <typename... Ts>
+void operator|(er& p, std::variant<Ts...>& t) noexcept {  // NOLINT
   pup(p, t);
 }
 }  // namespace PUP
--- tests/Unit/Parallel/Test_PupStlCpp17.cpp
+++ tests/Unit/Parallel/Test_PupStlCpp17.cpp
@@ -3,7 +3,9 @@
 
 #include "Framework/TestingFramework.hpp"
 
+#include <cstddef>
 #include <optional>
+#include <variant>
 
 #include "Framework/TestHelpers.hpp"
 #include "Parallel/PupStlCpp17.hpp"
@@ -21,5 +23,16 @@ SPECTRE_TEST_CASE("Unit.Serialization.PupStlCpp17", "[Serialization][Unit]") {
     const auto t_set_deserialized = serialize_and_deserialize(t_set);
     REQUIRE(t_set_deserialized.value());
     CHECK(*t_set_deserialized == 10);
+  }
+  {
+    INFO("Variant");
+    std::variant<int, char, size_t> t{-1};
+    CHECK(std::get<int>(serialize_and_deserialize(t)) == -1);
+    t = 'a';
+    CHECK(std::get<char>(serialize_and_deserialize(t)) == 'a');
+    t = 10;
+    CHECK(std::get<int>(serialize_and_deserialize(t)) == 10);
+    t = static_cast<size_t>(100);
+    CHECK(std::get<size_t>(serialize_and_deserialize(t)) == 100);
   }
 }
