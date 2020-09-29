c964eea29f99894b3c6927c5ecb33a38f9c1e7e5
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-09-29 17:42:18 +0200

    Document Xcts::Solutions namespace

---
+++ src/PointwiseFunctions/AnalyticSolutions/Xcts/Solutions.hpp
@@ -0,0 +1,15 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines Xcts::Solutions namespace
+
+#pragma once
+
+namespace Xcts {
+/*!
+ * \ingroup AnalyticSolutionsGroup
+ * \brief Analytic solutions to the \ref Xcts "XCTS" equations
+ */
+namespace Solutions {}
+}  // namespace Xcts
