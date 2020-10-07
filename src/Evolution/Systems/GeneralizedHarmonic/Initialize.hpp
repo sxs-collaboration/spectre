f941c6f76b1a5f77e41d855299e0537a429ae0aa
Prayush Kumar <prayush@astro.cornell.edu>
2020-10-06 18:37:34 -0700

    Add compute tags to GH initialization that are needed for Bjorhus BCs

--- src/Evolution/Systems/GeneralizedHarmonic/Initialize.hpp
+++ src/Evolution/Systems/GeneralizedHarmonic/Initialize.hpp
@@ -70,19 +70,28 @@ struct InitializeConstraints {
                     const ParallelComponent* const /*meta*/) noexcept {
     using compute_tags = tmpl::flatten<db::AddComputeTags<
         GeneralizedHarmonic::Tags::GaugeConstraintCompute<Dim, frame>,
+        GeneralizedHarmonic::Tags::FConstraintCompute<Dim, frame>,
+        GeneralizedHarmonic::Tags::TwoIndexConstraintCompute<Dim, frame>,
         // following tags added to observe constraints
         ::Tags::PointwiseL2NormCompute<
             GeneralizedHarmonic::Tags::GaugeConstraint<Dim, frame>>,
+        ::Tags::PointwiseL2NormCompute<
+            GeneralizedHarmonic::Tags::FConstraint<Dim, frame>>,
+        ::Tags::PointwiseL2NormCompute<
+            GeneralizedHarmonic::Tags::TwoIndexConstraint<Dim, frame>>,
         ::Tags::PointwiseL2NormCompute<
             GeneralizedHarmonic::Tags::ThreeIndexConstraint<Dim, frame>>,
         // The 4-index constraint is only implemented in 3d
         tmpl::conditional_t<
             Dim == 3,
-            tmpl::list<GeneralizedHarmonic::Tags::FourIndexConstraintCompute<
-                           Dim, frame>,
-                       ::Tags::PointwiseL2NormCompute<
-                           GeneralizedHarmonic::Tags::FourIndexConstraint<
-                               Dim, frame>>>,
+            tmpl::list<
+                GeneralizedHarmonic::Tags::FourIndexConstraintCompute<Dim,
+                                                                      frame>,
+                GeneralizedHarmonic::Tags::ConstraintEnergyCompute<Dim, frame>,
+                ::Tags::PointwiseL2NormCompute<
+                    GeneralizedHarmonic::Tags::FourIndexConstraint<Dim, frame>>,
+                ::Tags::PointwiseL2NormCompute<
+                    GeneralizedHarmonic::Tags::ConstraintEnergy<Dim, frame>>>,
             tmpl::list<>>>>;
 
     return std::make_tuple(
@@ -115,6 +124,7 @@ struct InitializeGhAnd3Plus1Variables {
         gr::Tags::SpacetimeNormalVectorCompute<Dim, frame, DataVector>,
         gr::Tags::InverseSpacetimeMetricCompute<Dim, frame, DataVector>,
         GeneralizedHarmonic::Tags::ThreeIndexConstraintCompute<Dim, frame>,
+        GeneralizedHarmonic::Tags::ExtrinsicCurvatureCompute<Dim, frame>,
         GeneralizedHarmonic::Tags::ConstraintGamma0Compute<Dim, frame>,
         GeneralizedHarmonic::Tags::ConstraintGamma1Compute<Dim, frame>,
         GeneralizedHarmonic::Tags::ConstraintGamma2Compute<Dim, frame>>;
