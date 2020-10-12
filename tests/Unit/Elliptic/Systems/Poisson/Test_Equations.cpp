e016101d942413b3f4fd9c10e9a4c250813f3e94
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-10-12 14:30:33 +0200

    Add test helper for elliptic first-order systems

--- tests/Unit/Elliptic/Systems/Poisson/Test_Equations.cpp
+++ tests/Unit/Elliptic/Systems/Poisson/Test_Equations.cpp
@@ -7,22 +7,16 @@
 #include <cstddef>
 #include <string>
 
-#include "DataStructures/DataBox/DataBox.hpp"
-#include "DataStructures/DataBox/Prefixes.hpp"
 #include "DataStructures/DataVector.hpp"
-#include "DataStructures/Tensor/Tensor.hpp"
 #include "Elliptic/Systems/Poisson/Equations.hpp"
-#include "Elliptic/Systems/Poisson/Tags.hpp"
+#include "Elliptic/Systems/Poisson/FirstOrderSystem.hpp"
+#include "Elliptic/Systems/Poisson/Geometry.hpp"
 #include "Framework/CheckWithRandomValues.hpp"
 #include "Framework/SetupLocalPythonEnvironment.hpp"
-#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
-#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
-#include "PointwiseFunctions/GeneralRelativity/DetAndInverseSpatialMetric.hpp"
-#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
-#include "Utilities/Gsl.hpp"
+#include "Helpers/Elliptic/FirstOrderSystem.hpp"
 #include "Utilities/MakeString.hpp"
-#include "Utilities/MakeWithValue.hpp"
-#include "Utilities/TMPL.hpp"
+
+namespace helpers = TestHelpers::elliptic;
 
 namespace {
 
@@ -40,119 +34,13 @@ void test_equations(const DataVector& used_for_size) {
       used_for_size);
 }
 
-template <size_t Dim>
+template <size_t Dim, Poisson::Geometry BackgroundGeometry>
 void test_computers(const DataVector& used_for_size) {
-  using field_tag = Poisson::Tags::Field;
-  using auxiliary_field_tag =
-      ::Tags::deriv<field_tag, tmpl::size_t<Dim>, Frame::Inertial>;
-  using field_flux_tag =
-      ::Tags::Flux<field_tag, tmpl::size_t<Dim>, Frame::Inertial>;
-  using auxiliary_flux_tag =
-      ::Tags::Flux<auxiliary_field_tag, tmpl::size_t<Dim>, Frame::Inertial>;
-  using field_source_tag = ::Tags::Source<field_tag>;
-  using auxiliary_source_tag = ::Tags::Source<auxiliary_field_tag>;
-
-  const size_t num_points = used_for_size.size();
-  {
-    INFO("EuclideanFluxes" << Dim << "D");
-    auto box =
-        db::create<db::AddSimpleTags<field_tag, auxiliary_field_tag,
-                                     field_flux_tag, auxiliary_flux_tag>>(
-            Scalar<DataVector>{num_points, 2.},
-            tnsr::i<DataVector, Dim>{num_points, 3.},
-            tnsr::I<DataVector, Dim>{num_points, 0.},
-            tnsr::Ij<DataVector, Dim>{num_points, 0.});
-
-    const Poisson::EuclideanFluxes<Dim> fluxes_computer{};
-    using argument_tags = typename Poisson::EuclideanFluxes<Dim>::argument_tags;
-
-    db::mutate_apply<tmpl::list<field_flux_tag>, argument_tags>(
-        fluxes_computer, make_not_null(&box), get<auxiliary_field_tag>(box));
-    auto expected_field_flux = tnsr::I<DataVector, Dim>{num_points, 0.};
-    Poisson::euclidean_fluxes(make_not_null(&expected_field_flux),
-                              get<auxiliary_field_tag>(box));
-    CHECK(get<field_flux_tag>(box) == expected_field_flux);
-
-    db::mutate_apply<tmpl::list<auxiliary_flux_tag>, argument_tags>(
-        fluxes_computer, make_not_null(&box), get<field_tag>(box));
-    auto expected_auxiliary_flux = tnsr::Ij<DataVector, Dim>{num_points, 0.};
-    Poisson::auxiliary_fluxes(make_not_null(&expected_auxiliary_flux),
-                              get<field_tag>(box));
-    CHECK(get<auxiliary_flux_tag>(box) == expected_auxiliary_flux);
-  }
-  {
-    INFO("NonEuclideanFluxes" << Dim << "D");
-    MAKE_GENERATOR(generator);
-    std::uniform_real_distribution<> dist(-0.1, 0.1);
-    const auto nn_generator = make_not_null(&generator);
-    const auto nn_dist = make_not_null(&dist);
-    auto spatial_metric = make_with_random_values<tnsr::ii<DataVector, Dim>>(
-        nn_generator, nn_dist, used_for_size);
-    for (size_t d = 0; d < Dim; d++) {
-      spatial_metric.get(d, d) += 1.;
-    }
-    auto box = db::create<
-        db::AddSimpleTags<
-            field_tag, auxiliary_field_tag, field_flux_tag, auxiliary_flux_tag,
-            gr::Tags::SpatialMetric<Dim, Frame::Inertial, DataVector>>,
-        db::AddComputeTags<gr::Tags::DetAndInverseSpatialMetricCompute<
-            Dim, Frame::Inertial, DataVector>>>(
-        make_with_random_values<Scalar<DataVector>>(nn_generator, nn_dist,
-                                                    used_for_size),
-        make_with_random_values<tnsr::i<DataVector, Dim>>(nn_generator, nn_dist,
-                                                          used_for_size),
-        make_with_value<tnsr::I<DataVector, Dim>>(
-            used_for_size, std::numeric_limits<double>::signaling_NaN()),
-        make_with_value<tnsr::Ij<DataVector, Dim>>(
-            used_for_size, std::numeric_limits<double>::signaling_NaN()),
-        std::move(spatial_metric));
-
-    const Poisson::NonEuclideanFluxes<Dim> fluxes_computer{};
-    using argument_tags =
-        typename Poisson::NonEuclideanFluxes<Dim>::argument_tags;
-
-    db::mutate_apply<tmpl::list<field_flux_tag>, argument_tags>(
-        fluxes_computer, make_not_null(&box), get<auxiliary_field_tag>(box));
-    auto expected_field_flux = make_with_value<tnsr::I<DataVector, Dim>>(
-        used_for_size, std::numeric_limits<double>::signaling_NaN());
-    Poisson::non_euclidean_fluxes(
-        make_not_null(&expected_field_flux),
-        get<gr::Tags::InverseSpatialMetric<Dim, Frame::Inertial, DataVector>>(
-            box),
-        get<gr::Tags::DetSpatialMetric<DataVector>>(box),
-        get<auxiliary_field_tag>(box));
-    CHECK(get<field_flux_tag>(box) == expected_field_flux);
-
-    db::mutate_apply<tmpl::list<auxiliary_flux_tag>, argument_tags>(
-        fluxes_computer, make_not_null(&box), get<field_tag>(box));
-    auto expected_auxiliary_flux = make_with_value<tnsr::Ij<DataVector, Dim>>(
-        used_for_size, std::numeric_limits<double>::signaling_NaN());
-    Poisson::auxiliary_fluxes(make_not_null(&expected_auxiliary_flux),
-                              get<field_tag>(box));
-    CHECK(get<auxiliary_flux_tag>(box) == expected_auxiliary_flux);
-  }
-  {
-    INFO("Sources" << Dim << "D");
-    auto box =
-        db::create<db::AddSimpleTags<field_source_tag, auxiliary_source_tag>>(
-            Scalar<DataVector>{num_points,
-                               std::numeric_limits<double>::signaling_NaN()},
-            tnsr::i<DataVector, Dim>{
-                num_points, std::numeric_limits<double>::signaling_NaN()});
-
-    const Poisson::Sources sources_computer{};
-    using argument_tags = typename Poisson::Sources::argument_tags;
-
-    db::mutate_apply<tmpl::list<field_source_tag, auxiliary_source_tag>,
-                     argument_tags>(
-        sources_computer, make_not_null(&box),
-        Scalar<DataVector>{num_points,
-                           std::numeric_limits<double>::signaling_NaN()},
-        tnsr::I<DataVector, Dim>{num_points,
-                                 std::numeric_limits<double>::signaling_NaN()});
-    auto expected_field_source = Scalar<DataVector>{num_points, 0.};
-    CHECK(get<field_source_tag>(box) == expected_field_source);
-  }
+  CAPTURE(Dim);
+  using system = Poisson::FirstOrderSystem<Dim, BackgroundGeometry>;
+  helpers::test_first_order_fluxes_computer<system>(typename system::fluxes{},
+                                                    used_for_size);
+  helpers::test_first_order_sources_computer<system>(used_for_size);
 }
 
 }  // namespace
@@ -163,5 +51,7 @@ SPECTRE_TEST_CASE("Unit.Elliptic.Systems.Poisson", "[Unit][Elliptic]") {
 
   GENERATE_UNINITIALIZED_DATAVECTOR;
   CHECK_FOR_DATAVECTORS(test_equations, (1, 2, 3));
-  CHECK_FOR_DATAVECTORS(test_computers, (1, 2, 3));
+  CHECK_FOR_DATAVECTORS(
+      test_computers, (1, 2, 3),
+      (Poisson::Geometry::Euclidean, Poisson::Geometry::NonEuclidean));
 }
---
+++ tests/Unit/Helpers/Elliptic/FirstOrderSystem.hpp
@@ -0,0 +1,189 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Helper functions to test elliptic first-order systems
+
+#pragma once
+
+#include <cstddef>
+#include <random>
+#include <tuple>
+#include <limits>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/PrefixHelpers.hpp"
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Variables.hpp"
+#include "DataStructures/VariablesTag.hpp"
+#include "Elliptic/FirstOrderComputeTags.hpp"
+#include "Elliptic/FirstOrderOperator.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Framework/TestingFramework.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace TestHelpers {
+/// \ingroup TestingFrameworkGroup
+/// Helper functions to test elliptic first-order systems
+namespace elliptic {
+
+/*!
+ * \brief Test the `System::fluxes` are functional
+ *
+ * This function tests the following properties of the `System::fluxes`:
+ *
+ * - They work with the `elliptic::first_order_fluxes` function.
+ * - They can be applied to a DataBox, i.e. their argument tags are consistent
+ *   with their apply function.
+ */
+template <typename System>
+void test_first_order_fluxes_computer(
+    const typename System::fluxes& fluxes_computer,
+    const DataVector& used_for_size) {
+  using FluxesComputer = typename System::fluxes;
+  static constexpr size_t volume_dim = System::volume_dim;
+  using vars_tag = typename System::fields_tag;
+  using primal_fields = typename System::primal_fields;
+  using auxiliary_fields = typename System::auxiliary_fields;
+  using VarsType = typename vars_tag::type;
+  using fluxes_tag =
+      db::add_tag_prefix<::Tags::Flux, vars_tag, tmpl::size_t<volume_dim>,
+                         Frame::Inertial>;
+  using argument_tags = typename FluxesComputer::argument_tags;
+
+  MAKE_GENERATOR(generator);
+  std::uniform_real_distribution<> dist(0.5, 2.);
+
+  // Generate random variables
+  const auto vars = make_with_random_values<VarsType>(
+      make_not_null(&generator), make_not_null(&dist), used_for_size);
+
+  // Generate fluxes from the variables with random arguments
+  tuples::tagged_tuple_from_typelist<argument_tags> args{};
+  tmpl::for_each<argument_tags>(
+      [&args, &generator, &dist, &used_for_size](auto tag_v) {
+        using tag = tmpl::type_from<decltype(tag_v)>;
+        get<tag>(args) = make_with_random_values<typename tag::type>(
+            make_not_null(&generator), make_not_null(&dist), used_for_size);
+      });
+  const auto expected_fluxes = tuples::apply(
+      [&vars, &fluxes_computer](const auto&... expanded_args) {
+        return ::elliptic::first_order_fluxes<volume_dim, primal_fields,
+                                              auxiliary_fields>(
+            vars, fluxes_computer, expanded_args...);
+      },
+      args);
+
+  // Create a DataBox
+  auto box = tuples::apply(
+      [&vars, &used_for_size](const auto&... expanded_args) {
+        return db::create<tmpl::append<db::AddSimpleTags<vars_tag, fluxes_tag>,
+                                       argument_tags>>(
+            vars,
+            make_with_value<typename fluxes_tag::type>(
+                used_for_size, std::numeric_limits<double>::signaling_NaN()),
+            expanded_args...);
+      },
+      args);
+
+  // Apply the fluxes computer to the DataBox
+  db::mutate_apply<db::wrap_tags_in<::Tags::Flux, primal_fields,
+                                    tmpl::size_t<volume_dim>, Frame::Inertial>,
+                   tmpl::append<argument_tags, auxiliary_fields>>(
+      fluxes_computer, make_not_null(&box));
+  db::mutate_apply<db::wrap_tags_in<::Tags::Flux, auxiliary_fields,
+                                    tmpl::size_t<volume_dim>, Frame::Inertial>,
+                   tmpl::append<argument_tags, primal_fields>>(
+      fluxes_computer, make_not_null(&box));
+  CHECK(expected_fluxes == get<fluxes_tag>(box));
+}
+
+/*!
+ * \brief Test the `System::sources` are functional
+ *
+ * This function tests the following properties of the `System::sources`:
+ *
+ * - They work with the `elliptic::first_order_sources` function.
+ * - They can be applied to a DataBox, i.e. their argument tags are consistent
+ *   with their apply function.
+ */
+template <typename System>
+void test_first_order_sources_computer(const DataVector& used_for_size) {
+  using SourcesComputer = typename System::sources;
+  static constexpr size_t volume_dim = System::volume_dim;
+  using vars_tag = typename System::fields_tag;
+  using primal_fields = typename System::primal_fields;
+  using auxiliary_fields = typename System::auxiliary_fields;
+  using VarsType = typename vars_tag::type;
+  using fluxes_tag =
+      db::add_tag_prefix<::Tags::Flux, vars_tag, tmpl::size_t<volume_dim>,
+                         Frame::Inertial>;
+  using FluxesType = typename fluxes_tag::type;
+  using sources_tag = db::add_tag_prefix<::Tags::Source, vars_tag>;
+  using argument_tags = typename SourcesComputer::argument_tags;
+
+  MAKE_GENERATOR(generator);
+  std::uniform_real_distribution<> dist(-1., 1.);
+
+  // Generate random variables and fluxes. Note that the sources make use of the
+  // pre-computed fluxes as an optimization (see elliptic::first_order_sources),
+  // but for the purpose of this random-value test the fluxes don't have to be
+  // computed from the variables.
+  const auto vars = make_with_random_values<VarsType>(
+      make_not_null(&generator), make_not_null(&dist), used_for_size);
+  const auto fluxes = make_with_random_values<FluxesType>(
+      make_not_null(&generator), make_not_null(&dist), used_for_size);
+
+  // Generate sources from the variables with random arguments
+  tuples::tagged_tuple_from_typelist<argument_tags> args{};
+  tmpl::for_each<argument_tags>(
+      [&args, &generator, &dist, &used_for_size](auto tag_v) {
+        using tag = tmpl::type_from<decltype(tag_v)>;
+        get<tag>(args) = make_with_random_values<typename tag::type>(
+            make_not_null(&generator), make_not_null(&dist), used_for_size);
+      });
+  const auto expected_sources = tuples::apply(
+      [&vars, &fluxes](const auto&... expanded_args) {
+        return ::elliptic::first_order_sources<
+            volume_dim, primal_fields, auxiliary_fields, SourcesComputer>(
+            vars, fluxes, expanded_args...);
+      },
+      args);
+
+  // Create a DataBox
+  auto box = tuples::apply(
+      [&vars, &fluxes, &used_for_size](const auto&... expanded_args) {
+        return db::create<
+            tmpl::append<db::AddSimpleTags<vars_tag, fluxes_tag, sources_tag>,
+                         argument_tags>>(
+            vars, fluxes,
+            make_with_value<typename sources_tag::type>(
+                used_for_size, std::numeric_limits<double>::signaling_NaN()),
+            expanded_args...);
+      },
+      args);
+  tmpl::for_each<auxiliary_fields>([&vars, &box](auto tag_v) {
+    using tag = tmpl::type_from<decltype(tag_v)>;
+    db::mutate<::Tags::Source<tag>>(
+        make_not_null(&box),
+        [&vars](const auto aux_source) { *aux_source = get<tag>(vars); });
+  });
+
+  // Apply the sources computer to the DataBox
+  db::mutate_apply<
+      db::wrap_tags_in<::Tags::Source, typename vars_tag::tags_list>,
+      tmpl::append<
+          argument_tags, primal_fields,
+          db::wrap_tags_in<::Tags::Flux, primal_fields,
+                           tmpl::size_t<volume_dim>, Frame::Inertial>>>(
+      SourcesComputer{}, make_not_null(&box));
+  CHECK(expected_sources == get<sources_tag>(box));
+}
+
+}  // namespace elliptic
+}  // namespace TestHelpers
