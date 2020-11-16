2c2734bab778f384965a421310a862bb329ef0fc
Prayush Kumar <prayush@astro.cornell.edu>
2020-11-16 13:17:24 -0800

    Add ComputeTag to compute Analytic Data

--- tests/Unit/Evolution/Test_ComputeTags.cpp
+++ tests/Unit/Evolution/Test_ComputeTags.cpp
@@ -41,20 +41,51 @@ struct AnalyticSolutionTag : db::SimpleTag {
   using type = AnalyticSolution;
 };
 
+struct AnalyticData {
+  tuples::TaggedTuple<FieldTag> variables(
+      const tnsr::I<DataVector, 1>& x,
+      const tmpl::list<FieldTag> /*meta*/) const noexcept {
+    return {Scalar<DataVector>{1. * get<0>(x)}};
+  }
+  void pup(PUP::er& /*p*/) noexcept {}  // NOLINT
+};
+
+struct AnalyticDataTag : db::SimpleTag {
+  using type = AnalyticData;
+};
+
 }  // namespace
 SPECTRE_TEST_CASE("Unit.Evolution.ComputeTags", "[Unit][Evolution]") {
-  tnsr::I<DataVector, 1, Frame::Inertial> inertial_coords{{{{1., 2., 3., 4.}}}};
   const double current_time = 2.;
-  const auto box = db::create<
-      db::AddSimpleTags<domain::Tags::Coordinates<1, Frame::Inertial>,
-                        AnalyticSolutionTag, Tags::Time>,
-      db::AddComputeTags<evolution::Tags::AnalyticCompute<
-          1, AnalyticSolutionTag, tmpl::list<FieldTag>>>>(
-      std::move(inertial_coords), AnalyticSolution{}, current_time);
-  const DataVector expected{2., 4., 6., 8.};
-  CHECK_ITERABLE_APPROX(get(get<::Tags::Analytic<FieldTag>>(box)), expected);
+  {
+    tnsr::I<DataVector, 1, Frame::Inertial> inertial_coords{
+        {{{1., 2., 3., 4.}}}};
+    const auto box = db::create<
+        db::AddSimpleTags<domain::Tags::Coordinates<1, Frame::Inertial>,
+                          AnalyticSolutionTag, Tags::Time>,
+        db::AddComputeTags<evolution::Tags::AnalyticCompute<
+            1, AnalyticSolutionTag, tmpl::list<FieldTag>>>>(
+        std::move(inertial_coords), AnalyticSolution{}, current_time);
+    const DataVector expected{2., 4., 6., 8.};
+    CHECK_ITERABLE_APPROX(get(get<::Tags::Analytic<FieldTag>>(box)), expected);
+  }
+  {
+    tnsr::I<DataVector, 1, Frame::Inertial> inertial_coords{
+        {{{1., 2., 3., 4.}}}};
+    const auto box = db::create<
+        db::AddSimpleTags<domain::Tags::Coordinates<1, Frame::Inertial>,
+                          AnalyticDataTag>,
+        db::AddComputeTags<evolution::Tags::AnalyticDataCompute<
+            1, AnalyticDataTag, tmpl::list<FieldTag>>>>(
+        std::move(inertial_coords), AnalyticData{});
+    const DataVector expected{1., 2., 3., 4.};
+    CHECK_ITERABLE_APPROX(get(get<::Tags::Analytic<FieldTag>>(box)), expected);
+  }
 
   TestHelpers::db::test_compute_tag<evolution::Tags::AnalyticCompute<
+      1, AnalyticSolutionTag, tmpl::list<FieldTag>>>(
+      "Variables(Analytic(FieldTag))");
+  TestHelpers::db::test_compute_tag<evolution::Tags::AnalyticDataCompute<
       1, AnalyticSolutionTag, tmpl::list<FieldTag>>>(
       "Variables(Analytic(FieldTag))");
 }
