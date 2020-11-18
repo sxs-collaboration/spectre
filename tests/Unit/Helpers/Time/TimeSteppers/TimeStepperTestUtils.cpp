54a8f8b951926d0e56823a479cf371f8aa568a41
William Throwe <wtt6@cornell.edu>
2020-11-17 21:25:36 -0500

    Improve robustness of TimeStepper order tests
    Particularly for the substep dense output tests, the accuracy of
    measurements can fluctuate significantly around the asymptotic
    convergence line.  Fitting to many points gives a more reliable rate
    estimate than a two-point fit.

--- tests/Unit/Helpers/Time/TimeSteppers/TimeStepperTestUtils.cpp
+++ tests/Unit/Helpers/Time/TimeSteppers/TimeStepperTestUtils.cpp
@@ -23,6 +23,7 @@
 #include "Utilities/ConstantExpressions.hpp"
 #include "Utilities/ErrorHandling/Assert.hpp"
 #include "Utilities/Gsl.hpp"
+#include "Utilities/Numeric.hpp"
 
 namespace TimeStepperTestUtils {
 
@@ -93,6 +94,35 @@ void initialize_history(
         TimeStepId(step_size.is_positive(), slab_number, time),
         analytic(time.value()), rhs(analytic(time.value()), time.value()));
   }
+}
+
+template <typename F>
+double convergence_rate(const int32_t large_steps, const int32_t small_steps,
+                        F&& error) noexcept {
+  // We do a least squares fit on a log-log error-vs-steps plot.  The
+  // unequal points caused by the log scale will introduce some bias,
+  // but the typical range this is used for is only a factor of a few,
+  // so it shouldn't be too bad.
+  const auto num_tests = static_cast<size_t>(small_steps - large_steps) + 1;
+  std::vector<double> log_steps;
+  std::vector<double> log_errors;
+  log_steps.reserve(num_tests);
+  log_errors.reserve(num_tests);
+  for (auto steps = large_steps; steps <= small_steps; ++steps) {
+    log_steps.push_back(log(steps));
+    log_errors.push_back(log(abs(error(steps))));
+  }
+  const double average_log_steps = alg::accumulate(log_steps, 0.0) / num_tests;
+  const double average_log_errors =
+      alg::accumulate(log_errors, 0.0) / num_tests;
+  double numerator = 0.0;
+  double denominator = 0.0;
+  for (size_t i = 0; i < num_tests; ++i) {
+    numerator += (log_steps[i] - average_log_steps) *
+        (log_errors[i] - average_log_errors);
+    denominator += square(log_steps[i] - average_log_steps);
+  }
+  return -numerator / denominator;
 }
 }  // namespace
 
@@ -419,8 +449,7 @@ void check_convergence_order(const TimeStepper& stepper,
   const int32_t large_steps = 10;
   // The high-order solvers have round-off error around here
   const int32_t small_steps = 40;
-  CHECK((log(do_integral(large_steps)) - log(do_integral(small_steps))) /
-            (log(small_steps) - log(large_steps)) ==
+  CHECK(convergence_rate(large_steps, small_steps, do_integral) ==
         approx(expected_order).margin(0.4));
 }
 
@@ -482,8 +511,7 @@ void check_dense_output(const TimeStepper& stepper,
       return abs(get_dense(slab.duration() / steps, 0.25 * M_PI) -
                  exp(0.25 * M_PI));
     };
-    CHECK((log(error(large_steps)) - log(error(small_steps))) /
-              (log(small_steps) - log(large_steps)) ==
+    CHECK(convergence_rate(large_steps, small_steps, error) ==
           approx(expected_order).margin(0.4));
   }
 }
