fb18f655f002484e12ad61816b1fc7d14af3b1a8
Nils Deppe <nd357@cornell.edu>
2020-11-23 12:07:55 -0800

    Add FloatingPointType enum
    This enum allows specifying whether to use single or double precision floating
    point types at runtime. One example use case is when observing volume data we
    generally only need single precision for visualization.

---
+++ tests/Unit/DataStructures/Test_FloatingPointType.cpp
@@ -0,0 +1,29 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <string>
+
+#include "DataStructures/FloatingPointType.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Utilities/GetOutput.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace {
+template <FloatingPointType FloatType>
+void test_construct_from_options() noexcept {
+  const auto created =
+      TestHelpers::test_creation<FloatingPointType>(get_output(FloatType));
+  CHECK(created == FloatType);
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.DataStructures.FloatingPointType",
+                  "[Unit][DataStructures]") {
+  CHECK(get_output(FloatingPointType::Float) == "Float");
+  CHECK(get_output(FloatingPointType::Double) == "Double");
+
+  test_construct_from_options<FloatingPointType::Float>();
+  test_construct_from_options<FloatingPointType::Double>();
+}
