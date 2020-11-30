e165319f89fea028d85ca1334449469adab546e2
Nils Deppe <nd357@cornell.edu>
2020-11-30 13:05:10 -0800

    Allow disabling git hooks
    I've found that the git hooks get set from build directories inside the
    container, but when set with e.g. python2 I can't commit from outside the
    container anymore. We will (rarely) need to disable adding the hooks.

--- cmake/SetupGitHooks.cmake
+++ cmake/SetupGitHooks.cmake
@@ -1,34 +1,46 @@
 # Distributed under the MIT License.
 # See LICENSE.txt for details.
 
-# Check that the source dir is writable. If it is we set up git hooks, if not
-# then there probably won't be any commits anyway...
-EXECUTE_PROCESS(COMMAND test -w ${CMAKE_SOURCE_DIR}
+# Allow disabling the Git hooks because if you are running the code in a
+# container the host may not have all the right things installed. For example,
+# if you are using python2 in the container, the host may not have python2
+# installed and we should instead be setting up hooks from the host or an
+# environment suitably similar to where the commits will be made.
+option(
+  USE_GIT_HOOKS
+  "Set up the git hooks for sanity checks."
+  ON)
+
+if(USE_GIT_HOOKS)
+  # Check that the source dir is writable. If it is we set up git hooks, if not
+  # then there probably won't be any commits anyway...
+  EXECUTE_PROCESS(COMMAND test -w ${CMAKE_SOURCE_DIR}
     RESULT_VARIABLE CHECK_SOURCE_DIR_WRITABLE_RESULT)
 
-# The logic is inverted because shell
-if(NOT CHECK_SOURCE_DIR_WRITABLE_RESULT AND EXISTS ${CMAKE_SOURCE_DIR}/.git)
-  find_package(PythonInterp REQUIRED)
+  # The logic is inverted because shell
+  if(NOT CHECK_SOURCE_DIR_WRITABLE_RESULT AND EXISTS ${CMAKE_SOURCE_DIR}/.git)
+    find_package(PythonInterp REQUIRED)
 
-  find_package(Git REQUIRED)
+    find_package(Git REQUIRED)
 
-  # We use several client-side git hooks to ensure commits are correct as
-  # early as possible.
-  configure_file(
+    # We use several client-side git hooks to ensure commits are correct as
+    # early as possible.
+    configure_file(
       ${CMAKE_SOURCE_DIR}/tools/Hooks/pre-commit.sh
       ${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit
       @ONLY
-  )
+      )
 
-  configure_file(
+    configure_file(
       ${CMAKE_SOURCE_DIR}/tools/Hooks/ClangFormat.py
       ${CMAKE_SOURCE_DIR}/.git/hooks/ClangFormat.py
       @ONLY
-  )
+      )
 
-  configure_file(
+    configure_file(
       ${CMAKE_SOURCE_DIR}/tools/Hooks/CheckFileSize.py
       ${CMAKE_SOURCE_DIR}/.git/hooks/CheckFileSize.py
       @ONLY
-  )
+      )
+  endif()
 endif()
