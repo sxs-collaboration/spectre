945936f849988a3ca3a6c0278ff5c1c3507d1430
Jordan Moxon <jem497@cornell.edu>
2020-12-01 16:27:22 -0800

    Add not_null version of serialization tool
    Several classes, including charm++ chares have deleted copy constructors of the
    base class, which are required for the return-by-value version of `deserialize`
    and `serialize_and_deserialize` in the `TestHelpers.
    This adds a return by not_null pointer version of the `deserialize` and
    `serialize_and_deserialize` tools to support testing the serialization of
    non-copyable classes

--- src/Parallel/Serialize.hpp
+++ src/Parallel/Serialize.hpp
@@ -6,9 +6,10 @@
 
 #pragma once
 
+#include <pup.h>
 #include <vector>
 
-#include <pup.h>
+#include "Utilities/Gsl.hpp"
 
 /*!
  * \ingroup ParallelGroup
@@ -21,10 +22,8 @@
  *
  * \tparam T type to serialize
  */
-template <typename T, typename U>
-std::vector<char> serialize(const U& obj) noexcept {
-  static_assert(std::is_same<T, U>::value,
-                "Explicit type for serialization differs from deduced type");
+template <typename T>
+std::vector<char> serialize(const T& obj) noexcept {
   const T& typed_obj = obj;
   // pup routine is non-const, but shouldn't modify anything in serialization
   // mode.
@@ -53,4 +52,18 @@ T deserialize(const void* const data) noexcept {  // NOLINT
   T result{};
   reader | result;
   return result;
+}
+
+/*!
+ * \ingroup ParallelGroup
+ * \brief Deserialize an object using PUP.
+ *
+ * \tparam T the type to deserialize to.
+ */
+template <typename T>
+void deserialize(const gsl::not_null<T*> result,
+                 const void* const data) noexcept {  // NOLINT
+  // clang-tidy: no const in forward decl (this is a definition)
+  PUP::fromMem reader(data);
+  reader | *result;
 }
