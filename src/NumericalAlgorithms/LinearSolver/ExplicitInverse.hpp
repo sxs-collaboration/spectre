f9c039ccc19f1b48c4d78ab271604137ba55e054
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-12-06 14:38:37 +0100

    Add ExplicitInverse linear solver
    This solver explicitly builds the operator matrix and inverts it.
    Mostly useful for testing, but can actually speed up convergence of
    production problems if no better preconditioner is available.

---
+++ src/NumericalAlgorithms/LinearSolver/ExplicitInverse.hpp
@@ -0,0 +1,248 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <vector>
+
+#include "DataStructures/DenseMatrix.hpp"
+#include "DataStructures/DenseVector.hpp"
+#include "NumericalAlgorithms/Convergence/HasConverged.hpp"
+#include "NumericalAlgorithms/LinearSolver/LinearSolver.hpp"
+#include "Options/Options.hpp"
+#include "Parallel/CharmPupable.hpp"
+#include "Utilities/MakeWithValue.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TypeTraits/CreateIsCallable.hpp"
+
+namespace LinearSolver::Serial {
+
+/// \cond
+template <typename LinearSolverRegistrars>
+struct ExplicitInverse;
+/// \endcond
+
+namespace Registrars {
+/// Registers the `LinearSolver::Serial::ExplicitInverse` linear solver
+using ExplicitInverse = Registration::Registrar<Serial::ExplicitInverse>;
+}  // namespace Registrars
+
+namespace detail {
+CREATE_IS_CALLABLE(reset)
+CREATE_IS_CALLABLE_V(reset)
+}  // namespace detail
+
+/*!
+ * \brief Linear solver that builds a matrix representation of the linear
+ * operator and inverts it directly
+ *
+ * This solver first constructs an explicit matrix representation by "sniffing
+ * out" the operator, i.e. feeding it with unit vectors, and then directly
+ * inverts the matrix. The result is an operator that solves the linear problem
+ * in a single step. This means that each element has a large initialization
+ * cost, but all successive solves converge immediately.
+ *
+ * \par Advice on using this linear solver:
+ *
+ * - This solver is entirely agnostic to the structure of the linear operator.
+ *   It is usually better to implement a linear solver that is specialized for
+ *   your linear operator to take advantage of its properties. For example, if
+ *   the operator has a tensor-product structure, the linear solver might take
+ *   advantage of that. Only use this solver if no alternatives are available
+ *   and if you have verified that it speeds up your solves.
+ * - Since this linear solver stores the full inverse operator matrix it can
+ *   have significant memory demands. For example, an operator representing a 3D
+ *   first-order Elasticity system (9 variables) discretized on 12 grid points
+ *   per dimension requires ca. 2GB of memory (per element) to store the matrix,
+ *   scaling quadratically with the number of variables and with a power of 6
+ *   with the number of grid points per dimension. Therefore, make sure to
+ *   distribute the elements on a sufficient number of nodes to meet the memory
+ *   requirements.
+ * - This linear solver can be `reset()` when the operator changes (e.g. in each
+ *   nonlinear-solver iteration). However, when using this solver as
+ *   preconditioner it can be advantageous to avoid the reset and the
+ *   corresponding cost of re-building the matrix and its inverse if the
+ *   operator only changes "a little". In that case the preconditioner solves
+ *   subdomain problems only approximately, but possibly still sufficiently to
+ *   provide effective preconditioning. You can toggle this behaviour with the
+ *   `EnableResets` option.
+ */
+template <typename LinearSolverRegistrars =
+              tmpl::list<Registrars::ExplicitInverse>>
+class ExplicitInverse : public LinearSolver<LinearSolverRegistrars> {
+ private:
+  using Base = LinearSolver<LinearSolverRegistrars>;
+
+ public:
+  struct EnableResets {
+    using type = bool;
+    static constexpr Options::String help =
+        "Enable or disable resets. This only has an effect in cases where the "
+        "operator changes, e.g. between nonlinear-solver iterations. Only "
+        "disable resets when using this solver as preconditioner and thus "
+        "approximate solutions are desirable. Disabling resets avoids "
+        "expensive re-building of the operator, but comes at the cost of less "
+        "accurate preconditioning and thus potentially more preconditioned "
+        "iterations. Whether or not this helps convergence overall is highly "
+        "problem-dependent.";
+  };
+
+  using options = tmpl::list<EnableResets>;
+  static constexpr Options::String help =
+      "Build a matrix representation of the linear operator and invert it "
+      "directly. This means that the first solve has a large initialization "
+      "cost, but all subsequent solves converge immediately.";
+
+  ExplicitInverse() = default;
+  ExplicitInverse(const ExplicitInverse& /*rhs*/) = default;
+  ExplicitInverse& operator=(const ExplicitInverse& /*rhs*/) = default;
+  ExplicitInverse(ExplicitInverse&& /*rhs*/) = default;
+  ExplicitInverse& operator=(ExplicitInverse&& /*rhs*/) = default;
+  ~ExplicitInverse() = default;
+
+  explicit ExplicitInverse(bool enable_resets) noexcept
+      : enable_resets_(enable_resets) {}
+
+  /// \cond
+  explicit ExplicitInverse(CkMigrateMessage* m) noexcept : Base(m) {}
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(ExplicitInverse);  // NOLINT
+  /// \endcond
+
+  /*!
+   * \brief Solve the equation \f$Ax=b\f$ by explicitly constructing the
+   * operator matrix \f$A\f$ and its inverse. The first solve is computationally
+   * expensive and successive solves are cheap.
+   *
+   * Building a matrix representation of the `linear_operator` requires
+   * iterating over the `SourceType` (which is also the type returned by the
+   * `linear_operator`) in a consistent way. This can be non-trivial for
+   * heterogeneous data structures because it requires they define a data
+   * ordering. Specifically, the `SourceType` must have a `size()` function as
+   * well as `begin()` and `end()` iterators that point into the data. If the
+   * iterators have a `reset()` function it is used to avoid repeatedly
+   * re-creating the `begin()` iterator. The `reset()` function must not
+   * invalidate the `end()` iterator.
+   */
+  template <typename LinearOperator, typename VarsType, typename SourceType>
+  Convergence::HasConverged solve(gsl::not_null<VarsType*> solution,
+                                  const LinearOperator& linear_operator,
+                                  const SourceType& source) const noexcept;
+
+  /// Flags the operator to require re-initialization. No memory is released.
+  /// Call this function to rebuild the solver when the operator changed.
+  void reset() noexcept override {
+    if (not enable_resets_) {
+      return;
+    }
+    size_ = std::numeric_limits<size_t>::max();
+  }
+
+  /// Size of the operator. The stored matrix will have `size^2` entries.
+  size_t size() const noexcept { return size_; }
+
+  /// The matrix representation of the solver. This matrix approximates the
+  /// inverse of the subdomain operator.
+  const DenseMatrix<double>& matrix_representation() const noexcept {
+    return inverse_;
+  }
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& p) noexcept override {
+    p | enable_resets_;
+    p | size_;
+    p | inverse_;
+    if (p.isUnpacking() and size_ != std::numeric_limits<size_t>::max()) {
+      source_workspace_.resize(size_);
+      solution_workspace_.resize(size_);
+    }
+  }
+
+  std::unique_ptr<Base> get_clone() const noexcept override {
+    return std::make_unique<ExplicitInverse>(*this);
+  }
+
+ private:
+  bool enable_resets_{};
+
+  // Caches for successive solves of the same operator
+  mutable size_t size_ = std::numeric_limits<size_t>::max();
+  mutable DenseMatrix<double, blaze::columnMajor> inverse_{};
+
+  // Buffers to avoid re-allocating memory for applying the operator
+  mutable DenseVector<double> source_workspace_{};
+  mutable DenseVector<double> solution_workspace_{};
+};
+
+template <typename LinearSolverRegistrars>
+template <typename LinearOperator, typename VarsType, typename SourceType>
+Convergence::HasConverged ExplicitInverse<LinearSolverRegistrars>::solve(
+    const gsl::not_null<VarsType*> solution,
+    const LinearOperator& linear_operator,
+    const SourceType& source) const noexcept {
+  if (UNLIKELY(size_ == std::numeric_limits<size_t>::max())) {
+    const auto& used_for_size = source;
+    size_ = used_for_size.size();
+    source_workspace_.resize(size_);
+    solution_workspace_.resize(size_);
+    inverse_.resize(size_, size_);
+    // Construct explicit matrix representation by "sniffing out" the operator,
+    // i.e. feeding it unit vectors
+    auto unit_vector = make_with_value<VarsType>(used_for_size, 0.);
+    auto result_buffer = make_with_value<SourceType>(used_for_size, 0.);
+    auto& operator_matrix = inverse_;
+    size_t i = 0;
+    // Re-using the iterators for all operator invocations
+    auto result_iterator_begin = result_buffer.begin();
+    auto result_iterator_end = result_buffer.end();
+    for (double& unit_vector_data : unit_vector) {
+      // Add a 1 at the unit vector location i
+      unit_vector_data = 1.;
+      // Invoke the operator on the unit vector
+      linear_operator(make_not_null(&result_buffer), unit_vector);
+      // Set the unit vector back to zero
+      unit_vector_data = 0.;
+      // Reset the iterator by calling its `reset` member function or by
+      // re-creating it
+      if constexpr (detail::is_reset_callable_v<decltype(
+                        result_iterator_begin)>) {
+        result_iterator_begin.reset();
+      } else {
+        result_iterator_begin = result_buffer.begin();
+        result_iterator_end = result_buffer.end();
+      }
+      // Store the result in column i of the matrix
+      std::copy(result_iterator_begin, result_iterator_end,
+                column(operator_matrix, i).begin());
+      ++i;
+    }
+    // Directly invert the matrix
+    try {
+      blaze::invert(inverse_);
+    } catch (const std::invalid_argument& e) {
+      ERROR("Could not invert subdomain matrix (size " << size_
+                                                       << "): " << e.what());
+    }
+  }
+  // Copy source into contiguous workspace. In cases where the source and
+  // solution data are already stored contiguously we might avoid the copy and
+  // the associated workspace memory. However, compared to the cost of building
+  // and storing the matrix this is likely insignificant.
+  std::copy(source.begin(), source.end(), source_workspace_.begin());
+  // Apply inverse
+  solution_workspace_ = inverse_ * source_workspace_;
+  // Reconstruct solution data from contiguous workspace
+  std::copy(solution_workspace_.begin(), solution_workspace_.end(),
+            solution->begin());
+  return {0, 0};
+}
+
+/// \cond
+template <typename LinearSolverRegistrars>
+// NOLINTNEXTLINE
+PUP::able::PUP_ID ExplicitInverse<LinearSolverRegistrars>::my_PUP_ID = 0;
+/// \endcond
+
+}  // namespace LinearSolver::Serial
---
+++ tests/Unit/NumericalAlgorithms/LinearSolver/Test_ExplicitInverse.cpp
@@ -0,0 +1,131 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <functional>
+#include <utility>
+
+#include "DataStructures/ApplyMatrices.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/DenseMatrix.hpp"
+#include "DataStructures/DenseVector.hpp"
+#include "DataStructures/Matrix.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/Structure/Direction.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/NumericalAlgorithms/LinearSolver/TestHelpers.hpp"
+#include "NumericalAlgorithms/LinearSolver/ExplicitInverse.hpp"
+#include "ParallelAlgorithms/LinearSolver/Schwarz/ElementCenteredSubdomainData.hpp"
+#include "ParallelAlgorithms/LinearSolver/Schwarz/OverlapHelpers.hpp"
+#include "Utilities/MakeWithValue.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace helpers = TestHelpers::LinearSolver;
+
+namespace {
+struct ScalarFieldTag {
+  using type = Scalar<DataVector>;
+};
+}  // namespace
+
+namespace LinearSolver::Serial {
+
+SPECTRE_TEST_CASE("Unit.LinearSolver.Serial.ExplicitInverse",
+                  "[Unit][NumericalAlgorithms][LinearSolver]") {
+  {
+    INFO("Solve a simple matrix");
+    const DenseMatrix<double> matrix{{4., 1.}, {3., 1.}};
+    const helpers::ApplyMatrix linear_operator{matrix};
+    const DenseVector<double> source{1., 2.};
+    const DenseVector<double> expected_solution{-1., 5.};
+    DenseVector<double> solution(2);
+    const ExplicitInverse<> solver{true};
+    const auto has_converged =
+        solver.solve(make_not_null(&solution), linear_operator, source);
+    REQUIRE(has_converged);
+    CHECK_MATRIX_APPROX(solver.matrix_representation(), blaze::inv(matrix));
+    CHECK_ITERABLE_APPROX(solution, expected_solution);
+    {
+      INFO("Resetting");
+      ExplicitInverse<> resetting_solver{true};
+      resetting_solver.solve(make_not_null(&solution), linear_operator, source);
+      // Solving a different operator after resetting should work
+      resetting_solver.reset();
+      const DenseMatrix<double> matrix2{{4., 1.}, {1., 3.}};
+      const helpers::ApplyMatrix linear_operator2{matrix2};
+      const DenseVector<double> expected_solution2{0.0909090909090909,
+                                                   0.6363636363636364};
+      resetting_solver.solve(make_not_null(&solution), linear_operator2,
+                             source);
+      CHECK_MATRIX_APPROX(resetting_solver.matrix_representation(),
+                          blaze::inv(matrix2));
+      CHECK_ITERABLE_APPROX(solution, expected_solution2);
+      // When resetting is disabled, the solver should keep applying the cached
+      // inverse even when solving a different operator
+      ExplicitInverse<> non_resetting_solver{false};
+      non_resetting_solver.solve(make_not_null(&solution), linear_operator,
+                                 source);
+      non_resetting_solver.reset();
+      non_resetting_solver.solve(make_not_null(&solution), linear_operator2,
+                                 source);
+      // Still the inverse of the operator we solved first
+      CHECK_MATRIX_APPROX(non_resetting_solver.matrix_representation(),
+                          blaze::inv(matrix));
+      CHECK_ITERABLE_APPROX(solution, blaze::inv(matrix) * source);
+    }
+  }
+  {
+    INFO("Solve a heterogeneous data structure");
+    using SubdomainData = ::LinearSolver::Schwarz::ElementCenteredSubdomainData<
+        1, tmpl::list<ScalarFieldTag>>;
+
+    const Matrix matrix_element{{4., 1., 1.}, {1., 1., 3.}, {0., 2., 0.}};
+    const Matrix matrix_overlap{{4., 1.}, {3., 1.}};
+    const ::LinearSolver::Schwarz::OverlapId<1> overlap_id{
+        Direction<1>::lower_xi(), ElementId<1>{0}};
+    const std::array<std::reference_wrapper<const Matrix>, 1> matrices_element{
+        matrix_element};
+    const std::array<std::reference_wrapper<const Matrix>, 1> matrices_overlap{
+        matrix_overlap};
+    const auto linear_operator = [&matrices_element, &matrices_overlap,
+                                  &overlap_id](
+                                     const gsl::not_null<SubdomainData*> result,
+                                     const SubdomainData& operand) noexcept {
+      apply_matrices(make_not_null(&result->element_data), matrices_element,
+                     operand.element_data, Index<1>{3});
+      apply_matrices(make_not_null(&result->overlap_data.at(overlap_id)),
+                     matrices_overlap, operand.overlap_data.at(overlap_id),
+                     Index<1>{2});
+    };
+
+    SubdomainData source{3};
+    get(get<ScalarFieldTag>(source.element_data)) = DataVector{1., 2., 1.};
+    source.overlap_data.emplace(overlap_id,
+                                typename SubdomainData::OverlapData{2});
+    get(get<ScalarFieldTag>(source.overlap_data.at(overlap_id))) =
+        DataVector{1., 2.};
+    auto expected_solution = make_with_value<SubdomainData>(source, 0.);
+    get(get<ScalarFieldTag>(expected_solution.element_data)) =
+        DataVector{0., 0.5, 0.5};
+    get(get<ScalarFieldTag>(expected_solution.overlap_data.at(overlap_id))) =
+        DataVector{-1., 5.};
+
+    const ExplicitInverse<> solver{true};
+    auto solution = make_with_value<SubdomainData>(source, 0.);
+    solver.solve(make_not_null(&solution), linear_operator, source);
+    CHECK(solver.size() == 5);
+    Matrix expected_matrix(5, 5, 0.);
+    blaze::submatrix(expected_matrix, 0, 0, 3, 3) = matrix_element;
+    blaze::submatrix(expected_matrix, 3, 3, 2, 2) = matrix_overlap;
+    blaze::invert(expected_matrix);
+    CHECK_MATRIX_APPROX(solver.matrix_representation(), expected_matrix);
+    CHECK_VARIABLES_APPROX(solution.element_data,
+                           expected_solution.element_data);
+    CHECK_VARIABLES_APPROX(solution.overlap_data.at(overlap_id),
+                           expected_solution.overlap_data.at(overlap_id));
+  }
+}
+
+}  // namespace LinearSolver::Serial
