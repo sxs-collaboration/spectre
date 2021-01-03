40bdb58bbc70f39fee4eada5dc8dfedde8a7f9aa
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-01-03 02:47:28 +0100

    Make git description in build system more robust

--- cmake/SpectreGetGitHash.cmake
+++ cmake/SpectreGetGitHash.cmake
@@ -22,12 +22,21 @@ if(EXISTS ${CMAKE_SOURCE_DIR}/.git)
       OUTPUT_STRIP_TRAILING_WHITESPACE
       )
     execute_process(
-      COMMAND ${GIT_EXECUTABLE} describe HEAD
+      COMMAND ${GIT_EXECUTABLE} describe
+        --always --first-parent --match "v[0-9]*" HEAD
       WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
       OUTPUT_VARIABLE GIT_DESCRIPTION
       OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_VARIABLE GIT_DESCRIPTION_ERROR
+      ERROR_STRIP_TRAILING_WHITESPACE
       )
-    message(STATUS "Git description: ${GIT_DESCRIPTION}")
+    if(GIT_DESCRIPTION)
+      message(STATUS "Git description: ${GIT_DESCRIPTION}")
+    else()
+      message(STATUS "Could not determine git description ("
+        "${GIT_DESCRIPTION_ERROR}). Using commit hash instead.")
+      set(GIT_DESCRIPTION ${GIT_HASH})
+    endif()
   endif()
 else()
   message(STATUS "Not running in a git repository. Some features are disabled.")
