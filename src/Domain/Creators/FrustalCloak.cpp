8b04efa5218c54fd4b8460269a7187c61b6d8dc9
Nils Deppe <nd357@cornell.edu>
2021-01-18 16:25:46 -0800

    Add boundary condition support to FrustalCloak domain creator

--- src/Domain/Creators/FrustalCloak.cpp
+++ src/Domain/Creators/FrustalCloak.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "Domain/Block.hpp"                   // IWYU pragma: keep
+#include "Domain/BoundaryConditions/Periodic.hpp"
 #include "Domain/Creators/DomainCreator.hpp"  // IWYU pragma: keep
 #include "Domain/Domain.hpp"
 #include "Domain/DomainHelpers.hpp"
@@ -35,7 +36,9 @@ FrustalCloak::FrustalCloak(
     typename LengthInnerCube::type length_inner_cube,
     typename LengthOuterCube::type length_outer_cube,
     typename OriginPreimage::type origin_preimage,
-    const Options::Context& /*context*/) noexcept
+    std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+        boundary_condition,
+    const Options::Context& context)
     // clang-tidy: trivially copyable
     : initial_refinement_level_(                      // NOLINT
           std::move(initial_refinement_level)),       // NOLINT
@@ -45,7 +48,15 @@ FrustalCloak::FrustalCloak(
       projection_factor_(projection_factor),          // NOLINT
       length_inner_cube_(length_inner_cube),          // NOLINT
       length_outer_cube_(length_outer_cube),          // NOLINT
-      origin_preimage_(origin_preimage) {}            // NOLINT
+      origin_preimage_(origin_preimage),              // NOLINT
+      boundary_condition_(std::move(boundary_condition)) {
+  using domain::BoundaryConditions::is_periodic;
+  if (boundary_condition_ != nullptr and is_periodic(boundary_condition_)) {
+    PARSE_ERROR(
+        context,
+        "Cannot have periodic boundary conditions with a frustal cloak");
+  }
+}
 
 Domain<3> FrustalCloak::create_domain() const noexcept {
   std::vector<
@@ -53,9 +64,24 @@ Domain<3> FrustalCloak::create_domain() const noexcept {
       coord_maps = frustum_coordinate_maps<Frame::Inertial>(
           length_inner_cube_, length_outer_cube_, use_equiangular_map_,
           origin_preimage_, projection_factor_);
+  std::vector<DirectionMap<
+      3, std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>>>
+      boundary_conditions_all_blocks{};
+  if (boundary_condition_ != nullptr) {
+    boundary_conditions_all_blocks.resize(10);
+    for (auto& boundary_conditions : boundary_conditions_all_blocks) {
+      boundary_conditions[Direction<3>::lower_zeta()] =
+          boundary_condition_->get_clone();
+      boundary_conditions[Direction<3>::upper_zeta()] =
+          boundary_condition_->get_clone();
+    }
+  }
+
   return Domain<3>{std::move(coord_maps),
                    corners_for_biradially_layered_domains(
-                       0, 1, false, false, {{1, 2, 3, 4, 5, 6, 7, 8}})};
+                       0, 1, false, false, {{1, 2, 3, 4, 5, 6, 7, 8}}),
+                   {},
+                   std::move(boundary_conditions_all_blocks)};
 }
 
 std::vector<std::array<size_t, 3>> FrustalCloak::initial_extents() const
--- src/Domain/Creators/FrustalCloak.hpp
+++ src/Domain/Creators/FrustalCloak.hpp
@@ -8,8 +8,11 @@
 
 #include <array>
 #include <cstddef>
+#include <memory>
 #include <vector>
 
+#include "Domain/BoundaryConditions/BoundaryCondition.hpp"
+#include "Domain/BoundaryConditions/GetBoundaryConditionsBase.hpp"
 #include "Domain/Creators/DomainCreator.hpp"  // IWYU pragma: keep
 #include "Domain/Domain.hpp"
 #include "Options/Options.hpp"
@@ -80,9 +83,29 @@ class FrustalCloak : public DomainCreator<3> {
     static constexpr Options::String help = {"The origin preimage in [x,y,z]."};
   };
 
-  using options = tmpl::list<InitialRefinement, InitialGridPoints,
-                             UseEquiangularMap, ProjectionFactor,
-                             LengthInnerCube, LengthOuterCube, OriginPreimage>;
+  template <typename BoundaryConditionsBase>
+  struct BoundaryCondition {
+    static std::string name() noexcept { return "BoundaryCondition"; }
+    static constexpr Options::String help =
+        "The boundary condition to impose on all sides.";
+    using type = std::unique_ptr<BoundaryConditionsBase>;
+  };
+
+  using basic_options =
+      tmpl::list<InitialRefinement, InitialGridPoints, UseEquiangularMap,
+                 ProjectionFactor, LengthInnerCube, LengthOuterCube,
+                 OriginPreimage>;
+
+  template <typename Metavariables>
+  using options = tmpl::conditional_t<
+      domain::BoundaryConditions::has_boundary_conditions_base_v<
+          typename Metavariables::system>,
+      tmpl::push_back<
+          basic_options,
+          BoundaryCondition<
+              domain::BoundaryConditions::get_boundary_conditions_base<
+                  typename Metavariables::system>>>,
+      basic_options>;
 
   static constexpr Options::String help{
       "Creates a cubical domain with two equal-sized abutting excised cubes\n"
@@ -109,7 +132,9 @@ class FrustalCloak : public DomainCreator<3> {
                typename LengthOuterCube::type length_outer_cube = 0.0,
                typename OriginPreimage::type origin_preimage = {{0.0, 0.0,
                                                                  0.0}},
-               const Options::Context& context = {}) noexcept;
+               std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+                   boundary_condition = nullptr,
+               const Options::Context& context = {});
 
   FrustalCloak() = default;
   FrustalCloak(const FrustalCloak&) = delete;
@@ -133,6 +158,8 @@ class FrustalCloak : public DomainCreator<3> {
   typename LengthInnerCube::type length_inner_cube_{0.0};
   typename LengthOuterCube::type length_outer_cube_{0.0};
   typename OriginPreimage::type origin_preimage_{{0.0, 0.0, 0.0}};
+  std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+      boundary_condition_;
 };
 }  // namespace creators
 }  // namespace domain
