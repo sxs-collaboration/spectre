c5d30677218a80f269b7e69886c14286e02de7a2
Nils Deppe <nd357@cornell.edu>
2021-02-04 17:43:43 -0800

    Add Shu-Osher shock tube analytic data for NewtonianEuler

---
+++ src/PointwiseFunctions/AnalyticData/NewtonianEuler/ShuOsherTube.cpp
@@ -0,0 +1,162 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "PointwiseFunctions/AnalyticData/NewtonianEuler/ShuOsherTube.hpp"
+
+#include <cmath>
+#include <cstddef>
+#include <pup.h>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/IdealFluid.hpp"
+#include "Utilities/ContainerHelpers.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/MakeWithValue.hpp"
+#include "Utilities/Math.hpp"
+
+namespace NewtonianEuler::AnalyticData {
+ShuOsherTube::ShuOsherTube(const double jump_position,
+                           const double mass_density_l, const double velocity_l,
+                           const double pressure_l, const double velocity_r,
+                           const double pressure_r, const double epsilon,
+                           const double lambda) noexcept
+    : mass_density_l_(mass_density_l),
+      velocity_l_(velocity_l),
+      pressure_l_(pressure_l),
+      jump_position_(jump_position),
+      epsilon_(epsilon),
+      lambda_(lambda),
+      velocity_r_(velocity_r),
+      pressure_r_(pressure_r) {
+  ASSERT(mass_density_l_ > 0.0,
+         "The left mass_density must be greater than 0 but is "
+             << mass_density_l_);
+  ASSERT(pressure_l_ > 0.0,
+         "The left pressure must be greater than 0 but is " << pressure_l_);
+  ASSERT(pressure_r_ > 0.0,
+         "The right pressure must be greater than 0 but is " << pressure_r_);
+  ASSERT(epsilon_ > 0.0,
+         "The sinusoid amplitude must be greater than 0 but is " << epsilon_);
+  ASSERT(epsilon_ < 1.0,
+         "The sinusoid amplitude must be less than 1 but is " << epsilon_);
+}
+
+void ShuOsherTube::pup(PUP::er& p) noexcept {
+  p | mass_density_l_;
+  p | velocity_l_;
+  p | pressure_l_;
+  p | jump_position_;
+  p | epsilon_;
+  p | lambda_;
+  p | velocity_r_;
+  p | pressure_r_;
+  p | adiabatic_index_;
+  p | equation_of_state_;
+}
+
+bool operator==(const ShuOsherTube& lhs, const ShuOsherTube& rhs) noexcept {
+  // No comparison for equation_of_state_. Comparing adiabatic_index_ should
+  // suffice.
+  return lhs.mass_density_l_ == rhs.mass_density_l_ and
+         lhs.velocity_l_ == rhs.velocity_l_ and
+         lhs.pressure_l_ == rhs.pressure_l_ and
+         lhs.jump_position_ == rhs.jump_position_ and
+         lhs.epsilon_ == rhs.epsilon_ and lhs.lambda_ == rhs.lambda_ and
+         lhs.velocity_r_ == rhs.velocity_r_ and
+         lhs.pressure_r_ == rhs.pressure_r_ and
+         lhs.adiabatic_index_ == rhs.adiabatic_index_;
+}
+
+bool operator!=(const ShuOsherTube& lhs, const ShuOsherTube& rhs) noexcept {
+  return not(lhs == rhs);
+}
+
+template <typename DataType>
+tuples::TaggedTuple<Tags::MassDensity<DataType>> ShuOsherTube::variables(
+    const tnsr::I<DataType, 1, Frame::Inertial>& x,
+    tmpl::list<Tags::MassDensity<DataType>> /*meta*/) const noexcept {
+  auto mass_density = make_with_value<Scalar<DataType>>(x, 0.0);
+  for (size_t s = 0; s < get_size(get<0>(x)); ++s) {
+    const double x_s = get_element(get<0>(x), s);
+    get_element(get(mass_density), s) =
+        x_s < jump_position_ ? mass_density_l_
+                             : 1.0 + epsilon_ * sin(lambda_ * x_s);
+  }
+  return mass_density;
+}
+
+template <typename DataType>
+tuples::TaggedTuple<Tags::Velocity<DataType, 1, Frame::Inertial>>
+ShuOsherTube::variables(
+    const tnsr::I<DataType, 1, Frame::Inertial>& x,
+    tmpl::list<Tags::Velocity<DataType, 1, Frame::Inertial>> /*meta*/)
+    const noexcept {
+  auto velocity =
+      make_with_value<tnsr::I<DataType, 1, Frame::Inertial>>(x, 0.0);
+  for (size_t s = 0; s < get_size(get<0>(x)); ++s) {
+    const double x_s = get_element(get<0>(x), s);
+    get_element(get<0>(velocity), s) =
+        x_s < jump_position_ ? velocity_l_ : velocity_r_;
+  }
+  return velocity;
+}
+
+template <typename DataType>
+tuples::TaggedTuple<Tags::Pressure<DataType>> ShuOsherTube::variables(
+    const tnsr::I<DataType, 1, Frame::Inertial>& x,
+    tmpl::list<Tags::Pressure<DataType>> /*meta*/) const noexcept {
+  auto pressure = make_with_value<Scalar<DataType>>(x, 0.0);
+  for (size_t s = 0; s < get_size(get<0>(x)); ++s) {
+    const double x_s = get_element(get<0>(x), s);
+    get_element(get(pressure), s) =
+        x_s < jump_position_ ? pressure_l_ : pressure_r_;
+  }
+  return pressure;
+}
+
+template <typename DataType>
+tuples::TaggedTuple<Tags::SpecificInternalEnergy<DataType>>
+ShuOsherTube::variables(
+    const tnsr::I<DataType, 1, Frame::Inertial>& x,
+    tmpl::list<Tags::SpecificInternalEnergy<DataType>> /*meta*/)
+    const noexcept {
+  return equation_of_state_.specific_internal_energy_from_density_and_pressure(
+      get<Tags::MassDensity<DataType>>(
+          variables(x, tmpl::list<Tags::MassDensity<DataType>>{})),
+      get<Tags::Pressure<DataType>>(
+          variables(x, tmpl::list<Tags::Pressure<DataType>>{})));
+}
+
+#define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+#define DTYPE(data) BOOST_PP_TUPLE_ELEM(1, data)
+#define TAG(data) BOOST_PP_TUPLE_ELEM(2, data)
+
+#define INSTANTIATE_SCALARS(_, data)                                         \
+  template tuples::TaggedTuple<TAG(data) < DTYPE(data)> >                    \
+      ShuOsherTube::variables(                                               \
+          const tnsr::I<DTYPE(data), DIM(data), Frame::Inertial>& x_shifted, \
+          tmpl::list<TAG(data) < DTYPE(data)> >) const noexcept;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE_SCALARS, (1), (double, DataVector),
+                        (Tags::MassDensity, Tags::Pressure,
+                         Tags::SpecificInternalEnergy))
+
+#define INSTANTIATE_VELOCITY(_, data)                                        \
+  template tuples::TaggedTuple<TAG(data) < DTYPE(data), DIM(data),           \
+                               Frame::Inertial> >                            \
+      ShuOsherTube::variables(                                               \
+          const tnsr::I<DTYPE(data), DIM(data), Frame::Inertial>& x_shifted, \
+          tmpl::list<TAG(data) < DTYPE(data), DIM(data), Frame::Inertial> >) \
+          const noexcept;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE_VELOCITY, (1), (double, DataVector),
+                        (Tags::Velocity))
+
+#undef DIM
+#undef DTYPE
+#undef TAG
+#undef INSTANTIATE_SCALARS
+#undef INSTANTIATE_VELOCITY
+}  // namespace NewtonianEuler::AnalyticData
---
+++ tests/Unit/PointwiseFunctions/AnalyticData/NewtonianEuler/Test_ShuOsherTube.cpp
@@ -0,0 +1,93 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cstddef>
+#include <string>
+#include <tuple>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Evolution/Systems/NewtonianEuler/Tags.hpp"
+#include "Framework/CheckWithRandomValues.hpp"
+#include "Framework/SetupLocalPythonEnvironment.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "PointwiseFunctions/AnalyticData/NewtonianEuler/ShuOsherTube.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace {
+struct ShuOsherTubeProxy : NewtonianEuler::AnalyticData::ShuOsherTube {
+  using NewtonianEuler::AnalyticData::ShuOsherTube::ShuOsherTube;
+
+  template <typename DataType>
+  using variables_tags =
+      tmpl::list<NewtonianEuler::Tags::MassDensity<DataType>,
+                 NewtonianEuler::Tags::Velocity<DataType, 1, Frame::Inertial>,
+                 NewtonianEuler::Tags::SpecificInternalEnergy<DataType>,
+                 NewtonianEuler::Tags::Pressure<DataType>>;
+
+  template <typename DataType>
+  tuples::tagged_tuple_from_typelist<variables_tags<DataType>>
+  primitive_variables(
+      const tnsr::I<DataType, 1, Frame::Inertial>& x) const noexcept {
+    return this->variables(x, variables_tags<DataType>{});
+  }
+};
+
+template <size_t Dim, typename DataType>
+void test_analytic_data(const DataType& used_for_size) noexcept {
+  const double mass_density_l = 3.857143;
+  const double velocity_l = 2.629369;
+  const double pressure_l = 10.33333;
+  const double jump_position = -4.0;
+  const double epsilon = 0.2;
+  const double lambda = 5.0;
+  const double velocity_r = 0.0;
+  const double pressure_r = 1.0;
+  const double adiabatic_index = 1.4;
+  const auto members =
+      std::make_tuple(mass_density_l, velocity_l, pressure_l, jump_position,
+                      epsilon, lambda, velocity_r, pressure_r, adiabatic_index);
+
+  ShuOsherTubeProxy shu_osher(jump_position, mass_density_l, velocity_l,
+                              pressure_l, velocity_r, pressure_r, epsilon,
+                              lambda);
+  pypp::check_with_random_values<1>(
+      &ShuOsherTubeProxy::template primitive_variables<DataType>, shu_osher,
+      "ShuOsherTube",
+      {"mass_density", "velocity", "specific_internal_energy", "pressure"},
+      {{{0.0, 1.0}}}, members, used_for_size);
+
+  const auto shu_osher_from_options =
+      TestHelpers::test_creation<NewtonianEuler::AnalyticData::ShuOsherTube>(
+          "  JumpPosition: -4.0\n"
+          "  LeftMassDensity: 3.857143\n"
+          "  LeftVelocity: 2.629369\n"
+          "  LeftPressure: 10.33333\n"
+          "  RightVelocity: 0.0\n"
+          "  RightPressure: 1.0\n"
+          "  Epsilon: 0.2\n"
+          "  Lambda: 5.0\n");
+  CHECK(shu_osher_from_options == shu_osher);
+
+  // run post-serialized state through checks with random numbers
+  pypp::check_with_random_values<1>(
+      &ShuOsherTubeProxy::template primitive_variables<DataType>,
+      serialize_and_deserialize(shu_osher), "ShuOsherTube",
+      {"mass_density", "velocity", "specific_internal_energy", "pressure"},
+      {{{0.0, 1.0}}}, members, used_for_size);
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.PointwiseFunctions.AnalyticData.NewtEuler.ShuOsherTube",
+                  "[Unit][PointwiseFunctions]") {
+  pypp::SetupLocalPythonEnvironment local_python_env{
+      "PointwiseFunctions/AnalyticData/NewtonianEuler"};
+
+  GENERATE_UNINITIALIZED_DOUBLE_AND_DATAVECTOR;
+  CHECK_FOR_DOUBLES_AND_DATAVECTORS(test_analytic_data, (1));
+}
