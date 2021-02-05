d19c663f232a48ee90917b17e74383e1eb864dba
Jordan Moxon <jem497@cornell.edu>
2021-02-04 21:28:41 -0800

    Add take_step function
    For ease of use in actions which need to invoke the stepper as a subroutine.

---
+++ src/Time/TakeStep.hpp
@@ -0,0 +1,26 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Parallel/GlobalCache.hpp"
+#include "Time/Actions/ChangeStepSize.hpp"
+#include "Time/Actions/RecordTimeStepperData.hpp"
+#include "Time/Actions/UpdateU.hpp"
+#include "Utilities/Gsl.hpp"
+
+/// Bundled method for recording the current system state in the history, and
+/// updating the evolved variables and step size.
+///
+/// This function is used to encapsulate any needed logic for updating the
+/// system, and in the case for which step parameters may need to be rejected
+/// and re-tried, looping until an acceptable step is performed.
+template <typename StepChooserRegistrars, typename VariablesTag = NoSuchType,
+          typename DbTags, typename Metavariables>
+void take_step(const gsl::not_null<db::DataBox<DbTags>*> box,
+               const Parallel::GlobalCache<Metavariables>& cache) noexcept {
+  record_time_stepper_data<typename Metavariables::system, VariablesTag>(box);
+  update_u<typename Metavariables::system, VariablesTag>(box);
+  change_step_size<StepChooserRegistrars>(box, cache);
+}
