a8a5d10f3f757f8c52a9cb696c773a09a14c4674
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-02-06 04:22:20 +0100

    Add zero Dirichlet and Neumann boundary conditions for elasticity
    These represent "fixed" and "free" boundaries, respectively.

---
+++ src/Elliptic/Systems/Elasticity/BoundaryConditions/Zero.cpp
@@ -0,0 +1,85 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Elliptic/Systems/Elasticity/BoundaryConditions/Zero.hpp"
+
+#include <cstddef>
+#include <string>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Elliptic/BoundaryConditions/BoundaryCondition.hpp"
+#include "Elliptic/BoundaryConditions/BoundaryConditionType.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace Elasticity::BoundaryConditions::detail {
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+std::string ZeroImpl<Dim, BoundaryConditionType>::name() noexcept {
+  if constexpr (BoundaryConditionType ==
+                elliptic::BoundaryConditionType::Dirichlet) {
+    return "Fixed";
+  } else {
+    return "Free";
+  }
+}
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+void ZeroImpl<Dim, BoundaryConditionType>::apply(
+    const gsl::not_null<tnsr::I<DataVector, Dim>*> displacement,
+    const gsl::not_null<tnsr::I<DataVector, Dim>*>
+        n_dot_minus_stress) noexcept {
+  if constexpr (BoundaryConditionType ==
+                elliptic::BoundaryConditionType::Dirichlet) {
+    (void)n_dot_minus_stress;
+    for (size_t d = 0; d < Dim; ++d) {
+      displacement->get(d) = 0.;
+    }
+  } else {
+    (void)displacement;
+    for (size_t d = 0; d < Dim; ++d) {
+      n_dot_minus_stress->get(d) = 0.;
+    }
+  }
+}
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+void ZeroImpl<Dim, BoundaryConditionType>::apply_linearized(
+    const gsl::not_null<tnsr::I<DataVector, Dim>*> displacement,
+    const gsl::not_null<tnsr::I<DataVector, Dim>*>
+        n_dot_minus_stress) noexcept {
+  apply(displacement, n_dot_minus_stress);
+}
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+bool operator==(const ZeroImpl<Dim, BoundaryConditionType>& /*lhs*/,
+                const ZeroImpl<Dim, BoundaryConditionType>& /*rhs*/) noexcept {
+  return true;
+}
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+bool operator!=(const ZeroImpl<Dim, BoundaryConditionType>& lhs,
+                const ZeroImpl<Dim, BoundaryConditionType>& rhs) noexcept {
+  return not(lhs == rhs);
+}
+
+#define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+#define BCTYPE(data) BOOST_PP_TUPLE_ELEM(1, data)
+
+#define INSTANTIATE(_, data)                                              \
+  template class ZeroImpl<DIM(data), BCTYPE(data)>;                       \
+  template bool operator==(const ZeroImpl<DIM(data), BCTYPE(data)>& lhs,  \
+                           const ZeroImpl<DIM(data), BCTYPE(data)>& rhs); \
+  template bool operator!=(const ZeroImpl<DIM(data), BCTYPE(data)>& lhs,  \
+                           const ZeroImpl<DIM(data), BCTYPE(data)>& rhs);
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (2, 3),
+                        (elliptic::BoundaryConditionType::Dirichlet,
+                         elliptic::BoundaryConditionType::Neumann))
+
+#undef DIM
+#undef BCTYPE
+#undef INSTANTIATE
+
+}  // namespace Elasticity::BoundaryConditions::detail
---
+++ src/Elliptic/Systems/Elasticity/BoundaryConditions/Zero.hpp
@@ -0,0 +1,133 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+#include <pup.h>
+#include <string>
+
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Elliptic/BoundaryConditions/BoundaryCondition.hpp"
+#include "Elliptic/BoundaryConditions/BoundaryConditionType.hpp"
+#include "Options/Options.hpp"
+#include "Parallel/CharmPupable.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+class DataVector;
+/// \endcond
+
+namespace Elasticity::BoundaryConditions {
+namespace detail {
+
+template <elliptic::BoundaryConditionType BoundaryConditionType>
+struct ZeroHelpString;
+template <>
+struct ZeroHelpString<elliptic::BoundaryConditionType::Dirichlet> {
+  static constexpr Options::String help =
+      "Zero Dirichlet boundary conditions imposed on the displacement vector, "
+      "i.e. the elastic material is held fixed at this boundary.";
+};
+template <>
+struct ZeroHelpString<elliptic::BoundaryConditionType::Neumann> {
+  static constexpr Options::String help =
+      "Zero Neumann boundary conditions imposed on the stress tensor "
+      "perpendicular to the surface, i.e. the elastic material is free to "
+      "deform at this boundary.";
+};
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+struct ZeroImpl {
+  static_assert(BoundaryConditionType ==
+                        elliptic::BoundaryConditionType::Dirichlet or
+                    BoundaryConditionType ==
+                        elliptic::BoundaryConditionType::Neumann,
+                "Unexpected boundary condition type. Supported are Dirichlet "
+                "and Neumann.");
+
+  static std::string name() noexcept;
+  using options = tmpl::list<>;
+  static constexpr Options::String help =
+      ZeroHelpString<BoundaryConditionType>::help;
+
+  using argument_tags = tmpl::list<>;
+  using volume_tags = tmpl::list<>;
+
+  static void apply(
+      gsl::not_null<tnsr::I<DataVector, Dim>*> displacement,
+      gsl::not_null<tnsr::I<DataVector, Dim>*> n_dot_minus_stress) noexcept;
+
+  using argument_tags_linearized = tmpl::list<>;
+  using volume_tags_linearized = tmpl::list<>;
+
+  static void apply_linearized(
+      gsl::not_null<tnsr::I<DataVector, Dim>*> displacement,
+      gsl::not_null<tnsr::I<DataVector, Dim>*> n_dot_minus_stress) noexcept;
+};
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+bool operator==(const ZeroImpl<Dim, BoundaryConditionType>& lhs,
+                const ZeroImpl<Dim, BoundaryConditionType>& rhs) noexcept;
+
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+bool operator!=(const ZeroImpl<Dim, BoundaryConditionType>& lhs,
+                const ZeroImpl<Dim, BoundaryConditionType>& rhs) noexcept;
+
+}  // namespace detail
+
+// The following implements the registration and factory-creation mechanism
+
+/// \cond
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType,
+          typename Registrars>
+struct Zero;
+
+namespace Registrars {
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType>
+struct Zero {
+  template <typename Registrars>
+  using f = BoundaryConditions::Zero<Dim, BoundaryConditionType, Registrars>;
+};
+}  // namespace Registrars
+/// \endcond
+
+/// Impose zero Dirichlet ("fixed") or Neumann ("free") boundary conditions.
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType,
+          typename Registrars =
+              tmpl::list<Registrars::Zero<Dim, BoundaryConditionType>>>
+class Zero
+    : public elliptic::BoundaryConditions::BoundaryCondition<Dim, Registrars>,
+      public detail::ZeroImpl<Dim, BoundaryConditionType> {
+ private:
+  using Base = elliptic::BoundaryConditions::BoundaryCondition<Dim, Registrars>;
+
+ public:
+  Zero() = default;
+  Zero(const Zero&) noexcept = default;
+  Zero& operator=(const Zero&) noexcept = default;
+  Zero(Zero&&) noexcept = default;
+  Zero& operator=(Zero&&) noexcept = default;
+  ~Zero() noexcept = default;
+
+  /// \cond
+  explicit Zero(CkMigrateMessage* m) noexcept : Base(m) {}
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(Zero);
+  /// \endcond
+
+  std::unique_ptr<domain::BoundaryConditions::BoundaryCondition> get_clone()
+      const noexcept override {
+    return std::make_unique<Zero>(*this);
+  }
+};
+
+/// \cond
+template <size_t Dim, elliptic::BoundaryConditionType BoundaryConditionType,
+          typename Registrars>
+PUP::able::PUP_ID Zero<Dim, BoundaryConditionType,
+                       Registrars>::my_PUP_ID = 0;  // NOLINT
+/// \endcond
+
+}  // namespace Elasticity::BoundaryConditions
