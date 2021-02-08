5e0d9ec12318eff6c5f077fcdbef279de5c367dd
Prayush Kumar <prayush.kumar@gmail.com>
2021-02-08 19:09:02 +0530

    Add functions to compute propagating modes of Weyl propto Psi4 and Psi0

---
+++ src/PointwiseFunctions/GeneralRelativity/WeylPropagating.hpp
@@ -0,0 +1,72 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+
+/// \cond
+namespace gsl {
+template <typename>
+struct not_null;
+}  // namespace gsl
+/// \endcond
+
+namespace gr {
+// @{
+/*!
+ * \ingroup GeneralRelativityGroup
+ * \brief Computes the propagating modes of the Weyl tensor
+ *
+ * \details The Weyl tensor evolution system in vacuum has six characteristic
+ * fields, of which two (\f$ U^{8\pm}\f$) are proportional to the
+ * Newman-Penrose components of the Weyl tensor \f$\Psi_4\f$ and \f$\Psi_0\f$.
+ * These represent the true gravitational-wave degrees of freedom, and
+ * can be written down in terms of \f$3+1\f$ quantities as
+ * \cite Kidder2004rw (see Eq. 75):
+ *
+ * \f{align}
+ * U^{8\pm}_{ij} &= \left(P^{k}_i P^{l}_j - \frac{1}{2} P_{ij} P^{kl}\right)
+ *                  \left(R_{kl} + K K_{kl} - K_k^m K_{ml}
+ *                       \mp n^m \nabla_m K_{kl} \pm n^m \nabla_{(k}K_{l)m}
+ *                       \right),\\
+ *               &= \left(P^{k}_i P^{l}_j - \frac{1}{2} P_{ij} P^{kl}\right)
+ *                  \left(E_{kl} \mp n^m \nabla_m K_{kl}
+ *                        \pm n^m \nabla_{(k}K_{l)m}\right),
+ * \f}
+ *
+ * where \f$R_{ij}\f$ is the spatial Ricci tensor, \f$K_{ij}\f$ is the
+ * extrinsic curvature, \f$K\f$ is the trace of \f$K_{ij}\f$, \f$E_{ij}\f$ is
+ * the electric part of the Weyl tensor in vacuum, \f$n^i\f$ is the outward
+ * directed unit normal vector to the interface, \f$\nabla_i\f$ denotes the
+ * covariant derivative, and \f$P^{ij}\f$ and its index-raised and lowered forms
+ * project tensors transverse to \f$n^i\f$.
+ */
+template <size_t SpatialDim, typename Frame, typename DataType>
+tnsr::ii<DataType, SpatialDim, Frame> weyl_propagating(
+    const tnsr::ii<DataType, SpatialDim, Frame>& ricci,
+    const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
+    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric,
+    const tnsr::ijj<DataType, SpatialDim, Frame>& cov_deriv_extrinsic_curvature,
+    const tnsr::I<DataType, SpatialDim, Frame>& unit_interface_normal_vector,
+    const tnsr::II<DataType, SpatialDim, Frame>& projection_IJ,
+    const tnsr::ii<DataType, SpatialDim, Frame>& projection_ij,
+    const tnsr::Ij<DataType, SpatialDim, Frame>& projection_Ij,
+    const double sign) noexcept;
+
+template <size_t SpatialDim, typename Frame, typename DataType>
+void weyl_propagating(
+    gsl::not_null<tnsr::ii<DataType, SpatialDim, Frame>*> weyl_prop_u8,
+    const tnsr::ii<DataType, SpatialDim, Frame>& ricci,
+    const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
+    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric,
+    const tnsr::ijj<DataType, SpatialDim, Frame>& cov_deriv_extrinsic_curvature,
+    const tnsr::I<DataType, SpatialDim, Frame>& unit_interface_normal_vector,
+    const tnsr::II<DataType, SpatialDim, Frame>& projection_IJ,
+    const tnsr::ii<DataType, SpatialDim, Frame>& projection_ij,
+    const tnsr::Ij<DataType, SpatialDim, Frame>& projection_Ij,
+    const double sign) noexcept;
+// @}
+}  // namespace gr
---
+++ tests/Unit/PointwiseFunctions/GeneralRelativity/Test_WeylPropagating.cpp
@@ -0,0 +1,90 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cstddef>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"  // IWYU pragma: keep
+#include "Framework/CheckWithRandomValues.hpp"
+#include "Framework/SetupLocalPythonEnvironment.hpp"
+#include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
+#include "PointwiseFunctions/GeneralRelativity/WeylPropagating.hpp"
+
+namespace {
+template <size_t SpatialDim, typename Frame, typename DataType>
+tnsr::ii<DataType, SpatialDim, Frame> weyl_propagating_plus_wrapper(
+    const tnsr::ii<DataType, SpatialDim, Frame>& ricci,
+    const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
+    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric,
+    const tnsr::ijj<DataType, SpatialDim, Frame>& cov_deriv_extrinsic_curvature,
+    const tnsr::I<DataType, SpatialDim, Frame>& unit_interface_normal_vector,
+    const tnsr::II<DataType, SpatialDim, Frame>& projection_IJ,
+    const tnsr::ii<DataType, SpatialDim, Frame>& projection_ij,
+    const tnsr::Ij<DataType, SpatialDim, Frame>& projection_Ij) noexcept {
+  return gr::weyl_propagating<SpatialDim, Frame, DataType>(
+      ricci, extrinsic_curvature, inverse_spatial_metric,
+      cov_deriv_extrinsic_curvature, unit_interface_normal_vector,
+      projection_IJ, projection_ij, projection_Ij, 1.);
+}
+template <size_t SpatialDim, typename Frame, typename DataType>
+tnsr::ii<DataType, SpatialDim, Frame> weyl_propagating_minus_wrapper(
+    const tnsr::ii<DataType, SpatialDim, Frame>& ricci,
+    const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
+    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric,
+    const tnsr::ijj<DataType, SpatialDim, Frame>& cov_deriv_extrinsic_curvature,
+    const tnsr::I<DataType, SpatialDim, Frame>& unit_interface_normal_vector,
+    const tnsr::II<DataType, SpatialDim, Frame>& projection_IJ,
+    const tnsr::ii<DataType, SpatialDim, Frame>& projection_ij,
+    const tnsr::Ij<DataType, SpatialDim, Frame>& projection_Ij) noexcept {
+  return gr::weyl_propagating<SpatialDim, Frame, DataType>(
+      ricci, extrinsic_curvature, inverse_spatial_metric,
+      cov_deriv_extrinsic_curvature, unit_interface_normal_vector,
+      projection_IJ, projection_ij, projection_Ij, -1.);
+}
+
+template <size_t SpatialDim, typename DataType>
+void test_weyl_propagating(const DataType& used_for_size) {
+  {
+    tnsr::ii<DataType, SpatialDim, Frame::Inertial> (*f)(
+        const tnsr::ii<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::ii<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::II<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::ijj<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::I<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::II<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::ii<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::Ij<DataType, SpatialDim, Frame::Inertial>&) =
+        &weyl_propagating_plus_wrapper<SpatialDim, Frame::Inertial, DataType>;
+    pypp::check_with_random_values<1>(f, "WeylPropagating",
+                                      "weyl_propagating_mode_plus",
+                                      {{{-1., 1.}}}, used_for_size);
+  }
+  {
+    tnsr::ii<DataType, SpatialDim, Frame::Inertial> (*f)(
+        const tnsr::ii<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::ii<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::II<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::ijj<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::I<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::II<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::ii<DataType, SpatialDim, Frame::Inertial>&,
+        const tnsr::Ij<DataType, SpatialDim, Frame::Inertial>&) =
+        &weyl_propagating_minus_wrapper<SpatialDim, Frame::Inertial, DataType>;
+    pypp::check_with_random_values<1>(f, "WeylPropagating",
+                                      "weyl_propagating_mode_minus",
+                                      {{{-1., 1.}}}, used_for_size);
+  }
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.PointwiseFunctions.GeneralRelativity.WeylPropagating",
+                  "[PointwiseFunctions][Unit]") {
+  pypp::SetupLocalPythonEnvironment local_python_env(
+      "PointwiseFunctions/GeneralRelativity/");
+
+  GENERATE_UNINITIALIZED_DOUBLE_AND_DATAVECTOR;
+
+  CHECK_FOR_DOUBLES_AND_DATAVECTORS(test_weyl_propagating, (1, 2, 3));
+}
