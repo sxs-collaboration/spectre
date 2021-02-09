0b6c7f28eeec705cd8250fa0bec33ad79f2beb90
Nils Deppe <nd357@cornell.edu>
2021-02-09 15:52:58 -0800

    Add Jacobians and normal magnitude to MortarData
    When using local time stepping on a moving mesh we need the magnitude of the
    normal vector at the past times because the scheme integrates the lifted
    boundary correction, and for the lifting we need the normal magnitude. When
    using Gauss points we also need the face and volume determinant of the
    Jacobian.

--- tests/Unit/Evolution/DiscontinuousGalerkin/Test_MortarData.cpp
+++ tests/Unit/Evolution/DiscontinuousGalerkin/Test_MortarData.cpp
@@ -22,7 +22,7 @@
 namespace evolution::dg {
 namespace {
 template <size_t Dim>
-void test() {
+void test_global_time_stepping_usage() {
   std::uniform_real_distribution<double> dist(-1.0, 2.3);
   MAKE_GENERATOR(gen);
   constexpr size_t number_of_components = 1 + Dim;
@@ -47,18 +47,18 @@ void test() {
   fill_with_random_values(make_not_null(&local_data), make_not_null(&gen),
                           make_not_null(&dist));
 
-  CHECK_FALSE(static_cast<bool>(mortar_data.local_mortar_data()));
-  CHECK_FALSE(static_cast<bool>(mortar_data.neighbor_mortar_data()));
+  CHECK_FALSE(mortar_data.local_mortar_data().has_value());
+  CHECK_FALSE(mortar_data.neighbor_mortar_data().has_value());
 
   mortar_data.insert_local_mortar_data(time_step_id, local_mesh, local_data);
 
-  CHECK(static_cast<bool>(mortar_data.local_mortar_data()));
-  CHECK_FALSE(static_cast<bool>(mortar_data.neighbor_mortar_data()));
+  CHECK(mortar_data.local_mortar_data().has_value());
+  CHECK_FALSE(mortar_data.neighbor_mortar_data().has_value());
 
   mortar_data.insert_neighbor_mortar_data(time_step_id, neighbor_mesh,
                                           neighbor_data);
-  CHECK(static_cast<bool>(mortar_data.local_mortar_data()));
-  CHECK(static_cast<bool>(mortar_data.neighbor_mortar_data()));
+  CHECK(mortar_data.local_mortar_data().has_value());
+  CHECK(mortar_data.neighbor_mortar_data().has_value());
 
   const auto deserialized_mortar_data = serialize_and_deserialize(mortar_data);
 
@@ -75,6 +75,99 @@ void test() {
   CHECK(extracted_data.second ==
         *deserialized_mortar_data.neighbor_mortar_data());
 }
+
+template <size_t Dim>
+void test_local_time_stepping_usage(const bool use_gauss_points) {
+  CAPTURE(Dim);
+  CAPTURE(use_gauss_points);
+  std::uniform_real_distribution<double> dist(-1.0, 2.3);
+  MAKE_GENERATOR(gen);
+  constexpr size_t number_of_components = 1 + Dim;
+
+  MortarData<Dim> mortar_data{};
+  const TimeStepId time_step_id{true, 3, Time{Slab{0.2, 7.1}, {2, 51}}};
+
+  const Mesh<Dim - 1> mortar_mesh{4, Spectral::Basis::Legendre,
+                                  Spectral::Quadrature::Gauss};
+
+  const Mesh<Dim - 1> local_mesh{4, Spectral::Basis::Legendre,
+                                 Spectral::Quadrature::GaussLobatto};
+  std::vector<double> local_data(mortar_mesh.number_of_grid_points() *
+                                 number_of_components);
+  fill_with_random_values(make_not_null(&local_data), make_not_null(&gen),
+                          make_not_null(&dist));
+
+  CHECK_FALSE(mortar_data.local_mortar_data().has_value());
+  CHECK_FALSE(mortar_data.neighbor_mortar_data().has_value());
+
+  mortar_data.insert_local_mortar_data(time_step_id, local_mesh, local_data);
+
+  CHECK(mortar_data.local_mortar_data().has_value());
+  CHECK_FALSE(mortar_data.neighbor_mortar_data().has_value());
+
+  const auto local_volume_det_inv_jacobian =
+      make_with_random_values<Scalar<DataVector>>(
+          make_not_null(&gen), make_not_null(&dist),
+          mortar_mesh.number_of_grid_points() * 4);
+  const auto local_face_det_jacobian =
+      make_with_random_values<Scalar<DataVector>>(
+          make_not_null(&gen), make_not_null(&dist),
+          mortar_mesh.number_of_grid_points());
+  const auto local_face_normal_magnitude =
+      make_with_random_values<Scalar<DataVector>>(
+          make_not_null(&gen), make_not_null(&dist),
+          mortar_mesh.number_of_grid_points());
+
+  if (use_gauss_points) {
+    mortar_data.insert_local_geometric_quantities(local_volume_det_inv_jacobian,
+                                                  local_face_det_jacobian,
+                                                  local_face_normal_magnitude);
+  } else {
+    mortar_data.insert_local_face_normal_magnitude(local_face_normal_magnitude);
+  }
+
+  const auto check_geometric_quantities = [&local_face_det_jacobian,
+                                           &local_volume_det_inv_jacobian,
+                                           &local_face_normal_magnitude,
+                                           use_gauss_points](
+                                              const auto& mortar_data_local) {
+    if (use_gauss_points) {
+      Scalar<DataVector> retrieved_local_face_det_jacobian{};
+      Scalar<DataVector> retrieved_local_volume_det_inv_jacobian{};
+      mortar_data_local.get_local_face_det_jacobian(
+          &retrieved_local_face_det_jacobian);
+      mortar_data_local.get_local_volume_det_inv_jacobian(
+          &retrieved_local_volume_det_inv_jacobian);
+      CHECK(retrieved_local_face_det_jacobian == local_face_det_jacobian);
+      CHECK(retrieved_local_volume_det_inv_jacobian ==
+            local_volume_det_inv_jacobian);
+    }
+    Scalar<DataVector> retrieved_local_face_normal_magnitude{};
+    mortar_data_local.get_local_face_normal_magnitude(
+        &retrieved_local_face_normal_magnitude);
+    CHECK(retrieved_local_face_normal_magnitude == local_face_normal_magnitude);
+  };
+
+  check_geometric_quantities(mortar_data);
+
+  const auto deserialized_mortar_data = serialize_and_deserialize(mortar_data);
+
+  CHECK(*mortar_data.local_mortar_data() ==
+        *deserialized_mortar_data.local_mortar_data());
+  CHECK_FALSE(deserialized_mortar_data.neighbor_mortar_data().has_value());
+  check_geometric_quantities(deserialized_mortar_data);
+
+  CHECK(mortar_data == deserialized_mortar_data);
+  CHECK_FALSE(mortar_data != deserialized_mortar_data);
+}
+
+template <size_t Dim>
+void test() {
+  test_global_time_stepping_usage<Dim>();
+  test_local_time_stepping_usage<Dim>(true);
+  test_local_time_stepping_usage<Dim>(false);
+}
+
 }  // namespace
 
 SPECTRE_TEST_CASE("Unit.Evolution.DG.MortarData", "[Unit][Evolution]") {
