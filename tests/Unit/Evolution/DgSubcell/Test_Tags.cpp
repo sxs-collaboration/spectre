99dc8f442c47a4608b95f1066742bc2486e79e7b
Nils Deppe <nd357@cornell.edu>
2021-02-12 18:24:54 -0800

    Add TciGridHistory tag to Dg-subcell

--- tests/Unit/Evolution/DgSubcell/Test_Tags.cpp
+++ tests/Unit/Evolution/DgSubcell/Test_Tags.cpp
@@ -6,10 +6,13 @@
 #include <string>
 
 #include "Evolution/DgSubcell/Tags/ActiveGrid.hpp"
+#include "Evolution/DgSubcell/Tags/TciGridHistory.hpp"
 #include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
 
 SPECTRE_TEST_CASE("Unit.Evolution.Subcell.Tags",
                   "[Evolution][Unit]") {
   TestHelpers::db::test_simple_tag<evolution::dg::subcell::Tags::ActiveGrid>(
       "ActiveGrid");
+  TestHelpers::db::test_simple_tag<
+      evolution::dg::subcell::Tags::TciGridHistory>("TciGridHistory");
 }
