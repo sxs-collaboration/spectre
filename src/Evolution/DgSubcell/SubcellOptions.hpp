80907c934342118fe6005f13d0dc729879d9159a
Nils Deppe <nd357@cornell.edu>
2021-02-16 20:40:00 -0800

    Add class to hold input file options for DG-subcell

---
+++ src/Evolution/DgSubcell/SubcellOptions.hpp
@@ -0,0 +1,149 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <limits>
+#include <string>
+
+#include "Options/Options.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+namespace PUP {
+class er;
+}  // namespace PUP
+/// \endcond
+
+namespace evolution::dg::subcell {
+/*!
+ * \brief Holds the system-agnostic subcell parameters, such as numbers
+ * controlling when to switch between DG and subcell.
+ */
+class SubcellOptions {
+ public:
+  struct InitialData {
+    static constexpr Options::String help =
+        "Parameters only used when setting up initial data.";
+  };
+
+  /// The \f$\delta_0\f$ parameter in the relaxed discrete maximum principle
+  /// troubled-cell indicator when applied to the initial data
+  struct InitialDataRdmpDelta0 {
+    static std::string name() noexcept { return "RdmpDelta0"; }
+    static constexpr Options::String help{"Absolute jump tolerance parameter."};
+    using type = double;
+    static type lower_bound() noexcept { return 0.0; }
+    using group = InitialData;
+  };
+  /// The \f$\epsilon\f$ parameter in the relaxed discrete maximum principle
+  /// troubled-cell indicator when applied to the initial data
+  struct InitialDataRdmpEpsilon {
+    static std::string name() noexcept { return "RdmpEpsilon"; }
+    static constexpr Options::String help{
+        "The jump-dependent relaxation constant."};
+    using type = double;
+    static type lower_bound() noexcept { return 0.0; }
+    static type upper_bound() noexcept { return 1.0; }
+    using group = InitialData;
+  };
+  /// The exponent \f$\alpha\f$ passed to the Persson troubled-cell indicator
+  /// when applied to the initial data.
+  struct InitialDataPerssonExponent {
+    static std::string name() noexcept { return "PerssonExponent"; }
+    static constexpr Options::String help{
+        "The exponent at which the error should decrease with N."};
+    using type = double;
+    static constexpr type lower_bound() noexcept { return 1.0; }
+    static constexpr type upper_bound() noexcept { return 10.0; }
+    using group = InitialData;
+  };
+
+  /// The \f$\delta_0\f$ parameter in the relaxed discrete maximum principle
+  /// troubled-cell indicator
+  struct RdmpDelta0 {
+    static std::string name() noexcept { return "RdmpDelta0"; }
+    static constexpr Options::String help{"Absolute jump tolerance parameter."};
+    using type = double;
+    static type lower_bound() noexcept { return 0.0; }
+  };
+  /// The \f$\epsilon\f$ parameter in the relaxed discrete maximum principle
+  /// troubled-cell indicator
+  struct RdmpEpsilon {
+    static std::string name() noexcept { return "RdmpEpsilon"; }
+    static constexpr Options::String help{
+        "The jump-dependent relaxation constant."};
+    using type = double;
+    static type lower_bound() noexcept { return 0.0; }
+    static type upper_bound() noexcept { return 1.0; }
+  };
+  /// The exponent \f$\alpha\f$ passed to the Persson troubled-cell indicator
+  struct PerssonExponent {
+    static std::string name() noexcept { return "PerssonExponent"; }
+    static constexpr Options::String help{
+        "The exponent at which the error should decrease with N."};
+    using type = double;
+    static constexpr type lower_bound() noexcept { return 1.0; }
+    static constexpr type upper_bound() noexcept { return 10.0; }
+  };
+  /// If true, then we always use the subcell method, not DG.
+  struct AlwaysUseSubcells {
+    static constexpr Options::String help{
+        "If true, then always use the subcell method (e.g. finite-difference) "
+        "instead of DG."};
+    using type = bool;
+  };
+
+  using options =
+      tmpl::list<InitialDataRdmpDelta0, InitialDataRdmpEpsilon, RdmpDelta0,
+                 RdmpEpsilon, InitialDataPerssonExponent, PerssonExponent,
+                 AlwaysUseSubcells>;
+
+  static constexpr Options::String help{
+      "System-agnostic options for the DG-subcell method."};
+
+  SubcellOptions() = default;
+  SubcellOptions(double initial_data_rdmp_delta0,
+                 double initial_data_rdmp_epsilon, double rdmp_delta0,
+                 double rdmp_epsilon, double initial_data_persson_exponent,
+                 double persson_exponent, bool always_use_subcells) noexcept;
+
+  void pup(PUP::er& p) noexcept;
+
+  double initial_data_rdmp_delta0() const noexcept {
+    return initial_data_rdmp_delta0_;
+  }
+
+  double initial_data_rdmp_epsilon() const noexcept {
+    return initial_data_rdmp_epsilon_;
+  }
+
+  double rdmp_delta0() const noexcept { return rdmp_delta0_; }
+
+  double rdmp_epsilon() const noexcept { return rdmp_epsilon_; }
+
+  double initial_data_persson_exponent() const noexcept {
+    return initial_data_persson_exponent_;
+  }
+
+  double persson_exponent() const noexcept { return persson_exponent_; }
+
+  bool always_use_subcells() const noexcept { return always_use_subcells_; }
+
+ private:
+  double initial_data_rdmp_delta0_ =
+      std::numeric_limits<double>::signaling_NaN();
+  double initial_data_rdmp_epsilon_ =
+      std::numeric_limits<double>::signaling_NaN();
+  double rdmp_delta0_ = std::numeric_limits<double>::signaling_NaN();
+  double rdmp_epsilon_ = std::numeric_limits<double>::signaling_NaN();
+  double initial_data_persson_exponent_ =
+      std::numeric_limits<double>::signaling_NaN();
+  double persson_exponent_ = std::numeric_limits<double>::signaling_NaN();
+  bool always_use_subcells_ = false;
+};
+
+bool operator==(const SubcellOptions& lhs, const SubcellOptions& rhs) noexcept;
+
+bool operator!=(const SubcellOptions& lhs, const SubcellOptions& rhs) noexcept;
+}  // namespace evolution::dg::subcell
---
+++ tests/Unit/Evolution/DgSubcell/Test_SubcellOptions.cpp
@@ -0,0 +1,72 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "Evolution/DgSubcell/SubcellOptions.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Framework/TestHelpers.hpp"
+
+namespace evolution::dg::subcell {
+namespace {
+void test_impl(const std::vector<double>& expected_values,
+               const size_t incorrect_value_index) {
+  std::vector<double> values = expected_values;
+  values[incorrect_value_index] += 0.1;
+
+  CHECK(SubcellOptions(expected_values[0], expected_values[1],
+                       expected_values[2], expected_values[3],
+                       expected_values[4], expected_values[5], false) !=
+        SubcellOptions(values[0], values[1], values[2], values[3], values[4],
+                       values[5], false));
+  CHECK_FALSE(SubcellOptions(expected_values[0], expected_values[1],
+                             expected_values[2], expected_values[3],
+                             expected_values[4], expected_values[5], false) ==
+              SubcellOptions(values[0], values[1], values[2], values[3],
+                             values[4], values[5], false));
+
+  CHECK(SubcellOptions(expected_values[0], expected_values[1],
+                       expected_values[2], expected_values[3],
+                       expected_values[4], expected_values[5], false) !=
+        SubcellOptions(expected_values[0], expected_values[1],
+                       expected_values[2], expected_values[3],
+                       expected_values[4], expected_values[5], true));
+  CHECK_FALSE(SubcellOptions(expected_values[0], expected_values[1],
+                             expected_values[2], expected_values[3],
+                             expected_values[4], expected_values[5], false) ==
+              SubcellOptions(expected_values[0], expected_values[1],
+                             expected_values[2], expected_values[3],
+                             expected_values[4], expected_values[5], true));
+}
+
+SPECTRE_TEST_CASE("Unit.Evolution.Subcell.SubcellOptions",
+                  "[Evolution][Unit]") {
+  const std::vector<double> expected_values{1.0e-3, 1.0e-4, 2.0e-3,
+                                            2.0e-4, 5.0,    4.0};
+  for (size_t i = 0; i < expected_values.size(); ++i) {
+    test_impl(expected_values, i);
+  }
+
+  const SubcellOptions options(expected_values[0], expected_values[1],
+                               expected_values[2], expected_values[3],
+                               expected_values[4], expected_values[5], true);
+  const SubcellOptions deserialized_options =
+      serialize_and_deserialize(options);
+  CHECK(options == deserialized_options);
+
+  CHECK(options == TestHelpers::test_creation<SubcellOptions>(
+                       "InitialData:\n"
+                       "  RdmpDelta0: 1.0e-3\n"
+                       "  RdmpEpsilon: 1.0e-4\n"
+                       "  PerssonExponent: 5.0\n"
+                       "RdmpDelta0: 2.0e-3\n"
+                       "RdmpEpsilon: 2.0e-4\n"
+                       "PerssonExponent: 4.0\n"
+                       "AlwaysUseSubcells: true\n"));
+}
+}  // namespace
+}  // namespace evolution::dg::subcell
