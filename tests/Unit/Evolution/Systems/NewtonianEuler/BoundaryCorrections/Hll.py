f095ea7314535baddef4e45954f19e1a868d3565
Yoonsoo Kim <ykim7@caltech.edu>
2021-02-18 13:21:40 +0000

    Add outgoing & ingoing char speeds

--- tests/Unit/Evolution/Systems/NewtonianEuler/BoundaryCorrections/Hll.py
+++ tests/Unit/Evolution/Systems/NewtonianEuler/BoundaryCorrections/Hll.py
@@ -50,3 +50,51 @@ def dg_package_data_normal_dot_flux_energy_density(
     specific_internal_energy, normal_covector, mesh_velocity,
     normal_dot_mesh_velocity, use_polytropic_fluid):
     return np.einsum("i,i", normal_covector, flux_energy_density)
+
+
+def dg_package_data_largest_outgoing_char_speed(
+    mass_density, momentum_density, energy_density, flux_mass_density,
+    flux_momentum_density, flux_energy_density, velocity,
+    specific_internal_energy, normal_covector, mesh_velocity,
+    normal_dot_mesh_velocity, use_polytropic_fluid):
+    velocity_dot_normal = np.einsum("i,i", normal_covector, velocity)
+    if use_polytropic_fluid:
+        polytropic_constant = 1.0e-3
+        polytropic_exponent = 2.0
+        sound_speed = np.sqrt(polytropic_constant * polytropic_exponent *
+                              pow(mass_density, polytropic_exponent - 1.0))
+    else:
+        adiabatic_index = 1.3
+        chi = specific_internal_energy * (adiabatic_index - 1.0)
+        kappa_times_p_over_rho_squared = ((adiabatic_index - 1.0)**2 *
+                                          specific_internal_energy)
+        sound_speed = np.sqrt(chi + kappa_times_p_over_rho_squared)
+
+    if normal_dot_mesh_velocity is None:
+        return velocity_dot_normal + sound_speed
+    else:
+        return velocity_dot_normal + sound_speed - normal_dot_mesh_velocity
+
+
+def dg_package_data_largest_ingoing_char_speed(
+    mass_density, momentum_density, energy_density, flux_mass_density,
+    flux_momentum_density, flux_energy_density, velocity,
+    specific_internal_energy, normal_covector, mesh_velocity,
+    normal_dot_mesh_velocity, use_polytropic_fluid):
+    velocity_dot_normal = np.einsum("i,i", normal_covector, velocity)
+    if use_polytropic_fluid:
+        polytropic_constant = 1.0e-3
+        polytropic_exponent = 2.0
+        sound_speed = np.sqrt(polytropic_constant * polytropic_exponent *
+                              pow(mass_density, polytropic_exponent - 1.0))
+    else:
+        adiabatic_index = 1.3
+        chi = specific_internal_energy * (adiabatic_index - 1.0)
+        kappa_times_p_over_rho_squared = ((adiabatic_index - 1.0)**2 *
+                                          specific_internal_energy)
+        sound_speed = np.sqrt(chi + kappa_times_p_over_rho_squared)
+
+    if normal_dot_mesh_velocity is None:
+        return velocity_dot_normal - sound_speed
+    else:
+        return velocity_dot_normal - sound_speed - normal_dot_mesh_velocity
