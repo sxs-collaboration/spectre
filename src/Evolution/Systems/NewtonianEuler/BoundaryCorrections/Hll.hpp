d0776a2398cb361442307eab53699b354bc580d5
Yoonsoo Kim <ykim7@caltech.edu>
2021-02-19 23:33:40 +0900

    Copy from Rusanov implementation, change class name

---
+++ src/Evolution/Systems/NewtonianEuler/BoundaryCorrections/Hll.hpp
@@ -0,0 +1,133 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <memory>
+#include <optional>
+
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Evolution/Systems/NewtonianEuler/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/NewtonianEuler/Tags.hpp"
+#include "NumericalAlgorithms/DiscontinuousGalerkin/Formulation.hpp"
+#include "Options/Options.hpp"
+#include "Parallel/CharmPupable.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
+#include "PointwiseFunctions/Hydro/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+class DataVector;
+namespace gsl {
+template <typename T>
+class not_null;
+}  // namespace gsl
+namespace PUP {
+class er;
+}  // namespace PUP
+/// \endcond
+
+namespace NewtonianEuler::BoundaryCorrections {
+/*!
+ * \brief An HLL (Harten-Lax-van Leer) Riemann solver
+ *
+ */
+template <size_t Dim>
+class Hll final : public BoundaryCorrection<Dim> {
+ private:
+  struct AbsCharSpeed : db::SimpleTag {
+    using type = Scalar<DataVector>;
+  };
+
+ public:
+  using options = tmpl::list<>;
+  static constexpr Options::String help = {
+      "Computes the HLL boundary correction term for the "
+      "Newtonian Euler/hydrodynamics system."};
+
+  Hll() = default;
+  Hll(const Hll&) = default;
+  Hll& operator=(const Hll&) = default;
+  Hll(Hll&&) = default;
+  Hll& operator=(Hll&&) = default;
+  ~Hll() override = default;
+
+  /// \cond
+  explicit Hll(CkMigrateMessage* msg) noexcept;
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(Hll);  // NOLINT
+  /// \endcond
+  void pup(PUP::er& p) override;  // NOLINT
+
+  std::unique_ptr<BoundaryCorrection<Dim>> get_clone() const noexcept override;
+
+  using dg_package_field_tags =
+      tmpl::list<Tags::MassDensityCons, Tags::MomentumDensity<Dim>,
+                 Tags::EnergyDensity,
+                 ::Tags::NormalDotFlux<Tags::MassDensityCons>,
+                 ::Tags::NormalDotFlux<Tags::MomentumDensity<Dim>>,
+                 ::Tags::NormalDotFlux<Tags::EnergyDensity>, AbsCharSpeed>;
+  using dg_package_data_temporary_tags = tmpl::list<>;
+  using dg_package_data_primitive_tags =
+      tmpl::list<NewtonianEuler::Tags::Velocity<DataVector, Dim>,
+                 NewtonianEuler::Tags::SpecificInternalEnergy<DataVector>>;
+  using dg_package_data_volume_tags =
+      tmpl::list<hydro::Tags::EquationOfStateBase>;
+
+  template <size_t ThermodynamicDim>
+  double dg_package_data(
+      gsl::not_null<Scalar<DataVector>*> packaged_mass_density,
+      gsl::not_null<tnsr::I<DataVector, Dim, Frame::Inertial>*>
+          packaged_momentum_density,
+      gsl::not_null<Scalar<DataVector>*> packaged_energy_density,
+      gsl::not_null<Scalar<DataVector>*> packaged_normal_dot_flux_mass_density,
+      gsl::not_null<tnsr::I<DataVector, Dim, Frame::Inertial>*>
+          packaged_normal_dot_flux_momentum_density,
+      gsl::not_null<Scalar<DataVector>*>
+          packaged_normal_dot_flux_energy_density,
+      gsl::not_null<Scalar<DataVector>*> packaged_abs_char_speed,
+
+      const Scalar<DataVector>& mass_density,
+      const tnsr::I<DataVector, Dim, Frame::Inertial>& momentum_density,
+      const Scalar<DataVector>& energy_density,
+
+      const tnsr::I<DataVector, Dim, Frame::Inertial>& flux_mass_density,
+      const tnsr::IJ<DataVector, Dim, Frame::Inertial>& flux_momentum_density,
+      const tnsr::I<DataVector, Dim, Frame::Inertial>& flux_energy_density,
+
+      const tnsr::I<DataVector, Dim, Frame::Inertial>& velocity,
+      const Scalar<DataVector>& specific_internal_energy,
+
+      const tnsr::i<DataVector, Dim, Frame::Inertial>& normal_covector,
+      const std::optional<tnsr::I<DataVector, Dim, Frame::Inertial>>&
+      /*mesh_velocity*/,
+      const std::optional<Scalar<DataVector>>& normal_dot_mesh_velocity,
+      const EquationsOfState::EquationOfState<false, ThermodynamicDim>&
+          equation_of_state) const noexcept;
+
+  void dg_boundary_terms(
+      gsl::not_null<Scalar<DataVector>*> boundary_correction_mass_density,
+      gsl::not_null<tnsr::I<DataVector, Dim, Frame::Inertial>*>
+          boundary_correction_momentum_density,
+      gsl::not_null<Scalar<DataVector>*> boundary_correction_energy_density,
+      const Scalar<DataVector>& mass_density_int,
+      const tnsr::I<DataVector, Dim, Frame::Inertial>& momentum_density_int,
+      const Scalar<DataVector>& energy_density_int,
+      const Scalar<DataVector>& normal_dot_flux_mass_density_int,
+      const tnsr::I<DataVector, Dim, Frame::Inertial>&
+          normal_dot_flux_momentum_density_int,
+      const Scalar<DataVector>& normal_dot_flux_energy_density_int,
+      const Scalar<DataVector>& abs_char_speed_int,
+      const Scalar<DataVector>& mass_density_ext,
+      const tnsr::I<DataVector, Dim, Frame::Inertial>& momentum_density_ext,
+      const Scalar<DataVector>& energy_density_ext,
+      const Scalar<DataVector>& normal_dot_flux_mass_density_ext,
+      const tnsr::I<DataVector, Dim, Frame::Inertial>&
+          normal_dot_flux_momentum_density_ext,
+      const Scalar<DataVector>& normal_dot_flux_energy_density_ext,
+      const Scalar<DataVector>& abs_char_speed_ext,
+      dg::Formulation dg_formulation) const noexcept;
+};
+}  // namespace NewtonianEuler::BoundaryCorrections
