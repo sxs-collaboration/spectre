e1c690394a2499e016cafebc107a9c8ee9828767
William Throwe <wtt6@cornell.edu>
2021-03-01 17:53:16 -0500

    Add option-creatable comparator class

---
+++ src/Options/Comparator.cpp
@@ -0,0 +1,45 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Options/Comparator.hpp"
+
+#include <pup.h>
+#include <pup_stl.h>
+#include <string>
+
+#include "Options/Options.hpp"
+#include "Options/ParseOptions.hpp"
+
+namespace Options {
+Comparator::Comparator(Comparison comparison) noexcept
+    : comparison_(comparison) {}
+
+// NOLINTNEXTLINE(google-runtime-references)
+void Comparator::pup(PUP::er& p) noexcept { p | comparison_; }
+
+Comparator create_from_yaml<Comparator>::create_impl(const Option& options) {
+  const auto name = options.parse_as<std::string>();
+  if (name == "EqualTo") {
+    return Comparator(Comparator::Comparison::EqualTo);
+  }
+  if (name == "NotEqualTo") {
+    return Comparator(Comparator::Comparison::NotEqualTo);
+  }
+  if (name == "LessThan") {
+    return Comparator(Comparator::Comparison::LessThan);
+  }
+  if (name == "GreaterThan") {
+    return Comparator(Comparator::Comparison::GreaterThan);
+  }
+  if (name == "LessThanOrEqualTo") {
+    return Comparator(Comparator::Comparison::LessThanOrEqualTo);
+  }
+  if (name == "GreaterThanOrEqualTo") {
+    return Comparator(Comparator::Comparison::GreaterThanOrEqualTo);
+  }
+  PARSE_ERROR(options.context(),
+              "Invalid comparison " << name << ".  Should be EqualTo, "
+              "NotEqualTo, LessThan, GreaterThan, LessThanOrEqualTo, or "
+              "GreaterThanOrEqualTo.");
+}
+}  // namespace Options
