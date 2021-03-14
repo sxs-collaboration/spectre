cabb3588cbf602bb350ad45916b122b3e0ebbe69
Prayush Kumar <prayush.kumar@gmail.com>
2021-03-14 20:01:47 +0530

    Add functions to compute in/outgoing null normals to inferface
    These are the spacetime null tangent vectors / one-forms to the
    congruence of outgoing / ingoing null geodesics through any
    boundary of a closed region in the spatial slice

---
+++ src/PointwiseFunctions/GeneralRelativity/InterfaceNullNormal.cpp
@@ -0,0 +1,132 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "PointwiseFunctions/GeneralRelativity/InterfaceNullNormal.hpp"
+
+#include <cmath>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Utilities/ContainerHelpers.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+
+/// \cond
+namespace gr {
+template <size_t VolumeDim, typename Frame, typename DataType>
+tnsr::a<DataType, VolumeDim, Frame> interface_null_normal(
+    const tnsr::a<DataType, VolumeDim, Frame>& spacetime_normal_one_form,
+    const tnsr::i<DataType, VolumeDim, Frame>& interface_unit_normal_one_form,
+    const double sign) noexcept {
+  ASSERT((sign == 1.) or (sign == -1.),
+         "Calculation of interface null normal accepts only +1/-1 to indicate "
+         "whether the outgoing/incoming normal is needed.");
+  tnsr::a<DataType, VolumeDim, Frame> null_one_form(
+      get_size(get<0>(spacetime_normal_one_form)));
+  interface_null_normal(make_not_null(&null_one_form),
+                        spacetime_normal_one_form,
+                        interface_unit_normal_one_form, sign);
+  return null_one_form;
+}
+
+template <size_t VolumeDim, typename Frame, typename DataType>
+void interface_null_normal(
+    gsl::not_null<tnsr::a<DataType, VolumeDim, Frame>*> null_one_form,
+    const tnsr::a<DataType, VolumeDim, Frame>& spacetime_normal_one_form,
+    const tnsr::i<DataType, VolumeDim, Frame>& interface_unit_normal_one_form,
+    const double sign) noexcept {
+  ASSERT((sign == 1.) or (sign == -1.),
+         "Calculation of interface null normal accepts only +1/-1 to indicate "
+         "whether the outgoing/incoming normal is needed.");
+  destructive_resize_components(null_one_form,
+                                get_size(get<0>(spacetime_normal_one_form)));
+
+  const double one_by_sqrt_2 = 1. / sqrt(2.);
+  get<0>(*null_one_form) = one_by_sqrt_2 * get<0>(spacetime_normal_one_form);
+  for (size_t a = 1; a < VolumeDim + 1; ++a) {
+    null_one_form->get(a) =
+        one_by_sqrt_2 * (spacetime_normal_one_form.get(a) +
+                         sign * interface_unit_normal_one_form.get(a - 1));
+  }
+}
+
+template <size_t VolumeDim, typename Frame, typename DataType>
+tnsr::A<DataType, VolumeDim, Frame> interface_null_normal(
+    const tnsr::A<DataType, VolumeDim, Frame>& spacetime_normal_vector,
+    const tnsr::I<DataType, VolumeDim, Frame>& interface_unit_normal_vector,
+    const double sign) noexcept {
+  ASSERT((sign == 1.) or (sign == -1.),
+         "Calculation of interface null normal accepts only +1/-1 to indicate "
+         "whether the outgoing/incoming normal is needed.");
+  tnsr::A<DataType, VolumeDim, Frame> null_vector(
+      get_size(get<0>(spacetime_normal_vector)));
+  interface_null_normal(make_not_null(&null_vector), spacetime_normal_vector,
+                        interface_unit_normal_vector, sign);
+  return null_vector;
+}
+
+template <size_t VolumeDim, typename Frame, typename DataType>
+void interface_null_normal(
+    gsl::not_null<tnsr::A<DataType, VolumeDim, Frame>*> null_vector,
+    const tnsr::A<DataType, VolumeDim, Frame>& spacetime_normal_vector,
+    const tnsr::I<DataType, VolumeDim, Frame>& interface_unit_normal_vector,
+    const double sign) noexcept {
+  ASSERT((sign == 1.) or (sign == -1.),
+         "Calculation of interface null normal accepts only +1/-1 to indicate "
+         "whether the outgoing/incoming normal is needed.");
+  destructive_resize_components(null_vector,
+                                get_size(get<0>(spacetime_normal_vector)));
+
+  const double one_by_sqrt_2 = 1. / sqrt(2.);
+  get<0>(*null_vector) = one_by_sqrt_2 * get<0>(spacetime_normal_vector);
+  for (size_t a = 1; a < VolumeDim + 1; ++a) {
+    null_vector->get(a) =
+        one_by_sqrt_2 * (spacetime_normal_vector.get(a) +
+                         sign * interface_unit_normal_vector.get(a - 1));
+  }
+}
+}  // namespace gr
+
+#define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+#define DTYPE(data) BOOST_PP_TUPLE_ELEM(1, data)
+#define FRAME(data) BOOST_PP_TUPLE_ELEM(2, data)
+
+#define INSTANTIATE(_, data)                                             \
+  template tnsr::a<DTYPE(data), DIM(data), FRAME(data)>                  \
+  gr::interface_null_normal(                                             \
+      const tnsr::a<DTYPE(data), DIM(data), FRAME(data)>&                \
+          spacetime_normal_one_form,                                     \
+      const tnsr::i<DTYPE(data), DIM(data), FRAME(data)>&                \
+          interface_unit_normal_one_form,                                \
+      const double sign) noexcept;                                       \
+  template void gr::interface_null_normal(                               \
+      const gsl::not_null<tnsr::a<DTYPE(data), DIM(data), FRAME(data)>*> \
+          null_one_form,                                                 \
+      const tnsr::a<DTYPE(data), DIM(data), FRAME(data)>&                \
+          spacetime_normal_one_form,                                     \
+      const tnsr::i<DTYPE(data), DIM(data), FRAME(data)>&                \
+          interface_unit_normal_one_form,                                \
+      const double sign) noexcept;                                       \
+  template tnsr::A<DTYPE(data), DIM(data), FRAME(data)>                  \
+  gr::interface_null_normal(                                             \
+      const tnsr::A<DTYPE(data), DIM(data), FRAME(data)>&                \
+          spacetime_normal_vector,                                       \
+      const tnsr::I<DTYPE(data), DIM(data), FRAME(data)>&                \
+          interface_unit_normal_vector,                                  \
+      const double sign) noexcept;                                       \
+  template void gr::interface_null_normal(                               \
+      const gsl::not_null<tnsr::A<DTYPE(data), DIM(data), FRAME(data)>*> \
+          null_vector,                                                   \
+      const tnsr::A<DTYPE(data), DIM(data), FRAME(data)>&                \
+          spacetime_normal_vector,                                       \
+      const tnsr::I<DTYPE(data), DIM(data), FRAME(data)>&                \
+          interface_unit_normal_vector,                                  \
+      const double sign) noexcept;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3), (double, DataVector),
+                        (Frame::Grid, Frame::Inertial))
+
+#undef DIM
+#undef DTYPE
+#undef FRAME
+#undef INSTANTIATE
+/// \endcond
---
+++ src/PointwiseFunctions/GeneralRelativity/InterfaceNullNormal.hpp
@@ -0,0 +1,77 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+#include "DataStructures/Tensor/TypeAliases.hpp"
+
+/// \cond
+namespace gsl {
+template <typename>
+struct not_null;
+}  // namespace gsl
+/// \endcond
+
+namespace gr {
+// @{
+/*!
+ * \ingroup GeneralRelativityGroup
+ * \brief Compute null normal one-form to the boundary of a closed
+ * region in a spatial slice of spacetime.
+ *
+ * \details Consider an \f$n-1\f$-dimensional boundary \f$S\f$ of a closed
+ * region in an \f$n\f$-dimensional spatial hypersurface \f$\Sigma\f$. Let
+ * \f$s^a\f$ be the unit spacelike vector orthogonal to \f$S\f$ in \f$\Sigma\f$,
+ * and \f$n^a\f$ be the timelike unit vector orthogonal to \f$\Sigma\f$.
+ * This function returns the null one-form that is outgoing/incoming on \f$S\f$:
+ *
+ * \f{align*}
+ * k_a = \frac{1}{\sqrt{2}}\left(n_a \pm s_a\right).
+ * \f}
+ */
+template <size_t VolumeDim, typename Frame, typename DataType>
+tnsr::a<DataType, VolumeDim, Frame> interface_null_normal(
+    const tnsr::a<DataType, VolumeDim, Frame>& spacetime_normal_one_form,
+    const tnsr::i<DataType, VolumeDim, Frame>& interface_unit_normal_one_form,
+    const double sign) noexcept;
+
+template <size_t VolumeDim, typename Frame, typename DataType>
+void interface_null_normal(
+    gsl::not_null<tnsr::a<DataType, VolumeDim, Frame>*> null_one_form,
+    const tnsr::a<DataType, VolumeDim, Frame>& spacetime_normal_one_form,
+    const tnsr::i<DataType, VolumeDim, Frame>& interface_unit_normal_one_form,
+    const double sign) noexcept;
+// @}
+
+// @{
+/*!
+ * \ingroup GeneralRelativityGroup
+ * \brief Compute null normal vector to the boundary of a closed
+ * region in a spatial slice of spacetime.
+ *
+ * \details Consider an \f$n-1\f$-dimensional boundary \f$S\f$ of a closed
+ * region in an \f$n\f$-dimensional spatial hypersurface \f$\Sigma\f$. Let
+ * \f$s^a\f$ be the unit spacelike vector orthogonal to \f$S\f$ in \f$\Sigma\f$,
+ * and \f$n^a\f$ be the timelike unit vector orthogonal to \f$\Sigma\f$.
+ * This function returns the null vector that is outgoing/ingoing on \f$S\f$:
+ *
+ * \f{align*}
+ * k^a = \frac{1}{\sqrt{2}}\left(n^a \pm s^a\right).
+ * \f}
+ */
+template <size_t VolumeDim, typename Frame, typename DataType>
+tnsr::A<DataType, VolumeDim, Frame> interface_null_normal(
+    const tnsr::A<DataType, VolumeDim, Frame>& spacetime_normal_vector,
+    const tnsr::I<DataType, VolumeDim, Frame>& interface_unit_normal_vector,
+    const double sign) noexcept;
+
+template <size_t VolumeDim, typename Frame, typename DataType>
+void interface_null_normal(
+    gsl::not_null<tnsr::A<DataType, VolumeDim, Frame>*> null_vector,
+    const tnsr::A<DataType, VolumeDim, Frame>& spacetime_normal_vector,
+    const tnsr::I<DataType, VolumeDim, Frame>& interface_unit_normal_vector,
+    const double sign) noexcept;
+// @}
+}  // namespace gr
