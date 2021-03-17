5ddc8db2c86a640f5dd1c2c1bf5b82c0b716eb99
William Throwe <wtt6@cornell.edu>
2021-03-16 23:04:09 -0400

    Move NearTimes implementation to cpp

--- src/Time/Triggers/NearTimes.hpp
+++ src/Time/Triggers/NearTimes.hpp
@@ -93,46 +93,10 @@ class NearTimes : public Trigger {
 
   using argument_tags = tmpl::list<Tags::Time, Tags::TimeStep>;
 
-  bool operator()(const double now, const TimeDelta& time_step) const noexcept {
-    const bool time_runs_forward = time_step.is_positive();
-
-    double range_code_units = range_;
-    if (unit_ == Unit::Slab) {
-      range_code_units *= time_step.slab().duration().value();
-    } else if (unit_ == Unit::Step) {
-      range_code_units *= std::abs(time_step.value());
-    }
-
-    if (not time_runs_forward) {
-      range_code_units = -range_code_units;
-    }
-
-    // Interval around now to look for trigger times in.
-    auto trigger_range = std::make_pair(
-        direction_ == Direction::Before ? now : now - range_code_units,
-        direction_ == Direction::After ? now : now + range_code_units);
-
-    if (not time_runs_forward) {
-      std::swap(trigger_range.first, trigger_range.second);
-    }
-
-    const auto nearby_times = times_->times_near(trigger_range.first);
-    for (const auto& time : nearby_times) {
-      if (time and *time >= trigger_range.first and
-          *time <= trigger_range.second) {
-        return true;
-      }
-    }
-    return false;
-  }
+  bool operator()(const double now, const TimeDelta& time_step) const noexcept;
 
   // NOLINTNEXTLINE(google-runtime-references)
-  void pup(PUP::er& p) noexcept override {
-    p | times_;
-    p | range_;
-    p | unit_;
-    p | direction_;
-  }
+  void pup(PUP::er& p) noexcept override;
 
  private:
   std::unique_ptr<TimeSequence<double>> times_{};
