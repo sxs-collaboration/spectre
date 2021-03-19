07febe16a93745835b60d8379d807842c081b2b4
William Throwe <wtt6@cornell.edu>
2021-03-18 20:50:59 -0400

    Factor common code in EvolutionOrdering

--- src/Time/EvolutionOrdering.hpp
+++ src/Time/EvolutionOrdering.hpp
@@ -3,108 +3,64 @@
 
 #pragma once
 
+#include <functional>
 #include <pup.h>
 #include <utility>
 
 /// \ingroup TimeGroup
-/// Ordering functors that reverse their order when time runs
-/// backwards.
+/// Implementation of \ref evolution_less, \ref evolution_greater,
+/// \ref evolution_less_equal, and \ref evolution_greater_equal.
 ///
-/// \see std::less
+/// This should only be used through the named aliases, but provides
+/// the documentation of the members.
 //@{
-template <typename T = void>
-struct evolution_less {
+template <typename T, template <typename> typename Comparator>
+struct evolution_comparator {
   bool time_runs_forward = true;
   constexpr bool operator()(const T& x, const T& y) const noexcept {
-    return time_runs_forward ? x < y : y < x;
+    return time_runs_forward ? Comparator<T>{}(x, y) : Comparator<T>{}(y, x);
   }
 
   // NOLINTNEXTLINE(google-runtime-references)
   void pup(PUP::er& p) noexcept { p | time_runs_forward; }
 };
 
-template <typename T = void>
-struct evolution_greater {
+template <template <typename> typename Comparator>
+struct evolution_comparator<void, Comparator> {
   bool time_runs_forward = true;
-  constexpr bool operator()(const T& x, const T& y) const noexcept {
-    return time_runs_forward ? x > y : y > x;
-  }
 
-  // NOLINTNEXTLINE(google-runtime-references)
-  void pup(PUP::er& p) noexcept { p | time_runs_forward; }
-};
-
-template <typename T = void>
-struct evolution_less_equal {
-  bool time_runs_forward = true;
-  constexpr bool operator()(const T& x, const T& y) const noexcept {
-    return time_runs_forward ? x <= y : y <= x;
-  }
-
-  // NOLINTNEXTLINE(google-runtime-references)
-  void pup(PUP::er& p) noexcept { p | time_runs_forward; }
-};
-
-template <typename T = void>
-struct evolution_greater_equal {
-  bool time_runs_forward = true;
-  constexpr bool operator()(const T& x, const T& y) const noexcept {
-    return time_runs_forward ? x >= y : y >= x;
-  }
-
-  // NOLINTNEXTLINE(google-runtime-references)
-  void pup(PUP::er& p) noexcept { p | time_runs_forward; }
-};
-
-template <>
-struct evolution_less<void> {
-  bool time_runs_forward = true;
   template <typename T, typename U>
   constexpr auto operator()(T&& t, U&& u) const noexcept {
-    return time_runs_forward ? std::forward<T>(t) < std::forward<U>(u)
-                             : std::forward<U>(u) < std::forward<T>(t);
-  }
-
-  // NOLINTNEXTLINE(google-runtime-references)
-  void pup(PUP::er& p) noexcept { p | time_runs_forward; }
-};
-
-template <>
-struct evolution_greater<void> {
-  bool time_runs_forward = true;
-  template <typename T, typename U>
-  constexpr auto operator()(T&& t, U&& u) const noexcept {
-    return time_runs_forward ? std::forward<T>(t) > std::forward<U>(u)
-                             : std::forward<U>(u) > std::forward<T>(t);
-  }
-
-  // NOLINTNEXTLINE(google-runtime-references)
-  void pup(PUP::er& p) noexcept { p | time_runs_forward; }
-};
-
-template <>
-struct evolution_less_equal<void> {
-  bool time_runs_forward = true;
-  template <typename T, typename U>
-  constexpr auto operator()(T&& t, U&& u) const noexcept {
-    return time_runs_forward ? std::forward<T>(t) <= std::forward<U>(u)
-                             : std::forward<U>(u) <= std::forward<T>(t);
-  }
-
-  // NOLINTNEXTLINE(google-runtime-references)
-  void pup(PUP::er& p) noexcept { p | time_runs_forward; }
-};
-
-template <>
-struct evolution_greater_equal<void> {
-  bool time_runs_forward = true;
-  template <typename T, typename U>
-  constexpr auto operator()(T&& t, U&& u) const noexcept {
-    return time_runs_forward ? std::forward<T>(t) >= std::forward<U>(u)
-                             : std::forward<U>(u) >= std::forward<T>(t);
+    return time_runs_forward
+               ? Comparator<void>{}(std::forward<T>(t), std::forward<U>(u))
+               : Comparator<void>{}(std::forward<U>(u), std::forward<T>(t));
   }
 
   // NOLINTNEXTLINE(google-runtime-references)
   void pup(PUP::er& p) noexcept { p | time_runs_forward; }
 };
 //@}
+
+/// \ingroup TimeGroup
+/// Ordering functors that reverse their order when time runs
+/// backwards.  See evolution_comparator and
+/// evolution_comparator<void,Comparator> for the provided interface.
+///
+/// \see std::less
+template <typename T = void>
+using evolution_less = evolution_comparator<T, std::less>;
+
+/// \ingroup TimeGroup
+/// \copydoc evolution_less
+template <typename T = void>
+using evolution_greater = evolution_comparator<T, std::greater>;
+
+/// \ingroup TimeGroup
+/// \copydoc evolution_less
+template <typename T = void>
+using evolution_less_equal = evolution_comparator<T, std::less_equal>;
+
+/// \ingroup TimeGroup
+/// \copydoc evolution_less
+template <typename T = void>
+using evolution_greater_equal = evolution_comparator<T, std::greater_equal>;
