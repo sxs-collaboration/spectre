57e7e69aca9a8105d3b8a375e35f370bec023eb9
Francois Hebert <fmh33@cornell.edu>
2021-03-22 17:13:31 -0700

    Add Rusanov boundary correction for M1Grey system

---
+++ src/Evolution/Systems/RadiationTransport/M1Grey/BoundaryCorrections/Rusanov.hpp
@@ -0,0 +1,207 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <memory>
+#include <optional>
+
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Evolution/Systems/RadiationTransport/M1Grey/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/RadiationTransport/M1Grey/Tags.hpp"
+#include "NumericalAlgorithms/DiscontinuousGalerkin/Formulation.hpp"
+#include "Options/Options.hpp"
+#include "Parallel/CharmPupable.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
+#include "PointwiseFunctions/Hydro/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+class DataVector;
+namespace gsl {
+template <typename T>
+class not_null;
+}  // namespace gsl
+namespace PUP {
+class er;
+}  // namespace PUP
+/// \endcond
+
+namespace RadiationTransport::M1Grey::BoundaryCorrections {
+
+namespace Rusanov_detail {
+void dg_package_data_impl(
+    gsl::not_null<Scalar<DataVector>*> packaged_tilde_e,
+    gsl::not_null<tnsr::i<DataVector, 3, Frame::Inertial>*> packaged_tilde_s,
+    gsl::not_null<Scalar<DataVector>*> packaged_normal_dot_flux_tilde_e,
+    gsl::not_null<tnsr::i<DataVector, 3, Frame::Inertial>*>
+        packaged_normal_dot_flux_tilde_s,
+    const Scalar<DataVector>& tilde_e,
+    const tnsr::i<DataVector, 3, Frame::Inertial>& tilde_s,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& flux_tilde_e,
+    const tnsr::Ij<DataVector, 3, Frame::Inertial>& flux_tilde_s,
+    const tnsr::i<DataVector, 3, Frame::Inertial>& normal_covector,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& normal_vector,
+    const std::optional<tnsr::I<DataVector, 3, Frame::Inertial>>& mesh_velocity,
+    const std::optional<Scalar<DataVector>>& normal_dot_mesh_velocity) noexcept;
+
+void dg_boundary_terms_impl(
+    gsl::not_null<Scalar<DataVector>*> boundary_correction_tilde_e,
+    gsl::not_null<tnsr::i<DataVector, 3, Frame::Inertial>*>
+        boundary_correction_tilde_s,
+    const Scalar<DataVector>& tilde_e_int,
+    const tnsr::i<DataVector, 3, Frame::Inertial>& tilde_s_int,
+    const Scalar<DataVector>& normal_dot_flux_tilde_e_int,
+    const tnsr::i<DataVector, 3, Frame::Inertial>& normal_dot_flux_tilde_s_int,
+    const Scalar<DataVector>& tilde_e_ext,
+    const tnsr::i<DataVector, 3, Frame::Inertial>& tilde_s_ext,
+    const Scalar<DataVector>& normal_dot_flux_tilde_e_ext,
+    const tnsr::i<DataVector, 3, Frame::Inertial>& normal_dot_flux_tilde_s_ext,
+    dg::Formulation dg_formulation) noexcept;
+}  // namespace Rusanov_detail
+
+/*!
+ * \brief A Rusanov/local Lax-Friedrichs Riemann solver
+ *
+ * Let \f$U\f$ be the state vector of evolved variables, \f$F^i\f$ the
+ * corresponding fluxes, and \f$n_i\f$ be the outward directed unit normal to
+ * the interface. Denoting \f$F := n_i F^i\f$, the %Rusanov boundary correction
+ * is
+ *
+ * \f{align*}
+ * G_\text{Rusanov} = \frac{F_\text{int} - F_\text{ext}}{2} -
+ * \frac{\text{max}\left(\{|\lambda_\text{int}|\},
+ * \{|\lambda_\text{ext}|\}\right)}{2} \left(U_\text{ext} - U_\text{int}\right),
+ * \f}
+ *
+ * where "int" and "ext" stand for interior and exterior, and
+ * \f$\{|\lambda|\}\f$ is the set of characteristic/signal speeds. The minus
+ * sign in front of the \f$F_{\text{ext}}\f$ is necessary because the outward
+ * directed normal of the neighboring element has the opposite sign, i.e.
+ * \f$n_i^{\text{ext}}=-n_i^{\text{int}}\f$.
+ *
+ * For radiation, the max characteristic/signal speed is 1 (the speed of light).
+ * Note that the characteristic speeds of this system are *not* yet fully
+ * implemented, see the warning in the documentation for the characteristics.
+ *
+ * \note In the strong form the `dg_boundary_terms` function returns
+ * \f$G - F_\text{int}\f$
+ */
+template <typename... NeutrinoSpecies>
+class Rusanov final : public BoundaryCorrection<NeutrinoSpecies...> {
+ public:
+  using options = tmpl::list<>;
+  static constexpr Options::String help = {
+      "Computes the Rusanov or local Lax-Friedrichs boundary correction term "
+      "for the M1Grey radiation transport system."};
+
+  Rusanov() = default;
+  Rusanov(const Rusanov&) = default;
+  Rusanov& operator=(const Rusanov&) = default;
+  Rusanov(Rusanov&&) = default;
+  Rusanov& operator=(Rusanov&&) = default;
+  ~Rusanov() override = default;
+
+  /// \cond
+  explicit Rusanov(CkMigrateMessage* msg) noexcept
+      : BoundaryCorrection<NeutrinoSpecies...>(msg) {}
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(Rusanov);  // NOLINT
+  /// \endcond
+  void pup(PUP::er& p) override {  // NOLINT
+    BoundaryCorrection<NeutrinoSpecies...>::pup(p);
+  }
+
+  std::unique_ptr<BoundaryCorrection<NeutrinoSpecies...>> get_clone()
+      const noexcept override {
+    return std::make_unique<Rusanov>(*this);
+  }
+
+  using dg_package_field_tags = tmpl::list<
+      Tags::TildeE<Frame::Inertial, NeutrinoSpecies>...,
+      Tags::TildeS<Frame::Inertial, NeutrinoSpecies>...,
+      ::Tags::NormalDotFlux<Tags::TildeE<Frame::Inertial, NeutrinoSpecies>>...,
+      ::Tags::NormalDotFlux<Tags::TildeS<Frame::Inertial, NeutrinoSpecies>>...>;
+  using dg_package_data_temporary_tags = tmpl::list<>;
+  using dg_package_data_primitive_tags = tmpl::list<>;
+  using dg_package_data_volume_tags = tmpl::list<>;
+
+  double dg_package_data(
+      const gsl::not_null<typename Tags::TildeE<
+          Frame::Inertial, NeutrinoSpecies>::type*>... packaged_tilde_e,
+      const gsl::not_null<typename Tags::TildeS<
+          Frame::Inertial, NeutrinoSpecies>::type*>... packaged_tilde_s,
+      const gsl::not_null<typename Tags::TildeE<
+          Frame::Inertial,
+          NeutrinoSpecies>::type*>... packaged_normal_dot_flux_tilde_e,
+      const gsl::not_null<typename Tags::TildeS<
+          Frame::Inertial,
+          NeutrinoSpecies>::type*>... packaged_normal_dot_flux_tilde_s,
+
+      const typename Tags::TildeE<Frame::Inertial,
+                                  NeutrinoSpecies>::type&... tilde_e,
+      const typename Tags::TildeS<Frame::Inertial,
+                                  NeutrinoSpecies>::type&... tilde_s,
+      const typename ::Tags::Flux<
+          Tags::TildeE<Frame::Inertial, NeutrinoSpecies>, tmpl::size_t<3>,
+          Frame::Inertial>::type&... flux_tilde_e,
+      const typename ::Tags::Flux<
+          Tags::TildeS<Frame::Inertial, NeutrinoSpecies>, tmpl::size_t<3>,
+          Frame::Inertial>::type&... flux_tilde_s,
+
+      const tnsr::i<DataVector, 3, Frame::Inertial>& normal_covector,
+      const tnsr::I<DataVector, 3, Frame::Inertial>& normal_vector,
+      const std::optional<tnsr::I<DataVector, 3, Frame::Inertial>>&
+          mesh_velocity,
+      const std::optional<Scalar<DataVector>>& normal_dot_mesh_velocity)
+      const noexcept {
+    EXPAND_PACK_LEFT_TO_RIGHT(Rusanov_detail::dg_package_data_impl(
+        packaged_tilde_e, packaged_tilde_s, packaged_normal_dot_flux_tilde_e,
+        packaged_normal_dot_flux_tilde_s, tilde_e, tilde_s, flux_tilde_e,
+        flux_tilde_s, normal_covector, normal_vector, mesh_velocity,
+        normal_dot_mesh_velocity));
+    // max speed = speed of light
+    return 1.0;
+  }
+
+  void dg_boundary_terms(
+      const gsl::not_null<typename Tags::TildeE<
+          Frame::Inertial,
+          NeutrinoSpecies>::type*>... boundary_correction_tilde_e,
+      const gsl::not_null<typename Tags::TildeS<
+          Frame::Inertial,
+          NeutrinoSpecies>::type*>... boundary_correction_tilde_s,
+      const typename Tags::TildeE<Frame::Inertial,
+                                  NeutrinoSpecies>::type&... tilde_e_int,
+      const typename Tags::TildeS<Frame::Inertial,
+                                  NeutrinoSpecies>::type&... tilde_s_int,
+      const typename Tags::TildeE<Frame::Inertial, NeutrinoSpecies>::
+          type&... normal_dot_flux_tilde_e_int,
+      const typename Tags::TildeS<Frame::Inertial, NeutrinoSpecies>::
+          type&... normal_dot_flux_tilde_s_int,
+      const typename Tags::TildeE<Frame::Inertial,
+                                  NeutrinoSpecies>::type&... tilde_e_ext,
+      const typename Tags::TildeS<Frame::Inertial,
+                                  NeutrinoSpecies>::type&... tilde_s_ext,
+      const typename Tags::TildeE<Frame::Inertial, NeutrinoSpecies>::
+          type&... normal_dot_flux_tilde_e_ext,
+      const typename Tags::TildeS<Frame::Inertial, NeutrinoSpecies>::
+          type&... normal_dot_flux_tilde_s_ext,
+      dg::Formulation dg_formulation) const noexcept {
+    EXPAND_PACK_LEFT_TO_RIGHT(Rusanov_detail::dg_boundary_terms_impl(
+        boundary_correction_tilde_e, boundary_correction_tilde_s, tilde_e_int,
+        tilde_s_int, normal_dot_flux_tilde_e_int, normal_dot_flux_tilde_s_int,
+        tilde_e_ext, tilde_s_ext, normal_dot_flux_tilde_e_ext,
+        normal_dot_flux_tilde_s_ext, dg_formulation));
+  }
+};
+
+/// \cond
+template <typename... NeutrinoSpecies>
+// NOLINTNEXTLINE
+PUP::able::PUP_ID Rusanov<NeutrinoSpecies...>::my_PUP_ID = 0;
+/// \endcond
+
+}  // namespace RadiationTransport::M1Grey::BoundaryCorrections
---
+++ tests/Unit/Evolution/Systems/RadiationTransport/M1Grey/BoundaryCorrections/Test_Rusanov.cpp
@@ -0,0 +1,84 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <array>
+#include <cstddef>
+#include <string>
+
+#include "Evolution/Systems/RadiationTransport/M1Grey/BoundaryCorrections/Rusanov.hpp"
+#include "Evolution/Systems/RadiationTransport/M1Grey/System.hpp"
+#include "Evolution/Systems/RadiationTransport/Tags.hpp"
+#include "Framework/SetupLocalPythonEnvironment.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Helpers/Evolution/DiscontinuousGalerkin/BoundaryCorrections.hpp"
+#include "Helpers/Evolution/DiscontinuousGalerkin/Range.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
+#include "Parallel/RegisterDerivedClassesWithCharm.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace {
+namespace helpers = TestHelpers::evolution::dg;
+
+void test(const gsl::not_null<std::mt19937*> gen, const size_t num_pts) {
+  using system = RadiationTransport::M1Grey::System<tmpl::list<
+      neutrinos::ElectronNeutrinos<1>, neutrinos::ElectronAntiNeutrinos<1>>>;
+  using rusanov = RadiationTransport::M1Grey::BoundaryCorrections::Rusanov<
+      neutrinos::ElectronNeutrinos<1>, neutrinos::ElectronAntiNeutrinos<1>>;
+
+  helpers::test_boundary_correction_conservation<system>(
+      gen, rusanov{},
+      Mesh<2>{num_pts, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss},
+      {}, {});
+
+  helpers::test_boundary_correction_with_python<system>(
+      gen, "Rusanov",
+      {{"dg_package_data_tilde_e_nue", "dg_package_data_tilde_e_bar_nue",
+        "dg_package_data_tilde_s_nue", "dg_package_data_tilde_s_bar_nue",
+        "dg_package_data_normal_dot_flux_tilde_e_nue",
+        "dg_package_data_normal_dot_flux_tilde_e_bar_nue",
+        "dg_package_data_normal_dot_flux_tilde_s_nue",
+        "dg_package_data_normal_dot_flux_tilde_s_bar_nue"}},
+      {{"dg_boundary_terms_tilde_e_nue", "dg_boundary_terms_tilde_e_bar_nue",
+        "dg_boundary_terms_tilde_s_nue", "dg_boundary_terms_tilde_s_bar_nue"}},
+      rusanov{},
+      Mesh<2>{num_pts, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss},
+      {}, {});
+
+  const auto rusanov_from_factory = TestHelpers::test_factory_creation<
+      RadiationTransport::M1Grey::BoundaryCorrections::BoundaryCorrection<
+          neutrinos::ElectronNeutrinos<1>,
+          neutrinos::ElectronAntiNeutrinos<1>>>("Rusanov:");
+
+  helpers::test_boundary_correction_with_python<system>(
+      gen, "Rusanov",
+      {{"dg_package_data_tilde_e_nue", "dg_package_data_tilde_e_bar_nue",
+        "dg_package_data_tilde_s_nue", "dg_package_data_tilde_s_bar_nue",
+        "dg_package_data_normal_dot_flux_tilde_e_nue",
+        "dg_package_data_normal_dot_flux_tilde_e_bar_nue",
+        "dg_package_data_normal_dot_flux_tilde_s_nue",
+        "dg_package_data_normal_dot_flux_tilde_s_bar_nue"}},
+      {{"dg_boundary_terms_tilde_e_nue", "dg_boundary_terms_tilde_e_bar_nue",
+        "dg_boundary_terms_tilde_s_nue", "dg_boundary_terms_tilde_s_bar_nue"}},
+      dynamic_cast<const rusanov&>(*rusanov_from_factory),
+      Mesh<2>{num_pts, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss},
+      {}, {});
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.RadiationTransport.M1Grey.Rusanov",
+                  "[Unit][Evolution]") {
+  using rusanov = RadiationTransport::M1Grey::BoundaryCorrections::Rusanov<
+      neutrinos::ElectronNeutrinos<1>, neutrinos::ElectronAntiNeutrinos<1>>;
+  PUPable_reg(rusanov);
+  Parallel::register_derived_classes_with_charm<rusanov>();
+
+  pypp::SetupLocalPythonEnvironment local_python_env{
+      "Evolution/Systems/RadiationTransport/M1Grey/BoundaryCorrections"};
+  MAKE_GENERATOR(gen);
+
+  test(make_not_null(&gen), 5);
+}
