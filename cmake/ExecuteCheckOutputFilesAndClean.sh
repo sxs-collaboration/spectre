83937427055d416bc6ba28366ecefdd4ceb4f944
Jordan Moxon <jem497@cornell.edu>
2021-03-25 16:53:30 -0700

    Add optional command line arguments to InputFile checks

--- cmake/ExecuteCheckOutputFilesAndClean.sh
+++ cmake/ExecuteCheckOutputFilesAndClean.sh
@@ -7,7 +7,7 @@
                     --input-file $2 --output-dir @CMAKE_BINARY_DIR@/$3
 mkdir @CMAKE_BINARY_DIR@/$3
 cd @CMAKE_BINARY_DIR@/$3
-@CMAKE_BINARY_DIR@/bin/$1 --input-file $2 &&
+@CMAKE_BINARY_DIR@/bin/$1 --input-file $2 ${4} &&
     @Python_EXECUTABLE@ @CMAKE_SOURCE_DIR@/tools/CheckOutputFiles.py \
      --input-file $2 --run-directory @CMAKE_BINARY_DIR@/$3 &&
 @Python_EXECUTABLE@ @CMAKE_SOURCE_DIR@/tools/CleanOutput.py -v \
