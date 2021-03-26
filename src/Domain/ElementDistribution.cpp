b9bd86045c2f24423ed54c87bbee60c064151230
Jordan Moxon <jem497@cornell.edu>
2021-03-26 16:04:23 -0700

    Add Z-curve based element distribution

---
+++ src/Domain/ElementDistribution.cpp
@@ -0,0 +1,169 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Domain/ElementDistribution.hpp"
+
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+#include "Domain/Structure/ElementId.hpp"
+#include "Utilities/Algorithm.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+
+namespace domain {
+
+namespace {
+// This interleaves the bits of the element index.
+// A sketch of a 2D block with 4x2 elements, with bit indices and resulting
+// z-curve
+//
+//        x-->
+//        00  01  10  11
+// y  0 |  0   2   4   6
+// |    |
+// v  1 |  1   3   5   7
+template <size_t Dim>
+size_t z_curve_index(const ElementId<Dim>& element_id) noexcept {
+  // for the bit manipulation of the element index, we need to interleave the
+  // indices in each dimension in order according to how many bits are in the
+  // index representation. This variable stores the refinement level and
+  // dimension index in ascending order of refinement level, representing a
+  // permutation of the dimensions
+  std::array<std::pair<size_t, size_t>, Dim>
+      dimension_by_highest_refinement_level;
+  for (size_t i = 0; i < Dim; ++i) {
+    dimension_by_highest_refinement_level.at(i) = std::make_pair(
+        gsl::at(element_id.segment_ids(), i).refinement_level(), i);
+  }
+  alg::sort(dimension_by_highest_refinement_level,
+            [](const std::pair<size_t, size_t>& lhs,
+               const std::pair<size_t, size_t>& rhs) {
+              return lhs.first < rhs.first;
+            });
+
+  size_t element_order_index = 0;
+
+  // 'gap' the lowest refinement direction bits as:
+  // ... x1 x0 -> ... x1 0 0 x0,
+  // then bitwise or in 'gap'ed and shifted next-lowest refinement direction
+  // bits as:
+  // ... y2 y1 y0 -> ... y2 0 y1 x1 0 y0 x0
+  // then bitwise or in 'gap'ed and shifted highest-refinement direction bits
+  // as:
+  // ... z3 z2 z1 z0 -> z3 z2 y2 z1 y1 x1 z0 y0 x0
+  // note that we must skip refinement-level 0 dimensions as though they are
+  // not present
+  size_t leading_gap = 0;
+  for (size_t i = 0; i < Dim; ++i) {
+    const size_t id_to_gap_and_shift =
+        gsl::at(element_id.segment_ids(),
+                gsl::at(dimension_by_highest_refinement_level, i).second)
+            .index();
+    size_t total_gap = leading_gap;
+    if (gsl::at(dimension_by_highest_refinement_level, i).first > 0) {
+      ++leading_gap;
+    }
+    for (size_t bit_index = 0;
+         bit_index < gsl::at(dimension_by_highest_refinement_level, i).first;
+         ++bit_index) {
+      // This operation will not overflow for our present use of `ElementId`s.
+      // This technique densely assigns an ElementID a unique size_t identifier
+      // determining the Morton curve order, and `ElementId` supports refinement
+      // levels such that a global index within a block will fit in a 64-bit
+      // unsigned integer.
+      element_order_index |=
+          ((id_to_gap_and_shift & two_to_the(bit_index)) << total_gap);
+      for (size_t j = 0; j < Dim; ++j) {
+        if (i != j and
+            bit_index + 1 <
+                gsl::at(dimension_by_highest_refinement_level, j).first) {
+          ++total_gap;
+        }
+      }
+    }
+  }
+  return element_order_index;
+}
+}  // namespace
+
+template <size_t Dim>
+BlockZCurveProcDistribution<Dim>::BlockZCurveProcDistribution(
+    size_t number_of_procs,
+    const std::vector<std::array<size_t, Dim>>& refinements_by_block) noexcept {
+  block_element_distribution_ =
+      std::vector<std::vector<std::pair<size_t, size_t>>>(
+          refinements_by_block.size());
+  auto add_number_of_elements_for_refinement =
+      [](size_t lhs, const std::array<size_t, Dim>& rhs) {
+        size_t value = 1;
+        for (size_t i = 0; i < Dim; ++i) {
+          value *= two_to_the(gsl::at(rhs, i));
+        }
+        return lhs + value;
+      };
+  const size_t number_of_elements =
+      std::accumulate(refinements_by_block.begin(), refinements_by_block.end(),
+                      0_st, add_number_of_elements_for_refinement);
+  ASSERT(not refinements_by_block.empty(),
+         "`refinements_by_block` must be non-empty.");
+  // currently, we just assign uniform weight to elements. In future, it will
+  // probably be better to take into account p-refinement, but then the z-curve
+  // method will also require weighting.
+  size_t remaining_elements_in_block =
+      add_number_of_elements_for_refinement(0_st, refinements_by_block[0]);
+  size_t current_block = 0;
+  for (size_t i = 0; i < number_of_procs; ++i) {
+    size_t remaining_elements_on_proc =
+        (number_of_elements / number_of_procs) +
+        (i < (number_of_elements % number_of_procs) ? 1 : 0);
+    while (remaining_elements_on_proc > 0) {
+      block_element_distribution_.at(current_block)
+          .emplace_back(
+              std::make_pair(i, std::min(remaining_elements_in_block,
+                                         remaining_elements_on_proc)));
+      if (remaining_elements_in_block <= remaining_elements_on_proc) {
+        remaining_elements_on_proc -= remaining_elements_in_block;
+        ++current_block;
+        if (current_block < refinements_by_block.size()) {
+          remaining_elements_in_block = add_number_of_elements_for_refinement(
+              0_st, gsl::at(refinements_by_block, current_block));
+        }
+      } else {
+        remaining_elements_in_block -= remaining_elements_on_proc;
+        remaining_elements_on_proc = 0;
+      }
+    }
+  }
+}
+
+template <size_t Dim>
+size_t BlockZCurveProcDistribution<Dim>::get_proc_for_element(
+    const size_t block_id, const ElementId<Dim>& element_id) const noexcept {
+  const size_t element_order_index = z_curve_index(element_id);
+  size_t total_so_far = 0;
+  for (const std::pair<size_t, size_t>& element_info :
+       gsl::at(block_element_distribution_, block_id)) {
+    if (total_so_far <= element_order_index and
+        element_info.second + total_so_far > element_order_index) {
+      return element_info.first;
+    }
+    total_so_far += element_info.second;
+  }
+  ERROR(
+      "Processor not successfully chosen. This indicates a flaw in the logic "
+      "of BlockZCurveProcDistribution.");
+}
+#define GET_DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATION(r, data) \
+  template class BlockZCurveProcDistribution<GET_DIM(data)>;
+
+GENERATE_INSTANTIATIONS(INSTANTIATION, (1, 2, 3))
+
+#undef GET_DIM
+#undef INSTANTIATION
+}  // namespace domain
---
+++ tests/Unit/Domain/Test_ElementDistribution.cpp
@@ -0,0 +1,293 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <set>
+#include <vector>
+
+#include "Domain/ElementDistribution.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Structure/SegmentId.hpp"
+#include "Utilities/Literals.hpp"
+
+namespace {
+
+// a global indexing to help aggregating the processor maps in the tests
+template <size_t Dim>
+std::array<SegmentId, Dim> element_index_to_segment_id(
+    const std::array<size_t, Dim>& refinement_levels,
+    const size_t element_index) noexcept {
+  std::array<SegmentId, Dim> segment_ids{};
+  size_t stride = 1;
+  for (size_t i = 0; i < Dim; ++i) {
+    const size_t element_index_in_dim =
+        (element_index / stride) % two_to_the(gsl::at(refinement_levels, i));
+    segment_ids.at(i) =
+        SegmentId{gsl::at(refinement_levels, i), element_index_in_dim};
+    stride *= two_to_the(gsl::at(refinement_levels, i));
+  }
+  return segment_ids;
+}
+
+template <size_t Dim>
+size_t segment_id_to_element_index(
+    const std::array<SegmentId, Dim>& segment_ids) noexcept {
+  size_t element_index = 0;
+  size_t stride = 1;
+  for (size_t i = 0; i < Dim; ++i) {
+    element_index += stride * gsl::at(segment_ids, i).index();
+    stride *= two_to_the(gsl::at(segment_ids, i).refinement_level());
+  }
+  return element_index;
+}
+
+template <size_t Dim>
+size_t number_of_elements_in_block(
+    const std::array<size_t, Dim>& refinement_levels) noexcept {
+  size_t number_of_elements = 1;
+  for (size_t i = 0; i < Dim; ++i) {
+    number_of_elements *= two_to_the(gsl::at(refinement_levels, i));
+  }
+  return number_of_elements;
+}
+
+template <size_t Dim>
+std::vector<std::vector<size_t>> make_proc_map_for_domain(
+    const size_t number_of_blocks, const size_t number_of_procs,
+    const std::vector<std::array<size_t, Dim>>&
+        refinement_levels_by_block) noexcept {
+  std::vector<std::vector<size_t>> proc_map(number_of_blocks);
+  const domain::BlockZCurveProcDistribution distribution{
+      number_of_procs, refinement_levels_by_block};
+  for (size_t block = 0; block < number_of_blocks; ++block) {
+    const size_t number_of_elements =
+        number_of_elements_in_block(gsl::at(refinement_levels_by_block, block));
+    proc_map.at(block) = std::vector<size_t>(number_of_elements);
+    for (size_t element_index = 0; element_index < number_of_elements;
+         ++element_index) {
+      const ElementId<Dim> element_id{
+          block,
+          element_index_to_segment_id(
+              gsl::at(refinement_levels_by_block, block), element_index)};
+      proc_map.at(block).at(element_index) =
+          distribution.get_proc_for_element(block, element_id);
+    }
+  }
+  return proc_map;
+}
+
+// check that the distribution portions out the number of elements approximately
+// evenly (within 1 element) to each processor
+template <size_t Dim>
+void check_element_distribution_uniformity(
+    const std::vector<std::vector<size_t>>& proc_map,
+    const size_t number_of_procs,
+    const std::vector<std::array<size_t, Dim>>&
+        refinement_levels_by_block) noexcept {
+  std::vector<size_t> elements_per_proc(number_of_procs);
+  for (const auto& block_proc_map : proc_map) {
+    for (const size_t proc : block_proc_map) {
+      ++elements_per_proc.at(proc);
+    }
+  }
+  const size_t number_of_elements = std::accumulate(
+      refinement_levels_by_block.begin(), refinement_levels_by_block.end(),
+      0_st, [](const size_t lhs, const std::array<size_t, Dim>& rhs) {
+        return lhs + number_of_elements_in_block(rhs);
+      });
+  for (const size_t element_count : elements_per_proc) {
+    CHECK((element_count == number_of_elements / number_of_procs or
+           element_count == number_of_elements / number_of_procs + 1));
+  }
+}
+
+// check that, for each block, no processor appears in more than two connected
+// groups, and that each processor does not appear in too many blocks.
+template <size_t Dim>
+void check_element_distribution_cohesion(
+    const std::vector<std::vector<size_t>>& proc_map,
+    const size_t number_of_procs,
+    const std::vector<std::array<size_t, Dim>>& refinement_levels_by_block,
+    const bool nonuniform_block = false) noexcept {
+  std::vector<std::set<size_t>> block_set_per_proc(number_of_procs);
+  for (size_t block = 0; block < proc_map.size(); ++block) {
+    std::array<size_t, Dim> strides{};
+    strides[0] = 1;
+    for (size_t i = 1; i < Dim; ++i) {
+      strides.at(i) = gsl::at(strides, i - 1) *
+                      two_to_the(gsl::at(
+                          gsl::at(refinement_levels_by_block, block), i - 1));
+    }
+    std::vector<size_t> number_of_clusters_per_proc(number_of_procs, 0_st);
+    std::vector<bool> seen(gsl::at(proc_map, block).size(), false);
+    for (size_t start_element = 0;
+         start_element < gsl::at(proc_map, block).size(); ++start_element) {
+      if (not seen.at(start_element)) {
+        const size_t current_proc =
+            gsl::at(gsl::at(proc_map, block), start_element);
+        block_set_per_proc.at(current_proc).insert(block);
+        ++number_of_clusters_per_proc.at(current_proc);
+        seen.at(start_element) = true;
+        // perform a bredth-first search to get all elements in the cluster that
+        // share a proc and mark them seen
+        std::deque<size_t> next_elements;
+        auto insert_adjacent_unseen = [&next_elements, &current_proc, &proc_map,
+                                       &refinement_levels_by_block, &strides,
+                                       &seen,
+                                       &block](const size_t index) noexcept {
+          for (size_t i = 0; i < Dim; ++i) {
+            const size_t index_in_dim =
+                (index / gsl::at(strides, i)) %
+                two_to_the(
+                    gsl::at(gsl::at(refinement_levels_by_block, block), i));
+            if (index_in_dim > 0 and
+                not seen.at(index - gsl::at(strides, i)) and
+                gsl::at(gsl::at(proc_map, block),
+                        index - gsl::at(strides, i)) == current_proc) {
+              next_elements.push_back(index - gsl::at(strides, i));
+              seen.at(index - gsl::at(strides, i)) = true;
+            }
+            if (index_in_dim + 1 <
+                    two_to_the(gsl::at(
+                        gsl::at(refinement_levels_by_block, block), i)) and
+                not seen.at(index + gsl::at(strides, i)) and
+                gsl::at(gsl::at(proc_map, block),
+                        index + gsl::at(strides, i)) == current_proc) {
+              next_elements.push_back(index + gsl::at(strides, i));
+              seen.at(index + gsl::at(strides, i)) = true;
+            }
+          }
+        };
+        insert_adjacent_unseen(start_element);
+        // note that this loop cannot be converted to an STL iterator algorithm
+        // because `insert_adjacent_unseen` violates iterator stability via the
+        // insertions.
+        while (not next_elements.empty()) {
+          const size_t front = next_elements.front();
+          next_elements.pop_front();
+          insert_adjacent_unseen(front);
+        }
+      }
+    }
+    // verify that the distribution is well-clustered -- the Z-curve should
+    // ensure no more than 2 clusters for each core
+    for (const size_t number_of_clusters : number_of_clusters_per_proc) {
+      CHECK(number_of_clusters < 3);
+    }
+  }
+  // verify that each processor has not been assigned to too many blocks -- the
+  // greedy algorithm will let extras 'overflow' into the next block, but that
+  // shouldn't give more than one extra block in the count.
+  // this check assumes that only one block is a different size to make the
+  // upper bound calculation simple -- the algorithm still works to keep the
+  // number of blocks in which a given processor participates low for more
+  // intricate cases, but the upper bound becomes more complicated to write out.
+  for (const auto& blocks : block_set_per_proc) {
+    CHECK(blocks.size() <=
+          static_cast<size_t>(
+              std::ceil(static_cast<double>(refinement_levels_by_block.size()) /
+                        number_of_procs) +
+              (nonuniform_block ? 2 : 1)));
+  }
+}
+
+template <size_t Dim>
+void test_single_block_domain() noexcept {
+  std::vector<std::array<size_t, Dim>> refinement_levels_by_block;
+  refinement_levels_by_block.emplace_back();
+  for (size_t i = 0; i < Dim; ++i) {
+    refinement_levels_by_block.at(0).at(i) = 3;
+  }
+
+  std::vector<std::vector<size_t>> proc_map =
+      make_proc_map_for_domain(1, two_to_the(Dim), refinement_levels_by_block);
+  // check that the domain is segmented into 4x4x4 cubes
+  std::set<size_t> procs_seen;
+  for (size_t cube_index = 0; cube_index < two_to_the(Dim); ++cube_index) {
+    std::array<SegmentId, Dim> reference_segment_id{};
+    size_t stride = 1;
+    for (size_t i = 0; i < Dim; ++i) {
+      reference_segment_id.at(i) =
+          SegmentId{gsl::at(gsl::at(refinement_levels_by_block, 0), i),
+                    (cube_index / stride) % 2 == 0_st ? 0_st : 4_st};
+      stride *= 2;
+    }
+    const size_t global_reference_id =
+        segment_id_to_element_index(reference_segment_id);
+    // check that the 2^Dim lower corners of the large cubes have unique procs
+    CHECK(procs_seen.count(
+              gsl::at(gsl::at(proc_map, 0), global_reference_id)) == 0);
+    procs_seen.insert(gsl::at(gsl::at(proc_map, 0), global_reference_id));
+    for (size_t element_within_cube = 0;
+         element_within_cube <
+         number_of_elements_in_block(gsl::at(refinement_levels_by_block, 0)) /
+             two_to_the(Dim);
+         ++element_within_cube) {
+      std::array<SegmentId, Dim> segment_id = reference_segment_id;
+      stride = 1;
+      for (size_t i = 0; i < Dim; ++i) {
+        segment_id.at(i) = SegmentId{gsl::at(segment_id, i).refinement_level(),
+                                     gsl::at(segment_id, i).index() +
+                                         (element_within_cube / stride) % 4};
+        stride *= 4_st;
+      }
+      // check that each element in the large cubes have the same processor as
+      // the corner elements.
+      size_t global_element_id = segment_id_to_element_index(segment_id);
+      CHECK(gsl::at(gsl::at(proc_map, 0), global_element_id) ==
+            gsl::at(gsl::at(proc_map, 0), global_reference_id));
+    }
+  }
+
+  // test a more scattered distribution because of the prime number of procs
+  proc_map = make_proc_map_for_domain(1, 5, refinement_levels_by_block);
+  check_element_distribution_uniformity(proc_map, 5,
+                                        refinement_levels_by_block);
+  check_element_distribution_cohesion(proc_map, 5, refinement_levels_by_block);
+}
+
+template <size_t Dim>
+void general_test(const size_t number_of_blocks, const size_t number_of_procs,
+                  const bool uneven_domain) noexcept {
+  std::vector<std::array<size_t, Dim>> refinement_levels_by_block;
+  for (size_t i = 0; i < number_of_blocks; ++i) {
+    refinement_levels_by_block.emplace_back();
+    for (size_t j = 0; j < Dim; ++j) {
+      refinement_levels_by_block.at(i).at(j) = uneven_domain ? j : 1;
+    }
+  }
+  if (uneven_domain) {
+    for (size_t j = 0; j < Dim; ++j) {
+      refinement_levels_by_block.at(0).at(j) = j % 2 == 0 ? 1 : 2;
+    }
+  }
+  const std::vector<std::vector<size_t>> proc_map = make_proc_map_for_domain(
+      number_of_blocks, number_of_procs, refinement_levels_by_block);
+  check_element_distribution_uniformity(proc_map, number_of_procs,
+                                        refinement_levels_by_block);
+  check_element_distribution_cohesion(
+      proc_map, number_of_procs, refinement_levels_by_block, uneven_domain);
+}
+
+SPECTRE_TEST_CASE("Unit.Domain.ElementDistribution", "[Domain][Unit]") {
+  {
+    INFO("Single block domain");
+    test_single_block_domain<1>();
+    test_single_block_domain<2>();
+    test_single_block_domain<3>();
+  }
+  for (const size_t number_of_blocks : {2_st, 6_st}) {
+    for (const size_t number_of_procs : {2_st, 7_st}) {
+      for (const bool uneven_domain : {true, false}) {
+        general_test<1>(number_of_blocks, number_of_procs, uneven_domain);
+        general_test<2>(number_of_blocks, number_of_procs, uneven_domain);
+        general_test<3>(number_of_blocks, number_of_procs, uneven_domain);
+      }
+    }
+  }
+}
+}  // namespace
