57b0cc9bdbe41bdb2e1a6524d98d837ff6c6706c
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-03-28 13:41:26 +0200

    Document the Xcts::AnalyticData namespace

---
+++ src/PointwiseFunctions/AnalyticData/Xcts/AnalyticData.hpp
@@ -0,0 +1,8 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+/// Analytic data for the \ref Xcts "XCTS" equations, i.e. field configurations
+/// that do not solve the equations but are used as background or initial guess.
+namespace Xcts::AnalyticData {}
