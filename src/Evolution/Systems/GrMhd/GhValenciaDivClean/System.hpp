b71f1236fd66e52e6976ddd0e85f0abec27c3288
Jordan Moxon <jem497@cornell.edu>
2021-04-06 14:05:54 -0700

    Add Tags for Valencia Stress-energy calculation

---
+++ src/Evolution/Systems/GrMhd/GhValenciaDivClean/System.hpp
@@ -0,0 +1,12 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace grmhd {
+
+/// Namespace associated with utilities for the combined Generalized Harmonic
+/// and Valencia formulation of ideal GRMHD with divergence cleaning systems.
+namespace GhValenciaDivClean {
+}  // namespace GhValenciaDivClean
+}  // namespace grmhd
