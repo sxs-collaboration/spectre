5f2ecc738b7961297ef31dc47b8e64fa8d4a1b03
Francois Hebert <fmh33@cornell.edu>
2021-04-09 20:43:33 -0700

    Add Parallel::local wrapper for ckLocal

---
+++ src/Parallel/Local.hpp
@@ -0,0 +1,28 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <charm++.h>
+
+#include "Parallel/TypeTraits.hpp"
+
+namespace Parallel {
+
+/// Wrapper for calling Charm++'s `.ckLocal()` on a proxy
+///
+/// The Proxy must be to a Charm++ singleton chare or to a Charm++ array element
+/// chare (i.e., the proxy obtained by indexing into a Charm++ array chare).
+///
+/// The function returns a pointer to the chare if it exists on the local
+/// processor, and NULL if it does not. See the Charm++ documentation.
+/// It is the responsibility of the user to check the result pointer is valid.
+template <typename Proxy>
+auto* local(Proxy&& proxy) noexcept {
+  // It only makes sense to call .ckLocal() on some kinds of proxies
+  static_assert(is_chare_proxy<std::decay_t<Proxy>>::value or
+                is_array_element_proxy<std::decay_t<Proxy>>::value);
+  return proxy.ckLocal();
+}
+
+}  // namespace Parallel
