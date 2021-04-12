9e44f7ab58d56da93dc050826aa139efb173e7ed
Lawrence Kidder <kidder@astro.cornell.edu>
2021-04-11 21:59:27 -0400

    Add tags for AMR flags of an Element and its neighbors

---
+++ src/Domain/Amr/Tags/Tags.hpp
@@ -0,0 +1,10 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+/// \ingroup AmrGroup
+namespace amr::domain {
+/// \brief %Tags for adaptive mesh refinement
+namespace Tags {}
+}  // namespace amr::domain
---
+++ tests/Unit/Domain/Amr/Test_Tags.cpp
@@ -0,0 +1,23 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include "Domain/Amr/Tags/Flags.hpp"
+#include "Domain/Amr/Tags/NeighborFlags.hpp"
+#include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
+
+namespace {
+template <size_t Dim>
+void test() {
+  TestHelpers::db::test_simple_tag<amr::domain::Tags::Flags<Dim>>("Flags");
+  TestHelpers::db::test_simple_tag<amr::domain::Tags::NeighborFlags<Dim>>(
+      "NeighborFlags");
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.Domain.Amr.Tags", "[Domain][Unit]") {
+  test<1>();
+  test<2>();
+  test<3>();
+}
