1e35f4e3ccac7fcc4346f465aa4086b09cedc4bf
Lawrence Kidder <kidder@astro.cornell.edu>
2021-04-12 01:38:29 -0400

    Add Mutator to initialize AMR flags for an Element and its neighbors.

---
+++ src/ParallelAlgorithms/Amr/Actions/Actions.hpp
@@ -0,0 +1,10 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+/// \ingroup AmrGroup
+namespace amr {
+/// \brief %Actions for adaptive mesh refinement
+namespace Actions {}
+}  // namespace amr
---
+++ src/ParallelAlgorithms/Amr/Actions/Initialization.hpp
@@ -0,0 +1,9 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace amr {
+/// \brief %Mutators used for initialization of adaptive mesh refinement
+namespace Initialization {}
+}  // namespace amr
