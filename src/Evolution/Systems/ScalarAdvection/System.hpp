af8efaaea26b21611bc3df1a08de72c6cfc506ef
Yoonsoo Kim <ykim7@caltech.edu>
2021-04-26 17:32:41 +0900

    Add fluxes to ScalarAdvection system

--- src/Evolution/Systems/ScalarAdvection/System.hpp
+++ src/Evolution/Systems/ScalarAdvection/System.hpp
@@ -5,6 +5,11 @@
 
 #include <cstddef>
 
+#include "DataStructures/VariablesTag.hpp"
+#include "Evolution/Systems/ScalarAdvection/Fluxes.hpp"
+#include "Evolution/Systems/ScalarAdvection/Tags.hpp"
+#include "Utilities/TMPL.hpp"
+
 /*!
  * \ingroup EvolutionSystemsGroup
  * \brief Items related to evolving the scalar advection equation.
@@ -18,5 +23,16 @@
  */
 namespace ScalarAdvection {
 template <size_t Dim>
-struct System {};
+struct System {
+  static constexpr bool is_in_flux_conservative_form = true;
+  static constexpr bool has_primitive_and_conservative_vars = false;
+  static constexpr size_t volume_dim = Dim;
+
+  using variables_tag = ::Tags::Variables<tmpl::list<Tags::U>>;
+  using flux_variables = tmpl::list<Tags::U>;
+  using gradient_variables = tmpl::list<>;
+  using sourced_variables = tmpl::list<>;
+
+  using volume_fluxes = Fluxes<Dim>;
+};
 }  // namespace ScalarAdvection
