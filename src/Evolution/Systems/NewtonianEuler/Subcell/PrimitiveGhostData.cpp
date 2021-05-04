e2ec12dfe63cb8741853c2905146a852dee72792
Nils Deppe <nd357@cornell.edu>
2021-05-04 15:08:04 -0700

    Add PrimitiveGhostDataToSlice mutator for NewtonianEuler

--- src/Evolution/Systems/NewtonianEuler/Subcell/PrimitiveGhostData.cpp
+++ src/Evolution/Systems/NewtonianEuler/Subcell/PrimitiveGhostData.cpp
@@ -7,7 +7,9 @@
 
 #include "DataStructures/Variables.hpp"
 #include "DataStructures/VariablesTag.hpp"
+#include "Evolution/DgSubcell/Projection.hpp"
 #include "Evolution/Systems/NewtonianEuler/Tags.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "Utilities/GenerateInstantiations.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -26,9 +28,29 @@ auto PrimitiveGhostDataOnSubcells<Dim>::apply(
   return prims_for_reconstruction;
 }
 
+template <size_t Dim>
+auto PrimitiveGhostDataToSlice<Dim>::apply(
+    const Variables<prim_tags>& prims, const Mesh<Dim>& dg_mesh,
+    const Mesh<Dim>& subcell_mesh) noexcept
+    -> Variables<prims_to_reconstruct_tags> {
+  // We send the projected prims. There are truncation level errors
+  // introduced here, but let's try it!
+  Variables<prims_to_reconstruct_tags> prims_for_reconstruction{
+      prims.number_of_grid_points()};
+  tmpl::for_each<prims_to_reconstruct_tags>(
+      [&prims, &prims_for_reconstruction](auto tag_v) noexcept {
+        using tag = tmpl::type_from<decltype(tag_v)>;
+        get<tag>(prims_for_reconstruction) = get<tag>(prims);
+      });
+
+  return evolution::dg::subcell::fd::project(prims_for_reconstruction, dg_mesh,
+                                             subcell_mesh.extents());
+}
+
 #define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
-#define INSTANTIATION(r, data) \
-  template class PrimitiveGhostDataOnSubcells<DIM(data)>;
+#define INSTANTIATION(r, data)                            \
+  template class PrimitiveGhostDataOnSubcells<DIM(data)>; \
+  template class PrimitiveGhostDataToSlice<DIM(data)>;
 GENERATE_INSTANTIATIONS(INSTANTIATION, (1, 2, 3))
 #undef INSTANTIATION
 #undef DIM
--- tests/Unit/Evolution/Systems/NewtonianEuler/Subcell/Test_PrimitiveGhostData.cpp
+++ tests/Unit/Evolution/Systems/NewtonianEuler/Subcell/Test_PrimitiveGhostData.cpp
@@ -11,6 +11,10 @@
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "DataStructures/Variables.hpp"
 #include "DataStructures/VariablesTag.hpp"
+#include "Domain/Tags.hpp"
+#include "Evolution/DgSubcell/Mesh.hpp"
+#include "Evolution/DgSubcell/Projection.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
 #include "Evolution/Systems/NewtonianEuler/Subcell/PrimitiveGhostData.hpp"
 #include "Evolution/Systems/NewtonianEuler/Tags.hpp"
 #include "Framework/TestHelpers.hpp"
@@ -51,6 +55,46 @@ void test_subcells(
                               get<tag>(expected_prims));
       });
 }
+
+template <size_t Dim>
+void test_dg(const gsl::not_null<std::mt19937*> gen,
+             const gsl::not_null<std::uniform_real_distribution<>*> dist) {
+  using MassDensity = NewtonianEuler::Tags::MassDensity<DataVector>;
+  using Velocity = NewtonianEuler::Tags::Velocity<DataVector, Dim>;
+  using SpecificInternalEnergy =
+      NewtonianEuler::Tags::SpecificInternalEnergy<DataVector>;
+  using Pressure = NewtonianEuler::Tags::Pressure<DataVector>;
+  using prim_tags =
+      tmpl::list<MassDensity, Velocity, SpecificInternalEnergy, Pressure>;
+  using PrimVars = Variables<prim_tags>;
+  using prims_to_reconstruct_tags = tmpl::list<MassDensity, Velocity, Pressure>;
+  using ReconsPrimVars = Variables<prims_to_reconstruct_tags>;
+
+  const Mesh<Dim> dg_mesh{5, Spectral::Basis::Legendre,
+                          Spectral::Quadrature::GaussLobatto};
+  const Mesh<Dim> subcell_mesh = evolution::dg::subcell::fd::mesh(dg_mesh);
+
+  // NOLINTNEXTLINE(modernize-use-auto)
+  const PrimVars prims = make_with_random_values<PrimVars>(
+      gen, dist, dg_mesh.number_of_grid_points());
+
+  auto box = db::create<
+      db::AddSimpleTags<::Tags::Variables<prim_tags>, domain::Tags::Mesh<Dim>,
+                        evolution::dg::subcell::Tags::Mesh<Dim>>>(
+      prims, dg_mesh, subcell_mesh);
+  const ReconsPrimVars prims_to_reconstruct =
+      db::mutate_apply<NewtonianEuler::subcell::PrimitiveGhostDataToSlice<Dim>>(
+          make_not_null(&box));
+
+  const PrimVars expected_prims = evolution::dg::subcell::fd::project(
+      prims, dg_mesh, subcell_mesh.extents());
+  tmpl::for_each<prims_to_reconstruct_tags>(
+      [&prims_to_reconstruct, &expected_prims](auto tag_v) {
+        using tag = tmpl::type_from<decltype(tag_v)>;
+        CHECK_ITERABLE_APPROX(get<tag>(prims_to_reconstruct),
+                              get<tag>(expected_prims));
+      });
+}
 }  // namespace
 
 SPECTRE_TEST_CASE(
@@ -61,4 +105,8 @@ SPECTRE_TEST_CASE(
   test_subcells<1>(make_not_null(&gen), make_not_null(&dist));
   test_subcells<2>(make_not_null(&gen), make_not_null(&dist));
   test_subcells<3>(make_not_null(&gen), make_not_null(&dist));
+
+  test_dg<1>(make_not_null(&gen), make_not_null(&dist));
+  test_dg<2>(make_not_null(&gen), make_not_null(&dist));
+  test_dg<3>(make_not_null(&gen), make_not_null(&dist));
 }
