847ca4efc93e511273bed24585eca440710d1afc
Mark Scheel <scheel@tapir.caltech.edu>
2021-05-05 14:00:47 -0700

    Add ErrorOnFailedApparentHorizon.

---
+++ src/NumericalAlgorithms/Interpolation/Callbacks/ErrorOnFailedApparentHorizon.hpp
@@ -0,0 +1,37 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "ApparentHorizons/FastFlow.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
+#include "Utilities/PrettyType.hpp"
+
+/// \cond
+namespace db {
+template <typename DbTags>
+class DataBox;
+}  // namespace db
+namespace Parallel {
+template <typename Metavariables>
+class GlobalCache;
+}  // namespace Parallel
+/// \endcond
+
+namespace intrp::callbacks {
+
+/// \brief Callback for a failed apparent horizon find that simply errors.
+struct ErrorOnFailedApparentHorizon {
+  template <typename InterpolationTargetTag, typename DbTags,
+            typename Metavariables, typename TemporalId>
+  static void apply(const db::DataBox<DbTags>& /*box*/,
+                    const Parallel::GlobalCache<Metavariables>& /*cache*/,
+                    const TemporalId& /*temporal_id*/,
+                    const FastFlow::Status failure_reason) noexcept {
+    ERROR("Apparent horizon finder "
+          << pretty_type::short_name<InterpolationTargetTag>()
+          << " failed, reason = " << failure_reason);
+  }
+};
+
+}  // namespace intrp::callbacks
