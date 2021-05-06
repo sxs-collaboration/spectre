aade3d26e8037819de6418f129f917fbee926ac3
Nils Deppe <nd357@cornell.edu>
2021-05-06 15:41:33 -0700

    Add FiniteDifference directory to ValenciaDivClean

---
+++ src/Evolution/Systems/GrMhd/ValenciaDivClean/FiniteDifference/FiniteDifference.hpp
@@ -0,0 +1,10 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace grmhd::ValenciaDivClean {
+/// Finite difference functionality for the ValenciaDivClean form of the GRMHD
+/// equations
+namespace fd {}
+}  // namespace grmhd::ValenciaDivClean
