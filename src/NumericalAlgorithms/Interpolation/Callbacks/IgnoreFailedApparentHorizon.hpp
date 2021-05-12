aca91a0dc975a7f374d879d781c9d9040b8df944
Mark Scheel <scheel@tapir.caltech.edu>
2021-05-12 15:00:22 -0700

    Add IgnoreFailedApparentHorizon.

---
+++ src/NumericalAlgorithms/Interpolation/Callbacks/IgnoreFailedApparentHorizon.hpp
@@ -0,0 +1,41 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "ApparentHorizons/FastFlow.hpp"
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Informer/Tags.hpp"
+#include "Parallel/Printf.hpp"
+#include "Utilities/PrettyType.hpp"
+
+/// \cond
+namespace Parallel {
+template <typename Metavariables>
+class GlobalCache;
+}  // namespace Parallel
+/// \endcond
+
+namespace intrp::callbacks {
+
+/// \brief Callback for a failed apparent horizon find that prints a
+/// message (if sufficient Verbosity is enabled) but does not
+/// terminate the executable.
+struct IgnoreFailedApparentHorizon {
+  template <typename InterpolationTargetTag, typename DbTags,
+            typename Metavariables, typename TemporalId>
+  static void apply(const db::DataBox<DbTags>& box,
+                    const Parallel::GlobalCache<Metavariables>& /*cache*/,
+                    const TemporalId& /*temporal_id*/,
+                    const FastFlow::Status failure_reason) noexcept {
+    const auto& verbosity =
+        db::get<logging::Tags::Verbosity<InterpolationTargetTag>>(box);
+    if (verbosity > ::Verbosity::Quiet) {
+      Parallel::printf("Remark: Horizon finder %s failed, reason = %s\n",
+                       pretty_type::short_name<InterpolationTargetTag>(),
+                       failure_reason);
+    }
+  }
+};
+
+}  // namespace intrp::callbacks
