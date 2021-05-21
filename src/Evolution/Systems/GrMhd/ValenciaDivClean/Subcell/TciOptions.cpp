cb375f0b8f2969e47a90e83885611849a1c3343f
Nils Deppe <nd357@cornell.edu>
2021-05-20 17:24:01 -0700

    Add ValenciaDivClean TCI options

---
+++ src/Evolution/Systems/GrMhd/ValenciaDivClean/Subcell/TciOptions.cpp
@@ -0,0 +1,14 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Evolution/Systems/GrMhd/ValenciaDivClean/Subcell/TciOptions.hpp"
+
+#include <pup.h>
+
+namespace grmhd::ValenciaDivClean::subcell {
+void TciOptions::pup(PUP::er& p) noexcept {
+  p | minimum_rest_mass_density_times_lorentz_factor;
+  p | atmosphere_density;
+  p | safety_factor_for_magnetic_field;
+}
+}  // namespace grmhd::ValenciaDivClean::subcell
