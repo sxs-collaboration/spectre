9f69f5f90f1c90fff769c2479525d0fecfbe888f
MarloMo <moralesmarlo486@gmail.com>
2021-06-17 20:20:50 -0700

    Add the IrreducibleMassCompute tag to EGHWH.hpp

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonicWithHorizon.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonicWithHorizon.hpp
@@ -59,7 +59,8 @@ struct EvolutionMetavars
 
   struct AhA {
     using tags_to_observe =
-        tmpl::list<StrahlkorperGr::Tags::AreaCompute<frame>>;
+        tmpl::list<StrahlkorperGr::Tags::AreaCompute<frame>,
+                   StrahlkorperGr::Tags::IrreducibleMassCompute<frame>>;
     using compute_items_on_source = tmpl::list<
         gr::Tags::SpatialMetricCompute<volume_dim, frame, DataVector>,
         ah::Tags::InverseSpatialMetricCompute<volume_dim, frame>,
