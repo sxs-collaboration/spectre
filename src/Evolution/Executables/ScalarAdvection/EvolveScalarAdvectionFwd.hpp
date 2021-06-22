0b9ae6405f55bf8b3114ec69649e270551404af1
Yoonsoo Kim <ykim7@caltech.edu>
2021-06-22 17:43:03 +0900

    Add ScalarAdvection 1D sinusoid executable

---
+++ src/Evolution/Executables/ScalarAdvection/EvolveScalarAdvectionFwd.hpp
@@ -0,0 +1,15 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+namespace ScalarAdvection {
+namespace Solutions {
+class Sinusoid;
+}  // namespace Solutions
+}  // namespace ScalarAdvection
+
+template <size_t Dim, typename InitialData>
+struct EvolutionMetavars;
