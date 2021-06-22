3a75bc8b7008968f88ac6300ca1fb16f0651dd4e
Jordan Moxon <jem497@cornell.edu>
2021-06-22 11:36:19 -0700

    Clarify DataBox error for cases with incorrect member functions

--- src/DataStructures/DataBox/DataBox.hpp
+++ src/DataStructures/DataBox/DataBox.hpp
@@ -1040,13 +1040,19 @@ CREATE_IS_CALLABLE_V(apply)
 
 template <typename Func, typename... Args>
 constexpr void error_function_not_callable() noexcept {
-  static_assert(std::is_same_v<Func, void>,
-                "The function is not callable with the expected arguments.  "
-                "See the first template parameter of "
-                "error_function_not_callable for the function type and "
-                "the remaining arguments for the parameters that cannot be "
-                "passed. If all the argument types match, it could be that you "
-                "have a template parameter that cannot be deduced.");
+  static_assert(
+      std::is_same_v<Func, void>,
+      "The function is not callable with the expected arguments.  "
+      "See the first template parameter of "
+      "error_function_not_callable for the function or object type and "
+      "the remaining arguments for the parameters that cannot be "
+      "passed. If all the argument types match, it could be that you "
+      "have a template parameter that cannot be deduced."
+      "Note that for most DataBox functions, you must pass either "
+      "a function pointer, a lambda, or a class with a call operator "
+      "or static apply function, and this error will also arise if "
+      "the provided entity does not satisfy that requirement (e.g. "
+      "if the provided class defines a function with the incorrect name).");
 }
 
 template <typename DataBoxTags, typename... TagsToRetrieve>
