190ad989e218dc0bf03a5f9fa3c7fb253924ca99
William Throwe <wtt6@cornell.edu>
2021-07-09 16:05:38 -0400

    Restore DataBox state after processing EaDT
    The current action lists overwrite the changed variables immediately
    after this action is called, but leaving the state unchanged makes
    modifying the lists less fragile.

--- src/Evolution/Actions/RunEventsAndDenseTriggers.hpp
+++ src/Evolution/Actions/RunEventsAndDenseTriggers.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <cstddef>
+#include <optional>
 #include <tuple>
 
 #include "DataStructures/DataBox/DataBox.hpp"
@@ -41,6 +42,47 @@ namespace evolution::Actions {
 /// - Modifies: nothing
 template <typename PrimFromCon = void>
 struct RunEventsAndDenseTriggers {
+ private:
+  // RAII object to restore the time and variables changed by dense
+  // output.
+  template <typename DbTags, typename Tag>
+  class StateRestorer {
+   public:
+    StateRestorer(const gsl::not_null<db::DataBox<DbTags>*> box) noexcept
+        : box_(box) {}
+
+    void save() noexcept {
+      // Only store the value the first time, because after that we
+      // are seeing the value after the previous change instead of the
+      // original.
+      if (not value_.has_value()) {
+        value_ = db::get<Tag>(*box_);
+      }
+    }
+
+    ~StateRestorer() {
+      if (value_.has_value()) {
+        db::mutate<Tag>(
+            box_,
+            [this](const gsl::not_null<typename Tag::type*> value) noexcept {
+#if defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 11
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif  // defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 11
+              *value = *value_;
+#if defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 11
+#pragma GCC diagnostic pop
+#endif  // defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 11
+            });
+      }
+    }
+
+   private:
+    gsl::not_null<db::DataBox<DbTags>*> box_ = nullptr;
+    std::optional<typename Tag::type> value_{};
+  };
+
+ public:
   template <typename DbTags, typename... InboxTags, typename Metavariables,
             typename ArrayIndex, typename ActionList,
             typename ParallelComponent>
@@ -50,6 +92,9 @@ struct RunEventsAndDenseTriggers {
       Parallel::GlobalCache<Metavariables>& cache,
       const ArrayIndex& array_index, const ActionList /*meta*/,
       const ParallelComponent* const component) noexcept {
+    using system = typename Metavariables::system;
+    using variables_tag = typename system::variables_tag;
+
     const auto& time_step_id = db::get<::Tags::TimeStepId>(box);
     if (time_step_id.slab_number() < 0) {
       // Skip dense output during self-start
@@ -64,6 +109,19 @@ struct RunEventsAndDenseTriggers {
         time_step_id.step_time() + db::get<::Tags::TimeStep>(box);
     const evolution_less<double> before{time_step_id.time_runs_forward()};
 
+    StateRestorer<DbTags, ::Tags::Time> time_restorer(make_not_null(&box));
+    StateRestorer<DbTags, variables_tag> variables_restorer(
+        make_not_null(&box));
+    auto primitives_restorer = [&box]() noexcept {
+      if constexpr (system::has_primitive_and_conservative_vars) {
+        return StateRestorer<DbTags, typename system::primitive_variables_tag>(
+            make_not_null(&box));
+      } else {
+        (void)box;
+        return 0;
+      }
+    }();
+    (void)primitives_restorer;
     for (;;) {
       const double next_trigger = events_and_dense_triggers.next_trigger(box);
       if (before(step_end.value(), next_trigger)) {
@@ -71,6 +129,7 @@ struct RunEventsAndDenseTriggers {
       }
 
       if (db::get<::Tags::Time>(box) != next_trigger) {
+        time_restorer.save();
         db::mutate<::Tags::Time>(
             make_not_null(&box),
             [&next_trigger](const gsl::not_null<double*> time) noexcept {
@@ -86,9 +145,9 @@ struct RunEventsAndDenseTriggers {
           return {std::move(box), Parallel::AlgorithmExecution::Retry};
         case TriggeringState::NeedsEvolvedVariables:
           {
-            using variables_tag = typename Metavariables::system::variables_tag;
             using history_tag = ::Tags::HistoryEvolvedVariables<variables_tag>;
             bool dense_output_succeeded = false;
+            variables_restorer.save();
             db::mutate<variables_tag>(
                 make_not_null(&box),
                 [&dense_output_succeeded, &next_trigger](
@@ -110,12 +169,11 @@ struct RunEventsAndDenseTriggers {
               return {std::move(box), Parallel::AlgorithmExecution::Continue};
             }
 
-            static_assert(
-                Metavariables::system::has_primitive_and_conservative_vars !=
-                    std::is_same_v<PrimFromCon, void>,
-                "Primitive update scheme not provided.");
-            if constexpr (
-                Metavariables::system::has_primitive_and_conservative_vars) {
+            static_assert(system::has_primitive_and_conservative_vars !=
+                              std::is_same_v<PrimFromCon, void>,
+                          "Primitive update scheme not provided.");
+            if constexpr (system::has_primitive_and_conservative_vars) {
+              primitives_restorer.save();
               db::mutate_apply<PrimFromCon>(make_not_null(&box));
             }
           }
