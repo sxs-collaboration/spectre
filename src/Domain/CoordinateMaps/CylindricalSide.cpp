b38046f27acff8ce55436bec64e4cd7d349b1adb
Mark Scheel <scheel@tapir.caltech.edu>
2021-07-26 13:54:04 -0700

    CylindricalSide: Change allowed bounds of parameters.
    This was done because the random-number tests that chose
    parameters were sometimes choosing parameters that failed
    by roundoff.
    
    Now this passes with --repeat-until-fail 100000
    
    The particular changes:
     - FocallyLiftedMapHelpers::try_scale_factor now has a choice when
       setting up the quadratic equation to solve for lambda.  It can
       choose to solve for lambda-1 (the default, and the previous
       behavior), or it can solve directly for lambda. The difference is
       only roundoff but makes a difference in roundoff tests.
     - Accordingly, FocallyLiftedSide, when solving for lambda_tide, now
       sets up the quadratic to solve for lambda_tilde instead of
       lambda_tilde-1.
     - We separated out a particular use case: projection point exactly on
       one of the z-planes when sphere_two > sphere_one. This use case is
       now tested tested separately, and has different allowed parameters.
       Changes in the allowed parameters for the (now 3) different cases:
     - For sphere_two > sphere_one and projection point on the z-plane:
        - center_one must have the same x and y coordinates as center_two.
        - Rho must be zero (to roundoff)
        - The z-planes must be farther from the edge of sphere_one.
        - Radius_two is allowed to be larger, and radius_one cannot be
          too small.
     - For sphere_two > sphere_one and projection point not on the z-plane:
        - rho must be smaller than previously allowed.
        - radius_one cannot be too small and radius_two cannot be too large.
        - proj_center must be sufficiently far from the z-planes.
     - For sphere_two < sphere_one
        - Keep projection_point far enough from surface of sphere_two
        - Keep sphere_one and sphere_two sufficiently far apart.
        - Keep the z-planes sufficiently far from the centers or edges of
          sphere_one.

--- src/Domain/CoordinateMaps/CylindricalSide.cpp
+++ src/Domain/CoordinateMaps/CylindricalSide.cpp
@@ -54,8 +54,8 @@ CylindricalSide::CylindricalSide(const std::array<double, 3>& center_one,
   //    avoid such cases.
   // b) We do not want to waste effort testing the map for parameters
   //    that we don't expect to be used.  For example, we demand
-  //    here that proj_center and sphere_one are contained within
-  //    sphere_two, but the map should still be valid for some choices
+  //    here that sphere_one is contained within sphere_two, or vice versa,
+  //    but the map should still be valid for some choices
   //    of parameters where sphere_one and sphere_two are disjoint;
   //    allowing those parameter choices would involve much more
   //    complicated logic to determine whether the map produces shapes
@@ -73,38 +73,124 @@ CylindricalSide::CylindricalSide(const std::array<double, 3>& center_one,
   const double dist_proj_one = sqrt(square(center_one[0] - proj_center[0]) +
                                     square(center_one[1] - proj_center[1]) +
                                     square(center_one[2] - proj_center[2]));
-  ASSERT(dist_proj_one < radius_one,
-         "CylindricalSide: The map has been tested only for the case when "
-         "proj_center is contained inside sphere_one");
-
   const double dist_proj_two = sqrt(square(center_two[0] - proj_center[0]) +
                                     square(center_two[1] - proj_center[1]) +
                                     square(center_two[2] - proj_center[2]));
-  ASSERT(dist_proj_two < radius_two,
+
+  ASSERT(dist_proj_one < radius_one,
          "CylindricalSide: The map has been tested only for the case when "
-         "proj_center is contained inside sphere_two. dist_proj_two="
-         << dist_proj_two << ", radius_two=" << radius_two);
+         "proj_center is contained inside sphere_one");
 
   ASSERT(proj_center[2] >= z_lower and proj_center[2] <= z_upper,
          "CylindricalSide: The map has been tested only for the case when "
          "proj_center is contained inside z_lower and z_upper: "
              << z_lower << " " << proj_center[2] << " " << z_upper);
 
-  ASSERT(center_one[2] - z_lower <= 0.92 * radius_one and
-             z_upper - center_one[2] <= 0.92 * radius_one,
-         "CylindricalSide: The map has been tested only when z_lower and "
-         "z_upper are sufficently far from the edge of sphere_one");
+  // We have three different usages for this map, and each usage
+  // is tested for slightly different ranges of parameters.
+  // - sphere_two is contained in sphere_one
+  // - sphere_one is contained in sphere_two, and projection_point is on/near
+  //   the center of one of the z planes.
+  // - sphere_one is contained in sphere_two, and projection_point is away
+  //   from the z planes.
 
-  ASSERT(center_one[2] - z_lower >= 0.01 * radius_one and
-             z_upper - center_one[2] >= 0.01 * radius_one,
-         "CylindricalSide: The map has been tested only when z_lower and "
-         "z_upper are sufficently far from the center of sphere_one");
-
-  if (dist_spheres + radius_two < radius_one) {
+  if (dist_spheres + radius_one < radius_two) {
+    // Sphere_one is contained in sphere_two
+    ASSERT(dist_proj_two < radius_two,
+           "CylindricalSide: The map has been tested only for the case when "
+           "proj_center is contained inside sphere_two. dist_proj_two="
+               << dist_proj_two << ", radius_two=" << radius_two);
+    ASSERT(center_one[2] - z_lower <= 0.9 * radius_one and
+               z_upper - center_one[2] <= 0.9 * radius_one,
+           "CylindricalSide: The map has been tested only when z_lower and "
+           "z_upper are sufficently far from the edge of sphere_one");
+    ASSERT(center_one[2] - z_lower >= 0.02 * radius_one and
+               z_upper - center_one[2] >= 0.02 * radius_one,
+           "CylindricalSide: The map has been tested only when z_lower and "
+           "z_upper are sufficently far from the center of sphere_one");
+    // rho_sphere_one is the radius of the circle formed by the intersection
+    // of sphere_one and a plane at coordinate proj_center[2].
+    const double rho_sphere_one =
+        radius_one *
+        sqrt(1.0 - square((proj_center[2] - center_one[2]) / radius_one));
+    const double rho = sqrt(square(center_one[0] - proj_center[0]) +
+                            square(center_one[1] - proj_center[1]));
+    // Here "projection point away from the z planes" means that
+    // proj_center[2] is between z_min_close and z_max_close.
+    const double z_min_close = z_lower + 1e-15 * (z_upper - z_lower);
+    const double z_max_close = z_upper - 1e-15 * (z_upper - z_lower);
+    if (proj_center[2] <= z_min_close or proj_center[2] >= z_max_close) {
+      // Projection point is near the center of one of the z planes.
+      ASSERT(center_one[0] == center_two[0] and center_one[1] == center_two[1],
+             "When projection point is at a z plane, the two spheres must be "
+             "centered in x and y");
+      ASSERT(center_one[2] - z_lower <= 0.75 * radius_one and
+                 z_upper - center_one[2] <= 0.75 * radius_one,
+             "CylindricalSide: The map has been tested only when z_lower and "
+             "z_upper are sufficently far from the edge of sphere_one");
+      ASSERT(radius_one >= dist_spheres+0.1 and
+                 radius_two <= 4.01 * (radius_one + dist_spheres),
+             "The map has not been tested for either radius_one this small or "
+             "for radius_two this large. radius_one = "
+                 << radius_one << ", radius_two = " << radius_two
+                 << ", dist_spheres = " << dist_spheres);
+      ASSERT(rho <= 1.e-14,
+             "The map has been tested only for the case when the projection "
+             "point is not too close to sphere_one. rho = "
+                 << rho << "\nz_lower = " << z_lower
+                 << "\nz_upper = " << z_upper << "\nradius_one = " << radius_one
+                 << "\nradius_two = " << radius_two << "\nproj_center = ("
+                 << proj_center[0] << "," << proj_center[1] << ","
+                 << proj_center[2] << ")\ncenter_one = (" << center_one[0]
+                 << "," << center_one[1] << "," << center_one[2]
+                 << ")\ncenter_two = (" << center_two[0] << "," << center_two[1]
+                 << "," << center_two[2] << ")\n");
+    } else {
+      // Projection point is not near the center of one of the z planes.
+      ASSERT(
+          proj_center[2] - z_lower >= 0.1 * (z_upper - z_lower) and
+              z_upper - proj_center[2] >= 0.1 * (z_upper - z_lower),
+          "CylindricalSide: The map has been tested only for the case when "
+          "proj_center is sufficiently contained inside z_lower and z_upper: "
+              << z_lower << " " << proj_center[2] << " " << z_upper);
+      ASSERT(radius_one >= 0.3 * dist_spheres and
+                 radius_two <= 2.01 * (radius_one + dist_spheres),
+             "The map has not been tested for either radius_one this small or "
+             "for radius_two this large. radius_one = "
+                 << radius_one << ", radius_two = " << radius_two
+                 << ", dist_spheres = " << dist_spheres);
+      ASSERT(rho <= rho_sphere_one * 0.85,
+             "The map has been tested only for the case when the projection "
+             "point is not too close to sphere_one. rho = "
+                 << rho << ", rho_sphere_one = " << rho_sphere_one
+                 << "\nz_lower = " << z_lower << "\nz_upper = " << z_upper
+                 << "\nradius_one = " << radius_one << "\nradius_two = "
+                 << radius_two << "\nproj_center = (" << proj_center[0] << ","
+                 << proj_center[1] << "," << proj_center[2]
+                 << ")\ncenter_one = (" << center_one[0] << "," << center_one[1]
+                 << "," << center_one[2] << ")\ncenter_two = (" << center_two[0]
+                 << "," << center_two[1] << "," << center_two[2] << ")\n");
+    }
+  } else {
+    // Sphere_two is contained in sphere_one
+    ASSERT(dist_proj_two < 0.9 * radius_two,
+           "CylindricalSide: The map has been tested only for the case when "
+           "proj_center is contained inside sphere_two. dist_proj_two="
+               << dist_proj_two << ", radius_two=" << radius_two);
+    ASSERT(center_one[2] - z_lower <= 0.92 * radius_one and
+               z_upper - center_one[2] <= 0.92 * radius_one,
+           "CylindricalSide: The map has been tested only when z_lower and "
+           "z_upper are sufficently far from the edge of sphere_one");
     ASSERT(center_one[2] - z_lower >= 0.2 * radius_one and
                z_upper - center_one[2] >= 0.2 * radius_one,
            "CylindricalSide: The map has been tested only when z_lower and "
            "z_upper are sufficently far from the center of sphere_one");
+    const double rho = sqrt(square(center_one[0] - center_two[0]) +
+                            square(center_one[1] - center_two[1]));
+    const double rho_max = 0.85 * sqrt(square(radius_one - radius_two) -
+                                      square(center_one[2] - center_two[2]));
+    ASSERT(rho <= rho_max,
+           "sphere_one and sphere_two are too close to each other");
   }
 #endif
 }
