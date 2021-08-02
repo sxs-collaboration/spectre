f2725648bdd19cd76ece54674e1af8e59ed2a17c
Nils Deppe <nd357@cornell.edu>
2021-08-02 13:03:20 -0700

    Do not switch back to DG if solution would be unphysical
    The TciOnFdGrid did not check that the reconstructed DG solution was physical so
    it could have negative density or energy. For high-resolution TOV star
    simulations this was actually the case.

--- src/Evolution/Systems/GrMhd/ValenciaDivClean/Subcell/TciOnFdGrid.cpp
+++ src/Evolution/Systems/GrMhd/ValenciaDivClean/Subcell/TciOnFdGrid.cpp
@@ -14,8 +14,12 @@ namespace grmhd::ValenciaDivClean::subcell {
 bool TciOnFdGrid::apply(const Scalar<DataVector>& tilde_d,
                         const Scalar<DataVector>& tilde_tau,
                         const bool vars_needed_fixing, const Mesh<3>& dg_mesh,
+                        const TciOptions& tci_options,
                         const double persson_exponent) noexcept {
   return vars_needed_fixing or
+         min(get(tilde_d)) <
+             tci_options.minimum_rest_mass_density_times_lorentz_factor or
+         min(get(tilde_tau)) < tci_options.minimum_tilde_tau or
          evolution::dg::subcell::persson_tci(tilde_d, dg_mesh, persson_exponent,
                                              1.0e-18) or
          evolution::dg::subcell::persson_tci(tilde_tau, dg_mesh,
