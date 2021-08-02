eac67202754b0551fd9992fdf8cd9279343a7855
Francois Hebert <fmh33@cornell.edu>
2021-08-01 17:46:49 -0700

    Move GRMHD tag tests into tag test file

--- tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/Test_Tags.cpp
+++ tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/Test_Tags.cpp
@@ -5,11 +5,30 @@
 
 #include <string>
 
+#include "DataStructures/Tensor/IndexType.hpp"  // IWYU pragma: keep
 #include "Evolution/Systems/GrMhd/ValenciaDivClean/Tags.hpp"
 #include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
 
 SPECTRE_TEST_CASE("Unit.Evolution.Systems.ValenciaDivClean.Tags",
                   "[Unit][Evolution]") {
+  TestHelpers::db::test_simple_tag<
+      grmhd::ValenciaDivClean::Tags::CharacteristicSpeeds>(
+      "CharacteristicSpeeds");
+  TestHelpers::db::test_simple_tag<grmhd::ValenciaDivClean::Tags::TildeD>(
+      "TildeD");
+  TestHelpers::db::test_simple_tag<grmhd::ValenciaDivClean::Tags::TildeTau>(
+      "TildeTau");
+  TestHelpers::db::test_simple_tag<
+      grmhd::ValenciaDivClean::Tags::TildeS<Frame::Grid>>("Grid_TildeS");
+  TestHelpers::db::test_simple_tag<
+      grmhd::ValenciaDivClean::Tags::TildeB<Frame::Grid>>("Grid_TildeB");
+  TestHelpers::db::test_simple_tag<grmhd::ValenciaDivClean::Tags::TildePhi>(
+      "TildePhi");
+
+  TestHelpers::db::test_simple_tag<
+      grmhd::ValenciaDivClean::Tags::ConstraintDampingParameter>(
+      "ConstraintDampingParameter");
+
   TestHelpers::db::test_simple_tag<
       grmhd::ValenciaDivClean::Tags::VariablesNeededFixing>(
           "VariablesNeededFixing");
