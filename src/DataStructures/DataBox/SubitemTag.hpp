5b919c85027516a9d191a465c0878aa9ec52f362
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-08-14 17:42:31 +0200

    Allow SFINAE with `::Tags::Subitem`

--- src/DataStructures/DataBox/SubitemTag.hpp
+++ src/DataStructures/DataBox/SubitemTag.hpp
@@ -12,13 +12,13 @@ namespace Tags {
 /// \cond
 // Declaration of a tag for a subitem
 // Unless specialized, it will be the reference tag below,
-template <typename Tag, typename ParentTag>
+template <typename Tag, typename ParentTag, typename = std::nullptr_t>
 struct Subitem;
 /// \endcond
 
 /// \brief a reference tag that refers to a particular Tag that is a subitem of
 /// an item tagged with ParentTag
-template <typename Tag, typename ParentTag>
+template <typename Tag, typename ParentTag, typename>
 struct Subitem : Tag, db::ReferenceTag {
   using base = Tag;
   using parent_tag = ParentTag;
