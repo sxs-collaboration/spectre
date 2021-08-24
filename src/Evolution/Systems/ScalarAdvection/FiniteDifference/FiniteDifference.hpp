8aa8a94b53c0d0292db1a3fdb55f27b30f997d9d
Yoonsoo Kim <ykim7@caltech.edu>
2021-08-24 15:24:08 -0700

    Add skeleton for advection subcell/FD

---
+++ src/Evolution/Systems/ScalarAdvection/FiniteDifference/FiniteDifference.hpp
@@ -0,0 +1,11 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace ScalarAdvection {
+/*!
+ * \brief Finite difference functionality for ScalarAdvection system
+ */
+namespace fd {}
+}  // namespace ScalarAdvection
---
+++ src/Evolution/Systems/ScalarAdvection/Subcell/Subcell.hpp
@@ -0,0 +1,12 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace ScalarAdvection {
+/*!
+ * \brief Code required by the DG-subcell/FD hybrid solver for ScalarAdvection
+ * system
+ */
+namespace subcell {}
+}  // namespace ScalarAdvection
