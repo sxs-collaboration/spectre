125688751b7f11bb61c045fd4ea4ccb224d31c29
Yoonsoo Kim <ykim7@caltech.edu>
2021-08-25 17:39:51 +0000

    Add DG grid TCI for advection

---
+++ src/Evolution/Systems/ScalarAdvection/Subcell/TciOnDgGrid.hpp
@@ -0,0 +1,36 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Domain/Tags.hpp"
+#include "Evolution/Systems/ScalarAdvection/Tags.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+class DataVector;
+template <size_t Dim>
+class Mesh;
+/// \endcond
+
+namespace ScalarAdvection::subcell {
+/*!
+ * \brief The troubled-cell indicator run on the DG grid to check if the
+ * solution is admissible.
+ *
+ * Applies the Persson TCI to \f$U\f$.
+ */
+template <size_t Dim>
+struct TciOnDgGrid {
+ public:
+  using return_tags = tmpl::list<>;
+  using argument_tags =
+      tmpl::list<ScalarAdvection::Tags::U, ::domain::Tags::Mesh<Dim>>;
+
+  static bool apply(const Scalar<DataVector>& dg_u, const Mesh<Dim>& dg_mesh,
+                    const double persson_exponent) noexcept;
+};
+}  // namespace ScalarAdvection::subcell
