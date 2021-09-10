df59ffb471150e706a0842c0e0f83c80010e1179
William Throwe <wtt6@cornell.edu>
2021-09-10 15:00:39 -0400

    Add control sys interpolation_target_tag collector

--- tests/Unit/ControlSystem/Test_Metafunctions.cpp
+++ tests/Unit/ControlSystem/Test_Metafunctions.cpp
@@ -5,7 +5,11 @@
 
 #include "ControlSystem/Component.hpp"
 #include "ControlSystem/Metafunctions.hpp"
+#include "ControlSystem/Protocols/ControlSystem.hpp"
+#include "ControlSystem/Protocols/Measurement.hpp"
+#include "ControlSystem/Protocols/Submeasurement.hpp"
 #include "Helpers/ControlSystem/TestStructs.hpp"
+#include "Utilities/ProtocolHelpers.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace control_system::metafunctions {
@@ -15,6 +19,7 @@ struct LabelB;
 struct LabelC;
 struct Metavariables;
 
+namespace Measurements {
 using MeasurementA = control_system::TestHelpers::Measurement<LabelA>;
 using MeasurementB = control_system::TestHelpers::Measurement<LabelB>;
 using SystemA0 = control_system::TestHelpers::System<2, LabelA, MeasurementA>;
@@ -65,5 +70,71 @@ static_assert(
     std::is_same_v<control_components<Metavariables, fewer_test_systems>,
                    tmpl::list<ControlComponent<Metavariables, SystemB0>,
                               ControlComponent<Metavariables, SystemA0>>>);
+}  // namespace Measurements
+
+namespace InterpolationTargetTags {
+template <typename Systems>
+struct Target;
+
+struct SubmeasurementTarget
+    : tt::ConformsTo<control_system::protocols::Submeasurement> {
+  template <typename ControlSystems>
+  using interpolation_target_tag = Target<ControlSystems>;
+
+  using argument_tags = tmpl::list<>;
+};
+
+struct SubmeasurementVoid
+    : tt::ConformsTo<control_system::protocols::Submeasurement> {
+  template <typename ControlSystems>
+  using interpolation_target_tag = void;
+
+  using argument_tags = tmpl::list<>;
+};
+
+struct Measurement : tt::ConformsTo<control_system::protocols::Measurement> {
+  using submeasurements = tmpl::list<SubmeasurementTarget, SubmeasurementVoid>;
+};
+
+struct MeasurementEmpty
+    : tt::ConformsTo<control_system::protocols::Measurement> {
+  using submeasurements = tmpl::list<>;
+};
+
+template <typename Label>
+struct ControlSystem
+    : tt::ConformsTo<control_system::protocols::ControlSystem> {
+  static std::string name() { return "ControlSystem"; }
+  using measurement = Measurement;
+
+  struct process_measurement {
+    template <typename Submeasurement>
+    using argument_tags = tmpl::list<>;
+  };
+};
+
+struct ControlSystemEmpty
+    : tt::ConformsTo<control_system::protocols::ControlSystem> {
+  static std::string name() { return "Empty"; }
+  using measurement = MeasurementEmpty;
+
+  struct process_measurement {
+    template <typename Submeasurement>
+    using argument_tags = tmpl::list<>;
+  };
+};
+
+static_assert(std::is_same_v<submeasurements<Measurement>::type,
+                             Measurement::submeasurements>);
+static_assert(std::is_same_v<submeasurements_t<Measurement>,
+                             submeasurements<Measurement>::type>);
+
+static_assert(
+    std::is_same_v<
+        interpolation_target_tags<tmpl::list<
+            ControlSystem<LabelA>, ControlSystem<LabelB>, ControlSystemEmpty>>,
+        tmpl::list<
+            Target<tmpl::list<ControlSystem<LabelA>, ControlSystem<LabelB>>>>>);
+}  // namespace InterpolationTargetTags
 }  // namespace
 }  // namespace control_system::metafunctions
