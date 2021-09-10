9526a44f8876f1afc0bf6c70a5cf7b554cdddaa1
Nikolas Wittek <nikolas.wittek@aei.mpg.de>
2021-09-10 14:11:17 +0200

    Change mesh pybindings unit test to loop over dims

--- tests/Unit/NumericalAlgorithms/Spectral/Python/Test_Mesh.py
+++ tests/Unit/NumericalAlgorithms/Spectral/Python/Test_Mesh.py
@@ -22,6 +22,7 @@ class TestMesh(unittest.TestCase):
             Quadrature.FaceCentered
         ]
         self.extents = range(12)
+        self.Mesh = [Mesh1D, Mesh2D, Mesh3D]
 
     def check_extents(self, mesh, extents):
         self.assertEqual(mesh.extents(0), extents[0])
@@ -37,80 +38,50 @@ class TestMesh(unittest.TestCase):
         self.assertEqual(mesh.quadrature(), quadratures)
 
     def test_uniform(self):
-        for extent in range(12):
-            for basis in self.bases:
-                for quadrature in self.quadratures:
-                    m1 = Mesh1D(extent, basis, quadrature)
-                    m2 = Mesh2D(extent, basis, quadrature)
-                    m3 = Mesh3D(extent, basis, quadrature)
-
-                    self.assertEqual(m1.dim, 1)
-                    self.assertEqual(m2.dim, 2)
-                    self.assertEqual(m3.dim, 3)
-
-                    self.check_extents(m1, [extent])
-                    self.check_extents(m2, [extent, extent])
-                    self.check_extents(m3, [extent, extent, extent])
-
-                    self.check_basis(m1, [basis])
-                    self.check_basis(m2, [basis, basis])
-                    self.check_basis(m3, [basis, basis, basis])
-
-                    self.check_quadrature(m1, [quadrature])
-                    self.check_quadrature(m2, [quadrature, quadrature])
-                    self.check_quadrature(m3,
-                                          [quadrature, quadrature, quadrature])
+        for dim in range(3):
+            for extent in range(12):
+                for basis in self.bases:
+                    for quadrature in self.quadratures:
+                        # the mesh constructor of dimension dim + 1
+                        Mesh = self.Mesh[dim]
+                        mesh = Mesh(extent, basis, quadrature)
+                        self.assertEqual(mesh.dim, dim + 1)
+                        self.check_extents(mesh,
+                                           [extent for _ in range(dim + 1)])
+                        self.check_basis(mesh, [basis for _ in range(dim + 1)])
+                        self.check_quadrature(
+                            mesh, [quadrature for _ in range(dim + 1)])
 
     def test_nonuniform_extents(self):
-        for basis in self.bases:
-            for quadrature in self.quadratures:
-                for i in range(100):
-                    extents1D = [random.choice(self.extents) for _ in range(1)]
-                    extents2D = [random.choice(self.extents) for _ in range(2)]
-                    extents3D = [random.choice(self.extents) for _ in range(3)]
-
-                    m1 = Mesh1D(extents1D, basis, quadrature)
-                    m2 = Mesh2D(extents2D, basis, quadrature)
-                    m3 = Mesh3D(extents3D, basis, quadrature)
-
-                    self.check_extents(m1, extents1D)
-                    self.check_extents(m2, extents2D)
-                    self.check_extents(m3, extents3D)
+        for dim in range(3):
+            for basis in self.bases:
+                for quadrature in self.quadratures:
+                    for i in range(100):
+                        # the mesh constructor of dimension dim + 1
+                        Mesh = self.Mesh[dim]
+                        extents = [
+                            random.choice(self.extents) for _ in range(dim + 1)
+                        ]
+                        mesh = Mesh(extents, basis, quadrature)
+                        self.check_extents(mesh, extents)
 
     def test_nonuniform_all_with_pickle(self):
-        for i in range(100):
-            extents1D = [random.choice(self.extents) for _ in range(1)]
-            extents2D = [random.choice(self.extents) for _ in range(2)]
-            extents3D = [random.choice(self.extents) for _ in range(3)]
+        for dim in range(3):
+            for i in range(100):
+                # the mesh constructor of dimension dim + 1
+                Mesh = self.Mesh[dim]
+                extents = [random.choice(self.extents) for _ in range(dim + 1)]
+                bases = [random.choice(self.bases) for _ in range(dim + 1)]
+                quadratures = [
+                    random.choice(self.quadratures) for _ in range(dim + 1)
+                ]
 
-            bases1D = [random.choice(self.bases) for _ in range(1)]
-            bases2D = [random.choice(self.bases) for _ in range(2)]
-            bases3D = [random.choice(self.bases) for _ in range(3)]
-
-            quadratures1D = [random.choice(self.quadratures) for _ in range(1)]
-            quadratures2D = [random.choice(self.quadratures) for _ in range(2)]
-            quadratures3D = [random.choice(self.quadratures) for _ in range(3)]
-
-            m1 = Mesh1D(extents1D, bases1D, quadratures1D)
-            m2 = Mesh2D(extents2D, bases2D, quadratures2D)
-            m3 = Mesh3D(extents3D, bases3D, quadratures3D)
-
-            # Use pickle protocol 2 for Py2 compatibility
-            m1 = pickle.loads(pickle.dumps(m1, protocol=2))
-            m2 = pickle.loads(pickle.dumps(m2, protocol=2))
-            m3 = pickle.loads(pickle.dumps(m3, protocol=2))
-
-            self.check_extents(m1, extents1D)
-            self.check_extents(m2, extents2D)
-            self.check_extents(m3, extents3D)
-
-            self.check_basis(m1, bases1D)
-            self.check_basis(m2, bases2D)
-            self.check_basis(m3, bases3D)
-
-            self.check_quadrature(m1, quadratures1D)
-            self.check_quadrature(m2, quadratures2D)
-            self.check_quadrature(m3, quadratures3D)
+                mesh = Mesh(extents, bases, quadratures)
+                # Use pickle protocol 2 for Py2 compatibility
+                mesh = pickle.loads(pickle.dumps(mesh, protocol=2))
+                self.check_extents(mesh, extents)
+                self.check_basis(mesh, bases)
+                self.check_quadrature(mesh, quadratures)
 
 
 if __name__ == '__main__':
