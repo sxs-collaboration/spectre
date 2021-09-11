42c3f77419b03a54ac64bc4fe86eb9c57c8b8b5d
Nikolas Wittek <nikolas.wittek@aei.mpg.de>
2021-09-11 14:15:36 +0200

    Add pybindings for collocation points

--- src/NumericalAlgorithms/Spectral/Python/Spectral.cpp
+++ src/NumericalAlgorithms/Spectral/Python/Spectral.cpp
@@ -3,6 +3,9 @@
 
 #include <pybind11/pybind11.h>
 
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Matrix.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
 
 namespace py = pybind11;
@@ -22,6 +25,14 @@ void bind_quadrature(py::module& m) {  // NOLINT
       .value("GaussLobatto", Spectral::Quadrature::GaussLobatto)
       .value("CellCentered", Spectral::Quadrature::CellCentered)
       .value("FaceCentered", Spectral::Quadrature::FaceCentered);
+}
+
+void bind_collocation_points(py::module& m) {  // NOLINT
+  m.def("collocation_points",
+        static_cast<const DataVector& (*)(const Mesh<1>&)>(
+            &Spectral::collocation_points),
+        py::arg("mesh"), py::return_value_policy::reference,
+        "Collocation points for a one-dimensional mesh.");
 }
 
 }  // namespace Spectral::py_bindings
