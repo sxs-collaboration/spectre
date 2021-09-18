a6812c7672758620589ba605d1fb809be0327062
Nils Deppe <nd357@cornell.edu>
2021-09-17 17:39:09 -0700

    Add ObservationBox class

---
+++ src/DataStructures/DataBox/ObservationBox.hpp
@@ -0,0 +1,121 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/Item.hpp"
+#include "Utilities/ErrorHandling/StaticAssert.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+template <typename ComputeTagsList, typename DataBoxType>
+class ObservationBox;
+/// \endcond
+
+/*!
+ * \ingroup DataStructuresGroup
+ * \brief Used for adding compute items to a `DataBox` without copying or moving
+ * any data from the original `DataBox`
+ *
+ * The intended use-case for this class is during IO/observing where additional
+ * compute tags are needed only for observation. The memory used by those
+ * compute tags does not need to be persistent and so we'd like a light-weight
+ * class to handle the on-demand computation.
+ */
+template <typename DataBoxType, typename... ComputeTags>
+class ObservationBox<tmpl::list<ComputeTags...>, DataBoxType>
+    : private db::detail::Item<ComputeTags>... {
+ public:
+  /// A list of all the compute item tags
+  using compute_item_tags = tmpl::list<ComputeTags...>;
+
+  ObservationBox() = default;
+  ObservationBox(const ObservationBox& rhs) = default;
+  ObservationBox& operator=(const ObservationBox& rhs) = default;
+  ObservationBox(ObservationBox&& rhs) = default;
+  ObservationBox& operator=(ObservationBox&& rhs) = default;
+  ~ObservationBox() = default;
+
+  /// Create an `ObservationBox` that can also retrieve things out of the
+  /// `databox` passed in.
+  ObservationBox(const DataBoxType& databox);
+
+  /// Retrieve the tag `Tag`, should be called by the free function db::get
+  template <typename Tag>
+  const auto& get() const;
+
+ private:
+  template <typename Tag>
+  const auto& get_item() const {
+    return static_cast<const db::detail::Item<Tag>&>(*this);
+  }
+
+  template <typename ComputeTag, typename... ArgumentTags>
+  void evaluate_compute_item(
+      tmpl::list<ArgumentTags...> /*meta*/) const;
+
+  using tags_list =
+      tmpl::push_back<typename DataBoxType::tags_list, ComputeTags...>;
+
+  const DataBoxType* databox_ = nullptr;
+};
+
+/*!
+ * \ingroup DataStructuresGroup
+ * \brief Retrieve a `Tag` from the `ObservationBox`.
+ */
+template <typename Tag, typename DataBoxType, typename... ComputeTags>
+const auto& get(
+    const ObservationBox<tmpl::list<ComputeTags...>, DataBoxType>& box) {
+  return box.template get<Tag>();
+}
+
+template <typename DataBoxType, typename... ComputeTags>
+ObservationBox<tmpl::list<ComputeTags...>, DataBoxType>::ObservationBox(
+    const DataBoxType& databox)
+    : databox_(&databox) {
+  DEBUG_STATIC_ASSERT(
+      (db::is_immutable_item_tag_v<ComputeTags> and ...),
+      "All tags passed to ObservationBox must be compute tags.");
+}
+
+template <typename DataBoxType, typename... ComputeTags>
+template <typename Tag>
+const auto& ObservationBox<tmpl::list<ComputeTags...>, DataBoxType>::get()
+    const {
+  DEBUG_STATIC_ASSERT(not db::detail::has_no_matching_tag_v<tags_list, Tag>,
+                      "Found no tags in the ObservationBox that match the tag "
+                      "being retrieved.");
+  DEBUG_STATIC_ASSERT(
+      db::detail::has_unique_matching_tag_v<tags_list, Tag>,
+      "Found more than one tag in the ObservationBox that matches the tag "
+      "being retrieved. This happens because more than one tag with the same "
+      "base (class) tag was added to the ObservationBox.");
+
+  if constexpr (db::tag_is_retrievable_v<Tag, DataBoxType>) {
+    return db::get<Tag>(*databox_);
+  } else {
+    using item_tag = db::detail::first_matching_tag<compute_item_tags, Tag>;
+    if (not get_item<item_tag>().evaluated()) {
+      evaluate_compute_item<item_tag>(typename item_tag::argument_tags{});
+    }
+    if constexpr (tt::is_a_v<std::unique_ptr, typename item_tag::type>) {
+      return *(get_item<item_tag>().get());
+    } else {
+      return get_item<item_tag>().get();
+    }
+  }
+}
+
+template <typename DataBoxType, typename... ComputeTags>
+template <typename ComputeTag, typename... ArgumentTags>
+void ObservationBox<tmpl::list<ComputeTags...>, DataBoxType>::
+    evaluate_compute_item(tmpl::list<ArgumentTags...> /*meta*/) const {
+  get_item<ComputeTag>().evaluate(get<ArgumentTags>()...);
+}
+
+template <typename ComputeTagsList, typename DataBoxType>
+auto make_observation_box(const DataBoxType& databox) {
+  return ObservationBox<ComputeTagsList, DataBoxType>{databox};
+}
---
+++ tests/Unit/DataStructures/DataBox/Test_ObservationBox.cpp
@@ -0,0 +1,78 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/ObservationBox.hpp"
+#include "DataStructures/DataBox/Tag.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace {
+void multiply_by_two(const gsl::not_null<double*> result, const double value) {
+  *result = 2.0 * value;
+}
+
+struct Tag0 : db::SimpleTag {
+  using type = double;
+};
+
+struct Tag1 : db::SimpleTag {
+  using type = double;
+};
+
+struct Tag1Compute : Tag1, db::ComputeTag {
+  using base = Tag1;
+  using return_type = double;
+  static constexpr auto function = multiply_by_two;
+  using argument_tags = tmpl::list<Tag0>;
+};
+
+struct Tag2 : db::SimpleTag {
+  using type = double;
+};
+
+struct Tag2Compute : Tag2, db::ComputeTag {
+  using base = Tag2;
+  using return_type = double;
+  static constexpr auto function = multiply_by_two;
+  using argument_tags = tmpl::list<Tag1>;
+};
+
+struct Tag3 : db::SimpleTag {
+  using type = double;
+};
+
+struct Tag3Compute : Tag3, db::ComputeTag {
+  using base = Tag3;
+  using return_type = double;
+  static auto function(const gsl::not_null<double*> result, const double value0,
+                       const double value1, const double value2) {
+    ++times_called;
+    *result = value0 + value1 + value2;
+  }
+  using argument_tags = tmpl::list<Tag0, Tag1, Tag2>;
+  static size_t times_called;
+};
+
+size_t Tag3Compute::times_called = 0;
+
+SPECTRE_TEST_CASE("Unit.DataStructures.DataBox.ObservationBox",
+                  "[Unit][DataStructures]") {
+  const auto db_box =
+      db::create<db::AddSimpleTags<Tag0>, db::AddComputeTags<Tag1Compute>>(2.0);
+  const auto obs_box =
+      make_observation_box<db::AddComputeTags<Tag2Compute, Tag3Compute>>(
+          db_box);
+  CHECK(Tag3Compute::times_called == 0);
+  CHECK(get<Tag0>(obs_box) == 2.0);
+  CHECK(get<Tag1>(obs_box) == 4.0);
+  CHECK(get<Tag2>(obs_box) == (2.0 * 2.0 * 2.0));
+  CHECK(get<Tag3>(obs_box) == (2.0 + 2.0 * 2.0 + 2.0 * 2.0 * 2.0));
+  CHECK(Tag3Compute::times_called == 1);
+  // Call a second time to make sure the compute tag didn't get evaluated again.
+  CHECK(get<Tag3>(obs_box) == (2.0 + 2.0 * 2.0 + 2.0 * 2.0 * 2.0));
+  CHECK(Tag3Compute::times_called == 1);
+}
+
+}  // namespace
