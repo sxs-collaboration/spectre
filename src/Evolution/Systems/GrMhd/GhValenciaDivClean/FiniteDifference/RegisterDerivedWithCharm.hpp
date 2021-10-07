4b9ab06f2b12678db4b88e2548596aeec6029967
Nils Deppe <nils@nilsdeppe.com>
2021-10-07 10:39:34 -0700

    Add RegisterDerivedWithCharm to GhValenciaDivClean FD

---
+++ src/Evolution/Systems/GrMhd/GhValenciaDivClean/FiniteDifference/RegisterDerivedWithCharm.hpp
@@ -0,0 +1,8 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace grmhd::GhValenciaDivClean::fd {
+void register_derived_with_charm();
+}  // namespace grmhd::GhValenciaDivClean::fd
