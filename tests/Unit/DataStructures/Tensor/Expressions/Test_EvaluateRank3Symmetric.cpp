e16fc18f27819a234c98b2bb8859e333c50dbf0b
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2021-10-20 17:40:47 -0700

    Split up bulky tensor expression test into two test files

--- tests/Unit/DataStructures/Tensor/Expressions/Test_EvaluateRank3Symmetric.cpp
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_EvaluateRank3Symmetric.cpp
@@ -1,6 +1,10 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
+// Rank 3 test cases for TensorExpressions::evaluate are split into this file
+// and Test_EvaluateRank3NonSymmetric.cpp in order to reduce compile time memory
+// usage per cpp file.
+
 #include "Framework/TestingFramework.hpp"
 
 #include "DataStructures/DataVector.hpp"
@@ -8,13 +12,9 @@
 #include "DataStructures/Tensor/IndexType.hpp"
 #include "Helpers/DataStructures/Tensor/Expressions/EvaluateRank3.hpp"
 
-SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.EvaluateRank3",
-                  "[DataStructures][Unit]") {
-  // Rank 3: double; nonsymmetric
-  TestHelpers::TensorExpressions::test_evaluate_rank_3_no_symmetry<
-      double, SpacetimeIndex, SpatialIndex, SpacetimeIndex, UpLo::Up, UpLo::Lo,
-      UpLo::Up, ti_D, ti_j, ti_B>();
-
+SPECTRE_TEST_CASE(
+    "Unit.DataStructures.Tensor.Expression.EvaluateRank3Symmetric",
+    "[DataStructures][Unit]") {
   // Rank 3: double; first and second indices symmetric
   TestHelpers::TensorExpressions::test_evaluate_rank_3_ab_symmetry<
       double, SpacetimeIndex, SpacetimeIndex, UpLo::Lo, UpLo::Up, ti_b, ti_a,
@@ -33,11 +33,6 @@ SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.EvaluateRank3",
   // Rank 3: double; symmetric
   TestHelpers::TensorExpressions::test_evaluate_rank_3_abc_symmetry<
       double, SpacetimeIndex, UpLo::Lo, ti_f, ti_d, ti_a>();
-
-  // Rank 3: DataVector; nonsymmetric
-  TestHelpers::TensorExpressions::test_evaluate_rank_3_no_symmetry<
-      DataVector, SpacetimeIndex, SpatialIndex, SpacetimeIndex, UpLo::Up,
-      UpLo::Lo, UpLo::Up, ti_D, ti_j, ti_B>();
 
   // Rank 3: DataVector; first and second indices symmetric
   TestHelpers::TensorExpressions::test_evaluate_rank_3_ab_symmetry<
