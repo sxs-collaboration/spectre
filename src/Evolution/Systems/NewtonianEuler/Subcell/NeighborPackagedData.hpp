0032c23e30d2680c8adfb65177634e3cc4915f33
Lawrence Kidder <kidder@astro.cornell.edu>
2021-10-29 11:48:23 -0400

    Add subcell::NeighborPackagedData to NewtonianEuler
    Adds struct that uses reconstruction to compute the neighbor's data to the
    boundary correction for any neighbors using subcell.

---
+++ src/Evolution/Systems/NewtonianEuler/Subcell/NeighborPackagedData.hpp
@@ -0,0 +1,215 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <boost/functional/hash.hpp>
+#include <cstddef>
+#include <optional>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/PrefixHelpers.hpp"
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/FixedHashMap.hpp"
+#include "DataStructures/Index.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "DataStructures/Variables.hpp"
+#include "DataStructures/VariablesTag.hpp"
+#include "Domain/Structure/Direction.hpp"
+#include "Domain/Structure/Element.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Structure/MaxNumberOfNeighbors.hpp"
+#include "Domain/Tags.hpp"
+#include "Domain/TagsTimeDependent.hpp"
+#include "Evolution/BoundaryCorrectionTags.hpp"
+#include "Evolution/DgSubcell/Projection.hpp"
+#include "Evolution/DgSubcell/Reconstruction.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
+#include "Evolution/DgSubcell/Tags/NeighborData.hpp"
+#include "Evolution/DiscontinuousGalerkin/Actions/NormalCovectorAndMagnitude.hpp"
+#include "Evolution/DiscontinuousGalerkin/Actions/PackageDataImpl.hpp"
+#include "Evolution/Systems/NewtonianEuler/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/NewtonianEuler/BoundaryCorrections/Factory.hpp"
+#include "Evolution/Systems/NewtonianEuler/FiniteDifference/Reconstructor.hpp"
+#include "Evolution/Systems/NewtonianEuler/FiniteDifference/Tag.hpp"
+#include "Evolution/Systems/NewtonianEuler/Subcell/ComputeFluxes.hpp"
+#include "Evolution/Systems/NewtonianEuler/System.hpp"
+#include "Evolution/Systems/NewtonianEuler/Tags.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "Parallel/Tags/Metavariables.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
+#include "PointwiseFunctions/Hydro/Tags.hpp"
+#include "Utilities/FakeVirtual.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace NewtonianEuler::subcell {
+/*!
+ * \brief On elements using DG, reconstructs the interface data from a
+ * neighboring element doing subcell.
+ *
+ * The neighbor's packaged data needed by the boundary correction is computed
+ * and returned so that it can be used for solving the Riemann problem on the
+ * interfaces.
+ *
+ * Note that for strict conservation the Riemann solve should be done on the
+ * subcells, with the correction being projected back to the DG interface.
+ * However, in practice such strict conservation doesn't seem to be necessary
+ * and can be explained by that we only need strict conservation at shocks, and
+ * if one element is doing DG, then we aren't at a shock.
+ */
+struct NeighborPackagedData {
+  template <size_t Dim, typename DbTagsList>
+  static FixedHashMap<maximum_number_of_neighbors(Dim),
+                      std::pair<Direction<Dim>, ElementId<Dim>>,
+                      std::vector<double>,
+                      boost::hash<std::pair<Direction<Dim>, ElementId<Dim>>>>
+  apply(const db::DataBox<DbTagsList>& box,
+        const std::vector<std::pair<Direction<Dim>, ElementId<Dim>>>&
+            mortars_to_reconstruct_to) {
+    using system =
+        typename std::decay_t<decltype(db::get<Parallel::Tags::Metavariables>(
+            box))>::system;
+    using evolved_vars_tag = typename system::variables_tag;
+    using evolved_vars_tags = typename evolved_vars_tag::tags_list;
+    using prim_tags = typename system::primitive_variables_tag::tags_list;
+    using fluxes_tags = db::wrap_tags_in<::Tags::Flux, evolved_vars_tags,
+                                         tmpl::size_t<Dim>, Frame::Inertial>;
+
+    ASSERT(not db::get<domain::Tags::MeshVelocity<Dim>>(box).has_value(),
+           "Haven't yet added support for moving mesh to DG-subcell. This "
+           "should be easy to generalize, but we will want to consider "
+           "storing the mesh velocity on the faces instead of "
+           "re-slicing/projecting.");
+
+    FixedHashMap<maximum_number_of_neighbors(Dim),
+                 std::pair<Direction<Dim>, ElementId<Dim>>, std::vector<double>,
+                 boost::hash<std::pair<Direction<Dim>, ElementId<Dim>>>>
+        neighbor_package_data{};
+
+    const auto& neighbor_subcell_data = db::get<
+        evolution::dg::subcell::Tags::NeighborDataForReconstruction<Dim>>(box);
+    const Mesh<Dim>& subcell_mesh =
+        db::get<evolution::dg::subcell::Tags::Mesh<Dim>>(box);
+    const Mesh<Dim>& dg_mesh = db::get<domain::Tags::Mesh<Dim>>(box);
+
+    const auto volume_prims = evolution::dg::subcell::fd::project(
+        db::get<typename system::primitive_variables_tag>(box), dg_mesh,
+        subcell_mesh.extents());
+
+    const auto& recons =
+        db::get<NewtonianEuler::fd::Tags::Reconstructor<Dim>>(box);
+    const auto& boundary_correction =
+        db::get<evolution::Tags::BoundaryCorrection<system>>(box);
+    using derived_boundary_corrections =
+        typename std::decay_t<decltype(boundary_correction)>::creatable_classes;
+    tmpl::for_each<
+        derived_boundary_corrections>([&box, &boundary_correction, &dg_mesh,
+                                       &mortars_to_reconstruct_to,
+                                       &neighbor_package_data,
+                                       &neighbor_subcell_data, &recons,
+                                       &subcell_mesh, &volume_prims](
+                                          auto derived_correction_v) {
+      using DerivedCorrection = tmpl::type_from<decltype(derived_correction_v)>;
+      if (typeid(boundary_correction) == typeid(DerivedCorrection)) {
+        using dg_package_data_temporary_tags =
+            typename DerivedCorrection::dg_package_data_temporary_tags;
+        using dg_package_data_argument_tags =
+            tmpl::append<evolved_vars_tags, prim_tags, fluxes_tags,
+                         dg_package_data_temporary_tags>;
+
+        const auto& element = db::get<domain::Tags::Element<Dim>>(box);
+        const auto& eos = get<hydro::Tags::EquationOfStateBase>(box);
+
+        using dg_package_field_tags =
+            typename DerivedCorrection::dg_package_field_tags;
+        Variables<dg_package_data_argument_tags> vars_on_face;
+        Variables<dg_package_field_tags> packaged_data;
+        for (const auto& mortar_id : mortars_to_reconstruct_to) {
+          const Direction<Dim>& direction = mortar_id.first;
+
+          Index<Dim> extents = subcell_mesh.extents();
+          // Switch to face-centered instead of cell-centered points on the FD.
+          // There are num_cell_centered+1 face-centered points.
+          ++extents[direction.dimension()];
+
+          // Computed prims and cons on face via reconstruction
+          const size_t num_face_pts = subcell_mesh.extents()
+                                          .slice_away(direction.dimension())
+                                          .product();
+          vars_on_face.initialize(num_face_pts);
+
+          call_with_dynamic_type<void,
+                                 typename NewtonianEuler::fd::Reconstructor<
+                                     Dim>::creatable_classes>(
+              &recons, [&element, &eos, &mortar_id, &neighbor_subcell_data,
+                        &subcell_mesh, &vars_on_face,
+                        &volume_prims](const auto& reconstructor) {
+                reconstructor->reconstruct_fd_neighbor(
+                    make_not_null(&vars_on_face), volume_prims, eos, element,
+                    neighbor_subcell_data, subcell_mesh, mortar_id.first);
+              });
+
+          NewtonianEuler::subcell::compute_fluxes<Dim>(
+              make_not_null(&vars_on_face));
+
+          tnsr::i<DataVector, Dim, Frame::Inertial> normal_covector = get<
+              evolution::dg::Tags::NormalCovector<Dim>>(
+              *db::get<evolution::dg::Tags::NormalCovectorAndMagnitude<Dim>>(
+                   box)
+                   .at(mortar_id.first));
+          for (auto& t : normal_covector) {
+            t *= -1.0;
+          }
+          if constexpr (Dim > 1) {
+            const auto dg_normal_covector = normal_covector;
+            for (size_t i = 0; i < Dim; ++i) {
+              normal_covector.get(i) = evolution::dg::subcell::fd::project(
+                  dg_normal_covector.get(i),
+                  dg_mesh.slice_away(mortar_id.first.dimension()),
+                  subcell_mesh.extents().slice_away(
+                      mortar_id.first.dimension()));
+            }
+          }
+
+          // Compute the packaged data
+          packaged_data.initialize(num_face_pts);
+          using dg_package_data_projected_tags = tmpl::append<
+              evolved_vars_tags, fluxes_tags, dg_package_data_temporary_tags,
+              typename DerivedCorrection::dg_package_data_primitive_tags>;
+          evolution::dg::Actions::detail::dg_package_data<system>(
+              make_not_null(&packaged_data),
+              dynamic_cast<const DerivedCorrection&>(boundary_correction),
+              vars_on_face, normal_covector, {std::nullopt}, box,
+              typename DerivedCorrection::dg_package_data_volume_tags{},
+              dg_package_data_projected_tags{});
+
+          if constexpr (Dim == 1) {
+            (void)dg_mesh;
+            neighbor_package_data[mortar_id] = std::vector<double>{
+                packaged_data.data(),
+                packaged_data.data() + packaged_data.size()};
+          } else {
+            // Reconstruct the DG solution.
+            // Really we should be solving the boundary correction and
+            // then reconstructing, but away from a shock this doesn't
+            // matter.
+            auto dg_packaged_data = evolution::dg::subcell::fd::reconstruct(
+                packaged_data, dg_mesh.slice_away(mortar_id.first.dimension()),
+                subcell_mesh.extents().slice_away(mortar_id.first.dimension()));
+            neighbor_package_data[mortar_id] = std::vector<double>{
+                dg_packaged_data.data(),
+                dg_packaged_data.data() + dg_packaged_data.size()};
+          }
+        }
+      }
+    });
+
+    return neighbor_package_data;
+  }
+};
+}  // namespace NewtonianEuler::subcell
---
+++ tests/Unit/Evolution/Systems/NewtonianEuler/Subcell/Test_NeighborPackagedData.cpp
@@ -0,0 +1,322 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/SliceVariables.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "DataStructures/Variables.hpp"
+#include "DataStructures/VariablesTag.hpp"
+#include "Domain/Block.hpp"
+#include "Domain/CoordinateMaps/Affine.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.tpp"
+#include "Domain/CoordinateMaps/Identity.hpp"
+#include "Domain/CoordinateMaps/ProductMaps.hpp"
+#include "Domain/CoordinateMaps/ProductMaps.tpp"
+#include "Domain/CreateInitialElement.hpp"
+#include "Domain/LogicalCoordinates.hpp"
+#include "Domain/Structure/Direction.hpp"
+#include "Domain/Structure/DirectionMap.hpp"
+#include "Domain/Structure/Element.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Structure/SegmentId.hpp"
+#include "Domain/Tags.hpp"
+#include "Domain/TagsTimeDependent.hpp"
+#include "Evolution/BoundaryCorrectionTags.hpp"
+#include "Evolution/DgSubcell/Mesh.hpp"
+#include "Evolution/DgSubcell/SliceData.hpp"
+#include "Evolution/DgSubcell/Tags/Coordinates.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
+#include "Evolution/DgSubcell/Tags/NeighborData.hpp"
+#include "Evolution/DiscontinuousGalerkin/Actions/NormalCovectorAndMagnitude.hpp"
+#include "Evolution/DiscontinuousGalerkin/MortarTags.hpp"
+#include "Evolution/Systems/NewtonianEuler/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/NewtonianEuler/BoundaryCorrections/Factory.hpp"
+#include "Evolution/Systems/NewtonianEuler/BoundaryCorrections/Hll.hpp"
+#include "Evolution/Systems/NewtonianEuler/ConservativeFromPrimitive.hpp"
+#include "Evolution/Systems/NewtonianEuler/FiniteDifference/AoWeno.hpp"
+#include "Evolution/Systems/NewtonianEuler/FiniteDifference/MonotisedCentral.hpp"
+#include "Evolution/Systems/NewtonianEuler/FiniteDifference/Tag.hpp"
+#include "Evolution/Systems/NewtonianEuler/Subcell/NeighborPackagedData.hpp"
+#include "Evolution/Systems/NewtonianEuler/Tags.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/NewtonianEuler/SmoothFlow.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/IdealFluid.hpp"
+#include "PointwiseFunctions/Hydro/Tags.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace {
+using Affine = domain::CoordinateMaps::Affine;
+using Affine2D = domain::CoordinateMaps::ProductOf2Maps<Affine, Affine>;
+using Affine3D = domain::CoordinateMaps::ProductOf3Maps<Affine, Affine, Affine>;
+
+template <size_t Dim>
+auto make_coord_map();
+
+template <>
+auto make_coord_map<1>() {
+  Affine affine_map{-1.0, 1.0, 2.0, 3.0};
+  return domain::make_coordinate_map<Frame::ElementLogical, Frame::Inertial>(
+      affine_map);
+}
+
+template <>
+auto make_coord_map<2>() {
+  Affine affine_map{-1.0, 1.0, 2.0, 3.0};
+  Affine2D product_map{affine_map, affine_map};
+  return domain::make_coordinate_map<Frame::ElementLogical, Frame::Inertial>(
+      product_map);
+}
+
+template <>
+auto make_coord_map<3>() {
+  Affine affine_map{-1.0, 1.0, 2.0, 3.0};
+  Affine3D product_map{affine_map, affine_map, affine_map};
+  return domain::make_coordinate_map<Frame::ElementLogical, Frame::Inertial>(
+      product_map);
+}
+
+template <size_t Dim>
+auto make_element();
+
+template <>
+auto make_element<1>() {
+  Affine affine_map{-1.0, 1.0, 2.0, 3.0};
+  return domain::Initialization::create_initial_element(
+      ElementId<1>{0, {SegmentId{3, 4}}},
+      Block<1>{domain::make_coordinate_map_base<Frame::BlockLogical,
+                                                Frame::Inertial>(affine_map),
+               0,
+               {},
+               {}},
+      std::vector<std::array<size_t, 1>>{std::array<size_t, 1>{{3}}});
+}
+
+template <>
+auto make_element<2>() {
+  Affine affine_map{-1.0, 1.0, 2.0, 3.0};
+  return domain::Initialization::create_initial_element(
+      ElementId<2>{0, {SegmentId{3, 4}, SegmentId{3, 4}}},
+      Block<2>{domain::make_coordinate_map_base<Frame::BlockLogical,
+                                                Frame::Inertial>(
+                   Affine2D{affine_map, affine_map}),
+               0,
+               {},
+               {}},
+      std::vector<std::array<size_t, 2>>{std::array<size_t, 2>{{3, 3}}});
+}
+
+template <>
+auto make_element<3>() {
+  Affine affine_map{-1.0, 1.0, 2.0, 3.0};
+  return domain::Initialization::create_initial_element(
+      ElementId<3>{0, {SegmentId{3, 4}, SegmentId{3, 4}, SegmentId{3, 4}}},
+      Block<3>{domain::make_coordinate_map_base<Frame::BlockLogical,
+                                                Frame::Inertial>(
+                   Affine3D{affine_map, affine_map, affine_map}),
+               0,
+               {},
+               {}},
+      std::vector<std::array<size_t, 3>>{std::array<size_t, 3>{{3, 3, 3}}});
+}
+
+template <size_t Dim>
+struct MetaVars {
+  using solution = NewtonianEuler::Solutions::SmoothFlow<Dim>;
+  using system = NewtonianEuler::System<Dim, solution>;
+};
+
+template <size_t Dim>
+double test(const size_t num_dg_pts) {
+  using solution = NewtonianEuler::Solutions::SmoothFlow<Dim>;
+  using eos = typename solution::equation_of_state_type;
+  using system = NewtonianEuler::System<Dim, solution>;
+  using variables_tag = typename system::variables_tag;
+  const auto coordinate_map = make_coord_map<Dim>();
+  const auto moving_mesh_map =
+      domain::make_coordinate_map<Frame::Grid, Frame::Inertial>(
+          domain::CoordinateMaps::Identity<Dim>{});
+  const auto element = make_element<Dim>();
+
+  const solution soln{make_array<Dim>(0.3), make_array<Dim>(-0.2), 0.5, 1.5,
+                      0.01};
+
+  const double time = 0.0;
+  const Mesh<Dim> dg_mesh{num_dg_pts, Spectral::Basis::Legendre,
+                          Spectral::Quadrature::GaussLobatto};
+  const Mesh<Dim> subcell_mesh = evolution::dg::subcell::fd::mesh(dg_mesh);
+  const auto dg_coords = coordinate_map(logical_coordinates(dg_mesh));
+
+  // Neighbor data for reconstruction.
+  //
+  // 0. neighbors coords (our logical coords +2)
+  // 1. compute prims from solution
+  // 2. compute prims needed for reconstruction
+  // 3. set neighbor data
+  typename evolution::dg::subcell::Tags::NeighborDataForReconstruction<
+      Dim>::type neighbor_data{};
+  using prims_to_reconstruct_tags =
+      tmpl::list<NewtonianEuler::Tags::MassDensity<DataVector>,
+                 NewtonianEuler::Tags::Velocity<DataVector, Dim>,
+                 NewtonianEuler::Tags::Pressure<DataVector>>;
+  using prim_tags = typename system::primitive_variables_tag::tags_list;
+  for (const Direction<Dim>& direction : Direction<Dim>::all_directions()) {
+    auto neighbor_logical_coords = logical_coordinates(subcell_mesh);
+    neighbor_logical_coords.get(direction.dimension()) +=
+        2.0 * direction.sign();
+    auto neighbor_coords = coordinate_map(neighbor_logical_coords);
+    const auto neighbor_prims =
+        soln.variables(neighbor_coords, time, prim_tags{});
+    Variables<prims_to_reconstruct_tags> prims_to_reconstruct{
+        subcell_mesh.number_of_grid_points()};
+    tmpl::for_each<prims_to_reconstruct_tags>(
+        [&prims_to_reconstruct, &neighbor_prims](auto tag_v) {
+          using tag = tmpl::type_from<decltype(tag_v)>;
+          get<tag>(prims_to_reconstruct) = get<tag>(neighbor_prims);
+        });
+    // Slice data so we can add it to the element's neighbor data
+    DirectionMap<Dim, bool> directions_to_slice{};
+    directions_to_slice[direction.opposite()] = true;
+    std::vector<double> neighbor_data_in_direction =
+        evolution::dg::subcell::slice_data(
+            prims_to_reconstruct, subcell_mesh.extents(),
+            NewtonianEuler::fd::MonotisedCentralPrim<Dim>{}.ghost_zone_size(),
+            directions_to_slice)
+            .at(direction.opposite());
+    neighbor_data[std::pair{direction,
+                            *element.neighbors().at(direction).begin()}] =
+        neighbor_data_in_direction;
+  }
+  Variables<prim_tags> dg_prim_vars{dg_mesh.number_of_grid_points()};
+  dg_prim_vars.assign_subset(soln.variables(dg_coords, time, prim_tags{}));
+  DirectionMap<Dim, std::optional<Variables<
+                        tmpl::list<evolution::dg::Tags::MagnitudeOfNormal,
+                                   evolution::dg::Tags::NormalCovector<Dim>>>>>
+      normal_vectors{};
+  for (const auto& direction : Direction<Dim>::all_directions()) {
+    const Mesh<Dim - 1> face_mesh = dg_mesh.slice_away(direction.dimension());
+    const auto face_logical_coords =
+        interface_logical_coordinates(face_mesh, direction);
+    std::unordered_map<Direction<Dim>,
+                       tnsr::i<DataVector, Dim, Frame::Inertial>>
+        unnormalized_normal_covectors{};
+    tnsr::i<DataVector, Dim, Frame::Inertial> unnormalized_covector{};
+    for (size_t i = 0; i < Dim; ++i) {
+      unnormalized_covector.get(i) =
+          coordinate_map.inv_jacobian(face_logical_coords)
+              .get(direction.dimension(), i);
+    }
+    unnormalized_normal_covectors[direction] = unnormalized_covector;
+    Variables<tmpl::list<
+        evolution::dg::Actions::detail::NormalVector<Dim>,
+        evolution::dg::Actions::detail::OneOverNormalVectorMagnitude>>
+        fields_on_face{face_mesh.number_of_grid_points()};
+    normal_vectors[direction] = std::nullopt;
+    evolution::dg::Actions::detail::
+        unit_normal_vector_and_covector_and_magnitude<system>(
+            make_not_null(&normal_vectors), make_not_null(&fields_on_face),
+            direction, unnormalized_normal_covectors, moving_mesh_map);
+  }
+  auto box = db::create<
+      db::AddSimpleTags<
+          Parallel::Tags::MetavariablesImpl<MetaVars<Dim>>,
+          domain::Tags::Element<Dim>, domain::Tags::Mesh<Dim>,
+          evolution::dg::subcell::Tags::Mesh<Dim>,
+          NewtonianEuler::fd::Tags::Reconstructor<Dim>,
+          evolution::Tags::BoundaryCorrection<system>,
+          hydro::Tags::EquationOfState<eos>,
+          typename system::primitive_variables_tag, variables_tag,
+          evolution::dg::subcell::Tags::NeighborDataForReconstruction<Dim>,
+          evolution::dg::Tags::MortarData<Dim>, domain::Tags::MeshVelocity<Dim>,
+          evolution::dg::Tags::NormalCovectorAndMagnitude<Dim>>,
+      db::AddComputeTags<
+          evolution::dg::subcell::Tags::LogicalCoordinatesCompute<Dim>>>(
+      MetaVars<Dim>{}, element, dg_mesh, subcell_mesh,
+      std::unique_ptr<NewtonianEuler::fd::Reconstructor<Dim>>{
+          std::make_unique<NewtonianEuler::fd::MonotisedCentralPrim<Dim>>()},
+      std::unique_ptr<
+          NewtonianEuler::BoundaryCorrections::BoundaryCorrection<Dim>>{
+          std::make_unique<NewtonianEuler::BoundaryCorrections::Hll<Dim>>()},
+      soln.equation_of_state(), dg_prim_vars,
+      typename variables_tag::type{dg_mesh.number_of_grid_points()},
+      neighbor_data, typename evolution::dg::Tags::MortarData<Dim>::type{},
+      std::optional<tnsr::I<DataVector, Dim, Frame::Inertial>>{},
+      normal_vectors);
+
+  db::mutate_apply<NewtonianEuler::ConservativeFromPrimitive<Dim>>(
+      make_not_null(&box));
+
+  std::vector<std::pair<Direction<Dim>, ElementId<Dim>>>
+      mortars_to_reconstruct_to{};
+  for (const auto& [direction, neighbors] : element.neighbors()) {
+    mortars_to_reconstruct_to.emplace_back(direction, *neighbors.begin());
+  }
+
+  const auto all_packaged_data =
+      NewtonianEuler::subcell::NeighborPackagedData::apply(
+          box, mortars_to_reconstruct_to);
+
+  // Parse out evolved vars, since those are easiest to check for correctness,
+  // then return absolute difference between analytic and reconstructed values.
+  FixedHashMap<maximum_number_of_neighbors(Dim),
+               std::pair<Direction<Dim>, ElementId<Dim>>,
+               typename variables_tag::type,
+               boost::hash<std::pair<Direction<Dim>, ElementId<Dim>>>>
+      evolved_vars_errors{};
+  double max_abs_error = 0.0;
+  for (const auto& [direction_and_id, data] : all_packaged_data) {
+    const auto& direction = direction_and_id.first;
+    using dg_package_field_tags =
+        typename NewtonianEuler::BoundaryCorrections::Hll<
+            Dim>::dg_package_field_tags;
+    const Mesh<Dim - 1> face_mesh = dg_mesh.slice_away(direction.dimension());
+    Variables<dg_package_field_tags> packaged_data{
+        face_mesh.number_of_grid_points()};
+    std::copy(data.begin(), data.end(), packaged_data.data());
+    auto sliced_vars = data_on_slice(
+        db::get<variables_tag>(box), dg_mesh.extents(), direction.dimension(),
+        direction.side() == Side::Upper
+            ? dg_mesh.extents(direction.dimension()) - 1
+            : 0);
+
+    tmpl::for_each<typename variables_tag::type::tags_list>(
+        [&sliced_vars, &max_abs_error, &packaged_data](auto tag_v) {
+          using tag = tmpl::type_from<decltype(tag_v)>;
+          auto& sliced_tensor = get<tag>(sliced_vars);
+          const auto& packaged_data_tensor = get<tag>(packaged_data);
+          for (size_t tensor_index = 0; tensor_index < sliced_tensor.size();
+               ++tensor_index) {
+            max_abs_error = std::max(
+                max_abs_error, max(abs(sliced_tensor[tensor_index] -
+                                       packaged_data_tensor[tensor_index])));
+          }
+        });
+  }
+  return max_abs_error;
+}
+}  // namespace
+
+SPECTRE_TEST_CASE(
+    "Unit.Evolution.Systems.NewtoniainEuler.Subcell.NeighborPackagedData",
+    "[Unit][Evolution]") {
+  // This tests sets up a cube [2,3]^3 for a SmoothFlow problem and verifies
+  // that the difference between the reconstructed evolved variables and the
+  // sliced (exact on LGL grid) evolved variables on the interfaces decreases.
+  CHECK(test<1>(3) > test<1>(6));
+  CHECK(test<2>(3) > test<2>(6));
+  CHECK(test<3>(3) > test<3>(6));
+  // Check that the error is "reasonably small"
+  CHECK(test<1>(4) < 1.0e-4);
+  CHECK(test<2>(4) < 1.0e-4);
+  CHECK(test<3>(4) < 1.0e-4);
+}
