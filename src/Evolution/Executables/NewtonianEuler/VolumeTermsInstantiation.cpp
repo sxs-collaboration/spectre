51815a662ba9bac275def187ae3ba38dcba7d247
Lawrence Kidder <kidder@astro.cornell.edu>
2021-10-29 19:15:29 -0400

    Fix instantiations for NewtoniainEuler systems with non-zero source

--- src/Evolution/Executables/NewtonianEuler/VolumeTermsInstantiation.cpp
+++ src/Evolution/Executables/NewtonianEuler/VolumeTermsInstantiation.cpp
@@ -17,10 +17,7 @@
 
 namespace evolution::dg::Actions::detail {
 #define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
-#define INITIAL_DATA_TYPE(data)                           \
-  BOOST_PP_IF(BOOST_PP_TUPLE_ELEM(2, data),               \
-              BOOST_PP_TUPLE_ELEM(1, data) < DIM(data) >, \
-              BOOST_PP_TUPLE_ELEM(1, data))
+#define INITIAL_DATA_TYPE(data) BOOST_PP_TUPLE_ELEM(1, data)<DIM(data)>
 #define SYSTEM(data) \
   ::NewtonianEuler::System<DIM(data), INITIAL_DATA_TYPE(data)>
 
@@ -58,24 +55,130 @@ namespace evolution::dg::Actions::detail {
       const tnsr::I<DataVector, DIM(data)>& velocity,                         \
       const Scalar<DataVector>& pressure);
 
-// The last list passed to GENERATE_INSTANTIATIONS has only one element that
-// must be `1` if the analytic solutions/data are templated on the dimension,
-// and `0` if the analytic solutions/data are _not_ templated on dimension.
 GENERATE_INSTANTIATIONS(INSTANTIATION, (1, 2, 3),
                         (::NewtonianEuler::Solutions::RiemannProblem,
-                         ::NewtonianEuler::Solutions::SmoothFlow),
-                        (1))
+                         ::NewtonianEuler::Solutions::SmoothFlow))
 
 GENERATE_INSTANTIATIONS(INSTANTIATION, (2, 3),
-                        (::NewtonianEuler::Solutions::IsentropicVortex,
-                         ::NewtonianEuler::AnalyticData::KhInstability),
-                        (1))
-
-GENERATE_INSTANTIATIONS(INSTANTIATION, (3),
-                        (::NewtonianEuler::Solutions::LaneEmdenStar), (0))
+                        (::NewtonianEuler::AnalyticData::KhInstability))
 
 #undef INSTANTIATION
 #undef SYSTEM
 #undef INITIAL_DATA_TYPE
 #undef DIM
+
+template <size_t Dim>
+using isentropic_vortex_system = ::NewtonianEuler::System<
+    Dim, ::NewtonianEuler::Solutions::IsentropicVortex<Dim>>;
+
+template void volume_terms<::NewtonianEuler::TimeDerivativeTerms<
+    2, ::NewtonianEuler::Solutions::IsentropicVortex<2>>>(
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::dt,
+        typename isentropic_vortex_system<2>::variables_tag::tags_list>>*>
+        dt_vars_ptr,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::Flux, typename isentropic_vortex_system<2>::flux_variables,
+        tmpl::size_t<2>, Frame::Inertial>>*>
+        volume_fluxes,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::deriv, typename isentropic_vortex_system<2>::gradient_variables,
+        tmpl::size_t<2>, Frame::Inertial>>*>
+        partial_derivs,
+    const gsl::not_null<Variables<typename isentropic_vortex_system<
+        2>::compute_volume_time_derivative_terms::temporary_tags>*>
+        temporaries,
+    const Variables<
+        typename isentropic_vortex_system<2>::variables_tag::tags_list>&
+        evolved_vars,
+    const ::dg::Formulation dg_formulation, const Mesh<2>& mesh,
+    [[maybe_unused]] const tnsr::I<DataVector, 2, Frame::Inertial>&
+        inertial_coordinates,
+    const InverseJacobian<DataVector, 2, Frame::ElementLogical,
+                          Frame::Inertial>&
+        logical_to_inertial_inverse_jacobian,
+    [[maybe_unused]] const Scalar<DataVector>* const det_inverse_jacobian,
+    const std::optional<tnsr::I<DataVector, 2, Frame::Inertial>>& mesh_velocity,
+    const std::optional<Scalar<DataVector>>& div_mesh_velocity,
+    const tnsr::I<DataVector, 2>& momentum_density,
+    const Scalar<DataVector>& energy_density,
+    const tnsr::I<DataVector, 2>& velocity, const Scalar<DataVector>& pressure,
+    const NewtonianEuler::Sources::VortexPerturbation<2>& source);
+
+template void volume_terms<::NewtonianEuler::TimeDerivativeTerms<
+    3, ::NewtonianEuler::Solutions::IsentropicVortex<3>>>(
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::dt,
+        typename isentropic_vortex_system<3>::variables_tag::tags_list>>*>
+        dt_vars_ptr,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::Flux, typename isentropic_vortex_system<3>::flux_variables,
+        tmpl::size_t<3>, Frame::Inertial>>*>
+        volume_fluxes,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::deriv, typename isentropic_vortex_system<3>::gradient_variables,
+        tmpl::size_t<3>, Frame::Inertial>>*>
+        partial_derivs,
+    const gsl::not_null<Variables<typename isentropic_vortex_system<
+        3>::compute_volume_time_derivative_terms::temporary_tags>*>
+        temporaries,
+    const Variables<
+        typename isentropic_vortex_system<3>::variables_tag::tags_list>&
+        evolved_vars,
+    const ::dg::Formulation dg_formulation, const Mesh<3>& mesh,
+    [[maybe_unused]] const tnsr::I<DataVector, 3, Frame::Inertial>&
+        inertial_coordinates,
+    const InverseJacobian<DataVector, 3, Frame::ElementLogical,
+                          Frame::Inertial>&
+        logical_to_inertial_inverse_jacobian,
+    [[maybe_unused]] const Scalar<DataVector>* const det_inverse_jacobian,
+    const std::optional<tnsr::I<DataVector, 3, Frame::Inertial>>& mesh_velocity,
+    const std::optional<Scalar<DataVector>>& div_mesh_velocity,
+    const tnsr::I<DataVector, 3>& momentum_density,
+    const Scalar<DataVector>& energy_density,
+    const tnsr::I<DataVector, 3>& velocity, const Scalar<DataVector>& pressure,
+    const NewtonianEuler::Sources::VortexPerturbation<3>& source,
+    const NewtonianEuler::Solutions::IsentropicVortex<3>& vortex,
+    const tnsr::I<DataVector, 3>& x, const double& time);
+
+using lane_emden_star_system =
+    ::NewtonianEuler::System<3, ::NewtonianEuler::Solutions::LaneEmdenStar>;
+
+template void volume_terms<::NewtonianEuler::TimeDerivativeTerms<
+    3, ::NewtonianEuler::Solutions::LaneEmdenStar>>(
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::dt,
+        typename lane_emden_star_system::variables_tag::tags_list>>*>
+        dt_vars_ptr,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::Flux, typename lane_emden_star_system::flux_variables,
+        tmpl::size_t<3>, Frame::Inertial>>*>
+        volume_fluxes,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::deriv, typename lane_emden_star_system::gradient_variables,
+        tmpl::size_t<3>, Frame::Inertial>>*>
+        partial_derivs,
+    const gsl::not_null<
+        Variables<typename lane_emden_star_system::
+                      compute_volume_time_derivative_terms::temporary_tags>*>
+        temporaries,
+    const Variables<typename lane_emden_star_system::variables_tag::tags_list>&
+        evolved_vars,
+    const ::dg::Formulation dg_formulation, const Mesh<3>& mesh,
+    [[maybe_unused]] const tnsr::I<DataVector, 3, Frame::Inertial>&
+        inertial_coordinates,
+    const InverseJacobian<DataVector, 3, Frame::ElementLogical,
+                          Frame::Inertial>&
+        logical_to_inertial_inverse_jacobian,
+    [[maybe_unused]] const Scalar<DataVector>* const det_inverse_jacobian,
+    const std::optional<tnsr::I<DataVector, 3, Frame::Inertial>>& mesh_velocity,
+    const std::optional<Scalar<DataVector>>& div_mesh_velocity,
+    const tnsr::I<DataVector, 3>& momentum_density,
+    const Scalar<DataVector>& energy_density,
+    const tnsr::I<DataVector, 3>& velocity, const Scalar<DataVector>& pressure,
+    const NewtonianEuler::Sources::LaneEmdenGravitationalField& source,
+    const Scalar<DataVector>& mass_density_cons_for_source,
+    const tnsr::I<DataVector, 3>& momentum_density_for_source,
+    const NewtonianEuler::Solutions::LaneEmdenStar& star,
+    const tnsr::I<DataVector, 3>& x);
 }  // namespace evolution::dg::Actions::detail
