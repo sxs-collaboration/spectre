60c7b1d91bfd55bcfce954ca649510547c8ced3e
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-11-22 18:55:13 +0100

    Add base classes for elliptic factory creation
    Tests are added in the next commit.

---
+++ src/PointwiseFunctions/InitialDataUtilities/Background.hpp
@@ -0,0 +1,53 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <pup.h>
+
+#include "Parallel/CharmPupable.hpp"
+
+namespace elliptic::analytic_data {
+/*!
+ * \brief Subclasses supply variable-independent background data for an elliptic
+ * solve.
+ *
+ * Examples for background fields are a background metric, associated curvature
+ * quantities, matter sources such as a mass-density in the XCTS equations, or
+ * just a source function \f$f(x)\f$ in a Poisson equation \f$\Delta u =
+ * f(x)\f$.
+ *
+ * Subclasses must define the following compile-time interface:
+ *
+ * - They are option-creatable.
+ * - They define a `variables` function that provides the fixed-sources in the
+ *   elliptic equations (see `elliptic::protocols::FirstOrderSystem`). The
+ *   function must have this signature:
+ *
+ *   \snippet Test_InitializeFixedSources.cpp background_vars_fct
+ *
+ *   It must support being called with a `tmpl::list` of all system-variable
+ *   tags prefixed with `::Tags::FixedSource`.
+ * - They define a `variables` function that provides data for all background
+ *   quantities, if any are listed in the `background_fields` of the system (see
+ *   `elliptic::protocols::FirstOrderSystem`). The function must have this
+ *   signature:
+ *
+ *   \snippet Test_SubdomainOperator.cpp background_vars_fct_derivs
+ *
+ *   It must support being called with a `tmpl::list` of all background tags. It
+ *   may use the `mesh` and `inv_jacobian` to compute numerical derivatives.
+ */
+class Background : public virtual PUP::able {
+ protected:
+  Background() = default;
+
+ public:
+  ~Background() override = default;
+
+  /// \cond
+  explicit Background(CkMigrateMessage* msg) : PUP::able(msg) {}
+  WRAPPED_PUPable_abstract(Background);
+  /// \endcond
+};
+}  // namespace elliptic::analytic_data
---
+++ src/PointwiseFunctions/InitialDataUtilities/InitialGuess.hpp
@@ -0,0 +1,42 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <pup.h>
+
+#include "Parallel/CharmPupable.hpp"
+
+/// Items related to pointwise analytic data for elliptic solves, such as
+/// initial guesses, analytic solutions, and background quantities in elliptic
+/// PDEs
+namespace elliptic::analytic_data {
+/*!
+ * \brief Subclasses represent an initial guess for an elliptic solve.
+ *
+ * Subclasses must define the following compile-time interface:
+ *
+ * - They are option-creatable.
+ * - They define a `variables` function that can provide data for all variables
+ *   that are being solved for. Specifically the function must have this
+ *   signature:
+ *
+ *   \snippet Test_InitializeFields.cpp initial_guess_vars_fct
+ *
+ *   It must support being called with a `tmpl::list` of all tags that are being
+ *   solved for. For this purpose it can be convenient to template the function
+ *   on the set of requested tags.
+ */
+class InitialGuess : public virtual PUP::able {
+ protected:
+  InitialGuess() = default;
+
+ public:
+  ~InitialGuess() override = default;
+
+  /// \cond
+  explicit InitialGuess(CkMigrateMessage* msg) : PUP::able(msg) {}
+  WRAPPED_PUPable_abstract(InitialGuess);
+  /// \endcond
+};
+}  // namespace elliptic::analytic_data
