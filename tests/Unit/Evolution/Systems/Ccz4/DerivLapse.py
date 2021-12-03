b6023e024e3c7d68318c379d09aec1c73be4c773
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2021-12-02 16:34:21 -0800

    Make Ccz4::divergence_lapse take square of conformal factor as arg

--- tests/Unit/Evolution/Systems/Ccz4/DerivLapse.py
+++ tests/Unit/Evolution/Systems/Ccz4/DerivLapse.py
@@ -11,7 +11,7 @@ def grad_grad_lapse(lapse, christoffel_second_kind, field_a, d_field_a):
             (np.einsum("ij", d_field_a) + np.einsum("ij->ji", d_field_a)))
 
 
-def divergence_lapse(conformal_factor, inverse_conformal_metric,
+def divergence_lapse(conformal_factor_squared, inverse_conformal_metric,
                      grad_grad_lapse):
-    return (conformal_factor * conformal_factor *
+    return (conformal_factor_squared *
             np.einsum("ij,ij", inverse_conformal_metric, grad_grad_lapse))
