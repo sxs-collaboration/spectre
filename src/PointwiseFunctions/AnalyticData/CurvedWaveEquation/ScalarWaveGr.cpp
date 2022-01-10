ce07e751afdad8284fd8451ee3d6cee04d3fbc42
Nikolas Wittek <nikolas.wittek@aei.mpg.de>
2022-01-10 12:16:25 +0100

    Refactor ScalarWaveGr

--- src/PointwiseFunctions/AnalyticData/CurvedWaveEquation/ScalarWaveGr.cpp
+++ src/PointwiseFunctions/AnalyticData/CurvedWaveEquation/ScalarWaveGr.cpp
@@ -1,19 +1,22 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#pragma once
-
 #include "PointwiseFunctions/AnalyticData/CurvedWaveEquation/ScalarWaveGr.hpp"
 
 #include <pup.h>
 
 #include "DataStructures/Tensor/EagerMath/DotProduct.hpp"
 #include "DataStructures/Variables.hpp"
+#include "PointwiseFunctions/AnalyticData/CurvedWaveEquation/PureSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrSchild.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/Minkowski.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/WaveEquation/PlaneWave.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/WaveEquation/RegularSphericalWave.hpp"
 #include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
 #include "Utilities/TMPL.hpp"
 
-/// \cond
-namespace CurvedScalarWave ::AnalyticData {
+namespace CurvedScalarWave::AnalyticData {
 
 template <typename ScalarFieldData, typename BackgroundGrData>
 void ScalarWaveGr<ScalarFieldData, BackgroundGrData>::pup(PUP::er& p) {
@@ -61,5 +64,30 @@ bool operator!=(const ScalarWaveGr<ScalarFieldData, BackgroundData>& lhs,
                 const ScalarWaveGr<ScalarFieldData, BackgroundData>& rhs) {
   return not(lhs == rhs);
 }
+
+// background
+#define BG(data) BOOST_PP_TUPLE_ELEM(0, data)
+// analytic soution/data
+#define SOL(data) BOOST_PP_TUPLE_ELEM(1, data)
+
+#define INSTANTIATE(_, data)                                              \
+  template class ScalarWaveGr<SOL(data), BG(data)>;                       \
+  template bool operator==(const ScalarWaveGr<SOL(data), BG(data)>& lhs,  \
+                           const ScalarWaveGr<SOL(data), BG(data)>& rhs); \
+  template bool operator!=(const ScalarWaveGr<SOL(data), BG(data)>& lhs,  \
+                           const ScalarWaveGr<SOL(data), BG(data)>& rhs);
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (gr::Solutions::Minkowski<1>),
+                        (ScalarWave::Solutions::PlaneWave<1>))
+GENERATE_INSTANTIATIONS(INSTANTIATE, (gr::Solutions::Minkowski<2>),
+                        (ScalarWave::Solutions::PlaneWave<2>))
+GENERATE_INSTANTIATIONS(INSTANTIATE,
+                        (gr::Solutions::Minkowski<3>,
+                         gr::Solutions::KerrSchild),
+                        (ScalarWave::Solutions::PlaneWave<3>,
+                         ScalarWave::Solutions::RegularSphericalWave))
+
+#undef BG
+#undef SOL
+#undef INSTANTIATE
 }  // namespace CurvedScalarWave::AnalyticData
-/// \endcond
