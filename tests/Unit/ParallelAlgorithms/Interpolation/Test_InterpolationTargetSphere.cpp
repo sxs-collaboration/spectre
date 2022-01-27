907db97e147c53689f28cecf01163664ae6e82a2
Nikolas Wittek <nikolas.wittek@aei.mpg.de>
2022-01-27 21:27:23 +0100

    Add Sphere InterpolationTarget

---
+++ tests/Unit/ParallelAlgorithms/Interpolation/Test_InterpolationTargetSphere.cpp
@@ -0,0 +1,115 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstddef>
+#include <vector>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/BlockLogicalCoordinates.hpp"
+#include "Domain/Creators/RegisterDerivedWithCharm.hpp"
+#include "Domain/Creators/Shell.hpp"
+#include "Domain/Domain.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
+#include "Helpers/ParallelAlgorithms/Interpolation/InterpolationTargetTestHelpers.hpp"
+#include "ParallelAlgorithms/Interpolation/Targets/Sphere.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Time/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/Spherepack.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace {
+struct MockMetavariables {
+  struct InterpolationTargetA {
+    using temporal_id = ::Tags::TimeStepId;
+    using vars_to_interpolate_to_target =
+        tmpl::list<gr::Tags::Lapse<DataVector>>;
+    using compute_items_on_target = tmpl::list<>;
+    using compute_target_points =
+        ::intrp::TargetPoints::Sphere<InterpolationTargetA, ::Frame::Inertial>;
+  };
+  static constexpr size_t volume_dim = 3;
+  using interpolator_source_vars = tmpl::list<gr::Tags::Lapse<DataVector>>;
+  using interpolation_target_tags = tmpl::list<InterpolationTargetA>;
+
+  using component_list =
+      tmpl::list<InterpTargetTestHelpers::mock_interpolation_target<
+                     MockMetavariables, InterpolationTargetA>,
+                 InterpTargetTestHelpers::mock_interpolator<MockMetavariables>>;
+  enum class Phase { Initialization, Testing, Exit };
+};
+}  // namespace
+
+void test_interpolation_target_sphere() {
+  const size_t l_max = 18;
+  const double radius = 3.6;
+  const std::array<double, 3> center = {{0.05, 0.06, 0.07}};
+
+  // Options for Sphere
+  intrp::OptionHolders::Sphere sphere_opts(l_max, center, radius);
+
+  // Test creation of options
+  const auto created_opts =
+      TestHelpers::test_creation<intrp::OptionHolders::Sphere>(
+          "Center: [0.05, 0.06, 0.07]\n"
+          "Radius: 3.6\n"
+          "Lmax: 18");
+  CHECK(created_opts == sphere_opts);
+
+  const auto domain_creator =
+      domain::creators::Shell(0.9, 4.9, 1, {{5, 5}}, false);
+
+  TestHelpers::db::test_simple_tag<
+      intrp::Tags::Sphere<MockMetavariables::InterpolationTargetA>>("Sphere");
+
+  const auto expected_block_coord_holders = [&domain_creator, &radius,
+                                             &center]() {
+    // How many points are supposed to be in a Strahlkorper,
+    // reproduced here by hand for the test.
+    const size_t n_theta = l_max + 1;
+    const size_t n_phi = 2 * l_max + 1;
+
+    // The theta points of a Strahlkorper are Gauss-Legendre points.
+    const std::vector<double> theta_points = []() {
+      std::vector<double> thetas(n_theta);
+      std::vector<double> work(n_theta + 1);
+      std::vector<double> unused_weights(n_theta);
+      int err = 0;
+      gaqd_(static_cast<int>(n_theta), thetas.data(), unused_weights.data(),
+            work.data(), static_cast<int>(n_theta + 1), &err);
+      return thetas;
+    }();
+
+    const double two_pi_over_n_phi = 2.0 * M_PI / n_phi;
+    tnsr::I<DataVector, 3, Frame::Inertial> points(n_theta * n_phi);
+    size_t s = 0;
+    for (size_t i_phi = 0; i_phi < n_phi; ++i_phi) {
+      const double phi = two_pi_over_n_phi * i_phi;
+      for (size_t i_theta = 0; i_theta < n_theta; ++i_theta) {
+        const double theta = theta_points[i_theta];
+        points.get(0)[s] = radius * sin(theta) * cos(phi) + center[0];
+        points.get(1)[s] = radius * sin(theta) * sin(phi) + center[1],
+        points.get(2)[s] = radius * cos(theta) + center[2];
+        ++s;
+      }
+    }
+    return block_logical_coordinates(domain_creator.create_domain(), points);
+  }();
+  InterpTargetTestHelpers::test_interpolation_target<
+      MockMetavariables,
+      intrp::Tags::Sphere<MockMetavariables::InterpolationTargetA>>(
+      domain_creator, sphere_opts, expected_block_coord_holders);
+}
+
+SPECTRE_TEST_CASE("Unit.NumericalAlgorithms.InterpolationTarget.Sphere",
+                  "[Unit]") {
+  domain::creators::register_derived_with_charm();
+  test_interpolation_target_sphere();
+}
