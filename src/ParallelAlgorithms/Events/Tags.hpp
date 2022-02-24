1c0e296ea1af6d38b753bcd43cd0eaec6c27ef36
Nils Deppe <nils@nilsdeppe.com>
2022-02-24 11:21:15 -0800

    Add ObserverCoordinates to Events::Tags

--- src/ParallelAlgorithms/Events/Tags.hpp
+++ src/ParallelAlgorithms/Events/Tags.hpp
@@ -4,10 +4,14 @@
 #pragma once
 
 #include <cstddef>
+#include <string>
 
 #include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
 #include "Domain/Tags.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "Utilities/GetOutput.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -31,6 +35,38 @@ struct ObserverMeshCompute : ObserverMesh<Dim>, db::ComputeTag {
   static void function(const gsl::not_null<return_type*> observer_mesh,
                        const ::Mesh<Dim>& mesh) {
     *observer_mesh = mesh;
+  }
+};
+
+/*!
+ * \brief The coordinates used for observation.
+ *
+ * In methods like DG-FD the mesh and coordinates change throughout the
+ * simulation, so we need to always grab the right ones.
+ */
+template <size_t Dim, typename Fr>
+struct ObserverCoordinates : db::SimpleTag {
+  static std::string name() { return get_output(Fr{}) + "Coordinates"; }
+  using type = tnsr::I<DataVector, Dim, Fr>;
+};
+
+/// \brief Sets the `ObserverCoordinates` to `domain::Tags::Coordinates`
+///
+/// This is what you would use for a single numerical method simulation. Hybrid
+/// methods will supply their own tags.
+template <size_t Dim, typename Fr>
+struct ObserverCoordinatesCompute : ObserverCoordinates<Dim, Fr>,
+                                    db::ComputeTag {
+  using base = ObserverCoordinates<Dim, Fr>;
+  using return_type = typename base::type;
+  using argument_tags = tmpl::list<::domain::Tags::Coordinates<Dim, Fr>>;
+  static void function(const gsl::not_null<return_type*> observer_coords,
+                       const return_type& coords) {
+    for (size_t i = 0; i < Dim; ++i) {
+      observer_coords->get(i).set_data_ref(
+          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+          make_not_null(&const_cast<DataVector&>(coords.get(i))));
+    }
   }
 };
 }  // namespace Events::Tags
--- tests/Unit/ParallelAlgorithms/Events/Test_Tags.cpp
+++ tests/Unit/ParallelAlgorithms/Events/Test_Tags.cpp
@@ -6,24 +6,49 @@
 #include <cstddef>
 
 #include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
 #include "Domain/Tags.hpp"
 #include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
 #include "ParallelAlgorithms/Events/Tags.hpp"
+#include "Utilities/Literals.hpp"
 
 namespace {
 template <size_t Dim>
 void test(const Mesh<Dim>& mesh) {
-  const auto box =
-      db::create<db::AddSimpleTags<domain::Tags::Mesh<Dim>>,
-                 db::AddComputeTags<Events::Tags::ObserverMeshCompute<Dim>>>(
-          mesh);
+  const tnsr::I<DataVector, Dim, Frame::Grid> grid_coords{5_st, 7.2};
+  const tnsr::I<DataVector, Dim, Frame::Inertial> inertial_coords{5_st, 7.2};
+  const auto box = db::create<
+      db::AddSimpleTags<domain::Tags::Mesh<Dim>,
+                        domain::Tags::Coordinates<Dim, Frame::Grid>,
+                        domain::Tags::Coordinates<Dim, Frame::Inertial>>,
+      db::AddComputeTags<
+          Events::Tags::ObserverMeshCompute<Dim>,
+          Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Grid>,
+          Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Inertial>>>(
+      mesh, grid_coords, inertial_coords);
   CHECK(db::get<Events::Tags::ObserverMesh<Dim>>(box) == mesh);
+  CHECK(db::get<Events::Tags::ObserverCoordinates<Dim, Frame::Grid>>(box) ==
+        grid_coords);
+  CHECK(db::get<Events::Tags::ObserverCoordinates<Dim, Frame::Inertial>>(box) ==
+        inertial_coords);
   TestHelpers::db::test_simple_tag<Events::Tags::ObserverMesh<Dim>>(
       "ObserverMesh");
   TestHelpers::db::test_compute_tag<Events::Tags::ObserverMeshCompute<Dim>>(
       "ObserverMesh");
+  TestHelpers::db::test_simple_tag<
+      Events::Tags::ObserverCoordinates<Dim, Frame::Grid>>("GridCoordinates");
+  TestHelpers::db::test_compute_tag<
+      Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Grid>>(
+      "GridCoordinates");
+  TestHelpers::db::test_simple_tag<
+      Events::Tags::ObserverCoordinates<Dim, Frame::Inertial>>(
+      "InertialCoordinates");
+  TestHelpers::db::test_compute_tag<
+      Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Inertial>>(
+      "InertialCoordinates");
 }
 }  // namespace
 
