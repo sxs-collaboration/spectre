001ab3b3379b3767d2e27e532a2c2ee1c43ecf14
Nils Vu <owls@nilsvu.de>
2022-03-07 16:46:48 +0100

    Use 'click' to build Python CLI
    - Compose the CLI from subcommands defined in other files.
    - Load subcommands lazily, i.e., only import the module when the
      subcommand invoked. This is important so the CLI responds quickly.
    - Enable autocompletion for the CLI

--- support/Python/__main__.py
+++ support/Python/__main__.py
@@ -1,27 +1,29 @@
 # Distributed under the MIT License.
 # See LICENSE.txt for details.
 
+import click
+
 SPECTRE_VERSION = "@SPECTRE_VERSION@"
 
 
-def main():
-    import argparse
+# Load subcommands lazily, i.e., only import the module when the subcommand is
+# invoked. This is important so the CLI responds quickly.
+class Cli(click.MultiCommand):
+    def list_commands(self, ctx):
+        return []
 
-    class HelpFormatter(argparse.ArgumentDefaultsHelpFormatter,
-                        argparse.RawDescriptionHelpFormatter):
-        pass
-
-    parser = argparse.ArgumentParser(
-        prog='spectre',
-        description="SpECTRE version: {}".format(SPECTRE_VERSION),
-        formatter_class=HelpFormatter)
-
-    # Version endpoint
-    parser.add_argument('--version', action='version', version=SPECTRE_VERSION)
-
-    parser.parse_args()
-    raise NotImplementedError("No subprograms are implemented yet.")
+    def get_command(self, ctx, name):
+        raise NotImplementedError(f"The command '{name}' is not implemented.")
 
 
-if __name__ == '__main__':
-    main()
+# Set up CLI entry point
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]),
+             help=f"SpECTRE version: {SPECTRE_VERSION}",
+             cls=Cli)
+@click.version_option(version=SPECTRE_VERSION, message="%(version)s")
+def cli():
+    pass
+
+
+if __name__ == "__main__":
+    cli(prog_name="spectre")
