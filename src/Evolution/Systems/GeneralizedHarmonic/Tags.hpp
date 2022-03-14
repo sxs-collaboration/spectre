4e21b64da7ad6b719ab1f4f822e6e4929db186ea
Kyle Nelli <knelli@caltech.edu>
2022-03-14 08:46:02 -0700

    Fix GH Phi tag docs

--- src/Evolution/Systems/GeneralizedHarmonic/Tags.hpp
+++ src/Evolution/Systems/GeneralizedHarmonic/Tags.hpp
@@ -22,8 +22,8 @@ namespace Tags {
  *
  * \details If \f$ \psi_{ab} \f$ is the spacetime metric, and \f$ N \f$ and
  * \f$ N^i \f$ are the lapse and shift respectively, then we define
- * \f$ \Pi_{ab} = -\frac{1}{N} ( \partial_t \psi_{ab} + N^{i} \phi_{iab} ) \f$
- * where \f$\phi_{iab}\f$ is the variable defined by the tag Phi.
+ * \f$ \Pi_{ab} = -\frac{1}{N} ( \partial_t \psi_{ab} + N^{i} \Phi_{iab} ) \f$
+ * where \f$\Phi_{iab}\f$ is the variable defined by the tag Phi.
  */
 template <size_t Dim, typename Frame>
 struct Pi : db::SimpleTag {
@@ -34,7 +34,7 @@ struct Pi : db::SimpleTag {
  * \brief Auxiliary variable which is analytically the spatial derivative of the
  * spacetime metric
  * \details If \f$\psi_{ab}\f$ is the spacetime metric then we define
- * \f$\phi_{iab} = \partial_i \psi_{ab}\f$
+ * \f$\Phi_{iab} = \partial_i \psi_{ab}\f$
  */
 template <size_t Dim, typename Frame>
 struct Phi : db::SimpleTag {
