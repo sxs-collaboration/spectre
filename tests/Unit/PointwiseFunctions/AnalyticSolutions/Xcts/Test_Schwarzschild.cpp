39a3007df3680e66ba4ef0cc8211d346ecc18cff
Nils Vu <owls@nilsvu.de>
2022-03-16 20:51:51 +0100

    Combine some tests to speed them up
    The combined call into the analytic solution is faster because it reuses
    intermediate computations.
    
    To be safe, also increase the timeout because this test has timed out
    on CI.

--- tests/Unit/PointwiseFunctions/AnalyticSolutions/Xcts/Test_Schwarzschild.cpp
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/Xcts/Test_Schwarzschild.cpp
@@ -27,23 +27,20 @@
 namespace Xcts::Solutions {
 namespace {
 
-using field_tags =
-    tmpl::list<Tags::ConformalFactor<DataVector>,
-               Tags::LapseTimesConformalFactor<DataVector>,
-               Tags::ShiftExcess<DataVector, 3, Frame::Inertial>>;
-using auxiliary_field_tags =
-    tmpl::list<::Tags::deriv<Tags::ConformalFactor<DataVector>, tmpl::size_t<3>,
-                             Frame::Inertial>,
-               ::Tags::deriv<Tags::LapseTimesConformalFactor<DataVector>,
-                             tmpl::size_t<3>, Frame::Inertial>,
-               Tags::ShiftStrain<DataVector, 3, Frame::Inertial>>;
-using flux_tags =
-    tmpl::list<::Tags::Flux<Tags::ConformalFactor<DataVector>, tmpl::size_t<3>,
-                            Frame::Inertial>,
-               ::Tags::Flux<Tags::LapseTimesConformalFactor<DataVector>,
-                            tmpl::size_t<3>, Frame::Inertial>,
-               Tags::LongitudinalShiftExcess<DataVector, 3, Frame::Inertial>>;
-using background_tags = tmpl::list<
+using py_test_tags = tmpl::list<
+    Tags::ConformalFactor<DataVector>,
+    Tags::LapseTimesConformalFactor<DataVector>,
+    Tags::ShiftExcess<DataVector, 3, Frame::Inertial>,
+    ::Tags::deriv<Tags::ConformalFactor<DataVector>, tmpl::size_t<3>,
+                  Frame::Inertial>,
+    ::Tags::deriv<Tags::LapseTimesConformalFactor<DataVector>, tmpl::size_t<3>,
+                  Frame::Inertial>,
+    Tags::ShiftStrain<DataVector, 3, Frame::Inertial>,
+    ::Tags::Flux<Tags::ConformalFactor<DataVector>, tmpl::size_t<3>,
+                 Frame::Inertial>,
+    ::Tags::Flux<Tags::LapseTimesConformalFactor<DataVector>, tmpl::size_t<3>,
+                 Frame::Inertial>,
+    Tags::LongitudinalShiftExcess<DataVector, 3, Frame::Inertial>,
     Tags::ConformalMetric<DataVector, 3, Frame::Inertial>,
     Tags::InverseConformalMetric<DataVector, 3, Frame::Inertial>,
     ::Tags::deriv<Tags::ConformalMetric<DataVector, 3, Frame::Inertial>,
@@ -53,47 +50,22 @@ using background_tags = tmpl::list<
                   tmpl::size_t<3>, Frame::Inertial>,
     Tags::ShiftBackground<DataVector, 3, Frame::Inertial>,
     Tags::LongitudinalShiftBackgroundMinusDtConformalMetric<DataVector, 3,
-                                                            Frame::Inertial>>;
-using matter_source_tags = tmpl::list<
+                                                            Frame::Inertial>,
     gr::Tags::Conformal<gr::Tags::EnergyDensity<DataVector>, 0>,
     gr::Tags::Conformal<gr::Tags::StressTrace<DataVector>, 0>,
     gr::Tags::Conformal<
-        gr::Tags::MomentumDensity<3, Frame::Inertial, DataVector>, 0>>;
-using gr_tags = tmpl::list<gr::Tags::Lapse<DataVector>,
-                           gr::Tags::Shift<3, Frame::Inertial, DataVector>>;
-using derived_tags = tmpl::list<
+        gr::Tags::MomentumDensity<3, Frame::Inertial, DataVector>, 0>,
+    gr::Tags::Lapse<DataVector>,
+    gr::Tags::Shift<3, Frame::Inertial, DataVector>,
     Tags::ShiftDotDerivExtrinsicCurvatureTrace<DataVector>,
     Tags::LongitudinalShiftMinusDtConformalMetricSquare<DataVector>,
     Tags::LongitudinalShiftMinusDtConformalMetricOverLapseSquare<DataVector>>;
 
 struct SchwarzschildProxy : Xcts::Solutions::Schwarzschild {
   using Xcts::Solutions::Schwarzschild::Schwarzschild;
-  tuples::tagged_tuple_from_typelist<
-      tmpl::append<field_tags, auxiliary_field_tags>>
-  field_variables(const tnsr::I<DataVector, 3, Frame::Inertial>& x) const {
-    return Xcts::Solutions::Schwarzschild::variables(
-        x, tmpl::append<field_tags, auxiliary_field_tags>{});
-  }
-  tuples::tagged_tuple_from_typelist<flux_tags> flux_variables(
+  tuples::tagged_tuple_from_typelist<py_test_tags> py_test_variables(
       const tnsr::I<DataVector, 3, Frame::Inertial>& x) const {
-    return Xcts::Solutions::Schwarzschild::variables(x, flux_tags{});
-  }
-  tuples::tagged_tuple_from_typelist<background_tags> background_variables(
-      const tnsr::I<DataVector, 3, Frame::Inertial>& x) const {
-    return Xcts::Solutions::Schwarzschild::variables(x, background_tags{});
-  }
-  tuples::tagged_tuple_from_typelist<matter_source_tags>
-  matter_source_variables(
-      const tnsr::I<DataVector, 3, Frame::Inertial>& x) const {
-    return Xcts::Solutions::Schwarzschild::variables(x, matter_source_tags{});
-  }
-  tuples::tagged_tuple_from_typelist<gr_tags> gr_variables(
-      const tnsr::I<DataVector, 3, Frame::Inertial>& x) const {
-    return Xcts::Solutions::Schwarzschild::variables(x, gr_tags{});
-  }
-  tuples::tagged_tuple_from_typelist<derived_tags> derived_variables(
-      const tnsr::I<DataVector, 3, Frame::Inertial>& x) const {
-    return Xcts::Solutions::Schwarzschild::variables(x, derived_tags{});
+    return Xcts::Solutions::Schwarzschild::variables(x, py_test_tags{});
   }
 };
 
@@ -137,34 +109,29 @@ void test_solution(const double mass,
     const double inner_radius = expected_radius_at_horizon;
     const double outer_radius = 3. * expected_radius_at_horizon;
     pypp::check_with_random_values<1>(
-        &SchwarzschildProxy::field_variables, solution_proxy, py_module,
-        {"conformal_factor", "lapse_times_conformal_factor", "shift",
-         "conformal_factor_gradient", "lapse_times_conformal_factor_gradient",
-         "shift_strain"},
-        {{{inner_radius, outer_radius}}}, std::make_tuple(mass), DataVector(5));
-    pypp::check_with_random_values<1>(
-        &SchwarzschildProxy::flux_variables, solution_proxy, py_module,
-        {"conformal_factor_gradient", "lapse_times_conformal_factor_gradient",
-         "longitudinal_shift"},
-        {{{inner_radius, outer_radius}}}, std::make_tuple(mass), DataVector(5));
-    pypp::check_with_random_values<1>(
-        &SchwarzschildProxy::background_variables, solution_proxy, py_module,
-        {"conformal_metric", "inv_conformal_metric", "deriv_conformal_metric",
-         "extrinsic_curvature_trace", "extrinsic_curvature_trace_gradient",
+        &SchwarzschildProxy::py_test_variables, solution_proxy, py_module,
+        {"conformal_factor",
+         "lapse_times_conformal_factor",
+         "shift",
+         "conformal_factor_gradient",
+         "lapse_times_conformal_factor_gradient",
+         "shift_strain",
+         "conformal_factor_gradient",
+         "lapse_times_conformal_factor_gradient",
+         "longitudinal_shift",
+         "conformal_metric",
+         "inv_conformal_metric",
+         "deriv_conformal_metric",
+         "extrinsic_curvature_trace",
+         "extrinsic_curvature_trace_gradient",
          "shift_background",
-         "longitudinal_shift_background_minus_dt_conformal_metric"},
-        {{{inner_radius, outer_radius}}}, std::make_tuple(mass), DataVector(5));
-    pypp::check_with_random_values<1>(
-        &SchwarzschildProxy::matter_source_variables, solution_proxy, py_module,
-        {"energy_density", "stress_trace", "momentum_density"},
-        {{{inner_radius, outer_radius}}}, std::make_tuple(mass), DataVector(5));
-    pypp::check_with_random_values<1>(
-        &SchwarzschildProxy::gr_variables, solution_proxy, py_module,
-        {"lapse", "shift"}, {{{inner_radius, outer_radius}}},
-        std::make_tuple(mass), DataVector(5));
-    pypp::check_with_random_values<1>(
-        &SchwarzschildProxy::derived_variables, solution_proxy, py_module,
-        {"shift_dot_extrinsic_curvature_trace_gradient",
+         "longitudinal_shift_background_minus_dt_conformal_metric",
+         "energy_density",
+         "stress_trace",
+         "momentum_density",
+         "lapse",
+         "shift",
+         "shift_dot_extrinsic_curvature_trace_gradient",
          "longitudinal_shift_minus_dt_conformal_metric_square",
          "longitudinal_shift_minus_dt_conformal_metric_over_lapse_square"},
         {{{inner_radius, outer_radius}}}, std::make_tuple(mass), DataVector(5));
@@ -181,6 +148,7 @@ void test_solution(const double mass,
 
 }  // namespace
 
+// [[Timeout, 8]]
 SPECTRE_TEST_CASE(
     "Unit.PointwiseFunctions.AnalyticSolutions.Xcts.Schwarzschild",
     "[PointwiseFunctions][Unit]") {
