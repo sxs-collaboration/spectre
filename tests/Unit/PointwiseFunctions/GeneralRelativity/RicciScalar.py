7cf0a6932d7a5db757053feb3ab8fd39b1fa13d4
Alex Carpenter <alexanderhcarpenter46@gmail.com>
2022-04-06 11:21:21 -0700

    Add RicciScalar to GeneralRelativity

---
+++ tests/Unit/PointwiseFunctions/GeneralRelativity/RicciScalar.py
@@ -0,0 +1,8 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+import numpy as np
+
+
+def ricci_scalar(ricci_tensor, inverse_metric):
+    ricci_up_down = (np.einsum("cb,ac", ricci_tensor, inverse_metric))
+    return np.einsum("aa", ricci_up_down)
