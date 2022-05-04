14949afb4c8612fa8154b34829009d253174df00
Yoonsoo Kim <ykim7@caltech.edu>
2022-05-04 09:23:17 -0700

    fixup

--- src/Evolution/DgSubcell/SliceVariable.hpp
+++ src/Evolution/DgSubcell/SliceVariable.hpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <vector>
 
 #include "DataStructures/Index.hpp"
 #include "DataStructures/Variables.hpp"
@@ -18,13 +19,9 @@ namespace evolution::dg::subcell {
 /*!
  * \brief Slice a Variables object on subcell mesh for a given direction and
  * slicing depth (number of ghost points).
- *
- * This is essentially a wrapper of
- * `evolution::dg::subcell::detail::slice_data_impl` function, but note that the
- * last argument has the type `Direction<Dim>`, not a DirectionMap.
  */
 template <size_t Dim, typename TagList>
-void slice_variable_for_subcell(
+void slice_variable(
     const gsl::not_null<Variables<TagList>*>& sliced_subcell_vars,
     const Variables<TagList>& volume_subcell_vars,
     const Index<Dim>& subcell_extents, const size_t ghost_zone_size,
@@ -34,15 +31,15 @@ void slice_variable_for_subcell(
       subcell_extents.slice_away(direction.dimension()).product() *
       ghost_zone_size;
   if (sliced_subcell_vars->size() != num_sliced_pts) {
-    *sliced_subcell_vars = Variables<TagList>(num_sliced_pts);
+    sliced_subcell_vars->initialize(num_sliced_pts);
   }
 
-  // Slice volume variables. Note that the return type of slicing function is
-  // std::vector<double>.
+  // Slice volume variables. Note that the return type of the
+  // `slice_data_impl()` function is std::vector<double>.
   DirectionMap<Dim, bool> directions_to_slice{};
   directions_to_slice[direction] = true;
 
-  std::vector<double> sliced_data{detail::slice_data_impl(
+  const std::vector<double> sliced_data{detail::slice_data_impl(
       gsl::make_span(volume_subcell_vars.data(), volume_subcell_vars.size()),
       subcell_extents, ghost_zone_size, directions_to_slice)[direction]};
 
@@ -56,13 +53,11 @@ Variables<TagList> slice_variable(const Variables<TagList>& volume_subcell_vars,
                                   const Index<Dim>& subcell_extents,
                                   const size_t ghost_zone_size,
                                   const Direction<Dim>& direction) {
-  Variables<TagList> sliced_subcell_vars{};
-  sliced_subcell_vars.initialize(
+  Variables<TagList> sliced_subcell_vars{
       subcell_extents.slice_away(direction.dimension()).product() *
-      ghost_zone_size);
-  slice_variable_for_subcell(make_not_null(&sliced_subcell_vars),
-                             volume_subcell_vars, subcell_extents,
-                             ghost_zone_size, direction);
+      ghost_zone_size};
+  slice_variable(make_not_null(&sliced_subcell_vars), volume_subcell_vars,
+                 subcell_extents, ghost_zone_size, direction);
   return sliced_subcell_vars;
 }
 /// @}
