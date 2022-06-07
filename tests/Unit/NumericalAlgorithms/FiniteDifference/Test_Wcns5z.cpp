1369158884041c7d65ffe48d9efbaf9f73b989f2
Yoonsoo Kim <ykim7@caltech.edu>
2022-06-06 17:44:36 -0700

    Enable Wcns5z reconstruction to fall back adaptively
    Extend the Wcns5z reconstruction class with an additional template
    parameter `FallbackReconstructor`, which enables switching to an
    auxiliary reconstruction method depending on how many extrema are in
    a given FD stencil.

--- tests/Unit/NumericalAlgorithms/FiniteDifference/Test_Wcns5z.cpp
+++ tests/Unit/NumericalAlgorithms/FiniteDifference/Test_Wcns5z.cpp
@@ -14,40 +14,45 @@
 #include "Framework/SetupLocalPythonEnvironment.hpp"
 #include "Helpers/NumericalAlgorithms/FiniteDifference/Exact.hpp"
 #include "Helpers/NumericalAlgorithms/FiniteDifference/Python.hpp"
+#include "NumericalAlgorithms/FiniteDifference/FallbackReconstructorType.hpp"
+#include "NumericalAlgorithms/FiniteDifference/Minmod.hpp"
+#include "NumericalAlgorithms/FiniteDifference/MonotonisedCentral.hpp"
 #include "NumericalAlgorithms/FiniteDifference/Wcns5z.hpp"
 #include "Utilities/Gsl.hpp"
 
+namespace fd::reconstruction {
 namespace {
 
-template <size_t NonlinearWeightExponent, size_t Dim>
-void test_function_pointers() {
+template <size_t Dim, size_t NonlinearWeightExponent,
+          typename FallbackReconstructor>
+void test_function_pointers(const FallbackReconstructorType fallback_recons) {
   const auto function_ptrs = fd::reconstruction::wcns5z_function_pointers<Dim>(
-      NonlinearWeightExponent);
+      NonlinearWeightExponent, fallback_recons);
   CHECK(get<0>(function_ptrs) ==
-        &fd::reconstruction::wcns5z<NonlinearWeightExponent, Dim>);
+        &wcns5z<NonlinearWeightExponent, FallbackReconstructor, Dim>);
   using function_type =
       void (*)(gsl::not_null<DataVector*>, const DataVector&, const DataVector&,
                const Index<Dim>&, const Index<Dim>&, const Direction<Dim>&,
-               const double&);
+               const double&, const size_t&);
   CHECK(get<1>(function_ptrs) ==
         static_cast<function_type>(
-            &fd::reconstruction::reconstruct_neighbor<
+            &reconstruct_neighbor<
                 Side::Lower,
                 ::fd::reconstruction::detail::Wcns5zReconstructor<
-                    NonlinearWeightExponent>,
+                    NonlinearWeightExponent, FallbackReconstructor>,
                 Dim>));
   CHECK(get<2>(function_ptrs) ==
         static_cast<function_type>(
-            &fd::reconstruction::reconstruct_neighbor<
+            &reconstruct_neighbor<
                 Side::Upper,
                 ::fd::reconstruction::detail::Wcns5zReconstructor<
-                    NonlinearWeightExponent>,
+                    NonlinearWeightExponent, FallbackReconstructor>,
                 Dim>));
 }
 
-template <size_t Dim>
-void test() {
-  // test for q=2 case
+template <size_t Dim, class FallbackReconstructor>
+void test(const FallbackReconstructorType fallback_recons) {
+  // test for NonlinearWeightExponent = 2 case
 
   const auto recons =
       [](const gsl::not_null<std::array<gsl::span<double>, Dim>*>
@@ -57,46 +62,73 @@ void test() {
          const gsl::span<const double>& volume_vars,
          const DirectionMap<Dim, gsl::span<const double>>& ghost_cell_vars,
          const Index<Dim>& volume_extents, const size_t number_of_variables) {
+        const size_t max_number_of_extrema = 1;
         const double epsilon = 2.0e-16;
 
-        fd::reconstruction::wcns5z<2>(
+        fd::reconstruction::wcns5z<2, FallbackReconstructor>(
             reconstructed_upper_side_of_face_vars,
             reconstructed_lower_side_of_face_vars, volume_vars, ghost_cell_vars,
-            volume_extents, number_of_variables, epsilon);
+            volume_extents, number_of_variables, epsilon,
+            max_number_of_extrema);
       };
   const auto recons_neighbor_data =
       [](const gsl::not_null<DataVector*> face_data,
          const DataVector& volume_data, const DataVector& neighbor_data,
          const Index<Dim>& volume_extents, const Index<Dim>& ghost_data_extents,
          const Direction<Dim>& direction_to_reconstruct) {
+        const size_t max_number_of_extrema = 1;
         const double epsilon = 2.0e-16;
 
         if (direction_to_reconstruct.side() == Side::Upper) {
           fd::reconstruction::reconstruct_neighbor<
-              Side::Upper, fd::reconstruction::detail::Wcns5zReconstructor<2>>(
+              Side::Upper, fd::reconstruction::detail::Wcns5zReconstructor<
+                               2, FallbackReconstructor>>(
               face_data, volume_data, neighbor_data, volume_extents,
-              ghost_data_extents, direction_to_reconstruct, epsilon);
+              ghost_data_extents, direction_to_reconstruct, epsilon,
+              max_number_of_extrema);
         }
         if (direction_to_reconstruct.side() == Side::Lower) {
           fd::reconstruction::reconstruct_neighbor<
-              Side::Lower, fd::reconstruction::detail::Wcns5zReconstructor<2>>(
+              Side::Lower, fd::reconstruction::detail::Wcns5zReconstructor<
+                               2, FallbackReconstructor>>(
               face_data, volume_data, neighbor_data, volume_extents,
-              ghost_data_extents, direction_to_reconstruct, epsilon);
+              ghost_data_extents, direction_to_reconstruct, epsilon,
+              max_number_of_extrema);
         }
       };
 
+  // if there are more than one extrema in any of finite difference stencils,
+  // reconstruction would be switched to FallbackReconstructor which may be only
+  // exact up to the polynomial of degree 1. However, if we are testing with a
+  // single (global) degree 2 polynomial (which is used in
+  // `test_reconstruction_is_exact_if_in_basis`) there cannot be more than one
+  // extrema. So we are safe to use degree 2 case for our test here in order to
+  // confirm that Wcns5z + MC reconstruction _does_ reproduce the accuracy of
+  // Wcns5z if not switched to MC.
   TestHelpers::fd::reconstruction::test_reconstruction_is_exact_if_in_basis<
       Dim>(2, 5, 5, recons, recons_neighbor_data);
 
-  TestHelpers::fd::reconstruction::test_with_python(
-      Index<Dim>{5}, 5, "Wcns5z", "test_wcns5z", recons, recons_neighbor_data);
+  if (fallback_recons == FallbackReconstructorType::Minmod) {
+    TestHelpers::fd::reconstruction::test_with_python(
+        Index<Dim>{5}, 5, "Wcns5z", "test_wcns5z_with_minmod", recons,
+        recons_neighbor_data);
+  } else if (fallback_recons == FallbackReconstructorType::MonotonisedCentral) {
+    TestHelpers::fd::reconstruction::test_with_python(
+        Index<Dim>{5}, 5, "Wcns5z", "test_wcns5z_with_mc", recons,
+        recons_neighbor_data);
+  } else {
+    TestHelpers::fd::reconstruction::test_with_python(
+        Index<Dim>{5}, 5, "Wcns5z", "test_wcns5z", recons,
+        recons_neighbor_data);
+  }
 
-  test_function_pointers<1, Dim>();
-  test_function_pointers<2, Dim>();
+  test_function_pointers<Dim, 2, FallbackReconstructor>(fallback_recons);
 
   // check for failing case (nonlinear weight exponent = 3)
   CHECK_THROWS_WITH(
-      ([]() { ::fd::reconstruction::wcns5z_function_pointers<Dim>(3); })(),
+      ([&fallback_recons]() {
+        ::fd::reconstruction::wcns5z_function_pointers<Dim>(3, fallback_recons);
+      })(),
       Catch::Contains("Nonlinear weight exponent should be 1 or 2"));
 }
 }  // namespace
@@ -105,7 +137,21 @@ SPECTRE_TEST_CASE("Unit.FiniteDifference.Wcns5z",
                   "[Unit][NumericalAlgorithms]") {
   pypp::SetupLocalPythonEnvironment local_python_env(
       "NumericalAlgorithms/FiniteDifference/");
-  test<1>();
-  test<2>();
-  test<3>();
+
+  test<1, detail::MinmodReconstructor>(FallbackReconstructorType::Minmod);
+  test<1, detail::MonotonisedCentralReconstructor>(
+      FallbackReconstructorType::MonotonisedCentral);
+  test<1, void>(FallbackReconstructorType::None);
+
+  test<2, detail::MinmodReconstructor>(FallbackReconstructorType::Minmod);
+  test<2, detail::MonotonisedCentralReconstructor>(
+      FallbackReconstructorType::MonotonisedCentral);
+  test<2, void>(FallbackReconstructorType::None);
+
+  test<3, detail::MinmodReconstructor>(FallbackReconstructorType::Minmod);
+  test<3, detail::MonotonisedCentralReconstructor>(
+      FallbackReconstructorType::MonotonisedCentral);
+  test<3, void>(FallbackReconstructorType::None);
 }
+
+}  // namespace fd::reconstruction
--- tests/Unit/NumericalAlgorithms/FiniteDifference/Wcns5z.py
+++ tests/Unit/NumericalAlgorithms/FiniteDifference/Wcns5z.py
@@ -2,70 +2,137 @@
 # See LICENSE.txt for details.
 
 import numpy as np
+import scipy.signal
+
 import Reconstruction
 
-
-def wcns5z_oneside(q, s):
-    j = 2
-
-    b0 = 0.25 * (q[j - 2 * s] - 4.0 * q[j - s] + 3.0 * q[j])**2 + (
-        13.0 / 12.0) * (q[j - 2 * s] - 2.0 * q[j - s] + q[j])**2
-    b1 = 0.25 * (q[j + s] - q[j - s])**2 + (13.0 / 12.0) * (
-        q[j - s] + q[j + s] - 2.0 * q[j])**2
-    b2 = 0.25 * (q[j + 2 * s] - 4.0 * q[j + s] + 3.0 * q[j])**2 + (
-        13.0 / 12.0) * (q[j + 2 * s] - 2.0 * q[j + s] + q[j])**2
-
-    e0 = 2e-16 * (1.0 + abs(q[j - 2 * s]) + abs(q[j - s]) + abs(q[j]))
-    e1 = 2e-16 * (1.0 + abs(q[j - s]) + abs(q[j]) + abs(q[j + s]))
-    e2 = 2e-16 * (1.0 + abs(q[j + 2 * s]) + abs(q[j + s]) + abs(q[j]))
-
-    beta = np.asarray([b0, b1, b2])
-    epsilon = np.asarray([e0, e1, e2])
-    tau5 = np.abs(b2 - b0)
-
-    alpha = np.asarray([1, 10, 5]) / 16.0 * (1.0 + (tau5 /
-                                                    (beta + epsilon))**2)
-    nw = alpha / np.sum(alpha)
-
-    recons_stencils = np.asarray([
-        3.0 * q[j - 2 * s] - 10.0 * q[j - s] + 15.0 * q[j], -q[j - s] +
-        6.0 * q[j] + 3.0 * q[j + s], 3.0 * q[j] + 6.0 * q[j + s] - q[j + 2 * s]
-    ]) / 8.0
-
-    return np.sum(nw * recons_stencils)
+import Minmod
+import MonotonisedCentral
 
 
 def wcns5z(q):
+    def wcns5z_oneside(q, s):
+        j = 2
+
+        b0 = 0.25 * (q[j - 2 * s] - 4.0 * q[j - s] + 3.0 * q[j])**2 + (
+            13.0 / 12.0) * (q[j - 2 * s] - 2.0 * q[j - s] + q[j])**2
+        b1 = 0.25 * (q[j + s] - q[j - s])**2 + (13.0 / 12.0) * (
+            q[j - s] + q[j + s] - 2.0 * q[j])**2
+        b2 = 0.25 * (q[j + 2 * s] - 4.0 * q[j + s] + 3.0 * q[j])**2 + (
+            13.0 / 12.0) * (q[j + 2 * s] - 2.0 * q[j + s] + q[j])**2
+
+        e0 = 2e-16 * (1.0 + abs(q[j - 2 * s]) + abs(q[j - s]) + abs(q[j]))
+        e1 = 2e-16 * (1.0 + abs(q[j - s]) + abs(q[j]) + abs(q[j + s]))
+        e2 = 2e-16 * (1.0 + abs(q[j + 2 * s]) + abs(q[j + s]) + abs(q[j]))
+
+        beta = np.asarray([b0, b1, b2])
+        epsilon = np.asarray([e0, e1, e2])
+        tau5 = np.abs(b2 - b0)
+
+        alpha = np.asarray([1, 10, 5]) / 16.0 * (1.0 + (tau5 /
+                                                        (beta + epsilon))**2)
+        nw = alpha / np.sum(alpha)
+
+        recons_stencils = np.asarray([
+            3.0 * q[j - 2 * s] - 10.0 * q[j - s] + 15.0 * q[j],
+            -q[j - s] + 6.0 * q[j] + 3.0 * q[j + s],
+            3.0 * q[j] + 6.0 * q[j + s] - q[j + 2 * s]
+        ]) / 8.0
+
+        return np.sum(nw * recons_stencils)
+
     return [wcns5z_oneside(q, -1), wcns5z_oneside(q, 1)]
+
+
+def compute_face_values_t(recons_upper_of_cell, recons_lower_of_cell, v, i, j,
+                          k, dim_to_recons, wcns5z_type):
+    if dim_to_recons == 0:
+        lower, upper = wcns5z_type(
+            np.asarray([
+                v[i - 2, j, k], v[i - 1, j, k], v[i, j, k], v[i + 1, j, k],
+                v[i + 2, j, k]
+            ]))
+        recons_lower_of_cell.append(lower)
+        recons_upper_of_cell.append(upper)
+    if dim_to_recons == 1:
+        lower, upper = wcns5z_type(
+            np.asarray([
+                v[i, j - 2, k], v[i, j - 1, k], v[i, j, k], v[i, j + 1, k],
+                v[i, j + 2, k]
+            ]))
+        recons_lower_of_cell.append(lower)
+        recons_upper_of_cell.append(upper)
+    if dim_to_recons == 2:
+        lower, upper = wcns5z_type(
+            np.asarray([
+                v[i, j, k - 2], v[i, j, k - 1], v[i, j, k], v[i, j, k + 1],
+                v[i, j, k + 2]
+            ]))
+        recons_lower_of_cell.append(lower)
+        recons_upper_of_cell.append(upper)
 
 
 def test_wcns5z(u, extents, dim):
     def compute_face_values(recons_upper_of_cell, recons_lower_of_cell, v, i,
                             j, k, dim_to_recons):
-        if dim_to_recons == 0:
-            lower, upper = wcns5z(
-                np.asarray([
-                    v[i - 2, j, k], v[i - 1, j, k], v[i, j, k], v[i + 1, j, k],
-                    v[i + 2, j, k]
-                ]))
-            recons_lower_of_cell.append(lower)
-            recons_upper_of_cell.append(upper)
-        if dim_to_recons == 1:
-            lower, upper = wcns5z(
-                np.asarray([
-                    v[i, j - 2, k], v[i, j - 1, k], v[i, j, k], v[i, j + 1, k],
-                    v[i, j + 2, k]
-                ]))
-            recons_lower_of_cell.append(lower)
-            recons_upper_of_cell.append(upper)
-        if dim_to_recons == 2:
-            lower, upper = wcns5z(
-                np.asarray([
-                    v[i, j, k - 2], v[i, j, k - 1], v[i, j, k], v[i, j, k + 1],
-                    v[i, j, k + 2]
-                ]))
-            recons_lower_of_cell.append(lower)
-            recons_upper_of_cell.append(upper)
+        return compute_face_values_t(recons_upper_of_cell,
+                                     recons_lower_of_cell, v, i, j, k,
+                                     dim_to_recons, wcns5z)
+
+    return Reconstruction.reconstruct(u, extents, dim, [2, 2, 2],
+                                      compute_face_values)
+
+
+def test_wcns5z_with_minmod(u, extents, dim):
+    def wcns5z_with_minmod(q):
+        j = 2
+
+        # indices of local maxima
+        idx_local_max = scipy.signal.argrelextrema(q, np.greater)[0]
+        # indices of local minima
+        idx_local_min = scipy.signal.argrelextrema(q, np.less)[0]
+        # number of local maxima and minima
+        n_extrema = len(idx_local_max) + len(idx_local_min)
+
+        if (n_extrema <= 1):
+            return wcns5z(q)
+        else:
+            slope = Minmod.minmod(q[j] - q[j - 1], q[j + 1] - q[j])
+            return [q[j] - 0.5 * slope, q[j] + 0.5 * slope]
+
+    def compute_face_values(recons_upper_of_cell, recons_lower_of_cell, v, i,
+                            j, k, dim_to_recons):
+        return compute_face_values_t(recons_upper_of_cell,
+                                     recons_lower_of_cell, v, i, j, k,
+                                     dim_to_recons, wcns5z_with_minmod)
+
+    return Reconstruction.reconstruct(u, extents, dim, [2, 2, 2],
+                                      compute_face_values)
+
+
+def test_wcns5z_with_mc(u, extents, dim):
+    def wcns5z_with_mc(q):
+        j = 2
+
+        # indices of local maxima
+        idx_local_max = scipy.signal.argrelextrema(q, np.greater)[0]
+        # indices of local minima
+        idx_local_min = scipy.signal.argrelextrema(q, np.less)[0]
+        # number of local maxima and minima
+        n_extrema = len(idx_local_max) + len(idx_local_min)
+
+        if (n_extrema <= 1):
+            return wcns5z(q)
+        else:
+            slope = MonotonisedCentral.monotonised_central(
+                q[j] - q[j - 1], q[j + 1] - q[j])
+            return [q[j] - 0.5 * slope, q[j] + 0.5 * slope]
+
+    def compute_face_values(recons_upper_of_cell, recons_lower_of_cell, v, i,
+                            j, k, dim_to_recons):
+        return compute_face_values_t(recons_upper_of_cell,
+                                     recons_lower_of_cell, v, i, j, k,
+                                     dim_to_recons, wcns5z_with_mc)
 
     return Reconstruction.reconstruct(u, extents, dim, [2, 2, 2],
                                       compute_face_values)
