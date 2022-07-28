58e8c09c475ab61e3f20d9cc02b89f5174839372
William Throwe <wtt6@cornell.edu>
2022-07-28 14:22:35 -0400

    TESTING

--- src/Utilities/IsNan.cpp
+++ src/Utilities/IsNan.cpp
@@ -8,31 +8,31 @@
 #include "ErrorHandling/FloatingPointExceptions.hpp"
 
 bool isnan_safe(const double arg) {
-  disable_floating_point_exceptions();
+  //disable_floating_point_exceptions();
 #if defined(__clang__) && __clang__ < 10
   // Old versions of clang still throw FPEs here, so prevent them from
   // reordering statements.
-  asm("");
+  //asm("");
 #endif  /* defined(__clang__) && __clang__ < 10 */
   const bool result = std::isnan(arg);
 #if defined(__clang__) && __clang__ < 10
-  asm("");
+  //asm("");
 #endif  /* defined(__clang__) && __clang__ < 10 */
-  enable_floating_point_exceptions();
+  //enable_floating_point_exceptions();
   return result;
 }
 
 bool isnan_safe(const float arg) {
-  disable_floating_point_exceptions();
+  //disable_floating_point_exceptions();
 #if defined(__clang__) && __clang__ < 10
   // Old versions of clang still throw FPEs here, so prevent them from
   // reordering statements.
-  asm("");
+  //asm("");
 #endif  /* defined(__clang__) && __clang__ < 10 */
   const bool result = std::isnan(arg);
 #if defined(__clang__) && __clang__ < 10
-  asm("");
+  //asm("");
 #endif  /* defined(__clang__) && __clang__ < 10 */
-  enable_floating_point_exceptions();
+  //enable_floating_point_exceptions();
   return result;
 }
