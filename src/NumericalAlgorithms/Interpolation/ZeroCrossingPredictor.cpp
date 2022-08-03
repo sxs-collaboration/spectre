1afd4afd21918b827b6ac97bd2d9590004f13794
Mark Scheel <scheel@tapir.caltech.edu>
2022-08-03 16:32:34 -0700

    Add ZeroCrossingPredictor.

---
+++ src/NumericalAlgorithms/Interpolation/ZeroCrossingPredictor.cpp
@@ -0,0 +1,94 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "NumericalAlgorithms/Interpolation/ZeroCrossingPredictor.hpp"
+
+#include <algorithm>
+#include <pup.h>
+#include <pup_stl.h>
+
+#include "DataStructures/DataVector.hpp"
+#include "NumericalAlgorithms/Interpolation/PredictedZeroCrossing.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
+
+namespace intrp {
+ZeroCrossingPredictor::ZeroCrossingPredictor(const size_t min_size,
+                                             const size_t max_size)
+    : min_size_(min_size), max_size_(max_size) {
+  if (min_size_ < 3) {
+    ERROR("min_size must be >= 3, not " << min_size_);
+  }
+  if (min_size_ > max_size_) {
+    ERROR("min_size must be <= max_size, but " << min_size_ << " > "
+                                               << max_size_);
+  }
+}
+
+void ZeroCrossingPredictor::add(const double t, DataVector data_at_time_t) {
+  times_.push_back(t);
+  data_.emplace_back(std::move(data_at_time_t));
+
+  while (times_.size() > max_size_) {
+    times_.pop_front();
+    data_.pop_front();
+  }
+}
+
+DataVector ZeroCrossingPredictor::zero_crossing_time(
+    const double current_time) const {
+  if (not is_valid()) {
+    ERROR("Invalid ZeroCrossingPredictor. min_size = "
+          << min_size_ << " times_.size() = " << times_.size());
+  }
+
+  // We need times relative to current_time.
+  std::deque<double> relative_times(times_.begin(), times_.end());
+  for (auto& time : relative_times) {
+    time -= current_time;
+  }
+  return predicted_zero_crossing_value(relative_times, data_);
+}
+
+double ZeroCrossingPredictor::min_positive_zero_crossing_time(
+    double current_time) const {
+  if (not is_valid()) {
+    return 0.0;
+  }
+  auto crossing_time = zero_crossing_time(current_time);
+  // Replace all negative crossing times with zero.
+  std::for_each(crossing_time.begin(), crossing_time.end(),
+                [](double& a) { a = std::max(0.0, a); });
+
+  return *std::min_element(crossing_time.begin(), crossing_time.end());
+}
+
+bool ZeroCrossingPredictor::is_valid() const {
+  return times_.size() >= min_size_;
+}
+
+void ZeroCrossingPredictor::clear() {
+  times_.clear();
+  data_.clear();
+}
+
+void ZeroCrossingPredictor::pup(PUP::er& p) {
+  p | min_size_;
+  p | max_size_;
+  p | times_;
+  p | data_;
+}
+
+bool operator==(const ZeroCrossingPredictor& predictor1,
+                const ZeroCrossingPredictor& predictor2) {
+  return (predictor1.min_size_ == predictor2.min_size_) and
+         (predictor1.max_size_ == predictor2.max_size_) and
+         (predictor1.times_ == predictor2.times_) and
+         (predictor1.data_ == predictor2.data_);
+}
+
+bool operator!=(const ZeroCrossingPredictor& predictor1,
+                const ZeroCrossingPredictor& predictor2) {
+  return not(predictor1 == predictor2);
+}
+
+}  // namespace intrp
---
+++ src/NumericalAlgorithms/Interpolation/ZeroCrossingPredictor.hpp
@@ -0,0 +1,78 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+#include <deque>
+
+#include "DataStructures/DataVector.hpp"
+
+/// \cond
+namespace PUP {
+class er;
+}  // namespace PUP
+/// \endcond
+
+namespace intrp {
+
+/// \ingroup NumericalAlgorithmsGroup
+/// \brief A class that predicts when a function crosses zero
+class ZeroCrossingPredictor {
+ public:
+  /// Uses at most max_size times for the fit; throws away old
+  /// times as new times are added.
+  /// The is_valid function returns false until min_size times
+  /// have been added.  min_size must be at least 3.
+  ZeroCrossingPredictor(size_t min_size, size_t max_size);
+
+  ZeroCrossingPredictor() = default;
+  ZeroCrossingPredictor(const ZeroCrossingPredictor& /*rhs*/) = delete;
+  ZeroCrossingPredictor& operator=(const ZeroCrossingPredictor& /*rhs*/) =
+      delete;
+  ZeroCrossingPredictor(ZeroCrossingPredictor&& /*rhs*/) = default;
+  ZeroCrossingPredictor& operator=(ZeroCrossingPredictor&& rhs) = default;
+  ~ZeroCrossingPredictor() = default;
+
+  /// Adds a data point at time t to the ZeroCrossingPredictor.
+  void add(double t, DataVector data_at_time_t);
+
+  /// For each component of the data, returns the time, relative to
+  /// current_time, that a linear fit to the given component of the
+  /// data crosses zero.  The length of the return value is the same
+  /// as the length of `data_at_time_t` in the `add` function.
+  ///
+  /// The zero-crossing time that is computed has error bars
+  /// associated with it.  If the error bars are large enough that it
+  /// is not clear whether the zero-crossing time is positive or
+  /// negative, then zero is returned instead of the best-fit
+  /// zero-crossing time.
+  DataVector zero_crossing_time(double current_time) const;
+
+  /// The minimum positive value over the DataVector returned
+  /// by zero_crossing_time.  Returns zero if is_valid() is false.
+  double min_positive_zero_crossing_time(double current_time) const;
+
+  /// Returns whether we have enough data to call zero_crossing_time.
+  bool is_valid() const;
+
+  /// Clears the internal arrays.  Used to reset if there is a
+  /// discontinuous change in the data that should not be fit over.
+  void clear();
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& p);
+
+  friend bool operator==(const ZeroCrossingPredictor&,   // NOLINT
+                         const ZeroCrossingPredictor&);  // NOLINT
+
+ private:
+  size_t min_size_{3};
+  size_t max_size_{3};
+  std::deque<double> times_{};
+  std::deque<DataVector> data_{};
+};
+
+bool operator!=(const ZeroCrossingPredictor&, const ZeroCrossingPredictor&);
+
+}  // namespace intrp
