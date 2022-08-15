c4c617e1243da8e409e77c00f496c2ecc7f918f1
Mark Scheel <scheel@tapir.caltech.edu>
2022-08-15 15:59:56 -0700

    ObserveCenters: Don't use inertial-frame horizon.
    Instead take the average of inertial-frame coordinates.

--- src/ApparentHorizons/ObserveCenters.hpp
+++ src/ApparentHorizons/ObserveCenters.hpp
@@ -3,22 +3,27 @@
 
 #pragma once
 
+#include <array>
+#include <cmath>
+#include <limits>
 #include <tuple>
 
+#include "ApparentHorizons/StrahlkorperGr.hpp"
+#include "ApparentHorizons/Tags.hpp"
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "IO/Observer/ObserverComponent.hpp"
 #include "IO/Observer/ReductionActions.hpp"
+#include "NumericalAlgorithms/SphericalHarmonics/Strahlkorper.hpp"
+#include "NumericalAlgorithms/SphericalHarmonics/Tags.hpp"
+#include "NumericalAlgorithms/SphericalHarmonics/YlmSpherepack.hpp"
 #include "Parallel/GlobalCache.hpp"
 #include "Parallel/Invoke.hpp"
 #include "Parallel/ParallelComponentHelpers.hpp"
 #include "ParallelAlgorithms/Interpolation/InterpolationTargetDetail.hpp"
+#include "Utilities/Gsl.hpp"
 #include "Utilities/PrettyType.hpp"
 
 /// \cond
-namespace StrahlkorperTags {
-template <typename Frame>
-struct Strahlkorper;
-}  // namespace StrahlkorperTags
 namespace Frame {
 struct Grid;
 struct Inertial;
@@ -45,9 +50,10 @@ namespace callbacks {
  * - InertialCenter_y
  * - InertialCenter_z
  *
- * \note Requires StrahlkorperTags::Strahlkorper<Frame::Grid> and
- * StrahlkorperTags::Strahlkorper<Frame::Inertial> be in the DataBox of the
- * InterpolationTarget.
+ * \note Requires StrahlkorperTags::Strahlkorper<Frame::Grid>
+ * and StrahlkorperTags::CartesianCoords<Frame::Inertial> and
+ * StrahlkorperGr::Tags::AreaElement<Frame::Grid> to be in the DataBox
+ * of the InterpolationTarget.
  */
 template <typename InterpolationTargetTag>
 struct ObserveCenters {
@@ -56,14 +62,36 @@ struct ObserveCenters {
                     Parallel::GlobalCache<Metavariables>& cache,
                     const TemporalId& temporal_id) {
     using GridTag = StrahlkorperTags::Strahlkorper<Frame::Grid>;
-    using InertialTag = StrahlkorperTags::Strahlkorper<Frame::Inertial>;
+    using CoordsTag = StrahlkorperTags::CartesianCoords<Frame::Inertial>;
+    using AreaElementTag = StrahlkorperGr::Tags::AreaElement<Frame::Grid>;
+    static_assert(
+        db::tag_is_retrievable_v<GridTag, db::DataBox<DbTags>>,
+        "DataBox must contain StrahlkorperTags::Strahlkorper<Frame::Grid>");
+    static_assert(db::tag_is_retrievable_v<CoordsTag, db::DataBox<DbTags>>,
+                  "DataBox must contain "
+                  "StrahlkorperTags::CartesianCoords<Frame::Inertial>");
+    static_assert(db::tag_is_retrievable_v<AreaElementTag, db::DataBox<DbTags>>,
+                  "DataBox must contain "
+                  "StrahlkorperGr::Tags::AreaElement<Frame::Grid>");
 
     const auto& grid_horizon = db::get<GridTag>(box);
-    const auto& inertial_horizon = db::get<InertialTag>(box);
-
     const std::array<double, 3> grid_center = grid_horizon.physical_center();
-    const std::array<double, 3> inertial_center =
-        inertial_horizon.physical_center();
+
+    // computes the inertial center to be the average value of the
+    // inertial coordinates over the Strahlkorper, where the average is
+    // computed by a surface integral.
+    const auto& inertial_coords = db::get<CoordsTag>(box);
+    const auto& area_element = db::get<AreaElementTag>(box);
+    std::array<double, 3> inertial_center =
+        make_array<3>(std::numeric_limits<double>::signaling_NaN());
+    auto integrand = make_with_value<Scalar<DataVector>>(get(area_element), 0.);
+    for (size_t i = 0; i < 3; ++i) {
+      get(integrand) = get(area_element) * inertial_coords.get(i);
+      gsl::at(inertial_center, i) =
+          grid_horizon.ylm_spherepack().definite_integral(
+              get(integrand).data()) /
+          (4.0 * M_PI);
+    }
 
     // time, grid_x, grid_y, grid_z, inertial_x, inertial_y, inertial_z
     const auto center_tuple = std::make_tuple(
