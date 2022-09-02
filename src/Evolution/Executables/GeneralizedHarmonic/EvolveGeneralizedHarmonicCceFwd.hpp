de3ee71b33b34655508a68c9fbce7b63148287b3
Sizheng Ma <sma@caltech.edu>
2022-09-01 17:29:40 -0700

    Add GH+CCE executable for a gauge wave

---
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonicCceFwd.hpp
@@ -0,0 +1,29 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+/// \cond
+namespace GeneralizedHarmonic {
+template <size_t Dim>
+struct System;
+namespace Solutions {
+template <typename GrSolution>
+struct WrappedGr;
+}  // namespace Solutions
+}  // namespace GeneralizedHarmonic
+namespace gr {
+namespace Solutions {
+template <size_t Dim>
+struct GaugeWave;
+}  // namespace Solutions
+}  // namespace gr
+namespace evolution {
+struct NumericInitialData;
+}  // namespace evolution
+
+template <size_t VolumeDim, bool UseNumericalInitialData>
+struct EvolutionMetavars;
+/// \endcond
