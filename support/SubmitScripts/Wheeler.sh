a3448e6cc9668758feb89d28eb6b2cf1c89c12d5
Kyle Nelli <knelli@caltech.edu>
2022-09-14 11:30:13 -0700

    Print more useful slurm info in wheeler submit script

--- support/SubmitScripts/Wheeler.sh
+++ support/SubmitScripts/Wheeler.sh
@@ -32,20 +32,37 @@ export RUN_DIR=/panfs/ds08/sxs/run/dir
 export SPECTRE_EXECUTABLE=EvolveScalarWave3D
 export SPECTRE_INPUT_FILE=./Input3DPeriodic.yaml
 
-echo "Running on the following nodes:"
-echo ${SLURM_NODELIST}
+export PATH=${SPECTRE_BUILD_DIR}/bin:$PATH
 
 CHARM_PPN=$(expr ${SLURM_CPUS_PER_TASK} - 1)
-echo "Slurm tasks: ${SLURM_NTASKS}"
-echo "Slurm cpus per task: ${SLURM_CPUS_PER_TASK}"
+
+echo "###################################"
+echo "######       JOB INFO        ######"
+echo "###################################"
+echo
+echo "Job ID: ${SLURM_JOB_ID}"
+echo "Run Directory: ${RUN_DIR}"
+echo "Submit Directory: ${SLURM_SUBMIT_DIR}"
+echo "Queue: ${SLURM_JOB_PARTITION}"
+echo "Nodelist: ${SLURM_JOB_NODELIST}"
+echo "Tasks: ${SLURM_NTASKS}"
+echo "CPUs per Task: ${SLURM_CPUS_PER_TASK}"
 echo "Charm ppn: ${CHARM_PPN}"
+echo "PATH: ${PATH}"
+echo
+
 /usr/bin/modulecmd bash list
 
 ############################################################################
 # Set desired permissions for files created with this script
 umask 0022
 
-export PATH=${SPECTRE_BUILD_DIR}/bin:$PATH
+echo
+echo "###################################"
+echo "######   Executable Output   ######"
+echo "###################################"
+echo
+
 cd ${RUN_DIR}
 
 mpirun -n ${SLURM_NTASKS} \
