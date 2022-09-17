08df2aed38e93a8c29f88e75e966a2d2ba566484
Nils Deppe <nils@nilsdeppe.com>
2022-09-17 10:50:31 -0700

    Remove unphysical GH+GR(M)HD instantiations
    The default values for these tests lead to immediate black hole formation.

--- src/PointwiseFunctions/AnalyticSolutions/GhRelativisticEuler/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhRelativisticEuler/InstantiateWrappedGr.cpp
@@ -5,14 +5,10 @@
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
 #include "PointwiseFunctions/AnalyticSolutions/RelativisticEuler/FishboneMoncriefDisk.hpp"
-#include "PointwiseFunctions/AnalyticSolutions/RelativisticEuler/SmoothFlow.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/RelativisticEuler/TovStar.hpp"
 #include "PointwiseFunctions/Hydro/Tags.hpp"
 #include "Utilities/GenerateInstantiations.hpp"
 
 GENERATE_INSTANTIATIONS(WRAPPED_GR_INSTANTIATE,
                         (RelativisticEuler::Solutions::FishboneMoncriefDisk,
-                         RelativisticEuler::Solutions::SmoothFlow<1>,
-                         RelativisticEuler::Solutions::SmoothFlow<2>,
-                         RelativisticEuler::Solutions::SmoothFlow<3>,
                          RelativisticEuler::Solutions::TovStar))
