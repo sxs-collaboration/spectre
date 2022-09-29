0b8cab48ca2455d445ab1ff0a90fe97d2c79ffc6
William Throwe <wtt6@cornell.edu>
2022-09-28 23:36:17 -0400

    Merge UpdateU test component definitions

--- tests/Unit/Time/Actions/Test_UpdateU.cpp
+++ tests/Unit/Time/Actions/Test_UpdateU.cpp
@@ -56,47 +56,39 @@ using dt_alternative_variables_tag = Tags::dt<AlternativeVar>;
 using alternative_history_tag =
     Tags::HistoryEvolvedVariables<alternative_variables_tag>;
 
-template <typename Metavariables>
+template <typename Metavariables, typename SimpleTags, typename UpdateAction>
 struct Component {
   using metavariables = Metavariables;
   using chare_type = ActionTesting::MockArrayChare;
   using array_index = int;
   using const_global_cache_tags = tmpl::list<Tags::TimeStepper<TimeStepper>>;
-  using simple_tags =
-      db::AddSimpleTags<Tags::TimeStep, variables_tag, history_tag,
-                        ::Tags::IsUsingTimeSteppingErrorControl<>>;
+  using simple_tags = tmpl::append<
+      tmpl::list<Tags::TimeStep, ::Tags::IsUsingTimeSteppingErrorControl<>>,
+      SimpleTags>;
 
   using phase_dependent_action_list =
       tmpl::list<Parallel::PhaseActions<
                      Parallel::Phase::Initialization,
                      tmpl::list<ActionTesting::InitializeDataBox<simple_tags>>>,
                  Parallel::PhaseActions<Parallel::Phase::Testing,
-                                        tmpl::list<Actions::UpdateU<>>>>;
+                                        tmpl::list<UpdateAction>>>;
 };
 
-template <typename Metavariables>
-struct ComponentWithTemplateSpecifiedVariables {
-  using metavariables = Metavariables;
-  using chare_type = ActionTesting::MockArrayChare;
-  using array_index = int;
-  using simple_tags =
-      db::AddSimpleTags<Tags::TimeStep, alternative_variables_tag,
-                        alternative_history_tag,
-                        ::Tags::IsUsingTimeSteppingErrorControl<>>;
-  using compute_tags = db::AddComputeTags<>;
-  using phase_dependent_action_list = tmpl::list<
-      Parallel::PhaseActions<Parallel::Phase::Initialization,
-                             tmpl::list<ActionTesting::InitializeDataBox<
-                                 simple_tags, compute_tags>>>,
-      Parallel::PhaseActions<Parallel::Phase::Testing,
-                             tmpl::list<Actions::UpdateU<AlternativeVar>>>>;
-};
+struct Metavariables;
+
+using component_with_default_variables =
+    Component<Metavariables, tmpl::list<variables_tag, history_tag>,
+              Actions::UpdateU<>>;
+using component_with_template_specified_variables =
+    Component<Metavariables,
+              tmpl::list<alternative_variables_tag, alternative_history_tag>,
+              Actions::UpdateU<AlternativeVar>>;
 
 struct Metavariables {
   using system = System;
   using component_list =
-      tmpl::list<Component<Metavariables>,
-                 ComponentWithTemplateSpecifiedVariables<Metavariables>>;
+      tmpl::list<component_with_default_variables,
+                 component_with_template_specified_variables>;
 };
 }  // namespace
 
@@ -110,17 +102,15 @@ SPECTRE_TEST_CASE("Unit.Time.Actions.UpdateU", "[Unit][Time][Actions]") {
     return 2. * t - 2. * (y - t * t);
   };
 
-  using component = Component<Metavariables>;
-  using component_with_template_specified_variables =
-      ComponentWithTemplateSpecifiedVariables<Metavariables>;
   using MockRuntimeSystem = ActionTesting::MockRuntimeSystem<Metavariables>;
   MockRuntimeSystem runner{{std::make_unique<TimeSteppers::RungeKutta3>()}};
-  ActionTesting::emplace_component_and_initialize<component>(
-      &runner, 0, {time_step, 1., history_tag::type{3}, false});
+  ActionTesting::emplace_component_and_initialize<
+      component_with_default_variables>(
+      &runner, 0, {time_step, false, 1., history_tag::type{3}});
 
   ActionTesting::emplace_component_and_initialize<
       component_with_template_specified_variables>(
-      &runner, 0, {time_step, 1., alternative_history_tag::type{3}, false});
+      &runner, 0, {time_step, false, 1., alternative_history_tag::type{3}});
   ActionTesting::set_phase(make_not_null(&runner), Parallel::Phase::Testing);
 
   const std::array<Time, 3> substep_times{
@@ -131,7 +121,8 @@ SPECTRE_TEST_CASE("Unit.Time.Actions.UpdateU", "[Unit][Time][Actions]") {
 
   for (size_t substep = 0; substep < 3; ++substep) {
     auto& before_box =
-        ActionTesting::get_databox<component>(make_not_null(&runner), 0);
+        ActionTesting::get_databox<component_with_default_variables>(
+            make_not_null(&runner), 0);
     db::mutate<history_tag>(
         make_not_null(&before_box),
         [&rhs, &substep, &substep_times](
@@ -159,9 +150,10 @@ SPECTRE_TEST_CASE("Unit.Time.Actions.UpdateU", "[Unit][Time][Actions]") {
         },
         db::get<alternative_variables_tag>(alternative_before_box));
 
-    runner.next_action<component>(0);
+    runner.next_action<component_with_default_variables>(0);
     runner.next_action<component_with_template_specified_variables>(0);
-    const auto& box = ActionTesting::get_databox<component>(runner, 0);
+    const auto& box =
+        ActionTesting::get_databox<component_with_default_variables>(runner, 0);
     auto& alternative_box =
         ActionTesting::get_databox<component_with_template_specified_variables>(
             make_not_null(&runner), 0);
