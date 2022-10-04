799aef3e8ce2c1a54076ed6245d9c427ff47abc0
Nils Deppe <nils@nilsdeppe.com>
2022-10-04 10:21:01 -0700

    Add observer tags for Jacobians and mesh velocity

--- tests/Unit/ParallelAlgorithms/Events/Test_Tags.cpp
+++ tests/Unit/ParallelAlgorithms/Events/Test_Tags.cpp
@@ -4,6 +4,7 @@
 #include "Framework/TestingFramework.hpp"
 
 #include <cstddef>
+#include <optional>
 
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataVector.hpp"
@@ -20,20 +21,50 @@ template <size_t Dim>
 void test(const Mesh<Dim>& mesh) {
   const tnsr::I<DataVector, Dim, Frame::Grid> grid_coords{5_st, 7.2};
   const tnsr::I<DataVector, Dim, Frame::Inertial> inertial_coords{5_st, 7.2};
+  const InverseJacobian<DataVector, Dim, Frame::ElementLogical, Frame::Inertial>
+      inv_jac{5_st, 9.3};
+  const Jacobian<DataVector, Dim, Frame::ElementLogical, Frame::Inertial> jac{
+      5_st, 10.3};
+  const Scalar<DataVector> det_inv_jac{5_st, 11.3};
+  const tnsr::I<DataVector, Dim, Frame::Inertial> mesh_velocity{5_st, 8.3};
   const auto box = db::create<
-      db::AddSimpleTags<domain::Tags::Mesh<Dim>,
-                        domain::Tags::Coordinates<Dim, Frame::Grid>,
-                        domain::Tags::Coordinates<Dim, Frame::Inertial>>,
+      db::AddSimpleTags<
+          domain::Tags::Mesh<Dim>, domain::Tags::Coordinates<Dim, Frame::Grid>,
+          domain::Tags::Coordinates<Dim, Frame::Inertial>,
+          domain::Tags::InverseJacobian<Dim, Frame::ElementLogical,
+                                        Frame::Inertial>,
+          domain::Tags::Jacobian<Dim, Frame::ElementLogical, Frame::Inertial>,
+          domain::Tags::DetInvJacobian<Frame::ElementLogical, Frame::Inertial>,
+          domain::Tags::MeshVelocity<Dim, Frame::Inertial>>,
       db::AddComputeTags<
           ::Events::Tags::ObserverMeshCompute<Dim>,
           Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Grid>,
-          Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Inertial>>>(
-      mesh, grid_coords, inertial_coords);
+          Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Inertial>,
+          Events::Tags::ObserverInverseJacobianCompute<
+              Dim, Frame::ElementLogical, Frame::Inertial>,
+          Events::Tags::ObserverJacobianCompute<Dim, Frame::ElementLogical,
+                                                Frame::Inertial>,
+          Events::Tags::ObserverDetInvJacobianCompute<Frame::ElementLogical,
+                                                      Frame::Inertial>,
+          Events::Tags::ObserverMeshVelocityCompute<Dim, Frame::Inertial>>>(
+      mesh, grid_coords, inertial_coords, inv_jac, jac, det_inv_jac,
+      std::optional{mesh_velocity});
   CHECK(db::get<Events::Tags::ObserverMesh<Dim>>(box) == mesh);
   CHECK(db::get<Events::Tags::ObserverCoordinates<Dim, Frame::Grid>>(box) ==
         grid_coords);
   CHECK(db::get<Events::Tags::ObserverCoordinates<Dim, Frame::Inertial>>(box) ==
         inertial_coords);
+  CHECK(
+      db::get<Events::Tags::ObserverInverseJacobian<Dim, Frame::ElementLogical,
+                                                    Frame::Inertial>>(box) ==
+      inv_jac);
+  CHECK(db::get<Events::Tags::ObserverJacobian<Dim, Frame::ElementLogical,
+                                               Frame::Inertial>>(box) == jac);
+  CHECK(db::get<Events::Tags::ObserverDetInvJacobian<Frame::ElementLogical,
+                                                     Frame::Inertial>>(box) ==
+        det_inv_jac);
+  CHECK(db::get<Events::Tags::ObserverMeshVelocity<Dim, Frame::Inertial>>(
+            box) == mesh_velocity);
   TestHelpers::db::test_simple_tag<Events::Tags::ObserverMesh<Dim>>(
       "ObserverMesh");
   TestHelpers::db::test_compute_tag<::Events::Tags::ObserverMeshCompute<Dim>>(
@@ -49,6 +80,31 @@ void test(const Mesh<Dim>& mesh) {
   TestHelpers::db::test_compute_tag<
       Events::Tags::ObserverCoordinatesCompute<Dim, Frame::Inertial>>(
       "InertialCoordinates");
+  TestHelpers::db::test_simple_tag<Events::Tags::ObserverInverseJacobian<
+      Dim, Frame::ElementLogical, Frame::Inertial>>(
+      "InverseJacobian(ElementLogical,Inertial)");
+  TestHelpers::db::test_compute_tag<
+      Events::Tags::ObserverInverseJacobianCompute<Dim, Frame::ElementLogical,
+                                                   Frame::Inertial>>(
+      "InverseJacobian(ElementLogical,Inertial)");
+  TestHelpers::db::test_simple_tag<Events::Tags::ObserverJacobian<
+      Dim, Frame::ElementLogical, Frame::Inertial>>(
+      "Jacobian(ElementLogical,Inertial)");
+  TestHelpers::db::test_compute_tag<Events::Tags::ObserverJacobianCompute<
+      Dim, Frame::ElementLogical, Frame::Inertial>>(
+      "Jacobian(ElementLogical,Inertial)");
+  TestHelpers::db::test_simple_tag<Events::Tags::ObserverDetInvJacobian<
+      Frame::ElementLogical, Frame::Inertial>>(
+      "DetInvJacobian(ElementLogical,Inertial)");
+  TestHelpers::db::test_compute_tag<Events::Tags::ObserverDetInvJacobianCompute<
+      Frame::ElementLogical, Frame::Inertial>>(
+      "DetInvJacobian(ElementLogical,Inertial)");
+  TestHelpers::db::test_simple_tag<
+      Events::Tags::ObserverMeshVelocity<Dim, Frame::Inertial>>(
+      "InertialMeshVelocity");
+  TestHelpers::db::test_compute_tag<
+      Events::Tags::ObserverMeshVelocityCompute<Dim, Frame::Inertial>>(
+      "InertialMeshVelocity");
 }
 }  // namespace
 
