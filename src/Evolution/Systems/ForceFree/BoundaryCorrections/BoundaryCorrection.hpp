d186e3d2985b8f5e522b9e0d2e0d93652828db82
Yoonsoo Kim <ykim7@caltech.edu>
2022-10-18 10:54:47 -0700

    Add Rusanov/LLF boundary correction to ForceFree system

---
+++ src/Evolution/Systems/ForceFree/BoundaryCorrections/BoundaryCorrection.hpp
@@ -0,0 +1,45 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <memory>
+#include <pup.h>
+
+#include "Parallel/CharmPupable.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace ForceFree {
+/*!
+ * \brief Boundary corrections/numerical fluxes for the GRFFE sytem.
+ */
+namespace BoundaryCorrections {
+
+/// \cond
+class Rusanov;
+/// \endcond
+
+/*!
+ * \brief The base class used to create boundary corrections from input files
+ * and store them in the global cache.
+ */
+class BoundaryCorrection : public PUP::able {
+ public:
+  BoundaryCorrection() = default;
+  BoundaryCorrection(const BoundaryCorrection&) = default;
+  BoundaryCorrection& operator=(const BoundaryCorrection&) = default;
+  BoundaryCorrection(BoundaryCorrection&&) = default;
+  BoundaryCorrection& operator=(BoundaryCorrection&&) = default;
+  ~BoundaryCorrection() override = default;
+
+  /// \cond
+  explicit BoundaryCorrection(CkMigrateMessage* msg) : PUP::able(msg) {}
+  WRAPPED_PUPable_abstract(BoundaryCorrection);  // NOLINT
+  /// \endcond
+
+  using creatable_classes = tmpl::list<Rusanov>;
+
+  virtual std::unique_ptr<BoundaryCorrection> get_clone() const = 0;
+};
+}  // namespace BoundaryCorrections
+}  // namespace ForceFree
---
+++ src/Evolution/Systems/ForceFree/BoundaryCorrections/RegisterDerived.hpp
@@ -0,0 +1,8 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace ForceFree::BoundaryCorrections {
+void register_derived_with_charm();
+}  // namespace ForceFree::BoundaryCorrections
