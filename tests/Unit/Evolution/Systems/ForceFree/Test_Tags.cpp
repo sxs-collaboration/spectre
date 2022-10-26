3822dcb6fda78b4da8767b16d9a8d1a0abca2182
Yoonsoo Kim <ykim7@caltech.edu>
2022-10-25 17:53:48 -0700

    Add tags for the ForceFree evolution system

---
+++ tests/Unit/Evolution/Systems/ForceFree/Test_Tags.cpp
@@ -0,0 +1,37 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include "Evolution/Systems/ForceFree/Tags.hpp"
+#include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
+
+SPECTRE_TEST_CASE("Unit.Evolution.Systems.ForceFree.Tags",
+                  "[Unit][Evolution]") {
+  // Primitive variable tags
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::ElectricField>(
+      "ElectricField");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::MagneticField>(
+      "MagneticField");
+  TestHelpers::db::test_simple_tag<
+      ForceFree::Tags::ElectricDivergenceCleaningField>(
+      "ElectricDivergenceCleaningField");
+  TestHelpers::db::test_simple_tag<
+      ForceFree::Tags::MagneticDivergenceCleaningField>(
+      "MagneticDivergenceCleaningField");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::ChargeDensity>(
+      "ChargeDensity");
+
+  // Evolved variable tags
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::TildeE>("TildeE");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::TildeB>("TildeB");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::TildePsi>("TildePsi");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::TildePhi>("TildePhi");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::TildeQ>("TildeQ");
+
+  // etc.
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::SpatialCurrentDensity>(
+      "SpatialCurrentDensity");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::KappaPsi>("KappaPsi");
+  TestHelpers::db::test_simple_tag<ForceFree::Tags::KappaPhi>("KappaPhi");
+}
