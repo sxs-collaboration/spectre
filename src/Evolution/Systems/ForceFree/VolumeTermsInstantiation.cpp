a2bf7e036846eb3b9e614100ac88dc2c970c90ff
Yoonsoo Kim <ykim7@caltech.edu>
2022-11-03 10:52:40 -0700

    Instantiate volume terms in ForceFree system

---
+++ src/Evolution/Systems/ForceFree/VolumeTermsInstantiation.cpp
@@ -0,0 +1,61 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "DataStructures/DataBox/PrefixHelpers.hpp"
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "Evolution/DiscontinuousGalerkin/Actions/VolumeTermsImpl.tpp"
+#include "Evolution/Systems/ForceFree/System.hpp"
+#include "Evolution/Systems/ForceFree/TimeDerivativeTerms.hpp"
+
+namespace evolution::dg::Actions::detail {
+template void volume_terms<::ForceFree::TimeDerivativeTerms>(
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::dt, typename ::ForceFree::System::variables_tag::tags_list>>*>
+        dt_vars_ptr,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::Flux, typename ::ForceFree::System::flux_variables,
+        tmpl::size_t<3>, Frame::Inertial>>*>
+        volume_fluxes,
+    const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::deriv, typename ::ForceFree::System::gradient_variables,
+        tmpl::size_t<3>, Frame::Inertial>>*>
+        partial_derivs,
+    const gsl::not_null<
+        Variables<typename ::ForceFree::System::
+                      compute_volume_time_derivative_terms::temporary_tags>*>
+        temporaries,
+    [[maybe_unused]] const gsl::not_null<Variables<db::wrap_tags_in<
+        ::Tags::div,
+        db::wrap_tags_in<::Tags::Flux,
+                         typename ::ForceFree::System::flux_variables,
+                         tmpl::size_t<3>, Frame::Inertial>>>*>
+        div_fluxes,
+    const Variables<typename ::ForceFree::System::variables_tag::tags_list>&
+        evolved_vars,
+    const ::dg::Formulation dg_formulation, const Mesh<3>& mesh,
+    [[maybe_unused]] const tnsr::I<DataVector, 3, Frame::Inertial>&
+        inertial_coordinates,
+    const InverseJacobian<DataVector, 3, Frame::ElementLogical,
+                          Frame::Inertial>&
+        logical_to_inertial_inverse_jacobian,
+    [[maybe_unused]] const Scalar<DataVector>* const det_inverse_jacobian,
+    const std::optional<tnsr::I<DataVector, 3, Frame::Inertial>>& mesh_velocity,
+    const std::optional<Scalar<DataVector>>& div_mesh_velocity,
+
+    const tnsr::I<DataVector, 3, Frame::Inertial>& tilde_e,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& tilde_b,
+    const Scalar<DataVector>& tilde_psi, const Scalar<DataVector>& tilde_phi,
+    const Scalar<DataVector>& tilde_q,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& spatial_current_density,
+    const double& kappa_psi, const double& kappa_phi,
+
+    const Scalar<DataVector>& lapse,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& shift,
+    const Scalar<DataVector>& sqrt_det_spatial_metric,
+    const tnsr::ii<DataVector, 3, Frame::Inertial>& spatial_metric,
+    const tnsr::II<DataVector, 3, Frame::Inertial>& inv_spatial_metric,
+    const tnsr::ii<DataVector, 3, Frame::Inertial>& extrinsic_curvature,
+    const tnsr::i<DataVector, 3, Frame::Inertial>& d_lapse,
+    const tnsr::iJ<DataVector, 3, Frame::Inertial>& d_shift,
+    const tnsr::ijj<DataVector, 3, Frame::Inertial>& d_spatial_metric);
+}  // namespace evolution::dg::Actions::detail
