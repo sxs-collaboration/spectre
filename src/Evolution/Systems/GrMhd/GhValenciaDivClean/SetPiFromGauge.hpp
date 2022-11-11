83991e265284c4e55bf940d66811b86e4fc4c3df
Nils Deppe <nils@nilsdeppe.com>
2022-11-11 09:08:30 -0800

    SetPiFromGauge for GH+GRMHD

---
+++ src/Evolution/Systems/GrMhd/GhValenciaDivClean/SetPiFromGauge.hpp
@@ -0,0 +1,93 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <random>
+#include <string>
+#include <unordered_map>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/ElementMap.hpp"
+#include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
+#include "Domain/FunctionsOfTime/Tags.hpp"
+#include "Domain/Tags.hpp"
+#include "Domain/TagsTimeDependent.hpp"
+#include "Evolution/DgSubcell/ActiveGrid.hpp"
+#include "Evolution/DgSubcell/Tags/ActiveGrid.hpp"
+#include "Evolution/DgSubcell/Tags/Coordinates.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
+#include "Evolution/Initialization/Tags.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/Gauges.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/SetPiFromGauge.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/Tags/GaugeCondition.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/Tags.hpp"
+#include "Time/Tags.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace grmhd::GhValenciaDivClean {
+/*!
+ * \brief Set \f$\Pi_{ab}\f$ from the gauge source function.
+ *
+ * This is necessary to ensure the initial data is in the desired evolution
+ * gauge.
+ */
+struct SetPiFromGauge {
+ public:
+  using return_tags =
+      tmpl::list<GeneralizedHarmonic::Tags::Pi<3, Frame::Inertial>>;
+  using argument_tags = tmpl::list<
+      ::Tags::Time, domain::Tags::Mesh<3>,
+      evolution::dg::subcell::Tags::Mesh<3>,
+      domain::Tags::ElementMap<3, Frame::Grid>,
+      domain::CoordinateMaps::Tags::CoordinateMap<3, Frame::Grid,
+                                                  Frame::Inertial>,
+      domain::Tags::FunctionsOfTime,
+      domain::Tags::Coordinates<3, Frame::ElementLogical>,
+      evolution::dg::subcell::Tags::Coordinates<3, Frame::ElementLogical>,
+      gr::Tags::SpacetimeMetric<3, Frame::Inertial, DataVector>,
+      GeneralizedHarmonic::Tags::Phi<3, Frame::Inertial>,
+      GeneralizedHarmonic::gauges::Tags::GaugeCondition,
+      evolution::dg::subcell::Tags::ActiveGrid>;
+
+  using const_global_cache_tags =
+      tmpl::list<GeneralizedHarmonic::gauges::Tags::GaugeCondition>;
+
+  static void apply(
+      const gsl::not_null<tnsr::aa<DataVector, 3, Frame::Inertial>*> pi,
+      const double initial_time, const Mesh<3>& dg_mesh,
+      const Mesh<3>& subcell_mesh,
+      const ElementMap<3, Frame::Grid>& logical_to_grid_map,
+      const domain::CoordinateMapBase<Frame::Grid, Frame::Inertial, 3>&
+          grid_to_inertial_map,
+      const std::unordered_map<
+          std::string,
+          std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>&
+          functions_of_time,
+      const tnsr::I<DataVector, 3, Frame::ElementLogical>&
+          dg_logical_coordinates,
+      const tnsr::I<DataVector, 3, Frame::ElementLogical>&
+          subcell_logical_coordinates,
+      const tnsr::aa<DataVector, 3, Frame::Inertial>& spacetime_metric,
+      const tnsr::iaa<DataVector, 3, Frame::Inertial>& phi,
+      const GeneralizedHarmonic::gauges::GaugeCondition& gauge_condition,
+      const evolution::dg::subcell::ActiveGrid active_grid) {
+    if (active_grid == evolution::dg::subcell::ActiveGrid::Dg) {
+      GeneralizedHarmonic::gauges::SetPiFromGauge<3>::apply(
+          pi, initial_time, dg_mesh, logical_to_grid_map, grid_to_inertial_map,
+          functions_of_time, dg_logical_coordinates, spacetime_metric, phi,
+          gauge_condition);
+    } else {
+      GeneralizedHarmonic::gauges::SetPiFromGauge<3>::apply(
+          pi, initial_time, subcell_mesh, logical_to_grid_map,
+          grid_to_inertial_map, functions_of_time, subcell_logical_coordinates,
+          spacetime_metric, phi, gauge_condition);
+    }
+  }
+};
+}  // namespace grmhd::GhValenciaDivClean
---
+++ tests/Unit/Evolution/Systems/GrMhd/GhValenciaDivClean/Test_SetPiFromGauge.cpp
@@ -0,0 +1,171 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <random>
+#include <string>
+#include <unordered_map>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "DataStructures/Variables.hpp"
+#include "DataStructures/VariablesTag.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.tpp"
+#include "Domain/CoordinateMaps/Identity.hpp"
+#include "Domain/CoordinateMaps/Tags.hpp"
+#include "Domain/ElementMap.hpp"
+#include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
+#include "Domain/FunctionsOfTime/Tags.hpp"
+#include "Domain/Tags.hpp"
+#include "Evolution/DgSubcell/ActiveGrid.hpp"
+#include "Evolution/DgSubcell/Tags/ActiveGrid.hpp"
+#include "Evolution/DgSubcell/Tags/Coordinates.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
+#include "Evolution/Initialization/Tags.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/DampedHarmonic.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/Dispatch.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/Gauges.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/SetPiFromGauge.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/GaugeSourceFunctions/Tags/GaugeCondition.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/Tags.hpp"
+#include "Evolution/Systems/GrMhd/GhValenciaDivClean/SetPiFromGauge.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+SPECTRE_TEST_CASE(
+    "Unit.Evolution.Systems.GrMhd.GhValenciaDivClean.SetPiFromGauge",
+    "[Unit][Evolution][Actions]") {
+  MAKE_GENERATOR(generator);
+  std::uniform_real_distribution<> metric_dist(0.1, 1.);
+  std::uniform_real_distribution<> deriv_dist(-1.e-5, 1.e-5);
+
+  using evolved_vars_tags =
+      tmpl::list<gr::Tags::SpacetimeMetric<3, Frame::Inertial, DataVector>,
+                 GeneralizedHarmonic::Tags::Pi<3, Frame::Inertial>,
+                 GeneralizedHarmonic::Tags::Phi<3, Frame::Inertial>>;
+
+  const Mesh<3> dg_mesh{5, Spectral::Basis::Legendre,
+                        Spectral::Quadrature::GaussLobatto};
+  const auto make_vars = [&generator, &deriv_dist,
+                          &metric_dist](const Mesh<3>& mesh) {
+    const size_t num_points = mesh.number_of_grid_points();
+    Variables<evolved_vars_tags> evolved_vars{mesh.number_of_grid_points()};
+    get<GeneralizedHarmonic::Tags::Pi<3, Frame::Inertial>>(evolved_vars) =
+        make_with_random_values<tnsr::aa<DataVector, 3, Frame::Inertial>>(
+            make_not_null(&generator), make_not_null(&deriv_dist), num_points);
+    get<GeneralizedHarmonic::Tags::Phi<3, Frame::Inertial>>(evolved_vars) =
+        make_with_random_values<tnsr::iaa<DataVector, 3, Frame::Inertial>>(
+            make_not_null(&generator), make_not_null(&deriv_dist), num_points);
+    get<gr::Tags::SpacetimeMetric<3, Frame::Inertial, DataVector>>(
+        evolved_vars) =
+        make_with_random_values<tnsr::aa<DataVector, 3, Frame::Inertial>>(
+            make_not_null(&generator), make_not_null(&metric_dist), num_points);
+    get<0, 0>(get<gr::Tags::SpacetimeMetric<3, Frame::Inertial, DataVector>>(
+        evolved_vars)) += -2.0;
+    for (size_t i = 0; i < 3; ++i) {
+      get<gr::Tags::SpacetimeMetric<3, Frame::Inertial, DataVector>>(
+          evolved_vars)
+          .get(i + 1, i + 1) += 4.0;
+      get<gr::Tags::SpacetimeMetric<3, Frame::Inertial, DataVector>>(
+          evolved_vars)
+          .get(i + 1, 0) *= 0.01;
+    }
+    return evolved_vars;
+  };
+
+  const auto initial_dg_vars = make_vars(dg_mesh);
+
+  auto box = db::create<
+      db::AddSimpleTags<::Tags::Time, ::Tags::Variables<evolved_vars_tags>,
+                        domain::Tags::Mesh<3>,
+                        domain::Tags::ElementMap<3, Frame::Grid>,
+                        domain::CoordinateMaps::Tags::CoordinateMap<
+                            3, Frame::Grid, Frame::Inertial>,
+                        domain::Tags::FunctionsOfTimeInitialize,
+                        domain::Tags::Coordinates<3, Frame::ElementLogical>,
+                        GeneralizedHarmonic::gauges::Tags::GaugeCondition,
+                        evolution::dg::subcell::Tags::ActiveGrid>,
+      db::AddComputeTags<
+          evolution::dg::subcell::Tags::MeshCompute<3>,
+          evolution::dg::subcell::Tags::LogicalCoordinatesCompute<3>>>(
+      0., initial_dg_vars, dg_mesh,
+      ElementMap<3, Frame::Grid>{
+          ElementId<3>{0},
+          domain::make_coordinate_map_base<Frame::BlockLogical, Frame::Grid>(
+              domain::CoordinateMaps::Identity<3>{})},
+      domain::make_coordinate_map_base<Frame::Grid, Frame::Inertial>(
+          domain::CoordinateMaps::Identity<3>{}),
+      std::unordered_map<
+          std::string,
+          std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>{},
+      logical_coordinates(dg_mesh),
+      std::unique_ptr<GeneralizedHarmonic::gauges::GaugeCondition>(
+          std::make_unique<GeneralizedHarmonic::gauges::DampedHarmonic>(
+              100., std::array{1.2, 1.5, 1.7}, std::array{2, 4, 6})),
+      evolution::dg::subcell::ActiveGrid::Dg);
+  const auto initial_subcell_vars =
+      make_vars(db::get<evolution::dg::subcell::Tags::Mesh<3>>(box));
+
+  db::mutate_apply<grmhd::GhValenciaDivClean::SetPiFromGauge>(
+      make_not_null(&box));
+
+  const auto check = [&box](const Mesh<3>& mesh, const auto& initial_vars) {
+    CAPTURE(mesh);
+    tnsr::aa<DataVector, 3, Frame::Inertial> expected_pi =
+        get<GeneralizedHarmonic::Tags::Pi<3>>(initial_vars);
+    GeneralizedHarmonic::gauges::SetPiFromGauge<3>::apply(
+        make_not_null(&expected_pi), 0., mesh,
+        db::get<domain::Tags::ElementMap<3, Frame::Grid>>(box),
+        db::get<domain::CoordinateMaps::Tags::CoordinateMap<3, Frame::Grid,
+                                                            Frame::Inertial>>(
+            box),
+        db::get<domain::Tags::FunctionsOfTime>(box), logical_coordinates(mesh),
+        get<gr::Tags::SpacetimeMetric<3, Frame::Inertial, DataVector>>(
+            initial_vars),
+        get<GeneralizedHarmonic::Tags::Phi<3, Frame::Inertial>>(initial_vars),
+        db::get<GeneralizedHarmonic::gauges::Tags::GaugeCondition>(box));
+
+    const auto& pi = db::get<GeneralizedHarmonic::Tags::Pi<3>>(box);
+    CHECK(pi == expected_pi);
+  };
+
+  // Check that initial grid is fine.
+  check(db::get<domain::Tags::Mesh<3>>(box), initial_dg_vars);
+
+  // Switch to subcell grid and check we compute Pi correctly
+  db::mutate<evolution::dg::subcell::Tags::ActiveGrid,
+             ::Tags::Variables<evolved_vars_tags>>(
+      make_not_null(&box), [&initial_subcell_vars](const auto active_grid_ptr,
+                                                   const auto variables_ptr) {
+        *active_grid_ptr = evolution::dg::subcell::ActiveGrid::Subcell;
+        *variables_ptr = initial_subcell_vars;
+      });
+  db::mutate_apply<grmhd::GhValenciaDivClean::SetPiFromGauge>(
+      make_not_null(&box));
+  check(db::get<evolution::dg::subcell::Tags::Mesh<3>>(box),
+        initial_subcell_vars);
+
+  // // Switch back to DG and check we compute Pi correctly
+  db::mutate<evolution::dg::subcell::Tags::ActiveGrid,
+             ::Tags::Variables<evolved_vars_tags>>(
+      make_not_null(&box),
+      [&initial_dg_vars](const auto active_grid_ptr, const auto variables_ptr) {
+        *active_grid_ptr = evolution::dg::subcell::ActiveGrid::Dg;
+        *variables_ptr = initial_dg_vars;
+      });
+  db::mutate_apply<grmhd::GhValenciaDivClean::SetPiFromGauge>(
+      make_not_null(&box));
+  check(db::get<domain::Tags::Mesh<3>>(box), initial_dg_vars);
+}
