85634105459903ee5240f5f04143453a4af3134e
Nils Deppe <nils@nilsdeppe.com>
2022-12-01 21:48:36 -0800

    Add FilterOptions class to GHMHD

---
+++ src/Evolution/Systems/GrMhd/GhValenciaDivClean/FiniteDifference/FilterOptions.hpp
@@ -0,0 +1,44 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <optional>
+
+#include "Options/Auto.hpp"
+#include "Options/Options.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+namespace PUP {
+class er;
+}  // namespace PUP
+/// \endcond
+
+namespace grmhd::GhValenciaDivClean::fd {
+/*!
+ * \brief Filtering/dissipation options
+ */
+struct FilterOptions {
+  /// Kreiss-Oliger dissipation applied to metric variables
+  ///
+  /// Must be positive and less than 1.
+  struct SpacetimeDissipation {
+    using type = Options::Auto<double, Options::AutoLabel::None>;
+    static constexpr Options::String help{
+        "The amount of Kreiss-Oliger filter dissipation to apply. Must be "
+        "positive and less than 1. If 'None' then no dissipation is applied."};
+  };
+  using options = tmpl::list<SpacetimeDissipation>;
+
+  static constexpr Options::String help{
+      "Parameters for controlling filter on the FD grid."};
+
+  FilterOptions() = default;
+  explicit FilterOptions(std::optional<double> in_spacetime_dissipation,
+                         const Options::Context& context = {});
+  void pup(PUP::er& p);
+
+  std::optional<double> spacetime_dissipation = std::nullopt;
+};
+}  // namespace grmhd::GhValenciaDivClean::fd
---
+++ tests/Unit/Evolution/Systems/GrMhd/GhValenciaDivClean/FiniteDifference/Test_FilterOptions.cpp
@@ -0,0 +1,34 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include <catch.hpp>
+#include "Framework/TestingFramework.hpp"
+
+#include "Evolution/Systems/GrMhd/GhValenciaDivClean/FiniteDifference/FilterOptions.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Framework/TestHelpers.hpp"
+
+SPECTRE_TEST_CASE(
+    "Unit.Evolution.Systems.GrMhd.GhValenciaDivClean.Fd.FilterOptions",
+    "[Unit][Evolution]") {
+  CHECK(not serialize_and_deserialize(
+                TestHelpers::test_creation<
+                    grmhd::GhValenciaDivClean::fd::FilterOptions>(
+                    "SpacetimeDissipation: None"))
+                .spacetime_dissipation.has_value());
+  CHECK(serialize_and_deserialize(
+            TestHelpers::test_creation<
+                grmhd::GhValenciaDivClean::fd::FilterOptions>(
+                "SpacetimeDissipation: 0.1"))
+            .spacetime_dissipation.value() == 0.1);
+  CHECK_THROWS_WITH(
+      TestHelpers::test_creation<grmhd::GhValenciaDivClean::fd::FilterOptions>(
+          "SpacetimeDissipation: 0.0"),
+      Catch::Matchers::Contains(
+          "Spacetime dissipation must be between 0 and 1, but got"));
+  CHECK_THROWS_WITH(
+      TestHelpers::test_creation<grmhd::GhValenciaDivClean::fd::FilterOptions>(
+          "SpacetimeDissipation: 1.0"),
+      Catch::Matchers::Contains(
+          "Spacetime dissipation must be between 0 and 1, but got"));
+}
