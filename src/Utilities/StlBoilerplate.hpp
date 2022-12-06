7cedf2a4321c83fa4faade359fe194a79c6cc19f
William Throwe <wtt6@cornell.edu>
2022-12-06 14:22:58 -0500

    Add boilerplate for a random-access sequence

--- src/Utilities/StlBoilerplate.hpp
+++ src/Utilities/StlBoilerplate.hpp
@@ -3,9 +3,16 @@
 
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 #include <iterator>
+#include <limits>
+#include <stdexcept>
 #include <type_traits>
+
+#include "Utilities/ErrorHandling/Assert.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeString.hpp"
 
 /// Classes to generate repetitive code needed for STL compatibility.
 namespace stl_boilerplate {
@@ -123,5 +130,190 @@ Iter operator-(
   auto result = static_cast<const Iter&>(a);
   result -= n;
   return result;
+}
+
+/// Base class to generate methods for a random-access sequence,
+/// similar to a `std::array`.  This class takes the derived class and
+/// the `value_type` of the sequence as template parameters.
+///
+/// The derived class must implement `size` and `operator[]`.
+///
+/// \snippet Test_StlBoilerplate.cpp RandomAccessSequence
+///
+/// This class provides methods for accessing and modifying elements
+/// of the sequence, such as `front` and `begin`, as well as iterators
+/// and reverse iterators.  Methods modifying the sequence itself,
+/// such as `insert`, require explicit implementations.
+template <typename Sequence, typename ValueType>
+class RandomAccessSequence {
+ protected:
+  RandomAccessSequence() = default;
+  RandomAccessSequence(const RandomAccessSequence&) = default;
+  RandomAccessSequence(RandomAccessSequence&&) = default;
+  RandomAccessSequence& operator=(const RandomAccessSequence&) = default;
+  RandomAccessSequence& operator=(RandomAccessSequence&&) = default;
+  ~RandomAccessSequence() = default;
+
+ public:
+  using value_type = ValueType;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using pointer = ValueType*;
+  using const_pointer = const ValueType*;
+
+  class const_iterator
+      : public RandomAccessIterator<const_iterator, const value_type> {
+   public:
+    const_iterator() = default;
+
+    const value_type& operator*() const { return (*container_)[offset_]; }
+    const_iterator& operator+=(const std::ptrdiff_t n) {
+      offset_ += static_cast<size_t>(n);
+      return *this;
+    }
+
+   private:
+    friend bool operator==(const const_iterator& a, const const_iterator& b) {
+      return a.container_ == b.container_ and a.offset_ == b.offset_;
+    }
+
+    friend std::ptrdiff_t operator-(const const_iterator& a,
+                                    const const_iterator& b) {
+      ASSERT(a.container_ == b.container_, "Subtracting unrelated iterators");
+      return static_cast<std::ptrdiff_t>(a.offset_) -
+             static_cast<std::ptrdiff_t>(b.offset_);
+    }
+
+    friend RandomAccessSequence;
+    const_iterator(const gsl::not_null<const Sequence*> container,
+                   const size_t offset)
+        : container_(container), offset_(offset) {}
+
+    const Sequence* container_{nullptr};
+    size_t offset_{0};
+  };
+
+  class iterator : public RandomAccessIterator<iterator, value_type> {
+   public:
+    iterator() = default;
+
+    value_type& operator*() const { return (*container_)[offset_]; }
+    iterator& operator+=(const std::ptrdiff_t n) {
+      offset_ += static_cast<size_t>(n);
+      return *this;
+    }
+
+    operator const_iterator() const {
+      return container_->cbegin() + static_cast<std::ptrdiff_t>(offset_);
+    }
+
+   private:
+    friend bool operator==(const iterator& a, const iterator& b) {
+      return a.container_ == b.container_ and a.offset_ == b.offset_;
+    }
+
+    friend std::ptrdiff_t operator-(const iterator& a, const iterator& b) {
+      ASSERT(a.container_ == b.container_, "Subtracting unrelated iterators");
+      return static_cast<std::ptrdiff_t>(a.offset_) -
+             static_cast<std::ptrdiff_t>(b.offset_);
+    }
+
+    friend RandomAccessSequence;
+    iterator(const gsl::not_null<Sequence*> container, const size_t offset)
+        : container_(container), offset_(offset) {}
+
+    Sequence* container_{nullptr};
+    size_t offset_{0};
+  };
+
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+  using difference_type =
+      typename std::iterator_traits<const_iterator>::difference_type;
+  using size_type = size_t;
+
+  iterator begin() { return {dthis(), 0}; }
+  const_iterator begin() const { return {dthis(), 0}; }
+  const_iterator cbegin() const { return begin(); }
+  iterator end() { return {dthis(), dthis()->size()}; }
+  const_iterator end() const { return {dthis(), dthis()->size()}; }
+  const_iterator cend() const { return end(); }
+
+  reverse_iterator rbegin() { return reverse_iterator(end()); }
+  const_reverse_iterator rbegin() const {
+    return const_reverse_iterator(end());
+  }
+  const_reverse_iterator crbegin() const { return rbegin(); }
+  reverse_iterator rend() { return reverse_iterator(begin()); }
+  const_reverse_iterator rend() const {
+    return const_reverse_iterator(begin());
+  }
+  const_reverse_iterator crend() const { return rend(); }
+
+  size_type max_size() const { return std::numeric_limits<size_type>::max(); }
+  bool empty() const { return begin() == end(); }
+
+  reference front() { return *begin(); }
+  const_reference front() const { return *begin(); }
+  reference back() { return *(end() - 1); }
+  const_reference back() const { return *(end() - 1); }
+
+  reference at(const size_type n) {
+    if (n >= dthis()->size()) {
+      throw std::out_of_range(MakeString{} << "RandomAccessSequence::at " << n
+                                           << " >= " << dthis()->size());
+    }
+    return (*dthis())[n];
+  }
+  const_reference at(const size_type n) const {
+    if (n >= dthis()->size()) {
+      throw std::out_of_range(MakeString{} << "RandomAccessSequence::at " << n
+                                           << " >= " << dthis()->size());
+    }
+    return (*dthis())[n];
+  }
+
+ private:
+  // derived this
+  Sequence* dthis() { return static_cast<Sequence*>(this); }
+  const Sequence* dthis() const { return static_cast<const Sequence*>(this); }
+};
+
+template <typename Sequence, typename ValueType>
+bool operator==(const RandomAccessSequence<Sequence, ValueType>& a,
+                const RandomAccessSequence<Sequence, ValueType>& b) {
+  return static_cast<const Sequence&>(a).size() ==
+             static_cast<const Sequence&>(b).size() and
+         std::equal(a.begin(), a.end(), b.begin());
+}
+
+template <typename Sequence, typename ValueType>
+bool operator!=(const RandomAccessSequence<Sequence, ValueType>& a,
+                const RandomAccessSequence<Sequence, ValueType>& b) {
+  return not(a == b);
+}
+
+template <typename Sequence, typename ValueType>
+bool operator<(const RandomAccessSequence<Sequence, ValueType>& a,
+               const RandomAccessSequence<Sequence, ValueType>& b) {
+  return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
+}
+
+template <typename Sequence, typename ValueType>
+bool operator>(const RandomAccessSequence<Sequence, ValueType>& a,
+               const RandomAccessSequence<Sequence, ValueType>& b) {
+  return b < a;
+}
+
+template <typename Sequence, typename ValueType>
+bool operator<=(const RandomAccessSequence<Sequence, ValueType>& a,
+                const RandomAccessSequence<Sequence, ValueType>& b) {
+  return not(a > b);
+}
+
+template <typename Sequence, typename ValueType>
+bool operator>=(const RandomAccessSequence<Sequence, ValueType>& a,
+                const RandomAccessSequence<Sequence, ValueType>& b) {
+  return not(a < b);
 }
 }  // namespace stl_boilerplate
--- tests/Unit/Utilities/Test_StlBoilerplate.cpp
+++ tests/Unit/Utilities/Test_StlBoilerplate.cpp
@@ -233,8 +233,191 @@ void test_random_access_iterator() {
   test_two_vector_iterator<false>();
   test_two_vector_iterator<true>();
 }
+
+// [RandomAccessSequence]
+template <typename T>
+class SequenceView
+    : public stl_boilerplate::RandomAccessSequence<SequenceView<T>, T> {
+ public:
+  SequenceView(const gsl::not_null<T*> data, const size_t size)
+      : data_(data), size_(size) {}
+
+  size_t size() const { return size_; }
+
+  T& operator[](const size_t n) { return *(data_ + n); }
+  const T& operator[](const size_t n) const { return *(data_ + n); }
+
+ private:
+  T* data_{nullptr};
+  size_t size_{0};
+};
+// [RandomAccessSequence]
+
+template <bool CheckConst>
+void test_sequence_view() {
+  using AccessType = tmpl::conditional_t<CheckConst, const int, int>;
+  using View = SequenceView<AccessType>;
+
+  static_assert(std::is_same_v<typename View::value_type, AccessType>);
+  static_assert(std::is_same_v<typename View::reference, AccessType&>);
+  static_assert(std::is_same_v<typename View::const_reference, const int&>);
+  static_assert(std::is_same_v<typename View::pointer, AccessType*>);
+  static_assert(std::is_same_v<typename View::const_pointer, const int*>);
+  static_assert(std::is_same_v<typename View::difference_type, std::ptrdiff_t>);
+  static_assert(std::is_same_v<typename View::size_type, size_t>);
+  static_assert(std::is_convertible_v<typename View::iterator,
+                                      typename View::const_iterator>);
+  static_assert(
+      std::is_convertible_v<typename std::iterator_traits<
+                                typename View::iterator>::iterator_category,
+                            std::random_access_iterator_tag>);
+  static_assert(std::is_convertible_v<
+                typename std::iterator_traits<
+                    typename View::const_iterator>::iterator_category,
+                std::random_access_iterator_tag>);
+
+  std::array data{1, 2, 3, 4, 5};
+  View view(&data[1], 3);
+  View view_same(&data[1], 3);
+  View view2(&data[1], 2);
+  View view3(&data[0], 3);
+  View view_empty(&data[1], 0);
+
+  const View& cview = view;
+
+  static_assert(
+      std::is_same_v<decltype(view.begin()), typename View::iterator>);
+  static_assert(std::is_same_v<decltype(view.end()), typename View::iterator>);
+  static_assert(
+      std::is_same_v<decltype(view.cbegin()), typename View::const_iterator>);
+  static_assert(
+      std::is_same_v<decltype(view.cend()), typename View::const_iterator>);
+  static_assert(
+      std::is_same_v<decltype(cview.begin()), typename View::const_iterator>);
+  static_assert(
+      std::is_same_v<decltype(cview.end()), typename View::const_iterator>);
+  CHECK(*view.begin() == 2);
+  CHECK(*(view.end() - 1) == 4);
+  CHECK(*cview.begin() == 2);
+  CHECK(*(cview.end() - 1) == 4);
+  {
+    typename View::iterator mi{};
+    typename View::const_iterator ci{};
+    mi = view.begin();
+    ci = view.begin();
+  }
+
+  CHECK(cview == view_same);
+  CHECK_FALSE(cview != view_same);
+  CHECK_FALSE(cview == view2);
+  CHECK(cview != view2);
+  CHECK_FALSE(cview == view3);
+  CHECK(cview != view3);
+  CHECK_FALSE(cview == view_empty);
+  CHECK(cview != view_empty);
+
+  CHECK(cview.size() == 3);
+  CHECK(view_empty.size() == 0);
+  CHECK(not cview.empty());
+  CHECK(view_empty.empty());
+  CHECK(view.end() - view.begin() == static_cast<std::ptrdiff_t>(view.size()));
+  CHECK(cview.end() - cview.begin() ==
+        static_cast<std::ptrdiff_t>(view.size()));
+  CHECK(view_empty.begin() == view_empty.end());
+  CHECK(cview.max_size() >= view.size());
+
+  static_assert(std::is_same_v<typename View::reverse_iterator,
+                               std::reverse_iterator<typename View::iterator>>);
+  static_assert(
+      std::is_same_v<typename View::const_reverse_iterator,
+                     std::reverse_iterator<typename View::const_iterator>>);
+  static_assert(
+      std::is_same_v<decltype(view.rbegin()), typename View::reverse_iterator>);
+  static_assert(
+      std::is_same_v<decltype(view.rend()), typename View::reverse_iterator>);
+  static_assert(std::is_same_v<decltype(view.crbegin()),
+                               typename View::const_reverse_iterator>);
+  static_assert(std::is_same_v<decltype(view.crend()),
+                               typename View::const_reverse_iterator>);
+  static_assert(std::is_same_v<decltype(cview.rbegin()),
+                               typename View::const_reverse_iterator>);
+  static_assert(std::is_same_v<decltype(cview.rend()),
+                               typename View::const_reverse_iterator>);
+  CHECK(*view.rbegin() == 4);
+  CHECK(*(view.rend() - 1) == 2);
+  CHECK(view.rend() - view.rbegin() ==
+        static_cast<std::ptrdiff_t>(view.size()));
+  CHECK(*cview.rbegin() == 4);
+  CHECK(*(cview.rend() - 1) == 2);
+  CHECK(cview.rend() - cview.rbegin() ==
+        static_cast<std::ptrdiff_t>(view.size()));
+
+  CHECK_FALSE(cview < view_same);
+  CHECK_FALSE(cview > view_same);
+  CHECK(cview <= view_same);
+  CHECK(cview >= view_same);
+  CHECK_FALSE(cview < view2);
+  CHECK_FALSE(view2 > cview);
+  CHECK(cview > view2);
+  CHECK(view2 < cview);
+  CHECK_FALSE(cview <= view2);
+  CHECK_FALSE(view2 >= cview);
+  CHECK(cview >= view2);
+  CHECK(view2 <= cview);
+  CHECK_FALSE(cview < view3);
+  CHECK_FALSE(view3 > cview);
+  CHECK(cview > view3);
+  CHECK(view3 < cview);
+  CHECK_FALSE(cview <= view3);
+  CHECK_FALSE(view3 >= cview);
+  CHECK(cview >= view3);
+  CHECK(view3 <= cview);
+  CHECK_FALSE(cview < view_empty);
+  CHECK_FALSE(view_empty > view3);
+  CHECK(cview > view_empty);
+  CHECK(view_empty < cview);
+  CHECK_FALSE(cview <= view_empty);
+  CHECK_FALSE(view_empty >= cview);
+  CHECK(cview >= view_empty);
+  CHECK(view_empty <= cview);
+
+  static_assert(
+      std::is_same_v<decltype(view.front()), decltype(*view.begin())>);
+  CHECK(&view.front() == &*view.begin());
+  static_assert(
+      std::is_same_v<decltype(cview.front()), decltype(*view.cbegin())>);
+  CHECK(&cview.front() == &*view.cbegin());
+  static_assert(
+      std::is_same_v<decltype(view.back()), decltype(*view.rbegin())>);
+  CHECK(&view.back() == &*view.rbegin());
+  static_assert(
+      std::is_same_v<decltype(cview.back()), decltype(*view.crbegin())>);
+  CHECK(&cview.back() == &*view.crbegin());
+
+  CHECK(view[1] == 3);
+  CHECK(&view[1] == &data[2]);
+  CHECK(view.at(1) == 3);
+  CHECK(&view.at(1) == &data[2]);
+  CHECK_THROWS_AS(view.at(view.size()), std::out_of_range);
+  CHECK(cview.at(1) == 3);
+  CHECK(&cview.at(1) == &data[2]);
+  CHECK_THROWS_AS(cview.at(view.size()), std::out_of_range);
+
+  if constexpr (not CheckConst) {
+    CHECK(&(view[1] = 12) == &data[2]);
+    CHECK(&(view.at(1) = 12) == &data[2]);
+    CHECK(&(view.front() = 12) == &data[1]);
+    CHECK(&(view.back() = 12) == &data[3]);
+  }
+}
+
+void test_random_access_sequence() {
+  test_sequence_view<false>();
+  test_sequence_view<true>();
+}
 }  // namespace
 
 SPECTRE_TEST_CASE("Unit.Utilities.StlBoilerplate", "[Unit][Utilities]") {
   test_random_access_iterator();
+  test_random_access_sequence();
 }
