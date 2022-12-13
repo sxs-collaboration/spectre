1e79e5757bdc5e8a5142e247f6f2b7adcaa5745a
Nils Vu <owls@nilsvu.de>
2022-12-12 22:47:43 -0800

    Add `CharmSimplifyTraces.py` to Python CLI
    Also add option to edit file in-place.

---
+++ tests/tools/Test_CharmSimplifyTraces.py
@@ -0,0 +1,18 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+import unittest
+from spectre.tools.CharmSimplifyTraces import extract_first_template_parameter
+
+
+class TestCharmSimplifyTraces(unittest.TestCase):
+    def test_extract_first_template_parameter(self):
+        # Test the example in the docs
+        self.assertEqual(
+            extract_first_template_parameter(
+                "A<1, B<2, 4>, 3>, C, D, E<7, 8, F<9>>"), "A<1, B<2, 4>, 3>")
+        # This test should be expanded further!
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
