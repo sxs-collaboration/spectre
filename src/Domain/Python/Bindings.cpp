51981dd0cdace25aec61f4f48e1e3eedffda7c60
Nils Vu <owls@nilsvu.de>
2022-12-29 16:10:08 -0800

    Bind FunctionsOfTime in Python

--- src/Domain/Python/Bindings.cpp
+++ src/Domain/Python/Bindings.cpp
@@ -5,13 +5,18 @@
 
 #include "Domain/Python/Domain.hpp"
 #include "Domain/Python/ElementId.hpp"
+#include "Domain/Python/FunctionsOfTime.hpp"
 #include "Domain/Python/SegmentId.hpp"
+
+namespace py = pybind11;
 
 namespace domain {
 
 PYBIND11_MODULE(_PyDomain, m) {  // NOLINT
+  py::module_::import("spectre.DataStructures");
   py_bindings::bind_domain(m);
   py_bindings::bind_element_id(m);
+  py_bindings::bind_functions_of_time(m);
   py_bindings::bind_segment_id(m);
 }
 
---
+++ tests/Unit/Domain/Python/Test_FunctionsOfTime.py
@@ -0,0 +1,34 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+from spectre.Domain import deserialize_functions_of_time
+
+import math
+import os
+import spectre.IO.H5 as spectre_h5
+import unittest
+from spectre.DataStructures import DataVector
+from spectre.Informer import unit_test_src_path
+
+
+class TestFunctionsOfTime(unittest.TestCase):
+    def test_deserialize(self):
+        volfile_name = os.path.join(unit_test_src_path(),
+                                    "Visualization/Python/VolTestData0.h5")
+        with spectre_h5.H5File(volfile_name, "r") as open_h5_file:
+            volfile = open_h5_file.get_vol("/element_data")
+            obs_id = volfile.list_observation_ids()[0]
+            serialized_fot = volfile.get_functions_of_time(obs_id)
+
+        functions_of_time = deserialize_functions_of_time(serialized_fot)
+        translation = functions_of_time["Translation"]
+        self.assertEqual(translation.time_bounds(), [0., math.inf])
+        self.assertEqual(translation.func(0.), [DataVector(size=3, fill=0.)])
+        self.assertEqual(translation.func_and_deriv(0.),
+                         2 * [DataVector(size=3, fill=0.)])
+        self.assertEqual(translation.func_and_2_derivs(0.),
+                         3 * [DataVector(size=3, fill=0.)])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
