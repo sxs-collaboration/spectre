3120b88aabf67593c2003851e9ec66f7e6d59290
Nils Vu <owls@nilsvu.de>
2023-01-09 11:55:45 -0800

    Bind partial_derivative in Python

---
+++ src/NumericalAlgorithms/LinearOperators/Python/Bindings.cpp
@@ -0,0 +1,15 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include <pybind11/pybind11.h>
+
+#include "NumericalAlgorithms/LinearOperators/Python/PartialDerivatives.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_PyLinearOperators, m) {  // NOLINT
+  py::module_::import("spectre.DataStructures");
+  py::module_::import("spectre.DataStructures.Tensor");
+  py::module_::import("spectre.Spectral");
+  py_bindings::bind_partial_derivatives(m);
+}
