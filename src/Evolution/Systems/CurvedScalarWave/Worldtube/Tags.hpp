5a6f8a013d64df007f94b55121ae31e460ecc5d1
Nikolas <niko.wittek@gmail.com>
2023-01-27 12:03:41 +0100

    Add worldtube library

---
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/Tags.hpp
@@ -0,0 +1,20 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "Options/Options.hpp"
+
+namespace CurvedScalarWave::Worldtube {
+/*!
+ * \brief Option tags for the worldtube
+ */
+namespace OptionTags {
+/*!
+ * \brief Options for the worldtube
+ */
+struct Worldtube {
+  static constexpr Options::String help = {"Options for the Worldtube"};
+};
+}  // namespace OptionTags
+}  // namespace CurvedScalarWave::Worldtube
