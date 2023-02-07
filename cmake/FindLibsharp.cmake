f5f52e5b248662a8f8e429a29a372691baee820a
Nils Vu <owls@nilsvu.de>
2023-02-07 11:57:19 -0800

    Find libsharp when specifying `LIBSHARP_ROOT`

--- cmake/FindLibsharp.cmake
+++ cmake/FindLibsharp.cmake
@@ -16,7 +16,7 @@ endif()
 # find the libsharp include directory
 find_path(LIBSHARP_INCLUDE_DIRS sharp_cxx.h
     PATH_SUFFIXES include include/libsharp
-    HINTS ${LIBSHARP_ROOT}/include/)
+    HINTS ${LIBSHARP_ROOT})
 
 find_library(LIBSHARP_LIBFFTPACK
     NAMES libfftpack libfftpack.a
