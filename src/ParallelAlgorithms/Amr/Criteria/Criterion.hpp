9d92ffd9c5f8cd39dc1b236683733854bb3c3ff2
Lawrence Kidder <kidder@astro.cornell.edu>
2023-02-07 22:06:21 -0500

    Improve documentation for amr::Criterion

--- src/ParallelAlgorithms/Amr/Criteria/Criterion.hpp
+++ src/ParallelAlgorithms/Amr/Criteria/Criterion.hpp
@@ -20,9 +20,23 @@ namespace amr {
 /// \brief Base class for something that determines how an adaptive mesh should
 /// be changed
 ///
-/// \details When AMR criteria are evaluated for each element, they should
-/// return a std::array<amr::domain::Flag, Dim> containing the recommended
-/// refinement choice in each logical dimension of the Element.
+/// \details Each class derived from this class should (see the examples below):
+/// - Be option-creatable
+/// - Be serializable
+/// - Define a call operator that returns a std::array<amr::domain::Flag, Dim>
+///   containing the recommended refinement choice in each logical dimension of
+///   the Element.
+/// - Define the type aliases `argument_tags` and
+///   `compute_tags_for_observation_box` that are type lists of tags used in the
+///   call operator.
+/// The call operator should take as arguments the values corresponding to each
+/// tag in `argument_tags` (in order), followed by the Parallel::GlobalCache,
+/// and the ArrayIndex (which is typically an ElementId).  The tags listed in
+/// `argument_tags` should either be tags in the DataBox of the array component,
+/// or listed in `compute_tags_for_observation_box`.
+///
+/// \example
+/// \snippet Test_Criterion.cpp criterion_examples
 class Criterion : public PUP::able {
  protected:
   /// \cond
@@ -39,6 +53,17 @@ class Criterion : public PUP::able {
 
   WRAPPED_PUPable_abstract(Criterion);  // NOLINT
 
+  /// Evaluates the AMR criteria by selecting the appropriate derived class
+  /// and forwarding its `argument_tags` from the ObservationBox (along with the
+  /// GlobalCache and ArrayIndex) to the call operator of the derived class
+  ///
+  /// \note In order to be available, a derived Criterion must be listed in
+  /// the entry for Criterion in
+  /// Metavarialbes::factory_creation::factory_classes
+  ///
+  /// \note The ComputeTagsList of the ObservationBox should contain the union
+  /// of the tags listed in `compute_tags_for_observation_box` for each derived
+  /// Criterion listed in the `factory_classes`.
   template <typename ComputeTagsList, typename DataBoxType,
             typename Metavariables, typename ArrayIndex>
   auto evaluate(const ObservationBox<ComputeTagsList, DataBoxType>& box,
