8ac6309a7166a8e1bec68e41f351ad69bea2ad14
Nils Vu <owls@nilsvu.de>
2023-02-09 13:15:42 -0800

    Fix finding Numpy packages in Pypp

--- tests/Unit/Framework/SetupLocalPythonEnvironment.cpp
+++ tests/Unit/Framework/SetupLocalPythonEnvironment.cpp
@@ -1,6 +1,7 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
+#include <boost/preprocessor.hpp>
 #include <codecvt>  // IWYU pragma: keep
 #include <locale>   // IWYU pragma: keep
 #include <string>
@@ -33,6 +34,40 @@ SetupLocalPythonEnvironment::SetupLocalPythonEnvironment(
     // tree. The overhead of not having the compile files is <= 0.01s
     Py_DontWriteBytecodeFlag = 1;
     Py_Initialize();
+
+    // clang-tidy: Do not use const-cast
+    PyObject* pyob_old_paths =
+        PySys_GetObject(const_cast<char*>("path"));  // NOLINT
+    const auto old_paths =
+        pypp::from_py_object<std::vector<std::string>>(pyob_old_paths);
+    std::string new_path =
+        unit_test_src_path() + cur_dir_relative_to_unit_test_path;
+    if (not file_system::check_if_dir_exists(new_path)) {
+      ERROR_NO_TRACE("Trying to add path '"
+                     << new_path
+                     << "' to the python environment during setup "
+                        "but this directory does not exist. Maybe "
+                        "you have a typo in your path?");
+    }
+
+    // Add directory for installed packages (see CMakeLists.txt for details)
+    new_path += ":";
+    new_path += PYTHON_SITELIB;
+
+    for (const auto& p : old_paths) {
+      new_path += ":";
+      new_path += p;
+    }
+
+#if PY_MAJOR_VERSION == 3
+    PySys_SetPath(std::wstring_convert<std::codecvt_utf8<wchar_t>>()
+                      .from_bytes(new_path)
+                      .c_str());
+#else
+    // clang-tidy: Do not use const-cast
+    PySys_SetPath(const_cast<char*>(new_path.c_str()));  // NOLINT
+#endif
+
     // On some python versions init_numpy() can throw an FPE, this occurred at
     // least with python 3.6, numpy 1.14.2.
     disable_floating_point_exceptions();
@@ -40,34 +75,6 @@ SetupLocalPythonEnvironment::SetupLocalPythonEnvironment(
     enable_floating_point_exceptions();
   }
   initialized = true;
-  // clang-tidy: Do not use const-cast
-  PyObject *pyob_old_paths =
-      PySys_GetObject(const_cast<char *>("path"));  // NOLINT
-  const auto old_paths =
-      pypp::from_py_object<std::vector<std::string>>(pyob_old_paths);
-  std::string new_path =
-      unit_test_src_path()+cur_dir_relative_to_unit_test_path;
-  if (not file_system::check_if_dir_exists(new_path)) {
-    ERROR_NO_TRACE("Trying to add path '"
-                   << new_path
-                   << "' to the python environment during setup "
-                      "but this directory does not exist. Maybe "
-                      "you have a typo in your path?");
-  }
-
-  for (const auto &p : old_paths) {
-    new_path += ":";
-    new_path += p;
-  }
-
-#if PY_MAJOR_VERSION == 3
-  PySys_SetPath(std::wstring_convert<std::codecvt_utf8<wchar_t>>()
-                    .from_bytes(new_path)
-                    .c_str());
-#else
-  // clang-tidy: Do not use const-cast
-  PySys_SetPath(const_cast<char*>(new_path.c_str()));  // NOLINT
-#endif
 }
 
 #if PY_MAJOR_VERSION == 3
