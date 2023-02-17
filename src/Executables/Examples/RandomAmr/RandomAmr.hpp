44c638924335d37b83247d5e62609dfd907d9e56
Lawrence Kidder <kidder@astro.cornell.edu>
2023-02-17 17:47:05 -0500

    Add action to collect AMR diagnostics
    As this action is a reduction action, also add the executable
    RandomAmr that uses it.  After more AMR actions are added RandomAmr
    can be used as a integration test of AMR actions and components.

---
+++ src/Executables/Examples/RandomAmr/RandomAmr.hpp
@@ -0,0 +1,92 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "Domain/Creators/DomainCreator.hpp"
+#include "Domain/Creators/Factory1D.hpp"
+#include "Domain/Creators/Factory2D.hpp"
+#include "Domain/Creators/Factory3D.hpp"
+#include "Domain/Creators/RegisterDerivedWithCharm.hpp"
+#include "Evolution/DiscontinuousGalerkin/DgElementArray.hpp"
+#include "Executables/Examples/RandomAmr/InitializeDomain.hpp"
+#include "Options/Options.hpp"
+#include "Options/Protocols/FactoryCreation.hpp"
+#include "Parallel/GlobalCache.hpp"
+#include "Parallel/InitializationFunctions.hpp"
+#include "Parallel/Phase.hpp"
+#include "Parallel/PhaseDependentActionList.hpp"
+#include "Parallel/RegisterDerivedClassesWithCharm.hpp"
+#include "ParallelAlgorithms/Actions/InitializeItems.hpp"
+#include "ParallelAlgorithms/Actions/TerminatePhase.hpp"
+#include "ParallelAlgorithms/Amr/Actions/Component.hpp"
+#include "ParallelAlgorithms/Amr/Actions/Initialize.hpp"
+#include "ParallelAlgorithms/Amr/Actions/SendAmrDiagnostics.hpp"
+#include "Utilities/ErrorHandling/FloatingPointExceptions.hpp"
+#include "Utilities/ProtocolHelpers.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace {
+struct DummySystem {};
+}  // namespace
+
+/// \page RandomAmrExecutablePage RandomAmr Executable
+/// The RandomAmr executable is being used to develop the mechanics of
+/// adaptive mesh refinement.
+///
+/// See RandomAmrMetavars for a description of the metavariables of this
+/// executable.
+
+/// \brief The metavariables for the RandomAmr executable
+template <size_t Dim>
+struct RandomAmrMetavars {
+  static constexpr size_t volume_dim = Dim;
+  using system = DummySystem;
+
+  static constexpr Options::String help{
+      "Test anisotropic refinement by randomly refining a grid.\n"};
+
+  struct factory_creation
+      : tt::ConformsTo<Options::protocols::FactoryCreation> {
+    using factory_classes = tmpl::map<
+        tmpl::pair<DomainCreator<volume_dim>, domain_creators<volume_dim>>>;
+  };
+
+  using const_global_cache_tags = tmpl::list<>;
+
+  static constexpr auto default_phase_order =
+      std::array{Parallel::Phase::Initialization, Parallel::Phase::CheckDomain,
+                 Parallel::Phase::Exit};
+
+  using amr_element_component = DgElementArray<
+      RandomAmrMetavars,
+      tmpl::list<
+          Parallel::PhaseActions<
+              Parallel::Phase::Initialization,
+              tmpl::list<Initialization::Actions::InitializeItems<
+                             amr::Initialization::Domain<volume_dim>,
+                             amr::Initialization::Initialize<volume_dim>>,
+                         Parallel::Actions::TerminatePhase>>,
+          Parallel::PhaseActions<
+              Parallel::Phase::CheckDomain,
+              tmpl::list<::amr::Actions::SendAmrDiagnostics,
+                         Parallel::Actions::TerminatePhase>>>>;
+
+  using component_list =
+      tmpl::list<amr::Component<RandomAmrMetavars>, amr_element_component>;
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& /*p*/) {}
+};
+
+static const std::vector<void (*)()> charm_init_node_funcs{
+    &setup_error_handling, &domain::creators::register_derived_with_charm,
+    &Parallel::register_factory_classes_with_charm<metavariables>};
+static const std::vector<void (*)()> charm_init_proc_funcs{
+    &enable_floating_point_exceptions};
---
+++ src/ParallelAlgorithms/Amr/Actions/SendAmrDiagnostics.hpp
@@ -0,0 +1,83 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <boost/rational.hpp>
+#include <cstddef>
+#include <vector>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Domain/Amr/Helpers.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Parallel/AlgorithmExecution.hpp"
+#include "Parallel/GlobalCache.hpp"
+#include "Parallel/PupBoost.hpp"
+#include "Parallel/Reduction.hpp"
+#include "ParallelAlgorithms/Amr/Actions/Component.hpp"
+#include "ParallelAlgorithms/Amr/Actions/RunAmrDiagnostics.hpp"
+
+namespace amr::Actions {
+
+/// \brief Send AMR diagnostics about Element%s to amr::Component
+///
+/// Sends the following:
+/// - The fraction of a Block volume (in the logical coordinate frame) covered
+///   by the Element
+/// - One, in order to count the number of Element%s
+/// - The number of grid points
+/// - The refinement level in each logical dimension
+/// - The number of grid points in each logical dimension
+///
+/// The information is sent to amr::Component which runs the action
+/// amr::Actions::RunAmrDiagnostics after all Element%s have contributed to the
+/// reduction
+struct SendAmrDiagnostics {
+  using ReductionData = Parallel::ReductionData<
+      // fraction of Block volume
+      Parallel::ReductionDatum<boost::rational<size_t>, funcl::Plus<>>,
+      // number of elements
+      Parallel::ReductionDatum<size_t, funcl::Plus<>>,
+      // number of grid points
+      Parallel::ReductionDatum<size_t, funcl::Plus<>>,
+      // average refinement level by dimension
+      Parallel::ReductionDatum<std::vector<size_t>,
+                               funcl::ElementWise<funcl::Plus<>>>,
+      // average number of grid points by dimension
+      Parallel::ReductionDatum<std::vector<size_t>,
+                               funcl::ElementWise<funcl::Plus<>>>>;
+
+  template <typename DbTagList, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent>
+  static Parallel::iterable_action_return_t apply(
+      db::DataBox<DbTagList>& box,
+      tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+      const Parallel::GlobalCache<Metavariables>& cache,
+      const ArrayIndex& array_index, const ActionList /*meta*/,
+      const ParallelComponent* /*meta*/) {
+    constexpr size_t volume_dim = Metavariables::volume_dim;
+    const ElementId<volume_dim> element_id{array_index};
+    const auto& mesh = db::get<::domain::Tags::Mesh<volume_dim>>(box);
+    const auto& my_proxy =
+        Parallel::get_parallel_component<ParallelComponent>(cache)[array_index];
+    const auto& target_proxy =
+        Parallel::get_parallel_component<amr::Component<Metavariables>>(cache);
+    std::vector<size_t> refinement_levels_by_dim(volume_dim);
+    std::vector<size_t> extents_by_dim(volume_dim);
+    const auto refinement_levels = element_id.refinement_levels();
+    size_t number_of_grid_points = 1;
+    for (size_t d = 0; d < volume_dim; ++d) {
+      refinement_levels_by_dim[d] = gsl::at(refinement_levels, d);
+      extents_by_dim[d] = mesh.extents(d);
+      number_of_grid_points *= mesh.extents(d);
+    }
+    Parallel::contribute_to_reduction<amr::Actions::RunAmrDiagnostics>(
+        ReductionData{amr::fraction_of_block_volume(element_id), 1,
+                      number_of_grid_points, refinement_levels_by_dim,
+                      extents_by_dim},
+        my_proxy, target_proxy);
+    return {Parallel::AlgorithmExecution::Continue, std::nullopt};
+  }
+};
+}  // namespace amr::Actions
