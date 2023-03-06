6544d0cca1a216cf5805f89fc2b05931cfe6515d
Kyle Nelli <knelli@caltech.edu>
2023-03-06 15:54:21 -0800

    Print full command upon startup

--- src/Informer/Informer.cpp
+++ src/Informer/Informer.cpp
@@ -5,6 +5,7 @@
 
 #include <charm++.h>
 #include <charm.h>
+#include <sstream>
 
 #include "Informer/InfoFromBuild.hpp"
 #include "Parallel/Printf.hpp"
@@ -12,12 +13,20 @@
 #include "Utilities/System/ParallelInfo.hpp"
 
 void Informer::print_startup_info(CkArgMsg* msg) {
+  std::stringstream ss{};
+  for (int i = 0; i < msg->argc - 1; i++) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    ss << msg->argv[i] << " ";
+  }
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+  ss << msg->argv[msg->argc - 1];
+
   Parallel::printf(
       "\n"
       "Executing '%s' using %d processors.\n"
       "Charm++ startup time in seconds: %f\n"
       "Date and time at startup: %s\n",
-      msg->argv[0], sys::number_of_procs(),  // NOLINT
+      ss.str(), sys::number_of_procs(),  // NOLINT
       sys::wall_time(), current_date_and_time());
 
   Parallel::printf("%s\n", info_from_build());
