c8a1bdefd91dc707d78f7915a7a5eb8276f7c5e0
Kyle Nelli <knelli@caltech.edu>
2023-03-17 20:54:06 -0700

    Add time dep maps to TimeDepMapOptions

--- tests/Unit/Domain/Creators/Test_BinaryCompactObjectHelpers.cpp
+++ tests/Unit/Domain/Creators/Test_BinaryCompactObjectHelpers.cpp
@@ -15,7 +15,15 @@
 #include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
 #include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
 #include "Domain/FunctionsOfTime/QuaternionFunctionOfTime.hpp"
+#include "Utilities/CartesianProduct.hpp"
 #include "Utilities/Gsl.hpp"
+#include "Utilities/MakeArray.hpp"
+
+namespace Frame {
+struct Grid;
+struct Distorted;
+struct Inertial;
+}  // namespace Frame
 
 namespace domain::creators::bco {
 using ExpMapOptions = TimeDependentMapOptions::ExpansionMapOptions;
@@ -108,6 +116,70 @@ void test() {
       dynamic_cast<SizeFoT&>(
           *functions_of_time.at(gsl::at(TimeDependentMapOptions::size_names, 1))
                .get()) == size_B);
+
+  const std::array<std::array<double, 3>, 2> centers{
+      std::array{5.0, 0.01, 0.02}, std::array{-5.0, -0.01, -0.02}};
+  const double domain_outer_radius = 20.0;
+
+  for (const auto& [include_size_A, include_size_B] :
+       cartesian_product(make_array(true, false), make_array(true, false))) {
+    std::optional<double> inner_radius_A{};
+    std::optional<double> inner_radius_B{};
+    std::optional<double> outer_radius_A{};
+    std::optional<double> outer_radius_B{};
+    if (include_size_A) {
+      inner_radius_A = 0.8;
+      outer_radius_A = 3.2;
+    }
+    if (include_size_B) {
+      inner_radius_B = 0.5;
+      outer_radius_B = 2.1;
+    }
+
+    const std::array<std::optional<double>, 2> inner_radii{inner_radius_A,
+                                                           inner_radius_B};
+    const std::array<std::optional<double>, 2> outer_radii{outer_radius_A,
+                                                           outer_radius_B};
+
+    time_dep_options.build_maps(centers, inner_radii, outer_radii,
+                                domain_outer_radius);
+
+    const auto grid_to_distorted_map_A =
+        time_dep_options.grid_to_distorted_map<domain::ObjectLabel::A>(
+            not include_size_A);
+    const auto grid_to_distorted_map_B =
+        time_dep_options.grid_to_distorted_map<domain::ObjectLabel::B>(
+            not include_size_B);
+    const auto everything_map_A =
+        time_dep_options
+            .everything_grid_to_inertial_map<domain::ObjectLabel::A>(
+                include_size_A);
+    const auto everything_map_B =
+        time_dep_options
+            .everything_grid_to_inertial_map<domain::ObjectLabel::B>(
+                include_size_B);
+    const auto grid_to_inertial_map =
+        time_dep_options.frame_to_inertial_map<Frame::Grid>();
+    const auto dist_to_inertial_map =
+        time_dep_options.frame_to_inertial_map<Frame::Distorted>();
+
+    // All of these maps are tested individually. Rather than going through the
+    // effort of coming up with a source coordinate and calculating analytically
+    // what we would get after it's mapped, we just check that it's not the
+    // identity and that the jacobians are time dependent.
+    const auto check_map = [](const auto& map, const bool is_identity = false) {
+      CHECK(map->is_identity() == is_identity);
+      CHECK(map->inv_jacobian_is_time_dependent() == not is_identity);
+      CHECK(map->jacobian_is_time_dependent() == not is_identity);
+    };
+
+    check_map(grid_to_distorted_map_A, not include_size_A);
+    check_map(grid_to_distorted_map_B, not include_size_B);
+    check_map(everything_map_A);
+    check_map(everything_map_B);
+    check_map(grid_to_inertial_map);
+    check_map(dist_to_inertial_map);
+  }
 }
 }  // namespace
 
