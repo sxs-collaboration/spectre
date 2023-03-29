e568307234c3b492e67cbc1ccd02547a008b37aa
Kyle Nelli <knelli@caltech.edu>
2023-03-28 22:57:36 -0700

    Add function to compute time deriv of strahlkorper

---
+++ src/ApparentHorizons/TimeDerivStrahlkorper.cpp
@@ -0,0 +1,86 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "ApparentHorizons/TimeDerivStrahlkorper.hpp"
+
+#include <deque>
+#include <utility>
+
+#include "DataStructures/DataVector.hpp"
+#include "NumericalAlgorithms/FiniteDifference/NonUniform1D.hpp"
+#include "NumericalAlgorithms/SphericalHarmonics/Strahlkorper.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace Frame {
+struct Grid;
+struct Distorted;
+struct Inertial;
+}  // namespace Frame
+
+namespace ah {
+namespace {
+template <size_t NumTimes>
+static DataVector compute_coefs(
+    const std::deque<double>& times,
+    const std::deque<const DataVector*>& coefficients) {
+  const auto weights = fd::non_uniform_1d_weights<NumTimes>(times);
+
+  DataVector new_coefficients{coefficients.front()->size(), 0.0};
+
+  for (size_t i = 0; i < NumTimes; i++) {
+    new_coefficients += *coefficients[i] * gsl::at(gsl::at(weights, 1), i);
+  }
+
+  return new_coefficients;
+}
+}  // namespace
+
+template <typename Frame>
+void time_deriv_of_strahlkorper(
+    gsl::not_null<Strahlkorper<Frame>*> time_deriv,
+    const std::deque<std::pair<double, ::Strahlkorper<Frame>>>&
+        previous_strahlkorpers) {
+  std::deque<double> times{};
+  std::deque<const DataVector*> coefficients{};
+
+  for (const auto& [time, strahlkorper] : previous_strahlkorpers) {
+    times.emplace_back(time);
+    coefficients.emplace_back(&strahlkorper.coefficients());
+  }
+
+  DataVector new_coefficients{};
+
+  // Switch needed to convert times size into template parameter
+  switch (previous_strahlkorpers.size()) {
+    case 2:
+      new_coefficients = compute_coefs<2>(times, coefficients);
+      break;
+    case 3:
+      new_coefficients = compute_coefs<3>(times, coefficients);
+      break;
+    case 4:
+      new_coefficients = compute_coefs<4>(times, coefficients);
+      break;
+    default:
+      ERROR("Unsupported size for number of previous Strahlkorpers "
+            << previous_strahlkorpers.size());
+  }
+
+  time_deriv->coefficients() = std::move(new_coefficients);
+}
+
+#define FRAME(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATE(_, data)                           \
+  template void time_deriv_of_strahlkorper(            \
+      const gsl::not_null<Strahlkorper<FRAME(data)>*>, \
+      const std::deque<std::pair<double, ::Strahlkorper<FRAME(data)>>>&);
+
+GENERATE_INSTANTIATIONS(INSTANTIATE,
+                        (::Frame::Grid, ::Frame::Distorted, ::Frame::Inertial))
+
+#undef INSTANTIATE
+#undef FRAME
+}  // namespace ah
---
+++ tests/Unit/ApparentHorizons/Test_TimeDerivStrahlkorper.cpp
@@ -0,0 +1,75 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <array>
+#include <cstddef>
+#include <deque>
+#include <utility>
+
+#include "ApparentHorizons/TimeDerivStrahlkorper.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "NumericalAlgorithms/SphericalHarmonics/SpherepackIterator.hpp"
+#include "NumericalAlgorithms/SphericalHarmonics/Strahlkorper.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace {
+void test_time_deriv_strahlkorper() {
+  const size_t l_max = 2;
+  Strahlkorper<Frame::Inertial> strahlkorper{l_max, l_max, 1.0,
+                                             std::array{0.0, 0.0, 0.0}};
+  SpherepackIterator iter{l_max, l_max};
+  // Set a random coefficient non-zero
+  strahlkorper.coefficients()[iter.set(2, 1)()] = 1.3;
+
+  for (size_t num_times = 2; num_times <= 4; num_times++) {
+    CAPTURE(num_times);
+    std::deque<std::pair<double, Strahlkorper<Frame::Inertial>>>
+        previous_strahlkorpers{};
+
+    // Set all strahlkorpers to be the same
+    for (size_t i = 0; i < num_times; i++) {
+      previous_strahlkorpers.emplace_front(
+          std::make_pair(static_cast<double>(i), strahlkorper));
+    }
+
+    auto time_deriv = strahlkorper;
+
+    ah::time_deriv_of_strahlkorper(make_not_null(&time_deriv),
+                                   previous_strahlkorpers);
+
+    const DataVector& time_deriv_strahlkorper_coefs = time_deriv.coefficients();
+
+    // Since we made all the Strahlkorpers the same, the time deriv should be
+    // zero.
+    CHECK_ITERABLE_APPROX(
+        time_deriv_strahlkorper_coefs,
+        (DataVector{time_deriv_strahlkorper_coefs.size(), 0.0}));
+
+    // Check that the deriv works for 2 times (easy to calculate by hand)
+    if (num_times == 2) {
+      // Set a single coefficient to a random value for each previous
+      // strahlkorper
+      previous_strahlkorpers.front().second.coefficients()[iter.set(2, -1)()] =
+          2.0;
+      previous_strahlkorpers.back().second.coefficients()[iter()] = 2.5;
+
+      ah::time_deriv_of_strahlkorper(make_not_null(&time_deriv),
+                                     previous_strahlkorpers);
+
+      const DataVector& coefs = time_deriv.coefficients();
+
+      DataVector expected_coefs{coefs.size(), 0.0};
+      expected_coefs[iter()] = -0.5;
+
+      CHECK_ITERABLE_APPROX(coefs, expected_coefs);
+    }
+  }
+}
+
+SPECTRE_TEST_CASE("Unit.ApparentHorizons.TimeDerivStrahlkorper", "[Unit]") {
+  test_time_deriv_strahlkorper();
+}
+
+}  // namespace
