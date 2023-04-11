083d9a7f290b9cff6308a1beedfd94b7b3b251e1
Kyle Nelli <knelli@caltech.edu>
2023-04-11 16:26:48 -0700

    Add queue tags for size control

--- src/ControlSystem/Tags/QueueTags.hpp
+++ src/ControlSystem/Tags/QueueTags.hpp
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "DataStructures/Tensor/TypeAliases.hpp"
+
 /// \cond
 class DataVector;
 template <typename Frame>
@@ -32,5 +34,52 @@ struct Center {
 template <typename Frame>
 struct Horizon {
   using type = ::Strahlkorper<Frame>;
+};
+
+/// \ingroup ControlSystemGroup
+/// Holds a full strahlkorper from measurements for the excision surface
+template <typename Frame>
+struct ExcisionSurface {
+  using type = ::Strahlkorper<Frame>;
+};
+
+/// \ingroup ControlSystemGroup
+/// Holds the lapse on the `ExcisionSurface`
+struct LapseOnExcisionSurface {
+  using type = Scalar<DataVector>;
+};
+
+/*!
+ * \ingroup ControlSystemGroup
+ * \brief Holds a quantity that's similar to the shift, but isn't the shift, on
+ * the `ExcisionSurface`.
+ *
+ * \details This holds
+ *
+ * \f{equation}{
+ * \beta^i \frac{\partial x^\hat{i}}{\partial x^i} =
+ * \hat{beta}^\hat{i} + \frac{\partial x^\hat{i}}{\partial t}
+ * \f}
+ *
+ * where hatted quantities are in the distorted frame and non-hatted quantities
+ * are in the grid frame.
+ */
+template <typename Frame>
+struct ShiftyQuantity {
+  using type = tnsr::I<DataVector, 3, Frame>;
+};
+
+/// \ingroup ControlSystemGroup
+/// Holds the spatial metric on the `ExcisionSurface`
+template <typename Frame>
+struct SpatialMetricOnExcisionSurface {
+  using type = tnsr::ii<DataVector, 3, Frame>;
+};
+
+/// \ingroup ControlSystemGroup
+/// Holds the inverse spatial metric on the `ExcisionSurface`
+template <typename Frame>
+struct InverseSpatialMetricOnExcisionSurface {
+  using type = tnsr::II<DataVector, 3, Frame>;
 };
 }  // namespace control_system::QueueTags
