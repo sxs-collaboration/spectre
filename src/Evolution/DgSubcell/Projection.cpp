2f9ff7c684a42b436bf502d45cba17e3e46062c4
Francois Foucart <francois.foucart@gmail.com>
2023-04-24 14:03:14 -0700

    Add projection matrices for cell faces

--- src/Evolution/DgSubcell/Projection.cpp
+++ src/Evolution/DgSubcell/Projection.cpp
@@ -26,7 +26,36 @@ void project_impl(gsl::span<double> subcell_u,
   auto projection_mat = make_array<Dim>(std::cref(empty));
   for (size_t d = 0; d < Dim; d++) {
     gsl::at(projection_mat, d) = std::cref(
-        projection_matrix(dg_mesh.slice_through(d), subcell_extents[d]));
+        projection_matrix(dg_mesh.slice_through(d), subcell_extents[d],
+                          Spectral::Quadrature::CellCentered));
+  }
+  DataVector result{subcell_u.data(), subcell_u.size()};
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+  const DataVector u{const_cast<double*>(dg_u.data()), dg_u.size()};
+  apply_matrices(make_not_null(&result), projection_mat, u, dg_mesh.extents());
+}
+
+// Project to a face of a Cartesian grid. We reconstruct on a FaceCentered basis
+// in the dimension of the desired face, and on a CellCentered basis in other
+// dimensions.
+template <size_t Dim>
+void project_to_face_impl(gsl::span<double> subcell_u,
+                          const gsl::span<const double> dg_u,
+                          const Mesh<Dim>& dg_mesh,
+                          const Index<Dim>& subcell_extents,
+                          const size_t& face_direction) {
+  const Matrix empty{};
+  auto projection_mat = make_array<Dim>(std::cref(empty));
+  for (size_t d = 0; d < Dim; d++) {
+    if (d == face_direction) {
+      gsl::at(projection_mat, d) = std::cref(
+          projection_matrix(dg_mesh.slice_through(d), subcell_extents[d],
+                            Spectral::Quadrature::FaceCentered));
+    } else {
+      gsl::at(projection_mat, d) = std::cref(
+          projection_matrix(dg_mesh.slice_through(d), subcell_extents[d],
+                            Spectral::Quadrature::CellCentered));
+    }
   }
   DataVector result{subcell_u.data(), subcell_u.size()};
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
@@ -61,17 +90,57 @@ DataVector project(const DataVector& dg_u, const Mesh<Dim>& dg_mesh,
   return subcell_u;
 }
 
+template <size_t Dim>
+void project_to_face(const gsl::not_null<DataVector*> subcell_u,
+                     const DataVector& dg_u, const Mesh<Dim>& dg_mesh,
+                     const Index<Dim>& subcell_extents,
+                     const size_t& face_direction) {
+  ASSERT(dg_u.size() % dg_mesh.number_of_grid_points() == 0,
+         "The vector dg_u must have size that is a multiple of the number of "
+         "grid points "
+             << dg_mesh.number_of_grid_points() << " but got " << dg_u.size());
+  subcell_u->destructive_resize(subcell_extents.product() * dg_u.size() /
+                                dg_mesh.number_of_grid_points());
+  detail::project_to_face_impl(
+      gsl::span<double>{subcell_u->data(), subcell_u->size()},
+      gsl::span<const double>{dg_u.data(), dg_u.size()}, dg_mesh,
+      subcell_extents, face_direction);
+}
+
+template <size_t Dim>
+DataVector project_to_face(const DataVector& dg_u, const Mesh<Dim>& dg_mesh,
+                           const Index<Dim>& subcell_extents,
+                           const size_t& face_direction) {
+  ASSERT(dg_u.size() % dg_mesh.number_of_grid_points() == 0,
+         "The vector dg_u must have size that is a multiple of the number of "
+         "grid points "
+             << dg_mesh.number_of_grid_points() << " but got " << dg_u.size());
+  DataVector subcell_u{};
+  project_to_face(&subcell_u, dg_u, dg_mesh, subcell_extents, face_direction);
+  return subcell_u;
+}
+
 #define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
 
-#define INSTANTIATION(r, data)                                            \
-  template DataVector project(const DataVector&, const Mesh<DIM(data)>&,  \
-                              const Index<DIM(data)>&);                   \
-  template void project(gsl::not_null<DataVector*>, const DataVector&,    \
-                        const Mesh<DIM(data)>&, const Index<DIM(data)>&); \
-  template void detail::project_impl(gsl::span<double> subcell_u,         \
-                                     const gsl::span<const double> dg_u,  \
-                                     const Mesh<DIM(data)>& dg_mesh,      \
-                                     const Index<DIM(data)>& subcell_extents);
+#define INSTANTIATION(r, data)                                                 \
+  template DataVector project(const DataVector&, const Mesh<DIM(data)>&,       \
+                              const Index<DIM(data)>&);                        \
+  template void project(gsl::not_null<DataVector*>, const DataVector&,         \
+                        const Mesh<DIM(data)>&, const Index<DIM(data)>&);      \
+  template void detail::project_impl(gsl::span<double> subcell_u,              \
+                                     const gsl::span<const double> dg_u,       \
+                                     const Mesh<DIM(data)>& dg_mesh,           \
+                                     const Index<DIM(data)>& subcell_extents); \
+  template DataVector project_to_face(const DataVector&,                       \
+                                      const Mesh<DIM(data)>&,                  \
+                                      const Index<DIM(data)>&, const size_t&); \
+  template void project_to_face(gsl::not_null<DataVector*>, const DataVector&, \
+                                const Mesh<DIM(data)>&,                        \
+                                const Index<DIM(data)>&, const size_t&);       \
+  template void detail::project_to_face_impl(                                  \
+      gsl::span<double> subcell_u, const gsl::span<const double> dg_u,         \
+      const Mesh<DIM(data)>& dg_mesh, const Index<DIM(data)>& subcell_extents, \
+      const size_t& face_direction);
 
 GENERATE_INSTANTIATIONS(INSTANTIATION, (1, 2, 3))
 
--- src/Evolution/DgSubcell/Projection.hpp
+++ src/Evolution/DgSubcell/Projection.hpp
@@ -24,6 +24,12 @@ namespace detail {
 template <size_t Dim>
 void project_impl(gsl::span<double> subcell_u, gsl::span<const double> dg_u,
                   const Mesh<Dim>& dg_mesh, const Index<Dim>& subcell_extents);
+template <size_t Dim>
+void project_to_face_impl(gsl::span<double> subcell_u,
+                          gsl::span<const double> dg_u,
+                          const Mesh<Dim>& dg_mesh,
+                          const Index<Dim>& subcell_extents,
+                          const size_t& face_direction);
 }  // namespace detail
 
 /// @{
@@ -75,6 +81,55 @@ Variables<TagList> project(const Variables<TagList>& dg_u,
                            const Index<Dim>& subcell_extents) {
   Variables<TagList> subcell_u(subcell_extents.product());
   project(make_not_null(&subcell_u), dg_u, dg_mesh, subcell_extents);
+  return subcell_u;
+}
+
+template <size_t Dim>
+DataVector project_to_face(const DataVector& dg_u, const Mesh<Dim>& dg_mesh,
+                           const Index<Dim>& subcell_extents,
+                           const size_t& face_direction);
+
+template <size_t Dim>
+void project_to_face(gsl::not_null<DataVector*> subcell_u,
+                     const DataVector& dg_u, const Mesh<Dim>& dg_mesh,
+                     const Index<Dim>& subcell_extents,
+                     const size_t& face_direction);
+
+template <typename SubcellTagList, typename DgTagList, size_t Dim>
+void project_to_face(const gsl::not_null<Variables<SubcellTagList>*> subcell_u,
+                     const Variables<DgTagList>& dg_u, const Mesh<Dim>& dg_mesh,
+                     const Index<Dim>& subcell_extents,
+                     const size_t& face_direction) {
+  static_assert(
+      std::is_same_v<
+          tmpl::transform<SubcellTagList,
+                          tmpl::bind<db::remove_all_prefixes, tmpl::_1>>,
+          tmpl::transform<DgTagList,
+                          tmpl::bind<db::remove_all_prefixes, tmpl::_1>>>,
+      "DG and subcell tag lists must be the same once prefix tags "
+      "are removed.");
+  ASSERT(dg_u.number_of_grid_points() == dg_mesh.number_of_grid_points(),
+         "dg_u has incorrect size " << dg_u.number_of_grid_points()
+                                    << " since the mesh is size "
+                                    << dg_mesh.number_of_grid_points());
+  if (UNLIKELY(subcell_u->number_of_grid_points() !=
+               subcell_extents.product())) {
+    subcell_u->initialize(subcell_extents.product());
+  }
+  detail::project_to_face_impl(
+      gsl::span<double>{subcell_u->data(), subcell_u->size()},
+      gsl::span<const double>{dg_u.data(), dg_u.size()}, dg_mesh,
+      subcell_extents, face_direction);
+}
+
+template <typename TagList, size_t Dim>
+Variables<TagList> project_to_face(const Variables<TagList>& dg_u,
+                                   const Mesh<Dim>& dg_mesh,
+                                   const Index<Dim>& subcell_extents,
+                                   const size_t& face_direction) {
+  Variables<TagList> subcell_u(subcell_extents.product());
+  project_to_face(make_not_null(&subcell_u), dg_u, dg_mesh, subcell_extents,
+                  face_direction);
   return subcell_u;
 }
 /// @}
