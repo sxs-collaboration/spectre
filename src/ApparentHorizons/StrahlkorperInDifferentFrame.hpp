03793947bf16b3bcd637ec628ec1edd3b9613dab
Mark Scheel <scheel@tapir.caltech.edu>
2023-04-28 13:29:19 -0700

    Add StrahlkorperInDifferentFrameAligned.
    This is a simplified version of StrahlkorperInDifferentFrame,
    that assumes:
     * The map preserves the center of the Strahlkorper.
     * For each point on the Strahlkorper, the map preserves the angles
       between that point and the center.

--- src/ApparentHorizons/StrahlkorperInDifferentFrame.hpp
+++ src/ApparentHorizons/StrahlkorperInDifferentFrame.hpp
@@ -26,13 +26,35 @@ class not_null;
 /// The destination Strahlkorper has the same l_max() and m_max() as the
 /// source Strahlkorper.
 ///
-/// Note that because the Blocks inside the Domain allow access to
+/// \note Because the Blocks inside the Domain allow access to
 /// maps only between a selected subset of frames, we cannot use
 /// strahlkorper_in_different_frame to map between arbitrary frames;
 /// allowing strahlkorper_in_different_frame to work on more frames
 /// requires adding member functions to Block.
 template <typename SrcFrame, typename DestFrame>
 void strahlkorper_in_different_frame(
+    gsl::not_null<Strahlkorper<DestFrame>*> dest_strahlkorper,
+    const Strahlkorper<SrcFrame>& src_strahlkorper, const Domain<3>& domain,
+    const std::unordered_map<
+        std::string, std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>&
+        functions_of_time,
+    double time);
+
+/// \brief Transforms a Strahlkorper from SrcFrame to DestFrame, for easy maps.
+///
+/// This is a simplified version of strahlkorper_in_different_frame
+/// with the following two assumptions: First, the map leaves the
+/// center of the Strahlkorper invariant. Second, for every point on
+/// the Strahlkorper, the map leaves the angles about the center
+/// (i.e. the quantity
+/// \f$(x^i-c^i)/\sqrt{(x^0-c^0)^2+(x^1-c^1)^2+(x^2-c^2)^2}\f$)
+/// invariant. If those assumptions are not satisfied, then
+/// strahlkorper_in_different_frame_aligned will give the wrong answer
+/// and you must use strahlkorper_in_different_frame.  Those
+/// assumptions are satisfied for the shape and size maps but not for
+/// general maps.
+template <typename SrcFrame, typename DestFrame>
+void strahlkorper_in_different_frame_aligned(
     gsl::not_null<Strahlkorper<DestFrame>*> dest_strahlkorper,
     const Strahlkorper<SrcFrame>& src_strahlkorper, const Domain<3>& domain,
     const std::unordered_map<
