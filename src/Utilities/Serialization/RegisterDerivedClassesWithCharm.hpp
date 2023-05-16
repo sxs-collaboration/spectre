f82d2923a1961fc3c93bacac064d47f97b851841
Nils Vu <owls@nilsvu.de>
2023-05-16 16:28:38 -0700

    Remove whitespace from registration names
    This change breaks compatibility with data written by older versions of
    the code (and intends to prevent such compatibility issues in the
    future).

--- src/Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp
+++ src/Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp
@@ -6,12 +6,52 @@
 
 #pragma once
 
+#include <boost/algorithm/string.hpp>
 #include <pup.h>
 #include <typeinfo>
 
 #include "Utilities/PrettyType.hpp"
 #include "Utilities/Serialization/CharmPupable.hpp"
 #include "Utilities/TMPL.hpp"
+
+/*!
+ * \brief String representation of a type that is somewhat stable
+ *
+ * This string representation is used to identify the type in serialized data,
+ * including data written to disk. Therefore, the registration name must be
+ * somewhat reliable across invocations of the same program, across different
+ * compilers, and across different executables. C++ provides no standard tool
+ * for this. In particular, the name returned by `typeid().name()` explicitly
+ * provides no guarantees, though for GCC and Clang it returns the mangled name
+ * which should be ABI-stable. See docs:
+ * https://en.cppreference.com/w/cpp/types/type_info/name
+ *
+ * To obtain a somewhat reliable string representation we use
+ * `pretty_type::get_name` (which actually demangles the `typeid().name()`). It
+ * also doesn't make any guarantees, but at least it gives a human-readable
+ * string representation close to the actual source code. We remove whitespaces
+ * from the name because we found inconsistent whitespace on different machines,
+ * likely because the compiler's internal demangling routine got updated:
+ * https://github.com/sxs-collaboration/spectre/issues/4944
+ *
+ * To improve reliability further in the future we may call a dedicated function
+ * on `T`, such as `static std::string registration_name()`, which is expected
+ * to return a unique name (including any template parameters etc). However,
+ * adding this requirement to all PUP::able classes requires quite a lot of code
+ * on our part (but is ultimately the safest option).
+ *
+ * \warning Renaming classes or namespaces will change the registration name and
+ * hence break compatibility with data written by older versions of the code, as
+ * does changing the implementation of this function. This means we can't
+ * deserialize coordinate maps written in H5 files for interpolation of volume
+ * data.
+ */
+template <typename T>
+std::string registration_name() {
+  std::string result = pretty_type::get_name<T>();
+  boost::algorithm::erase_all(result, " ");
+  return result;
+}
 
 /// Register specified classes.  This function can either take classes
 /// to register as template arguments or take a `tmpl::list` of
@@ -21,22 +61,11 @@ void register_classes_with_charm(
     const tmpl::list<Registrants...> /*meta*/ = {}) {
   const auto helper = [](auto class_v) {
     using class_to_register = typename decltype(class_v)::type;
-    // Notes:
-    // - We use PUPable_reg2 because this takes as a second argument the name of
-    //   the class (as a `const char*`), while PUPable_reg converts the argument
-    //   verbatim to a string using the `#` preprocessor operator.
-    // - We use pretty_type to get a string representation of the class that is
-    //   somewhat reliable: across invocations of the same program, across
-    //   different compilers, and across different executables. This reliability
-    //   is important when we serialize PUPable classes to disk and read them
-    //   back in. Note that typeid().name() provides no such guarantees. To
-    //   improve reliability further in the future (e.g. to guard against
-    //   changing pretty_type output) we may call a dedicated function on
-    //   `class_to_register`, such as `static std::string registration_name()`,
-    //   which is expected to return a unique name (including any template
-    //   parameters etc).
+    // We use PUPable_reg2 because this takes as a second argument the name of
+    // the class (as a `const char*`), while PUPable_reg converts the argument
+    // verbatim to a string using the `#` preprocessor operator.
     PUPable_reg2(class_to_register,
-                 pretty_type::get_name<class_to_register>().c_str());
+                 registration_name<class_to_register>().c_str());
   };
   (void)helper;
   EXPAND_PACK_LEFT_TO_RIGHT(helper(tmpl::type_<Registrants>{}));
