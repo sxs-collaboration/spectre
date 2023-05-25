447d5f7a47fbb2fe6edde3cb9c55108d7acccee4
William Throwe <wtt6@cornell.edu>
2023-05-25 12:56:51 -0400

    Add ScopedFpeState RAII class

--- src/Utilities/ErrorHandling/FloatingPointExceptions.cpp
+++ src/Utilities/ErrorHandling/FloatingPointExceptions.cpp
@@ -3,26 +3,25 @@
 
 #include "Utilities/ErrorHandling/FloatingPointExceptions.hpp"
 
+#include "Utilities/ErrorHandling/Assert.hpp"
 #include "Utilities/ErrorHandling/Error.hpp"
 
 #include <csignal>
 
-#ifdef __APPLE__
-#ifdef __arm64__
-#include <fenv.h>
-#else
+#if SPECTRE_FPE_CSR
 #include <xmmintrin.h>
-#endif
-#else
+#elif SPECTRE_FPE_FENV
 #include <cfenv>
 #endif
 
 namespace {
 
-#ifdef __APPLE__
-#ifndef __arm64__
-auto old_mask = _mm_getcsr();
-#endif
+#if SPECTRE_FPE_CSR
+const unsigned int disabled_mask = _mm_getcsr();
+const unsigned int exception_flags =
+    _MM_MASK_MASK & ~(_MM_MASK_OVERFLOW | _MM_MASK_INVALID | _MM_MASK_DIV_ZERO);
+#elif SPECTRE_FPE_FENV
+const int exception_flags = FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW;
 #endif
 
 [[noreturn]] void fpe_signal_handler(int /*signal*/) {
@@ -31,23 +30,62 @@ auto old_mask = _mm_getcsr();
 }  // namespace
 
 void enable_floating_point_exceptions() {
-#ifdef __APPLE__
-#ifndef __arm64__
-  _mm_setcsr(_MM_MASK_MASK &
-             ~(_MM_MASK_OVERFLOW | _MM_MASK_INVALID | _MM_MASK_DIV_ZERO));
-#endif
-#else
-  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+#if SPECTRE_FPE_CSR
+  _mm_setcsr(exception_flags);
+#elif SPECTRE_FPE_FENV
+  feenableexcept(exception_flags);
 #endif
   std::signal(SIGFPE, fpe_signal_handler);
 }
 
 void disable_floating_point_exceptions() {
-#ifdef __APPLE__
-#ifndef __arm64__
-  _mm_setcsr(old_mask);
+#if SPECTRE_FPE_CSR
+  _mm_setcsr(disabled_mask);
+#elif SPECTRE_FPE_FENV
+  fedisableexcept(exception_flags);
 #endif
+}
+
+ScopedFpeState::~ScopedFpeState() { restore_exceptions(); }
+
+ScopedFpeState::ScopedFpeState() { save_exceptions(); }
+
+ScopedFpeState::ScopedFpeState(const bool exceptions_enabled)
+    : ScopedFpeState() {
+  set_exceptions(exceptions_enabled);
+}
+
+ScopedFpeState::ScopedFpeState(ScopedFpeState::DoNotSave /*meta*/) {}
+
+void ScopedFpeState::set_exceptions(const bool exceptions_enabled) const {
+  ASSERT(original_state_.has_value(), "FPE state not saved.");
+  if (exceptions_enabled) {
+    enable_floating_point_exceptions();
+  } else {
+    disable_floating_point_exceptions();
+  }
+}
+
+void ScopedFpeState::save_exceptions() {
+  ASSERT(not original_state_.has_value(), "FPE state already saved.");
+#if SPECTRE_FPE_CSR
+  original_state_.emplace(_mm_getcsr());
+#elif SPECTRE_FPE_FENV
+  original_state_.emplace(fegetexcept());
 #else
-  fedisableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+  original_state_.emplace();
 #endif
+}
+
+void ScopedFpeState::restore_exceptions() {
+  if (not original_state_.has_value()) {
+    return;
+  }
+#if SPECTRE_FPE_CSR
+  _mm_setcsr(*original_state_);
+#elif SPECTRE_FPE_FENV
+  fedisableexcept(exception_flags);
+  feenableexcept(*original_state_);
+#endif
+  original_state_.reset();
 }
--- src/Utilities/ErrorHandling/FloatingPointExceptions.hpp
+++ src/Utilities/ErrorHandling/FloatingPointExceptions.hpp
@@ -6,6 +6,18 @@
 
 #pragma once
 
+#include <optional>
+
+/// \cond
+#ifdef __APPLE__
+#ifndef __arm64__
+#define SPECTRE_FPE_CSR 1
+#endif
+#else
+#define SPECTRE_FPE_FENV 1
+#endif
+/// \endcond
+
 /// \ingroup ErrorHandlingGroup
 /// After a call to this function, the code will terminate with a floating
 /// point exception on overflow, divide-by-zero, and invalid operations.
@@ -14,4 +26,60 @@ void enable_floating_point_exceptions();
 /// \ingroup ErrorHandlingGroup
 /// After a call to this function, the code will NOT terminate with a floating
 /// point exception on overflow, divide-by-zero, and invalid operations.
+///
+/// \warning Do not use this function to temporarily disable FPEs,
+/// because it will not interact correctly with C++ exceptions.  Use
+/// `ScopedFpeState` instead.
 void disable_floating_point_exceptions();
+
+/// \ingroup ErrorHandlingGroup
+/// An RAII object to temporarily modify the handling of floating
+/// point exceptions.
+class ScopedFpeState {
+ public:
+  ScopedFpeState(const ScopedFpeState&) = delete;
+  ScopedFpeState(ScopedFpeState&&) = delete;
+  ScopedFpeState& operator=(const ScopedFpeState&) = delete;
+  ScopedFpeState& operator=(ScopedFpeState&&) = delete;
+  ~ScopedFpeState();
+
+  /// Start a scope that will be restored, without changing the
+  /// current state.
+  ScopedFpeState();
+  /// Start a scope with the specified exception state.  This is
+  /// equivalent to calling the default constructor followed by
+  /// `set_exceptions`.
+  explicit ScopedFpeState(bool exceptions_enabled);
+
+  struct DoNotSave {};
+  /// Start a scope without saving the current state.  The only valid
+  /// method call from this state is `save_exceptions`.
+  explicit ScopedFpeState(DoNotSave /*meta*/);
+
+  /// Enable or disable floating point exceptions.  It is an error if
+  /// the exception state is not currently saved.
+  void set_exceptions(bool exceptions_enabled) const;
+
+  /// Save the current exception handling state after it has been
+  /// cleared by `restore_exceptions`.  It will be restored by a later
+  /// call to `restore_exceptions`.  It is an error to call this if
+  /// a state is already saved.
+  void save_exceptions();
+
+  /// Restore the FPE handling to the internally saved state if
+  /// present and clear that state.  This is called automatically by
+  /// the destructor if it is not called manually.
+  void restore_exceptions();
+
+ private:
+#if SPECTRE_FPE_CSR
+  std::optional<unsigned int> original_state_;
+#elif SPECTRE_FPE_FENV
+  std::optional<int> original_state_;
+#else
+  // FPEs not supported, but this is still used to check method calls
+  // are valid.
+  struct DummyState {};
+  std::optional<DummyState> original_state_;
+#endif
+};
