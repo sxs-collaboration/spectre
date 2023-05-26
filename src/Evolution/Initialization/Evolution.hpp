6a2a592117525a612cbe17cdc4e5adf12a216448
Lawrence Kidder <kidder@astro.cornell.edu>
2023-05-25 20:16:44 -0400

    Add AMR mutator for time stepping items

--- src/Evolution/Initialization/Evolution.hpp
+++ src/Evolution/Initialization/Evolution.hpp
@@ -3,15 +3,25 @@
 
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <optional>
+#include <unordered_map>
+#include <utility>
 
+#include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/PrefixHelpers.hpp"
 #include "DataStructures/DataBox/Prefixes.hpp"
+#include "Domain/Amr/Flag.hpp"
+#include "Domain/Amr/Helpers.hpp"
+#include "Domain/Amr/Tags/Flags.hpp"
+#include "Domain/Structure/ElementId.hpp"
 #include "Domain/Tags.hpp"
 #include "Evolution/Initialization/Tags.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "Parallel/Tags/ArrayIndex.hpp"
+#include "Time/AdaptiveSteppingDiagnostics.hpp"
 #include "Time/ChooseLtsStepSize.hpp"
 #include "Time/Slab.hpp"
 #include "Time/StepChoosers/StepChooser.hpp"
@@ -26,8 +36,10 @@
 #include "Time/TimeStepId.hpp"
 #include "Time/TimeSteppers/LtsTimeStepper.hpp"
 #include "Time/TimeSteppers/TimeStepper.hpp"
+#include "Utilities/Algorithm.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
 
 namespace Initialization {
 
@@ -141,6 +153,105 @@ struct TimeStepping {
                                   time_runs_forward, time_stepper);
     *time_step = (time_runs_forward ? 1 : -1) * initial_time.slab().duration();
     *next_time_step = *time_step;
+  }
+};
+
+/// \brief Initialize/update items related to time stepping after an AMR change
+template <size_t Dim>
+struct ProjectTimeStepping {
+  using return_tags =
+      tmpl::list<::Tags::TimeStepId, ::Tags::Next<::Tags::TimeStepId>,
+                 ::Tags::TimeStep, ::Tags::Next<::Tags::TimeStep>, ::Tags::Time,
+                 ::Tags::AdaptiveSteppingDiagnostics>;
+  using argument_tags = tmpl::list<Parallel::Tags::ArrayIndex>;
+
+  static void apply(
+      const gsl::not_null<TimeStepId*> /*time_step_id*/,
+      const gsl::not_null<TimeStepId*> /*next_time_step_id*/,
+      const gsl::not_null<TimeDelta*> /*time_step*/,
+      const gsl::not_null<TimeDelta*> /*next_time_step*/,
+      const gsl::not_null<double*> /*time*/,
+      const gsl::not_null<AdaptiveSteppingDiagnostics*>
+      /*adaptive_stepping_diagnostics*/,
+      const ElementId<Dim>& /*element_id*/,
+      const std::pair<Mesh<Dim>, Element<Dim>>& /*old_mesh_and_element*/) {
+    // Do not change anything for p-refinement
+  }
+
+  template <typename... Tags>
+  static void apply(const gsl::not_null<TimeStepId*> time_step_id,
+                    const gsl::not_null<TimeStepId*> next_time_step_id,
+                    const gsl::not_null<TimeDelta*> time_step,
+                    const gsl::not_null<TimeDelta*> next_time_step,
+                    const gsl::not_null<double*> time,
+                    const gsl::not_null<AdaptiveSteppingDiagnostics*>
+                        adaptive_stepping_diagnostics,
+                    const ElementId<Dim>& element_id,
+                    const tuples::TaggedTuple<Tags...>& parent_items) {
+    *time_step_id = get<::Tags::TimeStepId>(parent_items);
+    *next_time_step_id = get<::Tags::Next<::Tags::TimeStepId>>(parent_items);
+    *time_step = get<::Tags::TimeStep>(parent_items);
+    *next_time_step = get<::Tags::Next<::Tags::TimeStep>>(parent_items);
+    *time = get<::Tags::Time>(parent_items);
+
+    // Since AdaptiveSteppingDiagnostics are reduced over all elements, we
+    // set the slab quantities to the same value over all children, and the
+    // step quantities to belong to the first child
+    const auto& parent_diagnostics =
+        get<::Tags::AdaptiveSteppingDiagnostics>(parent_items);
+    const auto& parent_amr_flags = get<amr::Tags::Flags<Dim>>(parent_items);
+    const auto& parent_id =
+        get<Parallel::Tags::ArrayIndexImpl<ElementId<Dim>>>(parent_items);
+    auto children_ids = amr::ids_of_children(parent_id, parent_amr_flags);
+    if (element_id == children_ids.front()) {
+      *adaptive_stepping_diagnostics = parent_diagnostics;
+    } else {
+      adaptive_stepping_diagnostics->number_of_slabs =
+          parent_diagnostics.number_of_slabs;
+      adaptive_stepping_diagnostics->number_of_slab_size_changes =
+          parent_diagnostics.number_of_slab_size_changes;
+    }
+  }
+
+  template <typename... Tags>
+  static void apply(
+      const gsl::not_null<TimeStepId*> time_step_id,
+      const gsl::not_null<TimeStepId*> next_time_step_id,
+      const gsl::not_null<TimeDelta*> time_step,
+      const gsl::not_null<TimeDelta*> next_time_step,
+      const gsl::not_null<double*> time,
+      const gsl::not_null<AdaptiveSteppingDiagnostics*>
+          adaptive_stepping_diagnostics,
+      const ElementId<Dim>& /*element_id*/,
+      const std::unordered_map<ElementId<Dim>, tuples::TaggedTuple<Tags...>>&
+          children_items) {
+    const auto slowest_child =
+        alg::min_element(children_items, [](const auto& a, const auto& b) {
+          const auto& time_step_a = get<::Tags::TimeStep>(a.second);
+          const auto& time_step_b = get<::Tags::TimeStep>(b.second);
+          ASSERT(time_step_a.is_positive() == time_step_b.is_positive(),
+                 "Elements are not taking time steps in the same direction!");
+          return time_step_a.is_positive() ? (time_step_a < time_step_b)
+                                           : (time_step_a > time_step_b);
+        });
+    const auto& slowest_child_items = (*slowest_child).second;
+    *time_step_id = get<::Tags::TimeStepId>(slowest_child_items);
+    *next_time_step_id =
+        get<::Tags::Next<::Tags::TimeStepId>>(slowest_child_items);
+    *time_step = get<::Tags::TimeStep>(slowest_child_items);
+    *next_time_step = get<::Tags::Next<::Tags::TimeStep>>(slowest_child_items);
+    *time = get<::Tags::Time>(slowest_child_items);
+    const auto& slowest_child_diagnostics =
+        get<::Tags::AdaptiveSteppingDiagnostics>(slowest_child_items);
+
+    adaptive_stepping_diagnostics->number_of_slabs =
+        slowest_child_diagnostics.number_of_slabs;
+    adaptive_stepping_diagnostics->number_of_slab_size_changes =
+        slowest_child_diagnostics.number_of_slab_size_changes;
+    for (const auto& [_, child_items] : children_items) {
+      *adaptive_stepping_diagnostics +=
+          get<::Tags::AdaptiveSteppingDiagnostics>(child_items);
+    }
   }
 };
 
--- tests/Unit/Evolution/Initialization/Test_Evolution.cpp
+++ tests/Unit/Evolution/Initialization/Test_Evolution.cpp
@@ -3,24 +3,42 @@
 
 #include "Framework/TestingFramework.hpp"
 
+#include <array>
+#include <cstddef>
 #include <memory>
+#include <unordered_map>
 #include <utility>
 
 #include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "Domain/Amr/Flag.hpp"
+#include "Domain/Amr/Tags/Flags.hpp"
+#include "Domain/Structure/Direction.hpp"
+#include "Domain/Structure/DirectionMap.hpp"
+#include "Domain/Structure/Element.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Structure/Neighbors.hpp"
 #include "Evolution/Initialization/Evolution.hpp"
 #include "Evolution/Initialization/Tags.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "Options/Protocols/FactoryCreation.hpp"
 #include "Parallel/GlobalCache.hpp"
+#include "Parallel/Tags/ArrayIndex.hpp"
+#include "Time/AdaptiveSteppingDiagnostics.hpp"
 #include "Time/ChooseLtsStepSize.hpp"
 #include "Time/Slab.hpp"
 #include "Time/StepChoosers/Increase.hpp"
+#include "Time/Tags/AdaptiveSteppingDiagnostics.hpp"
 #include "Time/Tags/Time.hpp"
 #include "Time/Tags/TimeStep.hpp"
 #include "Time/Tags/TimeStepId.hpp"
 #include "Time/Tags/TimeStepper.hpp"
 #include "Time/Time.hpp"
 #include "Time/TimeSteppers/AdamsBashforth.hpp"
+#include "Utilities/Algorithm.hpp"
+#include "Utilities/Gsl.hpp"
 #include "Utilities/ProtocolHelpers.hpp"
+#include "Utilities/Rational.hpp"
 #include "Utilities/TMPL.hpp"
 #include "Utilities/TaggedTuple.hpp"
 
@@ -131,10 +149,176 @@ void test_lts() {
   CHECK(db::get<::Tags::Next<::Tags::TimeStep>>(box) ==
         expected_next_time_step);
 }
+using items_type =
+    tuples::TaggedTuple<Parallel::Tags::ArrayIndexImpl<ElementId<1>>,
+                        ::Tags::TimeStepId, ::Tags::Next<::Tags::TimeStepId>,
+                        ::Tags::TimeStep, ::Tags::Next<::Tags::TimeStep>,
+                        ::Tags::Time, ::Tags::AdaptiveSteppingDiagnostics>;
+
+using parent_items_type =
+    tuples::TaggedTuple<Parallel::Tags::ArrayIndexImpl<ElementId<1>>,
+                        ::Tags::TimeStepId, ::Tags::Next<::Tags::TimeStepId>,
+                        ::Tags::TimeStep, ::Tags::Next<::Tags::TimeStep>,
+                        ::Tags::Time, ::Tags::AdaptiveSteppingDiagnostics,
+                        ::amr::Tags::Flags<1>>;
+
+template <typename DbTagList>
+void check(const db::DataBox<DbTagList>& box,
+           const TimeStepId& expected_time_step_id,
+           const TimeStepId& expected_next_time_step_id,
+           const TimeDelta& expected_time_step,
+           const TimeDelta& expected_next_time_step, const double expected_time,
+           const AdaptiveSteppingDiagnostics& expected_diagnostics) {
+  CHECK(db::get<::Tags::TimeStepId>(box) == expected_time_step_id);
+  CHECK(db::get<::Tags::Next<::Tags::TimeStepId>>(box) ==
+        expected_next_time_step_id);
+  CHECK(db::get<::Tags::TimeStep>(box) == expected_time_step);
+  CHECK(db::get<::Tags::Next<::Tags::TimeStep>>(box) ==
+        expected_next_time_step);
+  CHECK(db::get<::Tags::Time>(box) == expected_time);
+  CHECK(db::get<::Tags::AdaptiveSteppingDiagnostics>(box) ==
+        expected_diagnostics);
+}
+
+void test_p_refine() {
+  const ElementId<1> element_id{0};
+  const Element<1> element{element_id, DirectionMap<1, Neighbors<1>>{}};
+  const Mesh<1> mesh{2, Spectral::Basis::Legendre,
+                     Spectral::Quadrature::GaussLobatto};
+  const Slab slab(0., 1.);
+  const Time start{slab.start()};
+  const TimeDelta time_step{slab.duration()};
+  const TimeDelta next_time_step = time_step;
+  const TimeStepId time_step_id{time_step.is_positive(), 8, start};
+  const TimeStepId next_time_step_id{time_step.is_positive(), 8,
+                                     start + time_step};
+  const double time = start.value();
+  const AdaptiveSteppingDiagnostics diagnostics{7, 2, 13, 4, 5};
+
+  auto box = db::create<
+      db::AddSimpleTags<Parallel::Tags::ArrayIndexImpl<ElementId<1>>,
+                        ::Tags::TimeStepId, ::Tags::Next<::Tags::TimeStepId>,
+                        ::Tags::TimeStep, ::Tags::Next<::Tags::TimeStep>,
+                        ::Tags::Time, ::Tags::AdaptiveSteppingDiagnostics>>(
+      element_id, time_step_id, next_time_step_id, time_step, next_time_step,
+      time, diagnostics);
+
+  db::mutate_apply<Initialization::ProjectTimeStepping<1>>(
+      make_not_null(&box), std::make_pair(mesh, element));
+
+  check(box, time_step_id, next_time_step_id, time_step, next_time_step, time,
+        diagnostics);
+}
+
+void test_split() {
+  const ElementId<1> parent_id{0};
+  const ElementId<1> child_1_id{0, std::array{SegmentId{1, 0}}};
+  const ElementId<1> child_2_id{0, std::array{SegmentId{1, 1}}};
+
+  const Slab slab(1., 1.5);
+  const Time start{slab.start()};
+  const TimeDelta time_step{slab.duration()};
+  const TimeDelta next_time_step = time_step;
+  const TimeStepId time_step_id{time_step.is_positive(), 8, start};
+  const TimeStepId next_time_step_id{time_step.is_positive(), 8,
+                                     start + time_step};
+  const double time = start.value();
+  const AdaptiveSteppingDiagnostics diagnostics{7, 2, 13, 4, 5};
+
+  const parent_items_type parent_items{
+      parent_id,         time_step_id,
+      next_time_step_id, time_step,
+      next_time_step,    time,
+      diagnostics,       std::array{::amr::Flag::Split}};
+
+  auto child_1_box = db::create<
+      db::AddSimpleTags<Parallel::Tags::ArrayIndexImpl<ElementId<1>>,
+                        ::Tags::TimeStepId, ::Tags::Next<::Tags::TimeStepId>,
+                        ::Tags::TimeStep, ::Tags::Next<::Tags::TimeStep>,
+                        ::Tags::Time, ::Tags::AdaptiveSteppingDiagnostics>>(
+      child_1_id, TimeStepId{}, TimeStepId{}, TimeDelta{}, TimeDelta{}, 0.0,
+      AdaptiveSteppingDiagnostics{});
+
+  auto child_2_box = db::create<
+      db::AddSimpleTags<Parallel::Tags::ArrayIndexImpl<ElementId<1>>,
+                        ::Tags::TimeStepId, ::Tags::Next<::Tags::TimeStepId>,
+                        ::Tags::TimeStep, ::Tags::Next<::Tags::TimeStep>,
+                        ::Tags::Time, ::Tags::AdaptiveSteppingDiagnostics>>(
+      child_2_id, TimeStepId{}, TimeStepId{}, TimeDelta{}, TimeDelta{}, 0.0,
+      AdaptiveSteppingDiagnostics{});
+
+  db::mutate_apply<Initialization::ProjectTimeStepping<1>>(
+      make_not_null(&child_1_box), parent_items);
+
+  check(child_1_box, time_step_id, next_time_step_id, time_step, next_time_step,
+        time, diagnostics);
+
+  db::mutate_apply<Initialization::ProjectTimeStepping<1>>(
+      make_not_null(&child_2_box), parent_items);
+
+  check(child_2_box, time_step_id, next_time_step_id, time_step, next_time_step,
+        time, AdaptiveSteppingDiagnostics{7, 2, 0, 0, 0});
+}
+
+template <bool ForwardInTime>
+void test_join() {
+  const ElementId<1> parent_id{0};
+  const ElementId<1> child_1_id{0, std::array{SegmentId{1, 0}}};
+  const ElementId<1> child_2_id{0, std::array{SegmentId{1, 1}}};
+
+  const Slab slab_1(1., 1.5);
+  const Time start_1{ForwardInTime ? slab_1.start() : slab_1.end()};
+  const TimeDelta time_step_1{ForwardInTime ? slab_1.duration()
+                                            : -slab_1.duration()};
+  const TimeDelta next_time_step_1 = time_step_1;
+  const TimeStepId time_step_id_1{time_step_1.is_positive(), 8, start_1};
+  const TimeStepId next_time_step_id_1{time_step_1.is_positive(), 8,
+                                       start_1 + time_step_1};
+  const double time_1 = start_1.value();
+  const AdaptiveSteppingDiagnostics diagnostics_1{7, 2, 13, 4, 5};
+
+  const Slab slab_2(1., 1.5);
+  const Time start_2{ForwardInTime ? slab_2.start() : slab_2.end()};
+  const TimeDelta time_step_2{slab_2, Rational{ForwardInTime ? 1 : -1, 2}};
+  const TimeDelta next_time_step_2 = time_step_2;
+  const TimeStepId time_step_id_2{time_step_2.is_positive(), 8, start_2};
+  const TimeStepId next_time_step_id_2{time_step_2.is_positive(), 8,
+                                       start_2 + time_step_2};
+  const double time_2 = start_2.value();
+  const AdaptiveSteppingDiagnostics diagnostics_2{7, 2, 27, 2, 8};
+
+  std::unordered_map<ElementId<1>, items_type> children_items;
+  children_items.emplace(
+      child_1_id,
+      items_type{child_1_id, time_step_id_1, next_time_step_id_1, time_step_1,
+                 next_time_step_1, time_1, diagnostics_1});
+  children_items.emplace(
+      child_2_id,
+      items_type{child_2_id, time_step_id_2, next_time_step_id_2, time_step_2,
+                 next_time_step_2, time_2, diagnostics_2});
+
+  auto parent_box = db::create<
+      db::AddSimpleTags<Parallel::Tags::ArrayIndexImpl<ElementId<1>>,
+                        ::Tags::TimeStepId, ::Tags::Next<::Tags::TimeStepId>,
+                        ::Tags::TimeStep, ::Tags::Next<::Tags::TimeStep>,
+                        ::Tags::Time, ::Tags::AdaptiveSteppingDiagnostics>>(
+      parent_id, TimeStepId{}, TimeStepId{}, TimeDelta{}, TimeDelta{}, 0.0,
+      AdaptiveSteppingDiagnostics{});
+
+  db::mutate_apply<Initialization::ProjectTimeStepping<1>>(
+      make_not_null(&parent_box), children_items);
+
+  check(parent_box, time_step_id_2, next_time_step_id_2, time_step_2,
+        next_time_step_2, time_2, AdaptiveSteppingDiagnostics{7, 2, 40, 6, 13});
+}
 }  // namespace
 
 SPECTRE_TEST_CASE("Unit.Evolution.Initialization.TimeStepping",
                   "[Evolution][Unit]") {
   test_gts();
   test_lts();
+  test_p_refine();
+  test_split();
+  test_join<true>();
+  test_join<false>();
 }
