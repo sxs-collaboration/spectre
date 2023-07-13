468bef149bb1c430705961fc006c5960cbb4e741
Kyle Nelli <knelli@caltech.edu>
2023-07-13 12:00:54 -0700

    Use ExcisionSpheres in SeparationLessThan

--- tests/Unit/Evolution/Triggers/Test_SeparationLessThan.cpp
+++ tests/Unit/Evolution/Triggers/Test_SeparationLessThan.cpp
@@ -13,11 +13,14 @@
 
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
-#include "Domain/Creators/Brick.hpp"
-#include "Domain/Creators/TimeDependence/TimeDependence.hpp"
-#include "Domain/Creators/TimeDependence/UniformTranslation.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.tpp"
+#include "Domain/CoordinateMaps/TimeDependent/Translation.hpp"
 #include "Domain/Domain.hpp"
+#include "Domain/ExcisionSphere.hpp"
 #include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
+#include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
+#include "Domain/FunctionsOfTime/RegisterDerivedWithCharm.hpp"
 #include "Domain/Structure/Element.hpp"
 #include "Domain/Structure/ElementId.hpp"
 #include "Evolution/Triggers/SeparationLessThan.hpp"
@@ -27,6 +30,7 @@
 #include "ParallelAlgorithms/EventsAndTriggers/Trigger.hpp"
 #include "Utilities/CartesianProduct.hpp"
 #include "Utilities/ProtocolHelpers.hpp"
+#include "Utilities/Serialization/CharmPupable.hpp"
 #include "Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -40,66 +44,89 @@ struct Metavariables {
   };
 };
 
-domain::creators::Brick get_creator(const bool include_distorted_frame) {
-  std::unique_ptr<domain::creators::time_dependence::TimeDependence<3>>
-      time_dependence{};
-  // We don't actually care about a moving mesh, just that we have a distorted
-  // frame so different functions are called inside the trigger.
-  if (include_distorted_frame) {
-    time_dependence = std::make_unique<
-        domain::creators::time_dependence::UniformTranslation<3>>(
-        0.0, std::array{0.0, 0.0, 0.0}, std::array{0.0, 0.0, 0.0});
-  } else {
-    time_dependence = std::make_unique<
-        domain::creators::time_dependence::UniformTranslation<3>>(
-        0.0, std::array{0.0, 0.0, 0.0});
-  }
+using TranslationMap = domain::CoordinateMaps::TimeDependent::Translation<3>;
 
-  return domain::creators::Brick{
-      std::array{-10.0, -10.0, -10.0}, std::array{10.0, 10.0, 10.0},
-      std::array{0_st, 0_st, 0_st},    std::array{5_st, 5_st, 5_st},
-      std::array{false, false, false}, std::move(time_dependence)};
-}
+void test() {
+  const std::string f_of_t_name = "LlamasWithHats";
+  std::unique_ptr<domain::CoordinateMapBase<Frame::Grid, Frame::Inertial, 3>>
+      map = std::make_unique<
+          domain::CoordinateMap<Frame::Grid, Frame::Inertial, TranslationMap>>(
+          TranslationMap{f_of_t_name});
 
-SPECTRE_TEST_CASE("Unit.Evolution.Triggers.SeparationLessThan",
-                  "[Unit][Time]") {
-  register_factory_classes_with_charm<Metavariables>();
-
-  Element<3> element{ElementId<3>{0}, {}};
-
-  const auto check = [&element](const bool include_distorted_frame,
-                                const double separation, const double time,
-                                const double x_center_a,
-                                const double x_center_b,
-                                const bool expected_is_triggered) {
-    const domain::creators::Brick creator =
-        get_creator(include_distorted_frame);
-    Domain<3> domain = creator.create_domain();
-    const std::unordered_map<
-        std::string, std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>
-        functions_of_time = creator.functions_of_time();
+  const auto check = [&map, &f_of_t_name](
+                         const double separation, const double time,
+                         const double x_center_a, const double x_center_b,
+                         const bool expected_is_triggered) {
     const tnsr::I<double, 3, Frame::Grid> center_a{
         std::array{x_center_a, 0.0, 0.0}};
     const tnsr::I<double, 3, Frame::Grid> center_b{
         std::array{x_center_b, 0.0, 0.0}};
 
+    ExcisionSphere<3> excision_sphere_a{2.0, center_a, {}};
+    ExcisionSphere<3> excision_sphere_b{2.0, center_b, {}};
+
+    excision_sphere_a.inject_time_dependent_maps(map->get_clone());
+    excision_sphere_b.inject_time_dependent_maps(map->get_clone());
+
+    std::unordered_map<std::string, ExcisionSphere<3>> excision_spheres{};
+    excision_spheres["ExcisionSphere" + get_output(domain::ObjectLabel::A)] =
+        std::move(excision_sphere_a);
+    excision_spheres["ExcisionSphere" + get_output(domain::ObjectLabel::B)] =
+        std::move(excision_sphere_b);
+
+    const Domain<3> domain{{}, std::move(excision_spheres)};
+
     const Triggers::SeparationLessThan trigger{separation};
 
+    // The coefs are zero because we want the inertial point to be the same
+    // as the grid point for easy checking
+    std::unordered_map<std::string,
+                       std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>
+        functions_of_time{};
+    functions_of_time[f_of_t_name] =
+        std::make_unique<domain::FunctionsOfTime::PiecewisePolynomial<0>>(
+            time, std::array{DataVector{3, 0.0}}, time + 1.0);
+
     const bool is_triggered =
-        trigger(time, domain, element, functions_of_time, center_a, center_b);
+        trigger(time, domain, functions_of_time, center_a, center_b);
 
     CHECK(is_triggered == expected_is_triggered);
   };
 
-  for (const auto& [include_distorted_frame, separation, x_center] :
-       cartesian_product(make_array(true, false), make_array(10.0, 5.0, 2.0),
-                         make_array(6.0, 5.0, 2.0, 0.75))) {
-    check(include_distorted_frame, separation, 0.0, x_center, -x_center,
-          2.0 * x_center <= separation);
+  for (const auto& [separation, x_center] : cartesian_product(
+           make_array(10.0, 5.0, 2.0), make_array(6.0, 5.0, 2.0, 0.75))) {
+    check(separation, 0.0, x_center, -x_center, 2.0 * x_center <= separation);
   }
 
   TestHelpers::test_creation<std::unique_ptr<Trigger>, Metavariables>(
       "SeparationLessThan:\n"
       "  Value: 2.3");
+}
+
+void test_errors() {
+  const Triggers::SeparationLessThan trigger{1.0};
+
+  std::unordered_map<std::string, ExcisionSphere<3>> excision_spheres{};
+
+  CHECK_THROWS_WITH(trigger(0.0, Domain<3>{{}, excision_spheres}, {}, {}, {}),
+                    Catch::Contains("SeparationLessThan trigger expects an "
+                                    "excision sphere named 'ExcisionSphere"));
+
+  ExcisionSphere<3> excision_sphere_a{};
+  excision_spheres["ExcisionSphere" + get_output(domain::ObjectLabel::A)] =
+      std::move(excision_sphere_a);
+
+  CHECK_THROWS_WITH(trigger(0.0, Domain<3>{{}, excision_spheres}, {}, {}, {}),
+                    Catch::Contains("to be time dependent, but it is not."));
+}
+
+SPECTRE_TEST_CASE("Unit.Evolution.Triggers.SeparationLessThan",
+                  "[Unit][Time]") {
+  register_factory_classes_with_charm<Metavariables>();
+  domain::FunctionsOfTime::register_derived_with_charm();
+  PUPable_reg(SINGLE_ARG(
+      domain::CoordinateMap<Frame::Grid, Frame::Inertial, TranslationMap>));
+  test();
+  test_errors();
 }
 }  // namespace
