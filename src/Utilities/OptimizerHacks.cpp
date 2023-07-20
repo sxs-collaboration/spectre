fb8545014377eff6699102be276bb32e6afab871
Lawrence Kidder <kidder@astro.cornell.edu>
2023-07-20 15:44:29 -0400

    Address linking errors for clang 16
    Clang 15 and 16 fail to instantiate a std template when using precompiled
    headers, so instantiate them in OptimizerHacks which will be found when
    Utilities is linked against.
    
    Clang 16 fails to instantiate a constructor for Composition despite the
    entire class being instantiated.

--- src/Utilities/OptimizerHacks.cpp
+++ src/Utilities/OptimizerHacks.cpp
@@ -8,3 +8,18 @@ namespace optimizer_hacks {
 void indicate_value_can_be_changed(void* /*variable*/) {}
 }  // namespace optimizer_hacks
 #endif  /* defined(__clang__) && __clang__ < 11 */
+
+#if defined(__clang__) && __clang_major__ >= 15 && defined(__GLIBCXX__)
+#include <string>
+// clang v15+ fails to instantiate the following std library templates if
+// pre-compiled headers are used
+template std::__cxx11::basic_string<char, std::char_traits<char>,
+                                    std::allocator<char>>::pointer
+std::__cxx11::basic_string<char, std::char_traits<char>,
+                           std::allocator<char>>::_M_use_local_data();
+template std::__cxx11::basic_string<wchar_t, std::char_traits<wchar_t>,
+                                    std::allocator<wchar_t>>::pointer
+std::__cxx11::basic_string<wchar_t, std::char_traits<wchar_t>,
+                           std::allocator<wchar_t>>::_M_use_local_data();
+#endif /* defined(__clang__) && __clang_major__ >= 15 && defined(__GLIBCXX__) \
+        */
