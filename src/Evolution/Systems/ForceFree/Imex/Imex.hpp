38aafc805df00be5d2090a0c54ca536f36a2bdc6
Yoonsoo Kim <ykim7@caltech.edu>
2023-07-22 17:46:14 -0700

    Add Imex sub-namespace to ForceFree system

---
+++ src/Evolution/Systems/ForceFree/Imex/Imex.hpp
@@ -0,0 +1,11 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace ForceFree {
+/*!
+ * \brief Code related to IMEX time integration of ForceFree evolution system.
+ */
+namespace Imex {}
+}  // namespace ForceFree
