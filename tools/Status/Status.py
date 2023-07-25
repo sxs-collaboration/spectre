265f60226b63f30279ecdbbf0103012517adf242
Nils Vu <owls@nilsvu.de>
2023-07-25 10:45:50 -0700

    Add short comment to SubmitTemplateBase.sh
    Points to the relevant docs when looking at submit scripts.

--- tools/Status/Status.py
+++ tools/Status/Status.py
@@ -119,7 +119,7 @@ def get_input_file(comment: Optional[str], work_dir: str) -> Optional[str]:
             input_file = yaml.safe_load(open_context_file)["input_file"]
             return os.path.join(work_dir, input_file)
     except:
-        logger.debug("Failed to read file '{context_file}'.", exc_info=True)
+        logger.debug(f"Failed to read file '{context_file}'.", exc_info=True)
     # Fallback: Check if there's a single YAML file in the work dir
     yaml_files = glob.glob(os.path.join(work_dir, "*.yaml"))
     if len(yaml_files) == 1:
