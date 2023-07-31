e5f4462580adb22d24f48ff944ef1d4790d6127d
Yoonsoo Kim <ykim7@caltech.edu>
2023-07-31 15:38:09 -0700

    Add type aliases for ForceFree initial data groups

--- src/PointwiseFunctions/AnalyticData/ForceFree/Factory.hpp
+++ src/PointwiseFunctions/AnalyticData/ForceFree/Factory.hpp
@@ -3,10 +3,21 @@
 
 #pragma once
 
+#include "Evolution/Systems/ForceFree/SimulationType.hpp"
 #include "PointwiseFunctions/AnalyticData/ForceFree/FfeBreakdown.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace ForceFree::AnalyticData {
-/// \brief List of all analytic data
+/*!
+ * \brief Typelist of all analytic data of GRFFE evolution system
+ */
 using all_data = tmpl::list<FfeBreakdown>;
+
+template <SimulationType simulation_type>
+using available_data = tmpl::append<
+    tmpl::conditional_t<simulation_type == SimulationType::FreeSpace,
+                        tmpl::list<FfeBreakdown>, tmpl::list<>>,
+    tmpl::conditional_t<simulation_type ==
+                            SimulationType::IsolatedNsMagnetosphere,
+                        tmpl::list<>, tmpl::list<>>>;
 }  // namespace ForceFree::AnalyticData
--- src/PointwiseFunctions/AnalyticSolutions/ForceFree/Factory.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/ForceFree/Factory.hpp
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include "Evolution/Systems/ForceFree/SimulationType.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/ForceFree/AlfvenWave.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/ForceFree/FastWave.hpp"
 #include "Utilities/TMPL.hpp"
@@ -12,4 +13,12 @@ namespace ForceFree::Solutions {
  * \brief Typelist of all analytic solutions of GRFFE evolution system
  */
 using all_solutions = tmpl::list<AlfvenWave, FastWave>;
+
+template <SimulationType simulation_type>
+using available_solutions = tmpl::append<
+    tmpl::conditional_t<simulation_type == SimulationType::FreeSpace,
+                        tmpl::list<AlfvenWave, FastWave>, tmpl::list<>>,
+    tmpl::conditional_t<simulation_type ==
+                            SimulationType::IsolatedNsMagnetosphere,
+                        tmpl::list<>, tmpl::list<>>>;
 }  // namespace ForceFree::Solutions
