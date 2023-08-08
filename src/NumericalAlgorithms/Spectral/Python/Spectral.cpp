42c3c24d53ebd58f384988322ee62c408fd2c01f
Nils Vu <owls@nilsvu.de>
2023-08-08 15:40:18 -0700

    Reduce number of Spectral Pybindings files

--- src/NumericalAlgorithms/Spectral/Python/Spectral.cpp
+++ src/NumericalAlgorithms/Spectral/Python/Spectral.cpp
@@ -14,43 +14,33 @@ namespace py = pybind11;
 
 namespace Spectral::py_bindings {
 
-void bind_basis(py::module& m) {
+void bind_spectral(py::module& m) {
   py::enum_<Spectral::Basis>(m, "Basis")
       .value("Legendre", Spectral::Basis::Legendre)
       .value("Chebyshev", Spectral::Basis::Chebyshev)
       .value("FiniteDifference", Spectral::Basis::FiniteDifference);
-}
-
-void bind_quadrature(py::module& m) {
   py::enum_<Spectral::Quadrature>(m, "Quadrature")
       .value("Gauss", Spectral::Quadrature::Gauss)
       .value("GaussLobatto", Spectral::Quadrature::GaussLobatto)
       .value("CellCentered", Spectral::Quadrature::CellCentered)
       .value("FaceCentered", Spectral::Quadrature::FaceCentered);
-}
-void bind_modal_to_nodal_matrix(py::module& m) {
+  m.def("collocation_points",
+        static_cast<const DataVector& (*)(const Mesh<1>&)>(
+            &Spectral::collocation_points),
+        py::arg("mesh"), py::return_value_policy::reference,
+        "Collocation points for a one-dimensional mesh.");
   m.def("modal_to_nodal_matrix",
         static_cast<const Matrix& (*)(const Mesh<1>&)>(
             &Spectral::modal_to_nodal_matrix),
         py::arg("mesh"), py::return_value_policy::reference,
         "Transformation matrix from modal to nodal coefficients for a "
         "one-dimensional mesh.");
-}
-void bind_nodal_to_modal_matrix(py::module& m) {
   m.def("nodal_to_modal_matrix",
         static_cast<const Matrix& (*)(const Mesh<1>&)>(
             &Spectral::nodal_to_modal_matrix),
         py::arg("mesh"), py::return_value_policy::reference,
         "Transformation matrix from nodal to modal coefficients for a "
         "one-dimensional mesh.");
-}
-
-void bind_collocation_points(py::module& m) {
-  m.def("collocation_points",
-        static_cast<const DataVector& (*)(const Mesh<1>&)>(
-            &Spectral::collocation_points),
-        py::arg("mesh"), py::return_value_policy::reference,
-        "Collocation points for a one-dimensional mesh.");
 }
 
 }  // namespace Spectral::py_bindings
--- tests/Unit/NumericalAlgorithms/Spectral/Python/Test_Spectral.py
+++ tests/Unit/NumericalAlgorithms/Spectral/Python/Test_Spectral.py
@@ -7,10 +7,17 @@ import numpy as np
 from numpy.polynomial import chebyshev, legendre
 
 from spectre.DataStructures import DataVector
-from spectre.Spectral import Basis, Mesh1D, Quadrature, collocation_points
+from spectre.Spectral import (
+    Basis,
+    Mesh1D,
+    Quadrature,
+    collocation_points,
+    modal_to_nodal_matrix,
+    nodal_to_modal_matrix,
+)
 
 
-class TestCollocationPoints(unittest.TestCase):
+class TestSpectral(unittest.TestCase):
     def test_collocation_points_legendre_gauss(self):
         for num_coll_points in range(2, 13):
             coefs = np.zeros(num_coll_points + 1)
@@ -62,6 +69,40 @@ class TestCollocationPoints(unittest.TestCase):
             np.testing.assert_allclose(
                 coll_points_numpy, coll_points_spectre, 1e-12, 1e-12
             )
+
+    def test_modal_to_nodal_matrix_legendre(self):
+        for quadrature in [Quadrature.Gauss, Quadrature.GaussLobatto]:
+            for num_coll_points in range(2, 13):
+                mesh = Mesh1D(num_coll_points, Basis.Legendre, quadrature)
+                coll_points = collocation_points(mesh)
+                mtn_numpy = legendre.legvander(coll_points, num_coll_points - 1)
+                mtn_spectre = modal_to_nodal_matrix(mesh)
+                np.testing.assert_allclose(mtn_spectre, mtn_numpy, 1e-12, 1e-12)
+                ntm_spectre = nodal_to_modal_matrix(mesh)
+                np.testing.assert_allclose(
+                    np.matmul(mtn_spectre, ntm_spectre),
+                    np.identity(num_coll_points),
+                    1e-12,
+                    1e-12,
+                )
+
+    def test_modal_to_nodal_matrix_chebyshev(self):
+        for quadrature in [Quadrature.Gauss, Quadrature.GaussLobatto]:
+            for num_coll_points in range(2, 13):
+                mesh = Mesh1D(num_coll_points, Basis.Chebyshev, quadrature)
+                coll_points = collocation_points(mesh)
+                mtn_numpy = chebyshev.chebvander(
+                    coll_points, num_coll_points - 1
+                )
+                mtn_spectre = modal_to_nodal_matrix(mesh)
+                np.testing.assert_allclose(mtn_spectre, mtn_numpy, 1e-12, 1e-12)
+                ntm_spectre = nodal_to_modal_matrix(mesh)
+                np.testing.assert_allclose(
+                    np.matmul(mtn_spectre, ntm_spectre),
+                    np.identity(num_coll_points),
+                    1e-12,
+                    1e-12,
+                )
 
 
 if __name__ == "__main__":
