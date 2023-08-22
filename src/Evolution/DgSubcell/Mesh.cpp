668fe736c7442e6bc4cad631ec6016bba9257ace
Nils Deppe <nils@nilsdeppe.com>
2023-08-22 07:59:00 -0700

    Add dg_mesh function to DG-subcell
    Computes the DG mesh from the FD mesh

--- src/Evolution/DgSubcell/Mesh.cpp
+++ src/Evolution/DgSubcell/Mesh.cpp
@@ -35,7 +35,45 @@ Mesh<Dim> mesh(const Mesh<Dim>& dg_mesh) {
                    Spectral::Quadrature::CellCentered};
 }
 
+template <size_t Dim>
+Mesh<Dim> dg_mesh(const Mesh<Dim>& subcell_mesh, const Spectral::Basis basis,
+                  const Spectral::Quadrature quadrature) {
+  ASSERT(subcell_mesh.basis() ==
+             make_array<Dim>(Spectral::Basis::FiniteDifference),
+         "The basis for computing the DG mesh must be FiniteDifference but got "
+             << subcell_mesh);
+  ASSERT(
+      subcell_mesh.quadrature() ==
+          make_array<Dim>(Spectral::Quadrature::CellCentered),
+      "The quadrature for computing the DG mesh must be CellCentered but got "
+          << subcell_mesh);
+  ASSERT(
+      basis == Spectral::Basis::Legendre or basis == Spectral::Basis::Chebyshev,
+      "The DG basis must be Legendre or Chebyshev but got " << basis);
+  ASSERT(quadrature == Spectral::Quadrature::Gauss or
+             quadrature == Spectral::Quadrature::GaussLobatto,
+         "The DG quadrature for computing the DG mesh must be Gauss or "
+         "GaussLobatto but "
+             << quadrature);
+  std::array<size_t, Dim> extents{};
+  for (size_t d = 0; d < Dim; ++d) {
+    ASSERT((subcell_mesh.extents(d) + 1) % 2 == 0,
+           "Subcell mesh must have odd extents " << subcell_mesh);
+    gsl::at(extents, d) = (subcell_mesh.extents(d) + 1) / 2;
+  }
+  return Mesh<Dim>{extents, basis, quadrature};
+}
+
 template Mesh<1> mesh(const Mesh<1>& dg_mesh);
 template Mesh<2> mesh(const Mesh<2>& dg_mesh);
 template Mesh<3> mesh(const Mesh<3>& dg_mesh);
+template Mesh<1> dg_mesh(const Mesh<1>& subcell_mesh,
+                         const Spectral::Basis basis,
+                         const Spectral::Quadrature quadrature);
+template Mesh<2> dg_mesh(const Mesh<2>& subcell_mesh,
+                         const Spectral::Basis basis,
+                         const Spectral::Quadrature quadrature);
+template Mesh<3> dg_mesh(const Mesh<3>& subcell_mesh,
+                         const Spectral::Basis basis,
+                         const Spectral::Quadrature quadrature);
 }  // namespace evolution::dg::subcell::fd
