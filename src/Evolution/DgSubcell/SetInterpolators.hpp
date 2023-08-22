ecb0216e74acd594b8e8c2967170c7ef5595827d
Nils Deppe <nils@nilsdeppe.com>
2023-08-21 19:52:31 -0700

    Add SetInterpolators
    This sets interpolators for going from DG to FD and FD to FD at block boundaries
    where the grid coordinates are not smooth. These are used to set ghost zones
    sent for reconstruction. A separate part of the code will need to be responsible
    for updating the interpolants should we allow p-refinement with DG-FD.

---
+++ src/Evolution/DgSubcell/SetInterpolators.hpp
@@ -0,0 +1,240 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <boost/functional/hash.hpp>
+#include <cstddef>
+#include <optional>
+#include <utility>
+
+#include "DataStructures/FixedHashMap.hpp"
+#include "DataStructures/Tensor/ExtractPoint.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/Creators/Tags/Domain.hpp"
+#include "Domain/Domain.hpp"
+#include "Domain/ElementMap.hpp"
+#include "Domain/Structure/BlockId.hpp"
+#include "Domain/Structure/Direction.hpp"
+#include "Domain/Structure/Element.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Structure/MaxNumberOfNeighbors.hpp"
+#include "Domain/Tags.hpp"
+#include "Evolution/DgSubcell/SliceTensor.hpp"
+#include "Evolution/DgSubcell/Tags/Interpolators.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
+#include "Evolution/DgSubcell/Tags/Reconstructor.hpp"
+#include "NumericalAlgorithms/Interpolation/IrregularInterpolant.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace evolution::dg::subcell {
+/*!
+ * \brief Sets the `intrp::IrregularInterpolant`s for interpolating to ghost
+ * zone data at block boundaries.
+ *
+ * The DG to FD interpolants are at full order of the DG grid. The FD to FD
+ * interpolant is piecewise linear with no support for neighboring
+ * elements. We will want to use high-order slope-limited FD interpolation in
+ * the future, but that requires neighbor communication. A slightly simpler
+ * approach would be to use high-order Lagrange interpolation, which still
+ * requires neighbor communication but does not require any additional changes
+ * to the reconstruction routines to work on non-uniform grids. This is what
+ * the Multipatch-MHD code does, relying on the slope limiting from the ghost
+ * zones to remove oscillations. I (Nils Deppe) am not sure I love that, but
+ * it's worth a try since it should be pretty easy to do.
+ *
+ * \warning Currently assumes that neighboring DG/FD elements are on the same
+ * refinement level and have the same DG mesh and subcell mesh.
+ */
+template <size_t Dim>
+struct SetInterpolators {
+  using return_tags = tmpl::list<
+      evolution::dg::subcell::Tags::InterpolatorsFromFdToNeighborFd<Dim>,
+      evolution::dg::subcell::Tags::InterpolatorsFromDgToNeighborFd<Dim>,
+      evolution::dg::subcell::Tags::InterpolatorsFromNeighborDgToFd<Dim>>;
+  using argument_tags =
+      tmpl::list<::domain::Tags::Element<Dim>, ::domain::Tags::Domain<Dim>,
+                 domain::Tags::Mesh<Dim>, domain::Tags::Mesh<Dim>,
+                 evolution::dg::subcell::Tags::Mesh<Dim>,
+                 evolution::dg::subcell::Tags::Mesh<Dim>,
+                 ::domain::Tags::ElementMap<Dim, Frame::Grid>,
+                 evolution::dg::subcell::Tags::Reconstructor>;
+
+  template <typename ReconstructorType>
+  static void apply(
+      const gsl::not_null<
+          FixedHashMap<maximum_number_of_neighbors(Dim),
+                       std::pair<Direction<Dim>, ElementId<Dim>>,
+                       std::optional<intrp::Irregular<Dim>>,
+                       boost::hash<std::pair<Direction<Dim>, ElementId<Dim>>>>*>
+          interpolators_fd_to_neighbor_fd_ptr,
+      const gsl::not_null<
+          FixedHashMap<maximum_number_of_neighbors(Dim),
+                       std::pair<Direction<Dim>, ElementId<Dim>>,
+                       std::optional<intrp::Irregular<Dim>>,
+                       boost::hash<std::pair<Direction<Dim>, ElementId<Dim>>>>*>
+          interpolators_dg_to_neighbor_fd_ptr,
+      const gsl::not_null<
+          FixedHashMap<maximum_number_of_neighbors(Dim),
+                       std::pair<Direction<Dim>, ElementId<Dim>>,
+                       std::optional<intrp::Irregular<Dim>>,
+                       boost::hash<std::pair<Direction<Dim>, ElementId<Dim>>>>*>
+          interpolators_neighbor_dg_to_fd_ptr,
+      const Element<Dim>& element, const Domain<Dim>& domain,
+      const Mesh<Dim>& my_dg_mesh,
+      // Needs to be updated to support non-uniform h/p-refinement
+      const Mesh<Dim>& neighbor_dg_mesh, const Mesh<Dim>& my_fd_mesh,
+      // Needs to be updated to support non-uniform h/p-refinement
+      const Mesh<Dim>& neighbor_fd_mesh,
+      const ElementMap<Dim, Frame::Grid>& element_map,
+      const ReconstructorType& reconstructor) {
+    const size_t number_of_ghost_zones = reconstructor.ghost_zone_size();
+    const size_t my_block_id = element.id().block_id();
+    for (const auto& direction_neighbors_in_direction : element.neighbors()) {
+      const auto& direction = direction_neighbors_in_direction.first;
+      const auto& neighbors_in_direction =
+          direction_neighbors_in_direction.second;
+      const auto& orientation = neighbors_in_direction.orientation();
+      const auto direction_from_neighbor = orientation(direction.opposite());
+      for (const ElementId<Dim>& neighbor_id : neighbors_in_direction) {
+        const size_t neighbor_block_id = neighbor_id.block_id();
+        if (neighbor_block_id == my_block_id) {
+          continue;
+        }
+        const auto& neighbor_block = domain.blocks()[neighbor_block_id];
+        // InterpolatorsFromFdToNeighborFd &
+        // InterpolatorsFromDgToNeighborFd
+        // 1. Compute the grid coordinates of my neighbor's ghost zones.
+        // 2. Compute the element logical coordinates of my neighbor's
+        //    ghost zones.
+        // 3. Create interpolators
+
+        if (not is_isotropic(neighbor_fd_mesh)) {
+          ERROR("We assume an isotropic mesh but got " << neighbor_fd_mesh);
+        }
+
+        const auto get_logical_coords = [&element, &neighbor_id, &direction](
+                                            const auto& map,
+                                            const auto& grid_coords) {
+          tnsr::I<DataVector, Dim, Frame::ElementLogical> logical_coords{
+              get<0>(grid_coords).size()};
+          for (size_t i = 0; i < get<0>(grid_coords).size(); ++i) {
+            try {
+              tnsr::I<double, Dim, Frame::ElementLogical> logical_coord =
+                  map.inverse(extract_point(grid_coords, i));
+              for (size_t d = 0; d < Dim; ++d) {
+                logical_coords.get(d)[i] = logical_coord.get(d);
+              }
+            } catch (const std::bad_optional_access& e) {
+              ERROR(
+                  "Failed to get logical coordinates for neighbor's "
+                  "ghost zone grid coordinates. This could be because the "
+                  "ghost zones are not in the nearest neighbor but instead in "
+                  "the next-to-nearest neighbor. The code assumes all ghost "
+                  "zones, even on curved meshes, are in the nearest neighbors. "
+                  "The current element is "
+                  << element.id() << " and the neighbor id is " << neighbor_id
+                  << " in direction " << direction
+                  << " The neighbor grid coordinates are \n"
+                  << extract_point(grid_coords, i) << "\n");
+            }
+          }
+          return logical_coords;
+        };
+
+        tnsr::I<DataVector, Dim, Frame::Grid> neighbor_grid_ghost_zone_coords{};
+        // Get the neighbor's ghost zone coordinates in the grid
+        // frame.
+        if (const tnsr::I<DataVector, Dim, Frame::ElementLogical>
+                neighbor_logical_ghost_zone_coords =
+                    evolution::dg::subcell::fd::ghost_zone_logical_coordinates(
+                        neighbor_fd_mesh, number_of_ghost_zones,
+                        direction_from_neighbor);
+            neighbor_block.is_time_dependent()) {
+          const ElementMap neighbor_element_map(
+              neighbor_id,
+              neighbor_block.moving_mesh_logical_to_grid_map().get_clone());
+          neighbor_grid_ghost_zone_coords =
+              neighbor_element_map(neighbor_logical_ghost_zone_coords);
+        } else {
+          const ElementMap neighbor_element_map(
+              neighbor_id, neighbor_block.stationary_map().get_clone());
+          const tnsr::I<DataVector, Dim, Frame::Inertial>
+              neighbor_inertial_ghost_zone_coords =
+                  neighbor_element_map(neighbor_logical_ghost_zone_coords);
+          for (size_t i = 0; i < Dim; ++i) {
+            neighbor_grid_ghost_zone_coords[i] =
+                neighbor_inertial_ghost_zone_coords[i];
+          }
+        }
+        // Map the ghost zone grid coordinates back to our logical
+        // coordinates.
+        const tnsr::I<DataVector, Dim, Frame::ElementLogical>
+            neighbor_logical_ghost_zone_coords = get_logical_coords(
+                element_map, neighbor_grid_ghost_zone_coords);
+
+        // Set up interpolators for our local element to our neighbor's
+        // ghost zones.
+        (*interpolators_fd_to_neighbor_fd_ptr)[std::pair{
+            direction, neighbor_id}] = intrp::Irregular<Dim>{
+            my_fd_mesh, neighbor_logical_ghost_zone_coords};
+        (*interpolators_dg_to_neighbor_fd_ptr)[std::pair{
+            direction, neighbor_id}] = intrp::Irregular<Dim>{
+            my_dg_mesh, neighbor_logical_ghost_zone_coords};
+
+        // InterpolatorsFromNeighborDgToFd: the interpolation from our
+        // neighbor's DG grid to our FD ghost zones.
+        //
+        // 1. Compute the grid coordinates of my ghost zones.
+        // 2. Compute neighbor's element logical coordinates of my ghost
+        //    zones
+        // 3. Create interpolator for InterpolatorsFromNeighborDgToFd
+        const tnsr::I<DataVector, Dim, Frame::ElementLogical>
+            my_logical_coords = logical_coordinates(my_fd_mesh);
+        tnsr::I<DataVector, Dim, Frame::ElementLogical>
+            my_logical_ghost_zone_coords =
+                evolution::dg::subcell::slice_tensor_for_subcell(
+                    my_logical_coords, neighbor_fd_mesh.extents(),
+                    number_of_ghost_zones, direction);
+        const double delta_xi =
+            get<0>(my_logical_coords)[1] - get<0>(my_logical_coords)[0];
+        // The sign accounts for whether we are shift along the
+        // positive or negative axis.
+        const double coordinate_shift =
+            direction.sign() * delta_xi * number_of_ghost_zones;
+        my_logical_ghost_zone_coords.get(direction.dimension()) +=
+            coordinate_shift;
+        const tnsr::I<DataVector, Dim, Frame::Grid> my_grid_ghost_zone_coords =
+            element_map(my_logical_ghost_zone_coords);
+        if (neighbor_block.is_time_dependent()) {
+          const ElementMap neighbor_element_map(
+              neighbor_id,
+              neighbor_block.moving_mesh_logical_to_grid_map().get_clone());
+          (*interpolators_neighbor_dg_to_fd_ptr)[std::pair{
+              direction, neighbor_id}] = intrp::Irregular<Dim>{
+              neighbor_dg_mesh, get_logical_coords(neighbor_element_map,
+                                                   my_grid_ghost_zone_coords)};
+        } else {
+          const ElementMap neighbor_element_map(
+              neighbor_id, neighbor_block.stationary_map().get_clone());
+          const tnsr::I<DataVector, Dim, Frame::Inertial>
+              view_my_grid_ghost_zone_coords{};
+          for (size_t i = 0; i < Dim; ++i) {
+            make_const_view(make_not_null(&view_my_grid_ghost_zone_coords[i]),
+                            my_grid_ghost_zone_coords[i], 0,
+                            my_grid_ghost_zone_coords[i].size());
+          }
+          (*interpolators_neighbor_dg_to_fd_ptr)[std::pair{
+              direction, neighbor_id}] = intrp::Irregular<Dim>{
+              neighbor_dg_mesh,
+              get_logical_coords(neighbor_element_map,
+                                 view_my_grid_ghost_zone_coords)};
+        }
+      }
+    }
+  }
+};
+}  // namespace evolution::dg::subcell
---
+++ tests/Unit/Evolution/DgSubcell/Test_SetInterpolators.cpp
@@ -0,0 +1,231 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cstddef>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/CoordinateMaps/Affine.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.tpp"
+#include "Domain/CoordinateMaps/Identity.hpp"
+#include "Domain/CoordinateMaps/ProductMaps.hpp"
+#include "Domain/CoordinateMaps/ProductMaps.tpp"
+#include "Domain/CoordinateMaps/Tags.hpp"
+#include "Domain/ElementMap.hpp"
+#include "Domain/Structure/BlockNeighbor.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Tags.hpp"
+#include "Evolution/DgSubcell/GhostZoneLogicalCoordinates.hpp"
+#include "Evolution/DgSubcell/Mesh.hpp"
+#include "Evolution/DgSubcell/SetInterpolators.hpp"
+#include "Evolution/DgSubcell/Tags/Coordinates.hpp"
+#include "NumericalAlgorithms/Spectral/LogicalCoordinates.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
+
+namespace {
+template <size_t Dim, typename TargetFrame>
+auto make_grid_map(const size_t id) {
+  CAPTURE(id);
+  REQUIRE((id == 0 or id == 1));
+  using domain::make_coordinate_map_base;
+  using domain::CoordinateMaps::Affine;
+  if constexpr (Dim == 1) {
+    return make_coordinate_map_base<Frame::BlockLogical, TargetFrame>(
+        Affine(-1.0, 1.0, 2.0, id == 0 ? 5.0 : 0.0));
+  } else if constexpr (Dim == 2) {
+    using domain::CoordinateMaps::ProductOf2Maps;
+    return make_coordinate_map_base<Frame::BlockLogical, TargetFrame>(
+        ProductOf2Maps<Affine, Affine>(
+            Affine(-1.0, 1.0, -1.0, id == 0 ? -0.8 : -1.9),
+            Affine(-1.0, 1.0, -1.0, -0.8)));
+  } else {
+    using domain::CoordinateMaps::ProductOf3Maps;
+    return make_coordinate_map_base<Frame::BlockLogical, TargetFrame>(
+        ProductOf3Maps<Affine, Affine, Affine>(
+            Affine(-1.0, 1.0, -1.0, id == 0 ? -0.8 : -1.9),
+            Affine(-1.0, 1.0, -1.0, -0.8), Affine(-1.0, 1.0, 0.8, 1.0)));
+  }
+}
+
+class DummyReconstructor {
+ public:
+  static size_t ghost_zone_size() { return 2; }
+};
+
+namespace Tags {
+struct Reconstructor : db::SimpleTag,
+                       evolution::dg::subcell::Tags::Reconstructor {
+  using type = std::unique_ptr<DummyReconstructor>;
+};
+}  // namespace Tags
+
+template <size_t Dim>
+void test() {
+  // Domain setup:
+  //   | lower_xi | element |
+  //
+  // No neighbors in upper xi or in eta/zeta.
+
+  const ::Mesh<Dim> dg_mesh{6, Spectral::Basis::Legendre,
+                            Spectral::Quadrature::GaussLobatto};
+  const ::Mesh<Dim> subcell_mesh = evolution::dg::subcell::fd::mesh(dg_mesh);
+
+  const ElementId<Dim> element_id{0};
+  const ElementId<Dim> lower_xi_element_id{1};
+  DirectionMap<Dim, Neighbors<Dim>> element_neighbors{};
+  const std::pair lower_xi_id{Direction<Dim>::lower_xi(), lower_xi_element_id};
+  OrientationMap<Dim> orientation{};
+  if constexpr (Dim == 1) {
+    orientation = OrientationMap<Dim>{{{Direction<Dim>::lower_xi()}}};
+    element_neighbors.insert(
+        std::pair{Direction<Dim>::lower_xi(),
+                  Neighbors<Dim>{{lower_xi_id.second}, orientation}});
+  } else if constexpr (Dim == 2) {
+    orientation = OrientationMap<Dim>{
+        {{Direction<Dim>::lower_xi(), Direction<Dim>::lower_eta()}}};
+    element_neighbors.insert(
+        std::pair{Direction<Dim>::lower_xi(),
+                  Neighbors<Dim>{{lower_xi_id.second}, orientation}});
+  } else if constexpr (Dim == 3) {
+    orientation = OrientationMap<Dim>{
+        {{Direction<Dim>::lower_xi(), Direction<Dim>::lower_eta(),
+          Direction<Dim>::upper_zeta()}}};
+    element_neighbors.insert(
+        std::pair{Direction<Dim>::lower_xi(),
+                  Neighbors<Dim>{{lower_xi_id.second}, orientation}});
+  }
+  const Element<Dim> element{element_id, element_neighbors};
+
+  DirectionMap<Dim, BlockNeighbor<Dim>> block0_neighbors{};
+  block0_neighbors[Direction<Dim>::lower_xi()] =
+      BlockNeighbor<Dim>{1, orientation};
+  Block<Dim> block0{make_grid_map<Dim, Frame::Inertial>(0), 0,
+                    block0_neighbors};
+  block0.inject_time_dependent_map(
+      domain::make_coordinate_map_base<Frame::Grid, Frame::Inertial>(
+          domain::CoordinateMaps::Identity<Dim>{}));
+
+  DirectionMap<Dim, BlockNeighbor<Dim>> block1_neighbors{};
+  block1_neighbors[Direction<Dim>::lower_xi()] =
+      BlockNeighbor<Dim>{0, orientation.inverse_map()};
+  Block<Dim> block1{make_grid_map<Dim, Frame::Inertial>(1), 1,
+                    block1_neighbors};
+  block1.inject_time_dependent_map(
+      domain::make_coordinate_map_base<Frame::Grid, Frame::Inertial>(
+          domain::CoordinateMaps::Identity<Dim>{}));
+  std::vector<Block<Dim>> blocks{2};
+  blocks[0] = std::move(block0);
+  blocks[1] = std::move(block1);
+
+  const auto element_logical_coords = logical_coordinates(dg_mesh);
+  tnsr::I<DataVector, Dim, Frame::BlockLogical> block_logical_coords{};
+  for (size_t i = 0; i < Dim; ++i) {
+    block_logical_coords[i] = element_logical_coords[i];
+  }
+
+  // NOLINTNEXTLINE(google-build-using-namespace)
+  using namespace evolution::dg::subcell::Tags;
+  auto box = db::create<
+      db::AddSimpleTags<
+          InterpolatorsFromFdToNeighborFd<Dim>,
+          InterpolatorsFromDgToNeighborFd<Dim>,
+          InterpolatorsFromNeighborDgToFd<Dim>,
+
+          ::domain::Tags::Element<Dim>, ::domain::Tags::Domain<Dim>,
+          domain::Tags::Mesh<Dim>, evolution::dg::subcell::Tags::Mesh<Dim>,
+          ::domain::Tags::ElementMap<Dim, Frame::Grid>, Tags::Reconstructor>,
+      db::AddComputeTags<
+          domain::Tags::LogicalCoordinates<Dim>,
+          domain::Tags::MappedCoordinates<
+              ::domain::Tags::ElementMap<Dim, Frame::Grid>,
+              domain::Tags::Coordinates<Dim, Frame::ElementLogical>>,
+          evolution::dg::subcell::Tags::LogicalCoordinatesCompute<Dim>,
+          domain::Tags::MappedCoordinates<
+              ::domain::Tags::ElementMap<Dim, Frame::Grid>,
+              evolution::dg::subcell::Tags::Coordinates<Dim,
+                                                        Frame::ElementLogical>,
+              evolution::dg::subcell::Tags::Coordinates>>>(
+      typename InterpolatorsFromFdToNeighborFd<Dim>::type{},
+      typename InterpolatorsFromDgToNeighborFd<Dim>::type{},
+      typename InterpolatorsFromNeighborDgToFd<Dim>::type{},
+
+      element, Domain<Dim>{std::move(blocks)}, dg_mesh, subcell_mesh,
+      ElementMap{element_id, make_grid_map<Dim, Frame::Grid>(0)},
+      std::make_unique<DummyReconstructor>());
+  db::mutate_apply<evolution::dg::subcell::SetInterpolators<Dim>>(
+      make_not_null(&box));
+
+  // Check that the interpolators were set.
+  REQUIRE(db::get<InterpolatorsFromDgToNeighborFd<Dim>>(box).size() == 1);
+  REQUIRE(db::get<InterpolatorsFromDgToNeighborFd<Dim>>(box)
+              .at(lower_xi_id)
+              .has_value());
+  REQUIRE(db::get<InterpolatorsFromFdToNeighborFd<Dim>>(box).size() == 1);
+  REQUIRE(db::get<InterpolatorsFromFdToNeighborFd<Dim>>(box)
+              .at(lower_xi_id)
+              .has_value());
+  REQUIRE(db::get<InterpolatorsFromNeighborDgToFd<Dim>>(box).size() == 1);
+  REQUIRE(db::get<InterpolatorsFromNeighborDgToFd<Dim>>(box)
+              .at(lower_xi_id)
+              .has_value());
+
+  // Interpolate to neighbor ghost zones.
+  tnsr::I<DataVector, Dim, Frame::Grid> dg_to_ghost_zones{};
+  tnsr::I<DataVector, Dim, Frame::Grid> fd_to_ghost_zones{};
+  for (size_t i = 0; i < Dim; ++i) {
+    dg_to_ghost_zones.get(i) =
+        db::get<InterpolatorsFromDgToNeighborFd<Dim>>(box)
+            .at(lower_xi_id)
+            .value()
+            .interpolate(
+                db::get<domain::Tags::Coordinates<Dim, Frame::Grid>>(box).get(
+                    i));
+    fd_to_ghost_zones.get(i) =
+        db::get<InterpolatorsFromFdToNeighborFd<Dim>>(box)
+            .at(lower_xi_id)
+            .value()
+            .interpolate(db::get<evolution::dg::subcell::Tags::Coordinates<
+                             Dim, Frame::Grid>>(box)
+                             .get(i));
+  }
+  // Compute the lower-xi neighbor expected ghost zones.
+  const auto lower_xi_logical_ghost_coords =
+      evolution::dg::subcell::fd::ghost_zone_logical_coordinates(
+          subcell_mesh, DummyReconstructor::ghost_zone_size(),
+          Direction<Dim>::lower_xi());
+  const ElementMap lower_xi_element_map{lower_xi_element_id,
+                                        make_grid_map<Dim, Frame::Grid>(1)};
+  const auto lower_xi_neighbor_grid_ghost_coords =
+      lower_xi_element_map(lower_xi_logical_ghost_coords);
+  // Check interpolation worked correctly.
+  CHECK_ITERABLE_APPROX(dg_to_ghost_zones, lower_xi_neighbor_grid_ghost_coords);
+  CHECK_ITERABLE_APPROX(fd_to_ghost_zones, lower_xi_neighbor_grid_ghost_coords);
+
+  // Check interpolating lower-xi DG data works to our FD ghost zones.
+  const auto lower_xi_dg_grid_coords =
+      lower_xi_element_map(element_logical_coords);
+  tnsr::I<DataVector, Dim, Frame::Grid> lower_xi_dg_to_ghost_zones{};
+  for (size_t i = 0; i < Dim; ++i) {
+    lower_xi_dg_to_ghost_zones.get(i) =
+        db::get<InterpolatorsFromNeighborDgToFd<Dim>>(box)
+            .at(lower_xi_id)
+            .value()
+            .interpolate(lower_xi_dg_grid_coords.get(i));
+  }
+  const auto expected_lower_xi_dg_to_ghost_zones =
+      db::get<domain::Tags::ElementMap<Dim, Frame::Grid>>(box)(
+          lower_xi_logical_ghost_coords);
+  CHECK_ITERABLE_APPROX(lower_xi_dg_to_ghost_zones,
+                        expected_lower_xi_dg_to_ghost_zones);
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.Evolution.Subcell.SetInterpolators",
+                  "[Evolution][Unit]") {
+  test<1>();
+  test<2>();
+  test<3>();
+}
