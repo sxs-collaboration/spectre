b1f1b497ab9d39dcaf5a8c06bc0a926dd8224cf6
Lawrence Kidder <kidder@astro.cornell.edu>
2023-08-23 17:03:11 -0400

    Document SpatialDiscretization namespace

---
+++ src/NumericalAlgorithms/SpatialDiscretization/SpatialDiscretization.hpp
@@ -0,0 +1,8 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+/// \ingroup AmrGroup
+/// \brief Items for specifying the spatial discretization
+namespace SpatialDiscretization {}  // namespace SpatialDiscretization
