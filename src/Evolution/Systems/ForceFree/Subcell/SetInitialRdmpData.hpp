7f667e1602f1c6a07fa2e5cf34ec045f4ef2d09d
Yoonsoo Kim <ykim7@caltech.edu>
2023-10-10 09:15:25 -0700

    Remove TildeQ from initial RDMP data

--- src/Evolution/Systems/ForceFree/Subcell/SetInitialRdmpData.hpp
+++ src/Evolution/Systems/ForceFree/Subcell/SetInitialRdmpData.hpp
@@ -30,17 +30,17 @@ namespace ForceFree::subcell {
  * Used on the subcells after the TCI marked the DG solution as inadmissible.
  */
 struct SetInitialRdmpData {
-  using argument_tags = tmpl::list<
-      ForceFree::Tags::TildeE, ForceFree::Tags::TildeB, ForceFree::Tags::TildeQ,
-      evolution::dg::subcell::Tags::ActiveGrid, ::domain::Tags::Mesh<3>,
-      evolution::dg::subcell::Tags::Mesh<3>>;
+  using argument_tags =
+      tmpl::list<ForceFree::Tags::TildeE, ForceFree::Tags::TildeB,
+                 evolution::dg::subcell::Tags::ActiveGrid,
+                 ::domain::Tags::Mesh<3>,
+                 evolution::dg::subcell::Tags::Mesh<3>>;
   using return_tags = tmpl::list<evolution::dg::subcell::Tags::DataForRdmpTci>;
 
   static void apply(
       gsl::not_null<evolution::dg::subcell::RdmpTciData*> rdmp_tci_data,
       const tnsr::I<DataVector, 3, Frame::Inertial>& subcell_tilde_e,
       const tnsr::I<DataVector, 3, Frame::Inertial>& subcell_tilde_b,
-      const Scalar<DataVector>& subcell_tilde_q,
       evolution::dg::subcell::ActiveGrid active_grid, const Mesh<3>& dg_mesh,
       const Mesh<3>& subcell_mesh);
 };
