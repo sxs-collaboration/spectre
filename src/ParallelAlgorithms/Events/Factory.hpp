ff441e5647776d78f7d9a0ae972cb425f899057f
William Throwe <wtt6@cornell.edu>
2023-11-03 00:06:27 -0400

    Add ErrorIfDataTooBig Event

--- src/ParallelAlgorithms/Events/Factory.hpp
+++ src/ParallelAlgorithms/Events/Factory.hpp
@@ -6,6 +6,7 @@
 #include <cstddef>
 #include <type_traits>
 
+#include "ParallelAlgorithms/Events/ErrorIfDataTooBig.hpp"
 #include "ParallelAlgorithms/Events/ObserveAdaptiveSteppingDiagnostics.hpp"
 #include "ParallelAlgorithms/Events/ObserveFields.hpp"
 #include "ParallelAlgorithms/Events/ObserveNorms.hpp"
@@ -16,11 +17,12 @@
 namespace dg::Events {
 template <size_t VolumeDim, typename Fields, typename NonTensorComputeTagsList,
           typename ArraySectionIdTag = void>
-using field_observations = tmpl::flatten<
-    tmpl::list<ObserveFields<VolumeDim, Fields, NonTensorComputeTagsList,
-                             ArraySectionIdTag>,
-               ::Events::ObserveNorms<Fields, NonTensorComputeTagsList,
-                                      ArraySectionIdTag>>>;
+using field_observations = tmpl::flatten<tmpl::list<
+    ::Events::ErrorIfDataTooBig<VolumeDim, Fields, NonTensorComputeTagsList>,
+    ObserveFields<VolumeDim, Fields, NonTensorComputeTagsList,
+                  ArraySectionIdTag>,
+    ::Events::ObserveNorms<Fields, NonTensorComputeTagsList,
+                           ArraySectionIdTag>>>;
 }  // namespace dg::Events
 
 namespace Events {
