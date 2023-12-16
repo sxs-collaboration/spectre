f100dd885163af74e097ccf64026814fb727f16b
Nils Vu <owls@nilsvu.de>
2023-12-15 16:24:42 -0800

    Store det times inv Jacobian for weak form

--- src/Elliptic/DiscontinuousGalerkin/Initialization.cpp
+++ src/Elliptic/DiscontinuousGalerkin/Initialization.cpp
@@ -49,6 +49,10 @@ void InitializeGeometry<Dim>::operator()(
                                         Frame::Inertial>*>
         inv_jacobian,
     const gsl::not_null<Scalar<DataVector>*> det_inv_jacobian,
+    const gsl::not_null<Scalar<DataVector>*> det_jacobian,
+    const gsl::not_null<InverseJacobian<DataVector, Dim, Frame::ElementLogical,
+                                        Frame::Inertial>*>
+        det_times_inv_jacobian,
     const std::vector<std::array<size_t, Dim>>& initial_extents,
     const std::vector<std::array<size_t, Dim>>& initial_refinement,
     const Domain<Dim>& domain,
@@ -74,9 +78,19 @@ void InitializeGeometry<Dim>::operator()(
   *inertial_coords =
       element_map->operator()(*logical_coords, 0., functions_of_time);
   // Jacobian
+  // Note: we can try to use `::dg::metric_identity_jacobian_quantities` here.
+  // When I tried (NV, Dec 2023) the DG residuals diverged on a sphere domain
+  // with a large outer boundary (1e9). This was possibly because no
+  // metric-identity Jacobians were used on faces, though I also tried slicing
+  // the metric-identity Jacobian to the faces and that didn't help.
   *inv_jacobian =
       element_map->inv_jacobian(*logical_coords, 0., functions_of_time);
   *det_inv_jacobian = determinant(*inv_jacobian);
+  get(*det_jacobian) = 1. / get(*det_inv_jacobian);
+  *det_times_inv_jacobian = *inv_jacobian;
+  for (auto& component : *det_times_inv_jacobian) {
+    component *= get(*det_jacobian);
+  }
 }
 
 namespace detail {
