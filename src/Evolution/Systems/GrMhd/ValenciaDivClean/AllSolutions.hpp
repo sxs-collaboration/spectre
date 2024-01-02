0f67b10e89ed4a909e405f040107d4d675beffc8
Jooheon Yoo <jy884@cornell.edu>
2024-01-02 12:53:12 -0800

    Add PolarMagnetizedFmDisk to list of initial data

--- src/Evolution/Systems/GrMhd/ValenciaDivClean/AllSolutions.hpp
+++ src/Evolution/Systems/GrMhd/ValenciaDivClean/AllSolutions.hpp
@@ -13,6 +13,7 @@
 #include "PointwiseFunctions/AnalyticData/GrMhd/MagnetizedFmDisk.hpp"
 #include "PointwiseFunctions/AnalyticData/GrMhd/MagnetizedTovStar.hpp"
 #include "PointwiseFunctions/AnalyticData/GrMhd/OrszagTangVortex.hpp"
+#include "PointwiseFunctions/AnalyticData/GrMhd/PolarMagnetizedFmDisk.hpp"
 #include "PointwiseFunctions/AnalyticData/GrMhd/RiemannProblem.hpp"
 #include "PointwiseFunctions/AnalyticData/GrMhd/SlabJet.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/AnalyticSolution.hpp"
@@ -26,16 +27,15 @@
 #include "PointwiseFunctions/AnalyticSolutions/RelativisticEuler/TovStar.hpp"
 
 namespace grmhd::ValenciaDivClean::InitialData {
-using initial_data_list =
-    tmpl::list<AnalyticData::BlastWave, AnalyticData::BondiHoyleAccretion,
-               AnalyticData::CcsnCollapse, AnalyticData::KhInstability,
-               AnalyticData::MagneticFieldLoop, AnalyticData::MagneticRotor,
-               AnalyticData::MagnetizedFmDisk, AnalyticData::MagnetizedTovStar,
-               AnalyticData::OrszagTangVortex, AnalyticData::RiemannProblem,
-               AnalyticData::SlabJet, Solutions::AlfvenWave,
-               grmhd::Solutions::BondiMichel, Solutions::KomissarovShock,
-               Solutions::SmoothFlow,
-               RelativisticEuler::Solutions::FishboneMoncriefDisk,
-               RelativisticEuler::Solutions::RotatingStar,
-               RelativisticEuler::Solutions::TovStar>;
+using initial_data_list = tmpl::list<
+    AnalyticData::BlastWave, AnalyticData::BondiHoyleAccretion,
+    AnalyticData::CcsnCollapse, AnalyticData::KhInstability,
+    AnalyticData::MagneticFieldLoop, AnalyticData::MagneticRotor,
+    AnalyticData::MagnetizedFmDisk, AnalyticData::MagnetizedTovStar,
+    AnalyticData::OrszagTangVortex, AnalyticData::PolarMagnetizedFmDisk,
+    AnalyticData::RiemannProblem, AnalyticData::SlabJet, Solutions::AlfvenWave,
+    grmhd::Solutions::BondiMichel, Solutions::KomissarovShock,
+    Solutions::SmoothFlow, RelativisticEuler::Solutions::FishboneMoncriefDisk,
+    RelativisticEuler::Solutions::RotatingStar,
+    RelativisticEuler::Solutions::TovStar>;
 }  // namespace grmhd::ValenciaDivClean::InitialData
