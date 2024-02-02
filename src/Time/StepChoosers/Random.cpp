7a81356fe109820e2d80bca7a260e63d51ae44fc
William Throwe <wtt6@cornell.edu>
2024-02-02 16:26:42 -0500

    Add Random StepChooser for debugging
    Not added to the default list of available StepChoosers.
    
    The dependence of Time on Domain is unpleasant, but it was already
    there and just didn't trigger an error before.

---
+++ src/Time/StepChoosers/Random.cpp
@@ -0,0 +1,83 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Time/StepChoosers/Random.hpp"
+
+#include <boost/functional/hash.hpp>
+#include <cmath>
+#include <cstddef>
+#include <pup.h>
+#include <random>
+#include <utility>
+
+#include "Domain/Structure/Element.hpp"
+#include "Options/Context.hpp"
+#include "Options/ParseError.hpp"
+#include "Time/StepChoosers/StepChooser.hpp"
+#include "Time/TimeStepId.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+
+namespace StepChoosers {
+
+template <typename StepChooserUse, size_t VolumeDim>
+Random<StepChooserUse, VolumeDim>::Random() = default;
+
+template <typename StepChooserUse, size_t VolumeDim>
+Random<StepChooserUse, VolumeDim>::Random(CkMigrateMessage* /*unused*/) {}
+
+template <typename StepChooserUse, size_t VolumeDim>
+Random<StepChooserUse, VolumeDim>::Random(const double minimum,
+                                          const double maximum,
+                                          const size_t seed,
+                                          const Options::Context& context)
+    : minimum_(minimum), maximum_(maximum), seed_(seed) {
+  if (minimum_ >= maximum_) {
+    PARSE_ERROR(context, "Must have Minimum < Maximum");
+  }
+}
+
+template <typename StepChooserUse, size_t VolumeDim>
+std::pair<double, bool> Random<StepChooserUse, VolumeDim>::operator()(
+    const Element<VolumeDim>& element, const TimeStepId& time_step_id,
+    const double /*last_step_magnitude*/) const {
+  size_t local_seed = seed_;
+  boost::hash_combine(local_seed, element.id());
+  boost::hash_combine(local_seed, time_step_id);
+  std::mt19937_64 rng(local_seed);
+  std::uniform_real_distribution<> dist(log(minimum_), log(maximum_));
+  for (;;) {
+    const double step = exp(dist(rng));
+    // Don't produce out-of-range values because of roundoff.
+    if (step >= minimum_ and step <= maximum_) {
+      return {step, true};
+    }
+  }
+}
+
+template <typename StepChooserUse, size_t VolumeDim>
+bool Random<StepChooserUse, VolumeDim>::uses_local_data() const {
+  return true;
+}
+
+template <typename StepChooserUse, size_t VolumeDim>
+void Random<StepChooserUse, VolumeDim>::pup(PUP::er& p) {
+  p | minimum_;
+  p | maximum_;
+  p | seed_;
+}
+
+template <typename StepChooserUse, size_t VolumeDim>
+PUP::able::PUP_ID Random<StepChooserUse, VolumeDim>::my_PUP_ID = 0;  // NOLINT
+
+#define USE(data) BOOST_PP_TUPLE_ELEM(0, data)
+#define DIM(data) BOOST_PP_TUPLE_ELEM(1, data)
+
+#define INSTANTIATE(_, data) \
+  template class Random<StepChooserUse::USE(data), DIM(data)>;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (Slab, LtsStep), (1, 2, 3))
+
+#undef INSTANTIATE
+#undef DIM
+#undef USE
+}  // namespace StepChoosers
---
+++ tests/Unit/Time/StepChoosers/Test_Random.cpp
@@ -0,0 +1,117 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cstddef>
+#include <iomanip>
+#include <memory>
+#include <random>
+#include <unordered_set>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Domain/Structure/Element.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Tags.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Options/Protocols/FactoryCreation.hpp"
+#include "Parallel/Tags/Metavariables.hpp"
+#include "Time/Slab.hpp"
+#include "Time/StepChoosers/Random.hpp"
+#include "Time/StepChoosers/StepChooser.hpp"
+#include "Time/Tags/TimeStepId.hpp"
+#include "Time/TimeStepId.hpp"
+#include "Utilities/MakeString.hpp"
+#include "Utilities/ProtocolHelpers.hpp"
+#include "Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace {
+constexpr size_t volume_dim = 2;
+
+struct Metavariables {
+  struct factory_creation
+      : tt::ConformsTo<Options::protocols::FactoryCreation> {
+    using factory_classes =
+        tmpl::map<tmpl::pair<StepChooser<StepChooserUse::LtsStep>,
+                             tmpl::list<StepChoosers::Random<
+                                 StepChooserUse::LtsStep, volume_dim>>>,
+                  tmpl::pair<StepChooser<StepChooserUse::Slab>,
+                             tmpl::list<StepChoosers::Random<
+                                 StepChooserUse::Slab, volume_dim>>>>;
+  };
+  using component_list = tmpl::list<>;
+};
+
+template <typename Use>
+double get_suggestion(const double min, const double max, const size_t seed,
+                      const Element<volume_dim>& element,
+                      const TimeStepId& time_step_id) {
+  using Random = StepChoosers::Random<Use, volume_dim>;
+
+  auto box = db::create<
+      db::AddSimpleTags<Parallel::Tags::MetavariablesImpl<Metavariables>,
+                        Tags::TimeStepId, domain::Tags::Element<volume_dim>>>(
+      Metavariables{}, time_step_id, element);
+
+  // Not used.
+  const double current_step = 1.23;
+
+  const Random random(min, max, seed);
+  const std::unique_ptr<StepChooser<Use>> random_base =
+      TestHelpers::test_factory_creation<StepChooser<Use>, Random>(
+          MakeString{} << std::setprecision(19) << "Random:\n"
+                       << "  Minimum: " << min << "\n"
+                       << "  Maximum: " << max << "\n"
+                       << "  Seed: " << seed);
+
+  CHECK(random.uses_local_data());
+
+  const auto result = random(element, time_step_id, current_step);
+  // Should be deterministic
+  CHECK(result == random(element, time_step_id, current_step));
+
+  CHECK(result.second);
+
+  CHECK(result.first >= min);
+  CHECK(result.first <= max);
+
+  CHECK(serialize_and_deserialize(random)(element, time_step_id,
+                                          current_step) == result);
+  CHECK(random_base->desired_step(current_step, box) == result);
+  CHECK(
+      serialize_and_deserialize(random_base)->desired_step(current_step, box) ==
+      result);
+
+  return result.first;
+}
+
+template <typename Use>
+void test_random() {
+  const Element<volume_dim> element1(ElementId<volume_dim>(1), {});
+  const Element<volume_dim> element2(ElementId<volume_dim>(2), {});
+  const TimeStepId time1(true, 0, Slab(0.0, 1.0).start());
+  const TimeStepId time2(true, 0, Slab(0.0, 1.0).end());
+
+  MAKE_GENERATOR(gen);
+  const double min = std::uniform_real_distribution(1.0e-6, 1.0)(gen);
+  const double max = min * std::uniform_real_distribution(1.1, 10.0)(gen);
+  const size_t seed = gen();
+
+  std::unordered_set<double> results{};
+  for (const auto& element : {element1, element2}) {
+    for (const auto& time : {time1, time2}) {
+      CHECK(results.insert(get_suggestion<Use>(min, max, seed, element, time))
+                .second);
+    }
+  }
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.Time.StepChoosers.Random", "[Unit][Time]") {
+  register_factory_classes_with_charm<Metavariables>();
+
+  test_random<StepChooserUse::LtsStep>();
+  test_random<StepChooserUse::Slab>();
+}
