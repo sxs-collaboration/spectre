66b4ccbaf8baf21047c857c75b3d778ba8267a28
Kyle Nelli <knelli@caltech.edu>
2024-02-14 23:09:44 -0800

    Prevent NumGridPointsAndGridSpacing element dist when using GTS

--- tests/Unit/Domain/Tags/Test_ElementDistribution.cpp
+++ tests/Unit/Domain/Tags/Test_ElementDistribution.cpp
@@ -12,19 +12,57 @@
 #include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
 #include "Parallel/Tags/Parallelization.hpp"
 
+namespace {
+template <bool UseLTS>
+struct TestMetavars {
+  static constexpr bool local_time_stepping = UseLTS;
+};
+
+template <bool UseLTS>
+std::optional<domain::ElementWeight> make_option(
+    const std::string& option_string) {
+  return TestHelpers::test_option_tag<domain::OptionTags::ElementDistribution,
+                                      TestMetavars<UseLTS>>(option_string);
+}
+
+std::optional<domain::ElementWeight> make_option_without_lts_metavars(
+    const std::string& option_string) {
+  return TestHelpers::test_option_tag<domain::OptionTags::ElementDistribution>(
+      option_string);
+}
+}  // namespace
+
 SPECTRE_TEST_CASE("Unit.Domain.Tags.ElementDistribution", "[Unit][Domain]") {
   TestHelpers::db::test_simple_tag<domain::Tags::ElementDistribution>(
       "ElementDistribution");
-  const auto make_option = [](const std::string& option_string)
-      -> std::optional<domain::ElementWeight> {
-    return TestHelpers::test_option_tag<
-        domain::OptionTags::ElementDistribution>(option_string);
-  };
-  CHECK(make_option("Uniform") ==
+  CHECK(make_option<true>("Uniform") ==
         std::optional{domain::ElementWeight::Uniform});
-  CHECK(make_option("NumGridPoints") ==
+  CHECK(make_option<true>("NumGridPoints") ==
         std::optional{domain::ElementWeight::NumGridPoints});
-  CHECK(make_option("NumGridPointsAndGridSpacing") ==
+  CHECK(make_option<true>("NumGridPointsAndGridSpacing") ==
         std::optional{domain::ElementWeight::NumGridPointsAndGridSpacing});
-  CHECK(make_option("RoundRobin") == std::nullopt);
+  CHECK(make_option<true>("RoundRobin") == std::nullopt);
+
+  CHECK(make_option<false>("Uniform") ==
+        std::optional{domain::ElementWeight::Uniform});
+  CHECK(make_option<false>("NumGridPoints") ==
+        std::optional{domain::ElementWeight::NumGridPoints});
+  CHECK_THROWS_WITH(make_option<false>("NumGridPointsAndGridSpacing"),
+                    Catch::Matchers::ContainsSubstring(
+                        "When not using local time stepping") and
+                        Catch::Matchers::ContainsSubstring(
+                            "Please choose another element distribution."));
+  CHECK(make_option<false>("RoundRobin") == std::nullopt);
+
+  CHECK(make_option_without_lts_metavars("Uniform") ==
+        std::optional{domain::ElementWeight::Uniform});
+  CHECK(make_option_without_lts_metavars("NumGridPoints") ==
+        std::optional{domain::ElementWeight::NumGridPoints});
+  CHECK_THROWS_WITH(
+      make_option_without_lts_metavars("NumGridPointsAndGridSpacing"),
+      Catch::Matchers::ContainsSubstring(
+          "When not using local time stepping") and
+          Catch::Matchers::ContainsSubstring(
+              "Please choose another element distribution."));
+  CHECK(make_option_without_lts_metavars("RoundRobin") == std::nullopt);
 }
