fc78f50337e949b076b8db6b6be3496f81a0457b
Nikolas <niko.wittek@gmail.com>
2024-02-16 16:41:39 +0100

    Change worldtube evolved vars + initializer

--- tests/Unit/Evolution/Systems/CurvedScalarWave/Worldtube/SingletonActions/Test_InitializeEvolvedVariables.cpp
+++ tests/Unit/Evolution/Systems/CurvedScalarWave/Worldtube/SingletonActions/Test_InitializeEvolvedVariables.cpp
@@ -3,9 +3,11 @@
 
 #include "Framework/TestingFramework.hpp"
 
+#include <array>
 #include <memory>
 
 #include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
 #include "Evolution/Systems/CurvedScalarWave/Worldtube/SingletonActions/InitializeEvolvedVariables.hpp"
 #include "Time/Tags/HistoryEvolvedVariables.hpp"
 #include "Time/Tags/TimeStepper.hpp"
@@ -18,22 +20,31 @@ namespace {
 SPECTRE_TEST_CASE(
     "Unit.Evolution.Systems.CSW.Worldtube.InitializeEvolvedVariables",
     "[Unit][Evolution]") {
-  using variables_tag = ::Tags::Variables<tmpl::list<Tags::Psi0, Tags::dtPsi0>>;
+  using variables_tag = ::Tags::Variables<
+      tmpl::list<Tags::EvolvedPosition<3>, Tags::EvolvedVelocity<3>>>;
   using dt_variables_tag = db::add_tag_prefix<::Tags::dt, variables_tag>;
-
+  const tnsr::I<double, 3> initial_pos{{1., 2., 3.}};
+  const tnsr::I<double, 3> initial_vel{{4., 5., 6.}};
   auto box = db::create<
       db::AddSimpleTags<variables_tag, dt_variables_tag,
                         ::Tags::HistoryEvolvedVariables<variables_tag>,
-                        ::Tags::ConcreteTimeStepper<TimeStepper>>,
+                        ::Tags::ConcreteTimeStepper<TimeStepper>,
+                        Tags::InitialPositionAndVelocity>,
       time_stepper_ref_tags<TimeStepper>>(
       variables_tag::type{}, dt_variables_tag::type{},
       TimeSteppers::History<variables_tag::type>{},
       static_cast<std::unique_ptr<TimeStepper>>(
-          std::make_unique<TimeSteppers::AdamsBashforth>(4)));
+          std::make_unique<TimeSteppers::AdamsBashforth>(4)),
+      std::array<tnsr::I<double, 3>, 2>{{initial_pos, initial_vel}});
 
   db::mutate_apply<Initialization::InitializeEvolvedVariables>(
       make_not_null(&box));
-  CHECK(db::get<variables_tag>(box) == variables_tag::type(size_t(1), 0.));
+
+  const auto vars = db::get<variables_tag>(box);
+  for (size_t i = 0; i < 3; ++i) {
+    CHECK(get<Tags::EvolvedPosition<3>>(vars).get(i)[0] == initial_pos.get(i));
+    CHECK(get<Tags::EvolvedVelocity<3>>(vars).get(i)[0] == initial_vel.get(i));
+  }
   CHECK(db::get<dt_variables_tag>(box) ==
         dt_variables_tag::type(size_t(1), 0.));
   CHECK(db::get<::Tags::HistoryEvolvedVariables<variables_tag>>(box) ==
