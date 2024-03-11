3d1249426f04e8fa08d97a9c1a2a2305ff2cabe8
Yoonsoo Kim <ykim7@caltech.edu>
2024-03-11 10:17:08 -0700

    Filter evolved variables in ForceFree DG executable

--- src/Evolution/Executables/ForceFree/EvolveForceFree.hpp
+++ src/Evolution/Executables/ForceFree/EvolveForceFree.hpp
@@ -38,10 +38,12 @@
 #include "Evolution/Systems/ForceFree/MaskNeutronStarInterior.hpp"
 #include "Evolution/Systems/ForceFree/System.hpp"
 #include "Evolution/Systems/ForceFree/Tags.hpp"
+#include "Evolution/Tags/Filter.hpp"
 #include "IO/Observer/Actions/RegisterEvents.hpp"
 #include "IO/Observer/Actions/RegisterWithObservers.hpp"
 #include "IO/Observer/Helpers.hpp"
 #include "IO/Observer/ObserverComponent.hpp"
+#include "NumericalAlgorithms/LinearOperators/ExponentialFilter.hpp"
 #include "Options/Options.hpp"
 #include "Options/Protocols/FactoryCreation.hpp"
 #include "Parallel/Local.hpp"
@@ -54,6 +56,7 @@
 #include "Parallel/Protocols/RegistrationMetavariables.hpp"
 #include "ParallelAlgorithms/Actions/AddComputeTags.hpp"
 #include "ParallelAlgorithms/Actions/AddSimpleTags.hpp"
+#include "ParallelAlgorithms/Actions/FilterAction.hpp"
 #include "ParallelAlgorithms/Actions/InitializeItems.hpp"
 #include "ParallelAlgorithms/Actions/LimiterActions.hpp"
 #include "ParallelAlgorithms/Actions/MutateApply.hpp"
@@ -192,7 +195,15 @@ struct EvolutionMetavars {
               evolution::Actions::RunEventsAndDenseTriggers<tmpl::list<>>,
               Actions::UpdateU<system>>>,
       Limiters::Actions::SendData<EvolutionMetavars>,
-      Limiters::Actions::Limit<EvolutionMetavars>>>;
+      Limiters::Actions::Limit<EvolutionMetavars>,
+
+      dg::Actions::Filter<
+          Filters::Exponential<0>,
+          tmpl::list<ForceFree::Tags::TildeE, ForceFree::Tags::TildeB,
+                     ForceFree::Tags::TildePsi, ForceFree::Tags::TildePhi,
+                     ForceFree::Tags::TildeQ>>
+
+      >>;
 
   using const_global_cache_tags =
       tmpl::list<evolution::initial_data::Tags::InitialData,
