f0d8e03e73fd1782de41d854ff113e2c706a4c4e
Francois Foucart <francois.foucart@gmail.com>
2024-04-09 13:05:05 -0700

    Calculation of proper and coordinate volume needed for MC

---
+++ src/Evolution/Particles/MonteCarlo/CellVolume.cpp
@@ -0,0 +1,33 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Evolution/Particles/MonteCarlo/CellVolume.hpp"
+
+namespace Particles::MonteCarlo {
+
+void cell_proper_four_volume_finite_difference(
+    const gsl::not_null<Scalar<DataVector>*> cell_proper_four_volume,
+    const Scalar<DataVector>& lapse,
+    const Scalar<DataVector>& determinant_spatial_metric,
+    const double time_step, const Mesh<3>& mesh,
+    const Scalar<DataVector>& det_jacobian_logical_to_inertial) {
+  const double cell_logical_volume =
+    8.0 / static_cast<double>(mesh.number_of_grid_points());
+  cell_proper_four_volume->get() =
+      get(lapse) * get(determinant_spatial_metric) * time_step *
+      cell_logical_volume * get(det_jacobian_logical_to_inertial);
+}
+
+void cell_inertial_coordinate_three_volume_finite_difference(
+    gsl::not_null<Scalar<DataVector>*> cell_inertial_three_volume,
+    const Mesh<3>& mesh,
+    const Scalar<DataVector>& det_jacobian_logical_to_inertial) {
+  const double cell_logical_volume =
+    8.0 / static_cast<double>(mesh.number_of_grid_points());
+  cell_inertial_three_volume->get() =
+      cell_logical_volume * get(det_jacobian_logical_to_inertial);
+}
+
+}  // namespace Particles::MonteCarlo
