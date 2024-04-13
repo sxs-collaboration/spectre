ea1c955c6da523aaa646a9536d7e383442a9d196
Nils Deppe <nils@nilsdeppe.com>
2024-04-13 13:09:34 -0400

    Support nodegroup DG collection in ObserveNorms

--- src/ParallelAlgorithms/Events/ObserveNorms.hpp
+++ src/ParallelAlgorithms/Events/ObserveNorms.hpp
@@ -32,6 +32,7 @@
 #include "Parallel/Invoke.hpp"
 #include "Parallel/Local.hpp"
 #include "Parallel/Reduction.hpp"
+#include "Parallel/TypeTraits.hpp"
 #include "ParallelAlgorithms/Events/Tags.hpp"
 #include "ParallelAlgorithms/EventsAndTriggers/Event.hpp"
 #include "Utilities/ErrorHandling/Assert.hpp"
@@ -447,17 +448,24 @@ operator()(const ObservationBox<ComputeTagsList, DataBoxType>& box,
                 norm_values_and_names["VolumeIntegral"].second.begin(),
                 norm_values_and_names["VolumeIntegral"].second.end());
 
-  // Send data to reduction observer
-  auto& local_observer = *Parallel::local_branch(
-      Parallel::get_parallel_component<observers::Observer<Metavariables>>(
-          cache));
   const std::string subfile_path_with_suffix =
       subfile_path_ + section_observation_key.value();
-  Parallel::simple_action<observers::Actions::ContributeReductionData>(
+  // Send data to reduction observer
+  auto& local_observer = *Parallel::local_branch(
+      Parallel::get_parallel_component<
+          tmpl::conditional_t<Parallel::is_nodegroup_v<ParallelComponent>,
+                              observers::ObserverWriter<Metavariables>,
+                              observers::Observer<Metavariables>>>(cache));
+  Parallel::simple_action<tmpl::conditional_t<
+      Parallel::is_nodegroup_v<ParallelComponent>,
+      observers::ThreadedActions::CollectReductionDataOnNode,
+      observers::Actions::ContributeReductionData>>(
       local_observer,
-      observers::ObservationId(observation_value.value,
-                               subfile_path_with_suffix + ".dat"),
-      Parallel::make_array_component_id<ParallelComponent>(array_index),
+      observers::ObservationId{observation_value.value,
+                               subfile_path_with_suffix + ".dat"},
+      Parallel::ArrayComponentId{
+          std::add_pointer_t<ParallelComponent>{nullptr},
+          Parallel::ArrayIndex<ElementId<VolumeDim>>(array_index)},
       subfile_path_with_suffix, std::move(legend),
       ReductionData{observation_value.value, number_of_points, local_volume,
                     std::move(norm_values_and_names["Max"].first),
