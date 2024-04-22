6b55eb38bc953b90633534aca8cc71e300727790
Nils Vu <nils.vu@black-holes.org>
2024-04-22 14:41:16 -0700

    Make SpEC Python scripts available to import
    These can be used until we have ported them to SpECTRE.

--- cmake/SetupSpec.cmake
+++ cmake/SetupSpec.cmake
@@ -3,9 +3,15 @@
 
 find_package(SpEC)
 
-if (SpEC_FOUND)
-  file(APPEND
-    "${CMAKE_BINARY_DIR}/BuildInfo.txt"
-    "SpEC exporter: ${SPEC_EXPORTER_ROOT}\n"
-    )
+if (NOT SpEC_FOUND)
+  return()
 endif()
+
+# Make SpEC scripts available in Python. These can be used until we have ported
+# them to SpECTRE.
+set(PYTHONPATH "${SPEC_ROOT}/Support/Python:${PYTHONPATH}")
+
+file(APPEND
+  "${CMAKE_BINARY_DIR}/BuildInfo.txt"
+  "SpEC exporter: ${SPEC_EXPORTER_ROOT}\n"
+  )
