471125fc355096df42075e63792c5485fe5c51b7
William Throwe <wtt6@cornell.edu>
2024-05-20 18:36:21 -0400

    Add SelfStart::step_unused function

--- src/Time/SelfStart.cpp
+++ src/Time/SelfStart.cpp
@@ -9,4 +9,9 @@ namespace SelfStart {
 bool is_self_starting(const TimeStepId& time_id) {
   return time_id.slab_number() < 0;
 }
+
+bool step_unused(const TimeStepId& time_id, const TimeStepId& next_time_id) {
+  return time_id.slab_number() < 0 and
+         time_id.slab_number() != next_time_id.slab_number();
+}
 }  // namespace SelfStart
--- src/Time/SelfStart.hpp
+++ src/Time/SelfStart.hpp
@@ -12,4 +12,8 @@ namespace SelfStart {
 /// negative if and only if self-start is in progress. If self start is
 /// modified to alter that behavior, this utility must also be modified.
 bool is_self_starting(const TimeStepId& time_id);
+
+/// Returns whether the result of the step from \p time_id to \p
+/// next_time_id is unused because of a self-start reset.
+bool step_unused(const TimeStepId& time_id, const TimeStepId& next_time_id);
 }  // namespace SelfStart
