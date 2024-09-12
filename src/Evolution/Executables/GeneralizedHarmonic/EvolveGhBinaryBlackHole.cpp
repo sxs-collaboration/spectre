83aa1f1acdbd626dedf2c3f381661ad57da93286
Nils Deppe <nils@nilsdeppe.com>
2024-09-11 21:41:31 -0400

    Document MPI debugging and enable in BBH exec

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGhBinaryBlackHole.cpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGhBinaryBlackHole.cpp
@@ -13,11 +13,13 @@
 #include "Evolution/Systems/GeneralizedHarmonic/ConstraintDamping/RegisterDerivedWithCharm.hpp"
 #include "Parallel/CharmMain.tpp"
 #include "Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp"
+#include "Utilities/System/AttachDebugger.hpp"
 
 extern "C" void CkRegisterMainModule() {
   Parallel::charmxx::register_main_module<EvolutionMetavars>();
   Parallel::charmxx::register_init_node_and_proc(
-      {&domain::creators::register_derived_with_charm,
+      {&sys::attach_debugger,
+       &domain::creators::register_derived_with_charm,
        &domain::creators::time_dependence::register_derived_with_charm,
        &domain::FunctionsOfTime::register_derived_with_charm,
        &gh::BoundaryCorrections::register_derived_with_charm,
