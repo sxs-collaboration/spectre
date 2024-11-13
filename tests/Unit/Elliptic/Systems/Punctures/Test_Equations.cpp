cf60bd4381e67f01c1c1ca15d02acd5f491b86ea
Nils Vu <owls@nilsvu.de>
2024-11-13 13:04:29 -0800

    WIP use autodiff to test punctures linearization

--- tests/Unit/Elliptic/Systems/Punctures/Test_Equations.cpp
+++ tests/Unit/Elliptic/Systems/Punctures/Test_Equations.cpp
@@ -4,10 +4,12 @@
 #include "Framework/TestingFramework.hpp"
 
 #include <array>
+#include <autodiff/forward/real.hpp>
 #include <cstddef>
 #include <string>
 
 #include "DataStructures/DataVector.hpp"
+#include "DataStructures/DynamicVector.hpp"
 #include "Elliptic/Protocols/FirstOrderSystem.hpp"
 #include "Elliptic/Systems/Punctures/FirstOrderSystem.hpp"
 #include "Elliptic/Systems/Punctures/Sources.hpp"
@@ -17,6 +19,25 @@
 #include "Utilities/ProtocolHelpers.hpp"
 
 namespace {
+
+template <typename DataType, typename VarDataType>
+VarDataType puncture_sources(const Scalar<DataType>& alpha,
+                        const Scalar<DataType>& beta,
+                        const Scalar<VarDataType>& field) {
+  return get(beta) / pow<7>(get(alpha) * (get(field) + 1.) + 1.);
+}
+
+void add_linearized_sources_autodiff(
+    const gsl::not_null<Scalar<DataVector>*> linearized_puncture_equation,
+    const Scalar<DataVector>& alpha, const Scalar<DataVector>& beta,
+    const Scalar<DataVector>& field,
+    const Scalar<DataVector>& field_correction) {
+  Scalar<blaze::DynamicVector<autodiff::real>> field_dual(get(field));
+  const auto dS_du = autodiff::derivative(
+      &puncture_sources<DataVector, blaze::DynamicVector<autodiff::real>>,
+      autodiff::wrt(get(field_dual)), autodiff::at(alpha, beta, field_dual));
+  get(*linearized_puncture_equation) -= dS_du * get(field_correction);
+}
 
 void test_equations(const DataVector& used_for_size) {
   const double eps = 1.e-12;
@@ -28,6 +49,10 @@ void test_equations(const DataVector& used_for_size) {
       fill_result_tensors);
   pypp::check_with_random_values<4>(
       &Punctures::add_linearized_sources, "Equations", {"linearized_sources"},
+      {{{-1., 1.}, {-1., 1.}, {-1., 1.}, {-1., 1.}}}, used_for_size, eps, seed,
+      fill_result_tensors);
+  pypp::check_with_random_values<4>(
+      &add_linearized_sources_autodiff, "Equations", {"linearized_sources"},
       {{{-1., 1.}, {-1., 1.}, {-1., 1.}, {-1., 1.}}}, used_for_size, eps, seed,
       fill_result_tensors);
 }
