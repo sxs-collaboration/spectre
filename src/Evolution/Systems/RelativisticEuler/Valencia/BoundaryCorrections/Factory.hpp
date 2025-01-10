1ef93f2314387e0045a5fedd362b3137ad15790a
Nils Deppe <nils@nilsdeppe.com>
2025-01-10 17:12:55 -0500

    Remove Valencia system
    We have optimizations in GRMHD to handle the zero magnetic field case reasonably
    well.

--- src/Evolution/Systems/RelativisticEuler/Valencia/BoundaryCorrections/Factory.hpp
+++
@@ -1,7 +0,0 @@
-// Distributed under the MIT License.
-// See LICENSE.txt for details.
-
-#pragma once
-
-#include "Evolution/Systems/RelativisticEuler/Valencia/BoundaryCorrections/BoundaryCorrection.hpp"
-#include "Evolution/Systems/RelativisticEuler/Valencia/BoundaryCorrections/Rusanov.hpp"
--- src/Evolution/Systems/RelativisticEuler/Valencia/BoundaryCorrections/RegisterDerived.hpp
+++
@@ -1,8 +0,0 @@
-// Distributed under the MIT License.
-// See LICENSE.txt for details.
-
-#pragma once
-
-namespace RelativisticEuler::Valencia::BoundaryCorrections {
-void register_derived_with_charm();
-}  // namespace RelativisticEuler::Valencia::BoundaryCorrections
