6656db23c767158c828b5c3c8908c2c02debb88a
Kyle Nelli <knelli@caltech.edu>
2025-01-16 19:28:53 -0800

    Add Skew map

---
+++ src/Domain/CoordinateMaps/TimeDependent/Skew.cpp
@@ -0,0 +1,408 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Domain/CoordinateMaps/TimeDependent/Skew.hpp"
+
+#include <array>
+#include <cmath>
+#include <limits>
+#include <ostream>
+#include <pup.h>
+#include <pup_stl.h>
+#include <type_traits>
+#include <unordered_set>
+#include <utility>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/DeterminantAndInverse.hpp"
+#include "DataStructures/Tensor/Identity.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
+#include "NumericalAlgorithms/RootFinding/TOMS748.hpp"
+#include "Utilities/ContainerHelpers.hpp"
+#include "Utilities/DereferenceWrapper.hpp"
+#include "Utilities/EqualWithinRoundoff.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeArray.hpp"
+#include "Utilities/MakeWithValue.hpp"
+#include "Utilities/StdArrayHelpers.hpp"
+#include "Utilities/StdHelpers.hpp"
+#include "Utilities/StlStreamDeclarations.hpp"
+#include "Utilities/TypeTraits/RemoveReferenceWrapper.hpp"
+
+namespace domain::CoordinateMaps::TimeDependent {
+
+Skew::Skew(std::string function_of_time_name,
+           const std::array<double, 3>& center, const double outer_radius)
+    : f_of_t_name_(std::move(function_of_time_name)),
+      center_(center),
+      outer_radius_(outer_radius),
+      outer_radius_squared_(square(outer_radius_)),
+      f_of_t_names_({f_of_t_name_}) {
+  if (magnitude(center_) >= outer_radius) {
+    ERROR("Center of Skew map "
+          << center_ << " with radius " << magnitude(center_)
+          << " must be within the outer radius " << outer_radius_);
+  }
+}
+
+template <typename T>
+std::array<tt::remove_cvref_wrap_t<T>, 3> Skew::operator()(
+    const std::array<T, 3>& source_coords, const double time,
+    const domain::FunctionsOfTimeMap& functions_of_time) const {
+  return map_and_velocity_helper(source_coords, time, functions_of_time, false);
+}
+
+std::optional<std::array<double, 3>> Skew::inverse(
+    const std::array<double, 3>& target_coords, const double time,
+    const domain::FunctionsOfTimeMap& functions_of_time) const {
+  const auto& function_of_time = functions_of_time.at(f_of_t_name_);
+  const DataVector func = function_of_time->func_and_deriv(time)[0];
+  ASSERT(func.size() == 2, "Expected a function of time with size 2, not "
+                               << func.size() << " in the Skew map.");
+
+  // Short circuit if there's no function of time
+  if (equal_within_roundoff(func[0], 0.0) and
+      equal_within_roundoff(func[1], 0.0)) {
+    return target_coords;
+  }
+
+  // Short circuit if we're at the outer radius
+  const double target_radius = magnitude(target_coords);
+  if (equal_within_roundoff(target_radius, outer_radius_)) {
+    return target_coords;
+  } else if (target_radius >= outer_radius_) {
+    return std::nullopt;
+  }
+
+  // Another short circuit. Target y & z are the same as source y & z
+  const double tan_sum = tan(func[0]) * (target_coords[1] - center_[1]) +
+                         tan(func[1]) * (target_coords[2] - center_[2]);
+  if (equal_within_roundoff(tan_sum, 0.0)) {
+    return target_coords;
+  }
+
+  std::array<double, 3> temporary_source_coord = target_coords;
+
+  const auto root_func = [&](const double source_coord_x) -> double {
+    temporary_source_coord[0] = source_coord_x;
+    const double width = get_width(temporary_source_coord);
+    return source_coord_x + width * tan_sum - target_coords[0];
+  };
+
+  // \bar{x} -> x + w * (tan(f_y)*(y-y_C) + tan(f_z)*(z-z_C)) and 0<=w<=1, then
+  // x <= \bar{x} <= x + (tan(f_y)*(y-y_C) + tan(f_z)*(z-z_C)), ==>
+  // 0 <= \bar{x} - x <= (tan(f_y)*(y-y_C) + tan(f_z)*(z-z_C)), ==>
+  // -\bar{x} <= - x <= -\bar{x} + (tan(f_y)*(y-y_C) + tan(f_z)*(z-z_C)), ==>
+  // \bar{x} >= x >= \bar{x} - (tan(f_y)*(y-y_C) + tan(f_z)*(z-z_C))
+  // This last line is our bracket for the root
+
+  // We give a small epsilon to give the root find a little buffer. It is highly
+  // unlikely there will be another root within these bounds
+  const double eps = std::numeric_limits<double>::epsilon() * 100.0;
+
+  const double x_0 = target_coords[0] - tan_sum - eps;
+  const double x_1 = target_coords[0] + eps;
+  const double lower_x = std::min(x_0, x_1);
+  const double upper_x = std::max(x_0, x_1);
+  const double lower_func = root_func(lower_x);
+  const double upper_func = root_func(upper_x);
+
+  // Since we adjusted the bounds by eps above, we have a slightly larger eps
+  // here to account for the arithmetic of the root func
+  if (equal_within_roundoff(lower_func, 0.0, 5 * eps)) {
+    temporary_source_coord[0] = lower_x;
+    return temporary_source_coord;
+  }
+
+  if (equal_within_roundoff(upper_func, 0.0, 5 * eps)) {
+    temporary_source_coord[0] = upper_x;
+    return temporary_source_coord;
+  }
+
+  if (lower_func * upper_func > 0.0) {
+    ERROR("No root in Skew map inverse!. Target coord = "
+          << target_coords << ", lower_x = " << lower_x
+          << ", lower_func = " << lower_func << ", upper_x = " << upper_x
+          << ", upper_func = " << upper_func << ", tan_sum = " << tan_sum);
+  }
+
+  temporary_source_coord[0] = RootFinder::toms748<true>(
+      root_func, lower_x, upper_x, lower_func, upper_func, eps, eps);
+
+  return temporary_source_coord;
+}
+
+template <typename T>
+std::array<tt::remove_cvref_wrap_t<T>, 3> Skew::frame_velocity(
+    const std::array<T, 3>& source_coords, const double time,
+    const domain::FunctionsOfTimeMap& functions_of_time) const {
+  return map_and_velocity_helper(source_coords, time, functions_of_time, true);
+}
+
+template <typename T>
+tnsr::Ij<tt::remove_cvref_wrap_t<T>, 3, Frame::NoFrame> Skew::jacobian(
+    const std::array<T, 3>& source_coords, const double time,
+    const domain::FunctionsOfTimeMap& functions_of_time) const {
+  using ResultT = tt::remove_cvref_wrap_t<T>;
+
+  check_for_singular_map(source_coords, time, functions_of_time);
+
+  const ResultT width = get_width(source_coords);
+  const std::array<ResultT, 3> width_deriv = get_width_deriv(source_coords);
+
+  const auto& function_of_time = functions_of_time.at(f_of_t_name_);
+  const DataVector func = function_of_time->func_and_deriv(time)[0];
+  ASSERT(func.size() == 2, "Expected a function of time with size 2, not "
+                               << func.size() << " in the Skew map.");
+
+  auto result = identity<3>(dereference_wrapper(source_coords[0]));
+
+  const DataVector tan_func = tan(func);
+  // Temporarily use component to avoid allocation
+  auto& tan_sum = get<0, 2>(result);
+  tan_sum = tan_func[0] * (source_coords[1] - center_[1]) +
+            tan_func[1] * (source_coords[2] - center_[2]);
+
+  get<0, 0>(result) += width_deriv[0] * tan_sum;
+  get<0, 1>(result) = width_deriv[1] * tan_sum + width * tan_func[0];
+  get<0, 2>(result) = width_deriv[2] * tan_sum + width * tan_func[1];
+
+  return result;
+}
+
+template <typename T>
+tnsr::Ij<tt::remove_cvref_wrap_t<T>, 3, Frame::NoFrame> Skew::inv_jacobian(
+    const std::array<T, 3>& source_coords, const double time,
+    const domain::FunctionsOfTimeMap& functions_of_time) const {
+  return determinant_and_inverse(
+             jacobian(source_coords, time, functions_of_time))
+      .second;
+}
+
+template <typename T>
+tt::remove_cvref_wrap_t<T> Skew::get_width(
+    const std::array<T, 3>& source_coords) const {
+  using ResultT = tt::remove_cvref_wrap_t<T>;
+  // Will be reused for result
+  ResultT lambda = dot(source_coords, source_coords) / outer_radius_squared_;
+
+  ResultT& result = lambda;
+
+  // Go point by point because we have to check roundoff
+  for (size_t i = 0; i < get_size(result); i++) {
+    if (equal_within_roundoff(get_element(lambda, i), 0.0)) {
+      get_element(result, i) = 1.0;
+    } else if (equal_within_roundoff(get_element(lambda, i), 1.0)) {
+      get_element(result, i) = 0.0;
+    } else if (get_element(lambda, i) > 0.0 and get_element(lambda, i) < 1.0) {
+      get_element(result, i) = 0.5 * (1.0 + cos(M_PI * get_element(lambda, i)));
+    } else {
+      using ::operator<<;
+      const std::vector<double> bad_point{
+          get_element(dereference_wrapper(source_coords[0]), i),
+          get_element(dereference_wrapper(source_coords[1]), i),
+          get_element(dereference_wrapper(source_coords[2]), i)};
+      ERROR("Skew map: Source coordinate "
+            << bad_point << " not in valid region. Lambda is "
+            << get_element(lambda, i) << ". Center is " << center_
+            << ". Outer radius is " << outer_radius_);
+    }
+  }
+
+  return result;
+}
+
+template <typename T>
+std::array<tt::remove_cvref_wrap_t<T>, 3> Skew::get_width_deriv(
+    const std::array<T, 3>& source_coords) const {
+  using ResultT = tt::remove_cvref_wrap_t<T>;
+
+  const ResultT lambda =
+      dot(source_coords, source_coords) / outer_radius_squared_;
+
+  std::array<ResultT, 3> grad_width{};
+  for (size_t i = 0; i < 3; i++) {
+    gsl::at(grad_width, i) = gsl::at(source_coords, i);
+  }
+  // Factors of two cancel from grad lambda and pi/2
+  grad_width *= -M_PI / outer_radius_squared_ * sin(M_PI * lambda);
+
+  for (size_t i = 0; i < get_size(lambda); i++) {
+    // sin(lambda * M_PI) is zero at both lambda=0 and lambda=1
+    if (equal_within_roundoff(get_element(lambda, i), 0.0) or
+        equal_within_roundoff(get_element(lambda, i), 1.0)) {
+      for (size_t j = 0; j < 3; j++) {
+        get_element(gsl::at(grad_width, j), i) = 0.0;
+      }
+    } else if (get_element(lambda, i) < 0.0 or get_element(lambda, i) > 1.0) {
+      using ::operator<<;
+      const std::vector<double> bad_point{
+          get_element(dereference_wrapper(source_coords[0]), i),
+          get_element(dereference_wrapper(source_coords[1]), i),
+          get_element(dereference_wrapper(source_coords[2]), i)};
+      ERROR("Skew map: Source coordinate "
+            << bad_point << " not in valid region. Lambda is "
+            << get_element(lambda, i) << ". Center is " << center_
+            << ". Outer radius is " << outer_radius_);
+    }
+  }
+
+  return grad_width;
+}
+
+template <typename T>
+std::array<tt::remove_cvref_wrap_t<T>, 3> Skew::map_and_velocity_helper(
+    const std::array<T, 3>& source_coords, const double time,
+    const FunctionsOfTimeMap& functions_of_time,
+    const bool return_velocity) const {
+  using ResultT = tt::remove_cvref_wrap_t<T>;
+
+  check_for_singular_map(source_coords, time, functions_of_time);
+
+  std::array<ResultT, 3> result{};
+  if (return_velocity) {
+    result = make_array<3>(
+        make_with_value<ResultT>(dereference_wrapper(source_coords[0]), 0.0));
+  } else {
+    for (size_t i = 0; i < 3; i++) {
+      gsl::at(result, i) = gsl::at(source_coords, i);
+    }
+  }
+
+  // Currently result is the source coords, but with the proper return type
+  const ResultT width = get_width(source_coords);
+
+  const auto& function_of_time = functions_of_time.at(f_of_t_name_);
+  const auto func_and_deriv = function_of_time->func_and_deriv(time);
+  ASSERT(func_and_deriv[0].size() == 2,
+         "Expected a function of time with size 2, not "
+             << func_and_deriv[0].size() << " in the Skew map.");
+
+  if (return_velocity) {
+    const auto& func = func_and_deriv[0];
+    const auto& deriv = func_and_deriv[1];
+
+    result[0] = width * (deriv[0] * (1.0 + square(tan(func[0]))) *
+                             (source_coords[1] - center_[1]) +
+                         deriv[1] * (1.0 + square(tan(func[1]))) *
+                             (source_coords[2] - center_[2]));
+  } else {
+    result[0] +=
+        width * (tan(func_and_deriv[0][0]) * (source_coords[1] - center_[1]) +
+                 tan(func_and_deriv[0][1]) * (source_coords[2] - center_[2]));
+  }
+
+  return result;
+}
+
+void Skew::pup(PUP::er& p) {
+  size_t version = 0;
+  p | version;
+  // Remember to increment the version number when making changes to this
+  // function. Retain support for unpacking data written by previous versions
+  // whenever possible. See `Domain` docs for details.
+  if (version >= 0) {
+    p | f_of_t_name_;
+    p | center_;
+    p | outer_radius_;
+  }
+
+  // No need to pup these because they are uniquely determined
+  if (p.isUnpacking()) {
+    outer_radius_squared_ = square(outer_radius_);
+    f_of_t_names_.clear();
+    f_of_t_names_.insert(f_of_t_name_);
+  }
+}
+
+template <typename T>
+void Skew::check_for_singular_map(
+    const std::array<T, 3>& source_coords, const double time,
+    const FunctionsOfTimeMap& functions_of_time) const {
+  (void)source_coords;
+  (void)time;
+  (void)functions_of_time;
+
+#ifdef SPECTRE_DEBUG
+  using ResultT = tt::remove_cvref_wrap_t<T>;
+  const ResultT lambda =
+      dot(source_coords, source_coords) / outer_radius_squared_;
+
+  const auto& function_of_time = functions_of_time.at(f_of_t_name_);
+  const auto func = function_of_time->func_and_deriv(time)[0];
+  ASSERT(func.size() == 2, "Expected a function of time with size 2, not "
+                               << func.size() << " in the Skew map.");
+  const ResultT tan_sum = tan(func[0]) * (source_coords[1] - center_[1]) +
+                          tan(func[1]) * (source_coords[2] - center_[2]);
+
+  const ResultT x_deriv_of_mapped_x_coord =
+      1.0 - tan_sum * M_PI * dereference_wrapper(source_coords[0]) /
+                outer_radius_squared_ * sin(M_PI * lambda);
+
+  for (size_t i = 0; i < get_size(lambda); i++) {
+    if (get_element(x_deriv_of_mapped_x_coord, i) < 0.0) {
+      using ::operator<<;
+      const std::vector<double> bad_point{
+          get_element(dereference_wrapper(source_coords[0]), i),
+          get_element(dereference_wrapper(source_coords[1]), i),
+          get_element(dereference_wrapper(source_coords[2]), i)};
+      ERROR("Skew map is singular. Found for point "
+            << bad_point << ". Important values are:\n Outer radius, R = "
+            << outer_radius_ << "\n |x|^2/R^2 = " << get_element(lambda, i)
+            << "\n tan(F_y)*y + tan(F_z)*z = " << tan_sum
+            << "\n F_y = " << func[0] << "\n F_z = " << func[1]
+            << "\n tan(F_y) = " << tan(func[0])
+            << "\n tan(F_z) = " << tan(func[1]) << "\n");
+    }
+  }
+#endif
+}
+
+bool operator==(const Skew& lhs, const Skew& rhs) {
+  return lhs.f_of_t_name_ == rhs.f_of_t_name_ and lhs.center_ == rhs.center_ and
+         lhs.outer_radius_ == rhs.outer_radius_;
+}
+
+bool operator!=(const Skew& lhs, const Skew& rhs) { return not(lhs == rhs); }
+
+#define DTYPE(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATE(_, data)                                                   \
+  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 3>                 \
+  Skew::operator()(const std::array<DTYPE(data), 3>& source_coords,            \
+                   double time,                                                \
+                   const domain::FunctionsOfTimeMap& functions_of_time) const; \
+  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 3>                 \
+  Skew::frame_velocity(                                                        \
+      const std::array<DTYPE(data), 3>& source_coords, const double time,      \
+      const domain::FunctionsOfTimeMap& functions_of_time) const;              \
+  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 3, Frame::NoFrame>   \
+  Skew::jacobian(const std::array<DTYPE(data), 3>& source_coords, double time, \
+                 const domain::FunctionsOfTimeMap& functions_of_time) const;   \
+  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 3, Frame::NoFrame>   \
+  Skew::inv_jacobian(                                                          \
+      const std::array<DTYPE(data), 3>& source_coords, double time,            \
+      const domain::FunctionsOfTimeMap& functions_of_time) const;              \
+  template tt::remove_cvref_wrap_t<DTYPE(data)> Skew::get_width(               \
+      const std::array<DTYPE(data), 3>& source_coords) const;                  \
+  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 3>                 \
+  Skew::get_width_deriv(const std::array<DTYPE(data), 3>& source_coords)       \
+      const;                                                                   \
+  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 3>                 \
+  Skew::map_and_velocity_helper(                                               \
+      const std::array<DTYPE(data), 3>& source_coords, double time,            \
+      const domain::FunctionsOfTimeMap& functions_of_time,                     \
+      bool return_velocity) const;                                             \
+  template void Skew::check_for_singular_map(                                  \
+      const std::array<DTYPE(data), 3>& source_coords, double time,            \
+      const domain::FunctionsOfTimeMap& functions_of_time) const;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (double, DataVector,
+                                      std::reference_wrapper<const double>,
+                                      std::reference_wrapper<const DataVector>))
+
+#undef DTYPE
+#undef INSTANTIATE
+}  // namespace domain::CoordinateMaps::TimeDependent
---
+++ tests/Unit/Domain/CoordinateMaps/TimeDependent/Test_Skew.cpp
@@ -0,0 +1,245 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <array>
+#include <cmath>
+#include <cstddef>
+#include <limits>
+#include <optional>
+#include <random>
+#include <string>
+#include <unordered_map>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/CoordinateMaps/TimeDependent/Skew.hpp"
+#include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
+#include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
+#include "Helpers/Domain/CoordinateMaps/TestMapHelpers.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/StdArrayHelpers.hpp"
+#include "Utilities/TypeTraits.hpp"
+
+namespace domain {
+namespace {
+constexpr size_t deriv_order = 2;
+using Polynomial = domain::FunctionsOfTime::PiecewisePolynomial<deriv_order>;
+using FoftPtr = std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>;
+
+template <typename Generator>
+void test(const gsl::not_null<Generator*> generator) {
+  const double initial_time = 0.5;
+  double t = initial_time + 0.05;
+  const double dt = 0.6;
+  const double expiration_time = 20.0;
+
+  const std::string function_of_time_name{"Skew"};
+
+  // NOLINTBEGIN
+  std::uniform_real_distribution<double> fot_dist{-0.01, 0.01};
+  std::uniform_real_distribution<double> outer_radius_dist{50.0, 150.0};
+  std::uniform_real_distribution<double> point_dist{-5.0, 5.0};
+  // NOLINTEND
+
+  std::unordered_map<std::string, FoftPtr> f_of_t_list{};
+  f_of_t_list[function_of_time_name] = std::make_unique<Polynomial>(
+      initial_time,
+      std::array{make_with_random_values<DataVector>(
+                     generator, make_not_null(&fot_dist), DataVector{2, 0.0}),
+                 make_with_random_values<DataVector>(
+                     generator, make_not_null(&fot_dist), DataVector{2, 0.0}),
+                 DataVector{2, 0.0}},
+      expiration_time);
+
+  const double outer_radius = outer_radius_dist(*generator);
+  const std::array<double, 3> center{
+      point_dist(*generator), point_dist(*generator), point_dist(*generator)};
+  CAPTURE(outer_radius);
+
+  const CoordinateMaps::TimeDependent::Skew skew_map{function_of_time_name,
+                                                     center, outer_radius};
+
+  CHECK(skew_map.function_of_time_names().contains(function_of_time_name));
+
+  // test serialized/deserialized map
+  const auto skew_map_deserialized = serialize_and_deserialize(skew_map);
+  CHECK(skew_map_deserialized.function_of_time_names().contains(
+      function_of_time_name));
+
+  while (t < expiration_time) {
+    CAPTURE(t);
+    const std::array<double, 3> point_xi{
+        point_dist(*generator), point_dist(*generator), point_dist(*generator)};
+
+    const std::array<DataVector, 3> dv_point_xi{
+        make_with_random_values<DataVector>(
+            generator, make_not_null(&point_dist), DataVector{10, 0.0}),
+        make_with_random_values<DataVector>(
+            generator, make_not_null(&point_dist), DataVector{10, 0.0}),
+        make_with_random_values<DataVector>(
+            generator, make_not_null(&point_dist), DataVector{10, 0.0})};
+
+    const auto run_checks = [&](const auto& points) {
+      CAPTURE(points);
+      test_jacobian(skew_map, points, t, f_of_t_list);
+      test_inv_jacobian(skew_map, points, t, f_of_t_list);
+      test_frame_velocity(skew_map, points, t, f_of_t_list);
+
+      test_jacobian(skew_map_deserialized, points, t, f_of_t_list);
+      test_inv_jacobian(skew_map_deserialized, points, t, f_of_t_list);
+      test_frame_velocity(skew_map_deserialized, points, t, f_of_t_list);
+    };
+
+    run_checks(point_xi);
+    test_coordinate_map_argument_types(skew_map, point_xi, t, f_of_t_list);
+    test_coordinate_map_argument_types(skew_map_deserialized, point_xi, t,
+                                       f_of_t_list);
+    test_inverse_map(skew_map, point_xi, t, f_of_t_list);
+    test_inverse_map(skew_map_deserialized, point_xi, t, f_of_t_list);
+    run_checks(dv_point_xi);
+
+    t += dt;
+  }
+
+  // Check inequivalence operator
+  CHECK_FALSE(skew_map != skew_map);
+  CHECK_FALSE(skew_map_deserialized != skew_map_deserialized);
+
+  // Check serialization
+  CHECK(skew_map == skew_map_deserialized);
+  CHECK_FALSE(skew_map != skew_map_deserialized);
+}
+
+void test_specific_points() {
+  const std::string function_of_time_name{"Skew"};
+  const double time = 0.0;
+  std::unordered_map<std::string, FoftPtr> f_of_t_list{};
+  // Use pi/4 so math is easy
+  f_of_t_list[function_of_time_name] = std::make_unique<Polynomial>(
+      time,
+      std::array{DataVector{2, M_PI_4}, DataVector{2, 0.0}, DataVector{2, 0.0}},
+      std::numeric_limits<double>::infinity());
+
+  // Make it unit so math is easy
+  const std::array<double, 3> center{0.0, -1.0 / sqrt(2.0), 1.0 / sqrt(2.0)};
+  const double outer_radius = 100.0;
+
+  const CoordinateMaps::TimeDependent::Skew skew_map{function_of_time_name,
+                                                     center, outer_radius};
+
+  std::array<double, 3> test_point{};
+  {
+    INFO("Center");
+    test_point = center;
+    CAPTURE(test_point);
+
+    const auto mapped_point = skew_map(test_point, time, f_of_t_list);
+    // Should be exact
+    CHECK(mapped_point == test_point);
+    const auto inverse_point =
+        skew_map.inverse(mapped_point, time, f_of_t_list);
+    CHECK(inverse_point.has_value());
+    CHECK_ITERABLE_APPROX(inverse_point.value(), test_point);
+
+    const auto jacobian = skew_map.jacobian(test_point, time, f_of_t_list);
+    auto expected_jacobian = identity<3>(0.0);
+    // Because the angles are pi/4
+    get<0, 1>(expected_jacobian) =
+        0.5 * (1.0 + cos(M_PI / square(outer_radius)));
+    get<0, 2>(expected_jacobian) = get<0, 1>(expected_jacobian);
+    CHECK_ITERABLE_APPROX(jacobian, expected_jacobian);
+  }
+  {
+    INFO("Half way along y-axis");
+    test_point = std::array{0.0, 0.5 * outer_radius, 0.0};
+    CAPTURE(test_point);
+
+    const auto mapped_point = skew_map(test_point, time, f_of_t_list);
+    const double falloff = 0.25 * (2.0 + sqrt(2.0));
+    const double tan_sum = test_point[1];
+    // Should be exact
+    CHECK_ITERABLE_APPROX(
+        mapped_point,
+        (std::array{falloff * tan_sum, test_point[1], test_point[2]}));
+    const auto inverse_point =
+        skew_map.inverse(mapped_point, time, f_of_t_list);
+    CHECK(inverse_point.has_value());
+    CHECK_ITERABLE_APPROX(inverse_point.value(), test_point);
+
+    const auto jacobian = skew_map.jacobian(test_point, time, f_of_t_list);
+    auto expected_jacobian = identity<3>(0.0);
+    get<0, 2>(expected_jacobian) = falloff;
+    get<0, 1>(expected_jacobian) =
+        -0.5 * M_PI * tan_sum / (sqrt(2.0) * outer_radius) +
+        get<0, 2>(expected_jacobian);
+    CHECK_ITERABLE_APPROX(jacobian, expected_jacobian);
+  }
+  {
+    INFO("Outer radius");
+    test_point = std::array{0.0, 0.0, outer_radius};
+    CAPTURE(test_point);
+
+    const auto mapped_point = skew_map(test_point, time, f_of_t_list);
+    CHECK_ITERABLE_APPROX(mapped_point, test_point);
+    const auto inverse_point =
+        skew_map.inverse(mapped_point, time, f_of_t_list);
+    CHECK(inverse_point.has_value());
+    CHECK_ITERABLE_APPROX(inverse_point.value(), test_point);
+    const auto jacobian = skew_map.jacobian(test_point, time, f_of_t_list);
+    CHECK_ITERABLE_APPROX(jacobian, identity<3>(0.0));
+  }
+  {
+    INFO("Outer radius plus eps");
+    const double eps = std::numeric_limits<double>::epsilon();
+    test_point = std::array{0.0, outer_radius + eps, 0.0};
+    CAPTURE(test_point);
+
+    const auto mapped_point = skew_map(test_point, time, f_of_t_list);
+    CHECK_ITERABLE_APPROX(mapped_point, test_point);
+    const auto inverse_point =
+        skew_map.inverse(mapped_point, time, f_of_t_list);
+    CHECK(inverse_point.has_value());
+    CHECK_ITERABLE_APPROX(inverse_point.value(), test_point);
+    const auto jacobian = skew_map.jacobian(test_point, time, f_of_t_list);
+    CHECK_ITERABLE_APPROX(jacobian, identity<3>(0.0));
+  }
+}
+
+void test_errors() {
+#ifdef SPECTRE_DEBUG
+  const std::string function_of_time_name{"Skew"};
+  const double time = 0.0;
+  std::unordered_map<std::string, FoftPtr> f_of_t_list{};
+  // Use values close to PI/2 to trigger the error
+  f_of_t_list[function_of_time_name] = std::make_unique<Polynomial>(
+      time,
+      std::array{DataVector{2, M_PI_2 - 1.e-3}, DataVector{2, 0.0},
+                 DataVector{2, 0.0}},
+      std::numeric_limits<double>::infinity());
+
+  const double outer_radius = 100.0;
+
+  const CoordinateMaps::TimeDependent::Skew skew_map{
+      function_of_time_name, std::array{0.0, 0.0, 0.0}, outer_radius};
+
+  const std::array<double, 3> point{50.0, 20.0, 30.0};
+
+  CHECK_THROWS_WITH((skew_map(point, time, f_of_t_list)),
+                    Catch::Matchers::ContainsSubstring("Skew map is singular"));
+#endif
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.Domain.CoordinateMaps.TimeDependent.Skew",
+                  "[Domain][Unit]") {
+  MAKE_GENERATOR(generator);
+  test(make_not_null(&generator));
+  test_specific_points();
+  test_errors();
+}
+}  // namespace domain
