93490950f58dc8372e1a92fe2316f61b7268658f
Kyle Nelli <knelli@caltech.edu>
2025-03-29 20:40:40 -0700

    Allow CCE ObserveFields to write volume data synchronously
    When running CCE on one core, avoids holding all the volume data in
    memory.

--- tests/Unit/Evolution/Systems/Cce/Events/Test_ObserveFields.cpp
+++ tests/Unit/Evolution/Systems/Cce/Events/Test_ObserveFields.cpp
@@ -14,12 +14,18 @@
 #include "Evolution/Systems/Cce/Tags.hpp"
 #include "Framework/ActionTesting.hpp"
 #include "Framework/TestHelpers.hpp"
+#include "IO/H5/AccessType.hpp"
+#include "IO/H5/Dat.hpp"
+#include "IO/H5/File.hpp"
 #include "IO/Observer/ObserverComponent.hpp"
+#include "IO/Observer/Tags.hpp"
 #include "NumericalAlgorithms/SpinWeightedSphericalHarmonics/SwshCollocation.hpp"
+#include "Parallel/NodeLock.hpp"
 #include "Parallel/Phase.hpp"
 #include "ParallelAlgorithms/EventsAndTriggers/Event.hpp"
 #include "Time/Tags/Time.hpp"
 #include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/FileSystem.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -64,14 +70,43 @@ struct MockWriteReductionDataRow {
   }
 };
 
+void check_h5_file(const std::string& filename_prefix) {
+  const h5::H5File<h5::AccessType::ReadOnly> h5_file{filename_prefix + ".h5"};
+  const size_t l_plus_one_squared = square(l_max + 1);
+  {
+    const auto& cce_subfile =
+        h5_file.get<h5::Dat>("/Cce/VolumeData/InertialRetardedTime.dat");
+    CHECK(cce_subfile.get_legend().size() == l_plus_one_squared + 1);
+    CHECK(cce_subfile.get_data()(0, 0) == time);
+    h5_file.close_current_object();
+  }
+  {
+    const auto& cce_subfile =
+        h5_file.get<h5::Dat>("/Cce/VolumeData/OneMinusY.dat");
+    CHECK(cce_subfile.get_legend().size() == num_radial_grid_points + 1);
+    CHECK(cce_subfile.get_data()(0, 0) == time);
+    h5_file.close_current_object();
+  }
+  {
+    const auto& cce_subfile =
+        h5_file.get<h5::Dat>("/Cce/VolumeData/J/CompactifiedRadius_0.dat");
+    CHECK(cce_subfile.get_legend().size() == 2 * l_plus_one_squared + 1);
+    CHECK(cce_subfile.get_data()(0, 0) == time);
+    h5_file.close_current_object();
+  }
+}
+
 template <typename Metavariables>
 struct MockObserverWriter {
   using metavariables = Metavariables;
   using chare_type = ActionTesting::MockNodeGroupChare;
   using array_index = int;
-  using phase_dependent_action_list = tmpl::list<Parallel::PhaseActions<
-      Parallel::Phase::Initialization,
-      tmpl::list<ActionTesting::InitializeDataBox<tmpl::list<>>>>>;
+  using phase_dependent_action_list = tmpl::list<
+      Parallel::PhaseActions<Parallel::Phase::Initialization,
+                             tmpl::list<ActionTesting::InitializeDataBox<
+                                 tmpl::list<observers::Tags::H5FileLock>>>>>;
+  using const_global_cache_tags =
+      tmpl::list<observers::Tags::ReductionFileName>;
   using component_being_mocked = observers::ObserverWriter<Metavariables>;
 
   using replace_these_threaded_actions =
@@ -99,20 +134,28 @@ struct Metavars {
       tmpl::list<MockObserverWriter<Metavars>, MockElement<Metavars>>;
 };
 
-void test() {
+void test(const bool write_synchronously) {
   using metavars = Metavars;
   using obs_writer = MockObserverWriter<metavars>;
   using element = MockElement<metavars>;
 
-  Cce::Events::ObserveFields fields{std::vector<std::string>{
+  const Cce::Events::ObserveFields fields{std::vector<std::string>{
       "InertialRetardedTime", "J", "Psi0", "Dy(H)", "OneMinusY"}};
-  Cce::Events::ObserveFields serialized_fields =
+  const Cce::Events::ObserveFields serialized_fields =
       serialize_and_deserialize(fields);
 
+  const std::string filename_prefix{"PineTree"};
+  if (file_system::check_if_file_exists(filename_prefix + ".h5")) {
+    file_system::rm(filename_prefix + ".h5", true);
+  }
+
   using MockRuntimeSystem = ActionTesting::MockRuntimeSystem<metavars>;
-  MockRuntimeSystem runner{{}};
+  MockRuntimeSystem runner{
+      {filename_prefix},
+      {},
+      std::vector<size_t>{write_synchronously ? 1_st : 2_st}};
   ActionTesting::emplace_nodegroup_component_and_initialize<obs_writer>(
-      make_not_null(&runner), {});
+      make_not_null(&runner), {Parallel::NodeLock{}});
   ActionTesting::emplace_singleton_component_and_initialize<element>(
       make_not_null(&runner), ActionTesting::NodeId{0},
       ActionTesting::LocalCoreId{0}, {});
@@ -160,13 +203,17 @@ void test() {
 
   serialized_fields(obs_box, cache, array_index, component, observation_value);
 
-  // 1 for InertialRetardedTime and OneMinusY and 2 for each other tag
-  const size_t expected_number_of_actions = 8;
-  CHECK(ActionTesting::number_of_queued_threaded_actions<obs_writer>(
-            runner, 0) == expected_number_of_actions);
-  for (size_t i = 0; i < expected_number_of_actions; i++) {
-    ActionTesting::invoke_queued_threaded_action<obs_writer>(
-        make_not_null(&runner), 0);
+  if (write_synchronously) {
+    check_h5_file(filename_prefix);
+  } else {
+    // 1 for InertialRetardedTime and OneMinusY and 2 for each other tag
+    const size_t expected_number_of_actions = 8;
+    CHECK(ActionTesting::number_of_queued_threaded_actions<obs_writer>(
+              runner, 0) == expected_number_of_actions);
+    for (size_t i = 0; i < expected_number_of_actions; i++) {
+      ActionTesting::invoke_queued_threaded_action<obs_writer>(
+          make_not_null(&runner), 0);
+    }
   }
 }
 
@@ -184,7 +231,8 @@ void test_errors() {
 
 SPECTRE_TEST_CASE("Unit.Evolution.Systems.Cce.Events.ObserveFields",
                   "[Unit][Evolution]") {
-  test();
+  test(true);
+  test(false);
   test_errors();
 }
 }  // namespace
