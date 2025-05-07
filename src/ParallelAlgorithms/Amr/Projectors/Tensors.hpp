0ce29b235ff84208882cdf35ca8c8017f16f077f
William Throwe <wtt6@cornell.edu>
2025-05-07 14:53:44 -0400

    Implement h-projection of Tensor

--- src/ParallelAlgorithms/Amr/Projectors/Tensors.hpp
+++ src/ParallelAlgorithms/Amr/Projectors/Tensors.hpp
@@ -9,6 +9,7 @@
 
 #include "DataStructures/ApplyMatrices.hpp"
 #include "DataStructures/Variables.hpp"
+#include "Domain/Structure/ChildSize.hpp"
 #include "Domain/Structure/Element.hpp"
 #include "Domain/Structure/ElementId.hpp"
 #include "Domain/Tags.hpp"
@@ -35,11 +36,12 @@ namespace amr::projectors {
 template <size_t Dim, typename... TensorTags>
 struct ProjectTensors : tt::ConformsTo<amr::protocols::Projector> {
   using return_tags = tmpl::list<TensorTags...>;
-  using argument_tags = tmpl::list<domain::Tags::Mesh<Dim>>;
+  using argument_tags =
+      tmpl::list<domain::Tags::Element<Dim>, domain::Tags::Mesh<Dim>>;
 
   static void apply(
       const gsl::not_null<typename TensorTags::type*>... tensors,
-      const Mesh<Dim>& new_mesh,
+      const Element<Dim>& /*element*/, const Mesh<Dim>& new_mesh,
       const std::pair<Mesh<Dim>, Element<Dim>>& old_mesh_and_element) {
     const auto& old_mesh = old_mesh_and_element.first;
     if (old_mesh == new_mesh) {
@@ -58,20 +60,65 @@ struct ProjectTensors : tt::ConformsTo<amr::protocols::Projector> {
   }
 
   template <typename... Tags>
-  static void apply(
-      const gsl::not_null<typename TensorTags::type*>... /*tensors*/,
-      const Mesh<Dim>& /*new_mesh*/,
-      const tuples::TaggedTuple<Tags...>& /*parent_items*/) {
-    ERROR("h-refinement not implemented yet");
+  static void apply(const gsl::not_null<typename TensorTags::type*>... tensors,
+                    const Element<Dim>& element, const Mesh<Dim>& new_mesh,
+                    const tuples::TaggedTuple<Tags...>& parent_items) {
+    const auto& element_id = element.id();
+    const auto& parent_id = get<domain::Tags::Element<Dim>>(parent_items).id();
+    const auto& parent_mesh = get<domain::Tags::Mesh<Dim>>(parent_items);
+    const auto child_sizes =
+        domain::child_size(element_id.segment_ids(), parent_id.segment_ids());
+    const auto projection_matrices =
+        Spectral::projection_matrix_parent_to_child(parent_mesh, new_mesh,
+                                                    child_sizes);
+    const auto project_tensor = [&](auto& new_tensor, const auto& old_tensor) {
+      for (size_t i = 0; i < new_tensor.size(); ++i) {
+        new_tensor[i] = apply_matrices(projection_matrices, old_tensor[i],
+                                       parent_mesh.extents());
+      }
+      return 0;
+    };
+    expand_pack(project_tensor(*tensors, get<TensorTags>(parent_items))...);
   }
 
   template <typename... Tags>
   static void apply(
-      const gsl::not_null<typename TensorTags::type*>... /*tensors*/,
-      const Mesh<Dim>& /*new_mesh*/,
+      const gsl::not_null<typename TensorTags::type*>... tensors,
+      const Element<Dim>& element, const Mesh<Dim>& new_mesh,
       const std::unordered_map<ElementId<Dim>, tuples::TaggedTuple<Tags...>>&
-      /*children_items*/) {
-    ERROR("h-refinement not implemented yet");
+          children_items) {
+    const auto& element_id = element.id();
+    bool first_child = true;
+    for (const auto& [child_id, child_items] : children_items) {
+      const auto& child_mesh = get<domain::Tags::Mesh<Dim>>(child_items);
+      const auto child_sizes =
+          domain::child_size(child_id.segment_ids(), element_id.segment_ids());
+      const auto projection_matrices =
+          Spectral::projection_matrix_child_to_parent(child_mesh, new_mesh,
+                                                      child_sizes);
+      if (first_child) {
+        const auto project_tensor = [&](auto& new_tensor,
+                                        const auto& old_tensor) {
+          for (size_t i = 0; i < new_tensor.size(); ++i) {
+            new_tensor[i] = apply_matrices(projection_matrices, old_tensor[i],
+                                           child_mesh.extents());
+          }
+          return 0;
+        };
+        expand_pack(project_tensor(*tensors, get<TensorTags>(child_items))...);
+        first_child = false;
+      } else {
+        const auto project_tensor = [&](auto& new_tensor,
+                                        const auto& old_tensor) {
+          for (size_t i = 0; i < new_tensor.size(); ++i) {
+            new_tensor[i] += apply_matrices(projection_matrices, old_tensor[i],
+                                            child_mesh.extents());
+          }
+          return 0;
+        };
+        expand_pack(project_tensor(*tensors, get<TensorTags>(child_items))...);
+      }
+    }
   }
 };
 
