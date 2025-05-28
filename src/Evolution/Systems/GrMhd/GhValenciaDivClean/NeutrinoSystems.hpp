c2f2814e9f7a6a38ecc7f3aad5388d73e36f4c3d
pajkosmi <mpajkos@caltech.edu>
2025-05-28 11:30:18 -0700

    M1Grey instantiations

--- src/Evolution/Systems/GrMhd/GhValenciaDivClean/NeutrinoSystems.hpp
+++ src/Evolution/Systems/GrMhd/GhValenciaDivClean/NeutrinoSystems.hpp
@@ -4,7 +4,16 @@
 #pragma once
 
 #include "Evolution/Particles/MonteCarlo/System.hpp"
+#include "Evolution/Systems/RadiationTransport/M1Grey/System.hpp"
 #include "Evolution/Systems/RadiationTransport/NoNeutrinos/System.hpp"
+#include "Evolution/Systems/RadiationTransport/Tags.hpp"
 
-#define GHMHD_NEUTRINOS \
-  (RadiationTransport::NoNeutrinos::System, Particles::MonteCarlo::System)
+using neutrino_species_elec = tmpl::list<neutrinos::ElectronNeutrinos<1>>;
+using neutrino_species_elec_anti =
+    tmpl::list<neutrinos::ElectronNeutrinos<1>,
+               neutrinos::ElectronAntiNeutrinos<1>>;
+
+#define GHMHD_NEUTRINOS                                                    \
+  (RadiationTransport::NoNeutrinos::System, Particles::MonteCarlo::System, \
+   RadiationTransport::M1Grey::System<neutrino_species_elec>,              \
+   RadiationTransport::M1Grey::System<neutrino_species_elec_anti>)
