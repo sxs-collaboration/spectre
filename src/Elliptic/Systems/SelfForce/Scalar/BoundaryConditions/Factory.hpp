95e5d352d493cfab5fd69cb3e578c3bda05957f1
Nils Vu <owls@nilsvu.de>
2025-06-20 11:14:16 -0700

    Add scalar self-force Sommerfeld boundary condition

--- src/Elliptic/Systems/SelfForce/Scalar/BoundaryConditions/Factory.hpp
+++ src/Elliptic/Systems/SelfForce/Scalar/BoundaryConditions/Factory.hpp
@@ -4,12 +4,13 @@
 #pragma once
 
 #include "Elliptic/Systems/SelfForce/Scalar/BoundaryConditions/Angular.hpp"
+#include "Elliptic/Systems/SelfForce/Scalar/BoundaryConditions/Sommerfeld.hpp"
 #include "Utilities/TMPL.hpp"
 
 /// Boundary conditions for the scalar self-force system.
 namespace ScalarSelfForce::BoundaryConditions {
 
 /// List of all boundary conditions for the scalar self-force system.
-using standard_boundary_conditions = tmpl::list<Angular>;
+using standard_boundary_conditions = tmpl::list<Angular, Sommerfeld>;
 
 }  // namespace ScalarSelfForce::BoundaryConditions
