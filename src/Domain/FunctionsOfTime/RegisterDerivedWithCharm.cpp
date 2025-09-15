21c7f75f2ac1ad9c696230cb6b1afa007cc9ea23
Clemens Dittmer <clemens_dittmer@hotmail.com>
2025-09-15 17:39:32 +0200

    Added the registration scope to include our new Quaternion Function of Time.

--- src/Domain/FunctionsOfTime/RegisterDerivedWithCharm.cpp
+++ src/Domain/FunctionsOfTime/RegisterDerivedWithCharm.cpp
@@ -12,6 +12,7 @@
 #include "Domain/FunctionsOfTime/IntegratedFunctionOfTime.hpp"
 #include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
 #include "Domain/FunctionsOfTime/QuaternionFunctionOfTime.hpp"
+#include "Domain/FunctionsOfTime/QuaternionWorldtubeFunctionOfTime.hpp"
 #include "Domain/FunctionsOfTime/SettleToConstant.hpp"
 #include "Domain/FunctionsOfTime/SettleToConstantQuaternion.hpp"
 #include "Utilities/PrettyType.hpp"
@@ -20,16 +21,18 @@
 
 namespace domain::FunctionsOfTime {
 void register_derived_with_charm() {
-  register_classes_with_charm<FunctionsOfTime::FixedSpeedCubic,
-                              FunctionsOfTime::IntegratedFunctionOfTime,
-                              FunctionsOfTime::PiecewisePolynomial<0>,
-                              FunctionsOfTime::PiecewisePolynomial<1>,
-                              FunctionsOfTime::PiecewisePolynomial<2>,
-                              FunctionsOfTime::PiecewisePolynomial<3>,
-                              FunctionsOfTime::PiecewisePolynomial<4>,
-                              FunctionsOfTime::QuaternionFunctionOfTime<2>,
-                              FunctionsOfTime::QuaternionFunctionOfTime<3>,
-                              FunctionsOfTime::SettleToConstant,
-                              FunctionsOfTime::SettleToConstantQuaternion>();
+  register_classes_with_charm<
+      FunctionsOfTime::FixedSpeedCubic,
+      FunctionsOfTime::IntegratedFunctionOfTime,
+      FunctionsOfTime::PiecewisePolynomial<0>,
+      FunctionsOfTime::PiecewisePolynomial<1>,
+      FunctionsOfTime::PiecewisePolynomial<2>,
+      FunctionsOfTime::PiecewisePolynomial<3>,
+      FunctionsOfTime::PiecewisePolynomial<4>,
+      FunctionsOfTime::QuaternionFunctionOfTime<2>,
+      FunctionsOfTime::QuaternionFunctionOfTime<3>,
+      FunctionsOfTime::QuaternionWorldtubeFunctionOfTime<1>,
+      FunctionsOfTime::SettleToConstant,
+      FunctionsOfTime::SettleToConstantQuaternion>();
 }
 }  // namespace domain::FunctionsOfTime
