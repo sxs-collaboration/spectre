b53a442ae787e87ffa18605cc91d5f0e42748a9a
Alex Carpenter <alexanderhcarpenter46@gmail.com>
2025-10-09 11:48:51 -0700

    fix docker Test_GridCenters.py

--- tests/Unit/Domain/Creators/TimeDependentOptions/Python/Test_GridCenters.py
+++ tests/Unit/Domain/Creators/TimeDependentOptions/Python/Test_GridCenters.py
@@ -1,18 +1,21 @@
 # Distributed under the MIT License.
 # See LICENSE.txt for details.
 
+import os
 import unittest
 
 from spectre.Domain.Creators.TimeDependentOptions import GridCentersOptions
-from spectre.Informer import unit_test_build_path
+from spectre.Informer import unit_test_src_path
 
 
 class TestGridCenters(unittest.TestCase):
     def test_construction(self):
         grid_centers = GridCentersOptions(
-            str(unit_test_build_path())
-            + "../../../tests/InputFiles/GrMhd/GhValenciaDivClean/"
-            + "EvolutionParameters.perl",
+            os.path.join(
+                str(unit_test_src_path()),
+                "../InputFiles/GrMhd/GhValenciaDivClean/",
+                "EvolutionParameters.perl",
+            ),
             2.0,
         )
         self.assertNotEqual(grid_centers, None)
