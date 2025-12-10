315b123bf9a0a19e33488129d78a9eda714eacd4
Michael Murphy <mdm373@cornell.edu>
2025-12-10 16:12:27 -0500

    Add Zernike GaussRadau weights to `lift_flux`

--- tests/Unit/NumericalAlgorithms/DiscontinuousGalerkin/Test_LiftFlux.cpp
+++ tests/Unit/NumericalAlgorithms/DiscontinuousGalerkin/Test_LiftFlux.cpp
@@ -20,6 +20,7 @@
 #include "Domain/CoordinateMaps/ProductMaps.tpp"
 #include "Domain/FaceNormal.hpp"
 #include "Domain/Structure/Direction.hpp"
+#include "Framework/TestHelpers.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/LiftFlux.hpp"
 #include "NumericalAlgorithms/Spectral/Basis.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
@@ -37,32 +38,86 @@ struct Var : db::SimpleTag {
 
 SPECTRE_TEST_CASE("Unit.DiscontinuousGalerkin.LiftFlux",
                   "[Unit][NumericalAlgorithms]") {
-  const Mesh<2> mesh{
-      {{3, 5}}, Spectral::Basis::Legendre, Spectral::Quadrature::GaussLobatto};
+  {
+    INFO("Testing with I1");
+    for (const auto& basis :
+         {Spectral::Basis::Legendre, Spectral::Basis::Chebyshev}) {
+      CAPTURE(basis);
 
-  using Affine = domain::CoordinateMaps::Affine;
-  const Affine xi_map(-1., 1., -5., 7.);
-  const Affine eta_map(-1., 1., 2., 5.);
-  const auto coordinate_map =
-      domain::make_coordinate_map<Frame::ElementLogical, Frame::Grid>(
-          domain::CoordinateMaps::ProductOf2Maps<Affine, Affine>(xi_map,
-                                                                 eta_map));
-  const double element_length = (eta_map(std::array<double, 1>{{1.}}) -
-                                 eta_map(std::array<double, 1>{{-1.}}))[0];
+      const Mesh<2> mesh{{{3, 5}}, basis, Spectral::Quadrature::GaussLobatto};
 
-  const double weight = Spectral::quadrature_weights(mesh.slice_through(1))[0];
-  const auto boundary_mesh = mesh.slice_through(0);
+      using Affine = domain::CoordinateMaps::Affine;
+      const Affine xi_map(-1., 1., -5., 7.);
+      const Affine eta_map(-1., 1., 2., 5.);
+      const auto coordinate_map =
+          domain::make_coordinate_map<Frame::ElementLogical, Frame::Grid>(
+              domain::CoordinateMaps::ProductOf2Maps<Affine, Affine>(xi_map,
+                                                                     eta_map));
+      const double element_length = (eta_map(std::array<double, 1>{{1.}}) -
+                                     eta_map(std::array<double, 1>{{-1.}}))[0];
 
-  const auto magnitude_of_face_normal = magnitude(unnormalized_face_normal(
-      boundary_mesh, coordinate_map, Direction<2>::lower_eta()));
+      const double weight =
+          Spectral::quadrature_weights(mesh.slice_through(1))[0];
+      const auto boundary_mesh = mesh.slice_through(0);
 
-  Variables<tmpl::list<Tags::NormalDotNumericalFlux<Var>>> flux(
-      boundary_mesh.number_of_grid_points());
-  get(get<Tags::NormalDotNumericalFlux<Var>>(flux)) = DataVector({2., 3., 5.});
+      const auto magnitude_of_face_normal = magnitude(unnormalized_face_normal(
+          boundary_mesh, coordinate_map, Direction<2>::lower_eta()));
 
-  const Variables<tmpl::list<Var>> expected =
-      -2. / (element_length * weight) * flux;
+      Variables<tmpl::list<Tags::NormalDotNumericalFlux<Var>>> flux(
+          boundary_mesh.number_of_grid_points());
+      get(get<Tags::NormalDotNumericalFlux<Var>>(flux)) =
+          DataVector({2., 3., 5.});
 
-  CHECK(dg::lift_flux(flux, mesh.extents(1), magnitude_of_face_normal) ==
-        expected);
+      const Variables<tmpl::list<Var>> expected =
+          -2. / (element_length * weight) * flux;
+
+      CAPTURE(dg::lift_flux(flux, mesh.extents(1), magnitude_of_face_normal,
+                            mesh.basis(1)));
+      CAPTURE(expected);
+      CHECK_VARIABLES_APPROX(
+          dg::lift_flux(flux, mesh.extents(1), magnitude_of_face_normal, basis),
+          expected);
+    }
+  }
+  {
+    INFO("Testing with Zernike");
+    for (const auto& basis :
+         {Spectral::Basis::ZernikeB1, Spectral::Basis::ZernikeB2,
+          Spectral::Basis::ZernikeB3}) {
+      CAPTURE(basis);
+      const Mesh<2> mesh{{3, 5},
+                         {basis, Spectral::Basis::Legendre},
+                         {Spectral::Quadrature::GaussRadauUpper,
+                          Spectral::Quadrature::GaussLobatto}};
+
+      using Affine = domain::CoordinateMaps::Affine;
+      const Affine xi_map(-1., 1., 0, 7.);
+      const Affine eta_map(-1., 1., 2., 5.);
+      const auto coordinate_map =
+          domain::make_coordinate_map<Frame::ElementLogical, Frame::Grid>(
+              domain::CoordinateMaps::ProductOf2Maps<Affine, Affine>(xi_map,
+                                                                     eta_map));
+      const double element_length = (xi_map(std::array<double, 1>{{1.}}) -
+                                     xi_map(std::array<double, 1>{{-1.}}))[0];
+
+      const double weight =
+          Spectral::quadrature_weights(mesh.slice_through(0))[2];
+      const auto boundary_mesh = mesh.slice_through(1);
+
+      const auto magnitude_of_face_normal = magnitude(unnormalized_face_normal(
+          boundary_mesh, coordinate_map, Direction<2>::upper_xi()));
+
+      Variables<tmpl::list<Tags::NormalDotNumericalFlux<Var>>> flux(
+          boundary_mesh.number_of_grid_points());
+      get(get<Tags::NormalDotNumericalFlux<Var>>(flux)) =
+          DataVector({2., 3., 5., 8., 6.});
+
+      const Variables<tmpl::list<Var>> expected =
+          -2. / (element_length * weight) * flux;
+
+      CHECK_VARIABLES_APPROX(
+          dg::lift_flux(flux, mesh.extents(0), magnitude_of_face_normal, basis),
+          expected);
+    }
+  }
 }
