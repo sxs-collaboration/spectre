70d1fc446c06f15a8f538f7564ccf2dcc1487f6d
Nils Vu <owls@nilsvu.de>
2025-12-23 17:45:18 -0800

    RayTracer: add numeric background spacetime

---
+++ src/ParallelAlgorithms/RayTracer/BackgroundSpacetimes/NumericData.cpp
@@ -0,0 +1,104 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "ParallelAlgorithms/RayTracer/BackgroundSpacetimes/NumericData.hpp"
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "IO/Exporter/PointwiseInterpolator.hpp"
+#include "IO/Exporter/SpacetimeInterpolator.hpp"
+#include "Parallel/Printf/Printf.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace ray_tracing {
+
+NumericData::NumericData(std::string file_glob, std::string subfile_name,
+                         std::optional<int> observation_step,
+                         ::Verbosity verbosity)
+    : file_glob_(std::move(file_glob)),
+      subfile_name_(std::move(subfile_name)),
+      observation_step_(observation_step),
+      verbosity_(verbosity) {
+  if (not observation_step_.has_value()) {
+    // Construct the spacetime interpolator but don't load any data yet
+    interpolator_ = spectre::Exporter::SpacetimeInterpolator<Dim, Frame>{
+        file_glob_, subfile_name_,
+        spectre::Exporter::get_tensor_components<tags>()};
+  }
+}
+
+NumericData::NumericData(const NumericData& rhs)
+    : NumericData(rhs.file_glob_, rhs.subfile_name_, rhs.observation_step_,
+                  rhs.verbosity_) {}
+
+NumericData& NumericData::operator=(const NumericData& rhs) {
+  if (this != &rhs) {
+    *this = NumericData(rhs);
+  }
+  return *this;
+}
+
+void NumericData::initialize(std::array<double, 2> new_time_bounds) {
+  if (verbosity_ >= ::Verbosity::Verbose) {
+    Parallel::printf("Loading numeric data...\n");
+  }
+  if (observation_step_.has_value()) {
+    interpolator_ = PointwiseInterpolator{
+        file_glob_, subfile_name_,
+        spectre::Exporter::ObservationStep{observation_step_.value()},
+        spectre::Exporter::get_tensor_components<tags>()};
+  } else {
+    std::get<SpacetimeInterpolator>(interpolator_)
+        .load_time_bounds(new_time_bounds);
+  }
+  if (verbosity_ >= ::Verbosity::Verbose) {
+    Parallel::printf("Numeric data loaded.\n");
+  }
+}
+
+std::array<double, 2> NumericData::time_bounds() const {
+  if (std::holds_alternative<SpacetimeInterpolator>(interpolator_)) {
+    return std::get<SpacetimeInterpolator>(interpolator_).time_bounds();
+  } else {
+    return {{-std::numeric_limits<double>::infinity(),
+             std::numeric_limits<double>::infinity()}};
+  }
+}
+
+tuples::tagged_tuple_from_typelist<typename NumericData::tags>
+NumericData::variables(const tnsr::I<DataType, Dim, Frame>& x, const double t,
+                       const std::optional<gsl::not_null<std::vector<size_t>*>>
+                           block_order) const {
+  std::vector<double> result{};
+  if (std::holds_alternative<PointwiseInterpolator>(interpolator_)) {
+    std::get<PointwiseInterpolator>(interpolator_)
+        .interpolate_to_point(make_not_null(&result), x, block_order);
+  } else {
+    std::get<SpacetimeInterpolator>(interpolator_)
+        .interpolate_to_point(make_not_null(&result), x, t, block_order);
+  }
+  return spectre::Exporter::make_tagged_tuple<tags>(std::move(result));
+}
+
+void NumericData::pup(PUP::er& p) {
+  BackgroundSpacetime::pup(p);
+  p | file_glob_;
+  p | subfile_name_;
+  p | observation_step_;
+  p | verbosity_;
+  // Don't copy interpolator, it must be reinitialized
+}
+
+bool operator==(const NumericData& lhs, const NumericData& rhs) {
+  return lhs.file_glob_ == rhs.file_glob_ and
+         lhs.subfile_name_ == rhs.subfile_name_ and
+         lhs.observation_step_ == rhs.observation_step_ and
+         lhs.verbosity_ == rhs.verbosity_;
+}
+
+bool operator!=(const NumericData& lhs, const NumericData& rhs) {
+  return not(lhs == rhs);
+}
+
+PUP::able::PUP_ID NumericData::my_PUP_ID = 0;  // NOLINT
+
+}  // namespace ray_tracing
---
+++ src/ParallelAlgorithms/RayTracer/BackgroundSpacetimes/NumericData.hpp
@@ -0,0 +1,116 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include <pup.h>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "IO/Exporter/PointwiseInterpolator.hpp"
+#include "IO/Exporter/SpacetimeInterpolator.hpp"
+#include "IO/Logging/Verbosity.hpp"
+#include "Options/Auto.hpp"
+#include "Options/String.hpp"
+#include "ParallelAlgorithms/RayTracer/BackgroundSpacetimes/BackgroundSpacetime.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/Serialization/CharmPupable.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace ray_tracing {
+
+/// Numeric data from volume data files.
+class NumericData : public BackgroundSpacetime {
+ public:
+  NumericData() = default;
+  NumericData(const NumericData& /*rhs*/);
+  NumericData& operator=(const NumericData& /*rhs*/);
+  NumericData(NumericData&& /*rhs*/) = default;
+  NumericData& operator=(NumericData&& /*rhs*/) = default;
+  ~NumericData() override = default;
+
+  static constexpr Options::String help = "Numeric data from volume data files";
+
+  struct FileGlob {
+    using type = std::string;
+    static constexpr Options::String help =
+        "Volume data files. Can be a glob pattern.";
+  };
+
+  struct SubfileName {
+    using type = std::string;
+    static constexpr Options::String help = "Subfile name in the volume files";
+  };
+
+  struct ObservationStep {
+    using type = Options::Auto<int>;
+    static constexpr Options::String help =
+        "Either a single observation step to load, or 'Auto' to load the full "
+        "spacetime. "
+        "When a single observation is loaded, then rays are traced "
+        "only through that single time slice (fast light approximation). "
+        "When 'Auto' is selected, then all time slices are loaded that cover "
+        "the required time range and the data is interpolated in both space "
+        "and time (slow light).";
+  };
+
+  struct Verbosity {
+    using type = ::Verbosity;
+    static constexpr Options::String help = "Verbosity of output.";
+  };
+
+  using options = tmpl::list<FileGlob, SubfileName, ObservationStep, Verbosity>;
+
+  NumericData(std::string file_glob, std::string subfile_name,
+              std::optional<int> observation_step,
+              ::Verbosity verbosity = ::Verbosity::Silent);
+
+  auto get_clone() const -> std::unique_ptr<BackgroundSpacetime> override {
+    return std::make_unique<NumericData>(*this);
+  }
+
+  /// \cond
+  explicit NumericData(CkMigrateMessage* msg) : BackgroundSpacetime(msg) {}
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(NumericData);
+  /// \endcond
+
+  void initialize(std::array<double, 2> new_time_bounds) override;
+
+  std::array<double, 2> time_bounds() const override;
+
+  tuples::tagged_tuple_from_typelist<tags> variables(
+      const tnsr::I<DataType, Dim, Frame>& x, double t,
+      std::optional<gsl::not_null<std::vector<size_t>*>> block_order =
+          std::nullopt) const override;
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& p) override;
+
+  friend bool operator==(const NumericData& lhs, const NumericData& rhs);
+
+ private:
+  using PointwiseInterpolator =
+      spectre::Exporter::PointwiseInterpolator<Dim, Frame>;
+  using SpacetimeInterpolator =
+      spectre::Exporter::SpacetimeInterpolator<Dim, Frame>;
+
+  std::string file_glob_;
+  std::string subfile_name_;
+  std::optional<int> observation_step_;
+  ::Verbosity verbosity_ = ::Verbosity::Silent;
+  // Cache that holds tensor data in memory. This isn't copied and must be
+  // reinitialized after a copy.
+  std::variant<PointwiseInterpolator, SpacetimeInterpolator> interpolator_;
+};
+
+bool operator!=(const NumericData& lhs, const NumericData& rhs);
+
+}  // namespace ray_tracing
