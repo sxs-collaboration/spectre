37786c032a4901f82b96973cd42b959084de73e1
Michael Murphy <mdm373@cornell.edu>
2026-02-05 17:34:43 -0500

    Add NtoM for two-index Zernike basis

--- src/NumericalAlgorithms/Spectral/NodalToModalMatrix.hpp
+++ src/NumericalAlgorithms/Spectral/NodalToModalMatrix.hpp
@@ -39,10 +39,34 @@ template <Basis BasisType, Quadrature QuadratureType>
 const Matrix& nodal_to_modal_matrix(size_t num_points);
 
 /*!
+ * \brief %Matrix used to transform from the nodal coefficients of a function to
+ * its spectral coefficients (modes). This two-index version is used for
+ * two-dimensional basis function (i.e. Zernike with GaussRadauUpper
+ * quadrature).
+ *
+ * For Zernike, \f$m\f$ is the angular index and \f$N\f$ is the
+ * maximum supported spectral index (usually taken to be the maximum value,
+ * \f$2 \, \texttt{num_points}-2\f$). Note that the size of a spectral space
+ * vector is \f$(N - m) / 2 + 1\f$, using integer division.
+ *
+ * \see nodal_to_modal_matrix(size_t)
+ */
+template <Basis BasisType, Quadrature QuadratureType>
+const Matrix& nodal_to_modal_matrix(size_t num_points, size_t m, size_t N);
+
+/*!
  * \brief Transformation matrix from nodal to modal coefficients for a
  * one-dimensional mesh.
  *
  * \see nodal_to_modal_matrix(size_t)
  */
 const Matrix& nodal_to_modal_matrix(const Mesh<1>& mesh);
+
+/*!
+ * \brief Transformation matrix from nodal to modal coefficients for a
+ * one-dimensional mesh with a Zernike basis.
+ *
+ * \see nodal_to_modal_matrix(size_t, size_t, size_t)
+ */
+const Matrix& nodal_to_modal_matrix(const Mesh<1>& mesh, size_t m, size_t N);
 }  // namespace Spectral
--- tests/Unit/NumericalAlgorithms/Spectral/Test_NodalToModalMatrix.cpp
+++ tests/Unit/NumericalAlgorithms/Spectral/Test_NodalToModalMatrix.cpp
@@ -37,8 +37,46 @@ void test() {
     }
   }
 }
+
+template <Basis basis, Quadrature quadrature>
+void test_two_indexed() {
+  static_assert(basis == Basis::ZernikeB1 or basis == Basis::ZernikeB2 or
+                basis == Basis::ZernikeB3);
+  CAPTURE(basis);
+  CAPTURE(quadrature);
+  const Approx custom_approx = Approx::custom().epsilon(1.0e-13).scale(1.0);
+  for (size_t n = minimum_number_of_points<basis, quadrature>;
+       n <= maximum_number_of_points<basis>; ++n) {
+    CAPTURE(n);
+    const DataVector xi = collocation_points<basis, quadrature>(n);
+    for (size_t N = 0; N < 2 * n - 1; ++N) {
+      CAPTURE(N);
+      for (size_t m = 0; m <= N; ++m) {
+        CAPTURE(m);
+        const Matrix nodal_to_modal =
+            nodal_to_modal_matrix<basis, quadrature>(n, m, N);
+        // The spectal space is only odd or even modes, based on m parity
+        // Note the integer division
+        const size_t spectral_size = (N - m) / 2 + 1;
+        for (size_t k = m; k <= N; k += 2) {
+          CAPTURE(k);
+          const auto f = compute_basis_function_value<basis>(k, m, xi);
+          const auto f_k = apply_matrix(nodal_to_modal, f);
+          DataVector f_k_expected{spectral_size, 0.0};
+          // Index in this compressed space (of specific parity) must be
+          // mapped
+          const size_t index = (k - m) / 2;
+          f_k_expected[index] = 1.0;
+          CHECK_ITERABLE_CUSTOM_APPROX(f_k, f_k_expected, custom_approx);
+        }
+      }
+    }
+  }
+}
+
 }  // namespace
 
+// [[TimeOut, 30]]
 SPECTRE_TEST_CASE("Unit.Numerical.Spectral.NodalToModalMatrix",
                   "[NumericalAlgorithms][Spectral][Unit]") {
   test<Basis::Legendre, Quadrature::Gauss>();
@@ -46,5 +84,8 @@ SPECTRE_TEST_CASE("Unit.Numerical.Spectral.NodalToModalMatrix",
   test<Basis::Chebyshev, Quadrature::Gauss>();
   test<Basis::Chebyshev, Quadrature::GaussLobatto>();
   test<Basis::Fourier, Quadrature::Equiangular>();
+  test_two_indexed<Basis::ZernikeB1, Quadrature::GaussRadauUpper>();
+  test_two_indexed<Basis::ZernikeB2, Quadrature::GaussRadauUpper>();
+  test_two_indexed<Basis::ZernikeB3, Quadrature::GaussRadauUpper>();
 }
 }  // namespace Spectral
