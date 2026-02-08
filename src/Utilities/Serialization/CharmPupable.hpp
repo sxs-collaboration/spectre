c4b7a0ea0949b1143c76905589bff74c7e23e1f4
Nils Deppe <nils@nilsdeppe.com>
2026-02-08 09:34:20 -0800

    Define CharmPupable macros for use with findus

--- src/Utilities/Serialization/CharmPupable.hpp
+++ src/Utilities/Serialization/CharmPupable.hpp
@@ -12,6 +12,7 @@
 #pragma GCC system_header
 #endif
 
+#if defined(SPECTRE_USE_CHARM)
 /*!
  * \ingroup ParallelGroup
  * \brief Mark derived classes as serializable
@@ -46,8 +47,60 @@
 /// Charm++
 #define SPECTRE_CHARM_DERIVED(derivedName, baseName) baseName
 
+/// Used to inherit from the correct base classes when building with findus when
+/// the base class for serialization is several up in the hierarchy.
+///
+/// Empty in Charm++ mode.
+#define SPECTRE_FINDUS_DERIVED(derivedName, baseName)
+
 /// \brief Only used for multi-inheritance auto-registration of derived classes
 /// for serialization by findus. Empty in Charm++ mode.
 ///
 /// \note findus supplies this macro directly.
 #define FINDUS_OVERRIDE_SERIALIZATION_ID(Derived, Base)
+
+/// \brief Expands to `virtual` if findus is used. Empty for Charm++.
+#define SPECTRE_FINDUS_VIRTUAL()
+
+/// \brief Expands to `override` if Charm++ is used. Empty for findus.
+#define SPECTRE_CHARM_OVERRIDE() override
+
+#elif defined(SPECTRE_USE_FINDUS)
+
+/// \brief Used for the base class to inherit from.
+#define SPECTRE_CHARM_PUPable(className) \
+  virtual findus::serialize::SerializableBase<className>
+
+/// Used to inherit from the correct base classes when building with Charm++
+#define SPECTRE_CHARM_DERIVED(derivedName, baseName)                           \
+  baseName, public virtual findus::serialize::SerializableDerived<derivedName, \
+                                                                  baseName>
+
+/// Used to inherit from the correct base classes when building with findus when
+/// the base class for serialization is several up in the hierarchy.
+///
+/// Empty in Charm++ mode.
+#define SPECTRE_FINDUS_DERIVED(derivedName, baseName) \
+  , public virtual findus::serialize::SerializableDerived<derivedName, baseName>
+
+/// \brief Interface with Charm++. Expands to empty.
+#define WRAPPED_PUPable_decl_template(className) \
+  static void unused_function_to_silence_compiler_warnings_for_charm_interop()
+
+/// \brief Interface with Charm++. Expands to empty.
+#define WRAPPED_PUPable_decl_base_template(baseClassName, className) \
+  static void unused_function_to_silence_compiler_warnings_for_charm_interop()
+
+/// \brief Interface with Charm++. Adds a virtual `pup` function to the base
+/// class.
+#define WRAPPED_PUPable_abstract(className) \
+  virtual void pup(PUP::er& /*p*/) {}       \
+  static void                               \
+  unused_function_to_silence_compiler_warnings_for_charm_interop_abstract()
+
+/// \brief Expands to `virtual` if findus is used. Empty for Charm++.
+#define SPECTRE_FINDUS_VIRTUAL() virtual
+
+/// \brief Expands to `override` if Charm++ is used. Empty for findus.
+#define SPECTRE_CHARM_OVERRIDE()
+#endif
