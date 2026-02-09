a1c12af88558850f9bd67d0073e2522b70c6d3fe
Nils Deppe <nils@nilsdeppe.com>
2026-02-09 14:04:11 -0800

    Add findus support to Serialize.hpp

--- src/Utilities/Serialization/Serialize.hpp
+++ src/Utilities/Serialization/Serialize.hpp
@@ -30,28 +30,24 @@ std::vector<char> serialize(const T& obj) {
   // clang-tidy: do not use const_cast
   auto& mut_obj = const_cast<T&>(typed_obj);  // NOLINT
 
+#if defined(SPECTRE_USE_CHARM)
   PUP::sizer sizer;
+#elif defined(SPECTRE_USE_FINDUS)
+  findus::serialize::Serializer sizer{findus::serialize::Serializer::Sizing};
+#endif  // SPECTRE_USE_CHARM
   sizer | mut_obj;
+#if defined(SPECTRE_USE_CHARM)
   std::vector<char> data(sizer.size());
   PUP::toMem writer(data.data());
+#elif defined(SPECTRE_USE_FINDUS)
+  std::vector<char> data(sizer.number_of_bytes());
+  findus::serialize::Serializer writer{
+      findus::serialize::Serializer::Packing,
+      reinterpret_cast<std::byte*>(data.data()), sizer.number_of_bytes()};
+#endif  // SPECTRE_USE_CHARM
   writer | mut_obj;
 
   return data;
-}
-
-/*!
- * \ingroup ParallelGroup
- * \brief Deserialize an object using PUP.
- *
- * \tparam T the type to deserialize to
- */
-template <typename T>
-T deserialize(const void* const data) {  // NOLINT
-  // clang-tidy: no const in forward decl (this is a definition)
-  PUP::fromMem reader(data);
-  T result{};
-  reader | result;
-  return result;
 }
 
 /*!
@@ -64,8 +60,31 @@ template <typename T>
 void deserialize(const gsl::not_null<T*> result,
                  const void* const data) {  // NOLINT
   // clang-tidy: no const in forward decl (this is a definition)
+#if defined(SPECTRE_USE_CHARM)
   PUP::fromMem reader(data);
+#elif defined(SPECTRE_USE_FINDUS)
+  // Note: because Charm++ didn't bounds check we can't do so here right now. We
+  // would need to pass in the number of points that data points to. That would
+  // be a good future improvement.
+  findus::serialize::Serializer reader{
+      findus::serialize::Serializer::Unpacking,
+      reinterpret_cast<const std::byte* const>(data), 0};
+#endif  // SPECTRE_USE_CHARM
   reader | *result;
+}
+
+/*!
+ * \ingroup ParallelGroup
+ * \brief Deserialize an object using PUP.
+ *
+ * \tparam T the type to deserialize to
+ */
+template <typename T>
+T deserialize(const void* const data) {  // NOLINT
+  // clang-tidy: no const in forward decl (this is a definition)
+  T result{};
+  deserialize(make_not_null(&result), data);
+  return result;
 }
 
 /*!
@@ -74,7 +93,13 @@ void deserialize(const gsl::not_null<T*> result,
  */
 template <typename T>
 size_t size_of_object_in_bytes(const T& obj) {
+#if defined(SPECTRE_USE_CHARM)
   PUP::sizer sizer;
   sizer | const_cast<T&>(obj);  // NOLINT
   return sizer.size();
+#elif defined(SPECTRE_USE_FINDUS)
+  findus::serialize::Serializer sizer{findus::serialize::Serializer::Sizing};
+  sizer | const_cast<T&>(obj);  // NOLINT
+  return sizer.number_of_bytes();
+#endif  // SPECTRE_USE_CHARM
 }
