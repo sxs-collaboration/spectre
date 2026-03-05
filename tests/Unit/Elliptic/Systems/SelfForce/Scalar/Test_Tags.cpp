d674a85e1ce18d43dcd49aaf18785474f6a6afe9
Nils Vu <owls@nilsvu.de>
2026-03-04 16:23:02 -0800

    Clang-format a file

--- tests/Unit/Elliptic/Systems/SelfForce/Scalar/Test_Tags.cpp
+++ tests/Unit/Elliptic/Systems/SelfForce/Scalar/Test_Tags.cpp
@@ -17,23 +17,17 @@ namespace {
 void test_null_slicing_tag_logic() {
   INFO("Testing NullSlicingBlocks structure");
   const std::array<std::vector<double>, 2> coords{
-    {{0.0, 0.25, 0.5}, {0.0, 0.5}}};
+      {{0.0, 0.25, 0.5}, {0.0, 0.5}}};
   using Region = domain::creators::RefinementRegion<2>;
   const std::unique_ptr<DomainCreator<2>> lattice =
-    std::make_unique<domain::creators::AlignedLattice<2>>(
-        std::array<std::vector<double>, 2>{
-            {{0., 0.25, 0.5}, {0., 0.5}}},
-        std::array<size_t, 2>{{0, 0}},
-        std::array<size_t, 2>{{3, 2}},
-        std::vector<Region>{
-            Region{{0, 0}, {1, 1}, {0, 1}}},
-        std::vector<Region>{},
-        std::vector<std::array<size_t, 2>>{},
-        std::array<bool, 2>{{false, false}},
-        Options::Context{});
+      std::make_unique<domain::creators::AlignedLattice<2>>(
+          std::array<std::vector<double>, 2>{{{0., 0.25, 0.5}, {0., 0.5}}},
+          std::array<size_t, 2>{{0, 0}}, std::array<size_t, 2>{{3, 2}},
+          std::vector<Region>{Region{{0, 0}, {1, 1}, {0, 1}}},
+          std::vector<Region>{}, std::vector<std::array<size_t, 2>>{},
+          std::array<bool, 2>{{false, false}}, Options::Context{});
 
-  const std::vector<std::string> blocks{
-    "Block(0,0)", "Block(1,0)"};
+  const std::vector<std::string> blocks{"Block(0,0)", "Block(1,0)"};
   const std::vector<size_t> result =
       Tags::NullSlicingBlocks::create_from_options(blocks, lattice);
   const std::vector<size_t> expected{0, 1};
@@ -41,8 +35,8 @@ void test_null_slicing_tag_logic() {
 }
 }  // namespace
 
-SPECTRE_TEST_CASE(
-    "Unit.Elliptic.Systems.ScalarSelfForce.Tags", "[Unit][Elliptic]") {
+SPECTRE_TEST_CASE("Unit.Elliptic.Systems.ScalarSelfForce.Tags",
+                  "[Unit][Elliptic]") {
   TestHelpers::db::test_simple_tag<Tags::MMode>("MMode");
   TestHelpers::db::test_simple_tag<Tags::Alpha>("Alpha");
   TestHelpers::db::test_simple_tag<Tags::Beta>("Beta");
