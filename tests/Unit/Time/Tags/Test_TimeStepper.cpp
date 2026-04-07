aa955d6e2bae8072d4c9d79c1743164b0bee2f79
William Throwe <wtt6@cornell.edu>
2026-04-07 17:38:15 -0400

    Add LtsOrError tag to the DataBox in GTS

--- tests/Unit/Time/Tags/Test_TimeStepper.cpp
+++ tests/Unit/Time/Tags/Test_TimeStepper.cpp
@@ -7,10 +7,13 @@
 #include <string>
 #include <type_traits>
 
+#include "DataStructures/DataBox/DataBox.hpp"
 #include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
 #include "Time/Tags/TimeStepper.hpp"
 #include "Time/TimeSteppers/AdamsBashforth.hpp"
+#include "Time/TimeSteppers/LtsError.hpp"
 #include "Time/TimeSteppers/LtsTimeStepper.hpp"
+#include "Time/TimeSteppers/Rk3HesthavenSsp.hpp"
 #include "Time/TimeSteppers/TimeStepper.hpp"
 #include "Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp"
 #include "Utilities/TMPL.hpp"
@@ -29,16 +32,17 @@ class MoreSpecificFakeTimeStepper : public FakeTimeStepper {
       tmpl::list<MoreSpecificFakeTimeStepper, FakeTimeStepper>;
 };
 
-static_assert(
-    std::is_same_v<
-        time_stepper_ref_tags<FakeTimeStepper>,
-        tmpl::list<Tags::TimeStepperRef<FakeTimeStepper, FakeTimeStepper>>>);
 static_assert(std::is_same_v<
-              time_stepper_ref_tags<MoreSpecificFakeTimeStepper>,
-              tmpl::list<Tags::TimeStepperRef<MoreSpecificFakeTimeStepper,
-                                              MoreSpecificFakeTimeStepper>,
-                         Tags::TimeStepperRef<FakeTimeStepper,
-                                              MoreSpecificFakeTimeStepper>>>);
+              time_stepper_ref_tags<FakeTimeStepper>,
+              tmpl::list<Tags::TimeStepperRef<FakeTimeStepper, FakeTimeStepper>,
+                         Tags::LtsOrError>>);
+static_assert(
+    std::is_same_v<time_stepper_ref_tags<MoreSpecificFakeTimeStepper>,
+                   tmpl::list<Tags::TimeStepperRef<MoreSpecificFakeTimeStepper,
+                                                   MoreSpecificFakeTimeStepper>,
+                              Tags::TimeStepperRef<FakeTimeStepper,
+                                                   MoreSpecificFakeTimeStepper>,
+                              Tags::LtsOrError>>);
 }  // namespace
 
 SPECTRE_TEST_CASE("Unit.Time.Tags.TimeStepper", "[Unit][Time]") {
@@ -47,6 +51,7 @@ SPECTRE_TEST_CASE("Unit.Time.Tags.TimeStepper", "[Unit][Time]") {
   TestHelpers::db::test_simple_tag<Tags::TimeStepper<DummyType>>("TimeStepper");
   TestHelpers::db::test_reference_tag<
       Tags::TimeStepperRef<DummyType, DummyType>>("TimeStepper");
+  TestHelpers::db::test_reference_tag<Tags::LtsOrError>("TimeStepper");
 
   register_classes_with_charm<TimeSteppers::AdamsBashforth>();
   // Check that these are allowed...
@@ -63,4 +68,28 @@ SPECTRE_TEST_CASE("Unit.Time.Tags.TimeStepper", "[Unit][Time]") {
       Catch::Matchers::ContainsSubstring(
           "Variable-order TimeSteppers are only supported in evolutions with "
           "local time-stepping."));
+
+  // Test LtsOrError
+  {
+    auto box =
+        db::create<db::AddSimpleTags<Tags::ConcreteTimeStepper<TimeStepper>>,
+                   time_stepper_ref_tags<TimeStepper>>(
+            static_cast<std::unique_ptr<TimeStepper>>(
+                std::make_unique<TimeSteppers::AdamsBashforth>(3)));
+    const LtsTimeStepper& lts_stepper =
+        db::get<Tags::TimeStepper<LtsTimeStepper>>(box);
+    CHECK(dynamic_cast<const TimeSteppers::LtsError*>(&lts_stepper) == nullptr);
+    CHECK(&lts_stepper == &db::get<Tags::TimeStepper<TimeStepper>>(box));
+  }
+  {
+    auto box =
+        db::create<db::AddSimpleTags<Tags::ConcreteTimeStepper<TimeStepper>>,
+                   time_stepper_ref_tags<TimeStepper>>(
+            static_cast<std::unique_ptr<TimeStepper>>(
+                std::make_unique<TimeSteppers::Rk3HesthavenSsp>()));
+    const LtsTimeStepper& lts_stepper =
+        db::get<Tags::TimeStepper<LtsTimeStepper>>(box);
+    CHECK(dynamic_cast<const TimeSteppers::LtsError*>(&lts_stepper) != nullptr);
+    CHECK(&lts_stepper != &db::get<Tags::TimeStepper<TimeStepper>>(box));
+  }
 }
