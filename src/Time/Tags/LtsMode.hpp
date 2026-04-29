dd027e96660bc782829bc6d1eb81adf9f0373063
William Throwe <wtt6@cornell.edu>
2026-04-29 13:13:43 -0400

    Add LtsModeForced tag to force a particular mode

--- src/Time/Tags/LtsMode.hpp
+++ src/Time/Tags/LtsMode.hpp
@@ -5,6 +5,7 @@
 
 #include "DataStructures/DataBox/Tag.hpp"
 #include "Time/LtsMode.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace Tags {
@@ -23,6 +24,30 @@ struct LtsMode : db::SimpleTag {
   static type create_from_options() {
     return Metavars::local_time_stepping ? ::LtsMode::Conservative
                                          : ::LtsMode::Off;
+  }
+};
+
+/// \ingroup DataBoxTagsGroup
+/// \ingroup TimeGroup
+/// Version of LtsMode that forces a specific value, primarily for
+/// executables without LTS support.
+template <::LtsMode Mode>
+struct LtsModeForced : LtsMode {
+  using base = LtsMode;
+
+  template <typename Metavars>
+  static type create_from_options() {
+    // Check consistency rather than just returning the forced mode so
+    // that other tag creation functions can rely on the parsed value
+    // being correct.
+    const auto lts_mode = Metavars::local_time_stepping
+                              ? ::LtsMode::Conservative
+                              : ::LtsMode::Off;
+    if (lts_mode != Mode) {
+      ERROR_NO_TRACE(
+          "This executable only supports LocalTimeStepping: " << Mode);
+    }
+    return lts_mode;
   }
 };
 }  // namespace Tags
