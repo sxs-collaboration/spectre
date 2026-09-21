51cbb65ec64d6bd04e020fe0c09c49a0d21f9d68
Emma Linscomb <egl49@cornell.edu>
2026-09-21 11:56:18 -0400

    Doc TOV expansion dynamic thresholding

--- src/PointwiseFunctions/AnalyticSolutions/RelativisticEuler/Tov.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/RelativisticEuler/Tov.hpp
@@ -73,11 +73,79 @@ namespace RelativisticEuler::Solutions {
  *
  * Note that Lindblom's paper labels the independent variable as \f$h\f$.
  * However the \f$h\f$ in Lindblom's paper is **not** the specific enthalpy but
- * its logarithm, \f$\ln(h)\f$.
+ * its logarithm, \f$\ln(h)\f$. In the code, this variable is often referred to
+ * as `H`.
  *
  * The ODEs are solved numerically when this class is constructed, and the
  * quantities \f$m(r)/r\f$ and \f$\ln(h)\f$ are interpolated and exposed as
- * member functions. With these quantities the metric can be constructed as:
+ * member functions.
+ *
+ * In numerically solving the TOV equations, there is a singularity at the
+ * beginning of the integration interval at the center of the star
+ * (where u = v = 0). These appear in the denominator of the TOV equations,
+ * so these zeroes cause the derivative to be ill-defined at the center.
+ * To avoid this, the code uses a Taylor series expansion of the solution
+ * to start the integration at a small radius away from the center.
+ *
+ * This code uses the first two terms of the Taylor series expansion in the
+ * initial integration step, then uses higher-order terms to estimate
+ * the radius at which this Taylor series is valid.
+ *
+ * * Here,
+ * \f$H = \ln(h)\f$ is the log of the specific enthalpy,
+ * \f$H_c\f$ is its central value, and \f$e_c\f$ and \f$p_c\f$
+ * are the central energy density and pressure. The coefficient
+ * \f$e_1\f$ is given by
+ *
+ * \f{equation}
+ * e_1 = -\frac{1}{c_s^2}(e_c+p_c).
+ * \f}
+ *
+ * Where \f$c_s^2\f$ is the sound speed squared at the center.
+ *
+ * The near-center derivatives are then expanded in powers of \f$H_c - H\f$
+ * as such:
+ *
+ * \f{align}
+ * \frac{\mathrm{d}u}{\mathrm{d}H} &=
+ * -\frac{3}{2\pi(e_c + 3p_c)}
+ * -2\frac{15(3p_c-e_c)-9e_1}
+ * {20\pi(e_c+3p_c)^2}(H_c-H) + \mathcal{O}((H_c-H)^2)
+ * \\
+ * \frac{\mathrm{d}v}{\mathrm{d}H} &=
+ * -\frac{2e_c}{e_c+3p_c}
+ * -2\frac{5e_c(3p_c-e_c)+3(e_c+6p_c)e_1}
+ * {5(e_c+3p_c)^2}(H_c-H) + \mathcal{O}((H_c-H)^2)
+ * \f}
+ *
+ * where the first-order terms of these expansions are identical to those
+ * in \cite Lindblom1998dp.
+ *
+ * The coefficients through second-order of the Taylor series expansion are
+ * calculated exactly, and the third-order coefficients are estimated using a
+ * 4th-order finite difference approximation of the second derivative of energy
+ * density with respect to pressure. The third-order coefficients are used
+ * to estimate a dynamically calculated threshold for the Taylor series
+ * expansion, which is used in a conditional statement to determine where the
+ * integration of the TOV equations switches from the Taylor series expansion to
+ * the full Lindblom TOV equations.
+ *
+ * The threshold is calculated by solving the following quadratic equation
+ * for \f$\delta H\f$ (and its analogue for \f$\delta H\f$ in the v equation):
+ * \f[
+ * |3u_3|(\Delta H)^2
+ * \leq
+ * \left(|u_1|+|u_2|\Delta H\right)\epsilon_{\mathrm{machine}}
+ * \f]
+ *
+ * If the threshold calculated from the third-order estimate is invalid,
+ * we use a fallback threshold from the second-order expansion coefficients
+ * instead. Then, the numerical integration of the TOV equations proceeds as
+ * normal.
+ *
+ *
+ * With these newly calculated quantities \f$m(r)/r\f$ and \f$\ln(h)\f$,
+ * the metric can be constructed as:
  *
  * \f{equation}
  * \mathrm{d}s^2 = -\alpha^2 \mathrm{d}t^2 + (1 - 2m/r)^{-1} \mathrm{d}r^2
